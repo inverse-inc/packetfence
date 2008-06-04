e598e2feb55fc1c144c4d85f99e918515faaf60c
Regis Balzard <rbalzard@inverse.ca>
2008-06-04 13:18:05 +0000

    - Added new Cisco VoIP phone fingerprints - Changed authorizeMac() signature to add deauthVlan - Bug fix to test if deauthVlan or authVlan is voiceVlan in authorizeMac()
    Monotone-Parent: 00f08d539e11612c536b32d01d1bdc0b52d5b237
    Monotone-Revision: e8ce7939919e9d4d41579a296e587aaf91870710
    
    Monotone-Author: rbalzard@inverse.qc.ca
    Monotone-Date: 2008-06-04T13:18:05

--- pf/lib/pf/SNMP/Cisco/Catalyst_2960.pm
+++ pf/lib/pf/SNMP/Cisco/Catalyst_2960.pm
@@ -222,7 +222,7 @@ sub getSecureMacAddresses {
 }
 
 sub authorizeMAC {
-    my ($this, $ifIndex, $deauthMac, $authMac, $vlan) = @_;
+    my ($this, $ifIndex, $deauthMac, $authMac, $deauthVlan, $authVlan) = @_;
     my $logger = Log::Log4perl::get_logger("pf::SNMP::Cisco::Catalyst_2960");
     my $oid_cpsIfVlanSecureMacAddrRowStatus = '1.3.6.1.4.1.9.9.315.1.2.3.1.5';
 
@@ -235,7 +235,8 @@ sub authorizeMAC {
         return 0;
     }
 
-    if ($vlan == $this->getVoiceVlan($ifIndex)) {
+    my $voiceVlan = $this->getVoiceVlan($ifIndex);
+    if (($deauthVlan == $voiceVlan) || ($authVlan == $voiceVlan)) {
         $logger->error("ERROR: authorizeMAC called with voice VLAN .... this should not have happened ... we won't add an entry to the SecureMacAddrTable");
         return 1;
     }
@@ -247,7 +248,7 @@ sub authorizeMAC {
         foreach my $macPiece (@macArray) {
             $completeOid .= "." . hex($macPiece);
         }
-        $completeOid .= "." . $vlan;
+        $completeOid .= "." . $deauthVlan;
         push @oid_value, ($completeOid, Net::SNMP::INTEGER, 6);
     }
     if ($authMac) {
@@ -256,7 +257,7 @@ sub authorizeMAC {
         foreach my $macPiece (@macArray) {
             $completeOid .= "." . hex($macPiece);
         }
-        $completeOid .= "." . $vlan;
+        $completeOid .= "." . $authVlan;
         push @oid_value, ($completeOid, Net::SNMP::INTEGER, 4);
     }
 
