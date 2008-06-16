fa43f8de0a015fe3c75916df76ceba654dbdc76c
Dominik Gehl <dgehl@inverse.ca>
2008-06-16 16:51:36 +0000

    more database optimizations http://www.packetfence.org/mantis/view.php?id=287
    Monotone-Parent: 834ca5f0c6445a1ae1cb460dc5aa1445e14c3b8b
    Monotone-Revision: f7557b4d02c17bfda7847deda3be06edc9d56b6d
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2008-06-16T16:51:36

--- pf/db/upgrade-1.7.0-1.7.1.sql
+++ pf/db/upgrade-1.7.0-1.7.1.sql
@@ -8,3 +8,35 @@ ALTER TABLE locationlog
   DROP KEY `locationlog_view_open_switchport`,
   DROP KEY `start_time`;
 
+ALTER TABLE node
+  DROP KEY `node_lookup_person`,
+  ADD KEY `node_status` (`status`, `unregdate`),
+  ADD KEY `node_dhcpfingerprint` (`dhcp_fingerprint`);
+
+ALTER TABLE os_mapping
+  ADD PRIMARY KEY (`os_type`, `os_class`),
+  DROP KEY `os_mapping_view`;
+
+ALTER TABLE person
+  DROP KEY `person_view`;
+
+ALTER TABLE violation
+  DROP KEY `violation_exist`;
+
+ALTER TABLE iplog
+  ADD KEY `ip_view_open` (`ip`, `end_time`),
+  ADD KEY `mac_view_open` (`mac`, `end_time`);
+
+ALTER TABLE iplog
+  DROP KEY `iplog_view_open`,
+  DROP KEY `ip`;
+
+ALTER TABLE os_type
+  DROP KEY `os_id_key`,
+  DROP KEY `os_type_view`;
+
+ALTER TABLE os_class
+  DROP KEY `os_class_view`;
+
+ALTER TABLE dhcp_fingerprint
+  DROP KEY `fingerprint_view`;
