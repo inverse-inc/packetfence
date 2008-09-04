32b0c179ceb8f0063b9b111ffc89d0c20028af5c
Regis Balzard <rbalzard@inverse.ca>
2008-09-04 16:55:48 +0000

    Added module for Procurve 4100 series
    Monotone-Parent: 2e48410eb681f974b2e00527578e75feac786891
    Monotone-Revision: bde52d428dae16d526fac015c2f10898adf6fdce
    
    Monotone-Author: rbalzard@inverse.qc.ca
    Monotone-Date: 2008-09-04T16:55:48

---
+++ pf/lib/pf/SNMP/HP/Procurve_4100.pm
@@ -0,0 +1,37 @@
+#
+# Copyright 2007-2008 Inverse groupe conseil
+#
+# See the enclosed file COPYING for license information (GPL).
+# If you did not receive this file, see
+# http://www.fsf.org/licensing/licenses/gpl.html
+#
+
+package pf::SNMP::HP::Procurve_4100;
+
+=head1 NAME
+
+pf::SNMP::HP::Procurve_4100 - Object oriented module to access SNMP enabled HP Procurve 4100 switches
+
+
+=head1 SYNOPSIS
+
+The pf::SNMP::HP::Procurve_4100 module implements an object 
+oriented interface to access SNMP enabled HP Procurve 4100 switches.
+
+=cut
+
+use strict;
+use warnings;
+use diagnostics;
+use Log::Log4perl;
+use Net::SNMP;
+
+use lib '/usr/local/pf/lib';
+
+use base ('pf::SNMP::HP::Procurve_2500');
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
