071b896973bcbb8eea0a4fc10367f79838b63896
Dominik Gehl <dgehl@inverse.ca>
2008-09-30 12:22:23 +0000

    support for Aironet 1250 (http://www.packetfence.org/mantis/view.php?id=397)
    Monotone-Parent: 344d2d6ce314a25cec7375fd1f8093052cc70625
    Monotone-Revision: 93af66f092c85580f1a45779d52b38bf4023ecb5
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2008-09-30T12:22:23

---
+++ pf/lib/pf/SNMP/Cisco/Aironet_1250.pm
@@ -0,0 +1,35 @@
+#
+# Copyright 2007-2008 Inverse groupe conseil
+#
+# See the enclosed file COPYING for license information (GPL).
+# If you did not receive this file, see
+# http://www.fsf.org/licensing/licenses/gpl.html
+#
+
+package pf::SNMP::Cisco::Aironet_1250;
+
+=head1 NAME
+
+pf::SNMP::Cisco::Aironet_1250 - Object oriented module to access SNMP enabled Cisco Aironet 1250 APs
+
+
+=head1 SYNOPSIS
+
+The pf::SNMP::Cisco::Aironet_1250 module implements an object oriented interface
+to access SNMP enabled Cisco Aironet_1250 APs.
+
+This modules extends pf::SNMP::Cisco::Aironet
+
+=cut
+
+use strict;
+use warnings;
+use diagnostics;
+use Log::Log4perl;
+use Net::SNMP;
+
+use lib '/usr/local/pf/lib';
+
+use base ('pf::SNMP::Cisco::Aironet');
+
+1;
