ef934f577cee7b0c4992212463c197bc3501f489
Dominik Gehl <dgehl@inverse.ca>
2008-11-11 19:44:47 +0000

    support for Cisco Controller 4400
    Monotone-Parent: ff2c066a93ed59120d21259feede31555cbd3cc9
    Monotone-Revision: 81c491af76e4e508890b51d3dace3630e0144041
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2008-11-11T19:44:47

---
+++ pf/lib/pf/SNMP/Cisco/Controller_4400_4_2_130.pm
@@ -0,0 +1,152 @@
+#
+# Copyright 2007-2008 Inverse groupe conseil
+#
+# See the enclosed file COPYING for license information (GPL).
+# If you did not receive this file, see
+# http://www.fsf.org/licensing/licenses/gpl.html
+#
+
+package pf::SNMP::Cisco::Controller_4400_4_2_130;
+
+=head1 NAME
+
+pf::SNMP::Cisco::Controller_4400_4_2_130 - Object oriented module to access SNMP enabled Cisco Controller 4400 with IOS version 4.2.130
+
+
+=head1 SYNOPSIS
+
+The pf::SNMP::Cisco::Controller_4400_4_2_130 module implements an object oriented interface
+to access SNMP enabled Cisco Controller 4400 with IOS version 4.2.130
+
+=cut
+
+use strict;
+use warnings;
+use diagnostics;
+
+use base ('pf::SNMP::Cisco');
+use Log::Log4perl;
+use Carp;
+use Net::Telnet;
+use Net::SNMP;
+use Data::Dumper;
+
+sub deauthenticateMac {
+    my ($this, $mac) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+
+    #format MAC
+    if (length($mac) == 17) {
+        $mac =~ s/://g;
+        $mac = substr($mac,0,4) . "." . substr($mac,4,4) . "." . substr($mac,8,4);
+    } else {
+        $logger->error("ERROR: MAC format is incorrect ($mac). Should be xx:xx:xx:xx:xx:xx");
+        return 1;
+    }
+    
+    my $session;
+    eval {
+        $session = Net::Telnet->new(Host => $this->{_ip}, Timeout=>5, Prompt => '/[\$%#>]$/', Dump_Log => '/tmp/dgl.txt');
+        $session->waitfor('/User: /');
+        $session->put($this->{_telnetUser} . "\n");
+        $session->waitfor('/Password:/');
+        $session->put($this->{_telnetPwd} . "\n");
+        $session->waitfor($session->prompt);
+    };
+
+    if ($@) {
+        $logger->error("ERROR: Can not connect to access point $this->{'_ip'} using telnet");
+        return 1;
+    }
+    #if (! $session->enable($this->{_telnetEnablePwd})) {
+    #    $logger->error("ERROR: Can not 'enable' telnet connection");
+    #    return 1;
+    #}
+    $logger->info("Deauthenticating mac $mac");
+    $session->cmd("config");
+    $session->cmd("client deauthenticate $mac");
+    $session->close();
+}
+
+sub isLearntTrapsEnabled {
+    my ($this, $ifIndex) = @_;
+    return (0==1);
+}
+
+sub setLearntTrapsEnabled {
+    my ($this, $ifIndex, $trueFalse) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return -1;
+}
+
+sub isRemovedTrapsEnabled {
+    my ($this, $ifIndex) = @_;
+    return (0==1);
+}
+
+sub setRemovedTrapsEnabled {
+    my ($this, $ifIndex, $trueFalse) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return -1;
+}
+
+sub getVmVlanType {
+    my ($this, $ifIndex) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return -1;
+}
+
+sub setVmVlanType {
+    my ($this, $ifIndex, $type) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return -1;
+}
+
+sub isTrunkPort {
+    my ($this, $ifIndex) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return -1;
+}
+
+sub getVlans {
+    my ($this) = @_;
+    my $vlans = {};
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return $vlans;
+}
+
+sub isDefinedVlan {
+    my ($this, $vlan) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    $logger->error("function is NOT implemented");
+    return 0;
+}
+
+sub getPhonesDPAtIfIndex {
+    my ($this, $ifIndex) = @_;
+    my $logger = Log::Log4perl::get_logger(ref($this));
+    my @phones = ();
+    if (! $this->isVoIPEnabled()) {
+        $logger->debug("VoIP not enabled on switch " . $this->{_ip} . ". getPhonesDPAtIfIndex will return empty list.");
+        return @phones;
+    }
+    $logger->debug("no DP is available on Controller 4400");
+    return @phones;
+}
+
+sub isVoIPEnabled {
+    my ($this) = @_;
+    return 0;
+}
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
