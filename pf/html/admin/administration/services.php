c936a63379027786f1c41de1ed1951c193434c2f
Dominik Gehl <dgehl@inverse.ca>
2009-01-13 17:02:54 +0000

    usability enhancements to administration/services.php (http://www.packetfence.org/mantis/view.php?id=524)
    Monotone-Parent: 274df021313d9d77f63c5c3b0d8b8280ca7d8d61
    Monotone-Revision: 3179a9b67b305ccafb5740013f1567b84d736fef
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-01-13T17:02:54

--- pf/html/admin/administration/services.php
+++ pf/html/admin/administration/services.php
@@ -35,31 +35,59 @@ function print_status_table(){
   global $current_top;
   global $current_sub;
 
+  $ordered_service_list=array('httpd', 'pfdetect', 'pfdhcplistener', 'pfmon', 'pfredirect', 'line', 'snmptrapd', 'pfsetvlan', 'line', 'snort');
   $data=array_slice(PFCMD('service pf status'), 1); // sliced to get rid of headers
   print "<table width=75% align=center>";
-  print "<tr><td align=center><b>Service (status)</b></td><td colspan=2 align=center><b>Action</b></td></tr>";
+  print "<tr><td><b>Service</b></td><td><b>Expected Status</b></td><td><b>Actual Status</b></td><td colspan=3 align=center><b>Action</b></td></tr>";
   
 
-  foreach($data as $line){
-    $services=preg_split("/\|/",$line);
+  foreach ($ordered_service_list as $current_service) {
+    if ($current_service == 'line') {
+      print "<tr><td colspan=6><hr></td></tr>\n";
+    } else {
+      foreach($data as $line){
+        $services=preg_split("/\|/",$line);
 
-    print "<tr>";
-    if($services[2] == 0){
-      print "<td><font color=red>$services[0] (stopped)</font></td>";
-      print "<td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=start><img src='images/start.png' title='Start $services[0]' border=0></a></td>";
+        if ($services[0] == $current_service) {
+          print "<tr>\n";
+          print "  <td";
+          if ($services[1] == 1) {
+            if ($services[2] == 0) {
+              print " style=\"color:red\"";
+            } else {
+              print " style=\"color:green\"";
+            }
+          }
+          print ">$services[0]</td>\n";
+          print "<td>" . (($services[1] == 0) ? 'Stopped' : 'Running') . "</td>\n";
+          print "<td>" . (($services[2] == 0) ? 'Stopped' : 'Running') . "</td>\n";
+          if (($services[1] == 1) && ($services[2] == 0)) {
+            print "<td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=start><img src='images/start.png' title='Start $services[0]' border=0></a></td>";
+          } else {
+            print "<td>&nbsp;</td>\n";
+          }
+          if (($services[1] == 1) && ($services[2] != 0)) {
+            print "<td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=stop><img src='images/stop.png' title='Stop $services[0]' border=0></a></td>\n";
+            print "<td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=restart><img src='images/restart.png' title='Restart $services[0]' border=0></a></td>\n";
+          } else {
+            print "<td>&nbsp;</td>\n";
+            print "<td>&nbsp;</td>\n";
+          }
+          print "</tr>\n";    
+        }
+      }
     }
-    else{
-      print "<td><font color=green>$services[0] (running)</font></td>";
-      print "<td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=stop><img src='images/stop.png' title='Stop $services[0]' border=0></a></td><td align=center><a href=$current_top/$current_sub.php?service=$services[0]&action=restart><img src='images/restart.png' title='Restart $services[0]' border=0></a></td>";
-    }
-    print "</tr>";    
   }
   
-  print "<tr>";
-  print "<td>All Services</td>";
-  print "<td align=center><a href=$current_top/$current_sub.php?service=pf&action=stop><img src='images/stop.png' title='Stop All' border=0></a></td><td align=center><a 
-href=$current_top/$current_sub.php?service=pf&action=start><img src='images/start.png' title='Start All' border=0></a></td><td align=center><a href=$current_top/$current_sub.php?service=pf&action=restart><img src='images/restart.png' title='Restart All' border=0></a></td>";
-
+  print "<tr><td colspan=6><hr></td></tr>\n";
+  print "<tr>\n";
+  print "<td>All Services</td>\n";
+  print "<td>&nbsp;</td>\n";
+  print "<td>&nbsp;</td>\n";
+  print "<td align=center><a href=$current_top/$current_sub.php?service=pf&action=start><img src='images/start.png' title='Start All' border=0></a></td>\n";
+  print "<td align=center><a href=$current_top/$current_sub.php?service=pf&action=stop><img src='images/stop.png' title='Stop All' border=0></a></td>\n";
+  print "<td align=center><a href=$current_top/$current_sub.php?service=pf&action=restart><img src='images/restart.png' title='Restart All' border=0></a></td>";
+  print "</tr>\n";
   print "</table>";
 
 }
