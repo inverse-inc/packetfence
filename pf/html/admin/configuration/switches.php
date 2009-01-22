bddc6392e180d19a9ee962b77f6b2193c36d2bee
Dominik Gehl <dgehl@inverse.ca>
2009-01-22 20:20:03 +0000

    Added view on switches.conf to web admin GUI
    Monotone-Parent: e9826bae4b8bb6fcb68d4313d8d446cf4d396c8f
    Monotone-Revision: e9b7d2b91935c3014abd0a64c11655ccfe53d043
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-01-22T20:20:03

---
+++ pf/html/admin/configuration/switches.php
@@ -0,0 +1,21 @@
+<?
+  require_once('../common.php');
+
+  $current_top="configuration";
+  $current_sub="switches";
+
+  $view_item = set_default($_REQUEST['view_item'], 'all');
+
+  $my_table=new table("switchconfig view $view_item");
+  $is_printable=true;
+
+  $my_table->set_page_num(set_default($_REQUEST['page_num'],1));
+  $my_table->set_per_page(set_default($_REQUEST['per_page'],25));
+
+  include_once('../header.php');
+
+  $my_table->tableprint(false);
+
+  include_once('../footer.php');
+
+?>
