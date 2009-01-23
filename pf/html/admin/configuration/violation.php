b2a5c1f7db2d111c67d8afa83591ef2780ca47ac
Dominik Gehl <dgehl@inverse.ca>
2009-01-23 12:58:38 +0000

    hide some less important columns in violation config view; this way the table fits better on screen
    Monotone-Parent: 084fc30b79ba878017cf8c42a138587af067059b
    Monotone-Revision: af8e1f99a6a926141a0766d803a4294fb87dea99
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-01-23T12:58:38

--- pf/html/admin/configuration/violation.php
+++ pf/html/admin/configuration/violation.php
@@ -8,6 +8,7 @@
 
   $my_table=new table("class view $view_item");
   $my_table->set_linkable(array( array('url', 'configuration/redirector.php')));
+  $my_table->set_hideable(array('max_enable_url', 'redirect_url', 'grace', 'priority'));
   $is_printable=true;
 
   $my_table->set_page_num(set_default($_REQUEST['page_num'],1));
