fd7b38f5759d8506ebbeaba513901373931831c8
Dominik Gehl <dgehl@inverse.ca>
2009-01-23 13:05:20 +0000

    fixed link from class/violation.php to configuration/violation.php
    Monotone-Parent: af8e1f99a6a926141a0766d803a4294fb87dea99
    Monotone-Revision: 2b826f7f5f0724d4a1c7e044ead01c87087a0113
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-01-23T13:05:20

--- pf/html/admin/configuration/trigger.php
+++ pf/html/admin/configuration/trigger.php
@@ -8,7 +8,7 @@
   $view_item = set_default($_REQUEST['view_item'], 'all');
 
   $my_table=new table("trigger view $view_item");
-  $my_table->set_linkable(array( array('vid', 'class/violation.php')));
+  $my_table->set_linkable(array( array('vid', 'configuration/violation.php')));
 
   $my_table->set_page_num(set_default($_REQUEST['page_num'],1));
   $my_table->set_per_page(set_default($_REQUEST['per_page'],100));
