a0b2ccb8303c6a4c06836bbd042c57b9e93c2b30
Dominik Gehl <dgehl@inverse.ca>
2009-01-27 15:03:12 +0000

    added button_text to hidden columns
    Monotone-Parent: 9c27022b65ccbfece8a859bb19d93b73e70353b4
    Monotone-Revision: b4cf980ad353da82360b0f62efb92174ee6462d7
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-01-27T15:03:12

--- pf/html/admin/configuration/violation.php
+++ pf/html/admin/configuration/violation.php
@@ -8,7 +8,7 @@
 
   $my_table=new table("class view $view_item");
   $my_table->set_linkable(array( array('url', 'configuration/instructions.php')));
-  $my_table->set_hideable(array('max_enable_url', 'redirect_url', 'grace', 'priority'));
+  $my_table->set_hideable(array('max_enable_url', 'redirect_url', 'grace', 'priority', 'button_text'));
   $is_printable=true;
 
   $my_table->set_page_num(set_default($_REQUEST['page_num'],1));
