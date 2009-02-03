edea769232de1d97eb7df0bd6c6f43394460d51e
Dominik Gehl <dgehl@inverse.ca>
2009-02-03 02:19:46 +0000

    adjusted web admin gui configuration violation to new violationconfig call
    Monotone-Parent: ae7138e44a15b11de89b258205b63e2efb423b6b
    Monotone-Revision: 368a6571efb0a3c65788d81649ad6fd5d9a8f252
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-02-03T02:19:46

--- pf/html/admin/configuration/violation.php
+++ pf/html/admin/configuration/violation.php
@@ -6,9 +6,9 @@
 
   $view_item = set_default($_REQUEST['view_item'], 'all');
 
-  $my_table=new table("class view $view_item");
+  $my_table=new table("violationconfig get $view_item");
   $my_table->set_linkable(array( array('url', 'configuration/instructions.php')));
-  $my_table->set_hideable(array('max_enable_url', 'redirect_url', 'grace', 'priority', 'button_text'));
+  $my_table->set_hideable(array('grace', 'priority', 'button_text'));
   $is_printable=true;
 
   $my_table->set_page_num(set_default($_REQUEST['page_num'],1));
