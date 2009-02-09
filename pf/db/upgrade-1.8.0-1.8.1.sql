51d350000e6ca0c63abbb228454a423bde42884e
Regis Balzard <rbalzard@inverse.ca>
2009-02-09 21:12:13 +0000

    Added missing ;
    Monotone-Parent: 049d01f1ce54ad737042298683f80cecffc6304e
    Monotone-Revision: 517f590bd38757c7bd8fca4fdaf47683170da058
    
    Monotone-Author: rbalzard@inverse.qc.ca
    Monotone-Date: 2009-02-09T21:12:13

--- pf/db/upgrade-1.8.0-1.8.1.sql
+++ pf/db/upgrade-1.8.0-1.8.1.sql
@@ -2,4 +2,4 @@ CREATE TABLE `configfile` (
   `filename` varchar(255) NOT NULL,
   `filecontent` text NOT NULL,
   `lastmodified` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
