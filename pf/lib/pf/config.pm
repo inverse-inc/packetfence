b66be613589716126cc26d249fe1896dd13d4ac7
Dominik Gehl <dgehl@inverse.ca>
2009-02-11 20:42:27 +0000

    fixed install_dir
    Monotone-Parent: 7ddc260f6c97185bb9e96c1372240e18863dde4c
    Monotone-Revision: 2223bc1df6d849c36c13d8727720248399d39516
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-02-11T20:42:27

--- pf/lib/pf/config.pm
+++ pf/lib/pf/config.pm
@@ -37,7 +37,7 @@ BEGIN {
 
 $thread=0;
 
-$install_dir = main::INSTALL_DIR;
+$install_dir = '/usr/local/pf';
 $bin_dir = File::Spec->catdir($install_dir, "bin");
 $conf_dir = File::Spec->catdir($install_dir, "conf");
 $lib_dir = File::Spec->catdir($install_dir, "lib");
