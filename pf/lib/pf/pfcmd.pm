82d3a5751c58d04ab65314d2c690b81fd3163973
Dominik Gehl <dgehl@inverse.ca>
2009-03-06 19:42:51 +0000

    converted split into regular expression matches
    Monotone-Parent: f3007ecd105d33820b50863555ad707a174201ec
    Monotone-Revision: 25e646e38662cf3fc92dae4dba824f08469d46d4
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-03-06T19:42:51

--- pf/lib/pf/pfcmd.pm
+++ pf/lib/pf/pfcmd.pm
@@ -14,174 +14,115 @@ use warnings;
 use diagnostics;
 
 use Log::Log4perl;
+use Regexp::Common qw(net);
 
 sub parseCommandLine {
     my ($commandLine) = @_;
     my $logger = Log::Log4perl::get_logger("pf::pfcmd");
     $logger->debug("starting to parse '$commandLine'");
 
-    my @arguments = split( / +/, $commandLine );
+    $commandLine =~ s/\s+$//;
+    my ($main, $params) = split( / +/, $commandLine, 2 );
+    #make sure params contains at least an empty string
+    $params = '' if (! defined($params));
 
-    my %cmd;
-
-    if ( ( scalar(@arguments) == 1 ) && ( $arguments[0] eq 'version' ) ) {
-        $cmd{'command'}[0] = 'version';
+    my %regexp = (
+        'class'           => qr{ ^ (view) \s+ ( all | \d+ ) $ }xms,
+        'configfiles'     => qr{ ^ ( push | pull ) $ }xms,
+        'help'            => qr{ ^ ( [a-z]* ) $ }xms,
+        'interfaceconfig' => qr{ ^ ( get | delete )
+                                   \s+
+                                   ( all | [a-z0-9\.\:]+ )
+                                 $  }xms,
+        'lookup'          => qr{ ^ ( person | node ) 
+                                   \s+
+                                   ( [0-9a-zA-Z_\-\.\:]+ )
+                                 $  }xms,
+        'networkconfig'   => qr/ ^ ( get | delete )
+                                   \s+
+                                   ( all | $RE{net}{IPv4} )
+                                 $  /xms,
+        'nodecategory'    => qr{ ^ (view) \s+ (\w+) $  }xms,
+        'reload'          => qr{ ^ ( fingerprints | violations ) $  }xms,
+        'report'          => qr{ ^ (?: #for grouping only
+                                     ( active | inactive | openviolations 
+                                       | os | osclass | registered | statics 
+                                       | unknownprints | unregistered )
+                                     |
+                                     (?: #for grouping only
+                                       ( openviolations | os | osclass 
+                                         | registered | statics
+                                         | unknownprints | unregistered 
+                                       )
+                                       \s+
+                                       ( all | active )
+                                     )
+                                   )
+                                 $  }xms,
+        'service'         => qr{ ^ ( dhcpd | httpd | named | pfdetect 
+                                     | pf | pfdhcplistener | pfmon 
+                                     | pfredirect | pfsetvlan | snmptrapd 
+                                     | snort )
+                                   \s+
+                                   ( restart | start | status | stop
+                                     | watch )
+                                 $  }xms,
+        'switchconfig'    => qr/ ^ ( get | delete ) 
+                                   \s+
+                                   ( all | default | $RE{net}{IPv4} )
+                                 $  /xms,
+        'switchlocation'  => qr/ ^ ( view )
+                                   \s+
+                                   ($RE{net}{IPv4})
+                                   \s+
+                                   (\d+)
+                                 $  /xms,
+        'update'          => qr{ ^ ( fingerprints | oui ) $  }xms,
+        'version'         => qr{ ^ $ }xms,
+        'violationconfig' => qr{ ^ ( get | delete )
+                                   \s+
+                                   ( all | defaults | \d+ )
+                                 $  }xms,
+    );
+    if ( exists($regexp{$main}) ) {
+        my %cmd;
+        if ($params =~ $regexp{$main}) {
+            $cmd{'command'}[0] = $main;
+            push @{$cmd{'command'}}, $1 if ($1);
+            push @{$cmd{'command'}}, $2 if ($2);
+            push @{$cmd{'command'}}, $3 if ($3);
+        } else {
+            if ($main =~ m{ ^ (?:
+                            node | person | interfaceconfig | networkconfig
+                            | switchconfig | violationconfig | violation
+                            | trigger | ui | fingerprint | config 
+                            | manage | graph | schedule | traplog |
+                            | locationhistoryswitch | locationhistorymac
+                            | ifoctetshistoryswitch | ifoctetshistorymac
+                            | ifoctetshistoryuser | ipmachistory
+                            | history
+                              ) $ }xms ) {
+                return parseWithGrammar($commandLine);
+            }
+            @{$cmd{'command'}} = ('help', $main);
+        }
+        use Data::Dumper;
+        print Dumper(%cmd);
         return %cmd;
     }
     
-    if ( ( scalar(@arguments) == 2 )
-        && ( $arguments[0] eq 'configfiles' )
-        && ( $arguments[1] =~ /^(push|pull)$/ ) ) {
-        $cmd{command}[0] = 'configfiles';
-        $cmd{command}[1] = $arguments[1];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 2 ) && ( $arguments[0] eq 'help' ) ) {
-        $cmd{command}[0] = $arguments[0];
-        $cmd{command}[1] = $arguments[1];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 2 )
-        && ( $arguments[0] eq 'reload' )
-        && ( $arguments[1] =~ /^(fingerprints|violations)$/ ) ) {
-        $cmd{command}[0] = 'reload';
-        $cmd{command}[1] = $arguments[1];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 2 )
-        && ( $arguments[0] eq 'report' )
-        && ( $arguments[1]
-            =~ /^(inactive|active|unregistered|registered|osclass|os|unknownprints|openviolations|statics)$/
-           )
-        ) {
-        $cmd{command}[0] = $arguments[0];
-        $cmd{command}[1] = $arguments[1];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 2 )
-        && ( $arguments[0] eq 'update' )
-        && ( $arguments[1] =~ /^(fingerprints|oui)$/ ) ) {
-        $cmd{command}[0] = 'update';
-        $cmd{command}[1] = $arguments[1];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'class' )
-        && ( $arguments[1] eq 'view' )
-        && ( $arguments[2] =~ /^(all|\d+)$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'lookup' )
-        && ( $arguments[1] =~ /^(person|node)$/ )
-        && ( $arguments[2] =~ /^[\/,0-9a-zA-Z_\*\.\-\:_\;\@\ ]*$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'nodecategory' )
-        && ( $arguments[1] eq 'view' )
-        && ( $arguments[2] =~ /^\w+$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'report' )
-        && ( $arguments[1]
-            =~ /^(unregistered|registered|osclass|os|unknownprints|openviolations|statics)$/
-           )
-        && ( $arguments[2] =~ /^(all|active)$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'service' )
-        && ( $arguments[1]
-            =~ /^(named|dhcpd|pfmon|pfdhcplistener|pfdetect|pfredirect|snort|httpd|pfsetvlan|snmptrapd|pf)$/
-           )
-        && ( $arguments[2] =~ /^(stop|start|restart|status|watch)$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'interfaceconfig' )
-        && ( $arguments[1] =~ /^(get|delete)$/ )
-        && ( $arguments[2] =~ /^(all|[^ ]+)$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'networkconfig' )
-        && ( $arguments[1] =~ /^(get|delete)$/ )
-        && ( $arguments[2] =~ /^(all|(\d{1,3}\.){3}\d{1,3})$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'switchconfig' )
-        && ( $arguments[1] =~ /^(get|delete)$/ )
-        && ( $arguments[2] =~ /^(all|default|(\d{1,3}\.){3}\d{1,3})$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 3 )
-        && ( $arguments[0] eq 'violationconfig' )
-        && ( $arguments[1] =~ /^(get|delete)$/ )
-        && ( $arguments[2] =~ /^(all|defaults|(\d+))$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        return %cmd;
-    } 
-    
-    if ( ( scalar(@arguments) == 4 )
-        && ( $arguments[0] eq 'switchlocation' )
-        && ( $arguments[1] eq 'view' )
-        && ( $arguments[2] =~ /^(\d{1,3}\.){3}\d{1,3}$/ )
-        && ( $arguments[3] =~ /^\d+$/ ) ) {
-        $cmd{'command'}[0] = $arguments[0];
-        $cmd{'command'}[1] = $arguments[1];
-        $cmd{'command'}[2] = $arguments[2];
-        $cmd{'command'}[3] = $arguments[3];
-        return %cmd;
-    } 
+    return parseWithGrammar($commandLine);
+}
 
-    # precompiled grammar (2.6x increase in speed)
+
+sub parseWithGrammar {
+    my ($commandLine) = @_;
     require pf::pfcmd::pfcmd_pregrammar;
     import pf::pfcmd::pfcmd_pregrammar;
     my $parser = pfcmd_pregrammar->new();
 
     my $result = $parser->start($commandLine);
+    my %cmd;
     $cmd{'grammar'} = ( defined($result) ? 1 : 0 );
     return %cmd;
 }
