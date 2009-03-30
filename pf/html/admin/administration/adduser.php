99aaadea9cbc25e65f4e08d699240ad31f5a4172
Dominik Gehl <dgehl@inverse.ca>
2009-03-30 18:11:01 +0000

    fixed error messages layout in adduser.php (http://www.packetfence.org/mantis/view.php?id=650)
    Monotone-Parent: 99651632723770b88f73f37c2d21fcd6d3aab491
    Monotone-Revision: e9ee5dc2f5c106d52c0073e954ed16d7a4a34c66
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-03-30T18:11:01

--- pf/html/admin/administration/adduser.php
+++ pf/html/admin/administration/adduser.php
@@ -13,32 +13,44 @@
 
   if($_POST['username'] && $_POST['password1'] && $_POST['password2']){
      if(preg_match("/(;|\&|\"|\')/", $_POST['username'])){
+       print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
        print "Error: Username contains invalid characters";
+       print "</b></div>\n";
        exit;
      }
      if(preg_match("/(;|\'|\"|\&)/", $_POST['password1'])){
+       print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
        print "Error: password contains invalid characters";
+       print "</b></div>\n";
        exit;
      }
 
 
-     if($_POST['password1'] != $_POST['password2'])
-       print "Passwords do not match!";
-     else{
+     if($_POST['password1'] != $_POST['password2']) {
+       print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
+       print "Error: Passwords do not match!";
+       print "</b></div>\n";
+     } else{
 	$filename = dirname(dirname($_SERVER['DOCUMENT_ROOT'])) . "/conf/users/" . $_POST['username'];
         if(file_exists($filename)){
+       print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
 	  print "Error: User $_POST[username] already exists!";
+       print "</b></div>\n";
 	}
 	else{
   	  exec("/usr/bin/htpasswd -b " . dirname(dirname($_SERVER['DOCUMENT_ROOT'])) . "/conf/admin.conf $_POST[username] $_POST[password1]");
           $defaults = array('font-size' => 'medium', 'homepage' => 'status/dashboard.php');
   	  if (!$handle = fopen($filename, 'w+')) {
-            echo "Cannot open file ($filename)";
+            print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
+            echo "Error: Cannot open file ($filename)";
+            print "</b></div>\n";
             exit;
           }
 
           if (fwrite($handle, serialize($defaults)) === FALSE) {
-            echo "Cannot write to file ($filename)";
+            print "<div id='error' style='text-align:left;padding:10px;background:#FF7575;'><b>";
+            echo "Error: Cannot write to file ($filename)";
+            print "</b></div>\n";
             exit;
           }
           fclose($handle);        
