98a9eb42bcf4210d50919afaa6db91cb4f329f89
Dominik Gehl <dgehl@inverse.ca>
2009-04-02 15:14:58 +0000

    fixed issues in pfcmd schedule call (http://www.packetfence.org/mantis/view.php?id=670)
    Monotone-Parent: e83efd6766c77bdb6a63f5102cc28919cf9028f4
    Monotone-Revision: 618937377e31763cc0ce057d5bfe58537c134651
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-04-02T15:14:58

--- pf/lib/pf/pfcmd/pfcmd.pm
+++ pf/lib/pf/pfcmd/pfcmd.pm
@@ -44,7 +44,7 @@ $grammar = q {
 
    violation_options : 'add' violation_edit_options | 'edit' /\d+/ violation_edit_options | 'delete' /\d+/ 
 
-   schedule_options : 'now' host_range edit_options(?) | 'add' host_range edit_options | 'edit' /\d+/ edit_options |
+   schedule_options : 'now' host_range edit_options(?) | 'add' host_range edit_options | 'edit' /\d+/ edit_options
 
    mac : 'all' | macaddr
 
