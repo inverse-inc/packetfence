70a55b12c4c0143cc5e472f15354e6006c7295f7
Dominik Gehl <dgehl@inverse.ca>
2009-06-12 19:33:25 +0000

    added perldoc to mrtg-wrapperl.pl
    Monotone-Parent: 897112803fbb91ad35d0a483fe349cd8da64daca
    Monotone-Revision: 21257373768743c348a45a95debab826e5bd8cc5
    
    Monotone-Author: dgehl@inverse.ca
    Monotone-Date: 2009-06-12T19:33:25

--- pf/addons/mrtg/mrtg-wrapper.pl
+++ pf/addons/mrtg/mrtg-wrapper.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+mrtg-wrapper.pl - use of MRTG to provide long-term graphing of data
+
+=head1 SYNOPSIS
+
+./mrtg-wrapperl.pl -r <report_type> <options>
+
+  Report_type:
+    - registration
+    - activity
+    - violations
+
+  Options:
+    -h <hostname>
+    -p <port>
+    -u <user>
+    -s <password>
+
+=cut
+
 #
 # This wrapper allows for plug-and-play use of MRTG to provide long-term graphing of data
 #
@@ -93,3 +114,23 @@ Usage:  $prog -r <$reports> [OPTIONS]
 EOF
   exit;
 }
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
