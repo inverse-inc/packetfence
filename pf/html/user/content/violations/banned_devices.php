24ff9536a66fc204cffc90e056f3490479fedd1c
Olivier Bilodeau <obilodeau@inverse.ca>
2009-07-10 20:41:01 +0000

    MAC address violation config template along with a violation page for the captive portal
    Monotone-Parent: 7128e3c709366cd5882c9a1d442ad5a1906f0d2c
    Monotone-Revision: 887d08017182e5951b4872de5f79f44be1f89684
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2009-07-10T20:41:01

---
+++ pf/html/user/content/violations/banned_devices.php
@@ -0,0 +1,7 @@
+<?
+
+$description_header = 'Unauthorized device';
+
+$description_text = 'You have been detected using a device that has been explicitly disallowed by your network administrator.';
+
+?>
