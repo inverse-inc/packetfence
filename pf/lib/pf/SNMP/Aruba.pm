2baf8d8c1645ccff00c4a569d821bbe68a69a2fb
Regis Balzard <rbalzard@inverse.ca>
2009-07-16 17:21:09 +0000

    Moved code from Controler_200.pm to Aruba.pm since there is only 1 OS, the code shoud be the same for all controlers
    Monotone-Parent: 49d44626dbdf07f3d279fc150c6e7ab17f1fa025
    Monotone-Revision: 2d4272006ae26c7d9a89ec4e1aed25b016fcf06c
    
    Monotone-Author: rbalzard@inverse.qc.ca
    Monotone-Date: 2009-07-16T17:21:09

--- pf/lib/pf/SNMP/Aruba.pm
+++ pf/lib/pf/SNMP/Aruba.pm
@@ -18,7 +18,7 @@ use diagnostics;
 use base ('pf::SNMP');
 use POSIX;
 use Log::Log4perl;
-use Data::Dumper;
+use Net::Telnet;
 
 sub getVersion {
     my ($this)       = @_;
@@ -57,6 +57,97 @@ sub parseTrap {
         $trapHashRef->{'trapType'} = 'unknown';
     }
     return $trapHashRef;
+}
+
+sub deauthenticateMac {
+    my ( $this, $mac ) = @_;
+    my $logger = Log::Log4perl::get_logger( ref($this) );
+    my $OID_nUserApBSSID = '1.3.6.1.4.1.14823.2.2.1.4.1.2.1.11';
+
+    if ( !$this->isProductionMode() ) {
+        $logger->info("not in production mode ... we won't write to the bnsMobileStationTable");
+        return 1;
+    }
+
+    if ( !$this->connectRead() ) {
+        $logger->error("ERROR: Can not connect using SNMP to Aruba Controler " . $this->{_ip});
+        return 1;
+    }
+
+    if ( length($mac) != 17 ) {
+        $logger->error("ERROR: MAC format is incorrect ($mac). Should be xx:xx:xx:xx:xx:xx");
+        return 1;
+    }
+
+    # In order to deauthenticate a client we need to retrieve the SSID (actually it is the MAC address of the AP) to which it is connected.
+    # We find this information in the nUserApBSSID entry of the WLSX-USER-MIB mib.
+    # The entry looks like:
+    # 1.3.6.1.4.1.14823.2.2.1.4.1.2.1.10.0.30.194.172.28.94.192.168.1.124 = STRING: 00:0b:86:cc:64:68';
+    # which is actually:
+    #        $OID_nUserApBSSID          .       mac        .      ip      = STRING: 00:0b:86:cc:64:68';
+
+    #format MAC
+    my @macArray = split( /:/, $mac );
+    my $completeOid = $OID_nUserApBSSID;
+    foreach my $macPiece (@macArray) {
+        $completeOid .= "." . hex($macPiece);
+    }
+
+    # Add the client IP
+    require pf::iplog;
+    my $ip = pf::iplog::mac2ip($mac) || 0;
+    if ($ip eq 0) {
+        $logger->error("ERROR: Can not find open entry in iplog for $mac");
+        return 1;
+    }
+    $completeOid .= "." . $ip;
+
+    # Query the controler to get the MAC address of the AP to which the client is associated
+    $logger->trace("SNMP get_request for nUserApBSSID: $completeOid");
+    my $result = $this->{_sessionRead}->get_request( -varbindlist => [$completeOid] );
+    if (defined($result)) {
+        my $apSSID = $result->{$completeOid};
+        if ($apSSID =~ /0x([A-Z0-9]{2})([A-Z0-9]{2})([A-Z0-9]{2})([A-Z0-9]{2})([A-Z0-9]{2})([A-Z0-9]{2})/i) {
+            $apSSID = uc("$1:$2:$3:$4:$5:$6");
+        } else {
+            $logger->error("ERROR: The MAC address format of the SSID is invalid: $apSSID");
+            return 1;
+        }
+
+        # use telnet to deauthenticate the client
+        my $session;
+        eval {
+            $session = Net::Telnet->new(
+                Host    => $this->{_ip},
+                Timeout => 5,
+                Prompt  => '/[\$%#>]$/'
+            );
+            $session->waitfor('/User: /');
+            $session->put( $this->{_cliUser} . "\n" );
+            $session->waitfor('/Password:/');
+            $session->put( $this->{_cliPwd} . "\n" );
+            $session->waitfor( $session->prompt );
+            $session->put( "en\n" );
+            $session->waitfor('/Password:/');
+            $session->put( $this->{_cliEnablePwd} . "\n" );
+            $session->waitfor( $session->prompt );
+        };
+
+        if ($@) {
+            $logger->error( "ERROR: Can not connect to Aruba Controler $this->{'_ip'} using " . $this->{_cliTransport} );
+            $logger->error( Dumper($@));
+            return 1;
+        }
+
+        my $cmd = "stm kick-off-sta $mac $apSSID";
+        $logger->debug( "DEBUG: deauthenticating $mac from SSID $apSSID with `$cmd`");
+        $session->cmd($cmd);
+        $session->close();
+        return 1;
+    } else {
+        $logger->error("ERROR: Can not get AP SSID from Aruba Controler for MAC $mac");
+    }
+
 }
 
 =head1 AUTHOR
