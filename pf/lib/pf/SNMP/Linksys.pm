38091f73da5f6e1c5ea24552515d92e7a4e5954f
Olivier Bilodeau <obilodeau@inverse.ca>
2009-09-11 20:23:00 +0000

    Deactivated port security on Linksys Known to be broken and the telnet code was sending SIGALRM and crashing pfsetvlan. Also added some trace level logging statements. Fixes bug #797; http://www.packetfence.org/mantis/view.php?id=797
    Monotone-Parent: e69d5f5489987e043be147bdaf0489af1b1fcdb4
    Monotone-Revision: cb201283467a5c43b2499ae0583f86c6d4a1d3c4
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2009-09-11T20:23:00

--- pf/lib/pf/SNMP/Linksys.pm
+++ pf/lib/pf/SNMP/Linksys.pm
@@ -19,7 +19,8 @@ use diagnostics;
 use base ('pf::SNMP');
 use Log::Log4perl;
 use Net::SNMP;
-use Net::Telnet;
+# disabling port-security since its known not to work
+#use Net::Telnet;
 
 sub getVersion {
     my ($this) = @_;
@@ -48,6 +49,7 @@ sub parseTrap {
     my ( $this, $trapString ) = @_;
     my $trapHashRef;
     my $logger = Log::Log4perl::get_logger( ref($this) );
+    $logger->trace("matching trap string");
     if ( $trapString
         =~ /BEGIN VARIABLEBINDINGS \.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.(\d+) = INTEGER: \d+\|\.1\.3\.6\.1\.2\.1\.2\.2\.1\.7\.\d+ = INTEGER: [^|]+\|\.1\.3\.6\.1\.2\.1\.2\.2\.1\.8\.\d+ = INTEGER: [^(]+\((\d)\) END VARIABLEBINDINGS/
         )
@@ -60,15 +62,17 @@ sub parseTrap {
     {
         $trapHashRef->{'trapType'} = ( ( $1 == 3 ) ? "down" : "up" );
         $trapHashRef->{'trapIfIndex'} = $2;
-    } elsif ( $trapString
-        =~ /PORT-W-LOCKPORTACTIVE: A packet with source MAC ([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}) tried to access through port e(\d+) which is locked/i
-        )
-    {
-        $trapHashRef->{'trapType'}    = 'secureMacAddrViolation';
-        $trapHashRef->{'trapMac'}     = lc($1);
-        $trapHashRef->{'trapIfIndex'} = $2;
-        $trapHashRef->{'trapVlan'}
-            = $this->getVlan( $trapHashRef->{'trapIfIndex'} );
+
+    # disabling port-security since its known not to work
+    #} elsif ( $trapString
+    #    =~ /PORT-W-LOCKPORTACTIVE: A packet with source MAC ([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}) tried to access through port e(\d+) which is locked/i
+    #    )
+    #{
+    #    $trapHashRef->{'trapType'}    = 'secureMacAddrViolation';
+    #    $trapHashRef->{'trapMac'}     = lc($1);
+    #    $trapHashRef->{'trapIfIndex'} = $2;
+    #    $trapHashRef->{'trapVlan'}
+    #        = $this->getVlan( $trapHashRef->{'trapIfIndex'} );
     } else {
         $logger->debug("trap currently not handled");
         $trapHashRef->{'trapType'} = 'unknown';
@@ -79,6 +83,7 @@ sub parseTrap {
 sub isDefinedVlan {
     my ( $this, $vlan ) = @_;
     my $logger = Log::Log4perl::get_logger( ref($this) );
+    $logger->trace("isDefinedVlan vlan: $vlan ?");
     if ( $vlan == 1 ) {
         return 1;
     }
@@ -292,124 +297,136 @@ sub _setVlan {
     return ( defined($result) );
 }
 
-sub connectTelnet {
-    my ($this)       = @_;
-    my $logger       = Log::Log4perl::get_logger( ref($this) );
-    my $maxTries     = 50;
-    my $tryNb        = 1;
-    my $cliAvailable = 0;
-    my $t = new Net::Telnet( Prompt => '/console# $/' );
-    while ( ( $tryNb <= $maxTries ) && ( !$cliAvailable ) ) {
-        eval {
-            $t->open( $this->{_ip} );
-            $t->waitfor(
-                '/Execute\\033\[21;18H\\033\[7mEdit\\033\[0m\\033\[21;18H/');
-            $t->put("\n");
-            $t->waitfor('/Execute/');
-            $t->put( $this->{_cliUser} . "\n" );
-            $t->waitfor('/Execute/');
-            $t->put( $this->{_cliPwd} . "\n" );
-            $t->waitfor('/Execute/');
-            $t->put("\033");
-            $t->waitfor('/Execute/');
-            $t->put("\033[C");
-            $t->waitfor('/Execute/');
-            $t->put("\n");
-            $t->waitfor('/Operation complete/');
-            $t->put(" ");
-            $t->waitfor('/Menu/');
-            $t->put("\032");
-            $t->waitfor('/>/');
-            $t->put("lcli\n");
-            $t->waitfor('/User Name:/');
-            $t->put( $this->{_cliUser} . "\n" );
-            $t->waitfor('/Password:/');
-            $t->put( $this->{_cliPwd} . "\n" );
-            $t->waitfor('/console/');
-        };
-        if ( !$@ ) {
-            $cliAvailable = 1;
-            $logger->debug("cli is available in try nb $tryNb");
-        } else {
-            $logger->debug("unable to connect in try nb $tryNb: $@");
-            sleep(1);
-            $tryNb++;
-        }
-    }
-    if ($cliAvailable) {
-        return $t;
-    } else {
-        $logger->error("unable to establish telnet connection");
-        return 0;
-    }
-}
+# Disabling port-security related code
+# --
+# Warning:
+# if you uncomment this sub, you need to fix it, the way it uses Net::Telnet is buggy. 
+# The connection should be made from inside the eval block. 
+# See other telnet consuming switches for correct implementation.
+#sub connectTelnet {
+#    my ($this)       = @_;
+#    my $logger       = Log::Log4perl::get_logger( ref($this) );
+#    my $maxTries     = 50;
+#    my $tryNb        = 1;
+#    my $cliAvailable = 0;
+#
+#    $logger->trace("About to try connecting to switch using telnet");
+#    my $t; = new Net::Telnet( Prompt => '/console# $/' );
+#    while ( ( $tryNb <= $maxTries ) && ( !$cliAvailable ) ) {
+#        eval {
+#            $t->open( $this->{_ip} );
+#            $t->waitfor(
+#                '/Execute\\033\[21;18H\\033\[7mEdit\\033\[0m\\033\[21;18H/');
+#            $t->put("\n");
+#            $t->waitfor('/Execute/');
+#            $t->put( $this->{_cliUser} . "\n" );
+#            $t->waitfor('/Execute/');
+#            $t->put( $this->{_cliPwd} . "\n" );
+#            $t->waitfor('/Execute/');
+#            $t->put("\033");
+#            $t->waitfor('/Execute/');
+#            $t->put("\033[C");
+#            $t->waitfor('/Execute/');
+#            $t->put("\n");
+#            $t->waitfor('/Operation complete/');
+#            $t->put(" ");
+#            $t->waitfor('/Menu/');
+#            $t->put("\032");
+#            $t->waitfor('/>/');
+#            $t->put("lcli\n");
+#            $t->waitfor('/User Name:/');
+#            $t->put( $this->{_cliUser} . "\n" );
+#            $t->waitfor('/Password:/');
+#            $t->put( $this->{_cliPwd} . "\n" );
+#            $t->waitfor('/console/');
+#        };
+#        if ( !$@ ) {
+#            $cliAvailable = 1;
+#            $logger->debug("cli is available in attempt nb $tryNb");
+#        } else {
+#            $logger->debug("unable to connect in atempt nb $tryNb: $@");
+#            sleep(1);
+#            $tryNb++;
+#        }
+#    }
+#    if ($cliAvailable) {
+#        return $t;
+#    } else {
+#        $logger->error("unable to establish telnet connection");
+#        return 0;
+#    }
+#}
 
-sub isPortSecurityEnabled {
-    my ( $this, $ifIndex ) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($this) );
-    my $OID_swIfHostMode
-        = '1.3.6.1.4.1.89.43.1.1.30';    #RADLAN-rlInterfaces MIB
+# disabling port-security since its known not to work
+#sub isPortSecurityEnabled {
+#    my ( $this, $ifIndex ) = @_;
+#    my $logger = Log::Log4perl::get_logger( ref($this) );
+#    my $OID_swIfHostMode
+#        = '1.3.6.1.4.1.89.43.1.1.30';    #RADLAN-rlInterfaces MIB
+#
+#    if ( !$this->connectRead() ) {
+#        return 0;
+#    }
+#
+#    #determine if port security is enabled
+#    $logger->trace(
+#        "SNMP get_request for swIfHostMode: $OID_swIfHostMode.$ifIndex");
+#    my $result = $this->{_sessionRead}
+#        ->get_request( -varbindlist => [ "$OID_swIfHostMode.$ifIndex" ] );
+#    return (   exists( $result->{"$OID_swIfHostMode.$ifIndex"} )
+#            && ( $result->{"$OID_swIfHostMode.$ifIndex"} ne 'noSuchInstance' )
+#            && ( $result->{"$OID_swIfHostMode.$ifIndex"} == 2 ) );
+#}
 
-    if ( !$this->connectRead() ) {
-        return 0;
-    }
+# disabling port-security since its known not to work
+#sub setPortSecurityDisabled {
+#    my ( $this, $ifIndex ) = @_;
+#    my $logger = Log::Log4perl::get_logger( ref($this) );
+#
+#    $logger->info("function not implemented yet !");
+#    return 1;
+#}
 
-    #determine if port security is enabled
-    $logger->trace(
-        "SNMP get_request for swIfHostMode: $OID_swIfHostMode.$ifIndex");
-    my $result = $this->{_sessionRead}
-        ->get_request( -varbindlist => [ "$OID_swIfHostMode.$ifIndex" ] );
-    return (   exists( $result->{"$OID_swIfHostMode.$ifIndex"} )
-            && ( $result->{"$OID_swIfHostMode.$ifIndex"} ne 'noSuchInstance' )
-            && ( $result->{"$OID_swIfHostMode.$ifIndex"} == 2 ) );
-}
+# disabling port-security since its known not to work
+#sub isDynamicPortSecurityEnabled {
+#    my ( $this, $ifIndex ) = @_;
+#    my $logger = Log::Log4perl::get_logger( ref($this) );
+#    return ( $this->isPortSecurityEnabled($ifIndex)
+#            && ( !$this->isStaticPortSecurityEnabled($ifIndex) ) );
+#}
 
-sub setPortSecurityDisabled {
-    my ( $this, $ifIndex ) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($this) );
-
-    $logger->info("function not implemented yet !");
-    return 1;
-}
-
-sub isDynamicPortSecurityEnabled {
-    my ( $this, $ifIndex ) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($this) );
-    return ( $this->isPortSecurityEnabled($ifIndex)
-            && ( !$this->isStaticPortSecurityEnabled($ifIndex) ) );
-}
-
-sub isStaticPortSecurityEnabled {
-    my ( $this, $ifIndex ) = @_;
-    my $logger                  = Log::Log4perl::get_logger( ref($this) );
-    my $OID_swIfLockAdminStatus = '1.3.6.1.4.1.89.43.1.1.8';
-
-    if ( !$this->connectRead() ) {
-        return -1;
-    }
-
-    #determine if port security is enabled
-    if ( !$this->isPortSecurityEnabled($ifIndex) ) {
-        $logger->debug("port security is not enabled");
-        return 0;
-    }
-
-    #determine swIfLockAdminStatus
-    $logger->trace(
-        "SNMP get_request for swIfLockAdminStatus: $OID_swIfLockAdminStatus.$ifIndex"
-    );
-    my $result = $this->{_sessionRead}->get_request(
-        -varbindlist => [ "$OID_swIfLockAdminStatus.$ifIndex" ] );
-    if (( !exists( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} ) )
-        || ( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} eq
-            'noSuchInstance' )
-        )
-    {
-        $logger->error("ERROR: could not obtain swIfLockAdminStatus");
-        return 0;
-    }
-    return ( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} == 1 );
-}
+# disabling port-security since its known not to work
+#sub isStaticPortSecurityEnabled {
+#    my ( $this, $ifIndex ) = @_;
+#    my $logger                  = Log::Log4perl::get_logger( ref($this) );
+#    my $OID_swIfLockAdminStatus = '1.3.6.1.4.1.89.43.1.1.8';
+#
+#    if ( !$this->connectRead() ) {
+#        return -1;
+#    }
+#
+#    #determine if port security is enabled
+#    if ( !$this->isPortSecurityEnabled($ifIndex) ) {
+#        $logger->debug("port security is not enabled");
+#        return 0;
+#    }
+#
+#    #determine swIfLockAdminStatus
+#    $logger->trace(
+#        "SNMP get_request for swIfLockAdminStatus: $OID_swIfLockAdminStatus.$ifIndex"
+#    );
+#    my $result = $this->{_sessionRead}->get_request(
+#        -varbindlist => [ "$OID_swIfLockAdminStatus.$ifIndex" ] );
+#    if (( !exists( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} ) )
+#        || ( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} eq
+#            'noSuchInstance' )
+#        )
+#    {
+#        $logger->error("ERROR: could not obtain swIfLockAdminStatus");
+#        return 0;
+#    }
+#    return ( $result->{"$OID_swIfLockAdminStatus.$ifIndex"} == 1 );
+#}
 
 sub getMaxMacAddresses {
     my ( $this, $ifIndex ) = @_;
@@ -443,35 +460,38 @@ sub getMaxMacAddresses {
     return $result->{"$OID_swIfLockMaxMacAddresses.$ifIndex"};
 }
 
-sub getSecureMacAddresses {
-    my ( $this, $ifIndex ) = @_;
-    my $logger               = Log::Log4perl::get_logger( ref($this) );
-    my $secureMacAddrHashRef = {};
-    my $ifName               = $this->getIfName($ifIndex);
-    if ( $ifName eq '' ) {
-        return $secureMacAddrHashRef;
-    }
-    my $telnetConnection = $this->connectTelnet();
-    if ( !$telnetConnection ) {
-        return $secureMacAddrHashRef;
-    }
+# disabling port-security since its known not to work
+#sub getSecureMacAddresses {
+#    my ( $this, $ifIndex ) = @_;
+#    my $logger               = Log::Log4perl::get_logger( ref($this) );
+#    my $secureMacAddrHashRef = {};
+#    my $ifName               = $this->getIfName($ifIndex);
+#    if ( $ifName eq '' ) {
+#        return $secureMacAddrHashRef;
+#    }
+#    my $telnetConnection = $this->connectTelnet();
+#    if ( !$telnetConnection ) {
+#        return $secureMacAddrHashRef;
+#    }
+#
+#    $logger->trace("telnet cmd: show bridge address-table static ethernet $ifName");
+#    my @lines = $telnetConnection->cmd(
+#        "show bridge address-table static ethernet $ifName");
+#    foreach my $line (@lines) {
+#        if ( $line
+#            =~ /(\d+)\s+([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}).+secure/
+#            )
+#        {
+#            my $vlan = $1;
+#            my $mac  = $2;
+#            push @{ $secureMacAddrHashRef->{$mac} }, $vlan;
+#        }
+#    }
+#    $telnetConnection->close();
+#    return $secureMacAddrHashRef;
+#}
 
-    my @lines = $telnetConnection->cmd(
-        "show bridge address-table static ethernet $ifName");
-    foreach my $line (@lines) {
-        if ( $line
-            =~ /(\d+)\s+([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}).+secure/
-            )
-        {
-            my $vlan = $1;
-            my $mac  = $2;
-            push @{ $secureMacAddrHashRef->{$mac} }, $vlan;
-        }
-    }
-    $telnetConnection->close();
-    return $secureMacAddrHashRef;
-}
-
+# disabling port-security since its known not to work
 #sub getAllSecureMacAddresses {
 #    my ($this)               = @_;
 #    my $logger               = Log::Log4perl::get_logger( ref($this) );
@@ -499,60 +519,69 @@ sub getSecureMacAddresses {
 #    return $secureMacAddrHashRef;
 #}
 
-sub authorizeMAC {
-    my ( $this, $ifIndex, $deauthMac, $authMac, $deauthVlan, $authVlan ) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($this) );
-    my $oid_cpsSecureMacAddrRowStatus = '1.3.6.1.4.1.9.9.315.1.2.2.1.4';
-
-    if ( !$this->isProductionMode() ) {
-        $logger->info(
-            "not in production mode ... we won't add an entry to the SecureMacAddrTable"
-        );
-        return 1;
-    }
-
-    my $ifName = $this->getIfName($ifIndex);
-    if ( $ifName eq '' ) {
-        return 0;
-    }
-
-    my $telnetConnection = $this->connectTelnet();
-    if ( !$telnetConnection ) {
-        return 0;
-    }
-
-    if ( ($deauthMac) && ($deauthVlan) ) {
-        $telnetConnection->put("configure\n");
-        $telnetConnection->waitfor('/console\(config\)#/');
-        $telnetConnection->put("interface vlan $deauthVlan\n");
-        $telnetConnection->waitfor('/console\(config-if\)#/');
-        $telnetConnection->put("no bridge address $deauthMac\n");
-        $telnetConnection->waitfor('/console\(config-if\)#/');
-        $telnetConnection->put("end\n");
-        $telnetConnection->waitfor('/console#/');
-    }
-    if ( ($authMac) && ($authVlan) ) {
-        $telnetConnection->put("configure\n");
-        $telnetConnection->waitfor('/console\(config\)#/');
-        $telnetConnection->put("interface vlan $authVlan\n");
-        $telnetConnection->waitfor('/console\(config-if\)#/');
-        $telnetConnection->put(
-            "bridge address $authMac ethernet $ifName secure\n");
-        $telnetConnection->waitfor('/console\(config-if\)#/');
-        $telnetConnection->put("end\n");
-        $telnetConnection->waitfor('/console#/');
-    }
-    $telnetConnection->close();
-    return 1;
-}
+# disabling port-security since its known not to work
+#sub authorizeMAC {
+#    my ( $this, $ifIndex, $deauthMac, $authMac, $deauthVlan, $authVlan ) = @_;
+#    my $logger = Log::Log4perl::get_logger( ref($this) );
+#    my $oid_cpsSecureMacAddrRowStatus = '1.3.6.1.4.1.9.9.315.1.2.2.1.4';
+#
+#    if ( !$this->isProductionMode() ) {
+#        $logger->info(
+#            "not in production mode ... we won't add an entry to the SecureMacAddrTable"
+#        );
+#        return 1;
+#    }
+#
+#    my $ifName = $this->getIfName($ifIndex);
+#    if ( $ifName eq '' ) {
+#        $logger->warn("unable to get ifName from ifIndex");
+#        return 0;
+#    }
+#
+#    my $telnetConnection = $this->connectTelnet();
+#    if ( !$telnetConnection ) {
+#        $logger->warn("unable to get telnet connection");
+#        return 0;
+#    }
+#
+#    if ( ($deauthMac) && ($deauthVlan) ) {
+#        $logger->trace("telnet interactive cmd: configure interface vlan $deauthVlan no bridge address $deauthMac");
+#        $telnetConnection->put("configure\n");
+#        $telnetConnection->waitfor('/console\(config\)#/');
+#        $telnetConnection->put("interface vlan $deauthVlan\n");
+#        $telnetConnection->waitfor('/console\(config-if\)#/');
+#        $telnetConnection->put("no bridge address $deauthMac\n");
+#        $telnetConnection->waitfor('/console\(config-if\)#/');
+#        $telnetConnection->put("end\n");
+#        $telnetConnection->waitfor('/console#/');
+#        $logger->trace("successful");
+#    }
+#    if ( ($authMac) && ($authVlan) ) {
+#        $logger->trace("telnet interactive cmd: configure interface vlan $authVlan bridge address $authMac ethernet $ifName secure");
+#        $telnetConnection->put("configure\n");
+#        $telnetConnection->waitfor('/console\(config\)#/');
+#        $telnetConnection->put("interface vlan $authVlan\n");
+#        $telnetConnection->waitfor('/console\(config-if\)#/');
+#        $telnetConnection->put(
+#            "bridge address $authMac ethernet $ifName secure\n");
+#        $telnetConnection->waitfor('/console\(config-if\)#/');
+#        $telnetConnection->put("end\n");
+#        $telnetConnection->waitfor('/console#/');
+#        $logger->trace("successful");
+#    }
+#    $telnetConnection->close();
+#    return 1;
+#}
 
 =head1 AUTHOR
 
 Dominik Gehl <dgehl@inverse.ca>
 
+Olivier Bilodeau <obilodeau@inverse.ca>
+
 =head1 COPYRIGHT
 
-Copyright (C) 2007-2008 Inverse inc.
+Copyright (C) 2007-2009 Inverse inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
