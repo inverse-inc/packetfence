bbadae189974769da8068402cf0b07662f943a76
Olivier Bilodeau <obilodeau@inverse.ca>
2009-10-02 19:02:33 +0000

    Added support for Enterasys SecureStack C3 Switch It works in linkUp/linkDown mode and port-security (called maclock on Enterasys hardware). Talks to the switch using SNMP. Added instructions to setup the SecureStack C3 Updated supported switches list Added comments about cleanup to do Minor formatting / whitespace cleanup
    Monotone-Parent: 8097e663108a27eaa1946d4066f0234a9c87378f
    Monotone-Revision: 0cb2621a09cdb6fc9215865afac1300e257b38db
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2009-10-02T19:02:33

---
+++ pf/lib/pf/SNMP/Enterasys/SecureStack_C3.pm
@@ -0,0 +1,76 @@
+package pf::SNMP::Enterasys::SecureStack_C3;
+
+=head1 NAME
+
+pf::SNMP::Enterasys::SecureStack_C3 - Object oriented module to parse SNMP traps and manage 
+Enterasys SecureStack C3 switches
+
+=head1 STATUS
+
+Developed and tested on an Enterasys SecureStack C3. 
+Firmware version: 06.03.01.0008
+
+It should work on all C3 switches and maybe more.
+
+=cut
+
+use strict;
+use warnings;
+use diagnostics;
+use Log::Log4perl;
+use Net::SNMP;
+use base ('pf::SNMP::Enterasys');
+
+=head1 BUGS AND LIMITATIONS
+
+=over
+
+=item Multiple untagged egress VLAN per port
+
+This switch supports multiple untagged VLAN per port but personally I don't think it's a "feature" ;). 
+What happens is since we only change the primary VLAN id (PVID) on a setVlan, you need to pre-approve all the 
+possible VLANs as untagged VLANs on the user ports. 
+This way when the PVID changes, the VLAN was already authorized to go out egress untagged.
+
+For example, if your normal VLAN is 10, isolation is 9 and registration is 8 and your users ports are 1 to 23 then you run:
+
+set vlan egress 8,9,10 ge.1.1-ge.1.23 untagged
+
+and you will be fine.
+
+=item SNMPv3
+    
+SNMPv3 support was not tested
+
+=back
+
+=head1 AUTHOR
+
+Olivier Bilodeau <obilodeau@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009 Inverse inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
