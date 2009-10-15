e04458b9ca2fb65956492feb87f1b1c12a720af1
Regis Balzard <rbalzard@inverse.ca>
2009-10-15 19:35:28 +0000

    Fixed redir.cgi crash when upgrading useragent. Before that, we got an ugly error mesage in logs: redir.cgi: Use of uninitialized value in concatenation (.) or string at /usr/local/pf/lib/pf/web.pm line 395
    Fixes #782
    http://www.packetfence.org/mantis/view.php?id=782
    
    Monotone-Parent: ab7d9f0848330d22fb5e46295256f380d2aedc87
    Monotone-Revision: 88bcdd0146de8b604fa21d409103b59d3985f9a1
    
    Monotone-Author: rbalzard@inverse.qc.ca
    Monotone-Date: 2009-10-15T19:35:28

--- pf/lib/pf/web.pm
+++ pf/lib/pf/web.pm
@@ -378,8 +378,8 @@ sub web_node_record_user_agent {
     my $logger = Log::Log4perl::get_logger('pf::web');
     
     # Recording useragent
-    $logger->info("calling $bin_dir/pfcmd 'node edit $mac user_agent=\"$user_agent\"'");
-    my $cmd    = $bin_dir . "/pfcmd 'node edit $mac user_agent=\"$user_agent\"'";
+    $logger->info("calling $bin_dir/pfcmd 'node edit $mac user_agent=\"" . $user_agent . "\"'");
+    my $cmd    = $bin_dir . "/pfcmd 'node edit $mac user_agent=\"" . $user_agent . "\"'";
     my $output = qx/$cmd/;
 
     # match provided useragent in useragent database
@@ -392,7 +392,7 @@ sub web_node_record_user_agent {
         pf::violation::violation_trigger( $mac, $user_agent_info[0]->{'useragent_id'}, "USERAGENT" );
 
     } else {
-        $logger->info("unknown User-Agent: $user_agent");
+        $logger->info("unknown User-Agent: " . $user_agent);
         # TODO: record unknown useragents here? (how does dhcp fingerprint does it?)
     }
 
