a07d9c0d0c2f7376f921a600bb1d3d99ff94354d
Olivier Bilodeau <obilodeau@inverse.ca>
2010-09-24 18:37:22 +0000

    Pre-loading PacketFence modules using a PerlRequire statement. Improves per-request latency and overall performance. Thanks to Josh Ward from University of Oregon for the initial research on the fix!
    Monotone-Parent: 423f1d6d53baaece19da7c6dc3a19794c8eaed3e
    Monotone-Revision: 0265caab9448e9ef4bf5e52c5d04fbd040115f8e
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2010-09-24T18:37:22
    Monotone-Branch: org.packetfence.trunk

---
+++ pf/lib/pf/mod_perl_require.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use lib "/usr/local/pf/lib";
+
+use strict;
+use warnings;
+
+use Log::Log4perl;
+
+use pf::config;
+use pf::locationlog;
+use pf::node;
+use pf::SNMP;
+use pf::SwitchFactory;
+use pf::util;
+
+1;
