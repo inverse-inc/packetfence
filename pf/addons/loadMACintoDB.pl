852c0e6b441af4c68800e662168dd3ac6af987ba
Olivier Bilodeau <obilodeau@inverse.ca>
2010-09-30 18:20:38 +0000

    Added minimal POD for file and added to minimum test suites
    Monotone-Parent: ca1828c1ff1f39d63e0613e7407f0852fd54f05d
    Monotone-Revision: 7e0796292084bfb5ccec233c4441376b52313ee3
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2010-09-30T18:20:38

--- pf/addons/loadMACintoDB.pl
+++ pf/addons/loadMACintoDB.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl -w
 
+=head1 NAME
+
+loadMACintoDB.pl - load MACs from a flat file into PacketFence's database
+
+TODO doc incomplete
+
+=cut
 use strict;
 use warnings;
 use diagnostics;
@@ -75,6 +82,32 @@ while (<MYFILE>) {
 
 close (MYFILE);
 
+=head1 AUTHOR
+
+Regis Balzard <rbalzard@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 # vim: set shiftwidth=4:
 # vim: set expandtab:
