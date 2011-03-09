778d9bd30c58eaa7c7d504fa2bd2feb960414b6f
Olivier Bilodeau <obilodeau@inverse.ca>
2011-03-09 15:18:21 +0000

    node importation can override default pid with info hash now
    Monotone-Parent: ba6afa1d4d8092af81a9b8bc4718feda4f3ab211
    Monotone-Revision: b6e4f15a22c2d338b676116c4a56fdd9f1fd62d0
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-03-09T15:18:21

--- pf/lib/pf/import.pm
+++ pf/lib/pf/import.pm
@@ -70,6 +70,7 @@ sub nodes {
         $logger->logdie("Nodes import expects first column of first row to be a MAC address. Not processing file.");
     }
 
+    # TODO extract these in parameters under [import] in pf.conf
     # pre-compute info hash
     my %info = (
         notes => POSIX::strftime("Imported on %Y-%m-%d %H:%M:%S", localtime(time))
@@ -94,7 +95,8 @@ sub nodes {
             # or if entry is valid and node is not registered 
             if (!defined($node) || (ref($node) eq 'HASH' && $node->{'status'} ne $pf::node::STATUS_REGISTERED)) {
                 # try to register
-                if (node_register($mac, $default_pid, %info)) {
+                my $pid = $info{'pid'} || $default_pid;
+                if (node_register($mac, $pid, %info)) {
                     printf("%04d: %s registered\n", $line, $mac);
                 } else {
                     $logger->warn("Problem with entry on line $line MAC $mac: node_register returned an error");
@@ -120,7 +122,7 @@ Olivier Bilodeau <obilodeau@inverse.ca>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2010 Inverse inc.
+Copyright (C) 2010,2011 Inverse inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
