96a06ca657b6f7866340dbb1c63750760513ba62
Olivier Bilodeau <obilodeau@inverse.ca>
2011-03-23 20:21:35 +0000

    missing from previous commit
    Monotone-Parent: e65822515784189980c3e5130f20e274b028052e
    Monotone-Revision: 8897c246585d5152a7f2fb3e4b8062f392edb6ba
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-03-23T20:21:35

--- pf/addons/guest-email-confirmation/schema-temporary-passwords.sql
+++ pf/addons/guest-email-confirmation/schema-temporary-passwords.sql
@@ -3,11 +3,11 @@
 --
 
 CREATE TABLE temporary_password (
-  `tp_id` int NOT NULL AUTO_INCREMENT,
   `pid` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `valid_from` DATETIME DEFAULT NULL,
   `expiration` DATETIME NOT NULL,
-  PRIMARY KEY (tp_id),
-  KEY (pid)
+  `access_duration` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (pid)
 ) ENGINE=InnoDB;
 
