abb48175768ea2fc8f2ea7483fb405f9d403fb02
Olivier Bilodeau <obilodeau@inverse.ca>
2011-06-07 15:57:56 +0000

    added LICENSE pod header
    Monotone-Parent: c6cc45ba1dd2fec340d0afc4539996466b4cadd2
    Monotone-Revision: 09e8c381d5404d88b5377b94158520ae6739ef69
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-06-07T15:57:56
    Monotone-Branch: org.packetfence.trunk

--- pf/lib/pf/floatingdevice.pm
+++ pf/lib/pf/floatingdevice.pm
@@ -206,6 +206,8 @@ Regis Balzard <rbalzard@inverse.ca>
 
 Copyright (C) 2010 Inverse inc.
 
+=head1 LICENSE
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
