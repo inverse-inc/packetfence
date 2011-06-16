1352a3c2c6aefe48110177fe5f09eba98fd645ce
Olivier Bilodeau <obilodeau@inverse.ca>
2011-06-16 14:44:33 +0000

    Fixed VoIP support on Cisco 2960 in IOS 12.2(46)SE or greater
    A lot of testing and documenting went into this one. Make sure to consult the appropriate modules and documentation if necessary.
    
    Monotone-Parent: 041ecebebee27a897becf05f5030ed3643d4fe2e
    Monotone-Revision: 72401bc3c8dcb746697344729731589b080c3096
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-06-16T14:44:33

--- pf/lib/pf/SNMP/Cisco/Catalyst_2970.pm
+++ pf/lib/pf/SNMP/Cisco/Catalyst_2970.pm
@@ -8,8 +8,12 @@ pf::SNMP::Cisco::Catalyst_2970 - Object oriented module to access and configure 
 
 This module is currently only a placeholder, see pf::SNMP::Cisco::Catalyst_2960.
 
-=cut
+=head1 BUGS AND LIMITATIONS
 
+Because a lot of code is shared with the 2960 make sure to check the BUGS AND LIMITATIONS section of 
+L<pf::SNMP::Cisco::Catalyst_2960> also.
+
+=cut
 use strict;
 use warnings;
 use diagnostics;
@@ -20,11 +24,15 @@ use base ('pf::SNMP::Cisco::Catalyst_2960');
 
 =head1 AUTHOR
 
+Olivier Bilodeau <obilodeau@inverse.ca>
+
 Dominik Gehl <dgehl@inverse.ca>
 
 =head1 COPYRIGHT
 
 Copyright (C) 2006-2011 Inverse inc.
+
+=head1 LICENSE
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
