37c8d84fa39346eb1dc4eda2c43303f953d0525a
Olivier Bilodeau <obilodeau@inverse.ca>
2011-07-04 19:10:55 +0000

    pushed incomplete iptables rework, will get back to it in the future
    Monotone-Parent: 63f8027ee9b1f929ce29a08a1571e6ab4221238c
    Monotone-Revision: 11aa3aebe0c81a82a031129a35bc53821e7de895
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-07-04T19:10:55

--- pf/lib/pf/config.pm
+++ pf/lib/pf/config.pm
@@ -298,7 +298,6 @@ sub readPfConfigFiles {
         my $mask           = $Config{$interface}{'mask'};
         my $gateway        = $Config{$interface}{'gateway'};
         my $type           = $Config{$interface}{'type'};
-        my $authorized_ips = $Config{$interface}{'authorizedips'} || '';
 
         if ( defined($ip) && defined($mask) ) {
             $ip   =~ s/ //g;
@@ -307,7 +306,6 @@ sub readPfConfigFiles {
             $int_obj->tag( "gw",      $gateway );
             $int_obj->tag( "ip",      $ip );
             $int_obj->tag( "int",     $int );
-            $int_obj->tag( "authips", $authorized_ips );
         }
         foreach my $type ( split( /\s*,\s*/, $type ) ) {
             if ( $type eq 'internal' ) {
