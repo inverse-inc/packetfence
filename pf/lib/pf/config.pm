b4b597cc0a037abbc5e9fa3587638fde1ed4cd98
Francois Gaudreault <fgaudreault@inverse.ca>
2011-07-26 15:18:36 +0000

    - Tabs in the code breaks the build!
    Monotone-Parent: 0fc140cfe44a4397e3f80362c4a34c66f84a1e50
    Monotone-Revision: db096ed1a030ab33a4396a1432bd6c2cab8abda0
    
    Monotone-Author: fgaudreault@inverse.ca
    Monotone-Date: 2011-07-26T15:18:36
    Monotone-Branch: org.packetfence.trunk

--- pf/lib/pf/config.pm
+++ pf/lib/pf/config.pm
@@ -330,10 +330,10 @@ sub readPfConfigFiles {
         foreach my $type ( split( /\s*,\s*/, $type ) ) {
             if ( $type eq 'internal' ) {
                 push @internal_nets, $int_obj;
-                if (!defined($Config{$interface}{'enforcement'}) { 
-		    $logger->warn("$int: interface type internal must have an enforcement mode defined.");
-		} elsif ($Config{$interface}{'enforcement'} eq $IF_ENFORCEMENT_VLAN) {
-                    push @vlan_enforcement_nets, $int_obj;
+                if (!defined($Config{$interface}{'enforcement'}) {
+                  $logger->warn("$int: interface type internal must have an enforcement mode defined.");
+                } elsif ($Config{$interface}{'enforcement'} eq $IF_ENFORCEMENT_VLAN) {
+                  push @vlan_enforcement_nets, $int_obj;
                 } elsif ($Config{$interface}{'enforcement'} eq $IF_ENFORCEMENT_INLINE) {
                     push @inline_enforcement_nets, $int_obj;
                 }
