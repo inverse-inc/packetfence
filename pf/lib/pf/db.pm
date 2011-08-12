e3adf032c869eec9c872a623845c4426d6459e6b
Olivier Bilodeau <obilodeau@inverse.ca>
2011-08-12 18:31:08 +0000

    added missing LICENSE pod section
    Monotone-Parent: 3afac1528fcf9f5aa76d7d0854ab81cc9bfd713b
    Monotone-Revision: fa9603aeaa59c8745dce67f7e1bf4f66ab721389
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-08-12T18:31:08
    Monotone-Branch: org.packetfence.trunk

--- pf/lib/pf/db.pm
+++ pf/lib/pf/db.pm
@@ -294,6 +294,8 @@ Copyright (C) 2005 Kevin Amorin
 
 Copyright (C) 2010 Inverse inc.
 
+=head1 LICENSE
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
