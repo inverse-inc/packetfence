d36e0195f4089543a2274292c4ad13f8fd908fe7
Olivier Bilodeau <obilodeau@inverse.ca>
2011-08-31 21:05:01 +0000

    Cisco 2950 12.1(22)EA13 apparently have problems regarding port-security traps
    Monotone-Parent: 11bf7f6ad27b7bc00995600463fdbaa82e0b352f
    Monotone-Revision: be3cc54a780a93f600cbc79971d88711ee164916
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-08-31T21:05:01
    Monotone-Branch: org.packetfence.trunk

--- pf/lib/pf/SNMP/Cisco/Catalyst_2950.pm
+++ pf/lib/pf/SNMP/Cisco/Catalyst_2950.pm
@@ -37,6 +37,11 @@ This module extends pf::SNMP::Cisco.
 =head1 BUGS AND LIMITATIONS
 
 =over
+
+=item Problematic firmware versions
+
+We got reports that 12.1(22)EA13 is buggy. 
+Not sending port-security traps under uncertain circumstances.
  
 =item 802.1X
  
