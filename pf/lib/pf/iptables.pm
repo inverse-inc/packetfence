7bd0890e4e67d5c3cb9bf97c870c7dadd46cb60a
Olivier Bilodeau <obilodeau@inverse.ca>
2011-09-13 21:28:14 +0000

    monitor interface firewall rule to allow all
    Monotone-Parent: be35ed79c049b58d6b7127530c51c0822fb6f5c2
    Monotone-Revision: fe9c320fdadc3d4b1936d1d087bb3e3cac32fcf8
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-09-13T21:28:14
    Monotone-Branch: org.packetfence.trunk

--- pf/lib/pf/iptables.pm
+++ pf/lib/pf/iptables.pm
@@ -45,6 +45,7 @@ Readonly my $FW_FILTER_INPUT_INT_VLAN => 'input-internal-vlan-if';
 Readonly my $FW_FILTER_INPUT_INT_INLINE => 'input-internal-inline-if';
 Readonly my $FW_FILTER_INPUT_MGMT => 'input-management-if';
 Readonly my $FW_FILTER_INPUT_INT_HA => 'input-highavailability-if';
+Readonly my $FW_FILTER_INPUT_INT_MON => 'input-monitor-if';
 Readonly my $FW_FILTER_FORWARD_INT_INLINE => 'forward-internal-inline-if';
 Readonly my $FW_PREROUTING_INT_INLINE => 'prerouting-internal-inline-if';
 
@@ -135,7 +136,12 @@ sub generate_filter_if_src_to_chain {
     foreach my $interface (@ha_ints) {
         $rules .= "-A INPUT --in-interface $interface --jump $FW_FILTER_INPUT_INT_HA\n";
     }
-    
+
+    # monitor interface handling
+    if ($monitor_int) {
+        $rules .= "-A INPUT --in-interface $monitor_int --jump $FW_FILTER_INPUT_INT_MON\n";
+    }
+
     return $rules;
 }
 
