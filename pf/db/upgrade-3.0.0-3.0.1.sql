8bc1ecdd63b8ae951dacdfefa257fb7668197579
Derek Wuelfrath <dwuelfrath@inverse.ca>
2011-10-07 14:01:49 +0000

    Add trigger or cascade to delete node_useragent when deleting a node (#1285)
    Monotone-Parent: 195d08faf44c05123d0cdec00f6e373730f27b9a
    Monotone-Revision: 403f38fb5859a3572f58f372cc01826ed35a8bf9
    
    Monotone-Author: dwuelfrath@inverse.ca
    Monotone-Date: 2011-10-07T14:01:49

--- pf/db/upgrade-3.0.0-3.0.1.sql
+++ pf/db/upgrade-3.0.0-3.0.1.sql
@@ -1,3 +1,16 @@
 -- Missing permissions on pf user for accounting
 
 GRANT execute ON pf.* TO 'pf'@'localhost';
+
+--
+-- Trigger to delete the node_useragent associated with a mac when deleting this mac from the node table
+--
+
+DROP TRIGGER IF EXISTS node_useragent_delete_trigger;
+DELIMITER /
+CREATE TRIGGER node_useragent_delete_trigger AFTER DELETE ON node
+FOR EACH ROW
+BEGIN
+  DELETE FROM node_useragent WHERE mac = OLD.mac;
+END /
+DELIMITER ;
