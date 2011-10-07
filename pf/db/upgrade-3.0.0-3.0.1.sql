c1fc4cdc2cb3fa493b5b23634ad82dac593c0038
Francois Gaudreault <fgaudreault@inverse.ca>
2011-10-07 20:37:02 +0000

    - Documentation for radiusSecret - Fix the upgrade scripts with proper versions
    Monotone-Parent: 403f38fb5859a3572f58f372cc01826ed35a8bf9
    Monotone-Revision: ad395afeca3b0a7fb38da331ca0b2391e73d54d5
    
    Monotone-Author: fgaudreault@inverse.ca
    Monotone-Date: 2011-10-07T20:37:02

--- pf/db/upgrade-3.0.0-3.0.1.sql
+++ pf/db/upgrade-3.0.0-3.0.1.sql
@@ -2,15 +2,3 @@
 
 GRANT execute ON pf.* TO 'pf'@'localhost';
 
---
--- Trigger to delete the node_useragent associated with a mac when deleting this mac from the node table
---
-
-DROP TRIGGER IF EXISTS node_useragent_delete_trigger;
-DELIMITER /
-CREATE TRIGGER node_useragent_delete_trigger AFTER DELETE ON node
-FOR EACH ROW
-BEGIN
-  DELETE FROM node_useragent WHERE mac = OLD.mac;
-END /
-DELIMITER ;
