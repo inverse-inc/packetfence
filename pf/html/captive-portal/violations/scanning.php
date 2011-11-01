fca74941989c88d21a7e34d12f70acbb4fd641c1
Olivier Bilodeau <obilodeau@inverse.ca>
2011-11-01 19:32:36 +0000

    new string 'contact support staff' instead of 'local support staff'
    kept both in case one wants to go to old name
    
    Monotone-Parent: dc2d497aad59a76f7e9218724ddf048f084298cb
    Monotone-Revision: db97d01398c031d0edc861d5065403af7884ddf1
    
    Monotone-Author: obilodeau@inverse.ca
    Monotone-Date: 2011-11-01T19:32:36

--- pf/html/captive-portal/violations/scanning.php
+++ pf/html/captive-portal/violations/scanning.php
@@ -28,6 +28,6 @@
  
   $description_text = 'Your system has been found to be scanning other systems. Due to the threat this infection poses for other systems on the network, network connectivity has been disabled until corrective action is taken.';
 
-  $remediation_text = 'Your system will likely need to be rebuilt. Please contact your local support staff for more information.';
+  $remediation_text = 'Your system will likely need to be rebuilt. Please contact your support staff for more information.';
 
 ?>
