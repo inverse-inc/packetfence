c20a613cb14e8589460feabf91ca883f24ebb763
Derek Wuelfrath <dwuelfrath@inverse.ca>
2012-04-26 10:01:06 -0400

    Syntax standardization

--- html/configurator/lib/configurator/Controller/Interface.pm
+++ html/configurator/lib/configurator/Controller/Interface.pm
@@ -1,8 +1,4 @@
 package configurator::Controller::Interface;
-use Moose;
-use namespace::autoclean;
-
-BEGIN {extends 'Catalyst::Controller'; }
 
 =head1 NAME
 
@@ -12,10 +8,21 @@ configurator::Controller::Interface - Catalyst Controller
 
 Catalyst Controller.
 
-=head1 METHODS
-
 =cut
 
+use strict;
+use warnings;
+
+# Catalyst includes
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+
+=head1 SUBROUTINES
+
+=over
 
 =head2 index
 
@@ -28,14 +35,32 @@ sub index :Path :Args(0) {
 }
 
 
+=back
+
 =head1 AUTHOR
 
-root
+Derek Wuelfrath <dwuelfrath@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
 
 =cut
 
--- html/configurator/lib/configurator/Model/Interface.pm
+++ html/configurator/lib/configurator/Model/Interface.pm
@@ -1,8 +1,4 @@
 package configurator::Model::Interface;
-use Moose;
-use namespace::autoclean;
-
-extends 'Catalyst::Model';
 
 =head1 NAME
 
@@ -12,14 +8,52 @@ configurator::Model::Interface - Catalyst Model
 
 Catalyst Model.
 
+=cut
+
+use strict;
+use warnings;
+
+# Catalyst includes
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::Model';
+
+# Package includes
+use IO::Interface::Simple;
+
+
+=head1 SUBROUTINES
+
+=over
+
+
+=back
+
 =head1 AUTHOR
 
-root
+Derek Wuelfrath <dwuelfrath@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
 
 =cut
 
