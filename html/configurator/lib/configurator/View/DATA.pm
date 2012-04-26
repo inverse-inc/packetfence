10ff7d51d4885e88c7389049623cbb7b80b1a5e0
Derek Wuelfrath <dwuelfrath@inverse.ca>
2012-04-26 09:45:57 -0400

    Added a JSON view - DATA

---
+++ html/configurator/lib/configurator/View/DATA.pm
@@ -0,0 +1,29 @@
+package configurator::View::DATA;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+configurator::View::DATA - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<configurator>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+root
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ html/configurator/t/view_DATA.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN { use_ok 'configurator::View::DATA' }
+
+done_testing();
