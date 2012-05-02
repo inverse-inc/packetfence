01f8bcaa6ff550d679ac8aec161a4614775637ca
Derek Wuelfrath <dwuelfrath@inverse.ca>
2012-05-02 13:09:03 -0400

    Added a new controller/model - Mode

---
+++ html/configurator/lib/configurator/Model/Mode.pm
@@ -0,0 +1,28 @@
+package configurator::Model::Mode;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::Model';
+
+=head1 NAME
+
+configurator::Model::Mode - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+root
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
