2791c9618b96505a788fa92e143b0423f10857fb
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-05-10 22:26:25 -0400

    reindent + exception handling in chained dispatch

--- html/configurator/lib/configurator/Controller/Config/Interface.pm
+++ html/configurator/lib/configurator/Controller/Config/Interface.pm
@@ -1,9 +1,10 @@
 package configurator::Controller::Config::Interface;
+use JSON;
 use Moose;
 use namespace::autoclean;
-use JSON;
+use Try::Tiny;
 
-BEGIN {extends 'Catalyst::Controller'; }
+BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
 
@@ -35,16 +36,21 @@ Chained dispatch for an interface.
 =cut
 
 sub object :Chained('/') :PathPart('config/interface') :CaptureArgs(1) {
-  my ($self, $c, $section) = @_;
-  $c->log->debug("chained dispatch called");
+    my ($self, $c, $section) = @_;
 
-  unless ($c->model('Config::Interface')->sectionExists($section)) {
-    $c->res->status(404);
-    $c->stash->{result} = "Unknown interface $section";
-    $c->detach();
-  }
-  
-  $c->stash->{section} = $section;
+    try {
+        unless ($c->model('Config::Interface')->sectionExists($section)) {
+            $c->res->status(404);
+            $c->stash->{result} = "Unknown interface $section";
+            $c->detach();
+        }
+        $c->stash->{section} = $section;
+
+    } catch {
+        chomp($_);
+        $c->error($_);
+        $c->detach();
+    };
 }
 
 =head2 get
@@ -54,24 +60,24 @@ sub object :Chained('/') :PathPart('config/interface') :CaptureArgs(1) {
 =cut
 
 sub get :Chained('object') :PathPart('get') :Args(0) {
-  my ($self, $c) = @_;
-  $c->log->debug("get called");
+    my ($self, $c) = @_;
+    $c->log->debug("get called");
 
-  my $section = $c->stash->{section};
+    my $section = $c->stash->{section};
 
-  my $result;
-  eval {
-    $result = $c->model('Config::Interface')->get($section);
-  };
-  if ($@) {
-    chomp $@;
-    $c->res->status(500);
-    $c->stash->{result} = $@;
-  }
-  else {
-    $c->res->status(200);
-    $c->stash->{interfaces} = $result;
-  }
+    my $result;
+    eval {
+        $result = $c->model('Config::Interface')->get($section);
+    };
+    if ($@) {
+        chomp $@;
+        $c->res->status(500);
+        $c->stash->{result} = $@;
+    }
+    else {
+        $c->res->status(200);
+        $c->stash->{interfaces} = $result;
+    }
 }
 
 =head2 delete
@@ -81,22 +87,22 @@ sub get :Chained('object') :PathPart('get') :Args(0) {
 =cut
 
 sub delete :Chained('object') :PathPart('delete') :Args(0) {
-  my ($self, $c) = @_;
-  my $section = $c->stash->{section};
+    my ($self, $c) = @_;
+    my $section = $c->stash->{section};
 
-  my $result;
-  eval {
-    $result = $c->model('Config::Interface')->remove($section);
-  };
-  if ($@) {
-    chomp $@;
-    $c->res->status(500);
-    $c->stash->{result} = $@;
-  }
-  else {
-    $c->res->status(200);
-    $c->stash->{result} = $result;
-  }
+    my $result;
+    eval {
+        $result = $c->model('Config::Interface')->remove($section);
+    };
+    if ($@) {
+        chomp $@;
+        $c->res->status(500);
+        $c->stash->{result} = $@;
+    }
+    else {
+        $c->res->status(200);
+        $c->stash->{result} = $result;
+    }
 }
 
 =head2 edit
@@ -106,39 +112,39 @@ sub delete :Chained('object') :PathPart('delete') :Args(0) {
 =cut
 
 sub edit :Chained('object') :PathPart('edit') :Args(0) {
-  my ($self, $c) = @_;
-  my $section = $c->stash->{section};
+    my ($self, $c) = @_;
+    my $section = $c->stash->{section};
 
-  my $assignments = $c->request->params->{assignments};
+    my $assignments = $c->request->params->{assignments};
 
-  if ($assignments) {
-    my $result;
-    eval {
-      $assignments = decode_json($assignments);
-    };
-    if ($@) {
-      # Malformed JSON
-      chomp $@;
-      $c->res->status(400);
-      $c->stash->{result} = $@;
+    if ($assignments) {
+        my $result;
+        eval {
+            $assignments = decode_json($assignments);
+        };
+        if ($@) {
+            # Malformed JSON
+            chomp $@;
+            $c->res->status(400);
+            $c->stash->{result} = $@;
+        }
+        else {
+            eval { $result = $c->model('Config::Interface')->edit($section, $assignments); };
+            if ($@) {
+                chomp $@;
+                $c->res->status(500);
+                $c->stash->{result} = $@;
+            }
+            else {
+                $c->res->status(201);
+                $c->stash->{result} = $result;
+            }
+        }
     }
     else {
-      eval { $result = $c->model('Config::Interface')->edit($section, $assignments); };
-      if ($@) {
-        chomp $@;
-        $c->res->status(500);
-        $c->stash->{result} = $@;
-      }
-      else {
-        $c->res->status(201);
-        $c->stash->{result} = $result;
-      }
+        $c->res->status(400);
+        $c->stash->{result} = 'Missing parameters';
     }
-  }
-  else {
-    $c->res->status(400);
-    $c->stash->{result} = 'Missing parameters';
-  }
 }
 
 =head2 add
@@ -149,45 +155,44 @@ sub edit :Chained('object') :PathPart('edit') :Args(0) {
 =cut
 
 sub add :Local {
-  my ($self, $c, $section) = @_;
+    my ($self, $c, $section) = @_;
 
-  $section = $c->request->params->{section} unless ($section);
-  my $assignments = $c->request->params->{assignments};
+    $section = $c->request->params->{section} unless ($section);
+    my $assignments = $c->request->params->{assignments};
 
-  if ($section && $assignments) {
-    my $result;
-    eval {
-      $assignments = decode_json($assignments);
-    };
-    if ($@) {
-      # Malformed JSON
-      chomp $@;
-      $c->res->status(400);
-      $c->stash->{result} = $@;
+    if ($section && $assignments) {
+        my $result;
+        eval {
+            $assignments = decode_json($assignments);
+        };
+        if ($@) {
+            # Malformed JSON
+            chomp $@;
+            $c->res->status(400);
+            $c->stash->{result} = $@;
+        }
+        else {
+            eval { $result = $c->model('Config::Interface')->add($section, $assignments); };
+            if ($@) {
+                 chomp $@;
+                 $c->res->status(500);
+                 $c->stash->{result} = $@;
+            }
+            else {
+                $c->res->status(201);
+                $c->stash->{result} = $result;
+            }
+        }
     }
     else {
-      eval { $result = $c->model('Config::Interface')->add($section, $assignments); };
-      if ($@) {
-        chomp $@;
-        $c->res->status(500);
-        $c->stash->{result} = $@;
-      }
-      else {
-        $c->res->status(201);
-        $c->stash->{result} = $result;
-      }
+        $c->res->status(400);
+        $c->stash->{message} = 'Missing parameters';
+        $c->forward('View::HTML');
     }
-  }
-  else {
-    $c->res->status(400);
-    $c->stash->{message} = 'Missing parameters';
-    $c->forward('View::HTML');
-  }
 }
 
 sub end : ActionClass('RenderView') {
     my ( $self, $c ) = @_;
-
     $c->forward('View::JSON');
 }
 
