c29b68d642fb84becc7b6f0d68068c5fc2767fe6
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-05-11 13:53:04 -0400

    POD metadata changes

--- html/configurator/lib/configurator/Controller/Config/Interface.pm
+++ html/configurator/lib/configurator/Controller/Config/Interface.pm
@@ -16,8 +16,6 @@ Catalyst Controller.
 
 =head1 METHODS
 
-=cut
-
 =head2 index
 
 =cut
@@ -162,14 +160,30 @@ sub end : ActionClass('RenderView') {
 
 =head1 AUTHOR
 
-Francis Lachapelle
+Francis Lachapelle <flachapelle@inverse.ca>
 
 Olivier Bilodeau <obilodeau@inverse.ca>
 
+=head1 COPYRIGHT
+
+Copyright 2012 Inverse inc.
+
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
 
 =cut
 
--- html/configurator/lib/configurator/Model/Config/Pf.pm
+++ html/configurator/lib/configurator/Model/Config/Pf.pm
@@ -16,35 +16,20 @@ pfws::Model::Config::Pf - Catalyst Model
 
 Catalyst Model.
 
-=head1 AUTHOR
-
-Francis Lachapelle <flachapelle@inverse.ca>
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
+=head1 METHODS
 
 =cut
 
 my $_pf_conf = undef;
 
+=item _pf_conf
+
+Load pf.conf into a Config::IniFiles tied hashref
+
+=cut
 sub _pf_conf {
     my ($self) = @_;
     my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-    $logger->info("Test");
 
     unless (defined $_pf_conf) {
         my %conf;
@@ -182,6 +167,35 @@ sub add {
 
     return ($TRUE, "Successfully created $interface");
 }
+
+=head1 AUTHOR
+
+Francis Lachapelle <flachapelle@inverse.ca>
+
+Olivier Bilodeau <obilodeau@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright 2012 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 
