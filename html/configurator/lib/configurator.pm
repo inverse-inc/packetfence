bae8ca6bfb423ef9a8d9aecbfc2ee4c3e1c5b5d4
Francis Lachapelle <flachapelle@inverse.ca>
2012-05-16 14:10:18 -0400

    Don't expose all the stash in JSON.

--- html/configurator/lib/configurator.pm
+++ html/configurator/lib/configurator.pm
@@ -44,6 +44,10 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
 
+    'View::JSON' => {
+       expose_stash    => [ qw(status_msg interfaces) ], # defaults to everything
+    },
+
     'Plugin::Session' => {
         storage => '/usr/local/pf/var/session'
     },
