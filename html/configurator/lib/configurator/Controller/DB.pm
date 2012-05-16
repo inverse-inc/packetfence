eb783981dd9face0db97f312f50c3c825c2c33c9
Derek Wuelfrath <dwuelfrath@inverse.ca>
2012-05-16 11:12:35 -0400

    Database configuration stuff

--- html/configurator/lib/configurator/Controller/DB.pm
+++ html/configurator/lib/configurator/Controller/DB.pm
@@ -19,24 +19,117 @@ use namespace::autoclean;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
-
 =head1 SUBROUTINES
 
 =over
 
-=item index
+=assign
 
-=cut 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
+Assign a new user to a database.
 
-    $c->response->body('Matched configurator::Controller::DB in DB.');
+Usage: /db/assign/<database_name>
+
+=cut
+sub assign :Path('assign') :Args(1) {
+    my ( $self, $c, $db ) = @_;
+
+    my $root_user       = $c->request->params->{root_user};
+    my $root_password   = $c->request->params->{root_password};
+    my $pf_user         = $c->request->params->{pf_user};
+    my $pf_password     = $c->request->params->{pf_password};
+
+    my ( $result, $status_msg, $dbHandler ) = $c->model('DB')->connect('mysql', $root_user, $root_password);
+
+    if ( $result && $dbHandler ) {
+        ( $result, $status_msg ) = $c->model('DB')->assign($dbHandler, $db, $pf_user, $pf_password);
+    }
+
+    if ( $result ) {
+        $c->response->status(200);
+        $c->stash->{status_msg} = $status_msg;
+    } else {
+        $c->response->status(501);
+        $c->stash->{status_msg} = $status_msg;
+    }
 }
 
+=item create
+
+Create a new database.
+
+Usage: /db/create/<database_name>
+
+=cut
+sub create :Path('create') :Args(1) {
+    my ( $self, $c, $db ) = @_;
+
+    my $root_user       = $c->request->params->{root_user};
+    my $root_password   = $c->request->params->{root_password};
+
+    my ( $result, $status_msg ) = $c->model('DB')->create($db, $root_user, $root_password);
+
+    if ( $result ) {
+        $c->response->status(200);
+        $c->stash->{status_msg} = $status_msg;
+    } else {
+        $c->response->status(501);
+        $c->stash->{status_msg} = $status_msg;
+    }
+}
+
+=item schema
+
+Apply the PF MySQL schema to the requested database
+
+Usage: /db/schema/<database_name>
+
+=cut
+sub schema :Path('schema') :Args(1) {
+    my ( $self, $c, $db ) = @_;
+
+    my $root_user       = $c->request->params->{root_user};
+    my $root_password   = $c->request->params->{root_password};
+
+    my ( $result, $status_msg ) = $c->model('DB')->schema($db, $root_user, $root_password );
+
+    if ( $result ) {
+        $c->response->status(200);
+        $c->stash->{status_msg} = $status_msg;
+    } else {
+        $c->response->status(501);
+        $c->stash->{status_msg} = $status_msg;
+    }
+}
+
+=item test
+
+Test the connection to the database server with the provided root user / password.
+
+Will try to connect to the 'mysql' database.
+
+Usage: /db/test
+
+=cut
+sub test :Path('test') :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $root_user       = $c->request->params->{root_user};
+    my $root_password   = $c->request->params->{root_password};
+
+    my ( $result, $status_msg ) = $c->model('DB')->connect('mysql', $root_user, $root_password);
+
+    if ( $result ) {
+        $c->response->status(200);
+        $c->stash->{status_msg} = $status_msg;
+    } else {
+        $c->response->status(501);
+        $c->stash->{status_msg} = $status_msg;
+    }
+}
 
 =back
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Derek Wuelfrath <dwuelfrath@inverse.ca>
 
--- html/configurator/lib/configurator/Model/DB.pm
+++ html/configurator/lib/configurator/Model/DB.pm
@@ -19,15 +19,141 @@ use namespace::autoclean;
 
 extends 'Catalyst::Model';
 
+use constant INSTALL_DIR => '/usr/local/pf';
+use lib INSTALL_DIR . "/lib";
+
+# Package includes
+use DBI;
+
+# PacketFence includes
+use pf::util;
 
 =head1 SUBROUTINES
 
 =over
 
+=item assign
+
+=cut
+sub assign {
+    my ( $self, $dbHandler, $db, $user, $password ) = @_;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+
+    my $status_msg;
+
+    my $sql_query = "GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, LOCK TABLES";
+
+    $dbHandler->do(
+        "$sql_query ON $db.* TO $user\@'%' IDENTIFIED BY '$password'");
+
+    if ( $DBI::errstr ) {
+        $status_msg = "Error creating the user $user on database $db";
+        $logger->warn("$status_msg | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $dbHandler->do(
+        "$sql_query ON $db.* TO $user\@localhost IDENTIFIED BY '$password'");
+
+    if ( $DBI::errstr ) {
+        $status_msg = "Error creating the user $user on database $db";
+        $logger->warn("$status_msg | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $dbHandler->do("FLUSH PRIVILEGES");
+
+    if ( $DBI::errstr ) {
+        $status_msg = "Error creating the user $user on database $db";
+        $logger->warn("$status_msg | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $status_msg = "Successfully created the user $user on database $db";
+    $logger->info("$status_msg");
+    return ( 1, $status_msg );
+}
+
+=item connect
+
+=cut
+sub connect {
+    my ( $self, $db, $user, $password ) = @_;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+
+    my $status_msg;
+
+    my $dbHandler = DBI->connect( "dbi:mysql:dbname=$db;host=localhost;port=3306", $user, $password );
+
+    if ( !$dbHandler ) {
+        $status_msg = "Error in connection to the database $db with user $user";
+        $logger->warn("$status_msg | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $status_msg = "Successfully connected to the database $db with user $user";
+    $logger->info("$status_msg");
+    return ( 1, $status_msg, $dbHandler );
+}
+
+=item create
+
+=cut
+sub create {
+    my ( $self, $db, $root_user, $root_password ) = @_;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+
+    my $status_msg;
+    my $result;
+
+    my $dbDriver = DBI->install_driver("mysql");
+
+    if ( !$dbDriver ) {
+        $status_msg = "Error in creating the database $db";
+        $logger->warn("$status_msg | USER: $root_user | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $result = $dbDriver->func('createdb', $db, 'localhost', $root_user, $root_password, 'admin');
+
+    if ( !$result ) {
+        $status_msg = "Error in creating the database $db";
+        $logger->warn("$status_msg | USER: $root_user | $DBI::errstr");
+        return ( 0, $status_msg );
+    }
+
+    $status_msg = "Successfully created the database $db";
+    $logger->info("$status_msg | USER: $root_user");
+    return ( 1, $status_msg );
+}
+
+=item schema
+
+=cut
+sub schema {
+    my ( $self, $db, $root_user, $root_password ) = @_;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+
+    my $status_msg;
+    my $result;
+
+    my $cmd = "`/usr/bin/mysql -u $root_user -p'$root_password' $db < /usr/local/pf/db/pf-schema.sql`";
+
+    eval { $result = pf_run($cmd) };
+    if ( $@ ) {
+        $status_msg = "Error applying the schema to the database $db";
+        $logger->warn("$status_msg | USER: $root_user");
+        return ( 0, $status_msg );
+    }
+
+    $status_msg = "Successfully applied the schema to the database $db";
+    $logger->info("$status_msg | USER: $root_user");
+    return ( 1, $status_msg );
+}
 
 =back
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Derek Wuelfrath <dwuelfrath@inverse.ca>
 
