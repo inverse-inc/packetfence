d9b5f6a0c1c51a8a81629b6ecd010bb6084b9c17
Francis Lachapelle <flachapelle@inverse.ca>
2012-05-23 09:17:49 -0400

    Added persistence to step 3 (PF config)

---
+++ html/configurator/root/static/app/config.js
@@ -0,0 +1,26 @@
+function registerExists() {
+    $('#tracker a, .form-actions a').click(function(event) {
+        var href = $(this).attr('href');
+        saveStep(href);
+        return false; // don't follow link
+    });
+}
+
+function saveStep(href) {
+    $.ajax({
+        type: 'POST',
+        url: window.location.pathname,
+        data: {'general.domain': $('#general_domain').val(),
+               'general.hostname': $('#general_hostname').val(),
+               'general.dhcpservers': $('#general_dhcpservers').val(),
+               'alerting.emailaddr': $('#alerting_emailaddr').val()}
+    }).done (function(data) {
+        window.location.href = href;
+    }).fail(function(jqXHR) {
+        var obj = $.parseJSON(jqXHR.responseText);
+        showError($('form'), obj.status_msg);
+        $("body").animate({scrollTop:0}, 'fast');
+    });
+
+   return false;
+}
