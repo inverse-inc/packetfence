b4bea697c89aabb5068cb272d0cc8d82e65aa6bc
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-05-24 16:29:02 -0400

    performance: don't fork on every violation_trigger
    Added caching to pf::util's getlocalmac (used by trappable_mac used by
    violation_trigger).
    
    This has the most impact on pfdhcplistener which triggers a violation
    for every MAC and dhcp fingerprint encountered.

--- lib/pf/util.pm
+++ lib/pf/util.pm
@@ -534,8 +534,11 @@ sub getlocalmac {
     return (-1) if ( !$dev );
     return ( $local_mac{$dev} ) if ( defined $local_mac{$dev} );
     foreach (`LC_ALL=C /sbin/ifconfig -a`) {
-        return ( clean_mac($1) )
-            if (/^$dev.+HWaddr\s+(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)/i);
+        if (/^$dev.+HWaddr\s+(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)/i) {
+            # cache the value
+            $local_mac{$dev} = clean_mac($1);
+            return $local_mac{$dev};
+        }
     }
     return (0);
 }
