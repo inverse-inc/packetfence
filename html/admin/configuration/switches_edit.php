e2640b84864d6626742d010a9796442245e8acc7
Francois Gaudreault <fgaudreault@inverse.ca>
2012-06-20 15:55:41 -0400

    Adding Catalyst 3560G support.

--- html/admin/configuration/switches_edit.php
+++ html/admin/configuration/switches_edit.php
@@ -124,6 +124,7 @@
                            'Cisco::Catalyst_3500XL' => 'Cisco Catalyst 3500XL Series',
                            'Cisco::Catalyst_3550' => 'Cisco Catalyst 3550',
                            'Cisco::Catalyst_3560' => 'Cisco Catalyst 3560',
+                           'Cisco::Catalyst_3560G' => 'Cisco Catalyst 3560G',
                            'Cisco::Catalyst_3750' => 'Cisco Catalyst 3750',
                            'Cisco::Catalyst_4500' => 'Cisco Catalyst 4500 Series',
                            'Cisco::Catalyst_6500' => 'Cisco Catalyst 6500 Series',
