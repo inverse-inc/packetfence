inverse-inc/packetfence

commit e134feb87c7bca66fe3a5e5dcb1f2814c73466d1

Fixing the DHCP Listener watch issue (#1490)

lib/pf/services.pm
@@ -223,11 +223,24 @@ sub service_ctl {
             };
             $action eq "status" && do {
                 # -x: this causes the program to also return process id's of shells running the named scripts.
-                my $cmd = "pidof -x $binary";
                 my $pid;
-                chop( $pid = `$cmd` );
-                $pid = 0 if ( !$pid );
-                $logger->info("$cmd returned $pid");
+                # Handle the pfdhcplistener case. Check how much internal interfaces + management we have, and if the number of pids
+                # are not equals this (internal+management), then return 0 to force a restart.
+                if ($exe ne "pfdhcplistener") {
+                    chop( $pid = `pidof -x $exe` );
+                    $pid = 0 if ( !$pid );
+                } else {
+                    my @devs = get_internal_devs_phy();
+                    my $numPids = $#devs+1;
+
+                    $pid = `pidof -x $exe`;
+                    my @pidArray = split(/ /, $pid);
+
+                    if ($#pidArray != $numPids) {
+                       $pid = 0
+                    }
+                }
+                $logger->info("pidof -x $exe returned $pid");
                 return ($pid);
             }
         }

bin/pfcmd
@@ -1225,7 +1225,7 @@ sub service {
             return 1;
         } else {
             if ( !pf::services::service_ctl( $service, "status" ) ) {
-                $command = "start";
+                $command = "restart";
             }
         }
     }

commit 33b4c842493959b571d46d904741362f74cd4cd9

fixed #1515: empty new line injected by pfdhcplistener status
Regression introduced in e134feb87c7bca66fe3a5e5dcb1f2814c73466d1.

lib/pf/services.pm
@@ -231,17 +231,18 @@ sub service_ctl {
             };
             $action eq "status" && do {
                 my $pid;
-                # Handle the pfdhcplistener case. Check how much internal interfaces + management we have, 
-                # and if the number of pids are not equals this (internal+management), then return 0 to force a restart.
                 # -x: this causes the program to also return process id's of shells running the named scripts.
                 if ($binary ne "pfdhcplistener") {
-                    chop( $pid = `pidof -x $binary` );
+                    chomp( $pid = `pidof -x $binary` );
                     $pid = 0 if ( !$pid );
-                } else {
+                }
+                # Handle the pfdhcplistener case. Check how much internal interfaces + management we have,
+                # and if the number of pids are not equals this (internal+management), then return 0 to force a restart.
+                else {
                     my @devs = get_internal_devs_phy();
                     my $numPids = $#devs+1;
 
-                    $pid = `pidof -x $binary`;
+                    $pid = chomp( `pidof -x $binary` );
                     my @pidArray = split(/ /, $pid);
 
                     if ($#pidArray != $numPids) {