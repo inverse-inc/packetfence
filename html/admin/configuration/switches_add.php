99962f312d58a65d4009470c52f6cab0ec807db3
Durand Fabrice <fdurand@inverse.ca>
2012-09-11 16:36:21 -0400

    Feature inline with radius auth

--- html/admin/configuration/switches_add.php
+++ html/admin/configuration/switches_add.php
@@ -208,6 +208,11 @@
         printSelect( array('' => 'please choose', 'yes' => 'Yes', 'no' => 'No'), 'hash', $val, "name='$key'");
         break;
 
+      case 'Enforcement':
+        print "<tr><td></td><td>$pretty_key:</td><td>";
+        printSelect( array('' => 'please choose', 'Vlan' => 'VLAN', 'Inline' => 'INLINE'), 'hash', $val, "name='$key'");
+        break;
+
       default:
         print "<tr><td></td><td>$pretty_key:</td><td><input type='text' name='$key' value='$val'>";
         break;
