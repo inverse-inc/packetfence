2bfc2f656090c55d3139beab0e3e3a73240a99c4
Francis Lachapelle <flachapelle@inverse.ca>
2012-10-17 16:49:54 -0400

    Live form validation in JavaScript
    Currently limited to required fields and number fields.

--- html/pfappserver/root/static/app/application.js
+++ html/pfappserver/root/static/app/application.js
@@ -33,11 +33,45 @@ $(function () {
          }},
         '.btn-toggle');
 
+    /* Activate button dropdowns */
     $('.dropdown-toggle').dropdown();
+
+    /* Activate on/off switches */
+    $('body').on('click', '.onoffswitch', function(event) {
+        var onoffswitch = $(this).find('.onoffswitch-switch');
+        var cssRight = parseInt(onoffswitch.css('right'));
+        var isOn = (cssRight == 0);
+        var checkbox = $(this).find('input');
+
+        checkbox.checked = isOn;
+    });
+
+    /* Activate button groups */
+    $('body').on('click', '.btn-group .btn', function(event) {
+        var btn = $(this);
+        var name = btn.attr('name');
+        var input = btn.prevAll('input[name="' + name + '"]');
+        input.val(btn.attr('value'));
+    });
+
+    /* Live validation for required fields */
+    $('body').on('blur', 'input[data-required]', function() {
+        isFormInputEmpty($(this));
+    });
+
+    /* Live validation for number fields */
+    $('body').on('blur', 'input[type="number"]', function() {
+        var input = $(this);
+        var min = input.attr('min');
+        var max = input.attr('max');
+        if ($.trim(input.val()).length > 0)
+            isInvalidNumber(input, min, max);
+    });
 });
 
 function resetAlert(parent) {
     parent.children('.alert').hide('fast', function() { $(this).remove(); }); //slideUp('fast', function() { $(this).remove(); });
+    parent.children('.error').removeClass('error');
 }
 
 function showSuccess(sibling, msg) {
@@ -47,18 +81,36 @@ function showSuccess(sibling, msg) {
     alert.fadeIn('fast').delay(5000).slideUp('fast', function() { $(this).remove(); });
 }
 
-function showError(sibling, msg) {
-    var alert = $('.alert-error').first().clone();
-    alert.find('span').first().html(msg);
-    sibling.before(alert);
-    alert.fadeIn('fast').delay(10000).slideUp('fast', function() { $(this).remove(); });
+function showError(sibling, msg, permanent) {
+    if (typeof msg == 'string') {
+        var alert = $('.alert-error').first().clone();
+        alert.find('span').first().html(msg);
+        sibling.before(alert);
+        if (permanent)
+            alert.fadeIn('fast');
+        else
+            alert.fadeIn('fast').delay(10000).slideUp('fast', function() { $(this).remove(); });
+    }
+    else {
+        $.each(msg, function(name, error) {
+            var input = $('[name="' + name + '"]');
+            var control = input.closest('.control-group');
+            control.addClass('error');
+            showTab(control, input);
+
+            var alert = $('.alert-error').first().clone();
+            alert.find('span').first().html(error);
+            sibling.before(alert);
+            if (permanent)
+                alert.fadeIn('fast');
+            else
+                alert.fadeIn('fast').delay(10000).slideUp('fast', function() { $(this).remove(); });
+        });
+    }
 }
 
 function showPermanentError(sibling, msg) {
-    var alert = $('.alert-error').first().clone();
-    alert.find('span').first().html(msg);
-    sibling.before(alert);
-    alert.fadeIn('fast');
+    showError(sibling, msg, true);
 }
 
 function btnError(btn) {
@@ -102,7 +154,8 @@ function isInvalidNumber(input, min, max) {
 
     if (/^[0-9]+$/.test($.trim(input.val()))) {
         var value = parseInt(input.val());
-        if (value < min || value > max)
+        if (typeof min != "undefined" && value < min ||
+            typeof max != "undefined" && value > max)
             isInvalid = true;
     }
     else {
@@ -119,6 +172,19 @@ function isInvalidNumber(input, min, max) {
     return isInvalid;
 }
 
+function isFormValid(form) {
+    var valid = true;
+    form.find('input[data-required], input[type="number"]').each(function() {
+        var input = $(this);
+        var control = input.closest('.control-group');
+        input.trigger('blur');
+        valid = !control.hasClass('error');
+
+        return valid;
+    });
+    return valid;
+}
+
 function switchIsOn(input) {
     var onoffswitch = input.closest('.onoffswitch').find('.onoffswitch-switch');
     var cssRight = parseInt(onoffswitch.css('right'));
@@ -132,9 +198,9 @@ function showTab(control, input) {
         var a = tab.closest('form').find('.nav-tabs a[href="#' + tab.attr('id') + '"]');
         a.tab('show');
         // Scroll to the input
-        var container = tab.closest('.modal-body');
-        if (!container)
-            container = $("body,html");
-        container.animate({scrollTop: input.position().top}, 'fast');
+//        var container = tab.closest('.modal-body');
+//        if (!container)
+//            container = $("body,html");
+//        container.animate({scrollTop: input.position().top}, 'fast');
     }
 }
