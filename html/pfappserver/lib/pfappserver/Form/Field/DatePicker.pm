e1fbfcd7c6bab01f2f4a8d0365eb9ecb0894478d
Francis Lachapelle <flachapelle@inverse.ca>
2012-10-22 15:49:25 -0400

    New F::F::DatePicker, used by F::F::DateTimePicker

---
+++ html/pfappserver/lib/pfappserver/Form/Field/DatePicker.pm
@@ -0,0 +1,49 @@
+package pfappserver::Form::Field::DatePicker;
+
+=head1 NAME
+
+pfappserver::Form::Field::DatePicker - to be used with date picker
+JavaScript widget
+
+=head1 DESCRIPTION
+
+This field is simply a text field to be formatted by a theme
+(Form::Widget::Theme::Pf).
+
+=cut
+
+use Moose;
+extends 'HTML::FormHandler::Field::Text';
+use namespace::autoclean;
+
+=head1 DESCRIPTION
+
+This field returns Y if true, N if false.
+
+=cut
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
