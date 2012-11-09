bc9394fbadaaee9bb04531c044f66a0753d044d7
Francis Lachapelle <flachapelle@inverse.ca>
2012-11-09 12:39:15 -0500

    Handling of auth rules conditions & actions
    The selection of rules conditions and rules actions will now refresh the
    associated fields to define the condition or rule.

--- html/pfappserver/root/static/admin/configuration/authentication.js
+++ html/pfappserver/root/static/admin/configuration/authentication.js
@@ -1,7 +1,7 @@
 function initAuthentication() {
 
     /* Save the sources list order */
-    $('#section').on('admin.ordered', 'table', function(event) {
+    $('#section').on('admin.ordered', '#sources', function(event) {
         var form = $(this).closest('form');
         $.ajax({
             type: 'POST',
@@ -140,18 +140,6 @@ function initAuthentication() {
         return false;
     });
 
-    /* Initial creation of a condition when no condition is defined */
-    $('body').on('click', '#ruleConditionsEmpty [href="#add"]', function(event) {
-        var tbody = $('#ruleConditions').children('tbody');
-        var row_model = tbody.children('.hidden').first();
-        if (row_model) {
-            $('#ruleConditionsEmpty').addClass('hidden');
-            var row_new = row_model.clone();
-            row_new.removeClass('hidden');
-            row_new.insertBefore(row_model);
-        }
-    });
-
     /* Show a rule */
     $('#section').on('click', '#sourceRules a:not(.btn-icon)', function(event) {
         var modal = $('#modalRule');
@@ -247,5 +235,120 @@ function initAuthentication() {
         }
 
         return false;
+    });
+
+    /* Initial creation of a rule condition when no condition is defined */
+    $('body').on('click', '#ruleConditionsEmpty [href="#add"]', function(event) {
+        var tbody = $('#ruleConditions').children('tbody');
+        var row_model = tbody.children('.hidden').first();
+        if (row_model) {
+            $('#ruleConditionsEmpty').addClass('hidden');
+            var row_new = row_model.clone();
+            row_new.removeClass('hidden');
+            row_new.insertBefore(row_model);
+            row_new.trigger('admin.added');
+        }
+    });
+
+    /* Initialize the rule condition and action fields when displaying a rule */
+    $('#section').on('shown', '#modalRule', function(event) {
+        $('#templates').find('option').removeAttr('id');
+        $('#ruleConditions tr:not(.hidden) select[name$=attribute]').each(function() {
+            updateCondition($(this));
+        });
+        $('#ruleActions tr:not(.hidden) select[name$=type]').each(function() {
+            updateAction($(this));
+        });
+    });
+
+    /* Initialize the rendering widgets of some elements */
+    function initWidgets(elements) {
+        elements.filter('.chzn-select').chosen();
+        elements.filter('.chzn-deselect').chosen({allow_single_deselect: true});
+        elements.filter('.timepicker-default').each(function() {
+            // Keep the placeholder visible if the input has no value
+            var defaultTime = $(this).val().length? 'value' : false;
+            $(this).timepicker({ defaultTime: defaultTime, showSeconds: false, showMeridian: false });
+        });
+        elements.filter('.datepicker').datepicker({ autoclose: true });
+    }
+
+    /* Update a rule condition input field depending on the type of the selected attribute */
+    function updateCondition(attribute) {
+        var type = attribute.find(':selected').attr('data-type');
+        var operator = attribute.next();
+
+        if (type != operator.attr('data-type')) {
+            // Disable fields to be replaced
+            var value = operator.next();
+            operator.attr('disabled', 1);
+            value.attr('disabled', 1);
+
+            // Replace operator field
+            var operator_new = $('#' + type + '_operator').clone();
+            operator_new.attr('id', operator.attr('id'));
+            operator_new.attr('name', operator.attr('name'));
+            operator_new.insertBefore(operator);
+
+            // Replace value field
+            var value_new = $('#' + type + '_value').clone();
+            value_new.attr('id', value.attr('id'));
+            value_new.attr('name', value.attr('name'));
+            value_new.insertBefore(value);
+
+            if (!operator.attr('data-type')) {
+                // Preserve values of an existing condition
+                operator_new.val(operator.val());
+                value_new.val(value.val());
+            }
+
+            // Remove previous fields
+            value.remove();
+            operator.remove();
+
+            // Remember the data type
+            operator_new.attr('data-type', type);
+
+            // Initialize rendering widgets
+            initWidgets(value_new);
+        }
+    }
+
+    /* Update the rule condition fields when adding a new condition */
+    $('#section').on('admin.added', '#ruleConditions tr', function(event) {
+        var attribute = $(this).find('select[name$=attribute]').first();
+        updateCondition(attribute);
+    });
+
+    /* Update the rule condition fields when changing a condition attribute */
+    $('#section').on('change', '#ruleConditions select[name$=attribute]', function(event) {
+        updateCondition($(this));
+    });
+
+    /* Update a rule action input field depending on the selected action type */
+    function updateAction(type) {
+        var action = type.val();
+        var value = type.next();
+
+        // Replace value field
+        var value_new = $('#' + action + '_action').clone();
+        value_new.attr('id', value.attr('id'));
+        value_new.attr('name', value.attr('name'));
+        value_new.insertBefore(value);
+
+        if (value.is('[type="hidden"]')) {
+            value_new.val(value.val());
+        }
+
+        // Remove previous field
+        value.remove();
+
+        // Initialize rendering widgets
+        initWidgets(value_new);
+    }
+
+    /* Update the rule action fields when changing an action type */
+    $('#section').on('change', '#ruleActions select[name$=type]', function(event) {
+        updateAction($(this));
     });
 }
