140305af3eeee65a481ed8b5e190d29f9b8bfe40
Francis Lachapelle <flachapelle@inverse.ca>
2012-11-09 12:39:15 -0500

    Handling of auth rules conditions & actions
    The selection of rules conditions and rules actions will now refresh the
    associated fields to define the condition or rule.

--- html/pfappserver/lib/pfappserver/Form/Authentication/Rule.pm
+++ html/pfappserver/lib/pfappserver/Form/Authentication/Rule.pm
@@ -15,6 +15,8 @@ use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler';
 with 'pfappserver::Form::Widget::Theme::Pf';
 
+use pf::config;
+use pf::Authentication::constants;
 use pf::Authentication::Action;
 
 has '+field_name_space' => ( default => 'pfappserver::Form::Field' );
@@ -65,18 +67,13 @@ has_field 'conditions.attribute' =>
 has_field 'conditions.operator' =>
   (
    type => 'Select',
-   options =>
-   [
-    { value => 'equals', label => 'equals' },
-    { value => 'contains', label => 'contains' },
-   ],
+   options_method => \&options_operators,
    widget_wrapper => 'None',
    element_class => ['span3'],
   );
 has_field 'conditions.value' =>
   (
-   type => 'Text',
-   element_class => ['span5'],
+   type => 'Hidden',
   );
 has_field 'actions' =>
   (
@@ -92,7 +89,111 @@ has_field 'actions.type' =>
   );
 has_field 'actions.value' =>
   (
+   type => 'Hidden',
+  );
+
+# The templates block contains the dynamic fields of the rule definition.
+#
+# The following fields depend on the selected condition attribute :
+#  - the condition operators select fields
+#  - the condition value fields
+# The following fields depend on the selected action type :
+#  - the action value fields
+#
+# The field substitution is made through JavaScript.
+
+has_block 'templates' =>
+  (
+   tag => 'div',
+   render_list => [
+                   map( { ("${_}_operator", "${_}_value") } keys %Conditions::OPERATORS),
+                   map( { "${_}_action" } @Actions::ACTIONS),
+                  ],
+   attr => { id => 'templates' },
+   class => [ 'hidden' ],
+  );
+has_field "${Conditions::STRING}_operator" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+   options_method => \&operators,
+   element_class => ['span3'],
+  );
+has_field "${Conditions::STRING}_value" =>
+  (
    type => 'Text',
+   do_label => 0,
+   wrapper => 0,
+   element_class => ['span5'],
+  );
+has_field "${Conditions::NUMBER}_operator" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+   options_method => \&operators,
+   element_class => ['span3'],
+ );
+has_field "${Conditions::NUMBER}_value" =>
+  (
+   type => 'PosInteger',
+   do_label => 0,
+   wrapper => 0,
+   element_class => ['span5'],
+  );
+has_field "${Conditions::DATE}_operator" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+   options_method => \&operators,
+   element_class => ['span3'],
+  );
+has_field "${Conditions::DATE}_value" =>
+  (
+   type => 'DatePicker',
+   do_label => 0,
+   wrapper => 0,
+  );
+has_field "${Conditions::TIME}_operator" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+   options_method => \&operators,
+   element_class => ['span3'],
+  );
+has_field "${Conditions::TIME}_value" =>
+  (
+   type => 'TimePicker',
+   do_label => 0,
+   wrapper => 0,
+   element_class => ['span5'],
+  );
+has_field "${Actions::MARK_AS_SPONSOR}_action" =>
+  (
+   type => 'Hidden',
+  );
+has_field "${Actions::SET_ACCESS_LEVEL}_action" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+   options_method => \&options_access_level,
+  );
+has_field "${Actions::SET_ROLE}_action" =>
+  (
+   type => 'Select',
+   do_label => 0,
+   wrapper => 0,
+#   options_method => \&options_roles,
+  );
+has_field "${Actions::SET_UNREG_DATE}_action" =>
+  (
+   type => 'DatePicker',
+   do_label => 0,
+   wrapper => 0,
   );
 
 =head2 options_attributes
@@ -106,9 +207,29 @@ sub options_attributes {
     my $self = shift;
     
     my $form = $self->form;
-    my @attributes = map { $_ => $_ } @{$form->attrs} if ($form->attrs);
+    my @attributes = map {{
+        label => $_->{value},
+        value => $_->{value},
+        attributes => { 'data-type' => $_->{type} }
+    }} @{$form->attrs} if ($form->attrs);
 
     return @attributes;
+}
+
+=head2 options_operators
+
+Populate the operators select field with all possible options.
+The options will be later limited using JavaScript when displaying the rule.
+
+=cut
+
+sub options_operators {
+    my $self = shift;
+
+    my %all_operators = map { map { $_ => 1 } @{$_} } values %Conditions::OPERATORS;
+    my @options = map { $_ => $self->_localize($_) } keys %all_operators;
+
+    return @options;
 }
 
 =head2 options_actions
@@ -122,9 +243,44 @@ sub options_actions {
     my $self = shift;
 
     my $actions_ref = pf::Authentication::Action::availableActions();
-    my @actions = map { $_ => $_ } @{$actions_ref};
+    my @actions = map { $_ => $self->_localize($_) } @{$actions_ref};
 
     return @actions;
+}
+
+=head2 operators
+
+Return the appropriate operators for the condition type select field.
+
+=cut
+
+sub operators {
+    my $self = shift;
+
+    my ($type) = $self->name =~ m/^([^_]+)_operator$/;
+    my @operators = map { $_ => $self->_localize($_) } @{$Conditions::OPERATORS{$type}};
+
+    return @operators;
+}
+
+=head2 options_access_level
+
+Populate the select field for the 'access level' template action.
+
+=cut
+
+sub options_access_level {
+    my $self = shift;
+
+    return ({
+             label => $self->_localize('None'),
+             value => $WEB_ADMIN_NONE,
+            },
+            {
+             label => $self->_localize('All'),
+             value => $WEB_ADMIN_ALL,
+            },
+           );
 }
 
 =head1 COPYRIGHT
