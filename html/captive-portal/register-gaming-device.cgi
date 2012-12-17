e14cd092d634276b2fc2f0030c2e4a1b961abcb6
James Rouzier <jrouzier@inverse.ca>
2012-12-17 10:40:37 -0500

    Added gaming registration page

---
+++ html/captive-portal/register-gaming-device.cgi
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+
+# CUSTOM: this page is all custom, started as a copy of register.cgi
+
+use CGI::Carp qw( fatalsToBrowser );
+use CGI;
+use Log::Log4perl;
+use strict;
+use warnings;
+
+use constant INSTALL_DIR => '/usr/local/pf';
+use lib INSTALL_DIR . "/lib";
+use pf::config;
+use pf::iplog;
+use pf::util;
+use pf::web::gaming;
+use pf::Portal::Session;
+use pf::web;
+use Data::Dumper;
+use Readonly;
+Readonly our $GAMING_CATEGORY  => "gaming_console";
+Readonly our $SCRIPT => 'register-gaming-device.cgi';
+
+
+
+sub main {
+    Log::Log4perl->init("$conf_dir/log.conf");
+    my $logger = Log::Log4perl->get_logger($SCRIPT);
+    Log::Log4perl::MDC->put('proc', $SCRIPT);
+    Log::Log4perl::MDC->put('tid', 0);
+
+    my %params;
+    my $portalSession   = new pf::Portal::Session();
+    my $cgi             = $portalSession->cgi;
+    my $session         = $portalSession->session;
+    my $ip              = $cgi->remote_addr;
+    my $mac             = $portalSession->getClientMac();
+
+    my %info;
+
+    # Pull username
+#    $logger->info(Dumper(\$cgi));
+
+    # Pull browser user-agent string
+    $info{'user_agent'}=$cgi->user_agent;
+
+    # pull parameters from query string
+    foreach my $param($cgi->url_param()) {
+        $params{$param} = $cgi->url_param($param);
+    }
+    foreach my $param($cgi->param()) {
+        $params{$param} = $cgi->param($param);
+    }
+
+    my $pid = $session->param("login");
+    #See if user is try to login and is not already authenticated
+    if(!$pid && $cgi->param('username') ne '' && $cgi->param('password') ne '') {
+      my ($auth_return,$err) = pf::web::gaming::web_student_authenticate($portalSession,$cgi, $session, \%info, $logger);
+      if ($auth_return != 1) {
+        pf::web::gaming::generate_login_page($portalSession);
+      }
+      else {
+        pf::web::gaming::generate_registration_page($portalSession);
+      }
+    }
+    #Verify is user is authenticated
+    elsif (!$pid) {
+      pf::web::gaming::generate_login_page($portalSession);
+    }
+    elsif (exists $params{cancel} )  {
+        $session->delete();
+        pf::web::gaming::generate_login_page($portalSession);
+    }
+    #User is authenticated and requesting to register gaming device
+    elsif (exists $params{'device_mac'}) {
+        $info{pid} = $pid;
+        my $device_mac = $params{'device_mac'};
+        $portalSession->stash->{device_mac} = $device_mac;
+        # register gaming device
+        $info{'category'} = $GAMING_CATEGORY;
+        my ($result,$msg) = pf::web::gaming::register_node($portalSession, $pid,$device_mac, %info);
+        if($result) {
+            pf::web::gaming::generate_landing_page($portalSession,$msg);
+            $portalSession->session->delete();
+        }
+        else {
+            pf::web::gaming::generate_registration_page($portalSession,$msg);
+        }
+    }
+    #User is authenticated so display registration page
+    else {
+      pf::web::gaming::generate_registration_page($portalSession);
+    }
+    exit(0);
+}
+
+main();
+
---
+++ lib/pf/web/gaming.pm
@@ -0,0 +1,168 @@
+package pf::web::gaming;
+=head1 NAME
+
+pf::web::gaming add documentation
+
+=cut
+
+=head1 DESCRIPTION
+
+gaming_registration
+
+=cut
+
+use strict;
+use warnings;
+use pf::web;
+use pf::config;
+use Readonly;
+use HTML::Entities;
+use Data::Dumper;
+use pf::enforcement qw(reevaluate_access);
+use pf::node qw(node_register is_max_reg_nodes_reached);
+
+use pf::config;
+use pf::util;
+
+Readonly our $GAMING_LOGIN_TEMPLATE   => 'gaming-login.html';
+Readonly our $GAMING_LANDING_TEMPLATE => 'gaming-landing.html';
+Readonly our $GAMING_REGISTRATION_TEMPLATE => 'gaming-registration.html';
+Readonly our @GAMING_OUI => (
+    '00:12:5A:' ,  #          Microsoft-Xbox
+    '00:0D:3A:' ,  #          Microsoft-Xbox
+    '00:50:F2:' ,  #          Microsoft-Xbox
+    '00:01:4A:' ,  #          Sony-PS2-PSP
+    '00:02:C7:' ,  #          Sony-PS2-PSP
+    '00:04:1F:' ,  #          Sony-PS2-PSP
+    '00:13:15:' ,  #          Sony-PS2-PSP
+    '00:09:BF:' ,  #          Nintendo-Wii
+    '00:17:AB:' ,  #          Nintendo-Wii
+    '00:17:FA:' ,  #          Microsoft-Xbox
+    '00:15:C1:' ,  #          Sony-PS3
+    '00:19:C5:' ,  #          Sony-PS3
+    '00:1D:D8:' ,  #          Microsoft-Xbox
+    '00:0B:E6:' ,  #          Nintendo-Wii
+    '00:16:56:' ,  #          Nintendo-Wii
+    '00:1A:E9:' ,  #          Nintendo-Wii
+    '00:1D:0D:' ,  #          Sony-PS3
+    '00:19:1D:' ,  #          Nintendo-Wii
+    '00:19:FD:' ,  #          Nintendo-Wii
+    '00:1F:32:' ,  #          Nintendo-Wii
+    '00:1C:BE:' ,  #          Nintendo-Wii
+    '00:1B:EA:' ,  #          Nintendo-Wii
+    '00:1E:35:' ,  #          Nintendo-Wii
+    '00:1B:7A:' ,  #          Ninetndo-Wii
+    '00:22:48:' ,  #          Microsoft-Xbox
+);
+
+sub web_student_authenticate {
+    my ($portalSession,$cgi, $session, $info,$logger) = @_;
+    my ($auth_return,$err) = pf::web::web_user_authenticate($portalSession, $cgi->param("auth"));
+    if ($auth_return == 1) {
+        $info->{'category'} = $portalSession->getProfile->getGuestCategory;
+        $session->param(login => $cgi->param('username'));
+    }
+    return ( $auth_return, $err );
+}
+
+sub generate_login_page {
+    my ( $portalSession, $err ) = @_;
+    _generate_page($portalSession,$GAMING_LOGIN_TEMPLATE,
+        txt_auth_error => ( (defined $err) ? i18n($err) : undef)  ,
+        username => encode_entities($portalSession->cgi->param("username")),
+        selected_auth => ( encode_entities($portalSession->cgi->param("auth")) || $portalSession->getProfile->getDefaultAuth),
+        list_authentications => pf::web::auth::list_enabled_auth_types(),
+        oauth2_google => $guest_self_registration{$SELFREG_MODE_GOOGLE},
+        oauth2_facebook => $guest_self_registration{$SELFREG_MODE_FACEBOOK},
+        oauth2_github => $guest_self_registration{$SELFREG_MODE_GITHUB},
+    );
+}
+
+sub generate_landing_page {
+    my ($portalSession, $msg) = @_;
+    _generate_page($portalSession,$GAMING_LANDING_TEMPLATE, status_msg => ( (defined $msg ) ? i18n($msg) : undef));
+}
+
+sub generate_registration_page {
+    my ($portalSession, $err) = @_;
+    _generate_page($portalSession,$GAMING_REGISTRATION_TEMPLATE, txt_auth_error => ( (defined $err) ? i18n($err) : undef));
+}
+
+sub _generate_page {
+    my ($portalSession, $template,%args) = @_;
+    my @keys = keys %args;
+    @{$portalSession->stash}{@keys} = @args{@keys};
+    render_template($portalSession, $template);
+}
+
+sub register_node {
+    my ( $portalSession, $pid, $mac, %info ) = @_;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+    my ($result,$msg);
+    if(!valid_mac($mac) || !is_gaming_mac($mac)) {
+        $msg = "Please verify Mac address provided";
+    }
+    elsif ( is_max_reg_nodes_reached($mac, $pid, $info{'category'}) ) {
+        $msg = "You have reached the maximum number of devices you are able to register with this username.";
+    }
+    else {
+        ($result,$msg) = _sanitize_and_register($portalSession->session, $mac, $pid, %info);
+    }
+    return ($result,$msg);
+}
+
+sub is_gaming_mac {
+    my ($mac) = @_;
+    foreach my $oui (@GAMING_OUI) {
+        return 1 if $mac =~ /^\Q$oui\E/i;
+    }
+    return 0;
+}
+
+sub _sanitize_and_register {
+    my ( $session, $mac, $pid, %info ) = @_;
+    my $logger = Log::Log4perl::get_logger('pf::web');
+    my ($result,$msg);
+    if(valid_mac($mac)) {
+        $logger->info("performing node registration Mac: $mac pid: $pid");
+        node_register( $mac, $pid, %info );
+        $result = $TRUE;
+        $msg = "The Mac address $mac has been successfully registered.";
+    }
+    else {
+        $msg = "The Mac address $mac provided is invalid please try agian";
+    }
+    return ($result,$msg);
+}
+
+=back
+
+=head1 AUTHOR
+
+YOUR NAME <flachapelle@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
