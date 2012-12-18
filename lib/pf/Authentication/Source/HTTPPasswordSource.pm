6693a3859c6b0d5d8ffc5ac9460725eaf990168a
Ludovic Marcotte <lmarcotte@inverse.ca>
2012-12-18 09:26:00 -0500

    Refactored to move most of the logic in the super class.

--- lib/pf/Authentication/Source/HTTPPasswordSource.pm
+++ lib/pf/Authentication/Source/HTTPPasswordSource.pm
@@ -56,74 +56,33 @@ sub authenticate {
 =item match
 
 =cut
-sub match {
-    my ( $self, $params ) = @_;
-    my $common_attributes = $self->SUPER::common_attributes();
-
-    my $logger = Log::Log4perl->get_logger('pf::authentication');
+sub match_in_subclass {
+    
+    my ($self, $params, $rule, $own_conditions, $matching_conditions) = @_;
+    
+    my $logger = Log::Log4perl->get_logger( __PACKAGE__ );
     $logger->info("Matching rules in htpasswd file.");
 
-    my @matching_rules = ();
-
-    foreach my $rule ( @{$self->{'rules'}} ) {
-        my @matching_conditions = ();
-        my @own_conditions = ();
-        if (!defined $rule->{'conditions'} || scalar @{$rule->{'conditions'}} == 0) {
-            push(@matching_rules, $rule);
-            goto done;
-        }
-
-        foreach my $condition ( @{$rule->{'conditions'}} ) {
-            if (grep {$_->{value} eq $condition->attribute } @$common_attributes) {
-                my $r = $self->SUPER::match_condition($condition, $params);
-	
-                if ($r == 1) {
-                    push(@matching_conditions, $condition);
-                }
-            } elsif (grep {$_->{value} eq $condition->attribute } @{$self->available_attributes()}) {
-                push(@own_conditions, $condition);
-            }
-        }                       # foreach my $condition (...)
-    
-        # We're done looping, let's match the htpasswd conditions alltogether. Normally, we
-        # should only have a condition based on the username attribute.
-        foreach my $condition (@own_conditions) {
-            if ($condition->{'attribute'} eq "username") {
-                # Let's check if our matching username is found in our htpasswd file
-                my $password_file = $self->{'path'};
-                if (-r $password_file) {
+    # Let's match the htpasswd conditions alltogether. Normally, we
+    # should only have a condition based on the username attribute.
+    foreach my $condition (@{ $own_conditions }) {
+        if ($condition->{'attribute'} eq "username") {
+            # Let's check if our matching username is found in our htpasswd file
+            my $password_file = $self->{'path'};
+            if (-r $password_file) {
+                
+                my $htpasswd = new Apache::Htpasswd({ passwdFile => $password_file, ReadOnly   => 1});
+                
+                if ( defined($htpasswd->fetchPass($params->{'username'})) ) {
                     
-                    my $htpasswd = new Apache::Htpasswd({ passwdFile => $password_file, ReadOnly   => 1});
-
-                    if ( defined($htpasswd->fetchPass($params->{'username'})) ) {
-                        
-                        # Now let's see if the condition actually matches
-                        if ( $condition->matches("username", $params->{'username'}) ) {
-                            push(@matching_conditions, $condition);
-                        }
+                    # Now let's see if the condition actually matches
+                    if ( $condition->matches("username", $params->{'username'}) ) {
+                        push(@{ $matching_conditions }, $condition);
                     }
                 }
             }
         }
-  
-        # We compare the matched conditions with how many we had
-        if ($rule->match eq pf::Authentication::Rule->ANY &&
-            scalar @matching_conditions > 0) {
-            push(@matching_rules, $rule);
-        } elsif ($rule->match eq pf::Authentication::Rule->ALL &&
-                 scalar @matching_conditions == scalar @{$rule->{'conditions'}}) {
-            push(@matching_rules, $rule);
-        }
-
-        # For now, we return the first matching rule. We might change this in the future
-        # so let's keep the @matching_rules array for now.
-        done:
-        if (scalar @matching_rules == 1) {
-            $logger->info("Matched rule ($rule->{'description'}), returning actions.");
-            return $rule->{'actions'};
-        }
-	
-    }                     # foreach my $rule ( @{$self->{'rules'}} ) {
+    }
 
     return undef;
 }
