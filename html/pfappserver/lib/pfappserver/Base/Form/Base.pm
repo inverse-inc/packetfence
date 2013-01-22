73cd81fb536703e56cab578e2ca947ea55a31ac3
James Rouzier <jrouzier@inverse.ca>
2013-01-22 10:30:38 -0500

    Form Base Class

---
+++ html/pfappserver/lib/pfappserver/Base/Form/Base.pm
@@ -0,0 +1,50 @@
+package pfappserver::Base::Form::Base;
+=head1 NAME
+
+/usr/local/pf/html/pfappserver/lib/pfappserver/Base/Form add documentation
+
+=cut
+
+=head1 DESCRIPTION
+
+Base
+
+=cut
+
+use strict;
+use warnings;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+with 'pfappserver::Form::Widget::Theme::Pf';
+
+has '+field_name_space' => ( default => 'pfappserver::Form::Field' );
+has '+widget_name_space' => ( default => 'pfappserver::Form::Widget' );
+has '+language_handle' => ( builder => 'get_language_handle_from_ctx' );
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
