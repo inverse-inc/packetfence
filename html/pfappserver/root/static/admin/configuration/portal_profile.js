7a0bcd096ac64e139c54f8ca8b3ff2b6098bc295
James Rouzier <jrouzier@inverse.ca>
2013-02-04 11:20:52 -0500

    Rename delete button and reformatted file

--- html/pfappserver/root/static/admin/configuration/portal_profile.js
+++ html/pfappserver/root/static/admin/configuration/portal_profile.js
@@ -46,7 +46,7 @@ function initSaveModal(editor,file_content) {
     var save_button = save_modal.find('a.btn-primary').first();
     save_button.off('click');
 
-    save_modal.click(function(event) {
+    save_button.click(function(event) {
         var form     = $('#file_editor_form');
         file_content.val(editor.getValue());
         submitFormHideModal(save_modal,form);
@@ -62,12 +62,10 @@ function initCancelModal() {
 
 function initEditor(editor,file_content) {
     var cancel_link = $("#cancel-link");
-    var file_editor_save = $('#file-editor-save');
-    var file_editor_reset = $('#file-editor-reset');
+    var file_editor_buttons = $('.form-actions a.btn');
     var enableButtonsOnChangeOnce = function(e) {
         cancel_link.attr('data-toggle','modal');
-        file_editor_save.removeClass('disabled');
-        file_editor_reset.removeClass('disabled');
+        file_editor_buttons.removeClass('disabled');
         editor.removeEventListener("change",enableButtonsOnChangeOnce);
     };
 
@@ -80,29 +78,22 @@ function initEditor(editor,file_content) {
     $('#resetContent').find('a.btn-primary').first().click(function(event) {
         editor.setValue(file_content.val(),-1);
         cancel_link.removeAttr('data-modal');
-        file_editor_save.addClass('disabled');
-        file_editor_reset.addClass('disabled');
+        file_editor_buttons.addClass('disabled');
         editor.on("change",enableButtonsOnChangeOnce);
     });
 }
 
-function initEditorPage() {
-    var file_content = $('#file_content');
-    var editor = ace.edit("editor");
-    initVariableList(editor);
-    initShowLinesCheckBox(editor);
-    initSaveModal(editor,file_content);
-    initCancelModal();
-    initEditor(editor,file_content);
-    $('#section').on('dblclick','#file_name',function(event){
-        var that = $(this);
-        var input_span = that.next().first();
-        var width_span = input_span.next().first();
-        var input = input_span.children().first();
+function initRenameForm(element) {
+    var file_name_span = $('#file_name');
+    var input_span = file_name_span.next().first();
+    var width_span = input_span.next().first();
+    var input = input_span.children().first();
+    var rename_form = $('#rename_file');
+    element.on('dblclick','#file_name',function(event){
         width_span.html(input.val());
         input.width(width_span.width() + 3);
         input_span.removeClass('hidden');
-        that.addClass('hidden');
+        file_name_span.addClass('hidden');
         input.focus();
     });
     $('#new_file_name').keyup(function(event){
@@ -110,40 +101,69 @@ function initEditorPage() {
         if (event.keyCode == 27) {
             input.focusout();
         } else {
-            var width_span = input.closest('span').next().first();
             width_span.html(input.val());
             input.width(width_span.width() + 3);
         }
     });
     $('#new_file_name').focusout(function(event){
-        var file_name_span = $('#file_name');
-        var input_span = file_name_span.next().first();
-        var width_span = input_span.next().first();
-        var input = input_span.children().first();
         width_span.html(input.val());
         input.width(width_span.width() + 2);
         input_span.addClass('hidden');
         file_name_span.removeClass('hidden');
-        $('#rename_file')[0].reset();
+        rename_form[0].reset();
     });
 }
 
-function swapClass(elem) {
-    var data_swap = elem.attr('data-swap');
-    var class_val = elem.attr('class');
-    elem.attr('data-swap',class_val);
-    elem.attr('class',data_swap);
+function initPreviewFile(element) {
+    var previewFile = $('#previewFile');
+    previewFile.on('hidden',function() {
+        $(this).data('modal').$element.removeData();
+    });
+    previewFile.on('shown',function() {
+        var that = $(this);
+        var modal_body = that.find('.modal-body');
+        modal_body.on('ready',function(){
+/*
+            alert(modal_body.innerHtml);
+            var iframe = modal_body.find('iframe');
+            var doc =  iframe[0].contentDocument;
+            var html = doc.find("html");
+            var scrollWidth = html[0].scrollWidth;
+            var scrollHeight = html[0].scrollHeight;
+            previewFile.width((scrollWidth + 40 ) + "px");
+            previewFile.height((scrollHeight + 60 ) + "px");
+            modal_body.width((scrollWidth + 40 ) + "px");
+            modal_body.height((scrollHeight + 60 ) + "px");
+            iframe.width((scrollWidth + 40 ) + "px");
+            iframe.height((scrollHeight + 60 ) + "px");
+*/
+        });
+    });
 }
 
-function initSwapClass(element) {
+function initEditorPage(element) {
+    var file_content = $('#file_content');
+    var editor = ace.edit("editor");
+    initVariableList(editor);
+    initShowLinesCheckBox(editor);
+    initSaveModal(editor,file_content);
+    initCancelModal();
+    initEditor(editor,file_content);
+    initRenameForm(element);
+    initPreviewFile(element);
+}
+
+function initCollapse(element) {
     element.on('show hidden','.collapse',function(event) {
+        console.log(this);
+        console.log(event);
         var that = $(this);
+        console.log(that);
         var tr = that.closest('tr').first();
-        swapClass(tr);
+        console.log(tr);
+        tr.swap_class('toggle');
         var link = element.find('[data-target="#' + that.attr('id') + '"]');
-        link.find('*[data-swap]').each(function(i,elem){
-            swapClass($(elem));
-        });
+        link.find('[data-swap]').swap_class('toggle');
         event.stopPropagation();//To stop the event from closing parents
     });
 }
@@ -203,25 +223,45 @@ function initNewFileModal(element) {
     });
 }
 
+function disabledLinks(element) {
+    element.on('click','.disabled',function() { return false;});
+}
+
 function initIndexPage(element) {
+}
+
+function initCreatePage(element) {
+    var modal = $('#saveProfile');
+    var button = modal.find('.btn-primary').first();
+    button.off("click");
+    button.click(function(event) {
+        var form = element.find("#create_profile");
+        submitFormHideModalGoToLocation(modal,form);
+    });
 }
 
 function initTemplatesPage(element) {
     initCopyModal(element);
-    initSwapClass(element);
+    initCollapse(element);
     initNewFileModal(element);
+}
+
+function portalProfileGlobalInit(element) {
+    disabledLinks(element);
 }
 
 $('#section').on('section.loaded',function(event) {
     var initializers = [
-        {id : "#file_editor", initializer: initEditorPage},
+        {id : "#portal_profile_file_editor", initializer: initEditorPage},
         {id : "#portal_profile_files", initializer: initTemplatesPage },
         {id : "#portal_profile_index", initializer: initIndexPage },
+        {id : "#portal_profile_create", initializer: initCreatePage },
     ];
     for (var i = 0; i < initializers.length; i++) {
         var initializer = initializers[i];
         var element = $(initializer.id);
         if (element.length > 0) {
+            portalProfileGlobalInit(element);
             initializer.initializer(element);
         }
     }
