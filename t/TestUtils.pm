47b63f562728286d33f718ad8ad442ace16038d5
James Rouzier <jrouzier@inverse.ca>
2013-04-04 03:00:27 -0400

    Removed unused test

--- t/TestUtils.pm
+++ t/TestUtils.pm
@@ -22,9 +22,9 @@ BEGIN {
     our ( @ISA, @EXPORT_OK );
     @ISA = qw(Exporter);
     @EXPORT_OK = qw(
-        @cli_tests @compile_tests @dao_tests @integration_tests @quality_tests @quality_failing_tests @unit_tests 
+        @cli_tests @compile_tests @dao_tests @integration_tests @quality_tests @quality_failing_tests @unit_tests
         use_test_db
-        get_all_perl_binaries get_all_perl_cgi get_all_perl_modules 
+        get_all_perl_binaries get_all_perl_cgi get_all_perl_modules
         get_all_php
         get_networkdevices_modules get_networkdevices_classes
     );
@@ -38,7 +38,7 @@ our @cli_tests = qw(
 );
 
 our @compile_tests = qw(
-    pf.t binaries.t php.t
+    pf.t binaries.t
 );
 
 our @dao_tests = qw(
@@ -58,8 +58,8 @@ our @quality_failing_tests = qw(
 );
 
 our @unit_tests = qw(
-    config.t enforcement.t floatingdevice.t hardware-snmp-objects.t import.t inline.t linux.t network-devices/cisco.t 
-    network-devices/roles.t network-devices/threecom.t network-devices/wireless.t nodecategory.t person.t pfsetvlan.t 
+    config.t enforcement.t floatingdevice.t hardware-snmp-objects.t import.t inline.t linux.t network-devices/cisco.t
+    network-devices/roles.t network-devices/threecom.t network-devices/wireless.t nodecategory.t person.t pfsetvlan.t
     Portal.t radius.t services.t SNMP.t soh.t SwitchFactory.t trigger.t useragent.t util.t util-dhcp.t util-radius.t
     vlan.t web.t
 );
@@ -68,11 +68,12 @@ our @unit_failing_tests = qw(
     network-devices/wired.t
 );
 
-=item use_test_db
+=head2 use_test_db
 
 Will override pf::config's globals regarding what database to connect to
 
 =cut
+
 sub use_test_db {
 
     # override database connection settings
@@ -82,7 +83,7 @@ sub use_test_db {
     $Config{'database'}{'db'} = 'pf-test';
 }
 
-=item get_all_perl_binaries
+=head2 get_all_perl_binaries
 
 Return all the files ending with .pl under
 
@@ -96,6 +97,7 @@ and return all the normal files under
 
 
 =cut
+
 sub get_all_perl_binaries {
 
     my @list;
@@ -104,7 +106,7 @@ sub get_all_perl_binaries {
     # Except that I'm explicitly throwing out addons/legacy/...
     File::Find::find({
         wanted => sub {
-            /^.*\.pl\z/s 
+            /^.*\.pl\z/s
             && $File::Find::name !~ /^.*addons\/legacy\/.*\.pl\z/s
             && push(@list, $File::Find::name);
         }}, '/usr/local/pf/lib/pf', '/usr/local/pf/addons'
@@ -114,7 +116,7 @@ sub get_all_perl_binaries {
     File::Find::find({
         wanted => sub {
             # add to list if it's a regular file
-            push(@list, $File::Find::name) if ((-f $File::Find::name) && ($File::Find::name ne "/usr/local/pf/bin/pfcmd")); 
+            push(@list, $File::Find::name) if ((-f $File::Find::name) && ($File::Find::name ne "/usr/local/pf/bin/pfcmd"));
         }}, '/usr/local/pf/bin', '/usr/local/pf/sbin'
     );
 
@@ -122,13 +124,14 @@ sub get_all_perl_binaries {
     return @list;
 }
 
-=item get_all_perl_cgi
+=head2 get_all_perl_cgi
 
 Return all the files ending with .cgi under
 
   /usr/local/pf/html
 
 =cut
+
 sub get_all_perl_cgi {
 
     my @list;
@@ -143,7 +146,7 @@ sub get_all_perl_cgi {
     return @list;
 }
 
-=item get_all_perl_modules
+=head2 get_all_perl_modules
 
 Return all the files ending with .pm under
 
@@ -154,6 +157,7 @@ Return all the files ending with .pm under
 One exception: pfcmd_pregrammar.pm because it's generated
 
 =cut
+
 sub get_all_perl_modules {
 
     my @list;
@@ -162,8 +166,8 @@ sub get_all_perl_modules {
     # Except that I'm explictly throwing out pfcmd_pregrammar.pm and anything in addons/legacy/
     File::Find::find({
         wanted => sub {
-            /^.*\.pm\z/s 
-            && ! /^.*pfcmd_pregrammar\.pm\z/s 
+            /^.*\.pm\z/s
+            && ! /^.*pfcmd_pregrammar\.pm\z/s
             && $File::Find::name !~ /^.*addons\/legacy\/.*\.pm\z/s
             && push(@list, $File::Find::name);
         }}, '/usr/local/pf/lib/pf', '/usr/local/pf/addons'
@@ -172,11 +176,12 @@ sub get_all_perl_modules {
     return @list;
 }
 
-=item get_all_php
+=head2 get_all_php
 
 Return all the files ending with .php or .inc under F</usr/local/pf/html>
 
 =cut
+
 sub get_all_php {
 
     my @list;
@@ -191,11 +196,12 @@ sub get_all_php {
     return @list;
 }
 
-=item get_networkdevices_modules
+=head2 get_networkdevices_modules
 
 Return all the files ending with .pm under /usr/local/pf/lib/pf/SNMP
 
 =cut
+
 sub get_networkdevices_modules {
 
     my @list;
@@ -211,11 +217,12 @@ sub get_networkdevices_modules {
     return @list;
 }
 
-=item get_networkdevices_classes
+=head2 get_networkdevices_classes
 
 Return the pf::SNMP::Device form for all modules under /usr/local/pf/lib/pf/SNMP except constants.pm
 
 =cut
+
 sub get_networkdevices_classes {
 
     my @modules = get_networkdevices_modules();
@@ -243,21 +250,22 @@ Inverse inc. <info@inverse.ca>
 Copyright (C) 2005-2013 Inverse inc.
 
 =head1 LICENSE
-    
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
-    
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-            
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.            
-                
+USA.
+
 =cut
+
 1;
