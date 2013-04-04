e74fd891872d60956c1f29dbd5c06332bad0695e
James Rouzier <jrouzier@inverse.ca>
2013-04-04 13:46:42 -0400

    Removed preloading of Model::Config::IniStyleBackend classes

--- html/pfappserver/lib/pfappserver.pm
+++ html/pfappserver/lib/pfappserver.pm
@@ -195,21 +195,6 @@ __PACKAGE__->log(Log::Log4perl::Catalyst->new(INSTALL_DIR . '/conf/log.conf'));
 # Handle warnings from Perl as error log messages
 $SIG{__WARN__} = sub { __PACKAGE__->log->error(@_); };
 
-# pfappserver::Model::Config::IniStyleBackend initialization
-after setup_finalize => sub {
-    __PACKAGE__->log->debug("==== READING CONFIGURATION FILES ====");
-    foreach my $module (pfappserver::Model::Config::IniStyleBackend->getConfigurationModules) {
-        my $module_path = 'pfappserver::Model::Config::' . $module;
-        try {
-            my $module_handler = new $module_path;
-            $module_handler->readConfig;
-        } catch {
-            chomp($_);
-            __PACKAGE__->log->error("Told to load module $module but this one does not seems to exist. Passing by...");
-        };
-    }
-    __PACKAGE__->log->debug("==== FINISH READING CONFIGURATION FILES ====");
-};
 
 # Start the application
 __PACKAGE__->setup();
