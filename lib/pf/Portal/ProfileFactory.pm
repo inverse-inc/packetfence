a1b5b6bd8fceb04ecf585a1b2c0897e9dbb4096a
James Rouzier <jrouzier@inverse.ca>
2013-04-16 11:33:23 -0400

    Read the default profile from  %Profiles_Config

--- lib/pf/Portal/ProfileFactory.pm
+++ lib/pf/Portal/ProfileFactory.pm
@@ -69,17 +69,7 @@ sub instantiate {
     return pf::Portal::Profile->new(_default_profile());
 }
 
-sub _default_profile {
-    return {
-        'name'              => 'default',
-        'description'       => 'The Default Profile',
-        'logo'              => $Config{'general'}{'logo'},
-        'guest_self_reg'    => $Config{'registration'}{'guests_self_registration'},
-        'guest_modes'       => $Config{'guests_self_registration'}{'modes'},
-        'template_path'     => '/',
-        'billing_engine'    => $Config{'registration'}{'billing_engine'},
-    };
-}
+sub _default_profile { return $Profiles_Config{default}; }
 
 sub _custom_profile {
     my ($name) = @_;
