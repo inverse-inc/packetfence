6821810f08c0a206fe156ce3ab532c5a61e7a3bf
James Rouzier <jrouzier@inverse.ca>
2013-04-18 19:42:32 -0400

    Refactored

--- lib/pf/config.pm
+++ lib/pf/config.pm
@@ -380,8 +380,8 @@ multi-threaded daemons.
 
 sub init_config {
     readPfDocConfigFiles();
-    readProfileConfigFile();
     readPfConfigFiles();
+    readProfileConfigFile();
     readNetworkConfigFile();
     readFloatingNetworkDeviceFile();
     readOAuthFile();
@@ -425,35 +425,35 @@ sub os_detection {
 
 sub readPfDocConfigFiles {
     $cached_pf_doc_config = pf::config::cached->new(
-            -file => $pf_doc_file,
-            -allowempty => 1,
-            -onreload => [ 'reload_pf_doc_config' =>  sub {
-                my ($config,$name) = @_;
-                $config->toHash(\%Doc_Config);
-                $config->cleanupWhitespace(\%Doc_Config);
-                foreach my $doc_data (values %Doc_Config) {
-                    if (exists $doc_data->{options} && defined $doc_data->{options}) {
-                        my $options = $doc_data->{options};
-                        $doc_data->{options} = [split(/\|/, $options)] if defined $options;
-                    } else {
-                        $doc_data->{options} = [];
-                    }
-                    if (exists $doc_data->{description} && defined $doc_data->{description}) {
-                        # Limited formatting from text to html
-                        my $description = $doc_data->{description};
-                        $description =~ s/</&lt;/g; # convert < to HTML entity
-                        $description =~ s/>/&gt;/g; # convert > to HTML entity
-                        $description =~ s/(\S*(&lt;|&gt;)\S*)\b/<code>$1<\/code>/g; # enclose strings that contain < or >
-                        $description =~ s/(\S+\.(html|tt|pm|pl|txt))\b(?!<\/code>)/<code>$1<\/code>/g; # enclose strings that ends with .html, .tt, etc
-                        $description =~ s/^ \* (.+?)$/<li>$1<\/li>/mg; # create list elements for lines beginning with " * "
-                        $description =~ s/(<li>.*<\/li>)/<ul>$1<\/ul>/s; # create lists from preceding substitution
-                        $description =~ s/\"([^\"]+)\"/<i>$1<\/i>/mg; # enclose strings surrounded by double quotes
-                        $description =~ s/\[(\S+)\]/<strong>$1<\/strong>/mg; # enclose strings surrounded by brakets
-                        $description =~ s/(https?:\/\/\S+)/<a href="$1">$1<\/a>/g; # make links clickable
-                        $doc_data->{description} = $description;
-                    }
+        -file => $pf_doc_file,
+        -allowempty => 1,
+        -onreload => [ 'reload_pf_doc_config' =>  sub {
+            my ($config,$name) = @_;
+            $config->toHash(\%Doc_Config);
+            $config->cleanupWhitespace(\%Doc_Config);
+            foreach my $doc_data (values %Doc_Config) {
+                if (exists $doc_data->{options} && defined $doc_data->{options}) {
+                    my $options = $doc_data->{options};
+                    $doc_data->{options} = [split(/\|/, $options)] if defined $options;
+                } else {
+                    $doc_data->{options} = [];
                 }
-            }]
+                if (exists $doc_data->{description} && defined $doc_data->{description}) {
+                    # Limited formatting from text to html
+                    my $description = $doc_data->{description};
+                    $description =~ s/</&lt;/g; # convert < to HTML entity
+                    $description =~ s/>/&gt;/g; # convert > to HTML entity
+                    $description =~ s/(\S*(&lt;|&gt;)\S*)\b/<code>$1<\/code>/g; # enclose strings that contain < or >
+                    $description =~ s/(\S+\.(html|tt|pm|pl|txt))\b(?!<\/code>)/<code>$1<\/code>/g; # enclose strings that ends with .html, .tt, etc
+                    $description =~ s/^ \* (.+?)$/<li>$1<\/li>/mg; # create list elements for lines beginning with " * "
+                    $description =~ s/(<li>.*<\/li>)/<ul>$1<\/ul>/s; # create lists from preceding substitution
+                    $description =~ s/\"([^\"]+)\"/<i>$1<\/i>/mg; # enclose strings surrounded by double quotes
+                    $description =~ s/\[(\S+)\]/<strong>$1<\/strong>/mg; # enclose strings surrounded by brakets
+                    $description =~ s/(https?:\/\/\S+)/<a href="$1">$1<\/a>/g; # make links clickable
+                    $doc_data->{description} = $description;
+                }
+            }
+        }]
     );
 }
 
@@ -566,8 +566,6 @@ sub readPfConfigFiles {
         $logger->logcroak( join( "\n", @Config::IniFiles::errors ) );
     }
 
-
-    # creating a call back to be called everytime the pf.conf file is reloaded
 }
 
 sub _set_guest_self_registration {
