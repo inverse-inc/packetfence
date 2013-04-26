496dd006fe7602343a15290e093aa1cdeeb53a32
James Rouzier <jrouzier@inverse.ca>
2013-04-26 03:11:45 -0400

    Role for Form help functions

---
+++ html/pfappserver/lib/pfappserver/Base/Form/Role/Help.pm
@@ -0,0 +1,71 @@
+package pfappserver::Base::Form::Role::Help;
+
+=head1 NAME
+
+pfappserver::Form::Widget::Theme::Pf - PacketFence theme
+
+=head1 DESCRIPTION
+
+This theme extends the Bootstrap theme. It modifies various aspects of
+the fields:
+
+ - add some element and wrapper classes to fields
+ - add 'data-required=required' attribute to required fields
+ - add 'data-type=number' attribute to number fields
+
+=cut
+
+use namespace::autoclean;
+use HTML::FormHandler::Moose::Role;
+
+=head2 help
+
+=cut
+
+sub help {
+    my $self = shift;
+    my $help = undef;
+
+    if ($self->get_tag('help')) {
+        return sprintf('<p class="help-block">%s</p>', $self->_localize($self->get_tag('help')));
+    }
+}
+
+=head2 help_list
+
+=cut
+
+sub help_list {
+    my $self = shift;
+    my $help = undef;
+
+    if ($self->get_tag('help')) {
+        return sprintf('<dl class="help-block">%s</dl>', $self->_localize($self->get_tag('help')));
+    }
+}
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
