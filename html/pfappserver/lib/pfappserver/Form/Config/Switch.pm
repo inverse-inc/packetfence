45433e1c364ffc23c8edb39a983ca1f96387c658
James Rouzier <jrouzier@inverse.ca>
2013-06-19 15:33:33 -0400

    Update help text for controllerIp

--- html/pfappserver/lib/pfappserver/Form/Config/Switch.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Switch.pm
@@ -344,7 +344,7 @@ has_field controllerIp =>
     label => 'Controller IP Address',
     tags => {
         after_element => \&help_list,
-        help => 'Only for Wi-fi , if the deauth request must be send to another device than the access point then set the ip of the controller'
+        help => 'Use instead this IP address for de-authentication requests. Normally used for WiFi only'
     },
   );
 
