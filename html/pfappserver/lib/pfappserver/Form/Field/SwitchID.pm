9f3b71804ee2ad1c090020c0f13518af9300c316
James Rouzier <jrouzier@inverse.ca>
2013-07-08 13:42:17 -0400

    added valid_mac_or_id function

--- html/pfappserver/lib/pfappserver/Form/Field/SwitchID.pm
+++ html/pfappserver/lib/pfappserver/Form/Field/SwitchID.pm
@@ -39,7 +39,7 @@ apply
         check => sub {
             my ( $value, $field ) = @_;
             return 1 if ($field->accept && grep { $_ eq $value } @{$field->accept});
-            return valid_ip( $value ) || valid_mac( $value );
+            return valid_mac_or_ip( $value );
         }
     },
     {
