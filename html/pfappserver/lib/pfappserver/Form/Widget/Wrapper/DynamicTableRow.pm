c7da75435556e206c935044f6ef00cc53c3fb688
James Rouzier <jrouzier@inverse.ca>
2013-08-01 11:39:07 -0400

    Will add sort handle when parent is sortable

--- html/pfappserver/lib/pfappserver/Form/Widget/Wrapper/DynamicTableRow.pm
+++ html/pfappserver/lib/pfappserver/Form/Widget/Wrapper/DynamicTableRow.pm
@@ -20,7 +20,9 @@ around wrap_field => sub {
     my ($orig, $self, $result, $rendered_widget ) = @_;
     my $class = $self->get_tag("dynamic_row_class");
     $class = " class=\"$class\"" if $class;
-    return "<tr${class}><td>" . $self->$orig($result, $rendered_widget) .
+    my $extra = "<td>";
+    $extra ="<td class=\"sort-handle\">" . ($self->name + 1) . "</td>\n<td>" if $self->parent->sortable ;
+    return "<tr${class}>${extra}" . $self->$orig($result, $rendered_widget) .
          '</td><td><div class="btn-group"><a class="btn-icon" href="#delete"><i class="icon-minus-sign"></i></a><a class="btn-icon" href="#add"><i class="icon-plus-sign"></i></a></div></td></tr>';
 };
 
