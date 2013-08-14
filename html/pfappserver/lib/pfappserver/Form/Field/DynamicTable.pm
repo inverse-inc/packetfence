f7f673896e5f5162274cbc7b011008634f032bb6
Francis Lachapelle <flachapelle@inverse.ca>
2013-08-14 13:43:54 -0400

    Cleanup Form::Field::DynamicTable

--- html/pfappserver/lib/pfappserver/Form/Field/DynamicTable.pm
+++ html/pfappserver/lib/pfappserver/Form/Field/DynamicTable.pm
@@ -17,20 +17,12 @@ use warnings;
 use Moose;
 extends 'HTML::FormHandler::Field::Repeatable';
 
-has '+widget' => (default => 'DynamicTable' );
-has '+num_extra' => (default => 1);
-has '+widget_wrapper' => (default => '+Simple');
-has '+do_wrapper' => (default => 1);
-has '+do_label' => (default => 1);
-has 'sortable' => (is=>'rw' , default => 0);
-has '+init_contains' => ( default => sub {
-    {
-        widget_wrapper => 'DynamicTableRow',
-        tags => {
-            after_element => \&add_delete
-        }
-    }
-} );
+has '+widget' => ( default => 'DynamicTable' );
+has '+num_extra' => ( default => 1 );
+has '+widget_wrapper' => ( default => '+Simple' );
+has '+do_wrapper' => ( default => 1 );
+has '+do_label' => ( default => 1 );
+has 'sortable' => ( is =>'rw', default => 0 );
 
 sub wrapper_tag {
     "table"
@@ -40,13 +32,8 @@ sub BUILD {
     my ($self) = @_;
     $self->add_wrapper_class(qw(table table-dynamic table-condensed));
     $self->add_wrapper_class(qw(table-sortable)) if $self->sortable;
-    $self->set_tag("wrapper_tag",$self->wrapper_tag);
-    $self->set_wrapper_attr("id",$self->id);
-}
-
-sub add_delete {
-    my ($self) = @_;
-    return ' <div class="btn-group"><a class="btn-icon" href="#delete"><i class="icon-minus-sign"></i></a><a class="btn-icon" href="#add"><i class="icon-plus-sign"></i></a></div>';
+    $self->set_tag("wrapper_tag", $self->wrapper_tag);
+    $self->set_wrapper_attr("id", $self->id);
 }
 
 =head1 AUTHOR
