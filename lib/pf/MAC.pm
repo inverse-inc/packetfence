6e405ab5b8c529b557fa8a7074d6293ec4005732
Louis Munro <lmunro@inverse.ca>
2013-09-30 18:48:04 -0400

    Added methods for macoui2nb and mac2nb.

--- lib/pf/MAC.pm
+++ lib/pf/MAC.pm
@@ -33,8 +33,9 @@ Cleans a MAC address.
 Returns an untainted pf::MAC with MAC in format: XX:XX:XX:XX:XX:XX (uppercased).
 
 =cut
+
 sub clean {
-    my $self = shift;
+    my $self    = shift;
     my $hex_mac = $self->as_IEEE();
 
     # untaint $hex_mac
@@ -51,9 +52,10 @@ sub clean {
 Returns the MAC address stripped of any delimiter (base is preserved).
 
 =cut
+
 sub get_stripped {
     my $self = shift;
-    my $mac = $self->get_mac();
+    my $mac  = $self->get_mac();
     $mac =~ s/[^[:xdigit:]]//g;
     return $mac;
 }
@@ -62,10 +64,10 @@ sub get_stripped {
 Returns a string containing the MAC address in hex base, stripped of any delimiter (uppercased).
 
 =cut
+
 sub get_hex_stripped {
-    my $self = shift;
-    # we clone the object so that the original is not modified.
-    my $IEEE_mac = pf::MAC->new( mac => $self)->as_IEEE->get_mac();
+    my $self     = shift;
+    my $IEEE_mac = $self->as_IEEE->get_mac();
     $IEEE_mac =~ s/[^[:xdigit:]]//g;
     return $IEEE_mac;
 }
@@ -75,6 +77,7 @@ Returns an uppercased, hex based and : delimited pf::MAC object.
 Intended for backward compatibility with pf::util::format_mac_for_acct.
 
 =cut 
+
 sub format_for_acct {
     my $self = shift;
     return pf::MAC->new( mac => $self->get_hex_stripped() );
@@ -85,39 +88,61 @@ Returns a new pf::MAC object formatted for Cisco ( example: 0002.03aa.abff ).
 See Net::MAC for implementation.
 
 =cut
+
 #sub as_Cisco {};
 
 =item as_integer
 Returns a string with the MAC as a decimal without delimiter.
 
 =cut
+
 sub get_dec_stripped {
     my $self = shift;
+
     # disabling warnings in this scope because a MAC address (48bit) is larger than an int on 32bit systems
-    #     # and perl warns about it but gives the right value.
+    # and perl warns about it but gives the right value.
     no warnings;
     return hex( $self->get_hex_stripped() );
 }
-
 
 =item get_oui
 Returns the OUI for the MAC as an uppercased hex string with - delimiters.
 This is the format the IEEE uses.
 
 =cut
+
 sub get_oui {
-    my $self = shift;
-    # we clone the object so that the original is not modified.
-    my $IEEE_mac = pf::MAC->new( mac => $self)->as_Microsoft->get_mac();
-    return substr($IEEE_mac, 0, 8);
+    my $self     = shift;
+    my $IEEE_mac = $self->as_Microsoft->get_mac();
+    return substr( $IEEE_mac, 0, 8 );
 }
 
+=item get_dec_oui
+Returns a decimal value of the OUI stripped of any delimiters.
+
+=cut
 
 sub get_dec_oui {
     my $self = shift;
-    my $oui = $self->get_oui();
+    my $oui  = $self->get_oui();
     $oui =~ s/-//g;
     return hex($oui);
 }
+
+
+=item macoui2nb
+Provided for backwards compatibility with pf::util::macoui2nb.
+Equivalent to get_dec_oui().
+
+=cut 
+sub macoui2nb { return $_[0]->get_dec_oui(@_); }
+
+=item mac2nb
+Provided for backwards compatibility with pf::util::mac2nb.
+Equivalent to get_dec_stripped().
+
+=cut
+sub mac2nb { return $_[0]->get_dec_stripped(@_); }
+
 
 1;
