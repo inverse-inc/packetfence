10d447e500d12776e5d292d772dec2dd3d826aa8
James Rouzier <jrouzier@inverse.ca>
2013-10-25 12:44:16 -0400

    Removed tabs

--- lib/pf/inline/accounting.pm
+++ lib/pf/inline/accounting.pm
@@ -77,16 +77,16 @@ sub accounting_db_prepare {
 
     $accounting_statements->{'accounting_reset_autoincrement_mem_sql'} = get_db_handle()->prepare(qq[
         ALTER TABLE $mem_table AUTO_INCREMENT = 1
-	]);
+    ]);
 
     $accounting_statements->{'accounting_add_active_session_sql'} = get_db_handle()->prepare(qq[
         INSERT into $accounting_table(src_ip, firstseen, lastmodified, outbytes, inbytes)
-		  VALUES (?, ?, ?, ?, ?)
+          VALUES (?, ?, ?, ?, ?)
           ON DUPLICATE KEY UPDATE
             lastmodified = ?,
-			outbytes = ?,
-			inbytes = ?
-	]);
+            outbytes = ?,
+            inbytes = ?
+    ]);
 
     $accounting_db_prepared = 1;
 }
@@ -96,71 +96,71 @@ sub import_ulogd_data {
     # Session that haven't been updated for more than this amount of seconds will
     # be dropped from the mem table. If the client connects again,
     # he'll simply get a new entry in the accounting table
-	# This should be higher than the interval at which import_ulogd_data is called
+    # This should be higher than the interval at which import_ulogd_data is called
     my $accounting_session_timeout = shift;
     my $logger = Log::Log4perl::get_logger(__PACKAGE__);
     $logger->debug("Importing ulogd data");
 
-	# lock table write
-	# fetch all relevant info
-	# loop quickly through returned data,
-	#   if lastmodified is not on same day than first seen
-	#     delete all from mem_table
-	#     reset auto increment counter
-	#   else
-	#     delete inactive sessions from mem table
-	# unlock
-	# Update real accounting table with fetched data
-	
-	my $dbh = get_db_handle();
-	$dbh->do("LOCK TABLE $mem_table WRITE");
+    # lock table write
+    # fetch all relevant info
+    # loop quickly through returned data,
+    #   if lastmodified is not on same day than first seen
+    #     delete all from mem_table
+    #     reset auto increment counter
+    #   else
+    #     delete inactive sessions from mem table
+    # unlock
+    # Update real accounting table with fetched data
 
-	# XXX if one of these statements fail it will reconnect. losing the table lock
-	# some accounting data updates may be lost
-	my $new_data_query = db_query_execute("inline::accounting",
-	                          $accounting_statements,
-							  'accounting_select_all_mem_sql') || return (0);
+    my $dbh = get_db_handle();
+    $dbh->do("LOCK TABLE $mem_table WRITE");
 
-	#my $new_accounting_data = $new_data_query->fetchall_hashref('src_ip');
-	my $new_accounting_data = $new_data_query->fetchall_arrayref();
+    # XXX if one of these statements fail it will reconnect. losing the table lock
+    # some accounting data updates may be lost
+    my $new_data_query = db_query_execute("inline::accounting",
+                              $accounting_statements,
+                              'accounting_select_all_mem_sql') || return (0);
 
-	my $dropall=0;
-	for my $row (@$new_accounting_data) {
-	  # this is kind of crude, but should be good enough to detect day changes
-	  # 2013-10-25 10:01:02
+    #my $new_accounting_data = $new_data_query->fetchall_hashref('src_ip');
+    my $new_accounting_data = $new_data_query->fetchall_arrayref();
+
+    my $dropall=0;
+    for my $row (@$new_accounting_data) {
+      # this is kind of crude, but should be good enough to detect day changes
+      # 2013-10-25 10:01:02
         $$row[-1] =~ /\d+-\d+-(\d+) /;
-	    my $lastmodified_day = $1;
+        my $lastmodified_day = $1;
         $$row[-2] =~ /\d+-\d+-(\d+) /;
-	    my $firstseen_day = $1;
+        my $firstseen_day = $1;
 
-	    if ($firstseen_day != $lastmodified_day) {
-	        $dropall=1;
-			last;
-	    }
-	}
-	if ($dropall) {
+        if ($firstseen_day != $lastmodified_day) {
+            $dropall=1;
+            last;
+        }
+    }
+    if ($dropall) {
         $logger->debug("New day, dropping all rows from ulogd memory table");
-	    # Need to use DELETE FROM + reset AUTO_INCREMENT since a locked table cannot be truncated
-	    db_query_execute("inline::accounting", $accounting_statements, 'accounting_delete_all_mem_sql');
-	    db_query_execute("inline::accounting", $accounting_statements, 'accounting_reset_autoincrement_mem_sql');
-	}
-	else {
-	    db_query_execute("inline::accounting", $accounting_statements, 'accounting_drop_inactive_sessions_mem_sql', $accounting_session_timeout);
-	}
-	$dbh->do("UNLOCK TABLES");
+        # Need to use DELETE FROM + reset AUTO_INCREMENT since a locked table cannot be truncated
+        db_query_execute("inline::accounting", $accounting_statements, 'accounting_delete_all_mem_sql');
+        db_query_execute("inline::accounting", $accounting_statements, 'accounting_reset_autoincrement_mem_sql');
+    }
+    else {
+        db_query_execute("inline::accounting", $accounting_statements, 'accounting_drop_inactive_sessions_mem_sql', $accounting_session_timeout);
+    }
+    $dbh->do("UNLOCK TABLES");
 
-	foreach my $row (@$new_accounting_data) {
-		my $src_ip = $$row[0];
-		my $inbytes = $$row[1];
-		my $outbytes = $$row[2];
-		my $firstseen = $$row[3];
-		my $lastmodified = $$row[4];
-	    db_query_execute("inline::accounting", $accounting_statements, 'accounting_add_active_session_sql',
-		                 $src_ip, $firstseen, $lastmodified, 
+    foreach my $row (@$new_accounting_data) {
+        my $src_ip = $$row[0];
+        my $inbytes = $$row[1];
+        my $outbytes = $$row[2];
+        my $firstseen = $$row[3];
+        my $lastmodified = $$row[4];
+        db_query_execute("inline::accounting", $accounting_statements, 'accounting_add_active_session_sql',
+                         $src_ip, $firstseen, $lastmodified,
                          $outbytes, $inbytes,
-						 $lastmodified, $outbytes, $inbytes);
+                         $lastmodified, $outbytes, $inbytes);
 
-	}
+    }
 
 }
 
