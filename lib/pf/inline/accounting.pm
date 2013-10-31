00e6e058ec30572f4256111cef0cc68e36aad363
Jean Raby <jraby@inverse.ca>
2013-10-31 16:06:49 -0400

    Rework inline accounting data management
    rename import_ulogd_data to inline_accounting_import_ulogd_data
    inline_accounting_import_ulogd_data can now accept an ip as a 2nd parameter,
    this will be used to import data for a single ip only.
    Will be called when the clients gets new bandwidth and when a
    bandwidth violation is raised for him.

--- lib/pf/inline/accounting.pm
+++ lib/pf/inline/accounting.pm
@@ -1,5 +1,10 @@
 package pf::inline::accounting;
 
+# TODO
+#  
+#  provide function to get accounting data for a single ip.
+#    Taking starttime, endtime as optional parameters
+
 =head1 NAME
 
 pf::inline::accounting - Inline accounting
@@ -63,89 +68,129 @@ sub accounting_db_prepare {
     my $logger = Log::Log4perl::get_logger(__PACKAGE__);
     $logger->debug("Preparing" . __PACKAGE__ . "database queries");
 
-    $accounting_statements->{'accounting_select_all_mem_sql'} = get_db_handle()->prepare(qq[
+    $accounting_statements->{'accounting_select_all_ip_stats_mem_sql'} =
+      get_db_handle()->prepare(qq[
         SELECT src_ip, inbytes, outbytes, firstseen, lastmodified from $mem_table
-    ]);
+      ]);
 
-    $accounting_statements->{'accounting_delete_all_mem_sql'} = get_db_handle()->prepare(qq[
+    $accounting_statements->{'accounting_select_single_ip_stats_mem_sql'} =
+      get_db_handle()->prepare(qq[
+        SELECT src_ip, inbytes, outbytes, firstseen, lastmodified from $mem_table
+      ]);
+
+    $accounting_statements->{'accounting_delete_all_mem_sql'} =
+      get_db_handle()->prepare(qq[
         DELETE FROM $mem_table
-    ]);
+      ]);
 
-    $accounting_statements->{'accounting_drop_inactive_sessions_mem_sql'} = get_db_handle()->prepare(qq[
+    $accounting_statements->{'accounting_drop_single_ip_stats_mem_sql'} = 
+      get_db_handle()->prepare(qq[
+        DELETE FROM $mem_table where `src_ip` =  ?
+      ]);
+
+    $accounting_statements->{'accounting_drop_inactive_sessions_mem_sql'} = 
+      get_db_handle()->prepare(qq[
         DELETE FROM $mem_table where `lastmodified` < NOW() - ?
-    ]);
+      ]);
 
-    $accounting_statements->{'accounting_reset_autoincrement_mem_sql'} = get_db_handle()->prepare(qq[
+    $accounting_statements->{'accounting_reset_autoincrement_mem_sql'} =
+      get_db_handle()->prepare(qq[
         ALTER TABLE $mem_table AUTO_INCREMENT = 1
-    ]);
+      ]);
 
-    $accounting_statements->{'accounting_add_active_session_sql'} = get_db_handle()->prepare(qq[
+    $accounting_statements->{'accounting_add_active_session_sql'} = 
+      get_db_handle()->prepare(qq[
         INSERT into $accounting_table(src_ip, firstseen, lastmodified, outbytes, inbytes)
           VALUES (?, ?, ?, ?, ?)
           ON DUPLICATE KEY UPDATE
             lastmodified = ?,
             outbytes = ?,
             inbytes = ?
-    ]);
+      ]);
+
+    $accounting_statements->{'accounting_select_single_ip_stats_sql'} =
+      get_db_handle()->prepare(qq[
+        SELECT src_ip, inbytes, outbytes, firstseen, lastmodified
+          from $mem_table where src_ip = ?
+      ]);
 
     $accounting_db_prepared = 1;
 }
 
 
-sub import_ulogd_data {
-    # Session that haven't been updated for more than this amount of seconds will
-    # be dropped from the mem table. If the client connects again,
-    # he'll simply get a new entry in the accounting table
-    # This should be higher than the interval at which import_ulogd_data is called
-    my $accounting_session_timeout = shift;
+sub inline_accounting_stats_for_ip {
+    # $ip: ip to get stats for
+    # $starttime:  get stats from that time until $endtime
+    # $endtime
+    # By default it will fetch / add all rows/stats for the requested ip
+    my ($ip, $starttime, $endtime) = @_;
+
+}
+
+sub inline_accounting_import_ulogd_data {
+    # Session that haven't been updated for more than
+    # $accounting_session_timeout seconds will be dropped from the mem table.
+    # When reconnecting, the client willget a new entry in the accounting table.
+    # Should be higher than the interval at which import_ulogd_data is called.
+    # $ip is optional. When call with this parameter, this function will import
+    # the stats for that ip only and then delete its row in the mem table.
+    # This is done to ensure that new statistics will be part of a new
+    # accounting session
+    my ($accounting_session_timeout, $ip)= @_;
     my $logger = Log::Log4perl::get_logger(__PACKAGE__);
     $logger->debug("Importing ulogd data");
-
-    # lock table write
-    # fetch all relevant info
-    # loop quickly through returned data,
-    #   if lastmodified is not on same day than first seen
-    #     delete all from mem_table
-    #     reset auto increment counter
-    #   else
-    #     delete inactive sessions from mem table
-    # unlock
-    # Update real accounting table with fetched data
 
     my $dbh = get_db_handle();
     $dbh->do("LOCK TABLE $mem_table WRITE");
 
     # XXX if one of these statements fail it will reconnect. losing the table lock
     # some accounting data updates may be lost
-    my $new_data_query = db_query_execute("inline::accounting",
+    my $new_data_query;
+    if (defined $ip) {
+      $new_data_query = db_query_execute("inline::accounting",
                               $accounting_statements,
-                              'accounting_select_all_mem_sql') || return (0);
+                              'accounting_select_single_ip_stats_mem_sql',
+                              $ip) || return (0);
+    } else {
+      $new_data_query = db_query_execute("inline::accounting",
+                              $accounting_statements,
+                              'accounting_select_all_ip_stats_mem_sql') || return (0);
+    }
 
-    #my $new_accounting_data = $new_data_query->fetchall_hashref('src_ip');
     my $new_accounting_data = $new_data_query->fetchall_arrayref();
 
-    my $dropall=0;
-    for my $row (@$new_accounting_data) {
-      # this is kind of crude, but should be good enough to detect day changes
-      # 2013-10-25 10:01:02
-        $$row[-1] =~ /\d+-\d+-(\d+) /;
-        my $lastmodified_day = $1;
-        $$row[-2] =~ /\d+-\d+-(\d+) /;
-        my $firstseen_day = $1;
-
-        if ($firstseen_day != $lastmodified_day) {
-            $dropall=1;
-            last;
+    if (defined $ip) {
+        # This is done to ensure that new stats will create a new 'session'
+        # in the accounting table.
+        $logger->debug("Dropping stats from memory table for ip $ip");
+        db_query_execute("inline::accounting", $accounting_statements,
+                         'accounting_drop_single_ip_stats_mem_sql', $ip);
+    } else {
+        # This drop all logic must be done only when called for all ips
+        my $dropall=0;
+        for my $row (@$new_accounting_data) {
+          # this is kind of crude, but should be good enough to detect day changes
+          # 2013-10-25 10:01:02
+            $$row[-1] =~ /\d+-\d+-(\d+) /;
+            my $lastmodified_day = $1;
+            $$row[-2] =~ /\d+-\d+-(\d+) /;
+            my $firstseen_day = $1;
+    
+            if ($firstseen_day != $lastmodified_day) {
+                $dropall=1;
+                last;
+            }
         }
-    }
-    if ($dropall) {
-        $logger->debug("New day, dropping all rows from ulogd memory table");
-        # Need to use DELETE FROM + reset AUTO_INCREMENT since a locked table cannot be truncated
-        db_query_execute("inline::accounting", $accounting_statements, 'accounting_delete_all_mem_sql');
-        db_query_execute("inline::accounting", $accounting_statements, 'accounting_reset_autoincrement_mem_sql');
-    }
-    else {
-        db_query_execute("inline::accounting", $accounting_statements, 'accounting_drop_inactive_sessions_mem_sql', $accounting_session_timeout);
+        if ($dropall) {
+            # Drop all data from mem table to get new accounting sessions every day
+            $logger->debug("New day, dropping all rows from ulogd memory table");
+            # Need to use DELETE FROM + reset AUTO_INCREMENT since a locked table cannot be truncated
+            db_query_execute("inline::accounting", $accounting_statements, 'accounting_delete_all_mem_sql');
+            db_query_execute("inline::accounting", $accounting_statements, 'accounting_reset_autoincrement_mem_sql');
+        }
+        else {
+            db_query_execute("inline::accounting", $accounting_statements, 'accounting_drop_inactive_sessions_mem_sql', $accounting_session_timeout);
+        }
     }
     $dbh->do("UNLOCK TABLES");
 
