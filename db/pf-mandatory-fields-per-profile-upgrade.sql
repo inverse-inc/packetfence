16109b0a186b95de0627cd408552109d327aaa4a
James Rouzier <jrouzier@inverse.ca>
2013-11-25 14:04:36 -0500

    Add db upgrade sql

---
+++ db/pf-mandatory-fields-per-profile-upgrade.sql
@@ -0,0 +1,23 @@
+ALTER TABLE person 
+  ADD `anniversary` varchar(255) NULL DEFAULT NULL,
+  ADD `birthday` varchar(255) NULL DEFAULT NULL,
+  ADD `gender` char(1) NULL DEFAULT NULL,
+  ADD `lang` varchar(255) NULL DEFAULT NULL,
+  ADD `nickname` varchar(255) NULL DEFAULT NULL,
+  ADD `organization` varchar(255) NULL DEFAULT NULL,
+  ADD `cell_phone` varchar(255) NULL DEFAULT NULL,
+  ADD `work_phone` varchar(255) NULL DEFAULT NULL,
+  ADD `title` varchar(255) NULL DEFAULT NULL,
+  ADD `building_number` varchar(255) NULL DEFAULT NULL,
+  ADD `apartment_number` varchar(255) NULL DEFAULT NULL,
+  ADD `room_number` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_1` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_2` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_3` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_4` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_5` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_6` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_7` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_8` varchar(255) NULL DEFAULT NULL,
+  ADD `custom_field_9` varchar(255) NULL DEFAULT NULL
+;
