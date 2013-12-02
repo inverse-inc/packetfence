f8e1829ec15834431a9e76d7433f251cd45547d9
Francis Lachapelle <flachapelle@inverse.ca>
2013-12-02 16:10:31 -0500

    Initial maintenance code for data of pfbandwidthd

--- lib/pf/inline/accounting.pm
+++ lib/pf/inline/accounting.pm
@@ -8,6 +8,19 @@ package pf::inline::accounting;
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
+=head2 MySQL SETUP
+
+CREATE TABLE `inline_accounting` (
+   `outbytes` bigint unsigned NOT NULL DEFAULT '0' COMMENT 'orig_raw_pktlen',
+   `inbytes` bigint unsigned NOT NULL DEFAULT '0' COMMENT 'reply_raw_pktlen',
+   `ip` varchar(16) NOT NULL,
+   `firstseen` DATETIME NOT NULL,
+   `lastmodified` DATETIME NOT NULL,
+   `status` int unsigned NOT NULL default 0, -- ACTIVE
+   PRIMARY KEY (`ip`, `firstseen`),
+   INDEX (`ip`)
+ ) ENGINE=InnoDB;
+
 =cut
 
 use strict;
@@ -21,7 +34,6 @@ my $accounting_table = 'inline_accounting';
 
 my $ACTIVE = 0;
 my $INACTIVE = 1;
-my $ANALYZED = 3;
 
 BEGIN {
     use Exporter ();
@@ -32,6 +44,7 @@ BEGIN {
         $inline_accounting_db_prepared
 
         inline_accounting_update_session_for_ip
+        inline_accounting_maintenance
     );
 }
 
@@ -83,7 +96,7 @@ sub accounting_db_prepare {
       get_db_handle()->prepare(qq[
         SELECT n.mac, i.ip, n.bandwidth_balance, COALESCE((a.outbytes + a.inbytes), 0) as bandwidth_consumed
         FROM node n, iplog i
-        LEFT JOIN $accounting_table a ON i.ip = a.src_ip AND a.status = $ACTIVE
+        LEFT JOIN $accounting_table a ON i.ip = a.ip AND a.status = $ACTIVE
         WHERE n.mac = i.mac
           AND i.end_time = 0
           AND (n.bandwidth_balance = 0
@@ -97,7 +110,7 @@ sub accounting_db_prepare {
           COALESCE(
             (SELECT SUM(a.outbytes+a.inbytes)
              FROM $accounting_table a, iplog i
-             WHERE a.src_ip = i.ip
+             WHERE a.ip = i.ip
                AND i.end_time = 0
                AND i.mac = n.mac
                AND a.status = $INACTIVE
@@ -106,7 +119,23 @@ sub accounting_db_prepare {
         WHERE n.bandwidth_balance > 0
       ]);
 
+    $accounting_statements->{'accounting_update_inactive_sessions_for_ip_sql'} =
+      get_db_handle()->prepare(qq[
+        UPDATE $accounting_table
+          SET status = $INACTIVE
+          WHERE ip = ?
+      ]);
+
+    $accounting_statements->{'accounting_update_inactive_sessions_for_interval_sql'} =
+      get_db_handle()->prepare(qq[
+        UPDATE $accounting_table
+          SET status = $INACTIVE
+          WHERE lastmodified < NOW() - INTERVAL ? SECOND
+      ]);
+
     $accounting_db_prepared = 1;
+
+    return $accounting_db_prepared;
 }
 
 sub inline_accounting_update_session_for_ip {
@@ -130,6 +159,50 @@ sub inline_accounting_update_session_for_ip {
 			  $ip, $inbytes, $outbytes, $firstseen, $lastmodified) || return(0);
     }
    
+    return 1;
+}
+
+sub inline_accounting_maintenance {
+    my $accounting_session_timeout = shift;
+    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
+
+    # Check if there's at least a violation using the 'Accounting::BandwidthExpired' trigger
+    my @tid = trigger_view_tid($ACCOUNTING_POLICY_BANDWIDTH);
+    if (scalar(@tid) > 0) {
+        my $violation_id = $tid[0]{'vid'}; # only consider the first violation
+        $logger->debug("Violation $violation_id is of type $TRIGGER_TYPE_ACCOUNTING::$ACCOUNTING_POLICY_BANDWIDTH; analyzing inline accounting data");
+
+        # Disable AutoCommit since we perform a SELECT .. FOR UPDATE statement
+        my $dbh = get_db_handle();
+        $dbh->{'AutoCommit'} = 0;
+
+        # Extract nodes with no more bandwidth left (considering also active sessions)
+        my $bandwidth_query = db_query_execute('inline::accounting', $accounting_statements, 'accounting_select_node_bandwidth_balance_sql');
+        if ($bandwidth_query) {
+            while (my $row = $bandwidth_query->fetchrow_arrayref()) {
+                my ($mac, $ip, $bandwidth_balance, $bandwidth_consumed) = @$row;
+                $logger->debug("Node $mac/$ip has no more bandwidth (balance $bandwidth_balance, consumed $bandwidth_consumed), triggering violation");
+
+                # Trigger violation for this node
+                violation_trigger($mac, $ACCOUNTING_POLICY_BANDWIDTH, $TRIGGER_TYPE_ACCOUNTING);
+
+                # Stop counters of active network sessions for this node
+                db_query_execute('inline::accounting', $accounting_statements,
+                                 'accounting_update_inactive_sessions_for_ip_sql', $ip);
+            }
+        }
+
+        # Switchting AutoCommit from 0 to 1 automatically issues a "commit"
+        $dbh->{'AutoCommit'} = 1;
+    }
+
+    # Stop counters of active network sessions that have exceed the timeout
+    db_query_execute('inline::accounting', $accounting_statements,
+                     'accounting_update_inactive_sessions_for_interval_sql', $accounting_session_timeout);
+
+    # Update bandwidth balance with new inactive sessions
+    db_query_execute('inline::accounting', $accounting_statements, 'accounting_update_node_bandwidth_balance_sql');
+
     return 1;
 }
 
