19d2ab36cd024caaa1421e2dfae16bcdddb9fd49
James Rouzier <jrouzier@inverse.ca>
2013-12-12 12:26:08 -0500

    Fixed issue with snort and suricata not starting

--- lib/pf/services/manager/suricata.pm
+++ lib/pf/services/manager/suricata.pm
@@ -18,7 +18,7 @@ use pf::config;
 use Moo;
 use pf::services::suricata qw(generate_suricata_conf);
 extends 'pf::services::manager';
-with 'pf::services::manager::roles::is_managed_by_pf_conf';
+with 'pf::services::manager::roles::pf_conf_trapping_engine';
 
 has '+name' => ( default => sub { 'suricata' } );
 
