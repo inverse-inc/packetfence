d524987a0050162dd7304e671909703974f20fe2
James Rouzier <jrouzier@inverse.ca>
2013-12-19 13:28:25 -0500

    Will use pf::DB for handling connecting to the database

--- lib/pf/db.pm
+++ lib/pf/db.pm
@@ -23,6 +23,7 @@ use DBI;
 use File::Basename;
 use pf::log;
 use pf::config;
+use pf::DB;
 
 # Constants
 use constant MAX_RETRIES  => 3;
@@ -100,16 +101,9 @@ sub db_connect {
 
     $logger->debug("(Re)Connecting to MySQL (thread id: $tid)");
 
-    my $host = $DB_Config->{'host'};
-    my $port = $DB_Config->{'port'};
-    my $user = $DB_Config->{'user'};
-    my $pass = $DB_Config->{'pass'};
-    my $db   = $DB_Config->{'db'};
-
     # TODO database prepared statements are disabled by default in dbd::mysql
     # we should test with them, see http://search.cpan.org/~capttofu/DBD-mysql-4.013/lib/DBD/mysql.pm#DESCRIPTION
-    $mydbh = DBI->connect( "dbi:mysql:dbname=$db;host=$host;port=$port",
-        $user, $pass, { RaiseError => 0, PrintError => 0 } );
+    $mydbh = pf::DB->new->retain_dbh;
 
     # make sure we have a database handle
     if ($mydbh) {
