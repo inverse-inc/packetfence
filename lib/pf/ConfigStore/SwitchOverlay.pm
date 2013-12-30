77e526f67188b91f5242ab374e82f5df52261958
James Rouzier <jrouzier@inverse.ca>
2013-12-30 18:54:02 -0500

    Move the cache reload logic from SwitchOverlay to Switch Split the logic of recalculating SwitchConfig and sync database information
    Conflicts:
    	lib/pf/ConfigStore/SwitchOverlay.pm

--- lib/pf/ConfigStore/SwitchOverlay.pm
+++ lib/pf/ConfigStore/SwitchOverlay.pm
@@ -16,13 +16,10 @@ use namespace::autoclean;
 use pf::log;
 use pf::file_paths;
 use pf::ConfigStore::Switch;
-use pf::freeradius;
 use HTTP::Status qw(:constants is_error is_success);
-our (%SwitchConfig, $switches_overlay_cached_config);
+our ($switches_overlay_cached_config);
 
 extends qw(pf::ConfigStore Exporter);
-
-our @EXPORT = qw(%SwitchConfig);
 
 $switches_overlay_cached_config = pf::config::cached->new(
     -file => $switches_overlay_file,
@@ -32,56 +29,35 @@ $switches_overlay_cached_config = pf::config::cached->new(
     -onfilereload => [
         on_switches_reload => sub  {
             my ($config, $name) = @_;
-            $config->toHash(\%SwitchConfig);
-            $config->cleanupWhitespace(\%SwitchConfig);
-            my $imported = $config->{imported};
-            delete @SwitchConfig { grep { !$imported->SectionExists($_) } keys %SwitchConfig };
-            foreach my $switch (values %SwitchConfig) {
-                # transforming uplink and inlineTrigger to arrays
-                foreach my $key (qw(uplink inlineTrigger)) {
-                    my $value = $switch->{$key} || "";
-                    $switch->{$key} = [split /\s*,\s*/,$value ];
-                }
-                # transforming vlans and roles to hashes
-                my %merged = ( Vlan => {}, Role => {});
-                foreach my $key ( grep { /(Vlan|Role)$/ } keys %{$switch}) {
-                    next unless my $value = $switch->{$key};
-                    if (my ($type_key,$type) = ($key =~ /^(.+)(Vlan|Role)$/)) {
-                        $merged{$type}{$type_key} = $value;
-                    }
-                }
-                $switch->{roles} = $merged{Role};
-                $switch->{vlans} = $merged{Vlan};
-                $switch->{VoIPEnabled} =  ($switch->{VoIPEnabled} =~ /^\s*(y|yes|true|enabled|1)\s*$/i ? 1 : 0);
-                $switch->{mode} =  lc($switch->{mode});
-                $switch->{'wsUser'} ||= $switch->{'htaccessUser'};
-                $switch->{'wsPwd'}  ||= $switch->{'htaccessPwd'} || '';
-                foreach my $cli_default (qw(EnablePwd Pwd User)) {
-                    $switch->{"cli${cli_default}"}  ||= $switch->{"telnet${cli_default}"};
-                }
-                foreach my $snmpDefault (qw(communityRead communityTrap communityWrite version)) {
-                    my $snmpkey = "SNMP" . ucfirst($snmpDefault);
-                    $switch->{$snmpkey}  ||= $switch->{$snmpDefault};
-                }
-            }
-            $SwitchConfig{'127.0.0.1'} = { %{$SwitchConfig{default}}, type => 'PacketFence', mode => 'production', uplink => ['dynamic'], SNMPVersionTrap => '1', SNMPCommunityTrap => 'public'};
-            freeradius_populate_nas_config(\%SwitchConfig);
-            $config->cache->set("SwitchConfig",\%SwitchConfig);
+            overlaySwitchConfig($config,$name);
         },
     ],
     -oncachereload => [
         on_cached_overlay_reload => sub  {
             my ($config, $name) = @_;
-            my $cache = $config->cache;
-            #getting rid of it from the l1_cache
-            $cache->l1_cache->remove("SwitchConfig");
-            my $data = $cache->get("SwitchConfig");
+            my $data = $config->fromCacheUntainted("SwitchConfig");
             if($data) {
                 %SwitchConfig = %$data;
+            } else {
+                #if not found then repopulate switch
+                $config->doCallbacks(1,0);
             }
         },
     ]
 );
+
+sub overlaySwitchConfig {
+    my ( $config, $name ) = @_;
+    my @switches = grep { exists $SwitchConfig{$_} } $config->Sections;
+    foreach my $switchId ( @switches ) {
+        my $switch = $SwitchConfig{$switchId};
+        # Overlaying switch configation
+        foreach my $key (qw(ip controllerIp)) {
+            $switch->{$key} = $config->val($switchId,$key);
+        }
+    }
+    $config->cache->set( "SwitchConfig", \%SwitchConfig );
+}
 
 =head1 METHODS
 
