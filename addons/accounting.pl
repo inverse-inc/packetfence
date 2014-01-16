eb21951bc79f8ab19fa6d35e26f0b27336cb5bc0
James Rouzier <jrouzier@inverse.ca>
2014-01-16 14:41:32 -0500

    Fixed issue with compiling error with thread lib

--- addons/accounting.pl
+++ addons/accounting.pl
@@ -19,9 +19,6 @@ use warnings;
 
 use Data::Dumper;
 use Net::SNMP;
-use threads;
-use threads::shared;
-use Thread::Pool;
 use Log::Log4perl;
 use Log::Log4perl::Appender::File; # HACK: compile tests failed on build env. without that
 
@@ -34,6 +31,9 @@ use pf::person;
 use pf::locationlog;
 use pf::node;
 use pf::ifoctetslog;
+use threads;
+use threads::shared;
+use Thread::Pool;
 
 Log::Log4perl->init( INSTALL_DIR . '/conf/log.conf' );
 my $logger = Log::Log4perl->get_logger('');
