1b84aa4f121a7b1b626fbb680150b29e36554d20
James Rouzier <jrouzier@inverse.ca>
2014-02-04 13:39:42 -0500

    Have actions add, edit & clone parse parameters from the command line properly

--- lib/pf/base/cmd/config_store.pm
+++ lib/pf/base/cmd/config_store.pm
@@ -18,29 +18,130 @@ use base qw(pf::base::cmd::action_cmd);
 sub action_clone {
     my ($self) = @_;
     my $configStore = $self->configStore;
-    my ($to,$from) = @{$self->{action_args}};
-    $configStore->copy($to,$from);
-    return $configStore->commit;
+    my ($from,$to,%attributes) = $self->action_args;
+    if ( $configStore->copy($from,$to) ) {
+        $configStore->update($to,\%attributes);
+        return $configStore->commit ? 0 : 1;
+    }
+    print "unable able to clone $from to $to\n";
+    return 1;
 }
+
+sub idKey { 'id' }
 
 sub action_get {
     my ($self) = @_;
+    my ($id) = $self->action_args;
+    my $configStore = $self->configStore;
+    my $items;
+    my $idKey = $self->idKey;
+    if($id eq 'all') {
+        $items = $configStore->readAll( $idKey );
+    } else {
+        if($configStore->hasId($id) ) {
+            $items = [$configStore->read($id, $idKey )];
+        }
+    }
+    if ($items) {
+        my @display_fields = $self->display_fields;
+        print join('|',@display_fields),"\n";
+        foreach my $item (@$items) {
+            print join('|',map { $self->format_param($_) } @$item{@display_fields}),"\n";
+        }
+    }
+    return 0;
 }
+
+sub format_param {
+    my ($self,$param) = @_;
+    return '' unless defined $param;
+    return join(',',@$param) if ref($param) eq 'ARRAY';
+    return $param;
+}
+
+sub display_fields { }
 
 sub action_delete {
     my ($self) = @_;
+    my ($id) = $self->action_args;
     my $configStore = $self->configStore;
-    my ($id) = @{$self->{action_args}};
-    $configStore->remove($id);
-    return $configStore->commit;
+    if($configStore->hasId($id) ) {
+        $configStore->remove($id);
+        my $results =  $configStore->commit;
+        return 0 if $results;
+    } else {
+        print "Unknown item $id!\n";
+    }
+    return 1;
 }
+
+sub parse_clone {
+    my ($self,$from,$to,@attributes) = @_;
+    return defined $from && defined $to;
+    $self->{action_args} = [$from,$to];
+    return $self->_parse_attributes(@attributes);
+}
+
+sub _id_defined {
+    my ($self,$id) = @_;
+    return defined $id;
+}
+
+*parse_get = \&_id_defined;
+*parse_delete = \&_id_defined;
 
 sub action_add {
     my ($self) = @_;
+    my $configStore = $self->configStore;
+    my ($id,%attributes) = $self->action_args;
+    if ($configStore->hasId($id)) {
+        print "'$id' already exists!\n";
+        return 1;
+    }
+    $configStore->create($id,\%attributes);
+    return $configStore->commit ? 0 : 1;
 }
 
 sub action_edit {
     my ($self) = @_;
+    my $configStore = $self->configStore;
+    my ($id,%attributes) = $self->action_args;
+    return 1 unless $configStore->hasId($id);
+    $configStore->update($id,\%attributes);
+    return $configStore->commit ? 0 : 1;
+}
+
+sub parse_add {
+    my ($self,$id,@attributes) = @_;
+    return undef unless defined $id;
+    $self->{action_args} = [$id];
+    return $self->_parse_attributes(@attributes);
+}
+
+sub parse_edit {
+    my ($self,$id,@attributes) = @_;
+    return undef unless defined $id;
+    $self->{action_args} = [$id];
+    return $self->_parse_attributes(@attributes);
+}
+
+sub _parse_attributes {
+    my ($self,@attributes) = @_;
+    my $rx = qr/
+        ([a-zA-Z_][a-zA-Z0-9_]*)  #The parameter name
+        =
+        (?|
+            "([&=?()\/,0-9a-zA-Z_\*\.\-\:_\;\@\ \+\!]*)" |
+            ([\/0-9a-zA-Z_\*\.\-\:_\;\@]+)
+        )
+    /x;
+    my @action_args;
+    foreach my $attribute (@attributes) {
+        my (@matches) = ($attribute =~ /$rx/g);
+        push @action_args, @matches;
+    }
+    push @{$self->{action_args}}, @action_args;
+    return 1;
 }
 
 sub configStore { $_[0]->configStoreName->new }
