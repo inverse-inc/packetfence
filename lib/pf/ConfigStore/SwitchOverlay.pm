8bad22353a08a18d67956b55a290298c1e8e7f1b
James Rouzier <jrouzier@inverse.ca>
2014-02-04 11:48:04 -0500

    Move the responsiblity for save SwitchConfig into the cache SwitchOverlay

--- lib/pf/ConfigStore/SwitchOverlay.pm
+++ lib/pf/ConfigStore/SwitchOverlay.pm
@@ -29,7 +29,7 @@ $switches_overlay_cached_config = pf::config::cached->new(
     -onfilereload => [
         on_switches_reload => sub  {
             my ($config, $name) = @_;
-            overlaySwitchConfig($config,$name);
+            populateSwitchConfig($config,$name);
         },
     ],
     -oncachereload => [
@@ -46,17 +46,54 @@ $switches_overlay_cached_config = pf::config::cached->new(
     ]
 );
 
-sub overlaySwitchConfig {
+sub populateSwitchConfig {
     my ( $config, $name ) = @_;
-    my @switches = grep { exists $SwitchConfig{$_} } $config->Sections;
-    foreach my $switchId ( @switches ) {
-        my $switch = $SwitchConfig{$switchId};
-        # Overlaying switch configation
-        foreach my $key (qw(ip controllerIp)) {
-            next unless $config->exists($switchId,$key);
-            $switch->{$key} = $config->val($switchId,$key);
+    $config->toHash( \%SwitchConfig );
+    $config->cleanupWhitespace( \%SwitchConfig );
+    foreach my $switch ( values %SwitchConfig ) {
+
+        # transforming uplink and inlineTrigger to arrays
+        foreach my $key (qw(uplink inlineTrigger)) {
+            my $value = $switch->{$key} || "";
+            $switch->{$key} = [ split /\s*,\s*/, $value ];
+        }
+
+        # transforming vlans and roles to hashes
+        my %merged = ( Vlan => {}, Role => {} );
+        foreach my $key ( grep {/(Vlan|Role)$/} keys %{$switch} ) {
+            next unless my $value = $switch->{$key};
+            if ( my ( $type_key, $type ) = ( $key =~ /^(.+)(Vlan|Role)$/ ) ) {
+                $merged{$type}{$type_key} = $value;
+            }
+        }
+        $switch->{roles}       = $merged{Role};
+        $switch->{vlans}       = $merged{Vlan};
+        $switch->{VoIPEnabled} = (
+            $switch->{VoIPEnabled} =~ /^\s*(y|yes|true|enabled|1)\s*$/i
+            ? 1
+            : 0
+        );
+        $switch->{mode} = lc( $switch->{mode} );
+        $switch->{'wsUser'} ||= $switch->{'htaccessUser'};
+        $switch->{'wsPwd'} ||= $switch->{'htaccessPwd'} || '';
+        foreach my $cli_default (qw(EnablePwd Pwd User)) {
+            $switch->{"cli${cli_default}"}
+              ||= $switch->{"telnet${cli_default}"};
+        }
+        foreach my $snmpDefault (
+            qw(communityRead communityTrap communityWrite version)) {
+            my $snmpkey = "SNMP" . ucfirst($snmpDefault);
+            $switch->{$snmpkey} ||= $switch->{$snmpDefault};
         }
     }
+    $SwitchConfig{'127.0.0.1'} = {
+        %{ $SwitchConfig{default} },
+        type              => 'PacketFence',
+        mode              => 'production',
+        uplink            => ['dynamic'],
+        SNMPVersionTrap   => '1',
+        SNMPCommunityTrap => 'public'
+    };
     $config->cacheForData->set( "SwitchConfig", \%SwitchConfig );
 }
 
