6a1e6c33608894127b9c5cdac1fe20e3650a3a63
Durand Fabrice <fdurand@inverse.ca>
2014-03-07 15:55:01 -0500

    Fixed locationlog_synchronize param in radius.pm

--- lib/pf/radius.pm
+++ lib/pf/radius.pm
@@ -148,7 +148,7 @@ sub authorize {
         if (!node_register($mac, $autoreg_node_defaults{'pid'}, %autoreg_node_defaults)) {
             $logger->error("auto-registration of node $mac failed");
         }
-        locationlog_synchronize($switch_ip, $port, undef, $mac,
+        locationlog_synchronize($switch, $switch_ip, $switch_mac, $port, undef, $mac,
             $isPhone ? $VOIP : $NO_VOIP, $connection_type, $user_name, $ssid
         );
     }
