6a4bd2f5fecfb267ea106b06e59b6d965a275cbc
James Rouzier <jrouzier@inverse.ca>
2014-04-08 14:55:22 -0400

    Added new command admin_roles updated usage to include chiconfig

--- addons/dev-helpers/dump.pl
+++ addons/dev-helpers/dump.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use lib qw(/usr/local/pf/lib);
+use Data::Dumper;
 
 package pf::dump;
 use base qw(pf::cmd::subcmd);
@@ -10,7 +11,7 @@ dump add documentation
 
 =head1 SYNOPSIS
 
-dump.pl <config|floatingdevices|profiles_filters|profiles|sources|switch <id>|switches>
+dump.pl <config|floatingdevices|profiles_filters|profiles|sources|switch <id>|switches|admin_roles|chiconfig>
 
 =head1 DESCRIPTION
 
@@ -25,59 +26,53 @@ use Module::Loaded qw(mark_as_loaded);
 
 package pf::dump::config;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::config;
-    print Dumper(\%pf::config::Config);
+    print Data::Dumper::Dumper(\%pf::config::Config);
 }
 
 
 package pf::dump::floatingdevices;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::config;
-    print Dumper(\%pf::config::ConfigFloatingDevices);
+    print Data::Dumper::Dumper(\%pf::config::ConfigFloatingDevices);
 }
 
 package pf::dump::profiles;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::config;
-    print Dumper(\%pf::config::Profiles_Config);
+    print Data::Dumper::Dumper(\%pf::config::Profiles_Config);
 }
 
 
 package pf::dump::profiles_filters;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::config;
-    print Dumper(\%pf::config::Profile_Filters);
+    print Data::Dumper::Dumper(\%pf::config::Profile_Filters);
 }
 
 package pf::dump::sources;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::authentication;
-    print Dumper(\@pf::authentication::authentication_sources);
+    print Data::Dumper::Dumper(\@pf::authentication::authentication_sources);
 }
 
 package pf::dump::switch;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub parseArgs {
@@ -88,27 +83,34 @@ sub parseArgs {
 sub _run {
     my ($self) = @_;
     require pf::SwitchFactory;
-    print Dumper(pf::SwitchFactory->getInstance->instantiate($self->args));
+    print Data::Dumper::Dumper(pf::SwitchFactory->getInstance->instantiate($self->args));
 }
 
 package pf::dump::switches;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::ConfigStore::Switch;
-    print Dumper(\%pf::ConfigStore::Switch::SwitchConfig);
+    print Data::Dumper::Dumper(\%pf::ConfigStore::Switch::SwitchConfig);
 }
 
 package pf::dump::chiconfig;
 use base qw(pf::dump::cmd);
-use Data::Dumper;
 __PACKAGE__->mark_as_loaded();
 
 sub _run {
     require pf::CHI;
-    print Dumper(pf::CHI::chiConfigFromIniFile());
+    print Data::Dumper::Dumper(pf::CHI::chiConfigFromIniFile());
+}
+
+package pf::dump::admin_roles;
+use base qw(pf::dump::cmd);
+__PACKAGE__->mark_as_loaded();
+
+sub _run {
+    require pf::admin_roles;
+    print Data::Dumper::Dumper(\%pf::admin_roles::ADMIN_ROLES);
 }
 
 package main;
