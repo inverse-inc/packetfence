783533f5fe02bee2a6a4a2274d3c08cd161ebeeb
Julien Semaan <jul.semaan@gmail.com>
2014-05-01 13:11:01 -0400

    Added I18n support to catalyst portal

--- html/captive-portal/lib/captiveportal/PacketFence/Controller/Root.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Controller/Root.pm
@@ -18,6 +18,7 @@ use pf::class;
 use Cache::FileCache;
 use pf::sms_activation;
 use List::Util qw(first);
+use POSIX;
 
 BEGIN { extends 'captiveportal::Base::Controller'; }
 
@@ -44,6 +45,7 @@ captiveportal::PacketFence::Controller::Root - Root Controller for captiveportal
 sub auto : Private {
     my ( $self, $c ) = @_;
     $c->forward('setupCommonStash');
+    $c->forward('setupLanguage');
     return 1;
 }
 
@@ -94,6 +96,121 @@ sub setupCommonStash : Private {
         list_help_info  => \@list_help_info,
     );
 }
+
+=head2 setupLanguage
+
+Define the locale
+
+=cut
+
+sub setupLanguage : Private {
+    my ($self, $c) = @_;
+    my $logger = get_logger;
+    my ($locales) = $c->forward('getLanguages');
+
+    my $locale = shift @$locales;
+    $logger->debug("Setting locale to ".$locale);
+    setlocale(POSIX::LC_MESSAGES, "$locale.utf8"); 
+}
+
+=head2 getLanguages
+
+Retrieve the user prefered languages from the following ordered sources:
+
+=over
+
+=item 1. the 'lang' URL parameter
+
+=item 2. the 'lang' parameter of the Web session
+
+=item 3. the browser accepted languages
+
+=back
+
+If no language matches the authorized locales from the configuration, the first locale
+of the configuration is returned.
+
+=cut
+
+sub getLanguages :Private {
+    my ($self, $c) = @_;
+    my $logger = get_logger;
+    my $portalSession = $c->portalSession;
+
+    my ($lang, @languages);
+
+    my @authorized_locales = $c->profile->getLocales();
+    unless (scalar @authorized_locales > 0) {
+        @authorized_locales = @WEB::LOCALES;
+    }
+
+    $logger->debug("Authorized locale(s) are " . join(', ', @authorized_locales));
+
+    # 1. Check if a language is specified in the URL
+    if ( defined($c->request->param('lang')) ) {
+        my $user_chosen_language = $c->request->param('lang');
+        $user_chosen_language =~ s/^(\w{2})(_\w{2})?/lc($1) . uc($2)/e;
+        if (grep(/^$user_chosen_language$/, @authorized_locales)) {
+            $lang = $user_chosen_language;
+            # Store the language in the session
+            $c->session->{lang} = $lang;
+            $logger->debug("locale from the URL is $lang");
+        }
+        else {
+            $logger->warn("locale from the URL $user_chosen_language is not supported");
+        }
+    }
+
+    # 2. Check if the language is set in the session
+    if ( defined($c->session->{lang}) ) {
+        $lang = $c->session->{lang};
+        push(@languages, $lang) unless (grep/^$lang$/, @languages);
+        $logger->debug("locale from the session is $lang");
+    }
+
+    # 3. Check the accepted languages of the browser
+    my $browser_languages = $c->forward('getRequestLanguages');
+    foreach my $browser_language (@$browser_languages) {
+        $browser_language =~ s/^(\w{2})(_\w{2})?/lc($1) . uc($2)/e;
+        if (grep(/^$browser_language$/, @authorized_locales)) {
+            $lang = $browser_language;
+            push(@languages, $lang) unless (grep/^$lang$/, @languages);
+            $logger->debug("locale from the browser is $lang");
+        }
+        else {
+            $logger->trace("locale from the browser $browser_language is not supported");
+        }
+    }
+
+    if (scalar @languages > 0) {
+        $logger->trace("prefered user languages are " . join(", ", @languages));
+    }
+    else {
+        push(@languages, $authorized_locales[0]);
+    }
+
+    return \@languages;
+}
+
+=head2 getRequestLanguages
+
+Extract the preferred languages from the HTTP request.
+Ex: Accept-Language: en-US,en;q=0.8,fr;q=0.6,fr-CA;q=0.4,no;q=0.2,es;q=0.2
+will return qw(en_US en fr fr_CA no es)
+
+=cut
+
+sub getRequestLanguages : Private{
+    my ($self, $c) = @_;
+    my $s = $c->request->header('Accept-language') || 'en_US';
+    my @l = split(/,/, $s);
+    map { s/;.+// } @l;
+    map { s/-/_/g } @l;
+    #@l = map { m/^en(_US)?/? ():$_ } @l;
+
+    return \@l;
+}
+
 
 
 =head2 end
