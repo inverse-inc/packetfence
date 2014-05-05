b9a7a56332eed1e67d5076cd5e2863208d2a157d
James Rouzier <jrouzier@inverse.ca>
2014-05-05 12:08:48 -0400

    Fixed password special characters in passwords not being supported

--- lib/pf/WebAPI/AuthenHandler.pm
+++ lib/pf/WebAPI/AuthenHandler.pm
@@ -25,6 +25,7 @@ use Apache2::RequestRec;
 use Apache2::Const -compile => qw(OK DECLINED HTTP_UNAUTHORIZED);
 
 use pf::config;
+use pf::log;
 
 
 =head1 METHODS
@@ -37,19 +38,21 @@ apache handler for webservices authentication
 
 sub handler {
     my $r = shift;
-
-    my ($status, $password) = $r->get_basic_auth_pw;
-    if ($status == Apache2::Const::OK) {
-        my $user = $r->user;
-        my $webservices = $Config{webservices};
-        unless ( defined $user && defined $password &&
-                $webservices->{user} eq $user &&
-                $webservices->{pass} eq $password  ) {
-            $r->note_basic_auth_failure;
-            $status = Apache2::Const::HTTP_UNAUTHORIZED;
+    my $status = Apache2::Const::OK;
+    my ($webservices_user,$webservices_pass) = @{$Config{webservices}}{qw(user pass)};
+    if(defined $webservices_user && $webservices_user ne '' && defined $webservices_pass && $webservices_pass ne '') {
+        my $pass;
+        ($status, $pass) = $r->get_basic_auth_pw;
+        if ($status == Apache2::Const::OK) {
+            my $user = $r->user;
+            unless ( defined $user && defined $pass &&
+                    $webservices_user eq $user &&
+                    $webservices_pass eq $pass  ) {
+                $r->note_basic_auth_failure;
+                $status = Apache2::Const::HTTP_UNAUTHORIZED;
+            }
         }
     }
-
     return $status;
 }
 
