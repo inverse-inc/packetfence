af206c9a937f71a1bbceea6567b039c23eb2abf0
James Rouzier <jrouzier@inverse.ca>
2014-05-16 07:51:00 -0400

    Added sql for pf-4.3.0

--- db/pf-schema-4.3.0.sql
+++ db/pf-schema-4.3.0.sql
@@ -893,3 +893,21 @@ CREATE TABLE wrix (
   `MAC_Address` varchar(255) NULL DEFAULT NULL,
    PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+--
+-- Table structure for table `pending_activation`
+--
+
+CREATE TABLE pending_activation (
+  `code_id` int NOT NULL AUTO_INCREMENT,
+  `pid` varchar(255) default NULL,
+  `mac` varchar(17) default NULL,
+  `contact_info` varchar(255) NOT NULL, -- email or phone number were approbation request is sent 
+  `activation_code` varchar(255) NOT NULL,
+  `expiration` datetime NOT NULL,
+  `status` varchar(60) default NULL,
+  `type` varchar(60) default NULL,
+  PRIMARY KEY (code_id),
+  KEY `identifier` (pid, mac),
+  KEY `activation` (activation_code, status)
+) ENGINE=InnoDB;
+
