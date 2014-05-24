fede98a3e3c12d54605195ac7d4387bd5f87967c
James Rouzier <jrouzier@inverse.ca>
2014-05-24 14:37:02 -0400

    New provisioners android and mobileconfig

---
+++ lib/pf/provisioner/mobileconfig.pm
@@ -0,0 +1,85 @@
+package pf::provisioner::mobileconfig;
+=head1 NAME
+
+pf::provisioner::mobileconfig add documentation
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::provisioner::mobileconfig
+
+=cut
+
+use strict;
+use warnings;
+use Moo;
+extends 'pf::provisioner';
+
+=head1 Atrributes
+
+=head2 hidden_ssid
+
+The hidden ssid
+
+=cut
+
+has hidden_ssid => (is => 'rw');
+
+=head2 category
+
+The category
+
+=cut
+
+has category => (is => 'rw');
+
+=head2 ca_cert_path
+
+The ca cert_path
+
+=cut
+
+has ca_cert_path => (is => 'rw');
+
+=head1 METHODS
+
+=head2 authorize
+
+authorize if 
+
+=cut
+
+sub authorize {
+    my ($self,$mac) = @_;
+    return 0;
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2013 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and::or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
