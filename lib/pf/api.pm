1cd84b58fa22080de9a06708510c1928fcdafbd4
Louis Munro <lmunro@inverse.ca>
2014-06-11 01:53:45 -0400

    Further corrections to pf::config imports.

--- lib/pf/api.pm
+++ lib/pf/api.pm
@@ -195,7 +195,7 @@ sub ReAssignVlan {
     my $switch = pf::SwitchFactory->getInstance()->instantiate( $postdata{'switch'} );
 
     # SNMP traps connections need to be handled specially to account for port-security etc.
-    if ( ($postdata{'connection_type'} & $WIRED_SNMP_TRAPS) == $pf::config::WIRED_SNMP_TRAPS ) {
+    if ( ($postdata{'connection_type'} & $pf::config::WIRED_SNMP_TRAPS) == $pf::config::WIRED_SNMP_TRAPS ) {
         _reassignSNMPConnections($switch, $postdata{'mac'}, $postdata{'ifIndex'}, $postdata{'connection_type'} );
     }
     elsif ( $postdata{'connection_type'} & $pf::config::WIRED) {
