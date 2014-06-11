b6edc5ba079da11f56d1a26ec49bcb28b261f079
James Rouzier <jrouzier@inverse.ca>
2014-06-11 11:43:35 -0400

    Create new table activation and merge email_activation and sms_activation into the activation table

--- db/upgrade-4.2.0-4.3.0.sql
+++ db/upgrade-4.2.0-4.3.0.sql
@@ -1,11 +1,34 @@
 --
--- Add a column to store the portal used for email/sponsor activation
---
-
-ALTER TABLE email_activation ADD `portal` varchar(255) default NULL AFTER `type`;
-
---
 -- Add column to store portal and source in person table
 --
+
 ALTER TABLE person ADD `portal` varchar(255) default NULL;
 ALTER TABLE person ADD `source` varchar(255) default NULL;
+
+--
+-- Table structure for table `activation`
+--
+
+CREATE TABLE activation (
+  `code_id` int NOT NULL AUTO_INCREMENT,
+  `pid` varchar(255) default NULL,
+  `mac` varchar(17) default NULL,
+  `contact_info` varchar(255) NOT NULL, -- email or phone number were approbation request is sent 
+  `carrier_id` int(11) NULL,
+  `activation_code` varchar(255) NOT NULL,
+  `expiration` datetime NOT NULL,
+  `status` varchar(60) default NULL,
+  `type` varchar(60) NOT NULL,
+  `portal` varchar(255) NOT NULL DEFAULT 'default',
+  PRIMARY KEY (code_id),
+  KEY `mac` (mac),
+  KEY `identifier` (pid, mac),
+  KEY `activation` (activation_code, status)
+) ENGINE=InnoDB;
+
+INSERT INTO activation ( pid, mac, contact_info, activation_code, expiration, status, `type`, portal) SELECT  pid, mac, email, activation_code, expiration, status, IFNULL(`type`,'guest'), 'default' from email_activation;
+INSERT INTO activation ( mac, contact_info, carrier_id, activation_code, expiration, status, `type`, portal) SELECT mac, phone_number, carrier_id, activation_code, expiration, status, 'sms', 'default' from sms_activation;
+
+-- DROP TABLE email_activation;
+-- DROP TABLE sms_activation;
+
