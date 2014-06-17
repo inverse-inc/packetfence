4144d911e4f7837f4f5edd0ced4dd925e4679916
James Rouzier <jrouzier@inverse.ca>
2014-06-17 14:23:19 -0400

    Use update_fields from Form::Portal::Common and force redirecturl to be required

--- html/pfappserver/lib/pfappserver/Form/Portal/Profile/Default.pm
+++ html/pfappserver/lib/pfappserver/Form/Portal/Profile/Default.pm
@@ -29,23 +29,7 @@ has_block 'definition' =>
    render_list => [ qw(id description logo redirecturl always_use_redirecturl billing_engine nbregpages) ],
   );
 
-
-=head1 METHODS
-
-=head2 update_fields
-
-The redirection URL is mandatory for the default profile.
-
-=cut
-
-sub update_fields {
-    my $self = shift;
-
-    $self->field('redirecturl')->required(1);
-
-    # Call the theme implementation of the method
-    $self->SUPER::update_fields();
-}
+has_field '+redirecturl' => ( required => 1 );
 
 
 =head1 COPYRIGHT
