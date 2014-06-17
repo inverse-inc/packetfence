0659ec00deb8db7248dd38e84b34d1b5fac1890f
James Rouzier <jrouzier@inverse.ca>
2014-06-17 14:17:47 -0400

    Make the id field readonly if it has a value

--- html/pfappserver/lib/pfappserver/Form/Portal/Profile.pm
+++ html/pfappserver/lib/pfappserver/Form/Portal/Profile.pm
@@ -18,6 +18,7 @@ extends 'pfappserver::Base::Form';
 with 'pfappserver::Form::Portal::Common';
 
 use pf::config;
+use pf::log;
 use List::MoreUtils qw(uniq);
 
 =head1 BLOCKS
@@ -66,6 +67,25 @@ has_field 'filter.contains' =>
    label => 'Filter',
    widget_wrapper => 'DynamicTableRow',
   );
+
+=head1 METHODS
+
+=head2 update_fields
+
+The redirection URL is mandatory for the default profile.
+
+=cut
+
+sub update_fields {
+    my $self = shift;
+    my $init_object = $self->init_object;
+
+    $self->field('id')->readonly(1) if (defined $init_object && defined $init_object->{id});
+
+    # Call the theme implementation of the method
+    $self->SUPER::update_fields();
+}
+
 
 =head1 COPYRIGHT
 
