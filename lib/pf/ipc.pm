63ee7285898b683298358ea266f765e8292ba975
mullagain <m5mulli@gmail.com>
2014-06-23 10:44:51 -0500

    initial commit of ipc.pm

---
+++ lib/pf/ipc.pm
@@ -0,0 +1,100 @@
+package pf::ipc;
+
+=head1 NAME
+
+pf::ipc - extended Redis::Fast class for IPC communication within PacketFence.
+
+=head1 SYNOPSIS
+
+    use pf::ipc;
+
+    my $ipc = pf::ipc->new(); # accepts any of the Redis::Fast arguments.
+
+    #strings
+    $ipc->enqueue('messages', 'hello world');
+
+    #refs
+    $ipc->enqueue('messages', { msg => 'hello world' });
+    $ipc->enqueue('messages', [ 'hello','world' ]);
+
+    my $item = $ipc->dequeue('messages');
+
+    my $count = $ipc->queueCount('messages');
+
+
+
+=head1 DESCRIPTION
+
+This class extends Redis::Fast. All functions in Redis::Fast are preserved. Helper methods have been added for enqueueing/dequeueing items into a redis list. This allows fast communication between processes via lists, hashes, and key/val. Please see the documentation for Redis.pm for more information on the Redis methods.
+
+Currently it is up to the developer to make sure the data is consisten between queues, i.e array refs vs hash refs vs strings.
+
+In the future I may add a queue/hash registration process to ensure data consistency. 
+
+=cut
+
+use Moose;
+use MooseX::NonMoose;
+extends 'Redis::Fast';
+use JSON::XS;
+
+has 'encoder' => (is => 'ro', default => sub { JSON::XS->new; });
+
+sub enqueue {
+    my ($self,$q,$item) = @_;
+    my $txt;
+    if ( ref($item) ) {
+        $txt = $self->encoder->encode( $item );
+    }
+    else {
+        $txt = $item;
+    }
+    $self->rpush($q,$txt);
+
+}
+
+sub dequeue {
+    my ($self,$q) = @_;
+    my $txt = $self->lpop($q);
+    my $ret;
+    if ($txt =~ /(\{|\[)/) {
+        $ret = $self->encoder->decode($txt);
+    }
+    else {
+        $ret = $txt;
+    }
+    return $ret;
+}
+
+# I hate cammel case...
+sub queueCount {
+    my ($self,$q) = @_;
+    return $self->llen($q);
+}
+
+__PACKAGE__->meta->make_immuntable;
+
+=head1 AUTHOR
+
+mullagain <m5mulli@gmail.com>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
