e241c87980772c718bb62072b62f31415afe211f
Julien Semaan <jul.semaan@gmail.com>
2014-06-27 12:46:10 -0400

    Fix POST to /pay (cleaned)

--- html/captive-portal/lib/captiveportal/PacketFence/Controller/Pay.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Controller/Pay.pm
@@ -36,7 +36,7 @@ Catalyst Controller.
 
 sub begin : Private {
     my ( $self, $c ) = @_;
-    if( isdisabled($Config{'registration'}{'billing_engine'}) ) {
+    if( isdisabled($c->profile->getBillingEngine) ) {
         $c->response->redirect("/captive-portal?destination_url=".uri_escape($c->portalSession->profile->getRedirectURL));
         $c->detach;
     }
@@ -50,7 +50,7 @@ sub begin : Private {
 sub index : Path : Args(0) {
     my ( $self, $c ) = @_;
     my $request = $c->request;
-    if ( defined($request->param('submit')) ) {
+    if ( $request->method eq 'POST' ) {
         $c->detach('processBilling');
     } 
     for my $p ('firstname', 'lastname', 'email', 'ccnumber', 'ccexpiration', 'ccvalidation') {
