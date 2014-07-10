9ad62472e753251e52e58d8ef39e89d24f9661e3
Julien Semaan <jul.semaan@gmail.com>
2014-07-10 15:43:18 -0400

    Added Anyfi switch module

---
+++ lib/pf/Switch/Anyfi.pm
@@ -0,0 +1,117 @@
+package pf::Switch::Anyfi;
+
+=head1 NAME
+
+pf::Switch::WirelessModuleTemplate
+
+=head1 SYNOPSIS
+
+The pf::Switch::Anyfi module implements an object oriented interface to 
+manage the Anyfi gateway
+
+=head1 STATUS
+
+Developed and tested on Anyfi gateway running some version
+
+=over
+
+=item Supports
+
+=over
+
+=item <feature a>
+
+=item <feature b>
+
+=back
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+=over
+
+=item <problem a>
+
+<problem description>
+
+=back
+
+=cut
+
+use strict;
+use warnings;
+
+use base ('pf::Switch');
+use Log::Log4perl;
+
+use pf::config;
+
+=head1 SUBROUTINES
+
+=over
+
+=cut
+
+# CAPABILITIES
+# access technology supported
+sub supportsWirelessDot1x { return $TRUE; }
+sub supportsWirelessMacAuth { return $TRUE; }
+# inline capabilities
+sub inlineCapabilities { return ($MAC,$SSID); }
+
+=item deauthenticateMacDefault
+
+De-authenticate a MAC address from wireless network (including 802.1x).
+
+New implementation using RADIUS Disconnect-Request.
+
+=cut
+
+sub deauthenticateMacDefault {
+    my ( $self, $mac, $is_dot1x ) = @_;
+    my $logger = Log::Log4perl::get_logger( ref($self) );
+
+    if ( !$self->isProductionMode() ) {
+        $logger->info("not in production mode... we won't perform deauthentication");
+        return 1;
+    }
+
+    $logger->debug("deauthenticate $mac using RADIUS Disconnect-Request deauth method");
+    return $self->radiusDisconnect($mac);
+}
+
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2013 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
