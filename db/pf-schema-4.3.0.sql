efba11876a6ad1ba9c8125b77570052298d81323
Durand Fabrice <fdurand@inverse.ca>
2014-07-14 15:24:31 -0400

    Update db schema

--- db/pf-schema-4.3.0.sql
+++ db/pf-schema-4.3.0.sql
@@ -274,7 +274,9 @@ CREATE TABLE `locationlog_history` (
   `end_time` datetime default NULL,
   `switch_ip` varchar(17) DEFAULT NULL,
   `switch_mac` varchar(17) DEFAULT NULL,
-  KEY `locationlog_history_view_mac` (`mac`, `end_time`),
+  `stripped_user_name` varchar (255) NOT NULL default '',
+  `realm`  varchar (255) NOT NULL default '',
+  KEY `locationlog_view_mac` (`mac`, `end_time`),
   KEY `locationlog_view_switchport` (`switch`,`port`,`end_time`,`vlan`)
 ) ENGINE=InnoDB;
 
