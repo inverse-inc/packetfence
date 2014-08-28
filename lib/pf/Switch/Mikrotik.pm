c1533de19e2edbc461fd5a7f56a5679dfc21b4d9
Durand Fabrice <oeufdure@gmail.com>
2014-08-28 19:34:19 -0400

    Remove parseTrap function

--- lib/pf/Switch/Mikrotik.pm
+++ lib/pf/Switch/Mikrotik.pm
@@ -46,23 +46,6 @@ sub supportsWirelessMacAuth { return $TRUE; }
 sub inlineCapabilities { return ($MAC,$SSID); }
 
 
-=item parseTrap
-
-This is called when we receive an SNMP-Trap for this device
-
-=cut
-
-sub parseTrap {
-    my ( $this, $trapString ) = @_;
-    my $trapHashRef;
-    my $logger = Log::Log4perl::get_logger( ref($this) );
-
-    $logger->debug("trap currently not handled");
-    $trapHashRef->{'trapType'} = 'unknown';
-
-    return $trapHashRef;
-}
-
 =item getVersion - obtain image version information from switch
 
 =cut
