691f585c13104224c1e87f0cd8a1186190fe7677
Durand Fabrice <fdurand@inverse.ca>
2014-09-05 11:15:33 -0400

    Change upgrade sql script

--- db/upgrade-4.3.0-4.4.0.sql
+++ db/upgrade-4.3.0-4.4.0.sql
@@ -28,4 +28,6 @@ ALTER TABLE locationlog
     ADD `stripped_user_name` varchar(255) DEFAULT NULL,
     ADD `realm` varchar(255) DEFAULT NULL;
 
-
+ALTER TABLE locationlog_history
+    ADD `stripped_user_name` varchar(255) DEFAULT NULL,
+    ADD `realm` varchar(255) DEFAULT NULL;
