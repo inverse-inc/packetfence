836177678ad110f1e50e873ccb32c8ec015f6b5d
Julien Semaan <jul.semaan@gmail.com>
2014-10-30 13:36:11 -0400

    reactivate flow installation

--- addons/opendaylight/deploy_devel.sh
+++ addons/opendaylight/deploy_devel.sh
@@ -1,2 +1,2 @@
 mvn package
-scp target/odlpf-0.1.jar odl:/var/odl/controller/opendaylight/distribution/opendaylight/target/distribution.opendaylight-osgipackage/opendaylight/plugins/
+scp target/odlpf-0.1.jar root@172.20.155.150:/var/odl/controller/opendaylight/distribution/opendaylight/target/distribution.opendaylight-osgipackage/opendaylight/plugins/
