a863eaaece842535186556b37f4ea3232783dcaf
James Rouzier <jrouzier@inverse.ca>
2014-11-12 15:41:12 -0500

    Refactor the destination_url handling

--- html/captive-portal/lib/captiveportal/PacketFence/Model/Portal/Session.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Model/Portal/Session.pm
@@ -83,8 +83,14 @@ has redirectURL => (
     is       => 'rw',
 );
 
-has destination_url => (
+has _destination_url => (
     is       => 'rw',
+);
+
+has destinationUrl => (
+    is      => 'ro',
+    builder => '_build_destinationUrl',
+    lazy => 1,
 );
 
 has [qw(forwardedFor guestNodeMac)] => ( is => 'rw', );
@@ -102,9 +108,8 @@ sub ACCEPT_CONTEXT {
     my $redirectURL;
     my $uri = $request->uri;
     my $options;
-    my $destination_url;
     my $mgmt_ip = $management_network->{'Tvip'} || $management_network->{'Tip'} if $management_network;
-    $destination_url = $request->param('destination_url') if defined($request->param('destination_url'));
+    my $destination_url = $request->param('destination_url');
 
     if( defined ( my $last_uri = $r->pnotes('last_uri') )) {
         $options = {
@@ -127,7 +132,7 @@ sub ACCEPT_CONTEXT {
         remoteAddress => $remoteAddress,
         forwardedFor  => $forwardedFor,
         options       => $options,
-        destination_url => $destination_url,
+        _destination_url => $destination_url,
         @args,
     );
     $c->session->{$class} = $model;
@@ -136,14 +141,14 @@ sub ACCEPT_CONTEXT {
 
 sub _build_destinationUrl {
     my ($self) = @_;
-
+    my $url = $self->_destination_url;
     # Return portal profile's redirection URL if destination_url is not set or if redirection URL is forced
-    if (!defined($self->destination_url) || isenabled($self->profile->forceRedirectURL)) {
+    if (!defined($url) || isenabled($self->profile->forceRedirectURL)) {
         return $self->profile->getRedirectURL;
     }
 
     # Respect the user's initial destination URL
-    return decode_entities(uri_unescape($self->destination_url));
+    return decode_entities(uri_unescape($url));
 }
 
 sub _build_clientIp {
