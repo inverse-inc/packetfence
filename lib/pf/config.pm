629fe270d2c25a952c9adfe85a230fb59d8ba997
Durand Fabrice <fdurand@inverse.ca>
2014-11-21 16:37:08 -0500

    Added admin gui active/active configuration

--- lib/pf/config.pm
+++ lib/pf/config.pm
@@ -858,7 +858,7 @@ sub _fetch_virtual_ip {
     # [interface $int].vip= ... always wins
     return $Config{$config_section}{'vip'} if defined($Config{$config_section}{'vip'});
 
-    return if defined($Config{$config_section}{'haproxyip'});
+    return if (defined($Config{$config_section}{'active_active_ip'}) && isenabled($Config{$config_section}{'active_active_enabled'}));
 
     my $if = Net::Interface->new($interface);
     return if (!defined($if));
