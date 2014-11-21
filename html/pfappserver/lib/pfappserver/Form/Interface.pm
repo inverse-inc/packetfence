64997a9c475d5fea121c6f4de837844673d8ee04
Durand Fabrice <fdurand@inverse.ca>
2014-11-21 15:13:29 -0500

    Changed for actif/actif setup

--- html/pfappserver/lib/pfappserver/Form/Interface.pm
+++ html/pfappserver/lib/pfappserver/Form/Interface.pm
@@ -42,7 +42,7 @@ has_field 'type' =>
   );
 has_field 'dns' =>
   (
-   type => 'IPAddress',
+   type => 'IPAddresses',
    label => 'DNS',
    wrapper_attr => { 'style' => 'display: none' },
    tags => { after_element => \&help,
@@ -64,6 +64,7 @@ has_field 'high_availability' =>
     unchecked_value => 0,
     default => 0,
    );
+
 has_field 'vip' =>
   (
    type => 'IPAddress',
@@ -77,6 +78,25 @@ has_field 'nat_enabled' => (
     default => 1,
     label => 'Enable NATting',
 );
+
+has_field 'active_active_enabled' => (
+    type => 'Toggle',
+    checkbox_value => 1,
+    unchecked_value => 0,
+    default => 0,
+    label => 'Enable Active/Active',
+);
+
+has_field 'active_active_ip' => (
+    type => 'IPAddress',
+    label => 'Active/Active IP Address',
+);
+
+has_field 'active_active_members' => (
+    type => 'IPAddresses',
+    label => 'Active/Active ip members',
+);
+
 
 =head2 options_type
 
