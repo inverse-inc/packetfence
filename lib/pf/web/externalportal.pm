3362ea9487db5c67d96008931705ac32f2473109
Derek Wuelfrath <dwuelfrath@inverse.ca>
2014-12-17 16:14:54 -0500

    100 seconds is way to short for an iplog record to live in the case of webauth. Pass this timeframe, PacketFence will not be able to fetch the MAC and won't be able to achieve anything else.

--- lib/pf/web/externalportal.pm
+++ lib/pf/web/externalportal.pm
@@ -78,7 +78,7 @@ sub external_captive_portal {
                 $logger->debug("Adding additionnal session parameter for url detected : $key : ".$req->param($key));
                 $portalSession->session->param("ecwp-original-param-$key", $req->param($key));
             }
-            iplog_update($client_mac,$client_ip,100) if (defined ($client_ip) && defined ($client_mac));
+            iplog_update($client_mac,$client_ip,3600) if (defined ($client_ip) && defined ($client_mac));
             return ($portalSession->session->id(), $redirect_url);
         } else {
             return 0;
@@ -98,7 +98,7 @@ sub external_captive_portal {
             my $portalSession = pf::Portal::Session->new(%session_id);
             $portalSession->setClientMac($session_id{client_mac}) if (defined($session_id{client_mac}));
             $portalSession->setDestinationUrl($r->headers_in->{'Referer'}) if (defined($r->headers_in->{'Referer'}));
-            iplog_update($session_id{client_mac},$r->connection->remote_ip,100) if (defined ($r->connection->remote_ip) && defined ($session_id{client_mac}));
+            iplog_update($session_id{client_mac},$r->connection->remote_ip,3600) if (defined ($r->connection->remote_ip) && defined ($session_id{client_mac}));
             return $portalSession->session->id();
         } else {
             return 0;
