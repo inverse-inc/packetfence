dd8571ce565b39d4c78626371a592a3f5c431701
Julien Semaan <jul.semaan@gmail.com>
2015-01-08 15:28:17 -0500

    compute dynamic unreg date when doing 802.1x

--- lib/pf/vlan.pm
+++ lib/pf/vlan.pm
@@ -415,6 +415,7 @@ sub getNormalVlan {
                 $value = &pf::authentication::match([@sources], $params, $Actions::SET_UNREG_DATE);
             }
             if (defined $value) {
+                $value = pf::config::dynamic_unreg_date($value);
                 my %info = (
                     'unregdate' => $value,
                     'category' => $role,
