6a6285c9379342b7e683d2cf433a8065305ff304
Julien Semaan <jul.semaan@gmail.com>
2015-01-08 08:32:00 -0500

    added domain.conf to configstore

--- lib/pf/file_paths.pm
+++ lib/pf/file_paths.pm
@@ -61,6 +61,7 @@ our (
     $realm_config_file,
     $cluster_config_file,
     $server_cert, $server_key, $server_pem,
+    $domain_config_file,
 );
 
 BEGIN {
@@ -98,8 +99,12 @@ BEGIN {
         $vlan_filters_config_file
         $pfcmd_binary
         $realm_config_file
+<<<<<<< HEAD
         $cluster_config_file
         $server_cert $server_key $server_pem
+=======
+        $domain_config_file
+>>>>>>> added domain.conf to configstore
     );
 }
 
@@ -147,10 +152,14 @@ $apache_filters_config_file = catfile($conf_dir, "apache_filters.conf");
 $vlan_filters_config_file = catfile($conf_dir, "vlan_filters.conf");
 $firewall_sso_config_file =  catfile($conf_dir,"firewall_sso.conf");
 $realm_config_file = catfile($conf_dir,"realm.conf");
+<<<<<<< HEAD
 $cluster_config_file = catfile($conf_dir,"cluster.conf");
 $server_key = catfile($conf_dir,"ssl/server.key");
 $server_cert = catfile($conf_dir,"ssl/server.crt");
 $server_pem = catfile($conf_dir,"ssl/server.pem");
+=======
+$domain_config_file = catfile($conf_dir,"domain.conf");
+>>>>>>> added domain.conf to configstore
 
 $oui_url               = 'http://standards.ieee.org/regauth/oui/oui.txt';
 $dhcp_fingerprints_url = 'http://www.packetfence.org/dhcp_fingerprints.conf';
