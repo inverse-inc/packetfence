2fcd4a0816aa38b21f6e59af4fb1a27bd8b559a7
Julien Semaan <jul.semaan@gmail.com>
2015-01-08 10:07:38 -0500

    Added unjoin to ad script + cleanup

--- addons/bind_to_ad.pl
+++ addons/bind_to_ad.pl
@@ -17,11 +17,18 @@ our $CONF_FILE = "/usr/local/pf/conf/domain.conf";
 
 sub register_new_domain {
   my $cfg = pf::ConfigStore::Domain->new;
+
+
   my $info;
 
   print "Enter the friendly domain name : ";
   my $domain = <STDIN>;
   $domain =~ s/\n//g;
+
+  if($cfg->read($domain)){
+    print "There is already a domain configured for this name. Unjoin first.";
+    exit 1;
+  }
 
   print "Enter the workgroup : ";
   $info->{workgroup} = <STDIN>;
@@ -53,6 +60,34 @@ sub register_new_domain {
 
   $cfg->update_or_create($domain, $info);
   $cfg->commit;
+
+  regenerate_configuration();  
+
+  print system("ip netns exec $domain net ads join -S $info->{ad_server} $info->{dns_name} -s /etc/samba/$domain.conf -U $info->{bind_dn}%$info->{bind_pass}");
+
+  print system("/etc/init.d/winbind.$domain restart");
+
+}
+
+sub unjoin_domain {
+  my $cfg = pf::ConfigStore::Domain->new;
+  
+  print "Enter the friendly domain name : ";
+  my $domain = <STDIN>;
+  $domain =~ s/\n//g;
+
+  my $info = $cfg->read($domain);
+  if($info){
+    print system("ip netns exec $domain net ads leave -S $info->{ad_server} $info->{dns_name} -s /etc/samba/$domain.conf -U $info->{bind_dn}%$info->{bind_pass}");
+    $cfg->remove($domain);
+    $cfg->commit;
+    exit 1;
+  }
+  else{
+    print "Domain is not configured";
+  }
+
+
 }
 
 sub generate_krb5_conf {
@@ -86,6 +121,7 @@ sub generate_resolv_conf {
     pf_run("mkdir -p /etc/netns/$domain");
     my %vars = (domain => $domain);
     my %tmp = (%vars, %{$ConfigDomain{$domain}});
+    %vars = %tmp;
     my $template = Template->new;
     $template->process("addons/AD/resolv.tt", \%vars, "/etc/netns/$domain/resolv.conf"); 
   }  
@@ -110,5 +146,12 @@ sub regenerate_configuration {
   print pf_run("/usr/local/pf/bin/pfcmd service iptables restart");
 }
 
-regenerate_configuration();
-#register_new_domain();
+my %actions = ("join" => \&register_new_domain, "unjoin" => \&unjoin_domain, "refresh" => \&regenerate_configuration);
+
+if(exists $actions{$ARGV[0]}){
+  $actions{$ARGV[0]}->();
+}
+else{
+  print "Unknown operation...";
+}
+
