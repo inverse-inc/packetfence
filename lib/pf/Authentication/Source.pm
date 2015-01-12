1b8c2bc36502b000879f0cebf53db767de1c8227
James Rouzier <jrouzier@inverse.ca>
2015-01-12 16:01:09 -0500

    Added pre and post action when matching rules

--- lib/pf/Authentication/Source.pm
+++ lib/pf/Authentication/Source.pm
@@ -137,6 +137,7 @@ sub match {
     $params->{current_time} = $current_time;
 
     my @matching_rules = ();
+    $self->preMatchProcessing;
 
     foreach my $rule ( @{$self->{'rules'}} ) {
         my @matching_conditions = ();
@@ -175,10 +176,12 @@ sub match {
         # so let's keep the @matching_rules array for now.
         if (scalar @matching_rules == 1) {
             $logger->info("Matched rule (".$rule->{'id'}.") in source ".$self->id.", returning actions.");
+            $self->postMatchProcessing;
             return $rule->{'actions'};
         }
 
     } # foreach my $rule ( @{$self->{'rules'}} ) {
+    $self->postMatchProcessing;
 
     return undef;
 }
@@ -214,6 +217,21 @@ sub search_attributes {
 
 }
 
+=head2 postMatchProcessing
+
+Tear down any resources created in preMatchProcessing
+
+=cut
+
+sub postMatchProcessing { }
+
+=head2 preMatchProcessing
+
+Setup any resouces need for matching
+
+=cut
+
+sub preMatchProcessing { }
 
 =head1 AUTHOR
 
