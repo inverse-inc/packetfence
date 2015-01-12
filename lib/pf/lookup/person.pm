d0234807ebc8a175b72a91dcef709f5959cc916c
Zammit Ludovic <lzammit@inverse.ca>
2015-01-12 13:29:46 -0500

    fix display error and fix lookup_person in vlan.pm

--- lib/pf/lookup/person.pm
+++ lib/pf/lookup/person.pm
@@ -26,8 +26,8 @@ sub lookup_person {
     my $logger = Log::Log4perl::get_logger('pf::lookup::person');
     my $source = pf::authentication::getAuthenticationSource($source_id);
     if (!$source) {
-       $logger->info("Unable to locate PID '$pid'");
-       return "Unable to locate PID '$pid'!\n";
+       $logger->info("Unable to locate the source '$pid'");
+       return "Unable to locate the source '$pid'!\n";
     } 
     
     unless (person_exist($pid)) {
