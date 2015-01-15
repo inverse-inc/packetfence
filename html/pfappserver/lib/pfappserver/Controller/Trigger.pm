ce15319a1fbb68c7cfec5b83db0436355cf644b0
James Rouzier <jrouzier@inverse.ca>
2015-01-15 12:03:40 -0500

    Updated copy right

--- html/pfappserver/lib/pfappserver/Controller/Trigger.pm
+++ html/pfappserver/lib/pfappserver/Controller/Trigger.pm
@@ -30,11 +30,9 @@ sub search : Local : Args(2) {
 
 Inverse inc. <info@inverse.ca>
 
-Minor parts of this file may have been contributed. See CREDITS.
-
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2014 Inverse inc.
+Copyright (C) 2005-2015 Inverse inc.
 
 =head1 LICENSE
 
