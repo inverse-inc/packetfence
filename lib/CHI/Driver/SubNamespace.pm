f4662936b1c79dccfb7c7b4c90cf956632c166db
James Rouzier <jrouzier@inverse.ca>
2015-01-22 15:07:01 -0500

    Updated pod

--- lib/CHI/Driver/SubNamespace.pm
+++ lib/CHI/Driver/SubNamespace.pm
@@ -20,11 +20,19 @@ extends 'CHI::Driver';
 
 has 'chi_object'   => ( is => 'ro', isa => InstanceOf['CHI::Driver'], required => 1 );
 
+=head2 fetch
+
+=cut
+
 sub fetch {
     my ($self, $key) = @_;
     my $namespace = $self->namespace;
     return scalar($self->chi_object->get("${namespace}:${key}"));
 }
+
+=head2 store
+
+=cut
 
 sub store {
     my ($self,$key,@args) = @_;
@@ -32,11 +40,19 @@ sub store {
     return $self->chi_object->set("${namespace}:${key}",@args);
 }
 
+=head2 remove
+
+=cut
+
 sub remove {
     my ($self, $key) = @_;
     my $namespace = $self->namespace;
     $self->chi_object->remove( "${namespace}:${key}");
 }
+
+=head2 clear
+
+=cut
 
 sub clear {
     my $self = shift;
@@ -44,12 +60,20 @@ sub clear {
     return $self->chi_object->clear( grep { /^\Q$namespace\E/ } $self->chi_object->get_keys(@_));
 }
 
+=head2 get_keys
+
+=cut
+
 sub get_keys {
     my $self = shift;
     my $namespace = $self->namespace;
     local $_;
     return grep { s/^\Q$namespace\E// } $self->chi_object->get_keys(@_);
 }
+
+=head2 get_namespaces
+
+=cut
 
 sub get_namespaces {
     my $self = shift;
