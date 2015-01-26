30cfcd7034a4d2e5e1f3d03a25645aa9d8a2f35e
James Rouzier <jrouzier@inverse.ca>
2015-01-26 10:59:00 -0500

    Copy parameters to allow caching of action

--- lib/pf/Authentication/Source.pm
+++ lib/pf/Authentication/Source.pm
@@ -134,6 +134,8 @@ sub match {
     my ($sec,$min,$hour,$mday,$mon,$year) = localtime(time);
     my $current_date = sprintf("%d-%02d-%02d", $year+1900, $mon+1, $mday);
     my $current_time = sprintf("%02d:%02d", $hour, $min);
+    # Make a copy of the keys to allow caching of the parameters
+    $params = {%$params};
     $params->{current_date} = $current_date;
     $params->{current_time} = $current_time;
 
