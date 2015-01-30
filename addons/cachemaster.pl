171422751ec92a1a0d8eaf1634c032940a12172e
Julien Semaan <jul.semaan@gmail.com>
2015-01-30 15:22:59 -0500

    Various changes :
    - Add object oriented namespaces that build the config overlay
    - Move the sockets to pf directory
    - Cleanup the code a little
    - Zicache is now object oriented (but still cached zicache)

--- addons/cachemaster.pl
+++ addons/cachemaster.pl
@@ -9,7 +9,7 @@ use Time::HiRes;
 use zicache::timeme;
 $zicache::timeme::VERBOSE = 1;
 
-my $socket_path = '/tmp/zicache';
+my $socket_path = '/usr/local/pf/var/run/config.sock';
 unlink($socket_path);
 
 my $listner = IO::Socket::UNIX->new(
@@ -19,39 +19,46 @@ my $listner = IO::Socket::UNIX->new(
 )
    or die("Can't create server socket: $!\n");
 
+my $cache = zicache::zicache->new;
 
 while(1) {
   my $socket = $listner->accept()
-   or die("Can't accept connection: $!\n");
-  chomp( my $line = <$socket> );
+     or die("Can't accept connection: $!\n");
+  eval {
+    chomp( my $line = <$socket> );
 
-  # we support hash namespaced queries
-  # where 
-  #  - line = 'config' return the whole config hash
-  #  - line = 'config-value' return the value in the config hash
-  my @keys = split ';', $line;
+    # we support hash namespaced queries
+    # where 
+    #  - line = 'config' return the whole config hash
+    #  - line = 'config;value' return the value in the config hash
+    my @keys = split ';', $line;
 
-  my $elem;
-  # let's get the top namespace element
-  zicache::timeme::timeme('get all config in cache', sub {
-    $elem = zicache::zicache::get_cache($keys[0]);
-  });
+    my $elem;
+    # let's get the top namespace element
+    zicache::timeme::timeme('get all config in cache', sub {
+      $elem = $cache->get_cache($keys[0]);
+    });
 
-  if($elem){
-    my $json_elem;
-    # if we want a subnamespace we handle it here
-    if($keys[1]){
-      my $sub_elem = $elem->{$keys[1]} || {};
-      $json_elem = encode_json($sub_elem);
+    if($elem){
+      my $json_elem;
+      # if we want a subnamespace we handle it here
+      if($keys[1]){
+        my $sub_elem = $elem->{$keys[1]} || {};
+        $json_elem = encode_json($sub_elem);
+      }
+      # we want the whole namespace
+      else {
+        $json_elem = encode_json($elem);
+      }
+      print $socket $json_elem;
     }
-    # we want the whole namespace
-    else {
-      $json_elem = encode_json($elem);
+    # sh*t happens
+    else{
+      print $socket encode_json({});
     }
-    print $socket $json_elem;
-  }
-  # sh*t happens
-  else{
-    print $socket encode_json({});
+  };
+  if($@){
+      print STDERR $@;
+      print $socket undef;
   }
 }
---
+++ lib/zicache/namespaces/Switch.pm
@@ -0,0 +1,69 @@
+package zicache::namespaces::Switch;
+
+use Config::IniFiles;
+
+my $file = "/usr/local/pf/conf/switches.conf";
+
+sub build {
+  tie %tmp_cfg, 'Config::IniFiles', ( -file => $file );
+
+  $tmp_cfg{'127.0.0.1'} = {
+      id                => '127.0.0.1',
+      type              => 'PacketFence',
+      mode              => 'production',
+      SNMPVersionTrap   => '1',
+      SNMPCommunityTrap => 'public'
+  };
+
+  foreach my $section_name (keys %tmp_cfg){
+    unless($section_name eq "default"){
+      foreach my $element_name (keys %{$tmp_cfg{default}}){
+        unless (exists $tmp_cfg{$section_name}{$element_name}){
+          $tmp_cfg{$section_name}{$element_name} = $tmp_cfg{default}{$element_name};
+        }
+      }
+    }
+  }
+
+
+  foreach my $switch ( values %tmp_cfg ) {
+
+      # transforming uplink and inlineTrigger to arrays
+      foreach my $key (qw(uplink inlineTrigger)) {
+          my $value = $switch->{$key} || "";
+          $switch->{$key} = [ split /\s*,\s*/, $value ];
+      }
+
+      # transforming vlans and roles to hashes
+      my %merged = ( Vlan => {}, Role => {}, AccessList => {} );
+      foreach my $key ( grep {/(Vlan|Role|AccessList)$/} keys %{$switch} ) {
+          next unless my $value = $switch->{$key};
+          if ( my ( $type_key, $type ) = ( $key =~ /^(.+)(Vlan|Role|AccessList)$/ ) ) {
+              $merged{$type}{$type_key} = $value;
+          }
+      }
+      $switch->{roles}        = $merged{Role};
+      $switch->{vlans}        = $merged{Vlan};
+      $switch->{access_lists} = $merged{AccessList};
+      $switch->{VoIPEnabled} = (
+          $switch->{VoIPEnabled} =~ /^\s*(y|yes|true|enabled|1)\s*$/i
+          ? 1
+          : 0
+      );
+      $switch->{mode} = lc( $switch->{mode} );
+      $switch->{'wsUser'} ||= $switch->{'htaccessUser'};
+      $switch->{'wsPwd'} ||= $switch->{'htaccessPwd'} || '';
+      foreach my $cli_default (qw(EnablePwd Pwd User)) {
+          $switch->{"cli${cli_default}"}
+            ||= $switch->{"telnet${cli_default}"};
+      }
+      foreach my $snmpDefault (
+          qw(communityRead communityTrap communityWrite version)) {
+          my $snmpkey = "SNMP" . ucfirst($snmpDefault);
+          $switch->{$snmpkey} ||= $switch->{$snmpDefault};
+      }
+  }
+
+  return %tmp_cfg;
+
+}
