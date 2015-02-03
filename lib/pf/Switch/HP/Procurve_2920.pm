4f69050a9b5b87989dcf02c43ad0fb74a107a17e
erSitzt <charlesdarw@gmail.com>
2015-02-03 11:56:10 +0100

    Changed indentation to 4 spaces Removed unnecessary function

--- lib/pf/Switch/HP/Procurve_2920.pm
+++ lib/pf/Switch/HP/Procurve_2920.pm
@@ -14,13 +14,14 @@ oriented interface to access SNMP enabled HP Procurve 2920 switches.
 VoIP not tested using MAC Authentication/802.1X
 
 =cut
+
 use strict;
 use warnings;
 use Log::Log4perl;
 use Net::SNMP;
 use base ('pf::Switch::HP');
 
-sub description { 'HP ProCurve 2920 Series' }
+sub description {'HP ProCurve 2920 Series'}
 
 # importing switch constants
 use pf::Switch::constants;
@@ -30,85 +31,85 @@ use pf::config;
 # CAPABILITIES
 # access technology supported
 sub supportsWiredMacAuth { return $TRUE; }
-sub supportsWiredDot1x { return $TRUE; }
-sub supportsLldp { return $TRUE; }
-sub supportsRadiusVoip { return $TRUE; }
+sub supportsWiredDot1x   { return $TRUE; }
+sub supportsLldp         { return $TRUE; }
+sub supportsRadiusVoip   { return $TRUE; }
+
 # inline capabilities
-sub inlineCapabilities { return ($MAC,$PORT); }
+sub inlineCapabilities { return ( $MAC, $PORT ); }
 
 =item getVoipVSA
 Get Voice over IP RADIUS Vendor Specific Attribute (VSA).
 =cut
+
 sub getVoipVsa {
-        my ($this) = @_;
-        my $logger = Log::Log4perl::get_logger(ref($this));
-        my $vlanid = sprintf("%03x\n",$this->getVlanByName('voice'));
-        my $hexvlan = hex("31000".$vlanid);
-        return ('Egress-VLANID' => $hexvlan,);
-}
-
-
-=item isVoIPEnabled
-Supports VoIP if enabled.
-=cut
-sub isVoIPEnabled {
-        my ($self) = @_;
-        return ( $self->{_VoIPEnabled} == 1 );
+    my ($this) = @_;
+    my $logger = Log::Log4perl::get_logger( ref($this) );
+    my $vlanid = sprintf( "%03x\n", $this->getVlanByName('voice') );
+    my $hexvlan = hex( "31000" . $vlanid );
+    return ( 'Egress-VLANID' => $hexvlan, );
 }
 
 =item getPhonesLLDPAtIfIndex
 Using SNMP and LLDP we determine if there is VoIP connected on the switch port
 =cut
+
 sub getPhonesLLDPAtIfIndex {
-        my ( $this, $ifIndex ) = @_;
-        my $logger = Log::Log4perl::get_logger( ref($this) );
-        my @phones;
-        if ( !$this->isVoIPEnabled() ) {
-                $logger->debug( "VoIP not enabled on switch "
-                        . $this->{_ip}
-                        . ". getPhonesLLDPAtIfIndex will return empty list." );
-                return @phones;
-        }
-        my $oid_lldpRemPortId = '1.0.8802.1.1.2.1.4.1.1.7';
-        my $oid_lldpRemChassisIdSubtype = '1.0.8802.1.1.2.1.4.1.1.4';
-        if ( !$this->connectRead() ) {
-                return @phones;
-        }
-        sleep(4);
-        $logger->trace(
-                "SNMP get_next_request for lldpRemSysDesc: $oid_lldpRemChassisIdSubtype");
-        my $result = $this->{_sessionRead}
-        ->get_table( -baseoid => $oid_lldpRemChassisIdSubtype );
-        foreach my $oid ( keys %{$result} ) {
-                if ( $oid =~ /^$oid_lldpRemChassisIdSubtype\.([0-9]+)\.([0-9]+)\.([0-9]+)$/ ) {
-                        if ( $ifIndex eq $2 ) {
-                                my $cache_lldpRemTimeMark = $1;
-                                my $cache_lldpRemLocalPortNum = $2;
-                                my $cache_lldpRemIndex = $3;
-                                if ( $result->{$oid} =~ /5/i ) {
-                                        $logger->trace(
-                                                "SNMP get_request for lldpRemPortId: $oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
-                                                );
-                                        my $MACresult = $this->{_sessionRead}->get_request(
-                                                -varbindlist => [
-                                                "$oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
-                                                ]
-                                                );
-                                        if ($MACresult
-                                                && (unpack('H*', $MACresult->{
-                                                        "$oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
-                                                })
-                                                =~ /^([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})/i
-                                                )
-                                                )
-                                        {
-                                                push @phones, lc("$1:$2:$3:$4:$5:$6");
-                                        }
-                                }
-                        }
-                }
-        }
+    my ( $this, $ifIndex ) = @_;
+    my $logger = Log::Log4perl::get_logger( ref($this) );
+    my @phones;
+    if ( !$this->isVoIPEnabled() ) {
+        $logger->debug( "VoIP not enabled on switch "
+                . $this->{_ip}
+                . ". getPhonesLLDPAtIfIndex will return empty list." );
         return @phones;
+    }
+    my $oid_lldpRemPortId           = '1.0.8802.1.1.2.1.4.1.1.7';
+    my $oid_lldpRemChassisIdSubtype = '1.0.8802.1.1.2.1.4.1.1.4';
+    if ( !$this->connectRead() ) {
+        return @phones;
+    }
+    $logger->trace(
+        "SNMP get_next_request for lldpRemSysDesc: $oid_lldpRemChassisIdSubtype"
+    );
+    my $result = $this->{_sessionRead}
+        ->get_table( -baseoid => $oid_lldpRemChassisIdSubtype );
+    foreach my $oid ( keys %{$result} ) {
+        if ( $oid
+            =~ /^$oid_lldpRemChassisIdSubtype\.([0-9]+)\.([0-9]+)\.([0-9]+)$/
+            )
+        {
+            if ( $ifIndex eq $2 ) {
+                my $cache_lldpRemTimeMark     = $1;
+                my $cache_lldpRemLocalPortNum = $2;
+                my $cache_lldpRemIndex        = $3;
+                if ( $result->{$oid} =~ /5/i ) {
+                    $logger->trace(
+                        "SNMP get_request for lldpRemPortId: $oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
+                    );
+                    my $MACresult = $this->{_sessionRead}->get_request(
+                        -varbindlist => [
+                            "$oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
+                        ]
+                    );
+                    if ($MACresult
+                        && (unpack(
+                                'H*',
+                                $MACresult->{
+                                    "$oid_lldpRemPortId.$cache_lldpRemTimeMark.$cache_lldpRemLocalPortNum.$cache_lldpRemIndex"
+                                }
+                            )
+                            =~ /^([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})([0-9A-Z]{2})/i
+                        )
+                        )
+                    {
+                        push @phones, lc("$1:$2:$3:$4:$5:$6");
+                    }
+                }
+            }
+        }
+    }
+    return @phones;
 }
 
 =head1 AUTHOR
@@ -117,7 +118,7 @@ Inverse inc. <info@inverse.ca>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2013 Inverse inc.
+Copyright (C) 2005-2015 Inverse inc.
 
 =head1 LICENSE
 
