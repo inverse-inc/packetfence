b303c317372e2bc2f133c94b3b28bb3f5c8e8e2e
Julien Semaan <jul.semaan@gmail.com>
2015-02-05 17:43:08 -0500

    Refactoring of pfconfig
    - Created first unit test (./t/pfconfig.t). YAY :D
      - Depends on the service running - that part should go in another test later
    - Added logging
    - Added init script (./pfconfig.init)
    - Refactored code

--- lib/pfconfig/manager.pm
+++ lib/pfconfig/manager.pm
@@ -1,5 +1,8 @@
 package pfconfig::manager;
 
+use strict;
+use warnings;
+
 #use Cache::BDB;
 use Cache::Memcached;
 use Config::IniFiles;
@@ -8,23 +11,33 @@ use Scalar::Util qw(refaddr reftype tainted blessed);
 use UNIVERSAL::require;
 use Data::Dumper;
 use pfconfig::backend::memcached;
+use pf::log;
 
 sub config_builder {
   my ($self, $namespace) = @_;
+  my $logger = get_logger;
 
-  my $type = "pfconfig::namespaces::$namespace";
-
-  # load the module to instantiate
-  if ( !(eval "$type->require()" ) ) {
-      print STDERR "Can not load namespace $namespace "
-          . "Read the following message for details: $@";
-  }
-
-  my $elem = $type->new($self);
+  my $elem = $self->get_namespace($namespace);
   my $tmp = $elem->build();
 
   return $tmp;
 };
+
+sub get_namespace {
+  my ($self, $name) = @_;
+  my $logger = get_logger;
+   my $type = "pfconfig::namespaces::$name";
+
+  # load the module to instantiate
+  if ( !(eval "$type->require()" ) ) {
+      $logger->error( "Can not load namespace $name "
+          . "Read the following message for details: $@" );
+  }
+
+  my $elem = $type->new($self); 
+
+  return $elem;
+}
 
 sub new {
   my ($class) = @_;
@@ -37,6 +50,7 @@ sub new {
 
 sub init_cache {
   my ($self) = @_;
+  my $logger = get_logger;
 
   $self->{cache} = pfconfig::backend::memcached->new;
 
@@ -48,63 +62,82 @@ sub init_cache {
 # send the signal that the raw memory is expired
 sub touch_cache {
   my ($self, $what) = @_;
+  my $logger = get_logger;
   $what =~ s/\//;/g;
-  open HANDLE, ">>/usr/local/pf/var/$what-control" or die "touch $filename: $!\n"; 
+  my $filename = "/usr/local/pf/var/$what-control";
+  open HANDLE, ">>$filename" or die "touch $filename: $!\n"; 
   close HANDLE;
   my $now = time;
-  utime $now, $now, "/usr/local/pf/var/$what-control";
+  utime $now, $now, "$filename";
 }
 
 # get a key in the cache
 sub get_cache {
   my ($self, $what) = @_;
-  print "Cache value : ".$cached."\n";
+  my $logger = get_logger;
   # we look in raw memory and make sure that it's not expired
   if(defined($self->{memory}->{$what}) && $self->is_valid($what)){
-    print "Getting from memory\n";
+    $logger->debug("Getting $what from memory");
     return $self->{memory}->{$what};
   }
   else {
     my $cached = $self->{cache}->get($what);
     # raw memory is expired but cache is not
     if($cached){
-      print "Getting from cache db\n";
+      $logger->debug("Getting $what from cache backend");
       $self->{memory}->{$what} = $cached;
       $self->{memorized_at}->{$what} = time; 
       return $cached;
     }
     # everything is expired. need to rebuild completely
     else {
-      print "loading from outside\n";
-      my $result = $self->config_builder($what);
-      print "in get cache".Dumper($result);
-      my $cache_w = $self->{cache}->set($what, $result, 864000) ;
-      print "Cache write gave : $cache_w \n";
-      $self->touch_cache($what);
-      $self->{memory}->{$what} = $result;
-      $self->{memorized_at}->{$what} = time; 
+      my $result = $self->cache_resource($what);
       return $result;
     }
   }
  
 }
 
+sub cache_resource {
+    my ($self, $what) = @_;
+    my $logger = get_logger;
+
+    $logger->debug("loading $what from outside");
+    my $result = $self->config_builder($what);
+    my $cache_w = $self->{cache}->set($what, $result, 864000) ;
+    $logger->trace("Cache write gave : $cache_w");
+    $self->touch_cache($what);
+    $self->{memory}->{$what} = $result;
+    $self->{memorized_at}->{$what} = time; 
+
+    return $result;
+
+}
+
 # helper to know if the raw memory cache is still valid
 sub is_valid {
   my ($self, $what) = @_;
+  my $logger = get_logger;
   my $control_file;
   ($control_file = $what) =~ s/\//;/g;
-  my $epoch_timestamp = (stat("var/".$control_file."-control"))[9];
-  print "ts : ".$epoch_timestamp."\n";
-  print "memorized ts : ".$self->{memorized_at}->{$what}."\n";
+  my $file_timestamp = (stat("var/".$control_file."-control"))[9];
+
+  unless(defined($file_timestamp)){
+    $logger->warn("Filesystem timestamp is not set for $what. Setting it as now and considering memory as invalid.");
+    $self->touch_cache($what);
+    return 0;
+  }
+
+  my $memory_timestamp = $self->{memorized_at}->{$what};
+  $logger->trace("Control file has timestamp $file_timestamp and memory has timestamp $memory_timestamp for key $what");
   # if the timestamp of the file is after the one we have in memory
   # then we are expired
-  if ($self->{memorized_at}->{$what} > $epoch_timestamp){
-    print "valid \n";
+  if ($memory_timestamp > $file_timestamp){
+    $logger->trace("Memory configuration is still valid for key $what");
     return 1;
   }
   else{
-    print "invalid \n";
+    $logger->info("Memory configuration is not valid anymore for key $what");
     return 0;
   }
 }
@@ -113,8 +146,18 @@ sub is_valid {
 # will expire the memory cache after building
 sub expire {
   my ($self, $what) = @_;
-  $self->{cache}->remove($what);
-  $self->get_cache($what);
+  my $logger = get_logger;
+  $logger->info("Expiring resource : $what");
+  $self->cache_resource($what);
+
+  my $namespace = $self->get_namespace($what);
+  if ($namespace->{child_resources}){
+    foreach my $child_resource (@{$namespace->{child_resources}}){
+      $logger->info("Expiring child resource $child_resource. Master resource is $what");
+      $self->expire($child_resource);
+    }
+  }
+
 }
 
 1;
--- lib/pfconfig/namespaces/config.pm
+++ lib/pfconfig/namespaces/config.pm
@@ -1,5 +1,8 @@
 package pfconfig::namespaces::config;
 
+use strict;
+use warnings;
+
 use Data::Dumper;
 
 use base 'pfconfig::namespaces::resource';
@@ -7,20 +10,14 @@ use base 'pfconfig::namespaces::resource';
 sub build {
   my ($self) = @_;
 
+  my %tmp_cfg;
+
   tie %tmp_cfg, 'Config::IniFiles', ( -file => $self->{file} );
 
   $self->{cfg} = \%tmp_cfg;
 
   my $child_resource = $self->build_child();
   return $child_resource;
-}
-
-sub init {
-  # abstact
-}
-
-sub build_child {
-  # abstract
 }
 
 1;
