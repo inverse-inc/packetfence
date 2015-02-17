cdd343a42554dd4cb483a85b9f9a7efff7456692
James Rouzier <jrouzier@inverse.ca>
2015-02-17 12:35:21 -0500

    Added the display of children

--- html/pfappserver/root/static/admin/config/fingerbank-devices.js
+++ html/pfappserver/root/static/admin/config/fingerbank-devices.js
@@ -25,6 +25,15 @@ FingerBankDevices.prototype.modalId   = '#modalFingerBankDevice';
 var FingerBankDeviceView = function(options) {
     ItemView.call(this,options);
     var that = this;
+    // Display sub children
+    var showChildren = $.proxy(this.showChildren, this);
+    options.parent.on('click', '#fingerbankdevices [href$="/children"]', showChildren);
+    options.parent.on('show hidden','.collapse',function(event) {
+        var that = $(this);
+        var tr = that.closest('tr').first();
+        tr.toggleClass('hidden');
+        event.stopPropagation(); //To stop the event from closing parents
+    });
 
 };
 
@@ -35,3 +44,28 @@ FingerBankDeviceView.prototype = (function(){
 })();
 
 FingerBankDeviceView.prototype.constructor = FingerBankDeviceView;
+
+FingerBankDeviceView.prototype.showChildren = function(e) {
+    var that = this;
+    var link = $(e.currentTarget);
+    var href = link.attr("href");
+    var target = link.attr("data-target");
+    console.log(target);
+    var children_div = $(target);
+    var row = children_div.closest("tr");
+    children_div.collapse({toggle: false});
+    if(row.hasClass('hidden')) {
+        this.items.get({
+            url: href,
+            success: function(data) {
+                children_div.find('table').html(data);
+                children_div.collapse('show');
+            },
+            errorSibling: $(that.id)
+        });
+    } else {
+        children_div.collapse('hide');
+    }
+
+    return false;
+};
