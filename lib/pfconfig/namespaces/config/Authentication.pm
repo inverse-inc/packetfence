e6d24d3fffee6abec6f3eac1e8aadac16ff3773e
Julien Semaan <jul.semaan@gmail.com>
2015-02-23 10:23:31 -0500

    ignore authentication resources

--- lib/pfconfig/namespaces/config/Authentication.pm
+++ lib/pfconfig/namespaces/config/Authentication.pm
@@ -1,16 +1,18 @@
-package pfconfig::namespaces::config::template;
+package pfconfig::namespaces::config::Authentication;
 
 =head1 NAME
 
-pfconfig::namespaces::config::template
+pfconfig::namespaces::config::Authentication
 
 =cut
 
 =head1 DESCRIPTION
 
-pfconfig::namespaces::config::template
+pfconfig::namespaces::config::Authentication
 
-This module creates the configuration hash associated to somefile.conf
+This module creates the configuration hash associated to authentication.conf
+
+It also stores the information for @authentication_sources and %authentication_lookup
 
 =cut
 
@@ -22,23 +24,106 @@ use pfconfig::namespaces::config;
 use Data::Dumper;
 use pfconfig::log;
 use pf::file_paths;
+use pf::constants::authentication;
+use pf::Authentication::constants;
+use pf::Authentication::Condition;
+use pf::Authentication::Rule;
+#use pf::authentication;
 
 use base 'pfconfig::namespaces::config';
 
 sub init {
   my ($self) = @_;
-  $self->{file} = "somefile.conf";
+  $self->{file} = $authentication_config_file;
+  $self->{child_resources} = [
+    'resource::authentication_lookup',
+    'resource::authentication_sources',
+  ];
 }
 
 sub build_child {
-  my ($self) = @_;
+    my ($self) = @_;
 
-  my %tmp_cfg = %{$self->{cfg}}; 
+    my %cfg = %{$self->{cfg}};
 
-  $self->{cfg} = \%tmp_cfg;
+    my @authentication_sources = ();
+    my %authentication_lookup = ();
+    foreach my $source_id ( keys %cfg ) {
 
-  return \%tmp_cfg;
+        # We skip groups from our ini files
+        if ($source_id =~ m/\s/) {
+          next;
+        }
 
+        # Keep aside the source type
+        my $type = $cfg{$source_id}{"type"};
+        delete $cfg{$source_id}{type};
+
+        # Instantiate the source object
+        my $current_source = $self->newAuthenticationSource($type, $source_id, $cfg{$source_id});
+
+        # Parse rules
+        foreach my $rule_id ( $self->GroupMembers($source_id) ) {
+
+            my ($id) = $rule_id =~ m/$source_id rule (\S+)$/;
+            my $current_rule = pf::Authentication::Rule->new({match => $Rules::ANY, id => $id});
+
+            foreach my $parameter ( keys %{$cfg{$rule_id}} ) {
+                if ($parameter =~ m/condition(\d+)/) {
+                    my ($attribute, $operator, $value) = split(',', $cfg{$rule_id}{$parameter}, 3);
+
+                    $current_rule->add_condition( pf::Authentication::Condition->new({attribute => $attribute,
+                                                                                      operator => $operator,
+                                                                                      value => $value}) );
+                } elsif ($parameter =~ m/action(\d+)/) {
+                    my ($type, $value) = split('=', $cfg{$rule_id}{$parameter}, 2);
+
+                    if (defined $value) {
+                        $current_rule->add_action( pf::Authentication::Action->new({type => $type,
+                                                                                    value => $value}) );
+                    } else {
+                        $current_rule->add_action( pf::Authentication::Action->new({type => $type}) );
+                    }
+
+                } elsif ($parameter =~ m/match/) {
+                    $current_rule->{'match'} = $cfg{$rule_id}{$parameter};
+                } elsif ($parameter =~ m/description/) {
+                    $current_rule->{'description'} = $cfg{$rule_id}{$parameter};
+                }
+            }
+
+            $current_source->add_rule($current_rule);
+        }
+        push(@authentication_sources, $current_source);
+        $authentication_lookup{$source_id} = $current_source;
+    }
+
+
+    my %resources;
+    $resources{authentication_sources} = \@authentication_sources;
+    $resources{authentication_lookup} = \%authentication_lookup;
+
+    return \%resources;
+
+}
+
+=item newAuthenticationSource
+
+Returns an instance of pf::Authentication::Source::* for the given type
+
+=cut
+
+sub newAuthenticationSource {
+    my ($self, $type, $source_id, $attrs) = @_;
+
+    my $source;
+    $type = lc($type);
+    if (exists $pf::constants::authentication::TYPE_TO_SOURCE{$type}) {
+        my $source_module = $TYPE_TO_SOURCE{$type};
+        $source = $source_module->new({ id => $source_id, %{$attrs} });
+    }
+
+    return $source;
 }
 
 =back
