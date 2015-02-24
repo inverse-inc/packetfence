1433b4ce797582774b19cb41f07e4090cf1f4a19
Louis Munro <lmunro@inverse.ca>
2015-02-24 13:31:16 -0500

    On the suggestion of fdurand we now return the vlan id if either the bypass vlan or bypass role is defined with precedence going to bypass_vlan.

--- lib/pf/vlan.pm
+++ lib/pf/vlan.pm
@@ -357,8 +357,8 @@ sub getNormalVlan {
         $logger->info("Can't find provisioner for $mac");
     }
 
-    ( $vlan, $role ) = _check_bypass($mac, $node_info);
-    return ( $vlan, $role ) if ( $vlan || $role );
+    $vlan = _check_bypass($mac, $node_info, $switch);
+    return $vlan if defined $vlan;
 
     $logger->debug("[$mac] Trying to determine VLAN from role.");
 
@@ -647,24 +647,25 @@ sub isInlineTrigger {
 }
 
 sub _check_bypass {
-    my ( $mac, $node_info ) = @_;
+    my ( $mac, $node_info, $switch ) = @_;
     my $logger = Log::Log4perl::get_logger( ref(__PACKAGE__) );
 
-    if ( not defined $mac or not defined $node_info ) {
-        return ( undef, undef );
-    }
+    if ( @_ < 3 ) { return undef; }
 
     # Bypass VLAN/role is configured in node record so we return accordingly
-    if (   ( defined( $node_info->{'bypass_vlan'} ) && $node_info->{'bypass_vlan'} ne '' )
-        or ( defined( $node_info->{'bypass_role'} ) && $node_info->{'bypass_role'} ne '' ) )
-    {
-        $logger->info( "[$mac] A bypass is configured. Returning (VLAN,role): "
-                . $node_info->{'bypass_vlan'} . ","
-                . $node_info->{'bypass_role'} );
-        return ( $node_info->{'bypass_vlan'}, $node_info->{'bypass_role'} );
+    if ( defined( $node_info->{'bypass_vlan'} ) && ( $node_info->{'bypass_vlan'} ne '' ) ) {
+        $logger->info( "[$mac] A bypass VLAN is configured. Returning VLAN: " . $node_info->{'bypass_vlan'} );
+        return $node_info->{'bypass_vlan'};
     }
-    return ( undef, undef );
+    elsif ( defined( $node_info->{'bypass_role'} ) && ( $node_info->{'bypass_role'} ne '' ) ) {
+        $logger->info( "[$mac] A bypass Role is configured. Returning Role: " . $node_info->{'bypass_role'} );
+        return $switch->getVlanByName( $node_info->{'bypass_role'} );
+    }
+    else {
+        return undef;
+    }
 }
+    
 
 =head1 AUTHOR
 
