45b5d484aace3011453aa6933fdc12fe4db66101
Julien Semaan <jul.semaan@gmail.com>
2015-02-27 10:18:03 -0500

    ameliorations to pfconfig cmd

--- addons/pfconfig/cmd.pl
+++ addons/pfconfig/cmd.pl
@@ -1,5 +1,35 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+Basic command line to interact with pfconfig
+
+=head1 SYNOPSIS
+
+cmd.pl reload|list|show|get
+
+=head1 DESCRIPTION
+
+cmd.pl gives basic commands to interact with pfconfig
+
+=item1 reload
+
+Reloads the configuration in pfconfig and sends the expiration signal
+
+=item1 list
+
+Lists the managed namespaces in pfconfig
+
+=item1 show
+
+Shows a namespace as viewed by pfconfig::manager
+
+=item1 get
+
+Shows a namespace as viewed by pfconfig::cached (does the get on the socket)
+
+=cut
+
 use lib '/usr/local/pf/lib';
 
 use Switch;
@@ -53,6 +83,44 @@ switch($cmd) {
   }
   else {
     print STDERR "ERROR ! Unknown command.\n";
+    print STDERR "Commands : \n";
+    print STDERR "reload|list|show <namespace>|get <namespace> \n";
     exit;
   }
 };
+
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
+
