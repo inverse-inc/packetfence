3a666fc08b4fc75a510c382a93322cd8d97746e4
James Rouzier <jrouzier@inverse.ca>
2015-03-12 10:46:01 -0400

    Added pf::worker::api and pf::worker::log

---
+++ lib/pf/worker/api.pm
@@ -0,0 +1,65 @@
+package pf::worker::api;
+=head1 NAME
+
+pf::worker::api
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::worker::api
+
+=cut
+
+use strict;
+use warnings;
+use base 'pf::worker';
+use pf::log;
+use threads;
+use pf::api;
+
+sub work {
+    my ($self) = @_;
+    my ($method,@args) = @$self;
+    my $logger = get_logger();
+    if(pf::api->isPublic($method)) {
+        eval {
+            pf::api->$method(@args);
+        };
+        if($@) {
+            $logger->error($@);
+        }
+    } else {
+        $logger->error("Invalid method given");
+    }
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
