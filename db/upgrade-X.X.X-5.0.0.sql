17766afcedc528d0adbcaba6315819ccdaeb8bf1
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-03-13 11:01:57 -0400

    No longer needed

--- db/upgrade-X.X.X-5.0.0.sql
+++
@@ -1,9 +0,0 @@
-
---
--- Add table to cache in MySQL
---
-CREATE TABLE keyed (
-  id VARCHAR(255),
-  value LONGBLOB,
-  PRIMARY KEY(id)
-) ENGINE=InnoDB;
--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -1,3 +1,13 @@
 --
 -- PacketFence SQL schema upgrade from X.X.X to X.Y.Z
 --
+
+--
+-- Add table to cache in MySQL
+--
+
+CREATE TABLE keyed (
+  id VARCHAR(255),
+  value LONGBLOB,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB;
