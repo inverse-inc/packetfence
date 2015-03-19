615a8c0ce0572cbd519b267131cf46eb594a488d
Julien Semaan <jul.semaan@gmail.com>
2015-03-19 17:32:32 -0400

    added missing info in configstores

--- lib/pf/ConfigStore/Switch.pm
+++ lib/pf/ConfigStore/Switch.pm
@@ -23,6 +23,8 @@ use pfconfig::manager;
 
 extends qw(pf::ConfigStore Exporter);
 
+sub configFile {$switches_config_file}
+
 sub pfconfigNamespace {'config::Switch'}
 
 our ( $switches_cached_config, %SwitchConfig );
