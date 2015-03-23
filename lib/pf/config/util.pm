2f02cb9be990429a8b2f731d78816ba2ab0fa1ec
Julien Semaan <jul.semaan@gmail.com>
2015-03-23 07:48:39 -0400

    fix pod doc + copyrights in some files

--- lib/pf/config/util.pm
+++ lib/pf/config/util.pm
@@ -1,6 +1,19 @@
 package pf::config::util;
 
-# config dependant utils
+
+=head1 NAME
+
+pf::util - module for generic functions and utilities used by all the
+modules.
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::util contains many functions and utilities used by the other different
+modules.
+
+=cut
 
 use strict;
 use warnings;
@@ -340,4 +353,39 @@ sub get_translatable_time {
    return;
 }
 
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
 1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
--- t/prepare-pfconfig.t
+++ t/prepare-pfconfig.t
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+prepare-pfconfig.t
+
+=cut
+
+=head1 DESCRIPTION
+
+Used to store the sample data into pfconfig before the tests instead of the
+data in the normal configuration directory
+
+=cut
+
+
+
 BEGIN {
     # log4perl init
     use constant INSTALL_DIR => '/usr/local/pf';
@@ -12,3 +27,39 @@ BEGIN {
 use pfconfig::manager;
 
 pfconfig::manager->new->expire_all;
+
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
+
