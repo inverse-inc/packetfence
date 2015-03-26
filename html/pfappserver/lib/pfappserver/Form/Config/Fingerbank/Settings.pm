6a3278d4f9f7a406eaadaf72e029769680a4de27
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-03-26 09:57:26 -0400

    Fingerbank Settings page

---
+++ html/pfappserver/lib/pfappserver/Form/Config/Fingerbank/Settings.pm
@@ -0,0 +1,84 @@
+package pfappserver::Form::Config::Fingerbank::Settings;
+
+=head1 NAME
+
+pfappserver::Form::Config::Fingerbank::Settings
+
+=head1 DESCRIPTION
+
+Form definition to modify Fingerbank configuration
+
+=cut
+
+use HTML::FormHandler::Moose;
+
+extends 'pfappserver::Base::Form';
+with 'pfappserver::Base::Form::Role::Help';
+
+has 'section' => ( is => 'ro' );
+
+sub field_list {
+    my ( $self ) = @_;
+
+    my $list = [];
+
+    my $config = fingerbank::Config::get_config;
+    my @sections = keys %$config;
+    push @$list, map{$_ => { id => $_, type => 'Compound' } } @sections;
+    foreach my $section ( keys %$config ) {
+        foreach my $parameter ( keys %{$config->{$section}} ) {
+            my $field = {
+                id => $section . "." . $parameter,
+                type => 'Text',
+            };
+            push ( @$list, $parameter => $field );
+        }
+    }
+
+    return $list;
+
+
+#    my $section = $self->section;
+#
+#    my @section_fields;
+#    my $config = fingerbank::Config::get_config($section);
+#    foreach my $field ( keys %$config ) {
+#        push @section_fields, $field;
+#    }
+#
+#    foreach my $name ( @section_fields ) {
+#        my $field = {
+#            id => $name,
+#            type => 'Text',
+#        };
+#        push ( @$list, $name => $field );
+#    }
+#
+#    return $list;
+}
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ html/pfappserver/lib/pfappserver/PacketFence/Controller/Config/Fingerbank/Settings.pm
@@ -0,0 +1,91 @@
+package pfappserver::PacketFence::Controller::Config::Fingerbank::Settings;
+
+=head1 NAME
+
+pfappserver::PacketFence::Controller::Config::Fingerbank::Settings
+
+=head1 DESCRIPTION
+
+Basic interaction with Fingerbank database.
+
+Customizations can be made using L<pfappserver::Controller::Config::Fingerbank::Settings>
+
+=cut
+
+use Moose;  # automatically turns on strict and warnings
+use namespace::autoclean;
+
+BEGIN { extends 'pfappserver::Base::Controller'; }
+
+=head2 check_for_api_key
+
+=cut
+
+sub check_for_api_key :Private {
+    my ( $self, $c ) = @_;
+    my $logger = pf::log::get_logger;
+
+    if ( !fingerbank::Config::is_api_key_configured ) {
+        $logger->warn("Fingerbank API key is not configured. Running with limited features");
+        my $status_msg = "It looks like Fingerbank API key is not configured. You may have forgot the onboard process. To fully beneficiate of Fingerbank, please proceed here: https://fingerbank.inverse.ca/onboard";
+        $c->stash->{template} = "config/fingerbank/settings/onboard.tt";
+        return 0;
+    }
+}
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) :AdminRole('FINGERBANK_READ') {
+    my ( $self, $c ) = @_;
+    my $logger = pf::log::get_logger;
+
+    $c->forward('check_for_api_key');
+
+    my $config = fingerbank::Config::get_config;
+
+    my $form = $c->form("Config::Fingerbank::Settings");
+    $form->process(init_object => $config);
+    $c->stash->{form} = $form;
+
+
+#    my $form;
+#    foreach my $section ( keys %$config ) {
+#        $c->stash->{section} = $section;
+#        $form .= $c->form("Config::Fingerbank::Settings", section => $section);
+#        $form->process(init_object => $config->{$section}); 
+#        $c->stash->{form} = $form;
+#    }
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
