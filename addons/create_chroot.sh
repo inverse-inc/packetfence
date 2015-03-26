4f22c4cf0544aeb29f86323e171270200afba6ba
Julien Semaan <jul.semaan@gmail.com>
2015-03-26 17:49:06 -0400

    move copy/paste style code to a loop

--- addons/create_chroot.sh
+++ addons/create_chroot.sh
@@ -6,29 +6,18 @@ if [ -z "$NS" ] || [ -z "$BASE" ]; then
     exit 1;
 fi
 
-[ -d $BASE/$NS ]                || mkdir $BASE/$NS
-[ -d $BASE/$NS/proc ]           || mkdir $BASE/$NS/proc
-[ -d $BASE/$NS/var ]            || mkdir $BASE/$NS/var
-[ -d $BASE/$NS/etc ]            || mkdir $BASE/$NS/etc
-[ -d $BASE/$NS/lib ]            || mkdir $BASE/$NS/lib
-[ -d $BASE/$NS/lib64 ]          || mkdir $BASE/$NS/lib64
-[ -d $BASE/$NS/usr ]            || mkdir $BASE/$NS/usr
-[ -d $BASE/$NS/sbin ]           || mkdir $BASE/$NS/sbin
-[ -d $BASE/$NS/bin ]            || mkdir $BASE/$NS/bin
-[ -d $BASE/$NS/var/cache ]      || mkdir $BASE/$NS/var/cache
-[ -d $BASE/$NS/sys ]            || mkdir $BASE/$NS/sys
-[ -d $BASE/$NS/var/lib/samba ]  || mkdir -p $BASE/$NS/var/lib/samba
-[ -d $BASE/$NS/dev ]            || mkdir $BASE/$NS/dev
+DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev)
+
+for dir in "${DIRS[@]}"; do
+  [ -d $BASE/$NS/$dir ]                || mkdir -p $BASE/$NS/$dir
+done
 
 cp -fr /var/cache/samba$NS $BASE/$NS/var/cache/
 
-mount -o bind /proc           $BASE/$NS/proc/
-mount -o bind /etc            $BASE/$NS/etc
-mount -o bind /lib            $BASE/$NS/lib
-mount -o bind /lib64          $BASE/$NS/lib64
-mount -o bind /bin            $BASE/$NS/bin
-mount -o bind /usr            $BASE/$NS/usr
-mount -o bind /sbin           $BASE/$NS/sbin
-mount -o bind /sys            $BASE/$NS/sys
-mount -o bind /var/lib/samba/ $BASE/$NS/var/lib/samba/
-mount -o bind /dev/           $BASE/$NS/dev
+DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev)
+
+for dir in "${DIRS[@]}"; do
+  mount -o bind /$dir           $BASE/$NS/$dir
+done
+
+
