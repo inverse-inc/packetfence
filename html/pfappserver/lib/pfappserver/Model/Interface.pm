d3c95c2af313b1a60a29f0c4f58289f81222c061
Julien Semaan <jul.semaan@gmail.com>
2015-03-27 13:24:03 -0400

    removed interfaces active/active config from administration interface

--- html/pfappserver/lib/pfappserver/Model/Interface.pm
+++ html/pfappserver/lib/pfappserver/Model/Interface.pm
@@ -244,11 +244,6 @@ sub get {
           $result->{"$interface"}->{'vlan'} = $vlan_id;
         }
         $result->{"$interface"}->{'vip'} = $config->{vip};
-        $result->{"$interface"}->{'active_active_enabled'} = $config->{active_active_enabled};
-        $result->{"$interface"}->{'active_active_ip'} = $config->{active_active_ip};
-        $result->{"$interface"}->{'active_active_members'} = $config->{active_active_members};
-        $result->{"$interface"}->{'active_active_dhcpd_master'} = $config->{active_active_dhcpd_master};
-        $result->{"$interface"}->{'active_active_mysql_master'} = $config->{active_active_mysql_master};
         if (($result->{"$interface"}->{'network'} = $networks_model->getNetworkAddress($interface_ref->{ipaddress}, $interface_ref->{netmask}))) {
             ($status, $return) = $networks_model->getRoutedNetworks($result->{"$interface"}->{'network'}, $interface_ref->{netmask});
             if (is_success($status)) {
@@ -628,16 +623,7 @@ sub _prepare_interface_for_pfconf {
         ip => $int_model->{'ipaddress'},
         mask => $int_model->{'netmask'},
         vip => $int_model->{'vip'},
-        active_active_enabled => $int_model->{'active_active_enabled'},
-        active_active_ip => $int_model->{'active_active_ip'},
-        active_active_members => $int_model->{'active_active_members'},
-        active_active_dhcpd_master => $int_model->{'active_active_dhcpd_master'},
-        active_active_mysql_master => $int_model->{'active_active_mysql_master'},
     };
-
-    if (isenabled($int_model->{'active_active_enabled'})) {
-        undef $int_config_ref->{'vip'};
-    }
 
     # logic to match our awkward relationship between pf.conf's type and
     # enforcement with networks.conf's type
