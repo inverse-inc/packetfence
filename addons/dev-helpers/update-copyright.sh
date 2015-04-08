484b8e3b1c488a456ecb41a53411798459e843e1
James Rouzier <jrouzier@inverse.ca>
2015-04-08 09:45:04 -0400

    Do not update yourself

--- addons/dev-helpers/update-copyright.sh
+++ addons/dev-helpers/update-copyright.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-perl -pi -e's/Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-4]).*Inverse/Copyright (C) 2005-2015 Inverse/' $(grep -Prl 'Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-4]).*Inverse' lib/ html/ addons/ sbin/ bin/ raddb/ ) 
+FILE=$0
+
+perl -pi -e's/Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-4]).*Inverse/Copyright (C) 2005-2015 Inverse/' $(grep --exclude="$FILE" -Prl 'Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-4]).*Inverse' lib/ html/ addons/ sbin/ bin/ raddb/ ) 
