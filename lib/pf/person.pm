c498c526ef94ea71b67a2b1df1a8b97f7c90726f
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-04-09 12:54:54 -0400

    'person' class now use Fingerbank data

--- lib/pf/person.pm
+++ lib/pf/person.pm
@@ -141,12 +141,8 @@ sub person_db_prepare {
             WHERE pid=? ]);
 
     $person_statements->{'person_nodes_sql'} = get_db_handle()->prepare(
-        qq[ SELECT mac, pid, regdate, unregdate, lastskip, status, user_agent, computername,
-                   IFNULL(os_class.description, ' ') as dhcp_fingerprint
+        qq[ SELECT mac, pid, regdate, unregdate, lastskip, status, user_agent, computername, device_class AS dhcp_fingerprint
             FROM node
-            LEFT JOIN dhcp_fingerprint ON node.dhcp_fingerprint = dhcp_fingerprint.fingerprint
-            LEFT JOIN os_mapping ON dhcp_fingerprint.os_id = os_mapping.os_type
-            LEFT JOIN os_class ON os_mapping.os_class = os_class.class_id
             WHERE pid = ? ]);
 
     $person_statements->{'person_violations_sql'} = get_db_handle()->prepare(
