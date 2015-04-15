8e0bdfaafb6a719c35759e50a86b2cf3b57b8149
James Rouzier <jrouzier@inverse.ca>
2015-04-15 10:44:07 -0400

    Return proper exit code

--- lib/pf/cmd/pf/reload/violations.pm
+++ lib/pf/cmd/pf/reload/violations.pm
@@ -13,15 +13,19 @@ pf::cmd::pf::reload::violations
 
 use strict;
 use warnings;
+use pf::constants::exit_code qw($EXIT_SUCCESS);
 use pf::services;
+use pf::violation_config;
+use pf::constants::exit_code qw($EXIT_SUCCESS);
 use pf::log;
 use base qw(pf::cmd);
 
 
 sub run {
-    pf::services::read_violations_conf();
+    pf::violation_config::loadViolationsIntoDb;
     get_logger()->info("Violation classes reloaded");
     print "Violation classes reloaded\n";
+    return $EXIT_SUCCESS;
 }
 
 =head1 AUTHOR
