47a7b87ac39d0deeb7e8e4ac6c20b483e3a4e544
Julien Semaan <jul.semaan@gmail.com>
2015-04-20 13:50:40 -0400

    Added doc for migrate.pl

--- addons/AD/migrate.pl
+++ addons/AD/migrate.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+migrate.pl
+
+=head1 DESCRIPTION
+
+Util to migrate existing OS Active Directory configuration into PacketFence and then bind to the domain.
+
+=cut
+
 use lib '/usr/local/pf/lib';
 
 BEGIN {
@@ -63,5 +73,33 @@ if($confirm eq 'y'){
 else{
   print "Please re-run the script again or configure the domain directly through the admin UI in 'Configuration->Domain'\n";
 }
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 
