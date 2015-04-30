a0c5d5f87e1a09cb16761cf80234b5c12fbc067c
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-04-30 11:54:04 -0400

    Should not be there

--- lib/pf/fingerbank.pm
+++ lib/pf/fingerbank.pm
@@ -27,7 +27,7 @@ use pf::CHI;
 use pf::log;
 use pf::node qw(node_modify);
 
-use constant FINGERBANK_CACHE_EXPIRE => 3;    # Expires cache entry after 300s (5 minutes)
+use constant FINGERBANK_CACHE_EXPIRE => 300;    # Expires cache entry after 300s (5 minutes)
 
 our @fingerbank_based_violation_triggers = ('Device', 'DHCP_Fingerprint', 'DHCP_Vendor', 'MAC_Vendor', 'User_Agent');
 
