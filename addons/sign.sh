1aa41156b7f805f461524219f789dda7d2ac72e8
James Rouzier <jrouzier@inverse.ca>
2015-05-08 15:36:46 -0400

    Remove space at end of line

--- addons/sign.sh
+++ addons/sign.sh
@@ -5,7 +5,7 @@ OUT_FILE=$2
 openssl smime \
 -sign \
 -signer /usr/local/pf/raddb/certs/eapca.pem \
--inkey /usr/local/pf/raddb/certs/eapca.key \ 
+-inkey /usr/local/pf/raddb/certs/eapca.key \
 -nodetach \
 -outform der \
 -in $IN_FILE \
