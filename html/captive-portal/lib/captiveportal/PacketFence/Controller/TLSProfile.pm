8346a006d94391d7f56f7bd1898ac5256a57d4da
James Rouzier <jrouzier@inverse.ca>
2015-05-11 16:30:17 -0400

    Do not used hard coded values

--- html/captive-portal/lib/captiveportal/PacketFence/Controller/TLSProfile.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Controller/TLSProfile.pm
@@ -12,6 +12,7 @@ use pf::authentication;
 use HTML::Entities;
 use MIME::Base64;
 use File::Slurp;
+use File::Basename;
 use pf::web;
 use Crypt::OpenSSL::X509;
 
@@ -210,19 +211,22 @@ sub export_fingerprint : Local {
     my ($self, $c) = @_;
     my $logger = $c->log;
     my $session = $c->session;
+    my $portalSession = $c->portalSession;
+    my $mac    = $portalSession->clientMac;
+    my $provisioner = $c->profile->findProvisioner($mac);
     my $stash = $c->stash;
     my $user_cache = $c->user_cache;
     my $pki_session = $user_cache->compute("pki_session", sub {});
-    my $capath = "/usr/local/pf/raddb/certs/TestEAP.pem";
-    my $svrpath = "/usr/local/pf/raddb/certs/svr.pem";
-    my $cacert = Crypt::OpenSSL::X509->new_from_file($capath);
+    
+    my $svrpath = $radius_svr_cert;
+    my $cacert = Crypt::OpenSSL::X509->new_from_string($provisioner->ca_cert);
     my $castr = $cacert->as_string();
     my $cafinger = $cacert->fingerprint_sha1();
     $c->session( fingerprint => $cafinger );
     my $svrcert = Crypt::OpenSSL::X509->new_from_file($svrpath);
     my $svrstr = $cacert->as_string();
-    my $cafile = basename($capath);
-    my $svrfile = basename($svrpath);
+    my $cafile = $cacert->subject;
+    my $svrfile = $svrcert->subject;
     $c->session( cacn => $cafile );
     $c->session( svrcn => $svrfile );
     $castr =~ s/-----END CERTIFICATE-----\n.*//smg;
