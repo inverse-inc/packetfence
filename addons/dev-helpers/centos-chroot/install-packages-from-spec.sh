ee8ae2c687028691648837f0f16083bca0bb7487
James Rouzier <jrouzier@inverse.ca>
2015-05-14 09:50:48 -0400

    Remove --skip-broken, added comments, and cleanup code

--- addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
+++ addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-PFDIR=/usr/local/pf
+#This is will attempt to install all the packages
+#from the spec file using just packetfence-devel repo
+#
 
-YUM="yum --disablerepo='*' --enablerepo=packetfence-devel -y --skip-broken"
+PFDIR=/usr/local/pf
+SPEC="$PFDIR/addons/packages/packetfence.spec"
+
+YUM="yum --disablerepo='*' --enablerepo=packetfence-devel -y"
 $YUM makecache
 echo installing the packetfence dependecies
 
 REPOQUERY="repoquery --queryformat=%{NAME} --disablerepo='*' --enablerepo=packetfence-devel -c /etc/yum.conf -C --pkgnarrow=all"
 
-rpm -q --requires --specfile $PFDIR/addons/packages/packetfence.spec | grep -v packetfence | perl -pi -e's/ +$//' | sort -u | xargs -d '\n' $REPOQUERY --whatprovides | sort -u | grep -v perl-LDAP | xargs $YUM install 
+rpm -q --requires --specfile $SPEC | grep -v packetfence \
+    | perl -pi -e's/ +$//' | sort -u \
+    | xargs -d '\n' $REPOQUERY --whatprovides \
+    | sort -u | grep -v perl-LDAP \
+    | xargs $YUM install
