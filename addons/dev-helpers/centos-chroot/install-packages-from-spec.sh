858f0773c98e345851ad6890917fe3118f5fc220
James Rouzier <jrouzier@inverse.ca>
2015-05-19 14:04:02 -0400

    Fix bash escaping and always use the yum cache when installing

--- addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
+++ addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
@@ -6,15 +6,16 @@
 
 PFDIR=/usr/local/pf
 SPEC="$PFDIR/addons/packages/packetfence.spec"
+REPO=packetfence-devel
 
-YUM="yum --disablerepo='*' --enablerepo=packetfence-devel -y"
+YUM="yum --disablerepo=* --enablerepo=$REPO -y"
 $YUM makecache
-echo installing the packetfence dependecies
+echo installing the packetfence dependencies from the $REPO repo
 
-REPOQUERY="repoquery --queryformat=%{NAME} --disablerepo=* --enablerepo=packetfence-devel -c /etc/yum.conf -C --pkgnarrow=all"
+REPOQUERY="repoquery --queryformat=%{NAME} --disablerepo=* --enablerepo=$REPO -c /etc/yum.conf -C --pkgnarrow=all"
 
 rpm -q --requires --specfile $SPEC | grep -v packetfence \
     | perl -pi -e's/ +$//' | sort -u \
     | xargs -d '\n' $REPOQUERY --whatprovides \
     | sort -u | grep -v perl-LDAP \
-    | xargs $YUM install
+    | xargs $YUM -C install
