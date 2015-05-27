e61431eef184ccd647c7091da7fd32046219c96d
Julien Semaan <jul.semaan@gmail.com>
2015-05-27 08:04:01 -0400

    keep only unique attributes

--- lib/pf/Authentication/Source/LDAPSource.pm
+++ lib/pf/Authentication/Source/LDAPSource.pm
@@ -19,6 +19,7 @@ use Net::LDAPS;
 use List::Util;
 use Net::LDAP::Util qw(escape_filter_value);
 use pf::config;
+use List::MoreUtils qw(uniq);
 
 use Moose;
 extends 'pf::Authentication::Source';
@@ -56,6 +57,7 @@ sub available_attributes {
   my $super_attributes = $self->SUPER::available_attributes;
   my @custom_attributes = @{$Config{advanced}->{custom_ldap_attributes}};
   my @attributes = (("uid", "cn", "department", "displayName", "distinguishedName", "givenName", "memberOf", "sn", "eduPersonPrimaryAffiliation", "mail", "postOfficeBox", "description", "groupMembership"), @custom_attributes);
+  @attributes = uniq @attributes;
   my @ldap_attributes = map { { value => $_, type => $Conditions::LDAP_ATTRIBUTE } } @attributes;
 
   # We check if our username attribute is present, if not we add it.
