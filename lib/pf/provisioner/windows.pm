b86571a6e10337dae6991d9b2ac857222b49a3c1
Julien Semaan <jul.semaan@gmail.com>
2015-06-05 15:08:23 -0400

    rename windows-profile.xml to pf-profile.xml

--- lib/pf/provisioner/windows.pm
+++ lib/pf/provisioner/windows.pm
@@ -33,7 +33,7 @@ The template to use for profile
 
 =cut
 
-has profile_template => (is => 'rw', default => sub { "windows-profile.xml" });
+has profile_template => (is => 'rw', default => sub { "pf-profile.xml" });
 
 =head1 AUTHOR
 
