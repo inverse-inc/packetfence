694f95793bd526a897e38af5eec101211bed6b97
James Rouzier <jrouzier@inverse.ca>
2015-06-11 16:01:49 -0400

    Remove print statement

--- lib/pf/ConfigStore/Switch.pm
+++ lib/pf/ConfigStore/Switch.pm
@@ -142,7 +142,6 @@ sub cleanupAfterRead {
     # Config::Inifiles expands the access lists into an array
     # We put it back as a string so it works in the admin UI
     foreach my $attr (keys %$switch){
-        print $attr;
         if($attr =~ /AccessList$/ && ref($switch->{$attr}) eq 'ARRAY'){
             $switch->{$attr} = join "\n", @{$switch->{$attr}};
         }
