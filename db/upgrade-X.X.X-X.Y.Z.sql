f63d3e03ee25f3d8af53b4477d76e042e22f7793
lzammit <lzammit@inverse.ca>
2015-06-12 15:56:05 -0400

    Update upgrade-X.X.X-X.Y.Z.sql
    Fix locationlog ALTER

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -6,11 +6,11 @@
 -- Alter locationlog
 --
 
-ALTER TABLE `locationlog` ADD `connection_sub_type` varchar(50) NOT NULL default '' AFTER connection_type;
+ALTER TABLE `locationlog` ADD `connection_sub_type` varchar(50) default NULL AFTER connection_type;
 
 --
 -- Alter locationlog_archive
 --
 
 ALTER TABLE `locationlog_archive`
-    ADD `connection_sub_type` varchar(50) NOT NULL default '' AFTER connection_type;
+    ADD `connection_sub_type` varchar(50) default NULL AFTER connection_type;
