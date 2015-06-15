513c0a003780ec45d4d6b3e1fe025cdbfa24413b
Julien Semaan <jul.semaan@gmail.com>
2015-06-15 08:04:12 -0400

    Update upgrade-X.X.X-X.Y.Z.sql

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -14,3 +14,25 @@ ALTER TABLE `locationlog` ADD `connection_sub_type` varchar(50) default NULL AFT
 
 ALTER TABLE `locationlog_archive`
     ADD `connection_sub_type` varchar(50) default NULL AFTER connection_type;
+
+--
+-- Setting the major/minor/sub-minor version of the DB
+--
+
+SET @MAJOR_VERSION = 5;
+SET @MINOR_VERSION = 2;
+SET @SUBMINOR_VERSION = 0;
+
+--
+-- The VERSION_INT to ensure proper ordering of the version in queries
+--
+
+SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
+
+--
+-- Updating to current version
+--
+
+INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
+
+
