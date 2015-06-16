fe320cf00823768c82348dff79b912a7679dbe95
James Rouzier <jrouzier@inverse.ca>
2015-06-15 23:07:11 -0400

    Ported mirapay_iframe to the mirapay billing source

--- lib/pf/Authentication/Source/MirapaySource.pm
+++ lib/pf/Authentication/Source/MirapaySource.pm
@@ -13,7 +13,10 @@ pf::Authentication::Source::MirapaySource
 
 use strict;
 use warnings;
+use Digest::SHA qw(sha256_hex);
+use URI::Escape::XS qw(uri_escape uri_unescape);
 use Moose;
+use pf::log;
 use pf::config qw($FALSE $TRUE $default_pid);
 use pf::Authentication::constants;
 use pf::util;
@@ -29,6 +32,107 @@ extends 'pf::Authentication::Source::BillingSource';
 has '+class' => (default => 'billing');
 
 has '+type' => (default => 'Mirapay');
+
+has base_url => (
+    is => 'rw',
+    default => "https://staging.eigendev.com/MiraSecure/GetToken.php",
+);
+
+has shared_secret => (
+    is => 'rw',
+    required => 1,
+);
+
+has merchant_id => (
+    is => 'rw',
+    required => 1,
+);
+
+=head2 prepare_payment
+
+Prepare the payment from mirapay
+
+=cut
+
+sub prepare_payment {
+    my ($self, $session, $tier, $params, $path) = @_;
+    my $hash = {
+        mirapay_url => $self->mirapay_url,
+    };
+    return $hash;
+}
+
+=head2 verify
+
+Verify the payment from mirapay
+
+=cut
+
+sub verify {
+    my ($self, $session, $parameters, $path) = @_;
+    return {};
+}
+
+=head2 cancel
+
+Not implemented
+
+=cut
+
+sub cancel {
+    my ($self, $session, $parameters, $path) = @_;
+    return {};
+}
+
+=head2 calc_mkey
+
+Calaulate the mkey from parameters given
+
+=cut
+
+sub calc_mkey {
+    my ($self, @params) = @_;
+    sha256_hex(@params, $self->shared_secret);
+}
+
+sub verify_mkey {
+    my ($self, $query) = @_;
+    my $logger = get_logger;
+    my @params;
+    for my $item (split ('&',$query)) {
+        my ($name,$value) = split ('=',$item);
+        push @params, uri_unescape($name),uri_unescape($value // '');
+    }
+    my $mkey = pop @params;
+    my $name = pop @params;
+    if ($name ne 'MKEY') {
+         $logger->error("Invalid query the last query parameter is not MKEY $query");
+         return 0;
+    }
+    my $test_key = $self->calc_mkey(@params);
+    return $test_key eq $mkey ;
+}
+
+=head2 mirapay_url
+
+=cut
+
+sub mirapay_url {
+    my ($self, $parameters, $tier) = @_;
+    my $url          = $self->base_url;
+    my $merchant_id  = $self->merchant_id;
+    my $redirect_url = $self->verify_url;
+    my @params       = (
+        MerchantID  => $merchant_id,
+        RedirectURL => $redirect_url,
+        EchoData    => $tier->{item},
+        Amount      => $tier->{price} * 100
+    );
+    my $mkey = $self->calc_mkey(@params);
+    my $query = join("&", pairmap {"$a=" . uri_escape($b)} @params, 'MKEY', $mkey);
+    return "$url?$query";
+}
+
 
 =head1 AUTHOR
 
