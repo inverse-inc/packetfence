e6f33d306750c6e89ea583e0675c3ff3d20ce5a3
Antoine Amacher <aamacher@inverse.ca>
2015-06-19 14:52:17 -0400

    added pfconfig command with pfcmd

--- lib/pf/cmd/pf/pfconfig.pm
+++ lib/pf/cmd/pf/pfconfig.pm
@@ -5,7 +5,7 @@ pf::cmd::pf::pfconfig add documentation
 
 =head1 SYNOPSIS
 
-pfcmd pfonfig reload|list|show|get
+pfcmd pfonfig expire|reload|list|show|get|clear_overlay
 
 =head1 DESCRIPTION
 
@@ -19,7 +19,11 @@ use pfconfig::manager;
 use pfconfig::util;
 use base qw(pf::base::cmd::action_cmd);
 
+=head2 new
 
+expire pfconfig's namespace
+
+=cut
 
 sub action_expire {
     my ($self) = @_;
@@ -32,6 +36,88 @@ sub action_expire {
 sub parse_expire {
     my ($self,@args) = @_;
     return @args == 1;
+}
+
+=head2 new
+
+reload pfconfig
+
+=cut
+
+sub action_reload {
+    my ($self) = @_;
+    my $manager = pfconfig::manager->new;
+    $manager->expire_all();
+    return 0;
+}
+
+=head2 new
+
+show from cache pfconfig's namespace
+
+=cut
+
+sub action_show {
+    my ($self) = @_;
+    my ($full_namespace) = $self->action_args;
+    my ($namespace, @args) = pfconfig::util::parse_namespace($full_namespace);
+    my $manager = pfconfig::manager->new;
+    if(defined($namespace)){
+        my @namespaces = $manager->list_namespaces();
+        if ( grep {$_ eq $namespace} @namespaces){
+            use Data::Dumper;
+            print Dumper($manager->get_cache($full_namespace));
+        }
+    }
+    return 0; 
+}
+
+=head2 new
+
+list all pfconfig's namespaces
+
+=cut
+
+sub action_list {
+    my ($self) = @_;
+    my $manager = pfconfig::manager->new;
+    my @namespaces = $manager->list_namespaces();
+    foreach my $namespace (@namespaces){
+        print "$namespace\n";
+    }
+    return 0;
+}
+
+=head2 new
+
+get from socket pfconfig's namespace
+
+=cut
+
+sub action_get {
+    my ($self) = @_;
+    my ($namespace) = $self->action_args;
+    if(defined($namespace)){
+        use pfconfig::cached;
+        use Data::Dumper;
+        my $obj = pfconfig::cached->new;
+        my $response = $obj->_get_from_socket($namespace, "element");
+        print Dumper($response);
+    }
+    return 0;
+}
+
+=head2 new
+
+clear_overlay of pfconfig
+
+=cut
+
+sub action_clear_overlay {
+    my ($self) = @_;
+    my $manager = pfconfig::manager->new;
+    $manager->clear_overlayed_namespaces(); 
+    return 0;
 }
 
 =head1 AUTHOR
@@ -62,4 +148,3 @@ USA.
 =cut
 
 1;
-
