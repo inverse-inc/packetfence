06281772c081219b541daa159ebda755bcc0105c
Julien Semaan <jul.semaan@gmail.com>
2015-06-26 11:03:05 -0400

    comment out code that breaks

--- lib/pf/web/constants.pm
+++ lib/pf/web/constants.pm
@@ -233,7 +233,9 @@ Return a regex that would match all the portal profile uri: filter
 =cut
 
 sub _clean_urls_match_filter {
-    return @uri_filters;
+    local $_;
+    # REMOVE ME !!!!!! TEMP FIX WHILE JAMES FIXES THIS
+    return ();#map { $_->value } grep { $_->isa('pf::profile::filter::uri') } @pf::config::Profile_Filters;
 }
 
 =item _clean_urls_match_mod_perl
