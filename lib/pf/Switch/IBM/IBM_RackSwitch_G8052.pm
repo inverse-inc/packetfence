46f8d055b69bc6849671eeabf57d73d83ae47925
Zammit Ludovic <lzammit@inverse.ca>
2015-06-30 16:23:41 -0400

    module and doc improvements

--- lib/pf/Switch/IBM/IBM_RackSwitch_G8052.pm
+++ lib/pf/Switch/IBM/IBM_RackSwitch_G8052.pm
@@ -4,7 +4,9 @@ package pf::Switch::IBM::IBM_RackSwitch_G8052;
 
 pf::Switch::HP::RackSwitch_G8052 - Object oriented module to access and configure IBM RackSwitch G8052 switches
 
-=head1 STATUS
+=head1 Firmware version
+
+It has been tested on version 7.9.11.0
 
 =head1 SUPPORTS
 
@@ -40,85 +42,6 @@ sub supportsRadiusDynamicVlanAssignment { return $TRUE; }
 
 =head1 SUBROUTINES
 
-=head2 radiusDisconnect
-
-Send a CoA to disconnect a mac
-
-=cut
-
-sub radiusDisconnect {
-    my ($self, $mac, $add_attributes_ref) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($self) );
-
-    # initialize
-    $add_attributes_ref = {} if (!defined($add_attributes_ref));
-
-    if (!defined($self->{'_radiusSecret'})) {
-        $logger->warn(
-            "[$mac] Unable to perform RADIUS CoA-Request on (".$self->{'_id'}."): RADIUS Shared Secret not configured"
-        );
-        return;
-    }
-
-    $logger->info("[$mac] deauthenticating");
-
-    # Where should we send the RADIUS CoA-Request?
-    # to network device by default
-    my $send_disconnect_to = $self->{'_ip'};
-    # but if controllerIp is set, we send there
-    if (defined($self->{'_controllerIp'}) && $self->{'_controllerIp'} ne '') {
-        $logger->info("[$mac] controllerIp is set, we will use controller $self->{_controllerIp} to perform deauth");
-        $send_disconnect_to = $self->{'_controllerIp'};
-    }
-    # allowing client code to override where we connect with NAS-IP-Address
-    $send_disconnect_to = $add_attributes_ref->{'NAS-IP-Address'}
-        if (defined($add_attributes_ref->{'NAS-IP-Address'}));
-
-    my $response;
-    try {
-        my $connection_info = {
-            nas_ip => $send_disconnect_to,
-            secret => $self->{'_radiusSecret'},
-            LocalAddr => $management_network->tag('vip'),
-        };
-
-        $logger->debug("[$mac] network device (".$self->{'_id'}.") supports roles. Evaluating role to be returned");
-        my $roleResolver = pf::roles::custom->instance();
-        my $role = $roleResolver->getRoleForNode($mac, $self);
-
-        my $node_info = node_attributes($mac);
-        # transforming MAC to the expected format 00-11-22-33-CA-FE
-        $mac = uc($mac);
-        $mac =~ s/:/-/g;
-
-        # Standard Attributes
-        my $attributes_ref = {
-            'Calling-Station-Id' => $mac,
-            'NAS-IP-Address' => $send_disconnect_to,
-        };
-
-        # merging additional attributes provided by caller to the standard attributes
-        $attributes_ref = { %$attributes_ref, %$add_attributes_ref };
-
-        $response = perform_coa($connection_info, $attributes_ref, [{ 'vendor' => 'Cisco', 'attribute' => 'Cisco-AVPair', 'value' => 'subscriber:command=reauthenticate' }]);
-
-    } catch {
-        chomp;
-        $logger->warn("[$mac] Unable to perform RADIUS CoA-Request on (".$self->{'_id'}."): $_");
-        $logger->error("[$mac] Wrong RADIUS secret or unreachable network device (".$self->{'_id'}.")...") if ($_ =~ /^Timeout/);
-    };
-    return if (!defined($response));
-
-    return $TRUE if ($response->{'Code'} eq 'CoA-ACK');
-
-    $logger->warn(
-        "Unable to perform RADIUS Disconnect-Request on (".$self->{'_id'}.")."
-        . ( defined($response->{'Code'}) ? " $response->{'Code'}" : 'no RADIUS code' ) . ' received'
-        . ( defined($response->{'Error-Cause'}) ? " with Error-Cause: $response->{'Error-Cause'}." : '' )
-    );
-    return;
-}
-
 =head2 wiredeauthTechniques
 
 Return the reference to the deauth technique or the default deauth technique.
@@ -132,17 +55,6 @@ sub wiredeauthTechniques {
         my $default = $SNMP::SNMP;
         my %tech = (
             $SNMP::SNMP => 'dot1xPortReauthenticate',
-        );
-
-        if (!defined($method) || !defined($tech{$method})) {
-            $method = $default;
-        }
-        return $method,$tech{$method};
-    }
-    if ($connection_type == $WIRED_MAC_AUTH) {
-        my $default = $SNMP::SNMP;
-        my %tech = (
-            $SNMP::SNMP => 'handleReAssignVlanTrapForWiredMacAuth',
         );
 
         if (!defined($method) || !defined($tech{$method})) {
@@ -174,36 +86,6 @@ sub returnRadiusAccessAccept {
             'Tunnel-Private-Group-ID' => $vlan,
         };
     }
-
-    
-    if ( isenabled($self->{_AccessListMap}) && $self->supportsAccessListBasedEnforcement ){
-        if( defined($user_role) && $user_role ne ""){
-            my $access_list = $self->getAccessListByName($user_role);
-            my @av_pairs;
-            while($access_list =~ /([^\n]+)\n?/g){
-                push(@av_pairs, $self->returnAccessListAttribute."=".$1);
-                $logger->info("[$mac] (".$self->{'_id'}.") Adding access list : $1 to the RADIUS reply");
-            } 
-            $radius_reply_ref->{'Cisco-AVPair'} = \@av_pairs; 
-            $logger->info("[$mac] (".$self->{'_id'}.") Added access lists to the RADIUS reply.");
-        }
-    }
-    if ( isenabled($self->{_RoleMap}) && $self->supportsRoleBasedEnforcement()) {
-        $logger->debug("[$mac] (".$self->{'_id'}.") Network device supports roles. Evaluating role to be returned");
-        if ( defined($user_role) && $user_role ne "" ) {
-            $role = $self->getRoleByName($user_role);
-        }
-        if ( defined($role) && $role ne "" ) {
-            $radius_reply_ref->{$self->returnRoleAttribute()} = $role;
-            $logger->info(
-                "[$mac] (".$self->{'_id'}.") Added role $role to the returned RADIUS Access-Accept under attribute " . $self->returnRoleAttribute()
-            );
-        }
-        else {
-            $logger->debug("[$mac] (".$self->{'_id'}.") Received undefined role. No Role added to RADIUS Access-Accept");
-        }
-    }
-
     $logger->info("[$mac] (".$self->{'_id'}.") Returning ACCEPT with VLAN $vlan and role $role");
     return [$RADIUS::RLM_MODULE_OK, %$radius_reply_ref];
 }
