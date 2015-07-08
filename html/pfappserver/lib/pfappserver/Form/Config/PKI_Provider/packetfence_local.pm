fda7652e103d6f2bc0ebaa486c0a0a8af3aaa745
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-07-08 19:53:17 -0400

    Add form for packetfence_local PKI creation

---
+++ html/pfappserver/lib/pfappserver/Form/Config/PKI_Provider/packetfence_local.pm
@@ -0,0 +1,101 @@
+package pfappserver::Form::Config::PKI_Provider::packetfence_local;
+
+=head1 NAME
+
+pfappserver::Form::Config::PKI_Provider::packetfence_local
+
+=cut
+
+use strict;
+use warnings;
+
+use HTML::FormHandler::Moose;
+
+extends 'pfappserver::Base::Form';
+with 'pfappserver::Base::Form::Role::Help';
+
+has_field 'id' => (
+    type     => 'Text',
+    label    => 'PKI Provider Name',
+    required => 1,
+    messages => { required => 'Please specify the name of the PKI provider' },
+    tags     => { 
+        after_element   => \&help,
+        help            => 'The unique id of the PKI provider',
+    },
+);
+
+has_field 'type' => (
+    type        => 'Hidden',
+    label       => 'PKI Provider type',
+    required    => 1,
+);
+
+has_field 'client_cert_path' => (
+    type         => 'Path',
+    required     => 1,
+    tags         => { 
+        after_element   => \&help,
+        help            => 'Path of the CA that will generate your certificates',
+    },
+);
+
+has_field 'ca_cert_path' => (
+    type        => 'Path',
+    required    => 1,
+    tags        => { 
+        after_element   => \&help,
+        help            => 'Path of the CA that will generate your certificates',
+    },
+);
+
+has_field 'cn_attribute' => (
+   type     => 'Select',
+   label    => 'Common name Attribute',
+   options  => [ { label => 'MAC address', value => 'mac' }, { label => 'Username', value => 'pid' } ],
+   default  => 'pid',
+   tags     => { 
+        after_element   => \&help,
+        help            => 'Defines what attribute of the node to use as the common name during the certificate generation.',
+    },
+);
+
+has_field 'server_cert_path' => (
+    type         => 'Path',
+    required     => 1,
+    tags         => { 
+        after_element   => \&help,
+        help            => 'Path of the RADIUS server authentication certificate',
+    },
+);
+
+has_block 'definition' => (
+    render_list => [ qw(type client_cert_path cn_attribute ca_cert_path server_cert_path) ],
+);
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
