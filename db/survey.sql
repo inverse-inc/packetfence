a5ca40c4bba5913efb29ff9b614c7d5a2de3e0e3
James Rouzier <jrouzier@inverse.ca>
2015-08-10 10:37:26 -0400

    Added additional fields

--- db/survey.sql
+++ db/survey.sql
@@ -4,5 +4,8 @@ CREATE TABLE survey (
   time_taken TIMESTAMP,
   survey_value int,
   email VARCHAR(255),
+  age VARCHAR(255),
+  gender VARCHAR(255),
+  origin VARCHAR(255),
    PRIMARY KEY (id)
 ) ENGINE=InnoDB;
