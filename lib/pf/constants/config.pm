47c68e2c5500593a4ada82f87b91889224242907
Julien Semaan <jul.semaan@gmail.com>
2015-08-21 14:51:55 -0400

    added auth source for kickbox.io

--- lib/pf/constants/config.pm
+++ lib/pf/constants/config.pm
@@ -42,6 +42,7 @@ our @EXPORT_OK = qw(
   $SELFREG_MODE_WIN_LIVE
   $SELFREG_MODE_TWITTER
   $SELFREG_MODE_NULL
+  $SELFREG_MODE_KICKBOX
   $SELFREG_MODE_CHAINED
   %NET_INLINE_TYPES
 );
@@ -75,6 +76,7 @@ Readonly our $SELFREG_MODE_LINKEDIN   => 'linkedin';
 Readonly our $SELFREG_MODE_WIN_LIVE   => 'windowslive';
 Readonly our $SELFREG_MODE_TWITTER   => 'twitter';
 Readonly our $SELFREG_MODE_NULL   => 'null';
+Readonly our $SELFREG_MODE_KICKBOX   => 'kickbox';
 Readonly our $SELFREG_MODE_CHAINED   => 'chained';
 
 Readonly our %NET_INLINE_TYPES =>  (
