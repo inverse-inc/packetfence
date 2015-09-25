2c8f67028b99e012c16c23188bbc56e80a4144c7
James Rouzier <jrouzier@inverse.ca>
2015-09-25 10:15:45 -0400

    Schema for bliing_transactions

--- db/billing-as-a-source.sql
+++ db/billing-as-a-source.sql
@@ -1,6 +1,20 @@
+DROP TABLE IF EXISTS billing_transaction;
 --
--- Alter billing
+-- Table structure for table `billing_transaction`
 --
 
-ALTER TABLE `billing` ADD `external_reference_id` varchar(255) default NULL;
+CREATE TABLE billing_transaction (
+  id int NOT NULL AUTO_INCREMENT,
+  ip varchar(255) NOT NULL,
+  mac varchar(17) NOT NULL,
+  source_id varchar(255) NOT NULL,
+  create_data datetime NOT NULL,
+  update_date timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  status varchar(255) NOT NULL,
+  tier_id varchar(255) DEFAULT NULL,
+  price varchar(255) DEFAULT NULL,
+  person varchar(255) NOT NULL,
+  external_transaction_id varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
