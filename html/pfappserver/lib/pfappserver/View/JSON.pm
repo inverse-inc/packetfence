8d9df99655351f8b520351dfcea3ddf1fad66338
James Rouzier <jrouzier@inverse.ca>
2015-09-29 14:21:59 -0400

    Fix safari not display message properly

--- html/pfappserver/lib/pfappserver/View/JSON.pm
+++ html/pfappserver/lib/pfappserver/View/JSON.pm
@@ -13,8 +13,20 @@ sub process {
     my ($self, $c) = @_;
     $self->SUPER::process($c);
     if( my $content_type = $c->stash->{json_view_content_type}) {
+        my $res = $c->res;
         my $encoding = $self->encoding || 'utf-8';
-        $c->res->content_type("$content_type; charset=$encoding");
+        $res->content_type("$content_type; charset=$encoding");
+        if($encoding eq 'utf-8' && $content_type eq 'text/plain') {
+            my $user_agent = $c->req->user_agent || '';
+            #Remove the utf-8 bom for safari
+            if ($user_agent =~ m/\bSafari\b/ and $user_agent !~ m/\bChrome\b/) {
+                use bytes;
+                my $output = $res->output();
+                $output =~ s/^(?:\357\273\277|\377\376\0\0|\0\0\376\377|\376\377|\377\376)//;
+                #$output =~ s/\x{FEFF}//;
+                $res->output($output);
+            }
+        }
     }
 };
 
