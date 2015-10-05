2300f6ae7817a1083d1a61aaad2b081568d0afea
Antoine Amacher <aamacher@inverse.ca>
2015-10-05 14:11:09 -0400

    typos in Iboss, fix mspki guide

--- html/pfappserver/lib/pfappserver/Form/Config/Firewall_SSO/Iboss.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Firewall_SSO/Iboss.pm
@@ -35,7 +35,7 @@ has_field 'password' =>
    required => 1,
    default => 'XS832CF2A',
    password => 0,
-   messages => { required => 'Change the default key is you have it' },
+   messages => { required => 'Change the default key if you have it' },
   );
 has_field 'port' =>
   (
