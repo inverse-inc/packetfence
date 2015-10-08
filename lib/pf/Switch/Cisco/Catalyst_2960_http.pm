09151ea2083b2f7e5eb7613383b128c3c5448c8e
Julien Semaan <jul.semaan@gmail.com>
2015-10-08 09:33:27 -0400

    started doc for IOS XE

--- lib/pf/Switch/Cisco/Catalyst_2960_http.pm
+++ lib/pf/Switch/Cisco/Catalyst_2960_http.pm
@@ -8,6 +8,8 @@ Catalyst 2960 with http redirect
 
 Developped and tested on IOS 15.0(2)SE5
 
+Also tested on IOS XE version 03.03.05SE
+
 =head1 SUPPORTS
 
 =head2 Deauthentication with RADIUS Disconnect (RFC3576)
