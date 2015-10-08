81fe07ce47e6926428077a96843b0f918157a18c
Julien Semaan <jul.semaan@gmail.com>
2015-10-08 08:26:53 -0400

    more doc

--- lib/pf/Portal/Profile.pm
+++ lib/pf/Portal/Profile.pm
@@ -127,6 +127,12 @@ sub getTemplatePath {
 
 *template_path = \&getTemplatePath;
 
+=item getBillingTiers
+
+Get the billing tiers for this portal profile
+
+=cut
+
 sub getBillingTiers {
     my ($self) = @_;
     my @tier_ids = split(/\s*,\s*/,$self->{_billing_tiers});
@@ -144,6 +150,12 @@ sub getBillingTiers {
 }
 
 *billing_tiers = \&getBillingTiers;
+
+=item getBillingTier
+
+Get the configuration of a specific billing tier
+
+=cut
 
 sub getBillingTier {
     my ($self, $id) = @_;
