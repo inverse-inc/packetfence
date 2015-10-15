048c77821732d8be7e21221eac24e47aca91399e
James Rouzier <jrouzier@inverse.ca>
2015-10-15 14:23:18 -0400

    Default to mac address if pid is not found

--- html/captive-portal/lib/captiveportal/PacketFence/Controller/Oauth2.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Controller/Oauth2.pm
@@ -247,7 +247,7 @@ sub oauth2Result : Path : Args(1) {
         }
 
         pf::auth_log::record_completed_oauth($provider, $c->portalSession->clientMac, $pid, $pf::auth_log::COMPLETED);
-        $c->session->{"username"} = $pid;
+        $c->session->{"username"} = $pid // $portalSession->clientMac;
         $c->session->{source_id} = $source->{id};
         $c->session->{source_match} = undef;
         $c->stash->{info}=\%info;
