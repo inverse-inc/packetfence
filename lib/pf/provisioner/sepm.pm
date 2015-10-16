761fb2fd3fe2f0ca8c0cb0dcbd66419c8dce6f8f
James Rouzier <jrouzier@inverse.ca>
2015-10-16 13:06:17 -0400

    When refreshing tokens for the opswat and sepm provisioners do not delete all the configuration value
    Fixes #960

--- lib/pf/provisioner/sepm.pm
+++ lib/pf/provisioner/sepm.pm
@@ -105,75 +105,75 @@ sub get_refresh_token {
     return $self->{'refresh_token'}
 }
 
-sub set_refresh_token {
-    my ($self, $refresh_token) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($self) );
-    if (!defined($refresh_token) || $refresh_token eq ''){
-        $logger->error("Called set_refresh_token but the refresh token is invalid");
-    } 
-    else{
-        $self->{'refresh_token'} = $refresh_token;
-        my $cs = pf::ConfigStore::Provisioning->new;
-        $logger->info($self->{'id'});
-        $cs->update($self->{'id'}, {refresh_token => $refresh_token});
-        $cs->commit();
-    }
-    
-}
-
 sub get_access_token {
     my ($self) = @_;
     my $logger = Log::Log4perl::get_logger( ref($self) );
-    return $self->{'access_token'};   
-}
-
-sub set_access_token {
-    my ($self, $access_token) = @_;
-    my $logger = Log::Log4perl::get_logger( ref($self) );
-    if (!defined($access_token) || $access_token eq ''){
-        $logger->error("Called set_access_token but the access token is invalid.");
-    }
-    else{
-        $self->{'access_token'} = $access_token;
-        my $cs = pf::ConfigStore::Provisioning->new;
-        $logger->info($self->{'id'});
-        $cs->update($self->{'id'}, {access_token => $access_token});
-        $cs->commit();
-    }
+    return $self->{'access_token'};
 }
 
 sub refresh_access_token {
     my ($self) = @_;
     my $logger = Log::Log4perl::get_logger( ref($self) );
-        
+
     my $refresh_token = $self->get_refresh_token();
     my $curl = WWW::Curl::Easy->new;
     my $url = $self->protocol."://".$self->host.":".$self->port."/sepm/oauth/token?grant_type=refresh_token&client_id=".$self->client_id."&client_secret=".$self->client_secret."&redirect_uri=https://".$self->host.":".$self->port."/sepm&refresh_token=$refresh_token";
-    
+
     my $response_body = '';
     open(my $fileb, ">", \$response_body);
     $curl->setopt(CURLOPT_URL, $url );
-    $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0) ; 
-    $curl->setopt(CURLOPT_HEADER, 0);   
+    $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0) ;
+    $curl->setopt(CURLOPT_HEADER, 0);
     $curl->setopt(CURLOPT_WRITEDATA,$fileb);
 
     my $curl_return_code = $curl->perform;
     my $curl_info = $curl->getinfo(CURLINFO_HTTP_CODE); # or CURLINFO_RESPONSE_CODE depending on libcurl version
 
-    if ( $curl_return_code != 0 or $curl_info != 200 ) { 
+    if ( $curl_return_code != 0 or $curl_info != 200 ) {
         # Failed to contact the SEPM.;
         $logger->error("Cannot connect to the SEPM to refresh the token");
-        return $pf::provisioner::COMMUNICATION_FAILED;   
+        return $pf::provisioner::COMMUNICATION_FAILED;
     }
-    else{
-        
+    else {
         my $json_response = decode_json($response_body);
+        my $updated_config = {};
         my $access_token = $json_response->{'access_token'};
         $refresh_token = $json_response->{'refresh_token'};
-        $self->set_access_token($access_token);
-        $self->set_refresh_token($refresh_token);
+        if (defined $access_token && $access_token ne '') {
+            $updated_config->{access_token} = $access_token;
+        }
+        else {
+            $logger->error("Cannot update the access token for $self->{id}");
+        }
+
+        if (defined $refresh_token && $refresh_token ne '') {
+            $updated_config->{refresh_token} = $refresh_token;
+        }
+        else {
+            $logger->error("Cannot update the refresh token for $self->{id}");
+        }
+        $self->update_config($updated_config);
         $logger->info("Refreshed the token to connect to the SEPM");
     }
+}
+
+=head2 update_config
+
+Update the config for this provisioner
+
+=cut
+
+sub update_config {
+    my ($self, $updated_config) = @_;
+    my $cs     = pf::ConfigStore::Provisioning->new;
+    my $config = $cs->read($self->{id});
+    unless ($config) {
+        get_logger->error("Error getting configuration for $self->{id}");
+        return;
+    }
+    %$config = (%$config, %$updated_config);
+    $cs->update($self->{'id'}, $config);
+    return $cs->commit();
 }
 
 sub validate_ip_in_sepm {
@@ -189,15 +189,15 @@ sub validate_ip_in_sepm {
         </Body>
     </Envelope>
     );
- 
+
     my $access_token = $self->get_access_token();
     my $curl = WWW::Curl::Easy->new;
     my $url = $self->protocol.'://' . $self->host . ':' .  $self->port . '/sepm/ws/v1/ClientService';
-    
+
     my $response_body = '';
     open(my $fileb, ">", \$response_body);
     $curl->setopt(CURLOPT_URL, $url );
-    $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0) ; 
+    $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0) ;
     $curl->setopt(CURLOPT_HEADER, 0);
     $curl->setopt(CURLOPT_POSTFIELDS, $postdata);
     $curl->setopt(CURLOPT_WRITEDATA,$fileb);
@@ -206,37 +206,37 @@ sub validate_ip_in_sepm {
     my $curl_return_code = $curl->perform;
     my $curl_info = $curl->getinfo(CURLINFO_HTTP_CODE); # or CURLINFO_RESPONSE_CODE depending on libcurl version
 
-    if ( $curl_info == 400 ) { 
+    if ( $curl_info == 400 ) {
         $logger->error("Unable to contact the SEPM on url ".$url);
-        return $pf::provisioner::COMMUNICATION_FAILED;   
+        return $pf::provisioner::COMMUNICATION_FAILED;
     }
     elsif($curl_info != 200){
         return $pf::provisioner::COMMUNICATION_FAILED;
-    } 
-    else { 
-        #check if ip address is there  
+    }
+    else {
+        #check if ip address is there
         #$logger->info($response_body);
         my $xml = XML::Simple->new;
         my $response = $xml->XMLin($response_body, KeyAttr=>{item=>'Body'});
-       
+
         if( $response->{'S:Body'}->{'ns2:getComputersByIPResponse'}->{'ns2:ComputerResult'}->{'totalNumberOfResults'} >= 1){
             $logger->info("IP $ip_to_search has been found in the SEPM.");
             return 1;
-            
+
         }
         else{
             $logger->info("IP $ip_to_search has not been found in the SEPM");
             return 0;
         }
-    } 
+    }
 }
 
 sub authorize {
     my ($self,$mac) = @_;
-    my $ip = pf::iplog::mac2ip($mac); 
+    my $ip = pf::iplog::mac2ip($mac);
     if(defined($ip)){
         my $logger = Log::Log4perl::get_logger( ref($self) );
-        my $result = $self->validate_ip_in_sepm($ip); 
+        my $result = $self->validate_ip_in_sepm($ip);
         if( $result == $pf::provisioner::COMMUNICATION_FAILED){
             $logger->info("SEPM Oauth access token is not valid anymore.");
             $self->refresh_access_token();
@@ -249,9 +249,9 @@ sub authorize {
         }
         else{
             return $result;
-        }   
+        }
     }
-       
+
 }
 
 =head1 AUTHOR
