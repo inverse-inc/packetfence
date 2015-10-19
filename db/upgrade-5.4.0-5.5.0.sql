d6d197abffabeec6b2d9749320dad7918f57e1ad
Louis Munro <lmunro@inverse.ca>
2015-10-19 07:52:49 -0400

    Post 5.4 release procedure.

---
+++ db/upgrade-5.4.0-5.5.0.sql
@@ -0,0 +1,22 @@
+--
+-- PacketFence SQL schema upgrade from 5.3.0 to 5.4.0
+--
+--
+-- Setting the major/minor/sub-minor version of the DB
+--
+
+SET @MAJOR_VERSION = 5;
+SET @MINOR_VERSION = 5;
+SET @SUBMINOR_VERSION = 0;
+
+--
+-- The VERSION_INT to ensure proper ordering of the version in queries
+--
+
+SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
+
+--
+-- Updating to current version
+--
+
+INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
