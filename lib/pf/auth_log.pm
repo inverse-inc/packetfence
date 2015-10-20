697359172fd48e65c0e1549081a9cd284763dcd9
Julien Semaan <jul.semaan@gmail.com>
2015-10-20 11:06:21 -0400

    quote constants

--- lib/pf/auth_log.pm
+++ lib/pf/auth_log.pm
@@ -43,7 +43,7 @@ sub auth_log_db_prepare {
 
     $auth_log_statements->{'auth_log_record_oauth_attempt_sql'} = get_db_handle()->prepare(qq[
         insert into auth_log (process_name,source,mac,attempted_at,status) 
-        VALUES(?, ?, ?, NOW(), $INCOMPLETE;
+        VALUES(?, ?, ?, NOW(), '$INCOMPLETE');
     ]);
 
     $auth_log_statements->{'auth_log_record_completed_oauth_sql'} = get_db_handle()->prepare(qq[
@@ -54,7 +54,7 @@ sub auth_log_db_prepare {
 
     $auth_log_statements->{'auth_log_record_guest_attempt_sql'} = get_db_handle()->prepare(qq[
         insert into auth_log (process_name,source,mac,pid,attempted_at,status) 
-        VALUES(?, ?, ?, ?, NOW(), $INCOMPLETE);
+        VALUES(?, ?, ?, ?, NOW(), '$INCOMPLETE');
     ]);
 
     $auth_log_statements->{'auth_log_record_completed_guest_sql'} = get_db_handle()->prepare(qq[
