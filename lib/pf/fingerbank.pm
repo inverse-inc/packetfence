0ef592910bf01bee6f624ec0b2d24faae2752482
Julien Semaan <jul.semaan@gmail.com>
2015-10-23 11:50:10 -0400

    compute fingerbank db values in a central point

--- lib/pf/fingerbank.pm
+++ lib/pf/fingerbank.pm
@@ -43,6 +43,17 @@ sub process {
     my ( $query_args ) = @_;
     my $logger = pf::log::get_logger;
 
+    if($query_args->{mac}){
+        my $node_info = pf::node::node_view($query_args->{mac});
+        next unless($node_info);
+        my @base_params = qw(dhcp_fingerprint dhcp_vendor dhcp6_fingerprint dhcp6_enterprise);
+        foreach my $param (@base_params){
+            $query_args->{$param} = $query_args->{$param} // $node_info->{$param};
+        }
+        # ip is a special case as it's not in the node_info
+        $query_args->{ip} = $query_args->{ip} // pf::iplog::mac2ip($query_args->{mac}) || undef;
+    }
+
     my $mac = $query_args->{'mac'};
 
     # Querying for a resultset
