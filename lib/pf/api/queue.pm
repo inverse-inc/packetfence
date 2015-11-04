5bb8462aea05ec4ebc5d688dcb45e237ac956a2e
James Rouzier <jrouzier@inverse.ca>
2015-11-04 11:18:53 -0500

    Move to pf::api::queue

---
+++ lib/pf/api/queue.pm
@@ -0,0 +1,90 @@
+package pf::api::queue;
+
+=head1 NAME
+
+pf::api::queue - The api queue notify
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::api::queue
+
+=cut
+
+use strict;
+use warnings;
+use Moo;
+use pf::client::queue;
+
+has queue => (
+    is      => 'rw',
+    required => 1,
+    default => 'general',
+);
+
+has client => (
+    is => 'rw',
+    builder => 1,
+    lazy => 1,
+);
+
+=head2 _build_client
+
+=cut
+
+sub _build_client {
+    my ($self) = @_;
+    return pf::client::queue->new;
+}
+
+=head2 call
+
+=cut
+
+sub call {
+    my ($self) = @_;
+    die "call not implemented\n";
+}
+
+=head2 notify
+
+calls the pf api ignoring the return value
+
+=cut
+
+sub notify {
+    my ($self, $method, @args) = @_;
+    $self->client->submit($self->queue, api => [$method, @args]);
+    return;
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
