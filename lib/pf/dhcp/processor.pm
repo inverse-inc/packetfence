a7a87edbc648783a3b0205c2e7b86df8b3dda146
Julien Semaan <jul.semaan@gmail.com>
2015-11-05 14:19:07 -0500

    poddoc for dhcp processor

--- lib/pf/dhcp/processor.pm
+++ lib/pf/dhcp/processor.pm
@@ -1,5 +1,17 @@
 package pf::dhcp::processor;
 
+=head1 NAME
+
+pf::dhcp::processor
+
+=cut
+
+=head1 DESCRIPTION
+
+Processes DHCP packets
+
+=cut
+
 use strict;
 use warnings;
 
@@ -477,4 +489,30 @@ sub get_local_dhcp_servers_by_mac {
     return @local_dhcp_servers_mac;
 }
 
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and::or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 1;
