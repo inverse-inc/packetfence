1aa79b26c2053c7d2d5e999916c50851fe479f5c
James Rouzier <jrouzier@inverse.ca>
2015-11-30 15:57:26 -0500

    Updated schema for audit log

--- db/radius_audit_log.sql
+++ db/radius_audit_log.sql
@@ -6,13 +6,25 @@ DROP TABLE IF EXISTS radius_audit_log;
 
 CREATE TABLE radius_audit_log (
   id int NOT NULL AUTO_INCREMENT,
+  created_at TIMESTAMP NOT NULL,
+  mac char(17) NULL,
+  user_name varchar(255) NULL,
+  status varchar(255) NULL,
+  auth_type varhcar(255) NULL,
+  source varchar(255) NULL,
+  role varchar(255) NULL,
+  status varchar(255) NULL,
+  unreg varchar(255) NULL,
+  ifindex varchar(255) NULL,
+  reason varchar(255) NULL,
+  nas_port varchar(255) NULL,
+  profile varchar(255) NULL,
+  event_type varchar(255) NULL,
+  uuid varchar(255) NULL,
   nas_ip_address varchar(255) NULL,
   nas_port_type varchar(255) NULL,
   called_station_id varchar(255) NULL,
   calling_station_id varchar(255) NULL,
-  module_failure_message varchar(255) NULL,
-  user_name varchar(255) NULL,
-  radius_request TEXT,
   radius_reply TEXT,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
