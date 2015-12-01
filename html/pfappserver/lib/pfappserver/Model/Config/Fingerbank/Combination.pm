421913b8266d7facd50688c36fa25f759dd32eb5
Julien Semaan <jul.semaan@gmail.com>
2015-12-01 15:07:06 -0500

    set combination attributes to empty if needed

--- html/pfappserver/lib/pfappserver/Model/Config/Fingerbank/Combination.pm
+++ html/pfappserver/lib/pfappserver/Model/Config/Fingerbank/Combination.pm
@@ -17,11 +17,46 @@ use Moose;
 use namespace::autoclean;
 use pf::config::cached;
 use HTTP::Status qw(:constants :is);
+use Readonly;
+
+Readonly our @EMPTYABLE_ATTRIBUTES => qw(dhcp_fingerprint_id dhcp_vendor_id dhcp6_fingerprint_id dhcp6_enterprise_id mac_vendor_id user_agent_id);
 
 extends 'pfappserver::Base::Model::Fingerbank';
 
 has '+fingerbankModel' => ( default => 'fingerbank::Model::Combination');
 
+
+sub set_empty_attributes {
+    my ($self, $assignments) = @_;
+    foreach my $key (@EMPTYABLE_ATTRIBUTES) {
+        $assignments->{$key} = $assignments->{$key} // '';
+    }
+    return $assignments;
+}
+
+=head2 create
+
+To create
+
+=cut
+
+sub create {
+    my ( $self, $id, $assignments ) = @_;
+    $assignments = $self->set_empty_attributes($assignments);
+    $self->SUPER::create($id, $assignments);
+}
+
+=head2 create
+
+To create
+
+=cut
+
+sub update {
+    my ( $self, $id, $assignments ) = @_;
+    $assignments = $self->set_empty_attributes($assignments);
+    $self->SUPER::update($id, $assignments);
+}
 
 __PACKAGE__->meta->make_immutable;
 
