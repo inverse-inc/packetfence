98e5a30327b46e55db9c650f59a868722516c834
Julien Semaan <jul.semaan@gmail.com>
2015-12-11 13:12:48 -0500

    doc

--- lib/pf/ConfigStore/Hierarchy.pm
+++ lib/pf/ConfigStore/Hierarchy.pm
@@ -1,14 +1,28 @@
 package pf::ConfigStore::Hierarchy;
 
+=head1 NAME
+
+pf::ConfigStore::Hierarchy
+
+=cut
+
+=head1 DESCRIPTION
+
+Allows to create a hierarchy between configuration sections
+
+=cut
+
 use Moo::Role;
 
 sub globalConfigStore { return $_[0] }
 
 sub topLevelGroup { return "default" }
 
+sub parentAttribute { return "group" }
+
 sub _inherit_from {
     my ($self, $switch) = @_;
-    my $group = $switch->{group} ? $self->_formatGroup($switch->{group}) : $self->topLevelGroup;
+    my $group = $switch->{$self->parentAttribute} ? $self->_formatGroup($switch->{group}) : $self->topLevelGroup;
     return $group;
 }
 
@@ -53,6 +67,31 @@ sub parentConfig {
     my $config = $self->parentConfigRaw($id);
     return $self->cleanupAfterRead($config);
 }
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2015 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 1;
 
