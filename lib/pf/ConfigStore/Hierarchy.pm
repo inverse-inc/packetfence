94e2cb84ae1ec06a29f08395138854fb67cd71fc
Julien Semaan <jul.semaan@gmail.com>
2015-12-14 08:49:47 -0500

    documentation for Hierarchy role

--- lib/pf/ConfigStore/Hierarchy.pm
+++ lib/pf/ConfigStore/Hierarchy.pm
@@ -14,27 +14,70 @@ Allows to create a hierarchy between configuration sections
 
 use Moo::Role;
 
+=head2 globalConfigStore
+
+Config store that contains both the groups and the childs
+
+=cut
+
 sub globalConfigStore { return $_[0] }
+
+=head2 topLevelGroup
+
+The top level group which cannot have any parent
+Behaves like default_section where all groups + childs inherit from this by default
+
+=cut
 
 sub topLevelGroup { return "default" }
 
+=head2 parentAttribute
+
+The configuration attribute that allows to find the parent of a section
+
+=cut
+
 sub parentAttribute { return "group" }
 
+=head2 _inherit_from
+
+Find the section id to inherit from
+
+=cut
+
 sub _inherit_from {
-    my ($self, $switch) = @_;
-    my $group = $switch->{$self->parentAttribute} ? $self->_formatGroup($switch->{group}) : $self->topLevelGroup;
+    my ($self, $config) = @_;
+    my $group = $config->{$self->parentAttribute} ? $self->_formatGroup($config->{group}) : $self->topLevelGroup;
     return $group;
 }
+
+=head2 _formatGroup
+
+Format the ID of a group into the ID it should be in the globalConfigStore
+
+=cut
 
 sub _formatGroup {
     my ($self, $group) = @_;
     return $group;
 }
 
+=head2 parentConfigRaw
+
+Get the configuration of the group the section is inheriting from
+
+=cut
+
 sub parentConfigRaw {
     my ($self, $id) = @_;
     return $self->fullConfigRaw($self->_inherit_from($self->read($id)));
 }
+
+=head2 fullConfigRaw
+
+Get the full config of a section (including inherited properties)
+
+=cut
 
 sub fullConfigRaw {
     my ($self, $id) = @_;
@@ -56,17 +99,35 @@ sub fullConfigRaw {
     }
 }
 
+=head2 fullConfig
+
+Like fullConfigRaw but with cleanupAfterRead executed after
+
+=cut
+
 sub fullConfig {
     my ($self, $id) = @_;
     my $config = $self->fullConfigRaw($id);
     return $self->cleanupAfterRead($config);
 }
 
+=head2 parentConfig
+
+Like parentConfigRaw but with cleanupAfterRead executed after
+
+=cut
+
 sub parentConfig {
     my ($self, $id) = @_;
     my $config = $self->parentConfigRaw($id);
     return $self->cleanupAfterRead($config);
 }
+
+=head2 members
+
+Find the members of a group by ID
+
+=cut
 
 sub members {
     my ($self, $id, $idKey) = @_;
