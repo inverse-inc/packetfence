e08576574d3d002a041bf09c99a0a4bf143478a4
James Rouzier <jrouzier@inverse.ca>
2015-12-17 13:08:16 -0500

    Added table creation to the upgrade sql scripts

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -941,6 +941,55 @@ CREATE TABLE keyed (
 CREATE TABLE pf_version ( `id` INT NOT NULL PRIMARY KEY, `version` VARCHAR(11) NOT NULL UNIQUE KEY);
 
 --
+-- Table structure for table 'radius_audit_log'
+--
+
+CREATE TABLE radius_audit_log (
+  id int NOT NULL AUTO_INCREMENT,
+  created_at TIMESTAMP NOT NULL,
+  mac char(17) NOT NULL,
+  ip varchar(255) NULL,
+  computer_name varchar(255) NULL,
+  user_name varchar(255) NULL,
+  stripped_user_name varchar(255) NULL,
+  realm varchar(255) NULL,
+  event_type varchar(255) NULL,
+  switch_id varchar(255) NULL,
+  switch_mac varchar(255) NULL,
+  switch_ip_address varchar(255) NULL,
+  radius_source_ip_address varchar(255),
+  called_station_id varchar(255) NULL,
+  calling_station_id varchar(255) NULL,
+  nas_port_type varchar(255) NULL,
+  ssid varchar(255) NULL,
+  nas_port_id varchar(255) NULL,
+  ifindex varchar(255) NULL,
+  nas_port varchar(255) NULL,
+  connection_type varchar(255) NULL,
+  nas_ip_address varchar(255) NULL,
+  nas_identifier varchar(255) NULL,
+  auth_status varchar(255) NULL,
+  reason TEXT NULL,
+  auth_type varchar(255) NULL,
+  eap_type varchar(255) NULL,
+  role varchar(255) NULL,
+  node_status varchar(255) NULL,
+  profile varchar(255) NULL,
+  source varchar(255) NULL,
+  auto_reg char(1) NULL,
+  is_phone char(1) NULL,
+  pf_domain varchar(255) NULL,
+  uuid varchar(255) NULL,
+  radius_request TEXT,
+  radius_reply TEXT,
+  PRIMARY KEY (id),
+  KEY `created_at` (created_at),
+  KEY `mac` (mac),
+  KEY `ip` (ip),
+  KEY `user_name` (user_name)
+) ENGINE=InnoDB;
+
+--
 -- Updating to current version
 --
 
