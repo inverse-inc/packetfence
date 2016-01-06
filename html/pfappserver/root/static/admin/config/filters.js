5f3984472c9ed1efcdf95cad707dd31ca8222094
Julien Semaan <jul.semaan@gmail.com>
2016-01-06 09:10:39 -0500

    fix identation

--- html/pfappserver/root/static/admin/config/filters.js
+++ html/pfappserver/root/static/admin/config/filters.js
@@ -1,16 +1,16 @@
 
 var filtersView;
 $(function() { // DOM ready
-    filtersView = new FiltersView({ parent: $('#section') });
+  filtersView = new FiltersView({ parent: $('#section') });
 });
 
 var FiltersView = function(options) {
-    var that = this;
-    this.parent = options.parent;
+  var that = this;
+  this.parent = options.parent;
 
-    // Save the modifications from the modal
-    var update = $.proxy(this.update, this);
-    options.parent.on('submit', '#filtersForm', update);
+  // Save the modifications from the modal
+  var update = $.proxy(this.update, this);
+  options.parent.on('submit', '#filtersForm', update);
 };
 
 FiltersView.prototype.setupEditor = function(){
@@ -20,21 +20,21 @@ FiltersView.prototype.setupEditor = function(){
 }
 
 FiltersView.prototype.update = function(e){
-    e.preventDefault();
+  e.preventDefault();
 
-    var jthis = $(e.target);
-    $.ajax({
-        url: jthis.attr("action"),
-        type: 'POST',
-        data: { content : this.editor.getValue() },
-    })
-    .done(function(data){
-      showSuccess(jthis, data.status_msg);
-    })
-    .fail(function(jqXHR) {
-        var status_msg = getStatusMsg(jqXHR);
-        showPermanentError(jthis, status_msg);
-    });
+  var jthis = $(e.target);
+  $.ajax({
+      url: jthis.attr("action"),
+      type: 'POST',
+      data: { content : this.editor.getValue() },
+  })
+  .done(function(data){
+    showSuccess(jthis, data.status_msg);
+  })
+  .fail(function(jqXHR) {
+      var status_msg = getStatusMsg(jqXHR);
+      showPermanentError(jthis, status_msg);
+  });
 
-    return false;
+  return false;
 }
