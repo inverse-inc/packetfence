06f9868e3093696c8080e9828d033a35d345abd8
Julien Semaan <jul.semaan@gmail.com>
2016-01-20 15:32:29 -0500

    more cleanup in enforcement

--- lib/pf/enforcement.pm
+++ lib/pf/enforcement.pm
@@ -47,9 +47,6 @@ use pf::config::util;
 use pf::role::custom $ROLE_API_LEVEL;
 use pf::client;
 use pf::cluster;
-use pf::firewallsso;
-use pf::constants::dhcp qw($DEFAULT_LEASE_LENGTH);
-use pf::iplog;
 
 use Readonly;
 
@@ -80,7 +77,6 @@ sub reevaluate_access {
 
     $logger->info("re-evaluating access ($function called)");
     $opts{'force'} = '1' if ($function eq 'admin_modify');
-
     my $locationlog_entry = locationlog_view_open_mac($mac);
     if ( !$locationlog_entry ) {
         $logger->warn("Can't re-evaluate access because no open locationlog entry was found");
