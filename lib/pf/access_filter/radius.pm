d9da2f7f78a8b0c710fe195f626d5f09430ee21d
Durand Fabrice <oeufdure@gmail.com>
2016-01-28 20:04:50 -0500

    Merge vsa attribute in radius filter

--- lib/pf/access_filter/radius.pm
+++ lib/pf/access_filter/radius.pm
@@ -79,6 +79,17 @@ sub handleAnswerInRule {
         if (defined($rule->{'merge_answer'}) && !(isenabled($rule->{'merge_answer'}))) {
             return ($radius_reply,$status);
         } else {
+            foreach my $key (keys %$radius_reply_ref) {
+                if (exists($radius_reply->{$key})) {
+                    if (defined(reftype($radius_reply->{$key})) && reftype($radius_reply->{$key}) eq 'ARRAY') {
+                        my @attribute;
+                        push(@attribute,@{$radius_reply_ref->{$key}});
+                        push(@attribute,@{$radius_reply->{$key}});
+                        $radius_reply_ref->{$key} = \@attribute;
+                        delete $radius_reply->{$key};
+                    }
+                }
+            }
             $radius_reply_ref = {%$radius_reply_ref, %$radius_reply} if (keys %$radius_reply);
             return ($radius_reply_ref,$status);
         }
@@ -86,6 +97,7 @@ sub handleAnswerInRule {
         return ($radius_reply_ref,$status);
     }
 }
+
 
 sub setSession {
     my($args) = @_;
