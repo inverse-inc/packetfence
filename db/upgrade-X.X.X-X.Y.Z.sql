e27f3e654fb74e310aa215b436b330d4bef1a791
Louis Munro <lmunro@inverse.ca>
2016-02-22 10:18:13 -0500

    Preparation for maintenance/5.7 branch.

---
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -0,0 +1,19 @@
+--
+-- Setting the major/minor/sub-minor version of the DB
+--
+
+-- SET @MAJOR_VERSION = 5;
+-- SET @MINOR_VERSION = 7;
+-- SET @SUBMINOR_VERSION = 0;
+
+--
+-- The VERSION_INT to ensure proper ordering of the version in queries
+--
+
+-- SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
+
+--
+-- Updating to current version
+--
+
+-- INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
