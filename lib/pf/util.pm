a95ff0612f1f88e4d893642ac32604341c0afd0c
Derek Wuelfrath <dwuelfrath@inverse.ca>
2016-02-25 16:37:02 -0500

    Removed double untaint

--- lib/pf/util.pm
+++ lib/pf/util.pm
@@ -773,11 +773,6 @@ sub pf_run {
     my ($command, %options) = @_;
     my $logger = get_logger();
 
-    # REVIEW AND DISCUSS THIS ! IS IT OK TO DO THIS ?
-    # IMO yes.
-    # Also this comment needs to be removed before the merge
-    $command = untaint_chain($command);
-
     local $OS_ERROR;
     # Using perl trickery to figure out what the caller expects so I can return him just that
     # this is to perfectly emulate the backtick operator behavior
