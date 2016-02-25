542c021b74953a4d57c9644d23e196c37778cdc7
Julien Semaan <jul.semaan@gmail.com>
2016-02-25 12:55:19 -0500

    validate a portal module is not used before deleting it

--- html/pfappserver/lib/pfappserver/Model/Config/PortalModule.pm
+++ html/pfappserver/lib/pfappserver/Model/Config/PortalModule.pm
@@ -16,11 +16,37 @@ use HTTP::Status qw(:constants is_error is_success);
 use Moose;
 use namespace::autoclean;
 use pf::ConfigStore::PortalModule;
+use List::MoreUtils qw(any);
 
 extends 'pfappserver::Base::Model::Config';
 
 
 sub _buildConfigStore { pf::ConfigStore::PortalModule->new }
+
+=head2 remove
+
+Override the parent method to validate we don't remove a portal module that is used
+
+=cut
+
+sub remove {
+    my ($self, $id) = @_;
+    pf::log::get_logger->info("Deleting $id");
+
+    my $modules = $self->configStore->readAll("id");
+    
+    foreach my $module (@$modules){
+        if($module->{modules}){
+            if(any { $_ eq $id } @{$module->{modules}}){
+                my $status_msg = ["Cannot remove group [_1] because it is still used by  : [_2]",$id, $module->{id}];
+                my $status =  HTTP_PRECONDITION_FAILED;
+                return ($status, $status_msg);
+            }
+        }
+    }
+
+    return $self->SUPER::remove($id);
+}
 
 __PACKAGE__->meta->make_immutable;
 
