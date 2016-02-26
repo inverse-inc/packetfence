9821454ad523f43963596aee2873f1352f4c37b3
Julien Semaan <jul.semaan@gmail.com>
2016-02-26 16:01:31 -0500

    add oauth forms

---
+++ html/pfappserver/lib/pfappserver/Form/Config/PortalModule/Authentication/OAuth/Twitter.pm
@@ -0,0 +1,49 @@
+package pfappserver::Form::Config::PortalModule::Authentication::OAuth::Twitter;
+
+=head1 NAME
+
+pfappserver::Form::Config::PortalModule::Authentcation::OAuth::Twitter
+
+=head1 DESCRIPTION
+
+Form definition to create or update an authentication portal module.
+
+=cut
+
+use HTML::FormHandler::Moose;
+extends 'pfappserver::Form::Config::PortalModule::Authentication::OAuth';
+
+use captiveportal::DynamicRouting::Module::Authentication::OAuth::Twitter;
+sub for_module {'captiveportal::DynamicRouting::Module::Authentication::OAuth::Twitter'}
+
+## Definition
+
+=over
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
