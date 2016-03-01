340528c8665b2af177d3dbce3ddf54f6296ffcb3
Louis Munro <lmunro@inverse.ca>
2016-03-01 11:40:46 -0500

    Renamed radsniff3 to radsniff.

--- lib/pf/services/manager/radsniff.pm
+++ lib/pf/services/manager/radsniff.pm
@@ -1,4 +1,4 @@
-package pf::services::manager::radsniff3;
+package pf::services::manager::radsniff;
 
 =head1 NAME
 
@@ -22,7 +22,7 @@ use pf::cluster;
 
 extends 'pf::services::manager';
 
-has '+name' => ( default => sub {'radsniff3'} );
+has '+name' => ( default => sub {'radsniff'} );
 has '+optional' => ( default => sub {1} );
 
 has '+launcher' => (
@@ -30,10 +30,10 @@ has '+launcher' => (
         if($cluster_enabled){
           my $cluster_management_ip = pf::cluster::management_cluster_ip();
           my $management_ip = pf::cluster::current_server()->{management_ip};
-          "sudo %1\$s -d $install_dir/raddb/ -D $install_dir/raddb/ -q -P $install_dir/var/run/radsniff3.pid -W10 -O $install_dir/var/run/collectd-unixsock -f '(host $management_ip and udp port 1812 or 1813)'";
+          "sudo %1\$s -d $install_dir/raddb/ -D $install_dir/raddb/ -q -P $install_dir/var/run/radsniff.pid -W10 -O $install_dir/var/run/collectd-unixsock -f '(host $management_ip and udp port 1812 or 1813)'";
         }
         else {
-          "sudo %1\$s -d $install_dir/raddb/ -D $install_dir/raddb/ -q -P $install_dir/var/run/radsniff3.pid -W10 -O $install_dir/var/run/collectd-unixsock -i $management_network->{Tint}";
+          "sudo %1\$s -d $install_dir/raddb/ -D $install_dir/raddb/ -q -P $install_dir/var/run/radsniff.pid -W10 -O $install_dir/var/run/collectd-unixsock -i $management_network->{Tint}";
         }
     }
 );
