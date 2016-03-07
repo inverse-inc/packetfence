f7b26072f3561688765d1a22f5a2c3bb1e32a174
Julien Semaan <jul.semaan@gmail.com>
2016-03-07 08:16:52 -0500

    perldoc for parking module

--- lib/pf/parking.pm
+++ lib/pf/parking.pm
@@ -1,5 +1,17 @@
 package pf::parking;
 
+=head1 NAME
+
+pf::parking - module to manage parked device.
+
+=cut
+
+=head1 DESCRIPTION
+
+Contains the necessary methods to manage parked devices
+
+=cut
+
 use strict;
 use warnings;
 
@@ -53,5 +65,34 @@ sub remove_parking_actions {
 
     pf_run("LANG=C sudo ipset del $PARKING_IPSET_NAME $ip -exist 2>&1");
 }
+
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
+=cut
 
 1;
