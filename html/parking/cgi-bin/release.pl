a7c481076554e1fb56911e9c19786934266035e6
Julien Semaan <jul.semaan@gmail.com>
2016-03-07 15:13:17 -0500

    perldoc for release script

--- html/parking/cgi-bin/release.pl
+++ html/parking/cgi-bin/release.pl
@@ -1,4 +1,18 @@
 #!/usr/bin/perl
+
+=head1 NAME
+
+release.pl
+
+=cut
+
+=head1 DESCRIPTION
+
+Tries to release the user from the parking state.
+Notifies of the result through back-on-network.html and max-attempts.html
+
+=cut
+
 use strict;
 use warnings;
 use lib '/usr/local/pf/lib';
@@ -17,4 +31,34 @@ else {
     print redirect("/max-attempts.html");
 }
 
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
+=cut
+
+1;
  
