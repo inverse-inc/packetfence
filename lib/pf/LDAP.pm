7f1dc903553a483206693c5065f94bd4cf482ea6
James Rouzier <jrouzier@inverse.ca>
2016-03-14 10:52:58 -0400

    New module for creating LDAP connections

---
+++ lib/pf/LDAP.pm
@@ -0,0 +1,86 @@
+package pf::LDAP;
+
+=head1 NAME
+
+pf::LDAP - Cache LDAP connections
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::LDAP
+
+=cut
+
+use strict;
+use warnings;
+
+use CHI;
+use Net::LDAP;
+use Net::LDAPS;
+# available encryption
+use constant {
+    NONE => "none",
+    SSL => "ssl",
+    TLS => "starttls",
+};
+
+our $CHI_CACHE = CHI->new(driver => 'RawMemory', datastore => {});
+
+sub new {
+    my ($class, @args) = @_;
+    return $CHI_CACHE->compute(\@args, { expire_if => sub { $class->expire_if(@_) } }, sub { $class->compute_connection(@args) });
+}
+
+sub expire_if {
+    my ($self, $object, $driver) = @_;
+    my $ldap = $object->value;
+    my $msg = $ldap->unbind;
+    return !$msg->is_error;
+}
+
+sub compute_connection {
+    my ($class, $server, %args) = @_;
+    my $encryption = delete $args{encryption};
+    my $connection;
+    if ( $encryption eq SSL ) {
+        $connection = Net::LDAPS->new($server, %args);
+    } else {
+        $connection = Net::LDAP->new($server, %args);
+    }
+    return $connection;
+}
+
+sub CLONE {
+    $CHI_CACHE->clear;
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
