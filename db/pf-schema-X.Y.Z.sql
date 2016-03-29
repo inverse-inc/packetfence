be7e02fc83bb289b945c6ed099498f502812fcc5
Julien Semaan <jul.semaan@gmail.com>
2016-03-29 08:28:59 -0400

    Add login_remaining to schema

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -369,6 +369,7 @@ CREATE TABLE `password` (
   `category` int DEFAULT NULL,
   `sponsor` tinyint(1) NOT NULL default 0,
   `unregdate` datetime NOT NULL default "0000-00-00 00:00:00",
+  `login_remaining` int DEFAULT NULL,
   PRIMARY KEY (pid)
 ) ENGINE=InnoDB;
 
