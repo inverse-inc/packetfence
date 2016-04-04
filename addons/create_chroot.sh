ad134602ac835dd0d107b611f26ea949a4af9435
Durand Fabrice <fdurand@inverse.ca>
2016-04-04 16:36:21 -0400

    Added directories in chroot for samba 4 and centos 7

--- addons/create_chroot.sh
+++ addons/create_chroot.sh
@@ -7,7 +7,7 @@ if [ -z "$NS" ] || [ -z "$BASE" ]; then
     exit 1;
 fi
 
-DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp)
+DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp run/samba var/log/samba)
 
 for dir in "${DIRS[@]}"; do
   [ -d $BASE/$NS/$dir ]                || mkdir -p $BASE/$NS/$dir
@@ -15,7 +15,7 @@ done
 
 cp -fr /var/cache/samba$NS $BASE/$NS/var/cache/
 
-DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev)
+DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev var/log/samba)
 
 for dir in "${DIRS[@]}"; do
   mount -o bind /$dir           $BASE/$NS/$dir
