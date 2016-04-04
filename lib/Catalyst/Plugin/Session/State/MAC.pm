370fb3aa747962a9171bf1c5adf6e2e41f573d15
Julien Semaan <jul.semaan@gmail.com>
2016-04-04 17:31:51 -0400

    change method signature in MAC state

--- lib/Catalyst/Plugin/Session/State/MAC.pm
+++ lib/Catalyst/Plugin/Session/State/MAC.pm
@@ -20,8 +20,7 @@ extends 'Catalyst::Plugin::Session::State::Cookie';
 
 our $VERSION = "0.01";
 
-around 'get_session_id' => sub {
-    my $orig = shift;
+sub get_session_id {
     my $c = shift;
 
     my $mac = $c->portalSession->clientMac;
@@ -37,8 +36,8 @@ around 'get_session_id' => sub {
     }
 };
 
-around 'update_session_cookie' => sub {
-    my ( $orig, $c, $updated ) = @_;
+sub update_session_cookie {
+    my ( $c, $updated ) = @_;
 
     unless ( $c->cookie_is_rejecting( $updated ) ) {
         my $cookie_name = $c->_session_plugin_config->{cookie_name};
@@ -67,5 +66,39 @@ sub browser_session_id {
     }
 }
 
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+Copyright (C) 2005 Kevin Amorin
+
+Copyright (C) 2005 David LaPorte
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 1;
