fc3f0667c519158aac4a66ebf92bd5ba23a10da0
Julien Semaan <jul.semaan@gmail.com>
2016-04-08 10:01:19 -0400

    add global caching to rate-limit fingerbank queries with same params

---
+++ lib/pf/constants/fingerbank.pm
@@ -0,0 +1,59 @@
+package pf::constants::fingerbank;
+
+=head1 NAME
+
+pf::constants::fingerbank - constants for fingerbank
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::constants::fingerbank
+
+=cut
+
+use strict;
+use warnings;
+use base qw(Exporter);
+use Readonly;
+
+our @EXPORT_OK = qw($RATE_LIMIT);
+
+=item $RATE_LIMIT
+
+The amount of seconds between each fingerbank process based on the query parameters
+
+=cut
+
+Readonly our $RATE_LIMIT => 300;
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and::or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+
