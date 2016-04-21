e2b18b98394a30bc575635107244974bd8880888
Antoine Amacher <aamacher@inverse.ca>
2016-04-21 12:48:28 -0400

    docs fixing

--- lib/pf/Switch/Meraki/AP_http_V2.pm
+++ lib/pf/Switch/Meraki/AP_http_V2.pm
@@ -15,6 +15,23 @@ Developed and tested on a MR12 access point
 
 =head1 BUGS AND LIMITATIONS
 
+In the current BETA version, VLAN assignment is broken in Mac Authentication Bypass.
+You can work around this by using the following RADIUS filter (conf/radius_filters.conf)
+
+    [your_ssid]
+    filter = ssid
+    operator = is
+    value = Meraki-Mac-Auth-SSID
+
+    [open_ssid_meraki_hack:your_ssid]
+    scope = returnRadiusAccessAccept
+    merge_answer = no
+    answer1 = Airespace-ACL-Name => VLAN$vlan
+
+Then creating a policy named VLANXYZ where XYZ is the VLAN ID you want to assign.
+
+Using this, you will be able to configure the VLAN ids in PacketFence and simply disable the RADIUS filter when the issue is fixed on the Meraki controller. 
+
 =cut
 
 use strict;
