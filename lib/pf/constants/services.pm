bc98bd096598553049a1ee5d9ffb0650bd752915
Julien Semaan <jul.semaan@gmail.com>
2016-04-22 15:21:31 -0400

    add api method to get status of the services

---
+++ lib/pf/constants/services.pm
@@ -0,0 +1,57 @@
+package pf::constants::services;
+
+=head1 NAME
+
+pf::constants::services
+
+=head1 DESCRIPTION
+
+Constants for the services
+
+=cut
+
+
+use base qw(Exporter);
+
+our @EXPORT_OK = qw(JUST_MANAGED INCLUDE_START_DEPENDS_ON INCLUDE_STOP_DEPENDS_ON);
+
+use constant {
+    JUST_MANAGED                => 0b0000001,
+    INCLUDE_START_DEPENDS_ON    => 0b0000010,
+    INCLUDE_STOP_DEPENDS_ON     => 0b0000100,
+};
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
