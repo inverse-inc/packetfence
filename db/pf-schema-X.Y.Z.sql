5f470b8c4529d21f702f94040349a69a72aaa591
Durand Fabrice <fdurand@inverse.ca>
2016-05-06 10:31:21 -0400

    Trigger only on locationlog update

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -334,33 +334,8 @@ CREATE TABLE `locationlog_archive` (
   KEY `locationlog_view_switchport` (`switch`,`port`,`end_time`,`vlan`)
 ) ENGINE=InnoDB;
 
-DROP TRIGGER IF EXISTS locationlog_insert_in_locationlog_archive_before_insert_trigger;
-DELIMITER /
-CREATE TRIGGER locationlog_insert_in_locationlog_archive_before_insert_trigger BEFORE INSERT ON locationlog
-FOR EACH ROW
-BEGIN
-  IF (NEW.connection_type IS NOT NULL AND NEW.connection_type = "DHCP") THEN
-    INSERT INTO locationlog_archive
-           ( mac, switch, port,
-             vlan, role, connection_type,
-             connection_sub_type, dot1x_username, ssid,
-             start_time, end_time, switch_ip,
-             switch_mac, stripped_user_name, realm,
-             session_id
-           )
-    VALUES
-           ( NEW.mac, NEW.switch, NEW.port,
-             NEW.vlan, NEW.role, NEW.connection_type,
-             NEW.connection_sub_type, NEW.dot1x_username, NEW.ssid,
-             NEW.start_time, NEW.end_time, NEW.switch_ip,
-             NEW.switch_mac, NEW.stripped_user_name, NEW.realm,
-             NEW.session_id
-           );
-
-  END IF;
-END /
-
 DROP TRIGGER IF EXISTS locationlog_insert_in_locationlog_archive_before_update_trigger;
+DELIMITER /
 CREATE TRIGGER locationlog_insert_in_locationlog_archive_before_update_trigger BEFORE UPDATE ON locationlog
 FOR EACH ROW
 BEGIN
@@ -374,14 +349,13 @@ BEGIN
              session_id
            )
     VALUES
-           ( NEW.mac, NEW.switch, NEW.port,
-             NEW.vlan, NEW.role, NEW.connection_type,
-             NEW.connection_sub_type, NEW.dot1x_username, NEW.ssid,
-             NEW.start_time, NEW.end_time, NEW.switch_ip,
-             NEW.switch_mac, NEW.stripped_user_name, NEW.realm,
-             NEW.session_id
+           ( OLD.mac, OLD.switch, OLD.port,
+             OLD.vlan, OLD.role, OLD.connection_type,
+             OLD.connection_sub_type, OLD.dot1x_username, OLD.ssid,
+             OLD.start_time, OLD.end_time, OLD.switch_ip,
+             OLD.switch_mac, OLD.stripped_user_name, OLD.realm,
+             OLD.session_id
            );
-
   END IF;
 END /
 DELIMITER ;
