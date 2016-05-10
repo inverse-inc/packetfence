403690d1ae766a19c53ea34f4f31d5205b9386ad
Derek Wuelfrath <dwlfrth@users.noreply.github.com>
2016-05-10 14:46:39 -0400

    removing useless variable and using username as name

--- addons/stress-tester/import-users.ps1
+++ addons/stress-tester/import-users.ps1
@@ -3,10 +3,8 @@ $Users = Import-Csv -Delimiter "|" -Path ".\mock_data.csv"
 foreach ($User in $Users)  
 {  
     $OU = "OU=PF-LOAD-TESTING,DC=DOMAIN,DC=NET"  
-    $Password = $User.password 
-    $Detailedname = $User.username
-    $UserFirstname = $User.username
-    $FirstLetterFirstname = $User.username
-    $SAM = $User.username 
-    New-ADUser -Name $Detailedname -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $user.firstname -Surname $user.name -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU  
+    $Password = $User.password
+    $SAM = $User.username
+    $DetailedName = $User.username
+    New-ADUser -Name $DetailedName -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $DetailedName -GivenName $DetailedName -Surname $DetailedName -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU  
 } 
