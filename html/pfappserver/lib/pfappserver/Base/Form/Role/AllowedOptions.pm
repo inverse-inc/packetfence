b1fc56b32b9a8b2efd7829144f4a39def45ba030
James Rouzier <jrouzier@inverse.ca>
2016-05-17 14:19:07 -0400

    Add missing import

--- html/pfappserver/lib/pfappserver/Base/Form/Role/AllowedOptions.pm
+++ html/pfappserver/lib/pfappserver/Base/Form/Role/AllowedOptions.pm
@@ -15,6 +15,8 @@ pfappserver::Base::Form::Role::AllowedOptions
 use namespace::autoclean;
 use HTML::FormHandler::Moose::Role;
 
+use pf::admin_roles;
+
 =head2 _get_allowed_options
 
 Get the allowed options for the current user based off their role.
@@ -22,8 +24,8 @@ Get the allowed options for the current user based off their role.
 =cut
 
 sub _get_allowed_options {
-    my ($self,$option) = @_;
-    return admin_allowed_options([$self->ctx->user->roles],$option);
+    my ($self, $option) = @_;
+    return admin_allowed_options([$self->ctx->user->roles], $option);
 }
 
 =head1 AUTHOR
