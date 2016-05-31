186ae18af940d551ac18030fa0d56a74732c3a71
Julien Semaan <jul.semaan@gmail.com>
2016-05-31 10:05:07 -0400

    make sure the destination_url is computed from the user browser

--- html/common/pf.js
+++ html/common/pf.js
@@ -132,3 +132,19 @@ function showWebNotification(message, icon){
     var notification = new Notification(message, {icon:icon});
   }  
 }
+
+/**
+   getQueryParams
+
+   Read a page's GET URL variables and return them as an associative array.
+*/
+function getQueryParams() {
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++){
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
