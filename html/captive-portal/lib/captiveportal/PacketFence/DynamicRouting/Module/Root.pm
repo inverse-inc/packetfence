0e60d35f7e9eb7e951d1c624dc8741c699a8fcdc
Julien Semaan <jul.semaan@gmail.com>
2016-06-03 13:08:45 -0400

    only add flash msg when meaningful

--- html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Root.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Root.pm
@@ -252,7 +252,9 @@ sub apply_new_node_info {
     my ( $status, $status_msg );
     ( $status, $status_msg ) = pf::node::node_register($self->current_mac, $self->username, %{$self->new_node_info()});
     if ($status) {
-        $self->app->flash->{notice} = [ "Role %s has been assigned to your device with unregistration date : %s", $self->new_node_info->{category}, $self->new_node_info->{unregdate} ];
+        if($self->new_node_info->{category} && $self->new_node_info->{unregdate}) {
+            $self->app->flash->{notice} = [ "Role %s has been assigned to your device with unregistration date : %s", $self->new_node_info->{category}, $self->new_node_info->{unregdate} ];
+        }
         return $TRUE;
     }
     else {
