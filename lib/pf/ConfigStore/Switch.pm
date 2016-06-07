27fcabe85cfc71d1c2e0d41b8ad6477680f9b39f
Julien Semaan <jul.semaan@gmail.com>
2016-06-07 13:18:54 -0400

    use non-persisted switch group when computing hierarchy
    fixes #1529

--- lib/pf/ConfigStore/Switch.pm
+++ lib/pf/ConfigStore/Switch.pm
@@ -118,7 +118,7 @@ sub cleanupBeforeCommit {
         $switch->{inlineTrigger} = join( ',', @triggers );
     }
 
-    my $parent_config = $self->parentConfigRaw($id);
+    my $parent_config = $self->fullConfigRaw($switch->{$self->parentAttribute} ? $self->_formatGroup($switch->{$self->parentAttribute}) : $self->topLevelGroup);
     if($id ne $self->topLevelGroup) {
         # Put the elements to undef if they are the same as in the inheritance
         while (my ($key, $value) = each %$switch){
