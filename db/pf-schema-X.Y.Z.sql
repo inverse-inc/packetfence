fb0c4f4eeba9cab342a3df5850084a70a767903f
James Rouzier <jrouzier@inverse.ca>
2016-06-08 14:14:42 -0400

    Database upgrade

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -1135,6 +1135,78 @@ CREATE TABLE radius_audit_log (
 ) ENGINE=InnoDB;
 
 --
+-- Table structure for table `node_option82`
+--
+
+CREATE TABLE `node_option82` (
+  `node_option82_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL,
+  `mac` varchar(17) NOT NULL,
+  `option82_switch` varchar(17) NULL,
+  `switch_id` varchar(17) NULL,
+  `port` varchar(8) NOT NULL default '',
+  `vlan` varchar(255) default NULL,
+  `circuit_id_string` varchar(255) default NULL,
+  `module` varchar(255) default NULL,
+  `host` varchar(255) default NULL,
+  UNIQUE KEY mac (mac)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `node_option82_history`
+--
+
+CREATE TABLE `node_option82_history` (
+  `node_option82_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL,
+  `mac` varchar(17) NOT NULL,
+  `option82_switch` varchar(17) NULL,
+  `switch_id` varchar(17) NULL,
+  `port` varchar(8) NOT NULL default '',
+  `vlan` varchar(255) default NULL,
+  `circuit_id_string` varchar(255) default NULL,
+  `module` varchar(255) default NULL,
+  `host` varchar(255) default NULL,
+  INDEX (mac)
+) ENGINE=InnoDB;
+
+--
+-- Trigger to archive node_option82 entries to the history table after an update
+--
+
+DROP TRIGGER IF EXISTS node_option82_after_update_trigger;
+DELIMITER /
+CREATE TRIGGER node_option82_after_update_trigger AFTER UPDATE ON node_option82
+FOR EACH ROW
+BEGIN
+    INSERT INTO node_option82_history
+           (
+            created_at,
+            mac,
+            option82_switch,
+            switch_id,
+            port,
+            vlan,
+            circuit_id_string,
+            module,
+            host
+           )
+    VALUES
+           (
+            OLD.created_at,
+            OLD.mac,
+            OLD.option82_switch,
+            OLD.switch_id,
+            OLD.port,
+            OLD.vlan,
+            OLD.circuit_id_string,
+            OLD.module,
+            OLD.host
+           );
+END /
+DELIMITER ;
+
+--
 -- Updating to current version
 --
 
