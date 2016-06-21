0b5b6e93e4f59e7d31cb12ff7f93a31e5e0f35f8
Louis Munro <lmunro@inverse.ca>
2016-06-21 14:18:03 -0400

    Post 6.1 release procedure.

---
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -0,0 +1,19 @@
+--
+-- PacketFence SQL schema upgrade from X.X.X to X.Y.Z
+--
+
+
+--
+-- Setting the major/minor/sub-minor version of the DB
+--
+
+SET @MAJOR_VERSION = 6;
+SET @MINOR_VERSION = 1;
+SET @SUBMINOR_VERSION = 9;
+
+--
+-- The VERSION_INT to ensure proper ordering of the version in queries
+--
+
+SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
+
