a8808882d5eea83d2c29d5ec4416a909c9f4c0e4
Julien Semaan <jul.semaan@gmail.com>
2016-06-27 13:59:44 -0400

    added tests for scan + provisioning matching in portal profile

--- t/test_paths.pm
+++ t/test_paths.pm
@@ -37,6 +37,8 @@ BEGIN {
     $pf::file_paths::vlan_filters_config_file = catfile($test_dir,'data/vlan_filters.conf');
     $pf::file_paths::violations_config_file = catfile($test_dir,'data/violations.conf');
     $pf::file_paths::firewall_sso_config_file = catfile($test_dir,'data/firewall_sso.conf');
+    $pf::file_paths::scan_config_file = catfile($test_dir,'data/scan.conf');
+    $pf::file_paths::provisioning_config_file = catfile($test_dir,'data/provisioning.conf');
     $pf::file_paths::config_file = catfile($test_dir,'data/pf.conf');
     $pf::file_paths::pf_config_file = catfile($test_dir,'data/pf.conf');
 
