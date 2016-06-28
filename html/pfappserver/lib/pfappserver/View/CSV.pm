6522370d9585d5b8255b8e348adb6cc184819c33
Julien Semaan <jul.semaan@gmail.com>
2016-06-28 17:22:01 -0400

    added perldoc to csv view

--- html/pfappserver/lib/pfappserver/View/CSV.pm
+++ html/pfappserver/lib/pfappserver/View/CSV.pm
@@ -1,5 +1,15 @@
 package pfappserver::View::CSV;
 
+=head1 NAME
+
+pfappserver::View::CSV
+
+=head1 DESCRIPTION
+
+Used to render CSV
+
+=cut
+
 use base qw ( Catalyst::View::CSV );
 
 __PACKAGE__->config ( sep_char => ",", suffix => "csv" );
@@ -15,5 +25,33 @@ sub process {
     $self->SUPER::process($c);
 }
 
-1;
+=head1 AUTHOR
 
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
