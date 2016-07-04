73e3cc986fe5e50bb90719f892b17a6b0206b8bb
Julien Semaan <jul.semaan@gmail.com>
2016-07-04 11:52:18 -0400

    add greater_equals + lower_equals conditions

---
+++ lib/pf/condition/greater_equals.pm
@@ -0,0 +1,71 @@
+package pf::condition::greater_equals;
+=head1 NAME
+
+pf::condition::greater_equals
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::condition::greater_equals
+
+=cut
+
+use strict;
+use warnings;
+use Moose;
+extends qw(pf::condition);
+use pf::constants;
+
+=head2 value
+
+The value to match against
+
+=cut
+
+has value => (
+    is => 'ro',
+    required => 1,
+    isa  => 'Str',
+);
+
+=head2 match
+
+Match a numeric greater than
+
+=cut
+
+sub match {
+    my ($self,$arg) = @_;
+    return $FALSE if(!defined($arg));
+    return $arg >= $self->value;
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
