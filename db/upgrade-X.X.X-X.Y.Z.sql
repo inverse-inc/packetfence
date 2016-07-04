e3e402b73fbae2e013340a5d6e4fc814cd7f28c9
Julien Semaan <jul.semaan@gmail.com>
2016-07-04 13:22:45 -0400

    added fields for fingerbank integration++

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -2,6 +2,12 @@
 -- PacketFence SQL schema upgrade from X.X.X to X.Y.Z
 --
 
+--
+-- Additionnal fields for the Fingerbank integration
+--
+
+ALTER TABLE node ADD COLUMN `device_version` varchar(255) DEFAULT NULL AFTER `device_class`;
+ALTER TABLE node ADD COLUMN `device_score` varchar(255) DEFAULT NULL AFTER `device_version`;
 
 --
 -- Setting the major/minor/sub-minor version of the DB
@@ -9,7 +15,7 @@
 
 SET @MAJOR_VERSION = 6;
 SET @MINOR_VERSION = 2;
-SET @SUBMINOR_VERSION = 9;
+SET @SUBMINOR_VERSION = 0;
 
 --
 -- The VERSION_INT to ensure proper ordering of the version in queries
