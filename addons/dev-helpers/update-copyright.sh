f68053034179fb6d5bbc6060e1157d2b2995d6bb
James Rouzier <jrouzier@inverse.ca>
2016-07-05 13:20:40 -0400

    Fix update copyright regex and lookup directories

--- addons/dev-helpers/update-copyright.sh
+++ addons/dev-helpers/update-copyright.sh
@@ -2,4 +2,4 @@
 
 FILE=$0
 
-perl -pi -e's/Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-6]).*Inverse/Copyright (C) 2005-2016 Inverse/' $(grep --exclude="$(basename $FILE)" -Prl 'Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-4]).*Inverse' lib/ html/ addons/ sbin/ bin/ raddb/ ) 
+perl -pi -e's/Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-6]).*Inverse/Copyright (C) 2005-2016 Inverse/' $(grep --exclude="$(basename $FILE)" -Prl 'Copyright.*(20(0[0-9]|1[0-4])-)?20(0[0-9]|1[0-6]).*Inverse' lib/ html/ addons/ sbin/ bin/ raddb/ t/ conf/ src/ docs/ debian/ )
