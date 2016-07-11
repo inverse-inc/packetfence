39fced8ba4a4ea3f32ae2d44ece0ee641766f425
Derek Wuelfrath <dwuelfrath@inverse.ca>
2016-07-11 14:08:30 -0400

    Remove obsolete methods

--- lib/pf/Switch/MockedSwitch.pm
+++ lib/pf/Switch/MockedSwitch.pm
@@ -96,7 +96,6 @@ sub supportsExternalPortal { return $TRUE; }
 sub supportsMABFloatingDevices { return $TRUE; }
 sub supportsWebFormRegistration { return $TRUE }
 sub supportsAccessListBasedEnforcement { return $TRUE }
-sub supportsUrlBasedEnforcement { return $TRUE }
 # VoIP technology supported
 sub supportsRadiusVoip { return $TRUE; }
 # special features supported
