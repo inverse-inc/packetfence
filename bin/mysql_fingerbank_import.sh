da2e81f47269858acaa53cca5d8130005dd71c7a
Julien Semaan <jul.semaan@gmail.com>
2016-07-13 15:43:47 -0400

    fix syntax

--- bin/mysql_fingerbank_import.sh
+++ bin/mysql_fingerbank_import.sh
@@ -2,7 +2,7 @@
 
 cd /usr/local/fingerbank/
 result=`make init-mysql 2>&1`
-local status=$?
+status=$?
 if [ $status -ne 0 ]; then
   perl -I/usr/local/pf/lib -Mpf::config::util -e "pfmailer(subject => 'Failed to import Fingerbank data inside MySQL', message => qq[Output of the command was : $result])"
 else
