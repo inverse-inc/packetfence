inverse-inc/packetfence

commit ab8a70a890c4b24d10a478c02119a5828447dc0c

Dynamic authentication sources.
By default, conf/authentication.conf points to admin.conf for
semi-backward compatibility.

lib/pf/Authentication/Source/KerberosSource.pm
@@ -1,0 +1,70 @@
+=item
+
+=cut 
+package pf::Authentication::Source::KerberosSource;
+use pf::Authentication::Source;
+use Moose;
+
+use pf::config qw($TRUE $FALSE);
+use Authen::Krb5::Simple;
+
+extends 'pf::Authentication::Source';
+
+has 'host' => (isa => 'Str', is => 'rw', required => 1);
+has 'realm' => (isa => 'Str', is => 'rw', required => 1);
+
+sub available_attributes {
+  my $self = shift;
+  my $super_attributes = $self->SUPER::available_attributes; 
+  my $own_attributes = ["username"];
+  return [@$super_attributes, @$own_attributes];
+}
+
+=item authenticate
+
+=cut
+sub authenticate_using_kerberos {
+  
+  my ( $self, $username, $password ) = @_;
+  
+  my $kerberos = Authen::Krb5::Simple->new( realm => $self->{'realm'} );
+  
+  if ($kerberos->authenticate($username, $password)) {
+    return ($TRUE, 'Successful authentication using Kerberos.');
+  } else {
+    return ($FALSE, 'Invalid login or password');
+  }
+  
+  return ($FALSE, 'Unable to connect to Kerberos server');
+}
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Inverse inc.
+
+=head1 LICENSE 
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:

conf/authentication.conf
@@ -1,0 +1,17 @@
+# We read the connection information from pf.conf
+[local]
+description=Local Users
+type=sql
+
+[file1]
+description=Legacy Source
+type=htpasswd
+path=/usr/local/pf/conf/admin.conf
+
+[file1 rule gonzesses]
+description=foo bar baz
+action0=set_role=marketing_chicks
+action1=set_unreg_date="2013-03-01 19:50"
+action2=set_access_level=4294967296
+match=any
+condition0=username,equals,*

conf/authentication/guest_managers.pm
@@ -1,139 +1,0 @@
-package authentication::guest_managers;
-=head1 NAME
-
-authentication::guest_managers - authenticate guest managers
-
-=head1 DESCRIPTION
-
-htaccess style file authentication on the F<conf/guest-managers.conf> file
-
-This module extends pf::web::auth.
-
-=cut
-use strict;
-use warnings;
-use Apache::Htpasswd;
-use Log::Log4perl;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE $conf_dir);
-
-our $VERSION = 1.20;
-
-# TODO: regroup authenticate() portion of local and this module in authentication::htaccess 
-#       (or even pf::web::auth::htaccess) to prevent code duplication
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown
-
-=cut
-our $name = "Guest Management";
-
-=back
-
-=head1 OBJECT METHODS
-
-=over
-
-=item authenticate( $login, $password )
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-  my ($this, $username, $password) = @_;
-  my $logger = Log::Log4perl::get_logger('authentication::guest_managers');
-
-  foreach my $passwdFile ("$conf_dir/guest-managers.conf", "$conf_dir/admin.conf") {
-
-    if (! -r $passwdFile) {
-        $logger->error("unable to read password file '$passwdFile'");
-        $this->_setLastError('Unable to validate credentials at the moment');
-        next;
-    }
-  
-    my $htpasswd = new Apache::Htpasswd({
-        passwdFile => $passwdFile,
-        ReadOnly   => 1}
-    );
-    if ($htpasswd->htCheckPassword($username, $password)) {
-        return $TRUE;
-    }
-
-  }
-  $this->_setLastError('Invalid login or password');
-  return $FALSE;
-}
-
-=item isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-Here we strip the domain portion and validate if the user exists in conf/guest_mamangers.conf and conf/admin.conf.
-
-=cut    
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    foreach my $password_file ("$conf_dir/guest-managers.conf", "$conf_dir/admin.conf") {
-
-        if (! -r $password_file) {
-            $logger->error("unable to read password file '$password_file'");
-            return $FALSE;
-        }   
-    
-        # strip @domain...
-        $sponsor_email =~ s/@.*$//;
-    
-        # does the user exists in the password file?
-        my $htpasswd = new Apache::Htpasswd({ passwdFile => $password_file, ReadOnly   => 1});
-        return $TRUE if ( $htpasswd->fetchPass($sponsor_email) );
-
-    }
-
-    # otherwise
-    $logger->error("unable to find user $sponsor_email in both password files");
-    return $FALSE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodeau <obilodeau@inverse.ca>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2011, 2012 Inverse inc.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;

conf/authentication/kerberos.pm
@@ -1,122 +1,0 @@
-package authentication::kerberos;
-=head1 NAME
-
-authentication::kerberos - kerberos authentication
-
-=head1 DESCRIPTION
-
-authentication::kerberos allows to validate a username/password combination using Kerberos5
-
-This module extends pf::web::auth
-
-=cut
-use strict;
-use warnings;
-use Authen::Krb5::Simple;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE);
-
-our $VERSION = 1.20;
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Don't forget to install the Authen::Krb5::Simple module. 
-This is done automatically if you use a packaged version of PacketFence.
-
-Define the variables C<Krb5Realm> at the top of the module.
-
-=cut
-my $Krb5Realm = 'EXAMPLE.COM';
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown
-
-=cut
-our $name = "Kerberos";
-
-=back
-
-=head1 OBJECT METHODS
-
-=over
-
-=item authenticate( $login, $password )
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-    my ($this, $username, $password) = @_;
-    my $krb = Authen::Krb5::Simple->new( realm => $Krb5Realm );
-
-    if ($krb->authenticate($username, $password)) {
-        return $TRUE;
-    } else {
-        $this->_setLastError('Invalid login or password');
-        return $FALSE;
-    }
-}
-
-=item isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-Can't perform user validation with Authen::Krb5::Simple.
-Override with what you deem necessary.
-
-=cut
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    $logger->error(q{Unimplemented! Kerberos module doesn't support checking for a user's existence});
-    return $FALSE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodeau <obilodeau@inverse.ca>
-
-Brad Lhotsky <brad@divisionbyzero.net> 
-
-Based on pf::authenticate::radius by:
-
-Maikel van der roest <mvdroest@utelisys.com>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2011, 2012 Inverse inc.
-
-Copyright (C) 2008 Utelisys Communications B.V.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;

conf/authentication/ldap.pm
@@ -1,290 +1,0 @@
-package authentication::ldap;
-
-=head1 NAME
-
-authentication::ldap - LDAP authentication
-
-=head1 DESCRIPTION
-
-authentication::ldap allows to validate a username/password combination using LDAP
-
-This module extends pf::web::auth
-
-=cut
-use strict;
-use warnings;
-use Log::Log4perl;
-use Net::LDAP;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE);
-
-our $VERSION = 1.20;
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Don't forget to install the Net::LDAP module. 
-This is done automatically if you use a packaged version of PacketFence.
-
-Define the variables C<LDAPServer>, C<LDAPBindDN>, C<LDAPBindPassword>, C<LDAPUserBase>, C<LDAPUserKey>
-and C<LDAPUserScope> at the top of the module.
-
-=over
-
-=item * C<LDAPServer>
-
-LDAPServer hostname of IP address to connect to
-
-=item * C<LDAPBindDN> and C<LDAPBindPassword>
-
-DN and password of a user which is allowed to search for user accounts
-
-=item * C<LDAPUserKey> 
-
-The name of the parameter you would like to find your users by (in order to retrieve their DN).
-
-=item * C<LDAPUserBase>
-
-LDAP branch your users are in
-
-=item * C<LDAPUserScope>
-
-Do you want to search the whole subbranch of $LDAPUserBase for users or only direct entries at $LDAPUserBase ?
-
-=cut
-my $LDAPUserBase = "";
-my $LDAPUserKey = "cn";
-my $LDAPUserScope = "sub";
-my $LDAPBindDN = "";
-my $LDAPBindPassword = "";
-my $LDAPServer = "";
-my $LDAPGroupMemberKey; # usually 'memberOf'
-my $LDAPGroupDN;
-my $LDAPSponsorUserKey = "userPrincipalName";
-
-=back
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown (displayed only if more than 1 auth type is configured).
-
-=cut
-our $name = "LDAP";
-
-=back
-
-=head1 TESTING
-
-You can try your LDAP query with:
-
-  ldapsearch -x -b <LDAPUserBase> -h <LDAPServer> -W -D <LDAPBindDN> <LDAPUserKey>=username dn
-
-For example:
-
-  ldapsearch -x -b "ou=users,dc=packetfence,dc=org" -h ldap.packetfence.org -W -D "cn=PacketFence,ou=IT,dc=packetfence,dc=org" "cn=obilodeau" dn
-
-=head1 EXAMPLES
-
-Here's an example modification in order to test several 
-LDAP servers:
-
-  my @LDAPServers = ('ad1.example.com', 'ad2.example.com', 'ad3.example.com');
-
-  sub authenticate {
-    my ($username, $password) = @_;
-    my $logger = Log::Log4perl::get_logger('authentication::ldap');
-
-    my $connection = undef;
-    my $i = 0;
-    while ( ( $i < scalar(@LDAPServers) ) && ( !defined($connection) ) ) {
-       $connection = Net::LDAP->new($LDAPServers[$i]);
-       if (! defined($connection)) {
-         $logger->warn("Unable to connect to '$LDAPServers[$i]'");
-       }
-    }
-
-    if (! defined($connection)) {
-       return (0,2);
-    }
-
-    [...]
-
-=cut
-
-=head1 OBJECT METHODS
-
-=over 
-
-=item * authenticate ($login, $password)
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-  my ($this, $username, $password) = @_;
-  my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-  my $connection = Net::LDAP->new($LDAPServer);
-  if (! defined($connection)) {
-    $logger->error("Unable to connect to '$LDAPServer'");
-    $this->_setLastError('Unable to validate credentials at the moment');
-    return $FALSE;
-  }
-
-  my $result = $connection->bind($LDAPBindDN, password => $LDAPBindPassword);
-
-  if ($result->is_error) {
-    $logger->error("Unable to bind with '$LDAPBindDN'");
-    $this->_setLastError('Unable to validate credentials at the moment');
-    return $FALSE;
-  }
- 
-  $result = $connection->search(
-    base => $LDAPUserBase,
-    filter => "($LDAPUserKey=$username)",
-#    filter => "(&($LDAPUserKey=$username)($LDAPGroupMemberKey=$LDAPGroupDN))",
-    scope => $LDAPUserScope,
-    #attrs => ['dn']
-  );
-  
-  if ($result->is_error) {
-    $logger->error("Unable to execute search");
-    $this->_setLastError('Unable to validate credentials at the moment');
-    return $FALSE;
-  }
-  
-  if ($result->count != 1) {
-    $logger->warn("Unable to find user '$username'");
-    $this->_setLastError('Invalid login or password');
-    return $FALSE;
-  }
-
-  my $user = $result->entry(0);
-
-  $result = $connection->bind($user->dn, password => $password);
-
-  if ($result->is_error) {
-    $logger->info("invalid password for $username");
-    $this->_setLastError('Invalid login or password');
-    return $FALSE;
-  }
-  
-  $connection->unbind;
-  return $TRUE;
-}
-
-=item * getMemberGroups
-
-Returns a list of all groups' DN in a given group DN.
-
-=cut
-sub getMemberGroups {
-    my ($this, $connection, $group_dn) = @_;
-
-    my $result = $connection->search(
-        base => $LDAPUserBase,
-        filter => "(&(objectClass=group)($LDAPGroupMemberKey=$group_dn))",
-        scope => $LDAPUserScope,
-        attrs => ['dn']
-    );
-
-    my @membergroups;
-    foreach my $entry ($result->entries) {
-        push @membergroups, $entry->dn();
-    }
-    return @membergroups;
-}
-
-=item * isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-We search in LDAP to see if user exists.
-
-=cut
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    my $connection = Net::LDAP->new($LDAPServer);
-    if (!defined($connection)) {
-      $logger->error("Unable to connect to '$LDAPServer'");
-      return $FALSE;
-    }
-  
-    my $result = $connection->bind($LDAPBindDN, password => $LDAPBindPassword);
-    if ($result->is_error) {
-      $logger->error("Unable to bind with '$LDAPBindDN'");
-      return $FALSE;
-    }
-  
-    # build filter
-    my $filter;
-    if (defined($LDAPGroupMemberKey) && defined($LDAPGroupDN)) {
-        $filter = "(&($LDAPSponsorUserKey=$sponsor_email)($LDAPGroupMemberKey=$LDAPGroupDN))";
-    }
-    else {
-        $filter = "($LDAPSponsorUserKey=$sponsor_email)";
-    }
-
-    $result = $connection->search(
-      base => $LDAPUserBase,
-      filter => $filter,
-      scope => $LDAPUserScope,
-      #attrs => ['dn']
-    );
-  
-    if ($result->is_error) {
-      $logger->error("Unable to execute search");
-      return $FALSE;
-    }
-  
-    if ($result->count != 1) {
-      $logger->warn("Unable to find user '$sponsor_email'");
-      return $FALSE;
-    }
-
-    # The user exists: success!
-    $connection->unbind;
-    return $TRUE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodau <obilodeau@inverse.ca>
-
-Dominik Gehl <dgehl@inverse.ca>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2006-2012 Inverse inc.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;

conf/authentication/local.pm
@@ -1,141 +1,0 @@
-package authentication::local;
-=head1 NAME
-
-authentication::local - htaccess file authentication
-
-=head1 DESCRIPTION
-
-authentication::local allows to validate a username/password combination using the htaccess file F<conf/user.conf>
-
-This module extends pf::web::auth
-
-=cut
-use strict;
-use warnings;
-use Apache::Htpasswd;
-use Log::Log4perl;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE $conf_dir);
-
-our $VERSION = 1.20;
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=over
-
-=item Password file
-
-Defaults to conf/user.conf.
-
-=cut
-our $password_file = "$conf_dir/user.conf";
-
-=back
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown (displayed only if more than 1 auth type is configured).
-
-=cut
-our $name = "Local";
-
-=back
-
-=head1 OBJECT METHODS
-
-=over
-
-=item authenticate( $login, $password )
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-  my ($this, $username, $password) = @_;
-  my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-  if (! -r $password_file) {
-      $logger->error("unable to read password file '$password_file'");
-      $this->_setLastError('Unable to validate credentials at the moment');
-      return $FALSE;
-  }
-
-  my $htpasswd = new Apache::Htpasswd({ passwdFile => $password_file, ReadOnly   => 1});
-  if ( (!defined($htpasswd->htCheckPassword($username, $password))) 
-      or ($htpasswd->htCheckPassword($username, $password) == 0) ) {
-
-      $this->_setLastError('Invalid login or password');
-      return $FALSE;
-  } else {
-      return $TRUE;
-  }
-}
-
-=item isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-Here we strip the domain portion and validate if the user exists in conf/user.conf.
-
-=cut
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    if (! -r $password_file) {
-        $logger->error("unable to read password file '$password_file'");
-        return $FALSE;
-    }
-
-    # strip @domain...
-    $sponsor_email =~ s/@.*$//;
-
-    # does the user exists in the password file?
-    my $htpasswd = new Apache::Htpasswd({ passwdFile => $password_file, ReadOnly   => 1});
-    return $TRUE if ( $htpasswd->fetchPass($sponsor_email) );
-
-    # otherwise
-    $logger->error("unable to find user $sponsor_email in password file '$password_file'");
-    return $FALSE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodeau <obilodeau@inverse.ca>
-
-Dominik Gehl <dgehl@inverse.ca>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2008-2012 Inverse inc.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;

conf/authentication/preregistered_guests.pm
@@ -1,139 +1,0 @@
-package authentication::preregistered_guests;
-=head1 NAME
-
-authentication::preregistered_guests
-
-=head1 DESCRIPTION
-
-Validates provided credentials against the temporary_password table (local guest accounts)
-
-This module extends pf::web::auth.
-
-=cut
-use strict;
-use warnings;
-use Log::Log4perl;
-use POSIX;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE normalize_time %Config);
-use pf::temporary_password;
-
-our $VERSION = 1.20;
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown
-
-=cut
-our $name = "Guests";
-
-=back
-
-=head1 OBJECT METHODS
-
-=over
-
-=item * authenticate( $login, $password )
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-    my ($this, $username, $password) = @_;
-    my $logger = Log::Log4perl::get_logger('authentication::preregistered_guests');
-
-    my ($status, $access_duration) = pf::temporary_password::validate_password($username, $password);
-    $logger->debug("password validation returned: $status");
-
-    if ($status == $pf::temporary_password::AUTH_SUCCESS) {
-        $this->{_accessDuration} = $access_duration;
-        return $TRUE;
-
-    } elsif ($status == $pf::temporary_password::AUTH_FAILED_INVALID) {
-        $this->_setLastError('Invalid login or password');
-        return $FALSE;
-
-    } elsif ($status == $pf::temporary_password::AUTH_FAILED_EXPIRED) {
-        $logger->info("authentication of guest $username failed because password is expired");
-        $this->_setLastError('This account is expired.');
-        return $FALSE;
-
-    } elsif ($status == $pf::temporary_password::AUTH_FAILED_NOT_YET_VALID) {
-        $logger->info("authentication of guest $username failed because password is not yet activated");
-        $this->_setLastError('This account is not yet activated.');
-        return $FALSE;
-    }
-
-    $this->_setLastError('Invalid login or password');
-    return $FALSE;
-}
-
-=item * getNodeAttributes
-
-Return unregdate based on access_duration for the user who just authenticated.
-
-=cut
-sub getNodeAttributes {
-    my ($this) = @_;
-
-    return (
-        unregdate => POSIX::strftime("%Y-%m-%d %H:%M:%S", localtime(time + normalize_time($this->{_accessDuration}))),
-        category => $Config{'guests_admin_registration'}{'category'},
-    );
-}
-
-=item * isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-Guest can't sponsor other guests.
-
-=cut
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    $logger->error(q{Guest can't sponsor other guests});
-    return $FALSE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodeau <obilodeau@inverse.ca>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2011, 2012 Inverse inc.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;

conf/authentication/radius.pm
@@ -1,146 +1,0 @@
-package authentication::radius;
-=head1 NAME
-
-authentication::radius - radius authentication
-
-=head1 DESCRIPTION
-
-authentication::radius allows to validate a username/password combination using RADIUS
-
-=cut
-use strict;
-use warnings;
-
-use Authen::Radius;
-use Log::Log4perl;
-
-use base ('pf::web::auth');
-
-use pf::config qw($TRUE $FALSE);
-
-our $VERSION = 1.20;
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Define the C<radiusServers> variable at the top of the module.
-
-=over
-
-=item Servers are always validated from top to bottom.
-
-=item Multiple servers are useful for fault tolerance not to try users on different RADIUS
-
-=back
-
-=cut
-
-# uncomment the second line to add another server to the list to check
-# you can add more lines also
-my $radiusServers = [ 
-    { 'host' => 'server1:1819', secret => 'secret' },
-#    { 'host' => 'server2:1819', secret => 'secret2' },
-];
-
-=head2 Optional
-
-=over
-
-=item name
-
-Name displayed on the captive portal dropdown
-
-=cut
-our $name = "RADIUS";
-
-=back
-
-=head1 OBJECT METHODS
-
-=over
-
-=item * authenticate ($login, $password)
-
-True if successful, false otherwise. 
-If unsuccessful errors meant for users are available in getLastError(). 
-Errors meant for administrators are logged in F<logs/packetfence.log>.
-
-=cut
-sub authenticate {
-    my ($this, $username, $password) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    foreach my $server (@$radiusServers) {
-
-        my $radcheck = new Authen::Radius(
-            Host => $server->{'host'}, 
-            Secret => $server->{'secret'},
-        );
-
-        my $response = $radcheck->check_pwd($username, $password);
-        if (Authen::Radius::get_error() eq 'ENONE') {
-
-            if ($response) {
-                return $TRUE;
-            } else {
-                $this->_setLastError('Invalid login or password');
-                return $FALSE;
-            }
-        }
-    }
-
-    $logger->error("Unable to perform RADIUS authentication on any server: " . Authen::Radius::get_error() );
-    $this->_setLastError('Unable to authenticate successfully');
-    return $FALSE;
-}
-
-=item * isAllowedToSponsorGuests
-
-Is the given email allowed to sponsor guest access?
-
-Can't perform user validation with Authen::Radius in a standard way.
-Override with what you deem necessary.
-
-=cut
-sub isAllowedToSponsorGuests {
-    my ($this, $sponsor_email) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-
-    $logger->error(q{Unimplemented! RADIUS module doesn't support checking for a user's existence});
-    return $FALSE;
-}
-
-=back
-
-=head1 AUTHOR
-
-Olivier Bilodeau <obilodeau@inverse.ca>
-
-Maikel van der roest <mvdroest@utelisys.com>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2011, 2012 Inverse inc.
-
-Copyright (C) 2008 Utelisys Communications B.V.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-USA.
-
-=cut
-
-1;
-

commit 8fabdd88a3855616b89d6e6fddb78d23d4a60134

Fix for Kerberos authentication issue #1617
Looks like a regression from "ab8a70a"

Signed-off-by: Andre Ziviani <apz11@c3sl.ufpr.br>

lib/pf/Authentication/Source/KerberosSource.pm
@@ -44,7 +44,7 @@ sub available_attributes {
 
 =cut
 
-sub authenticate_using_kerberos {
+sub authenticate {
 
   my ( $self, $username, $password ) = @_;
 