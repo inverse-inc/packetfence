5f7a74bd33eae2584fd8ce2c026ef5bb16c5db38
Durand Fabrice <fdurand@inverse.ca>
2016-07-18 16:11:52 -0400

    Added radreply table

--- db/pf-schema-6.3.0.sql
+++ db/pf-schema-6.3.0.sql
@@ -1201,3 +1201,17 @@ CREATE TABLE auth_log (
   PRIMARY KEY (id),
   KEY pid (pid)
 ) ENGINE=InnoDB;
+
+--
+-- Creating radreply table
+--
+
+CREATE TABLE radreply (
+  id int(11) unsigned NOT NULL auto_increment,
+  username varchar(64) NOT NULL default '',
+  attribute varchar(64) NOT NULL default '',
+  op char(2) NOT NULL DEFAULT '=',
+  value varchar(253) NOT NULL default '',
+  PRIMARY KEY  (id),
+  KEY username (username(32))
+);
