8d0a8493801037463d70caf6d09197cf4f3a39cc
James Rouzier <jrouzier@inverse.ca>
2016-07-20 15:16:38 -0400

    Added pf::node_option82 headings to the translation file

--- addons/extract_i18n_strings.pl
+++ addons/extract_i18n_strings.pl
@@ -30,6 +30,7 @@ use pfappserver::Form::Config::ProfileCommon;
 use pf::config;
 use pf::radius_audit_log;
 use pf::constants::admin_roles qw(@ADMIN_ACTIONS);
+use pf::node_option82;
 
 use constant {
     APP => 'html/pfappserver',
@@ -320,6 +321,7 @@ sub extract_modules {
     const('pf::config', 'Inline triggers', [$pf::config::MAC, $pf::config::PORT, $pf::config::SSID, $pf::config::ALWAYS]);
     const('pf::config', 'Network types', [$pf::config::NET_TYPE_VLAN_REG, $pf::config::NET_TYPE_VLAN_ISOL, $pf::config::NET_TYPE_INLINE, 'management', 'other']);
     const('pf::radius_audit_log', 'RADIUS Audit Log', \@pf::radius_audit_log::FIELDS);
+    const('pf::node_option82', 'Node Options 82', [values %pf::node_option82::HEADINGS]);
 
     my @values = map { "${_}_action" } @pf::action::VIOLATION_ACTIONS;
     const('pf::action', 'VIOLATION_ACTIONS', \@values);
