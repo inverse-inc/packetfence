95b34da5a570656d186a87da9787153cf96db5b9
Durand Fabrice <fdurand@inverse.ca>
2016-07-25 15:39:59 -0400

    Test the format of the mac and return undef is there is no snmp connection

--- lib/pf/Switch/Cisco/Catalyst_2950.pm
+++ lib/pf/Switch/Cisco/Catalyst_2950.pm
@@ -1209,14 +1209,15 @@ sub getRelayAgentInfoOptRemoteIdSub {
     my $oid_cdsRelayAgentInfoOptRemoteIdSub = '1.3.6.1.4.1.9.9.380.1.1.8.0';
     my $logger = $self->logger;
     if ( !$self->connectRead() ) {
-        return '';
+        return undef;
     }
     $logger->trace("SNMP get_request for cdsRelayAgentInfoOptRemoteIdSub: $oid_cdsRelayAgentInfoOptRemoteIdSub");
     my $result = $self->{_sessionRead}
         ->get_request( -varbindlist => [$oid_cdsRelayAgentInfoOptRemoteIdSub] );
     my $cdsRelayAgentInfoOptRemoteIdSub = $result->{$oid_cdsRelayAgentInfoOptRemoteIdSub};
     $cdsRelayAgentInfoOptRemoteIdSub =~ s/^0x//i;
-    return clean_mac($cdsRelayAgentInfoOptRemoteIdSub);
+    my $mac = clean_mac($cdsRelayAgentInfoOptRemoteIdSub);
+    return $mac if ($mac);
 }
 =back
 
