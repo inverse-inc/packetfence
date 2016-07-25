ce23fcee40102c425f9e75805fe2c7045c52835c
James Rouzier <jrouzier@inverse.ca>
2016-07-25 14:13:56 -0400

    Fix extracting dependencies from the spec file

--- addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
+++ addons/dev-helpers/centos-chroot/install-packages-from-spec.sh
@@ -18,7 +18,7 @@ REPOQUERY="repoquery --queryformat=%{NAME} --disablerepo=* $PF_REPO $STD_REPOS -
 
 EL_VERSION=$(cat /etc/redhat-release | perl -p -e's/^.*(\d+)\..*$/$1/' )
 
-rpm -q -D"el$EL_VERSION 1" --requires  --specfile $SPEC | grep -v packetfence \
+rpm -q -D"el$EL_VERSION 1" -D"builddoc 0" --requires  --specfile $SPEC | grep -v packetfence \
     | perl -pi -e's/ +$//' | sort -u \
     | xargs -d '\n' $REPOQUERY --whatprovides \
     | sort -u | grep -v perl-LDAP \
