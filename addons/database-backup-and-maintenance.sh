5c31b25e318aea69895270c5137fdc30ceb9925d
Derek Wuelfrath <dwuelfrath@inverse.ca>
2016-08-16 00:42:13 -0400

    Removed "pfmon handled" tasks

--- addons/database-backup-and-maintenance.sh
+++ addons/database-backup-and-maintenance.sh
@@ -66,18 +66,10 @@ fi
 # is MySQL running? meaning we are the live packetfence
 if [ -f /var/run/$SQL_ENGINE/$SQL_ENGINE.pid ]; then
 
-    /usr/local/pf/addons/database-cleaner.pl --table=locationlog --date-field=end_time --older-than="1 WEEK" --additionnal-condition="(end_time <> 0)"
-    
-    /usr/local/pf/addons/database-cleaner.pl --table=iplog_history --date-field=end_time --older-than="1 WEEK"
-    
     /usr/local/pf/addons/database-cleaner.pl --table=radacct --date-field=acctstarttime --older-than="1 WEEK" --additionnal-condition="acctstoptime IS NOT NULL"
     
     /usr/local/pf/addons/database-cleaner.pl --table=radacct_log --date-field=timestamp --older-than="1 WEEK"
     
-    /usr/local/pf/addons/database-cleaner.pl --table=auth_log --date-field=attempted_at --older-than="1 WEEK"
-
-    /usr/local/pf/addons/database-cleaner.pl --table=radius_audit_log --date-field=created_at --older-than="30 DAY"
-
     # lets optimize on Sunday
     DOW=`date +%w`
     if [ $DOW -eq 0 ]
