860d387f47cb9c3b3c0ac1bc2d969402f0e08a4a
James Rouzier <jrouzier@inverse.ca>
2016-08-22 11:13:49 -0400

    Make form aup customize

--- html/captive-portal/lib/captiveportal/Form/Widget/Field/AUP.pm
+++ html/captive-portal/lib/captiveportal/Form/Widget/Field/AUP.pm
@@ -11,37 +11,7 @@ AUP Widget
 =cut
 
 use Moose::Role;
-with 'HTML::FormHandler::Widget::Field::Checkbox';
-
-=head2 render_element
-
-Render the AUP with its checkbox
-
-=cut
-
-sub render_element {
-    my ($self, $result) = @_;
-    my $checkbox = HTML::FormHandler::Widget::Field::Checkbox::render_element($self, $result);
-    my $divs = '';
-    $divs .= '<div class="box box--large-height box--tint">'.$self->form->app->_render($self->form->app->current_module ? $self->form->app->current_module->aup_template() : "aup_text.html").'</div>';
-    $divs .= '<div class="layout--center u-pt"><div class="layout__item u-2/3 u-1/1-palm btn btn--light">'.$checkbox.'<label for="'.$self->id.'">'.
-      $self->form->app->i18n('I accept the terms').'</label></div></div>';
-
-    return $divs;
-}
-
-=head2 render
-
-Render the field
-
-=cut
-
-sub render {
-    my ($self, $result) = @_;
-    $result ||= $self->result;
-    die "No result for form field '" . $self->full_name . "'. Field may be inactive." unless $result;
-    return $self->render_element( $result );
-}
+with 'captiveportal::PacketFence::Form::Widget::Field::AUP';
 
 =head1 AUTHOR
 
@@ -71,4 +41,3 @@ USA.
 =cut
 
 1;
-
---
+++ html/captive-portal/lib/captiveportal/PacketFence/Form/Widget/Field/AUP.pm
@@ -0,0 +1,73 @@
+package captiveportal::PacketFence::Form::Widget::Field::AUP;
+
+=head1 NAME
+
+captiveportal::PacketFence::Form::Widget::Field::AUP
+
+=head1 DESCRIPTION
+
+AUP Widget
+
+=cut
+
+use Moose::Role;
+with 'HTML::FormHandler::Widget::Field::Checkbox';
+
+=head2 render_element
+
+Render the AUP with its checkbox
+
+=cut
+
+sub render_element {
+    my ($self, $result) = @_;
+    my $checkbox = HTML::FormHandler::Widget::Field::Checkbox::render_element($self, $result);
+    my $divs = '';
+    $divs .= '<div class="box box--large-height box--tint">'.$self->form->app->_render($self->form->app->current_module ? $self->form->app->current_module->aup_template() : "aup_text.html").'</div>';
+    $divs .= '<div class="layout--center u-pt"><div class="layout__item u-2/3 u-1/1-palm btn btn--light">'.$checkbox.'<label for="'.$self->id.'">'.
+      $self->form->app->i18n('I accept the terms').'</label></div></div>';
+
+    return $divs;
+}
+
+=head2 render
+
+Render the field
+
+=cut
+
+sub render {
+    my ($self, $result) = @_;
+    $result ||= $self->result;
+    die "No result for form field '" . $self->full_name . "'. Field may be inactive." unless $result;
+    return $self->render_element( $result );
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
