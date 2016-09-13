7caa24f8f48d3631f4ade44e50ba3c4dc0f8c0d4
Thierry Laurion <tlaurion@inverse.ca>
2016-09-13 15:41:00 -0400

    suppression of /etc in the mount loop. Overlay filesystem usage to make sure we use resolv.conf of the namespace inside of the chroot.

--- addons/create_chroot.sh
+++ addons/create_chroot.sh
@@ -7,7 +7,7 @@ if [ -z "$NS" ] || [ -z "$BASE" ]; then
     exit 1;
 fi
 
-DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp run/samba var/log/samba var/lock)
+DIRS=(proc var etc lib lib64 usr sbin bin var/cache sys var/lib/samba dev tmp run/samba var/log/samba var/lock var/run)
 
 for dir in "${DIRS[@]}"; do
   [ -d $BASE/$NS/$dir ]                || mkdir -p $BASE/$NS/$dir
@@ -16,7 +16,7 @@ done
 touch /var/cache/samba$NS/secrets.tdb
 cp -fr /var/cache/samba$NS $BASE/$NS/var/cache/
 
-DIRS=(proc etc lib lib64 bin usr sbin sys var/lib/samba dev var/log/samba var/lock)
+DIRS=(proc lib lib64 bin usr sbin sys var/lib/samba dev var/log/samba var/lock )
 
 MOUNTS=(`mount | awk '{print $3}'`)
 
@@ -26,4 +26,8 @@ for dir in "${DIRS[@]}"; do
     mount -o bind /$dir           $BASE/$NS/$dir
   fi
 done
+
+#We make sure we use /etc/resolv.conf of the namespace  (RHEL7+)
+mkdir /tmp/$NS
+mount -t overlay -o lowerdir=/etc,upperdir=/etc/netns/$NS,workdir=/tmp/$NS overlay /chroots/$NS/etc
 
