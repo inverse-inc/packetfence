aab071aa2cddf596f633bacb84a1950a9ba57165
Antoine Amacher <aamacher@inverse.ca>
2016-09-16 15:58:36 -0400

    import all scans and rules properly

--- html/pfappserver/lib/pfappserver/Model/Node/Tab/WMI.pm
+++ html/pfappserver/lib/pfappserver/Model/Node/Tab/WMI.pm
@@ -27,39 +27,38 @@ Process view
 
 sub process_view {
     my ($self, $c, @args) = @_;
-
-    my ($status, $result) = $c->model('Node')->view($c->stash->{mac});
-    if (is_success($status)) {
-        $c->stash->{node} = $result;
-    }
-
-    my $scan = pf::scan::wmi::rules->new();
-    my $profile = pf::Portal::ProfileFactory->instantiate($result->{mac});
     my $mac = $c->stash->{mac};
-    my ($scan_exist, $scan_config);
+    my ($status, @scans);
+    ($status, my $node) = $c->model("Node")->view($mac);
+    my $scan_model = $c->model("Config::Scan");
     eval {
-        ($scan_exist, $scan_config) = $c->model('Config::Scan')->read($profile->{_scans});
+        my $profile = pf::Portal::ProfileFactory->instantiate($mac);
+        foreach my $scan (split(/\s*,\s*/, $profile->{_scans})) {
+            ($status, my $item) = $scan_model->read($scan);
+            if (is_success($status)) {
+                push @scans, $item;
+            }
+        }
     };
     if ($@) {
         $c->log->error($@);
         return ($STATUS::INTERNAL_SERVER_ERROR, {status_msg => "Error retrieving information for $mac"});
     }
-    my $host = $result->{iplog}->{ip};
-    
-    foreach my $value ( keys %{$scan_config} ) {
-        $scan_config->{'_' . $value} = $scan_config->{$value};
+    my $wmi_model = $c->model("Config::WMI");
+    my @items;
+    foreach my $scan ( @scans) {
+        foreach my $rule_id (@{$scan->{wmi_rules} // []}) {
+            ($status, my $rule) = $wmi_model->read($rule_id);
+            if (is_success($status) && $rule->{on_tab}) {
+                push @items, { scan_id => $scan->{id}, rule_id => $rule->{id} };
+            }
+        }
     }
+    
  
-    $scan_config->{_scanIp} = $host;
-    $c->stash({
-        scan => $scan,
-        scan_exist => $scan_exist,
-        scan_config => $scan_config,
-        result => $result,
-    });
     use Data::Dumper;
-    $c->log->info(Dumper($scan));
-    return ($STATUS::OK, {scan => $scan, scan_exist => $scan_exist, scan_config => $scan_config, result => $result});
+    $c->log->info(Dumper(\@items, \@scans, $node->{iplog}->{ip}));
+    return ($STATUS::OK, {items => \@items, node => $node});
 }
 
 =head2 process_tab
@@ -69,27 +68,19 @@ Process tab
 =cut
 
 sub process_tab {
-    my ($self, $c, @args) = @_;
-    #scanSecuritySoftware(); 
-    #scanProcess();
-    my $scan_config = $c->stash->{scan_config};
-    my $scan_exist = $c->stash->{scan_exist};
-    my $scan = $c->stash->{scan};
-
+    my ($self, $c, $scan_id, $rule_id, $node, @args) = @_;
+    
     use Data::Dumper;
-    $c->log->info(Dumper($scan));
-    my $rules = $self->parseWmi($c, $scan, $scan_config);
-    $c->log->info(Dumper($rules));
-
-    if (is_success($scan_exist) && $rules) {
-        $c->stash->{rules} = $rules;
-    }else {
-        $c->response->status($scan_exist);
-        $c->stash->{status_msg} = $scan_config;
-        $c->stash->{current_view} = 'JSON';
-    }
-
-    return ($STATUS::OK, {rules => $rules})
+    
+    $self->parseWmi($c, $scan_id, $rule_id);
+    #scanProcess();
+    #my $host = $result->{iplog}->{ip};
+    #foreach my $value ( keys %{$scan_config} ) {
+    #    $scan_config->{'_' . $value} = $scan_config->{$value};
+    #}
+    #$scan_config->{_scanIp} = $host;
+    
+    return ($STATUS::OK);
 
 }
 
@@ -99,51 +90,79 @@ parsing Wmi answer
 
 =cut
 
-sub parseWmi {#:Chained('object') :PathPart :Args(0) :AdminRole('WMI_READ'){
-    my ($self, $c, $scan, $scan_config) = @_;
-    my $rule_config = $c->model('Config::WMI')->readAll();
-    my @rules = grep {$_->{on_tab}} @$rule_config;
-    foreach my $rule (@rules) { 
-        my $config = $c->model('Config::WMI')->read($rule);
-        my $config_rule = $config->[1];
-        my $scan_result = $scan->runWmi($scan_config, $config_rule);
-        if ($scan_result =~ /0x80041010/ || !@$scan_result) {
-            $rule->{item_exist} = 'No';
-        }elsif ($scan_result =~ /TIMEOUT/ || $scan_result =~ /UNREACHABLE/) {
-            $rule->{item_exist} = 'Request failed';
-        }else {
-            $rule->{item_exist} = 'Yes';
-        }
-        $rule->{scan_result} = $scan_result;
-    }
-    return \@rules;
+sub parseWmi {
+    my ($self, $c, $scan_id, $rule_id, $node) = @_;
+    my $scan_model = $c->model("Config::Scan");
+    my $wmi_model = $c->model("Config::WMI");
+    my $scan = pf::scan::wmi::rules->new();
+    my $scan_config = $scan_model->read($scan_id);
+    my $rule_config = $wmi_model->read($rule_id);
+
+    my $host = $node->{iplog}->{ip};
+    use Data::Dumper;
+    $c->log->info(Dumper($scan_config, $rule_config, $node, $host));
+    #    my $rule_detail = $model->read($rule);
+    #    my $config_rule = $rule_detail->[1];
+    my $scan_result = $scan->runWmi($scan_config, $rule_config);
+    $c->log->info(Dumper($scan_result));
+    #    if ($scan_result =~ /0x80041010/ || !@$scan_result) {
+    #        $rule->{item_exist} = 'No';
+    #    }elsif ($scan_result =~ /TIMEOUT/ || $scan_result =~ /UNREACHABLE/) {
+    #        $rule->{item_exist} = 'Request failed';
+    #    }else {
+    #        $rule->{item_exist} = 'Yes';
+    #    }
+    #    $rule->{scan_result} = $scan_result;
 }
 
-#=head2 scanSecuritySoftware
-#
-#Launch standard security scans
-#
-#=cut
-#
-#sub scanSecuritySoftware {#:Chained('object') :PathPart :Args(0) :AdminRole('WMI_READ') {
-#    my ($self, $c) = @_;
-#
-#    my $scan_config = $c->stash->{scan_config};
-#    my $scan_exist = $c->stash->{scan_exist};
-#    my $scan = $c->stash->{scan};
-#
-#    my $rules = parseWmi($self, $c, $scan, $scan_config);
-#    use Data::Dumper;
-#    $c->log->info(Dumper($rules));
-#
-#    if (is_success($scan_exist) && $rules) {
-#        $c->stash->{rules} = $rules;
-#    }else {
-#        $c->response->status($scan_exist);
-#        $c->stash->{status_msg} = $scan_config;
-#        $c->stash->{current_view} = 'JSON';
-#    }
-#}
+=head2 scanSecuritySoftware
+
+Launch standard security scans
+
+=cut
+
+sub scanSecuritySoftware {
+    my ($self, $c) = @_;
+
+    my $scan_config = $c->stash->{scan_config};
+    my $scan = pf::scan::wmi::rules->new();
+    my $scan_exist = "200";#->$scan_exist;
+    #my ($status, $result) = $c->model('Node')->view($c->stash->{mac});
+    #if (is_success($status)) {
+    #    $c->stash->{node} = $result;
+    #}
+
+    #my $profile = pf::Portal::ProfileFactory->instantiate($result->{mac});
+    #my $mac = $c->stash->{mac};
+    #my ($scan_exist, $scan_config);
+    #eval {
+    #    ($scan_exist, $scan_config) = $c->model('Config::Scan')->read($profile->{_scans});
+    #};
+    #if ($@) {
+    #    $c->log->error($@);
+    #    return ($STATUS::INTERNAL_SERVER_ERROR, {status_msg => "Error retrieving information for $mac"});
+    #}
+    #my $host = $result->{iplog}->{ip};
+    #
+    #foreach my $value ( keys %{$scan_config} ) {
+    #    $scan_config->{'_' . $value} = $scan_config->{$value};
+    #}
+ 
+    #$scan_config->{_scanIp} = $host;
+
+
+    my $rules = $self->parseWmi($c, $scan, $scan_config);
+    use Data::Dumper;
+    $c->log->info(Dumper($rules));
+
+    if (is_success($scan_exist) && $rules) {
+        $c->stash->{rules} = $rules;
+    }else {
+        $c->response->status($scan_exist);
+        $c->stash->{status_msg} = $scan_config;
+        $c->stash->{current_view} = 'JSON';
+    }
+}
 
 =head2 scanProcess
 
