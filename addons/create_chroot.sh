de588744327d0d8f1af5432265b1917da7aa402b
Thierry Laurion <tlaurion@inverse.ca>
2016-09-21 12:35:01 -0400

    same code now works for both CentOS6 and 7. No more overlayfs. This is a hack. bind mount of all /etc first level directories, and first level copy of files under /etc to prepare chroots. Might also work for Debian, will see.

--- addons/create_chroot.sh
+++ addons/create_chroot.sh
@@ -2,8 +2,13 @@
 NS=$1
 BASE=$2
 
+#May be needed later on to differenciate CentOS and Debian. Useless now since CentOS code is the same for RHEL6 and RHEL7
+#if [ -e /etc/redhat-release ]; then
+#    RHEL=$(cat /etc/redhat-release | grep -Eo "[0-9]+(\.[0-9]+)*" | cut -d '.' -f1);
+#fi
+
 if [ -z "$NS" ] || [ -z "$BASE" ]; then
-    echo "Missing parameter"
+    echo "Missing parameters. First is NS second is BASE."
     exit 1;
 fi
 
@@ -13,19 +18,27 @@ for dir in "${DIRS[@]}"; do
   [ -d $BASE/$NS/$dir ]                || mkdir -p $BASE/$NS/$dir
 done
 
-DIRS=(proc etc lib lib64 bin usr sbin sys dev var/log/samba)
+DIRS=(proc lib lib64 bin usr sbin sys dev var/log/samba)
 
 MOUNTS=(`mount | awk '{print $3}'`)
 
 for dir in "${DIRS[@]}"; do
     value=$BASE/$NS/$dir
     if [[ ! " ${MOUNTS[@]} " =~ " ${value} " ]]; then
-        if [[ "$dir" != "etc" ]]; then
-            mount -o bind /$dir           $BASE/$NS/$dir
-        else
-            #We make sure we use /etc/resolv.conf of the namespace  (RHEL7+)
-            mkdir -p /tmp/$NS
-            mount -t overlay -o lowerdir=/etc,upperdir=/etc/netns/$NS,workdir=/tmp/$NS overlay /chroots/$NS/etc
+        mount -o bind /$dir $BASE/$NS/$dir
+        if [ ! -f $BASE/$NS/$dir/resolv.conf ]; then
+            ETC_DIRS=$(find /etc -maxdepth 1 -type d|tail -n+2);
+            ETC_FILES=$(find /etc -maxdepth 1 -type f|grep -v resolv.conf)
+            echo "$ETC_DIRS"|while read etc_dir;
+            do
+                if [ ! -d "/$BASE/$NS$etc_dir" ]; then
+                    mkdir -p /$BASE/$NS$etc_dir;
+                    mount -o bind $etc_dir /$BASE/$NS$etc_dir;
+                fi
+            done
+            echo "$ETC_FILES"|while read etc_file; do yes 2>/dev/null|cp $etc_file /$BASE/$NS$etc_file; done
+            cp /etc/netns/$NS/resolv.conf /$BASE/$NS/etc
         fi
     fi
 done
+
