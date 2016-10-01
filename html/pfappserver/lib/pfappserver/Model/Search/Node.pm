7ebe24d8d8ed07711ac4d196519d8fe9dd67b1ad
James Rouzier <jrouzier@inverse.ca>
2016-10-01 16:30:57 -0400

    Fix displaying of the ssid

--- html/pfappserver/lib/pfappserver/Model/Search/Node.pm
+++ html/pfappserver/lib/pfappserver/Model/Search/Node.pm
@@ -109,6 +109,7 @@ sub make_builder {
             { table => 'locationlog', name => 'switch_ip', as => 'switch_ip' },
             { table => 'locationlog', name => 'switch_mac', as => 'switch_mac' },
             { table => 'locationlog', name => 'port', as => 'switch_port' },
+            { table => 'locationlog', name => 'ssid', as => 'last_ssid' },
         )->from('node',
                 {
                     'table' => 'node_category',
