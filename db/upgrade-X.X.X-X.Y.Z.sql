66780643fb933d88322358474954b8ffc5044c85
Durand Fabrice <fdurand@inverse.ca>
2016-10-13 14:29:38 -0400

    Fixed unknown column 'p_timestamp' in acct_start procedure

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -67,7 +67,7 @@ AND (acctstoptime IS NULL OR acctstoptime = 0) LIMIT 1;
 
 IF (Previous_Session_Time IS NOT NULL) THEN
     UPDATE radacct SET
-      acctstoptime = p_timestamp,
+      acctstoptime = p_acctstarttime,
       acctterminatecause = 'UNKNOWN'
       WHERE acctuniqueid = p_acctuniqueid
       AND (acctstoptime IS NULL OR acctstoptime = 0);
