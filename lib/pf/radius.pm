9e7be680fd3fb987d77cea689102587088eecbe8
Julien Semaan <jul.semaan@gmail.com>
2016-10-13 07:57:10 -0400

    ensure switch_ip (NAS-IP-Address) is used as controllerIp in switch overlay

--- lib/pf/radius.pm
+++ lib/pf/radius.pm
@@ -97,7 +97,7 @@ sub authorize {
     my $RAD_REPLY_REF;
 
     $logger->debug("instantiating switch");
-    my $switch = pf::SwitchFactory->instantiate({ switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $source_ip});
+    my $switch = pf::SwitchFactory->instantiate({ switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $switch_ip});
 
     # is switch object correct?
     if (!$switch) {
@@ -370,7 +370,7 @@ sub update_locationlog_accounting {
     my ( $switch_mac, $switch_ip, $source_ip, $stripped_user_name, $realm ) = $self->_parseRequest($radius_request);
 
     $logger->debug("instantiating switch");
-    my $switch = pf::SwitchFactory->instantiate( { switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $source_ip } );
+    my $switch = pf::SwitchFactory->instantiate( { switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $switch_ip } );
 
     # is switch object correct?
     if ( !$switch ) {
@@ -653,7 +653,7 @@ sub switch_access {
     my($switch_mac, $switch_ip,$source_ip,$stripped_user_name,$realm) = $self->_parseRequest($radius_request);
 
     $logger->debug("instantiating switch");
-    my $switch = pf::SwitchFactory->instantiate({ switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $source_ip});
+    my $switch = pf::SwitchFactory->instantiate({ switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $switch_ip});
 
     # is switch object correct?
     if (!$switch) {
