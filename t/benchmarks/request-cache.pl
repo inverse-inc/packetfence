6eab127518b906a5aec3a80f770b317eb5d53f3c
James Rouzier <jrouzier@inverse.ca>
2016-10-14 13:21:35 -0400

    Refactor benchmark script

--- t/benchmarks/request-cache.pl
+++ t/benchmarks/request-cache.pl
@@ -17,53 +17,45 @@ use warnings;
 use lib qw(/usr/local/pf/lib);
 use pf::node;
 use pf::SwitchFactory;
+use pf::Portal::ProfileFactory;
 use Data::Dumper;
 use Benchmark qw(timethis cmpthese);
 use CHI::Memoize qw(unmemoize);
-my %counts;
 
-{
-    print "\nnode_view\n";
+my $mac = "00:00:00:00:00:ff";
+bench_node_view();
+bench_node_exist();
+bench_switch_factory();
+bench_profile_factory();
 
-    $counts{"with caching"} = timethis(0,sub { my $node = pf::node::node_view("00:00:00:00:00:ff") }, "with caching");
-
-    unmemoize("pf::node::node_view");
-
-    $counts{"without caching"} = timethis(0,sub { my $node = pf::node::node_view("00:00:00:00:00:ff") }, "without caching");
-
-    cmpthese(\%counts);
+sub bench_node_view {
+    bench_cache("pf::node::node_view", sub { my $node = pf::node::node_view($mac) });
 }
 
-{
-    %counts = ();
-
-    print "\nnode_exist\n";
-
-    $counts{"with caching"} = timethis(0,sub { my $node = pf::node::node_exist("00:00:00:00:00:ff") }, "with caching");
-
-    unmemoize("pf::node::node_exist");
-
-    $counts{"without caching"} = timethis(0,sub { my $node = pf::node::node_exist("00:00:00:00:00:ff") }, "without caching");
-
-    cmpthese(\%counts);
+sub bench_node_exist {
+    bench_cache("pf::node::node_exist", sub { my $node = pf::node::node_exist($mac) });
 }
 
-{
-    %counts = ();
-
-    my $switch = pf::SwitchFactory->instantiate("192.168.0.1");
-
-    print "\npf::SwitchFactory->instantiate\n";
-
-    $counts{"with caching"} = timethis(0, sub { my $switch = pf::SwitchFactory->instantiate("192.168.0.1")}, "with caching");
-
-    unmemoize("pf::SwitchFactory::instantiate");
-
-    $counts{"without caching"} = timethis(0, sub { my $switch = pf::SwitchFactory->instantiate("192.168.0.1")}, "without caching");
-
-    cmpthese(\%counts);
+sub bench_switch_factory {
+    bench_cache("pf::SwitchFactory::instantiate", sub { my $switch = pf::SwitchFactory->instantiate("192.168.0.1")});
 }
 
+sub bench_profile_factory {
+    bench_cache("pf::Portal::ProfileFactory::instantiate", sub { my $switch = pf::Portal::ProfileFactory->instantiate($mac)});
+}
+
+sub bench_cache {
+    my ($func_id, $func, $count) = @_;
+    print "\n$func_id\n";
+    my %counts;
+    $count //= 0;
+    $counts{"with caching"} = timethis($count, $func, "with caching");
+
+    unmemoize($func_id);
+
+    $counts{"without caching"} = timethis($count, $func, "without caching");
+    cmpthese(\%counts);
+}
 
 =head1 AUTHOR
 
