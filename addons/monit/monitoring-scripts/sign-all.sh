954e3c82e5ba374c02e36f090d300d08007c4cb0
Julien Semaan <jul.semaan@gmail.com>
2016-10-17 07:35:56 -0400

    make sign-all.sh use an argument for diretory

--- addons/monit/monitoring-scripts/sign-all.sh
+++ addons/monit/monitoring-scripts/sign-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-DIR="/tmp/scripts"
+DIR="$1"
+
+if [ -z "$DIR" ]; then
+  echo "You need to specify a directory"
+  exit 255
+fi
 
 find $DIR -type f ! -name '*.sig' -exec gpg --batch --yes --output {}.sig --sign {} \;
 
