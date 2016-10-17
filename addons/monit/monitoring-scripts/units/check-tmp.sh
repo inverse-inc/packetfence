3b8088e87fc867909d75c725f98b1e36c8224fe1
Julien Semaan <jul.semaan@gmail.com>
2016-10-17 08:32:08 -0400

    default value for tmp file var

--- addons/monit/monitoring-scripts/units/check-tmp.sh
+++ addons/monit/monitoring-scripts/units/check-tmp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #fname:check-tmp.sh
 
+MAX_TMP_FILES="${MAX_TMP_FILES:-"1000"}"
+
 if [ `find /tmp/ -type f | wc -l` -gt 1000 ] ; then 
   echo "There are too much temp files in /tmp"
   exit 1
