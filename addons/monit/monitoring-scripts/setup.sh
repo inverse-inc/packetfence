9eea269dd36056ba0a7e202c5b6d96dc12388a9d
Julien Semaan <jul.semaan@gmail.com>
2016-10-17 08:31:56 -0400

    touchups to download urls

--- addons/monit/monitoring-scripts/setup.sh
+++ addons/monit/monitoring-scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export script_registry_url="http://inverse.ca/downloads/PacketFence/monit-script-registry.txt"
+export script_registry_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/monit-script-registry.txt"
 export script_registry_file="/etc/monit.d/checks-script-registry"
 export script_dir="/usr/local/pf/var/monitoring-scripts/"
 
@@ -15,13 +15,13 @@ export uuid=$(cat $uuid_file)
 export uuid_vars_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/vars/$uuid.txt"
 export uuid_vars_file="/etc/monit.d/uuid-vars"
 
-export global_vars_url="http://inverse.ca/downloads/PacketFence/vars.txt"
+export global_vars_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/vars.txt"
 export global_vars_file="/etc/monit.d/global-vars"
 
 export uuid_ignores_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/ignores/$uuid.txt"
 export uuid_ignores_file="/etc/monit.d/uuid-ignores"
 
-export global_ignores_url="http://inverse.ca/downloads/PacketFence/ignores.txt"
+export global_ignores_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/ignores.txt"
 export global_ignores_file="/etc/monit.d/global-ignores"
 
 export combined_vars_file="/etc/monit.d/vars"
