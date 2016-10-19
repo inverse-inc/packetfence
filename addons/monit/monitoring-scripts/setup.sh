5d4050174e2d36d4790ee2f6d8438dd975ee6ec5
Julien Semaan <jul.semaan@gmail.com>
2016-10-19 12:52:12 -0400

    handle monit dir in setup.sh for debian

--- addons/monit/monitoring-scripts/setup.sh
+++ addons/monit/monitoring-scripts/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ -f /etc/redhat-release ] ; then
+  monit_dir="/etc/monit.d"
+else
+  monit_dir="/etc/monit/conf.d"
+fi
+
 export script_registry_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/v1/monit-script-registry.txt"
-export script_registry_file="/etc/monit.d/checks-script-registry"
+export script_registry_file="$monit_dir/checks-script-registry"
 export script_dir="/usr/local/pf/var/monitoring-scripts/"
 
 export functions_script="$script_dir/.functions.sh"
 
-export uuid_file="/etc/monit.d/srv-uuid"
+export uuid_file="$monit_dir/srv-uuid"
 
 if ! [ -f "$uuid_file" ]; then
   echo "UUID not generated. Proceeding with UUID generation now."
@@ -15,18 +21,18 @@ fi
 
 export uuid=$(cat $uuid_file)
 export uuid_vars_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/v1/vars/$uuid.txt"
-export uuid_vars_file="/etc/monit.d/uuid-vars"
+export uuid_vars_file="$monit_dir/uuid-vars"
 
 export global_vars_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/v1/vars.txt"
-export global_vars_file="/etc/monit.d/global-vars"
+export global_vars_file="$monit_dir/global-vars"
 
 export uuid_ignores_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/v1/ignores/$uuid.txt"
-export uuid_ignores_file="/etc/monit.d/uuid-ignores"
+export uuid_ignores_file="$monit_dir/uuid-ignores"
 
 export global_ignores_url="http://inverse.ca/downloads/PacketFence/monitoring-scripts/v1/ignores.txt"
-export global_ignores_file="/etc/monit.d/global-ignores"
+export global_ignores_file="$monit_dir/global-ignores"
 
-export combined_vars_file="/etc/monit.d/vars"
+export combined_vars_file="$monit_dir/vars"
 
 function is_ignored {
   touch "$global_ignores_file"
