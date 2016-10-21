08e6537c88a1e9c84a5ee49a54ec70cc008bae8d
James Rouzier <jrouzier@inverse.ca>
2016-10-21 10:34:15 -0400

    Use pf::authentication::match2

--- lib/pf/api.pm
+++ lib/pf/api.pm
@@ -1047,9 +1047,14 @@ sub dynamic_register_node : Public {
     };
 
     my $source;
-    my $role = pf::authentication::match([@sources], $params, $Actions::SET_ROLE, \$source);
-    #Compute autoreg if we use autoreg
-    my $value = pf::authentication::match([@sources], $params, $Actions::SET_UNREG_DATE);
+    my $matched = pf::authentication::match2([@sources], $params);
+    unless ($matched) {
+        $logger->warn("Did not find any actions to match");
+        return;
+    }
+    my $values = $matched->{values};
+    my $role = $values->{$Actions::SET_ROLE};
+    my $value = $values->{$Actions::SET_UNREG_DATE};
     if (defined $value) {
         my %info = (
             'unregdate' => $value,
