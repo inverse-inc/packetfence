0edbd20e0954a48e0d5cc79222dc7fb3f7e3adf1
Thierry Laurion <thierry.laurion@gmail.com>
2016-10-26 16:23:13 -0400

    logic error, even though its impossible that there would be 2 ha_ips in the array.

--- lib/pf/services/manager/p0f.pm
+++ lib/pf/services/manager/p0f.pm
@@ -40,7 +40,7 @@ has '+launcher' => (
                 push (@ha_ips, $ha_int->{Tip});
             }
             my @tmp_bpf_filter = map { "( host $_ and port 7788)"} @ha_ips;
-            my $p0f_bpf_filter = join(" or not", @tmp_bpf_filter);
+            my $p0f_bpf_filter = join(" and not", @tmp_bpf_filter);
             $p0f_cmdline="sudo %1\$s -d -i any -p -f $p0f_map -s $p0f_sock" . " 'not $p0f_bpf_filter' " . " > /dev/null && pidof $name > $pid_file";
         }
         else
