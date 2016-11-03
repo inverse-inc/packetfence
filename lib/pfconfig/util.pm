a7e0d80a57b957b8094cc4f7a331b42968c07234
James Rouzier <jrouzier@inverse.ca>
2016-11-03 10:19:28 -0400

    Always untaint

--- lib/pfconfig/util.pm
+++ lib/pfconfig/util.pm
@@ -46,7 +46,9 @@ sub fetch_socket {
     $payload .= "\n";
     my $bytes_sent = syswrite_all($socket,$payload);
     read_data_with_length($socket,my $sereal_buffer);
-    return $sereal_buffer;
+    ## Match all bytes to always untaint
+    $sereal_buffer =~ /\A^(.*)\z/ms;
+    return $1;
 }
 
 sub fetch_decode_socket {
