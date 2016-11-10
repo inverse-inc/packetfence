9280506b2d069a87981d09a031f6507964218bd5
Durand Fabrice <fdurand@inverse.ca>
2016-11-10 15:43:39 -0500

    First draft

---
+++ lib/pf/Switch/Ubiquity/EdgeSwitch.pm
@@ -0,0 +1,119 @@
+package pf::Switch::Ubiquity::EdgeSwitch;
+
+
+=head1 NAME
+
+pf::Switch::Ubiquity::EdgeSwitch
+
+=head1 SYNOPSIS
+
+pf::Switch::Ubiquity::EdgeSwitch module manages access to EdgeSwitch
+
+=head1 STATUS
+
+Should work on the EdgeSwitch version started 1.7
+
+=cut
+
+use strict;
+use warnings;
+use pf::log;
+
+use base ('pf::Switch::Ubiquity');
+use pf::constants;
+use pf::config qw(
+    $WIRED_802_1X
+    $WIRED_MAC_AUTH
+);
+
+sub description { 'EdgeSwitch' }
+
+# importing switch constants
+use pf::Switch::constants;
+
+# CAPABILITIES
+# access technology supported
+sub supportsWiredMacAuth { return $TRUE; }
+sub supportsWiredDot1x { return $TRUE; }
+# VoIP technology supported
+sub supportsRadiusVoip { return $TRUE; }
+# override 2950's FALSE
+sub supportsRadiusDynamicVlanAssignment { return $TRUE; }
+
+=head2 wiredeauthTechniques
+
+Return the reference to the deauth technique or the default deauth technique.
+
+=cut
+
+sub wiredeauthTechniques {
+    my ($self, $method, $connection_type) = @_;
+    my $logger = $self->logger;
+    if ($connection_type == $WIRED_802_1X) {
+        my $default = $SNMP::SNMP;
+        my %tech = (
+            $SNMP::SNMP => 'dot1xPortReauthenticate',
+        );
+
+        if (!defined($method) || !defined($tech{$method})) {
+            $method = $default;
+        }
+        return $method,$tech{$method};
+    }
+    if ($connection_type == $WIRED_MAC_AUTH) {
+        my $default = $SNMP::SNMP;
+        my %tech = (
+            $SNMP::SNMP => 'handleReAssignVlanTrapForWiredMacAuth',
+        );
+
+        if (!defined($method) || !defined($tech{$method})) {
+            $method = $default;
+        }
+        return $method,$tech{$method};
+    }
+}
+
+=head2 dot1xPortReauthenticate
+
+Points to pf::Switch implementation bypassing Catalyst_2950's overridden behavior.
+
+=cut
+
+sub dot1xPortReauthenticate {
+    my ($self, $ifIndex, $mac) = @_;
+
+    return $self->_dot1xPortReauthenticate($ifIndex);
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# # vim: set expandtab:
+# # vim: set backspace=indent,eol,start:
