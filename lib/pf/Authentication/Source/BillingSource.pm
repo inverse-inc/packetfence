5de772aa6c338093e3d6ad8eb488591c254e6b6a
James Rouzier <jrouzier@inverse.ca>
2016-11-15 12:00:48 -0500

    Use https

--- lib/pf/Authentication/Source/BillingSource.pm
+++ lib/pf/Authentication/Source/BillingSource.pm
@@ -109,7 +109,7 @@ sub cancel_url {
 sub _build_base_path {
     my ($self) = @_;
     my $id     = $self->id;
-    my $base_path = "http://$fqdn/billing/$id";
+    my $base_path = "https://$fqdn/billing/$id";
     return $base_path;
 }
 
