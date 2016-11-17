d850c161f85d458f1f2d161b0bf5f426c3fe952c
James Rouzier <jrouzier@inverse.ca>
2016-11-17 10:53:59 -0500

    Support list that have a label and remove the temporary field used for rendering

--- html/pfappserver/lib/pfappserver/Form/Field/DynamicList.pm
+++ html/pfappserver/lib/pfappserver/Form/Field/DynamicList.pm
@@ -58,14 +58,14 @@ sub build_init_contains {
 
 sub append_add_button {
     my ($self) = @_;
-    my $index = $self->index;
-    $self->add_extra(1);
-    my $extra_field = $self->field($index);
+    my $extra_field = $self->_add_extra(999);
     set_disabled($extra_field);
-    $extra_field->name(999);
     my $id = $self->id;
     my $button_text = "Add " . $extra_field->label;
     my $content = $extra_field->render;
+    # remove extra result & field, now that it's rendered
+    $self->result->_pop_result;
+    $self->_pop_field;
     my $template_id = $self->template_id;
     my $template_target = $self->template_target;
     my $target = $self->target_id;
@@ -75,7 +75,7 @@ sub append_add_button {
         <div id="$template_id" class="hidden">$content</div>
         <div>
             <div class="controls">
-                <a data-toggle="dynamic-list" data-target="#${target}" data-template-parent="#$template_target" data-base-id="$id" class="btn">$button_text</a>
+                <a data-toggle="dynamic-list" data-target="${target}" data-template-parent="#$template_target" data-base-id="$id" class="btn">$button_text</a>
             </div>
         </div>
     </div>
@@ -104,7 +104,11 @@ sub template_target {
 
 sub target_id {
     my ($self) = @_;
-    return escape_jquery_id($self->id);
+    my $target_id = '#' . escape_jquery_id($self->id);
+    if ($self->do_label) {
+       $target_id .= " .controls:first";
+    }
+    return $target_id;
 }
 
 sub escape_jquery_id {
