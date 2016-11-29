8bdbfdbfea9d4caa26e888cbe373180956fe5aa6
Derek Wuelfrath <dwuelfrath@inverse.ca>
2016-11-29 11:34:12 -0500

    Added active/passive synchronization jobs to cron file

--- packetfence.cron.d
+++ packetfence.cron.d
@@ -3,5 +3,13 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+# Database backup and maintenance script
 30 00 * * * root /usr/local/pf/addons/database-backup-and-maintenance.sh
+
+# Active / Passive synchronization
+#SYNC_SERVER_IP=X.X.X.X
+# Synchronization for /usr/local/pf
+#*/15 * * * *  pf [ ! -d /var/lib/mysql/pf ] && rsync -e 'ssh -i /usr/local/pf/.ssh/packetfence-sync' --delete -logDprtuv --exclude=logs/* --exclude=var/* --exclude=.ssh/* $SYNC_SERVER_IP:/usr/local/pf /usr/local
+# Synchronization for /usr/local/fingerbank
+#*/15 * * * *  pf [ ! -d /var/lib/mysql/pf ] && rsync -e 'ssh -i /usr/local/pf/.ssh/packetfence-sync' --delete -logDprtuv --exclude=logs/* $SYNC_SERVER_IP:/usr/local/fingerbank /usr/local
 
