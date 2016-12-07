4def61ae588fd321c889b3ef7c982872c368a6c5
Derek Wuelfrath <dwuelfrath@inverse.ca>
2016-12-07 13:48:26 -0500

    Fixayde tipau ande Booooobbbeee iz naow Zayme

--- html/pfappserver/lib/pfappserver/Model/DB.pm
+++ html/pfappserver/lib/pfappserver/Model/DB.pm
@@ -157,15 +157,15 @@ sub create {
 
 =head2 secureInstallation
 
-Intented to integrate the "/usr/bin/mysql_secure_installation" steps
+Intended to integrate the "/usr/bin/mysql_secure_installation" steps
 
-1. Set a password for the database “root” user (different from the Linux root user!), which is blank by default;
+1. Set a password for the database "root" user (different from the Linux root user!), which is blank by default;
 
-2. Delete “anonymous” users, i.e. users with the empty string as user name;
+2. Delete "anonymous" users, i.e. users with the empty string as user name;
 
 3. Ensure the root user can not log in remotely;
 
-4. Remove the database named “test”;
+4. Remove the database named "test";
 
 5. Flush the privileges tables, i.e. ensure that the changes to user access applied in the previous steps are committed immediately.
 
@@ -177,7 +177,7 @@ sub secureInstallation {
 
     my ($status, $status_msg);
 
-    # 1. Set a password for the database “root” user (different from the Linux root user!), which is blank by default;
+    # 1. Set a password for the database "root" user (different from the Linux root user!), which is blank by default;
     my $sql_query = "UPDATE mysql.user SET Password=PASSWORD(?) WHERE User=?";
     $dbHandler->do($sql_query, undef, $root_password, $root_user);
     if ( $DBI::errstr ) {
@@ -186,7 +186,7 @@ sub secureInstallation {
         return ($STATUS::INTERNAL_SERVER_ERROR, $status_msg);
     }
 
-    # 2. Delete “anonymous” users, i.e. users with the empty string as user name;
+    # 2. Delete "anonymous" users, i.e. users with the empty string as user name;
     $dbHandler->do("DELETE FROM mysql.user WHERE User=''");
     if ( $DBI::errstr ) {
         $status_msg = ["Error deleting MySQL anonymous users" ];
@@ -203,7 +203,7 @@ sub secureInstallation {
         return ($STATUS::INTERNAL_SERVER_ERROR, $status_msg);
     }
 
-    # 4. Remove the database named “test”;
+    # 4. Remove the database named "test";
     $dbHandler->do("DROP DATABASE IF EXISTS test");
     if ( $DBI::errstr ) {
         $status_msg = ["Error dropping the 'test' database" ];
