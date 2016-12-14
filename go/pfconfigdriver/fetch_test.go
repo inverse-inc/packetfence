48423808b741a61a048d5fe15c2da0255c5bfb0f
Julien Semaan <jul.semaan@gmail.com>
2016-12-14 09:46:58 -0500

    godoc for benchmarks

--- go/pfconfigdriver/fetch_test.go
+++ go/pfconfigdriver/fetch_test.go
@@ -57,48 +57,57 @@ func TestFetchDecodeSocket(t *testing.T) {
 
 }
 
+// fetches resource::fqdn requesting Sereal encoding for the reply
 func BenchmarkFetchSocketSerealSimple(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"resource::fqdn"}`+"\n")
 	}
 }
 
+// fetches resource::fqdn requesting JSON encoding for the reply
 func BenchmarkFetchSocketJsonSimple(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"resource::fqdn", "encoding":"json"}`+"\n")
 	}
 }
 
+// fetches interfaces requesting Sereal encoding for the reply
 func BenchmarkFetchSocketSerealComplexWithToJson(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"interfaces"}`+"\n")
 	}
 }
 
+// fetches interfaces requesting JSON encoding for the reply.
+// Some of the objects of that namespace need to be transformed from Perl objects to JSON.
 func BenchmarkFetchSocketJsonComplexWithToJson(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"interfaces", "encoding":"json"}`+"\n")
 	}
 }
 
+// fetches config::Pf requesting Sereal encoding for the reply
 func BenchmarkFetchSocketSerealComplex(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"config::Pf"}`+"\n")
 	}
 }
 
+// fetches config::Pf requesting JSON encoding for the reply
 func BenchmarkFetchSocketJsonComplex(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"element", "key":"config::Pf", "encoding":"json"}`+"\n")
 	}
 }
 
+// fetches the subnamespace config::Pf;general requesting Sereal encoding for the reply
 func BenchmarkFetchSocketSerealSubNamespace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"hash_element", "key":"config::Pf;general"}`+"\n")
 	}
 }
 
+// fetches the subnamespace config::Pf;general requesting JSON encoding for the reply.
 func BenchmarkFetchSocketJsonSubNamespace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FetchSocket(ctx, `{"method":"hash_element", "key":"config::Pf;general", "encoding":"json"}`+"\n")
