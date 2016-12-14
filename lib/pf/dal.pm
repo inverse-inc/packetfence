e69011b2cf8b44d080d4826feac37a1637986ad0
James Rouzier <jrouzier@inverse.ca>
2016-12-14 14:26:46 -0500

    Use pf::dal::iterator

--- lib/pf/dal.pm
+++ lib/pf/dal.pm
@@ -17,23 +17,99 @@ use warnings;
 use pf::db;
 use pf::log;
 use SQL::Abstract::More;
+use pf::dal::iterator;
+
+=head2 new
+
+Create a new pf::dal object
+
+=cut
 
 sub new {
     my ($proto, $args) = @_;
     my $class = ref($proto) || $proto;
-    my %data = %{$args // {}};
-    $data{__old_data} = {%data};
+    my $defaults = $class->_defaults;
+    my %data = (%$defaults, %{$args // {}});
     return bless \%data, $class;
 }
 
-sub get_dbh {
-    db_connect();
+=head2 new_from_table
+
+Create a new pf::dal object marking it that it came from the database
+
+=cut
+
+sub new_from_table {
+    my ($proto, $args) = @_;
+    my $class = ref($proto) || $proto;
+    my %data = %{$args // {}};
+    $data{__old_data} = {%data};
+    $data{__from_table} = 1;
+    return bless \%data, $class;
 }
+
+=head2 get_dbh
+
+Get the database handle
+
+=cut
+
+sub get_dbh {
+    get_db_handle();
+}
+
+=head2 _default
+
+Return the default values for pf::dal object.
+Should be overridden in sub class
+
+=cut
+
+sub _defaults {
+    return {};
+}
+
+=head2 db_execute
+
+Execute the sql query with it's bind parameters
+
+=cut
 
 sub db_execute {
     my ($self, $sql, @params) = @_;
-    my $dbh = $self->get_dbh;
+    my $attempts = 3;
+    my $logger = $self->logger;
+    while ($attempts) {
+        my $dbh = $self->get_dbh;
+        unless ($dbh) {
+            $logger->error("Cannot connect to database retrying connection");
+            next;
+        }
+        $logger->trace(sub{"preparing statement query $sql"});
+        my $sth = $dbh->prepare_cached($sql);
+        unless ($sth && $sth->execute(@params)) {
+            my $err = $dbh->err;
+            my $errstr = $dbh->errstr;
+            if ($err < 2000) {
+                $logger->error("database query failed with non retryable error: $errstr (errno: $err)");
+                last;
+            }
+            # retry client errors
+            $logger->warn("database query failed with: $errstr (errno: $err), will try again");
+            next;
+        }
+        return $sth;
+    } continue {
+        $attempts--;
+    }
+    return undef;
 }
+
+=head2 find
+
+Find the pf::dal object by it's primaries keys
+
+=cut
 
 sub find {
     my ($proto, @ids) = @_;
@@ -46,21 +122,32 @@ sub find {
     return $proto->new($row);
 }
 
+=head2 search
+
+Search for pf::dal using SQL::Abstract::More syntax
+
+=cut
+
 sub search {
     my ($proto, $where, $extra) = @_;
-    my $fields = $proto->field_names;
+    my $class = ref($proto) || $proto;
     my $sqla = SQL::Abstract::More->new;
-    my($stmt, @bind) = $sql->select(
-        -columns => $fields,
-        -from    => $self->table,
-        -where   => $where,
+    my($stmt, @bind) = $sqla->select(
+        -columns => $proto->field_names,
+        -from    => $proto->table,
+        -where   => $where // {},
         %{$extra // {}},
     );
-    my $dbh = db_connect();
-    my $sth = $dbh->prepare_cached($sql);
-    my $results = $sth->execute(@ids);
-    my $row = $sth->fetchrow_hashref;
+    my $sth = $proto->db_execute($stmt, @bind);
+    return undef unless defined $sth;
+    return pf::dal::iterator->new({sth => $sth, class => $class});
 }
+
+=head2 save
+
+Save the pf::dal object in the database
+
+=cut
 
 sub save {
     my ($self) = @_;
