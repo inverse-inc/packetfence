25b37e074489989a666533d1a91cf8cf6e31c171
Julien Semaan <jul.semaan@gmail.com>
2016-12-14 09:02:29 -0500

    added process PID to logging

--- go/logging/logging.go
+++ go/logging/logging.go
@@ -5,9 +5,11 @@ import (
 	log "github.com/inconshreveable/log15"
 	"github.com/nu7hatch/gouuid"
 	"os"
+	"strconv"
 )
 
 const requestUuidKey = "request-uuid"
+const processPidKey = "pid"
 
 var srvlog = initSrvlog()
 
@@ -20,12 +22,14 @@ func initSrvlog() log.Logger {
 
 // Get the current logger for the request (includes the request UUID)
 func Logger(ctx context.Context) log.Logger {
-	return srvlog.New(requestUuidKey, ctx.Value(requestUuidKey).(string))
+	return srvlog.New(requestUuidKey, ctx.Value(requestUuidKey).(string), processPidKey, ctx.Value(processPidKey).(string))
 }
 
 // Grab a context that includes a UUID of the request for logging purposes
 func NewContext(ctx context.Context) context.Context {
 	u, _ := uuid.NewV4()
 	uStr := u.String()
-	return context.WithValue(ctx, requestUuidKey, uStr)
+	ctx = context.WithValue(ctx, requestUuidKey, uStr)
+	ctx = context.WithValue(ctx, processPidKey, strconv.Itoa(os.Getpid()))
+	return ctx
 }
