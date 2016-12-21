0f7f3526b37d687834524e43ab34146f072cafb0
Julien Semaan <jul.semaan@gmail.com>
2016-12-21 13:22:23 -0500

    missing perldoc

--- lib/pf/util/dns.pm
+++ lib/pf/util/dns.pm
@@ -1,5 +1,17 @@
 package pf::util::dns;
 
+=head1 NAME
+
+pf::util::dns
+
+=cut
+
+=head1 DESCRIPTION
+
+Util functions for DNS
+
+=cut
+
 use strict;
 use warnings;
 
@@ -7,6 +19,16 @@ use pf::constants;
 use pfconfig::cached_hash;
 
 tie our %passthroughs, 'pfconfig::cached_hash', 'resource::passthroughs';
+
+=head2 matches_passthrough
+
+Whether or not a domain matches a configured DNS passthrough
+
+Returns 2 values
+- Whether or not it matched
+- List of ports that should be opened for this domain
+
+=cut
 
 sub matches_passthrough {
     my ($domain) = @_;
@@ -34,4 +56,43 @@ sub matches_passthrough {
     return ($FALSE, []);
 }
 
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+Copyright (C) 2005 Kevin Amorin
+
+Copyright (C) 2005 David LaPorte
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
 1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
