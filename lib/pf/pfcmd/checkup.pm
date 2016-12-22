6948833ff3fe632cb703828428b41fc9e3a05af0
Julien Semaan <jul.semaan@gmail.com>
2016-12-22 13:50:45 -0500

    add error when version check dies

--- lib/pf/pfcmd/checkup.pm
+++ lib/pf/pfcmd/checkup.pm
@@ -1102,7 +1102,7 @@ sub db_check_version {
         }
     };
     if($@) {
-        add_problem($FATAL, "Cannot connect to database to check schema version");
+        add_problem($FATAL, "Cannot connect to database to check schema version: $@");
     }
 }
 
