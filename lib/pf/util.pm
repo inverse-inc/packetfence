9efd1109b94a34ef94c1aeff7b70a1b7bea873d2
Durand Fabrice <fdurand@inverse.ca>
2016-12-23 10:51:42 -0500

    unpretty_bandwidth

--- lib/pf/util.pm
+++ lib/pf/util.pm
@@ -740,9 +740,21 @@ Returns the bandwidth in bytes depending of the incombing unit
 
 =cut
 
-sub unpretty_bandwidth {
-    my ($bw,$unit) = @_;
 
+sub unpretty_bandwidth {
+    my (@bw) = @_;
+
+    my ($bw,$unit);
+
+    if (!defined($bw[1])) {
+        if ($bw[0] =~ /(\d+)(\w+)/) {
+            $bw = $1;
+            $unit = $2;
+        }
+    } else {
+        $bw = $bw[0];
+        $unit = $bw[1];
+    }
     # Check what units we have, and multiple by 1024 exponent something
     if ($unit eq 'PB') {
         return $bw * 1024**5;
