e1b9748300d88087aa42b0a05c8428712bdd4739
Derek Wuelfrath <dwuelfrath@inverse.ca>
2017-01-09 11:59:38 -0500

    Added Twilio authentication source

---
+++ html/pfappserver/lib/pfappserver/Form/Config/Authentication/Source/Twilio.pm
@@ -0,0 +1,99 @@
+package pfappserver::Form::Config::Authentication::Source::Twilio;
+
+=head1 NAME
+
+pfappserver::Form::Config::Authentication::Source::Twilio
+
+=cut
+
+=head1 DESCRIPTION
+
+Form definition to create or update a Twilio authentication source.
+
+=cut
+
+use strict;
+use warnings;
+
+use HTML::FormHandler::Moose;
+
+extends 'pfappserver::Form::Config::Authentication::Source';
+with 'pfappserver::Base::Form::Role::Help';
+
+has_field 'api_url' => (
+    type        => 'Text',
+    label       => 'API URL',
+    required    => 1,
+    tags        => {
+        after_element   => \&help,
+        help            => 'POST URL of the Twilio API',
+    },
+    element_attr    => {
+        placeholder     => pf::Authentication::Source::TwilioSource->meta->get_attribute('api_url')->default,
+    },
+    default         => pf::Authentication::Source::TwilioSource->meta->get_attribute('api_url')->default,
+);
+
+has_field 'account_sid' => (
+    type        => 'Text',
+    label       => 'Account SID',
+    required    => 1,
+    tags        => {
+        after_element   => \&help,
+        help            => 'Twilio Account SID',
+    },
+);
+
+has_field 'auth_token' => (
+    type        => 'Text',
+    label       => 'Auth Token',
+    required    => 1,
+    tags        => {
+        after_element   => \&help,
+        help            => 'Twilio Auth Token',
+    },
+);
+
+has_field 'twilio_phone_number' => (
+    type            => 'Text',
+    label           => 'Phone Number (From)',
+    tags            => {
+        after_element   => \&help,
+        help            => 'Twilio provided phone number which will be used as a From',
+    },
+    element_attr    => {
+        placeholder     => pf::Authentication::Source::TwilioSource->meta->get_attribute('twilio_phone_number')->default,
+    },
+    default         => pf::Authentication::Source::TwilioSource->meta->get_attribute('twilio_phone_number')->default,
+);
+
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
---
+++ lib/pf/Authentication/Source/TwilioSource.pm
@@ -0,0 +1,119 @@
+package pf::Authentication::Source::TwilioSource;
+
+=head1 NAME
+
+pf::Authentication::Source::TwilioSource
+
+=head1 DESCRIPTION
+
+=cut
+
+use pf::Authentication::constants;
+use pf::constants qw($TRUE $FALSE);
+use pf::log;
+
+use Moose;
+extends 'pf::Authentication::Source';
+
+has '+type'                     => (default => 'Twilio');
+has '+class'                    => (isa => 'Str', is => 'ro', default => 'external');
+has '+dynamic_routing_module'   => (is => 'rw', default => 'Authentication::SMS');
+has 'api_url'                   => (isa=> 'Maybe[Str]', is => 'rw', default => 'https://api.twilio.com/2010-04-01/Accounts/$account_sid/Messages.json');
+has 'account_sid'               => (isa => 'Str', is => 'rw');
+has 'auth_token'                => (isa => 'Str', is => 'rw');
+has 'twilio_phone_number'       => (isa => 'Str', is => 'rw', default => '+15555551234');
+
+
+=head2 available_rule_classes
+
+Only allow 'authentication' rules
+
+=cut
+
+sub available_rule_classes {
+    return [ grep { $_ ne $Rules::ADMIN } @Rules::CLASSES ];
+}
+
+
+=head2 available_actions
+
+Only allow 'authentication' actions
+
+=cut
+
+sub available_actions {
+    my @actions = map( { @$_ } $Actions::ACTIONS{$Rules::AUTH});
+    return \@actions;
+}
+
+
+=head2 available_attributes
+
+Allow to make a condition on the provided phone number
+
+=cut
+
+sub available_attributes {
+  my $self = shift;
+
+  my $super_attributes = $self->SUPER::available_attributes;
+  my $own_attributes = [{ value => "phonenumber", type => $Conditions::SUBSTRING }];
+
+  return [@$super_attributes, @$own_attributes];
+}
+
+
+=head2 mandatoryFields
+
+List of mandatory fields for this source
+
+=cut
+
+sub mandatoryFields {
+    return qw(telephone);
+}
+
+
+=head2 match_in_subclass
+
+=cut
+
+sub match_in_subclass {
+    my ($self, $params, $rule, $own_conditions, $matching_conditions) = @_;
+    return $params->{'username'};
+}
+
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2016 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
