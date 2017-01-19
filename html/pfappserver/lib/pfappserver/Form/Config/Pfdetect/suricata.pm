8ba5cca45cbd858710a8caf193a3ac91a41c58c2
James Rouzier <jrouzier@inverse.ca>
2017-01-19 17:01:10 -0500

    Updated Copyright

--- html/pfappserver/lib/pfappserver/Form/Config/Pfdetect/suricata.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Pfdetect/suricata.pm
@@ -19,7 +19,7 @@ extends 'pfappserver::Form::Config::Pfdetect';
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2016 Inverse inc.
+Copyright (C) 2005-2017 Inverse inc.
 
 =head1 LICENSE
 
--- html/pfappserver/lib/pfappserver/Form/Field/Regex.pm
+++ html/pfappserver/lib/pfappserver/Form/Field/Regex.pm
@@ -47,7 +47,7 @@ apply
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2016 Inverse inc.
+Copyright (C) 2005-2017 Inverse inc.
 
 =head1 LICENSE
 
