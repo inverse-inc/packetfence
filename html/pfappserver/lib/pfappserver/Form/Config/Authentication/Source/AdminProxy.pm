1fd4832e6e62311820f77bc3912fb5bbef927421
James Rouzier <jrouzier@inverse.ca>
2017-02-07 13:57:10 -0500

    Add proxy_addresses help and validate function

--- html/pfappserver/lib/pfappserver/Form/Config/Authentication/Source/AdminProxy.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Authentication/Source/AdminProxy.pm
@@ -15,15 +15,21 @@ pfappserver::Form::Config::Authentication::Source::AdminProxy
 use strict;
 use warnings;
 use HTML::FormHandler::Moose;
+use pf::util qw(valid_ip);
 extends 'pfappserver::Form::Config::Authentication::Source';
+with 'pfappserver::Base::Form::Role::Help';
 
 has_block definition => (
     render_list => [qw(proxy_addresses user_header group_header)],
 );
 
 has_field 'proxy_addresses' => (
-    type => 'Text',
-    required => 1
+    type => 'TextArea',
+    required => 1,
+    tags => {
+        after_element => \&help,
+        help => 'A comma seperated list of IP Address',
+    }
 );
 
 has_field 'user_header' => (
@@ -35,6 +41,24 @@ has_field 'group_header' => (
     type => 'Text',
     required => 1
 );
+
+=head2 validate
+
+validate
+
+=cut
+
+sub validate {
+    my ($self) = @_;
+    my $proxy_addresses_field = $self->field('proxy_addresses');
+    my $proxy_addresses = $proxy_addresses_field->value;
+    for my $ip (split(/\s*,\s*/, $proxy_addresses)) {
+        unless (valid_ip($ip)) {
+            $proxy_addresses_field->add_error("$ip is invalid");
+        }
+    }
+    return;
+}
 
 =head1 AUTHOR
 
