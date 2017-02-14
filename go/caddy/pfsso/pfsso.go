cec8666612bc18f58be4fb0e12c671715aa38a4b
Julien Semaan <jul.semaan@gmail.com>
2017-02-14 13:00:40 -0500

    basic start handler

--- go/caddy/pfsso/pfsso.go
+++ go/caddy/pfsso/pfsso.go
@@ -1,11 +1,15 @@
 package pfsso
 
 import (
+	"context"
 	"fmt"
 	"github.com/fingerbank/processor/log"
 	"github.com/fingerbank/processor/statsd"
 	"github.com/inverse-inc/packetfence/go/caddy/caddy"
 	"github.com/inverse-inc/packetfence/go/caddy/caddy/caddyhttp/httpserver"
+	"github.com/inverse-inc/packetfence/go/firewallsso"
+	pflog "github.com/inverse-inc/packetfence/go/logging"
+	"github.com/inverse-inc/packetfence/go/pfconfigdriver"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"net/http"
@@ -25,12 +29,41 @@ func (h PfssoHandler) handlePing(w http.ResponseWriter, r *http.Request, p httpr
 	io.WriteString(w, "pong")
 }
 
+func (h PfssoHandler) handleStart(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	ctx := r.Context()
+	defer statsd.NewStatsDTiming(ctx).Send("PfssoHandler.handleStart")
+	for _, firewall := range h.firewalls {
+		firewallsso.ExecuteStart(ctx, firewall, map[string]string{"ip": "172.20.0.1", "role": "default", "mac": "00:11:22:33:44:55", "username": "lzammit"}, 0)
+	}
+
+	io.WriteString(w, "handled")
+}
+
 func setup(c *caddy.Controller) error {
+	ctx := context.Background()
 
 	pfsso := PfssoHandler{}
+
+	var firewallIds pfconfigdriver.PfconfigKeys
+	firewallIds.PfconfigNS = "config::Firewall_SSO"
+	pfconfigdriver.FetchDecodeSocketStruct(ctx, &firewallIds)
+
+	fssoFactory := firewallsso.NewFactory(ctx)
+	for i := range firewallIds.Keys {
+		firewallId := firewallIds.Keys[i]
+		log.LoggerWContext(ctx).Info(fmt.Sprintf("Adding firewall %s", firewallId))
+
+		firewall, err := fssoFactory.Instantiate(ctx, firewallId)
+		if err != nil {
+			log.LoggerWContext(ctx).Error(fmt.Sprintf("Cannot instantiate firewall %s. Ignoring it.", firewallId))
+		} else {
+			pfsso.firewalls = append(pfsso.firewalls, firewall)
+		}
+	}
+
 	router := httprouter.New()
 	router.GET("/ping", pfsso.handlePing)
-	//router.POST("/pfsso/start", pfsso.handleStart)
+	router.POST("/pfsso/start", pfsso.handleStart)
 	//router.POST("/pfsso/stop", pfsso.handleStop)
 
 	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
@@ -43,12 +76,16 @@ func setup(c *caddy.Controller) error {
 }
 
 type PfssoHandler struct {
-	Next   httpserver.Handler
-	router *httprouter.Router
+	Next      httpserver.Handler
+	router    *httprouter.Router
+	firewalls []firewallsso.FirewallSSOInt
 }
 
 func (h PfssoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 	ctx := r.Context()
+
+	//TODO: rework this to use the fingerbank processor logger which is cleaner
+	r = r.WithContext(pflog.NewContext(ctx))
 
 	defer func() {
 		if r := recover(); r != nil {
