c73af356551a6b59d0a0bf7a45e823cb8db0524a
Durand Fabrice <fdurand@inverse.ca>
2017-02-15 11:44:40 -0500

    Cleaned code and add static

--- go/httpdispatcher/proxy.go
+++ go/httpdispatcher/proxy.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -42,7 +41,7 @@ var passThrough *passthrough
 // newProxy creates a new instance of proxy.
 // It sets request logger using rLogPath as output file or os.Stdout by default.
 // If whitePath of blackPath is not empty they are parsed to set endpoint lists.
-func newProxy(rLogPath string, whitePath string, blackPath string) *proxy {
+func newProxy(rLogPath string) *proxy {
 	var p proxy
 	var l *log.Logger
 
@@ -58,54 +57,20 @@ func newProxy(rLogPath string, whitePath string, blackPath string) *proxy {
 	}
 	p.requestLogger = l
 
-	if whitePath != "" {
-		err := p.addEndpointListFromFile(whitePath, true)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-	if blackPath != "" {
-		err := p.addEndpointListFromFile(blackPath, false)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
 	passThrough = newProxyPassthrough()
 	passThrough.readConfig()
 	spew.Dump(passThrough)
 	return &p
 }
 
-// addToEndpointListFromFile reads file line by line and calls
-// addToEndpointList for each
-// use t to choose list type: true for whitelist false for blacklist
-func (p *proxy) addEndpointListFromFile(path string, t bool) error {
-	f, err := os.Open(path)
-	if err == nil {
-		defer f.Close()
-		s := bufio.NewScanner(f)
-		for s.Scan() {
-			err = p.addToEndpointList(s.Text(), t)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return err
-}
-
 // addToEndpointList compiles regex and adds it to an endpointList
 // if regex is valid
 // use t to choose list type: true for whitelist false for blacklist
-func (p *proxy) addToEndpointList(r string, t bool) error {
+func (p *proxy) addToEndpointList(r string) error {
 	rgx, err := regexp.Compile(r)
 	if err == nil {
 		p.mutex.Lock()
-		if t {
-			p.endpointWhiteList = append(p.endpointWhiteList, rgx)
-		} else {
-			p.endpointBlackList = append(p.endpointBlackList, rgx)
-		}
+		p.endpointBlackList = append(p.endpointBlackList, rgx)
 		p.mutex.Unlock()
 	}
 	return err
@@ -124,15 +89,7 @@ func (p *proxy) checkEndpointList(e string) bool {
 		}
 	}
 
-	if p.endpointWhiteList == nil {
-		return true
-	}
-	for _, rgx := range p.endpointWhiteList {
-		if rgx.MatchString(e) {
-			return true
-		}
-	}
-	return false
+	return true
 }
 
 // ServeHTTP satisfy HandlerFunc interface and
@@ -180,7 +137,27 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Location", passThrough.PortalURL.String())
 		w.WriteHeader(http.StatusFound)
 		t := template.New("foo")
-		t, _ = t.Parse("<html><head><title>302 Moved Temporarily</title></head><body><h1>Moved</h1><p>The document has moved <a href=\"{{.PortalURL.String}}\">here</a>.</p><!--<?xml version=\"1.0\" encoding=\"UTF-8\"?><WISPAccessGatewayParam xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.wballiance.net/wispr/wispr_2_0.xsd\"><Redirect><MessageType>100</MessageType><ResponseCode>0</ResponseCode><AccessProcedure>1.0</AccessProcedure><VersionLow>1.0</VersionLow><VersionHigh>2.0</VersionHigh><AccessLocation>CDATA[[isocc=,cc=,ac=,network=PacketFence,]]</AccessLocation><LocationName>CDATA[[PacketFence]]</LocationName><LoginURL>{{.WisprUrl.String}}</LoginURL></Redirect></WISPAccessGatewayParam>--></body></html>")
+		t, _ = t.Parse(`
+			<html>
+			  <head><title>302 Moved Temporarily</title></head>
+			  <body>
+				  <h1>Moved</h1>
+					  <p>The document has moved <a href=\"{{.PortalURL.String}}\">here</a>.</p>
+						<!--<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+						  <WISPAccessGatewayParam xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.wballiance.net/wispr/wispr_2_0.xsd\">
+							  <Redirect>
+								  <MessageType>100</MessageType>
+									<ResponseCode>0</ResponseCode>
+									<AccessProcedure>1.0</AccessProcedure>
+									<VersionLow>1.0</VersionLow>
+									<VersionHigh>2.0</VersionHigh>
+									<AccessLocation>CDATA[[isocc=,cc=,ac=,network=PacketFence,]]</AccessLocation>
+									<LocationName>CDATA[[PacketFence]]</LocationName>
+									<LoginURL>{{.WisprUrl.String}}</LoginURL>
+							  </Redirect>
+							</WISPAccessGatewayParam>-->
+					</body>
+				</html>`)
 		t.Execute(w, passThrough)
 
 		//spew.Dump(r)
@@ -188,11 +165,11 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		//p.requestLogger.Println(host, "REDIRECT")
 		return
 	}
-	// if !p.checkEndpointList(host) {
-	// 	p.requestLogger.Println(host, "FORBIDDEN")
-	// 	w.WriteHeader(http.StatusForbidden)
-	// 	return
-	// }
+	if !p.checkEndpointList(host) {
+		p.requestLogger.Println(host, "FORBIDDEN")
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 	//p.requestLogger.Println(host, "REVERSE")
 	rp := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
@@ -205,8 +182,8 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // run add localhost to blacklist and launch proxy
 func (p *proxy) run(port string) {
-	p.addToEndpointList("localhost", false)
-	p.addToEndpointList("127.0.0.1", false)
+	p.addToEndpointList("localhost")
+	p.addToEndpointList("127.0.0.1")
 	log.Fatal(http.ListenAndServe(":"+port, p))
 }
 
@@ -248,7 +225,7 @@ func (p *passthrough) readConfig() {
 	portalURL.Scheme = scheme
 
 	wisprURL.Host = general.Hostname + "." + general.Domain
-	wisprURL.Path = "/captive-portal"
+	wisprURL.Path = "/wispr"
 	wisprURL.Scheme = scheme
 
 	p.WisprURL = &wisprURL
