7e0e91d77a065ef4617f857c527248ae46a9fef1
Julien Semaan <jul.semaan@gmail.com>
2017-02-15 18:06:08 -0500

    basic test of a valid resource

--- go/pfconfigdriver/pool_test.go
+++ go/pfconfigdriver/pool_test.go
@@ -2,7 +2,9 @@ package pfconfigdriver
 
 import (
 	"github.com/davecgh/go-spew/spew"
+	"github.com/fingerbank/processor/sharedutils"
 	"os"
+	"os/exec"
 	"testing"
 	"time"
 )
@@ -10,18 +12,38 @@ import (
 func TestLoadResource(t *testing.T) {
 	rp := NewResourcePool(ctx)
 	gen := PfConfGeneral{}
-	rp.LoadResource(ctx, &gen, false)
+	rp.LoadResource(ctx, &gen, true)
 
 	expected := "pfdemo.org"
 	if gen.Domain != expected {
 		t.Error("Resource domain wasn't loaded correctly through resource pool. Got %s instead of %s", gen.Domain, expected)
 	}
 
-	if _, ok := rp.loadedResources["pfconfigdriver.PfConfGeneral"]; !ok {
+	res, ok := rp.loadedResources["pfconfigdriver.PfConfGeneral"]
+	if !ok {
 		t.Error("The loaded resource wasn't stored in the pool")
+		return
 	}
 
-	spew.Dump(rp)
+	cmd := exec.Command("sed", "-i.bak", "s/domain=pfdemo.org/domain=zammitcorp.com/g", "/usr/local/pf/t/data/pf.conf")
+	err := cmd.Run()
+	sharedutils.CheckError(err)
+
+	// Expire data in pfconfig
+	FetchSocket(ctx, `{"method":"expire", "encoding":"json", "namespace":"config::Pf"}`+"\n")
+
+	os.Chtimes(res.controlFile(), time.Now(), time.Now())
+
+	rp.LoadResource(ctx, &gen, true)
+	spew.Dump(gen)
+
+	// Restore the prestine version of pf.conf
+	err = os.Rename("/usr/local/pf/t/data/pf.conf.bak", "/usr/local/pf/t/data/pf.conf")
+	sharedutils.CheckError(err)
+
+	// Reset the pfconfig namespace after putting back the old data
+	FetchSocket(ctx, `{"method":"expire", "encoding":"json", "namespace":"config::Pf"}`+"\n")
+
 }
 
 func TestResourceIsValid(t *testing.T) {
