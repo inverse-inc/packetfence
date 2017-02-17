be06a12c705486090298a4608de1d44216262af9
Julien Semaan <jul.semaan@gmail.com>
2017-02-17 13:46:06 -0500

    always call resource pool in sso factory

--- go/firewallsso/factory.go
+++ go/firewallsso/factory.go
@@ -22,12 +22,10 @@ func NewFactory(ctx context.Context) Factory {
 }
 
 // Instantiate a new FirewallSSO given its configuration ID in PacketFence
-// TODO: This currently calls FetchDecodeSocket twice which generates 2 calls to pfconfig
-//       This should be reworked so that only 1 call is done and the same payload is used to determine the type and to create the struct
 func (f *Factory) Instantiate(ctx context.Context, id string, firstLoad bool) (FirewallSSOInt, error) {
 	firewall := FirewallSSO{}
 	firewall.PfconfigHashNS = id
-	err := pfconfigdriver.FetchDecodeSocket(ctx, &firewall)
+	err := pfconfigdriver.GlobalPfconfigResourcePool.LoadResource(ctx, &firewall)
 	if err != nil {
 		return nil, err
 	}
