4d6563b870f98b68cd06f085df47e0987cac46a4
James Rouzier <jrouzier@inverse.ca>
2017-02-21 15:39:28 -0500

    Format and add documetation and constructors

--- go/filter_client/filter_client.go
+++ go/filter_client/filter_client.go
@@ -1,95 +1,117 @@
 package filter_client
 
 import (
-    "bufio"
-    "encoding/json"
-    "net"
-    "fmt"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
 )
 
+const default_socket_path string = "/usr/local/pf/var/run/pffilter.sock"
+
+// Represents the json message to send to the pffilter service
 type Request struct {
-    Method string `json:"method"`
-    Params interface{} `json:"params"`
+	Method string      `json:"method"`
+	Params interface{} `json:"params"`
 }
 
+// Represents the json error received from the pffilter service
 type ClientError struct {
-    Code int `json:"code"`
-    Message string `json:message`
-    Data interface{} `json:data`
+	Code    int         `json:"code"`
+	Message string      `json:message`
+	Data    interface{} `json:data`
 }
 
+// Represents the json repsonse received from the pffilter service
 type Response struct {
-    Result *interface{} `json:"result"`
-    Error *ClientError `json:"error"`
+	Result *interface{} `json:"result"`
+	Error  *ClientError `json:"error"`
 }
 
+// The Client struct
 type Client struct {
-    SocketPath string
+	SocketPath string
 }
 
+// The Client constructor
+func NewClient() Client {
+	return NewClientWithPath(default_socket_path)
+}
 
+// The Client constructor with a path
+func NewClientWithPath(path string) Client {
+	return Client{SocketPath: path}
+}
+
+// Sends a filter_profile message to the pffilter service
 func (c *Client) FilterProfile(data interface{}) (interface{}, error) {
-    socket, err := net.Dial("unix", c.SocketPath)
-    if (err != nil) {
-        return nil, err
-    }
-    defer socket.Close()
-    return c.SendResponse("filter_profile", data, socket)
+	socket, err := net.Dial("unix", c.SocketPath)
+	if err != nil {
+		return nil, err
+	}
+	defer socket.Close()
+	return c.SendRequest("filter_profile", data, socket)
 }
 
-func (c *Client) accessFilter(filter string, scope string, data interface{}) (interface{}, error) {
-    socket, err := net.Dial("unix", c.SocketPath)
-    if (err != nil) {
-        return nil, err
-    }
-    defer socket.Close()
-    return c.SendResponse(filter, []interface{}{scope, data}, socket)
+// A generalize function for sending access filter messages
+func (c *Client) AccessFilter(filter string, scope string, data interface{}) (interface{}, error) {
+	socket, err := net.Dial("unix", c.SocketPath)
+	if err != nil {
+		return nil, err
+	}
+	defer socket.Close()
+	return c.SendRequest(filter, []interface{}{scope, data}, socket)
 }
 
+// Sends a filter_vlan message to the pffilter service
 func (c *Client) FilterVlan(scope string, data interface{}) (interface{}, error) {
-    return c.accessFilter("filter_vlan", scope, data)
+	return c.AccessFilter("filter_vlan", scope, data)
 }
 
+// Sends a filter_dhcp message to the pffilter service
 func (c *Client) FilterDhcp(scope string, data interface{}) (interface{}, error) {
-    return c.accessFilter("filter_dhcp", scope, data)
+	return c.AccessFilter("filter_dhcp", scope, data)
 }
 
+// Sends a filter_dns message to the pffilter service
 func (c *Client) FilterDns(scope string, data interface{}) (interface{}, error) {
-    return c.accessFilter("filter_dns", scope, data)
+	return c.AccessFilter("filter_dns", scope, data)
 }
 
+// Sends a filter_radius message to the pffilter service
 func (c *Client) FilterRadius(scope string, data interface{}) (interface{}, error) {
-    return c.accessFilter("filter_radius", scope, data)
+	return c.AccessFilter("filter_radius", scope, data)
 }
 
-func (c *Client) SendResponse(method string, params interface{}, conn net.Conn) (interface{}, error) {
-    request := Request{Method:method, Params: params}
-    b, err := json.Marshal(request)
-    if (err != nil) {
-        return nil, err
-    }
-    b = append(b,'\n')
-    _, err = conn.Write(b);
-    if (err != nil) {
-        return nil, err
-    }
-    reader := bufio.NewReader(conn)
-    b2, err := reader.ReadBytes('\n')
-    if (err != nil) {
-        return nil, err
-    }
-    var response Response
-    err = json.Unmarshal(b2, &response);
-    if (err != nil) {
-        return nil, err
-    }
-    if response.Error != nil {
-        return nil, fmt.Errorf("%s", response.Error.Message );
-    }
+// Send a request to the pffilter service
+func (c *Client) SendRequest(method string, params interface{}, conn net.Conn) (interface{}, error) {
+	request := Request{Method: method, Params: params}
+	b, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+	b = append(b, '\n')
+	_, err = conn.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	reader := bufio.NewReader(conn)
+	b2, err := reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+	var response Response
+	err = json.Unmarshal(b2, &response)
+	if err != nil {
+		return nil, err
+	}
+	if response.Error != nil {
+		return nil, fmt.Errorf("%s", response.Error.Message)
+	}
 
-    if response.Result == nil {
-        return nil, fmt.Errorf("No valid result returned")
-    }
+	if response.Result == nil {
+		return nil, fmt.Errorf("No valid result returned")
+	}
 
-    return *response.Result, nil
+	return *response.Result, nil
 }
