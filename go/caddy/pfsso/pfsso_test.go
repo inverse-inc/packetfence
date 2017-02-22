c1d94755cda3323aba02f7642d81193c204da4af
Julien Semaan <jul.semaan@gmail.com>
2017-02-22 07:48:25 -0500

    rework pfsso tests

--- go/caddy/pfsso/pfsso_test.go
+++ go/caddy/pfsso/pfsso_test.go
@@ -1,25 +1,36 @@
 package pfsso
 
 import (
+	"bytes"
 	"context"
 	"github.com/fingerbank/processor/log"
-	"github.com/fingerbank/processor/sharedutils"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 var ctx = log.LoggerNewContext(context.Background())
 var pfsso, err = buildPfssoHandler(ctx)
 
-func BenchmarkReadConfig(b *testing.B) {
-	sharedutils.CheckError(err)
-	for i := 0; i < b.N; i++ {
-		readConfig(ctx, &pfsso, false)
+func TestHandleStart(t *testing.T) {
+	req := httptest.NewRequest("POST", "/pfsso/start", bytes.NewBuffer([]byte(`{"ip":"1.2.3.4", "mac": "00:11:22:33:44:55", "username":"lzammit", "role": "default", "timeout":"86400"}`)))
+	recorder := httptest.NewRecorder()
+	pfsso.handleStart(recorder, req, httprouter.Params{})
+
+	if recorder.Code != http.StatusAccepted {
+		t.Error("Wrong status code from handleStart")
 	}
+
 }
 
-func BenchmarkReadConfigFirstLoad(b *testing.B) {
-	sharedutils.CheckError(err)
-	for i := 0; i < b.N; i++ {
-		readConfig(ctx, &pfsso, true)
+func TestHandleStop(t *testing.T) {
+	req := httptest.NewRequest("POST", "/pfsso/stop", bytes.NewBuffer([]byte(`{"ip":"1.2.3.4", "mac": "00:11:22:33:44:55", "username":"lzammit", "role": "default", "timeout":"86400"}`)))
+	recorder := httptest.NewRecorder()
+	pfsso.handleStop(recorder, req, httprouter.Params{})
+
+	if recorder.Code != http.StatusAccepted {
+		t.Error("Wrong status code from handleStop")
 	}
+
 }
