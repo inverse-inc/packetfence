bce32b047a067d50118c64c43818a1e4f9c26a11
Julien Semaan <jul.semaan@gmail.com>
2017-02-23 13:04:38 -0500

    launch pfconfig pool refresh with context from request

--- go/caddy/pfconfig/pool.go
+++ go/caddy/pfconfig/pool.go
@@ -2,11 +2,11 @@ package caddypfconfig
 
 import (
 	"context"
-	"github.com/fingerbank/processor/log"
 	"github.com/inverse-inc/packetfence/go/caddy/caddy"
 	"github.com/inverse-inc/packetfence/go/caddy/caddy/caddyhttp/httpserver"
 	"github.com/inverse-inc/packetfence/go/pfconfigdriver"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -20,28 +20,33 @@ func init() {
 // Setup an async goroutine that refreshes the pfconfig pool every second
 func setup(c *caddy.Controller) error {
 	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
-		return PoolHandler{Next: next}
+		return PoolHandler{Next: next, refreshLauncher: &sync.Once{}}
 	})
-
-	ctx := log.LoggerNewContext(context.Background())
-	go func() {
-		for {
-			pfconfigdriver.PfconfigPool.Refresh(ctx)
-			time.Sleep(1 * time.Second)
-		}
-	}()
 
 	return nil
 }
 
 type PoolHandler struct {
-	Next httpserver.Handler
+	Next            httpserver.Handler
+	refreshLauncher *sync.Once
 }
 
 // Middleware that ensures there is a read-lock on the pool during every request and released when the request is done
 func (h PoolHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 	pfconfigdriver.PfconfigPool.ReadLock(r.Context())
 	defer pfconfigdriver.PfconfigPool.ReadUnlock(r.Context())
+
+	// We launch the refresh job once, the first time a request comes in
+	// This ensures that the pool will run with a context that represents a request (log level for instance)
+	h.refreshLauncher.Do(func() {
+		ctx := r.Context()
+		go func(ctx context.Context) {
+			for {
+				pfconfigdriver.PfconfigPool.Refresh(ctx)
+				time.Sleep(1 * time.Second)
+			}
+		}(ctx)
+	})
 
 	return h.Next.ServeHTTP(w, r)
 }
