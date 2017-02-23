0380d2d3f8edcf962e03682ca1b39c63a6c9424a
Julien Semaan <jul.semaan@gmail.com>
2017-02-23 14:48:20 -0500

    initial version of sbin/winbindd-wrapper

--- lib/pf/services/manager/winbindd.pm
+++ lib/pf/services/manager/winbindd.pm
@@ -27,52 +27,74 @@ use Errno qw(EINTR EAGAIN);
 use pf::log;
 use pf::file_paths qw(
     $var_dir
-    $systemd_unit_dir
+    $domains_chroot_dir
 );
 use pf::domain;
-use pf::services::manager::winbindd_child;
 
-extends 'pf::services::manager::submanager';
-
-has winbinddManagers => (is => 'rw', builder => 1, lazy => 1);
-
-has _pidFiles => (is => 'rw', default => sub { {} } );
+extends 'pf::services::manager';
 
 has '+name' => (default => sub { 'winbindd'} );
-
-
-sub _build_winbinddManagers {
-    my ($self) = @_;
-
-    my @managers = map {
-        my $DOMAIN=$_;
-        my $CHROOT_PATH=pf::domain::chroot_path($DOMAIN);
-        my $CONFIGFILE="/etc/samba/$DOMAIN.conf";
-        my $LOGDIRECTORY="/var/log/samba$DOMAIN";
-        my $binary = $Config{services}{winbindd_binary};
-
-        pf::services::manager::winbindd_child->new ({
-            executable => $self->executable,
-            name => "winbindd-$_.conf",
-            forceManaged => $self->isManaged,
-            domain => $_,
-        })
-    } uniq keys %ConfigDomain;
-    return \@managers;
-}
-
-
-
-sub managers {
-    my ($self) = @_;
-    return @{$self->winbinddManagers};
-}
 
 sub isManaged {
     my ($self) = @_;
     return $TRUE if (keys %ConfigDomain);
     return $FALSE;
 }
+
+sub generateConfig {
+    my ($self, $quick) = @_;
+
+    $self->_refresh_domains();
+
+    $self->build_namespaces();
+}
+
+sub _refresh_domains {
+    my ($self) = @_;
+    pfconfig::manager->new->expire("config::Domain");
+}
+
+sub build_namespaces(){
+    my ($self) = @_;
+
+    my $out = pf_run("sudo /sbin/ip netns list");
+    if (defined $out) {
+        foreach my $net (split /\n/, $out) {
+            # untaint the variable
+            $net =~ m|([\w\-\/]*)|;
+            $net = $1;
+            my @args = ("sudo", "ip", "link", "delete", "$net-b");
+            system(@args);
+            @args = ("sudo", "ip", "netns", "delete", $net);
+            system(@args);
+        }
+    }
+
+    my $i = 1;
+
+    foreach my $domain (keys %ConfigDomain){
+        my $CHROOT_PATH=pf::domain::chroot_path($domain);
+        my $LOGDIRECTORY="/var/log/samba$domain";
+        my $OUTERLOGDIRECTORY="$CHROOT_PATH/$LOGDIRECTORY";
+        my $OUTERRUNDIRECTORY="$CHROOT_PATH/var/run/samba$domain";
+        my $PIDDIRECTORY="$var_dir/run/$domain";
+        pf_run("sudo mkdir -p $OUTERLOGDIRECTORY && sudo chown root.root $OUTERLOGDIRECTORY");
+        pf_run("sudo mkdir -p $OUTERRUNDIRECTORY && sudo chown root.root $OUTERRUNDIRECTORY");
+        pf_run("sudo mkdir -p $PIDDIRECTORY && sudo chown root.root $PIDDIRECTORY");
+        pf_run("sudo /usr/local/pf/addons/create_chroot.sh $domain $domains_chroot_dir");
+        my $ip_a = "169.254.0.".$i;
+        my $ip_b = "169.254.0.".($i+1);
+        pf_run("sudo ip netns add $domain");
+        pf_run("sudo ip link add $domain-a type veth peer name $domain-b");
+        pf_run("sudo ip link set $domain-a netns $domain");
+        pf_run("sudo ip netns exec $domain ifconfig $domain-a up $ip_a netmask 255.255.255.252");
+        pf_run("sudo ifconfig $domain-b up $ip_b netmask 255.255.255.252");
+        pf_run("sudo ip netns exec $domain route add default gw $ip_b dev $domain-a");
+        pf_run("sudo ip netns exec $domain ip link set dev lo up");
+        $i+=4;
+    }
+}
+
 
 =head1 AUTHOR
 
