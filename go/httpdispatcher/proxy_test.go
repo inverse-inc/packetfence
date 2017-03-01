84c9d77aaaf8a5f9aca232bdb3e5ccecd887f437
Durand Fabrice <fdurand@inverse.ca>
2017-03-01 12:26:40 -0500

    Changed test

--- go/httpdispatcher/proxy_test.go
+++ go/httpdispatcher/proxy_test.go
@@ -1,50 +1,75 @@
 package httpdispatcher
 
+package main
+
 import (
-	"net/http"
+	"bytes"
+	"log"
+	"net/http/httptest"
 	"net/url"
 	"os"
+	"regexp"
 	"testing"
 )
 
+var testproxy proxy
+
 func TestMain(m *testing.M) {
-	proxy := newProxy("")
-	go proxy.run("8888")
+	passThrough = newProxyPassthrough()
+	rgx, _ := regexp.Compile("www.padl.com")
+	passThrough.proxypassthrough = append(passThrough.proxypassthrough, rgx)
+	rgx, _ = regexp.Compile("www.gstatic.com/generate_204")
+	passThrough.detectionmechanisms = append(passThrough.detectionmechanisms, rgx)
+	passThrough.DetectionMecanismBypass = "enabled"
+	rgx, _ = regexp.Compile("CaptiveNetworkSupport")
+	passThrough.URIException = rgx
+
+	var portalURL url.URL
+	var wisprURL url.URL
+
+	portalURL.Host = "www.packetfence.org"
+	portalURL.Path = "/captive-portal"
+	portalURL.Scheme = "http"
+
+	wisprURL.Host = "www.packetfence.org"
+	wisprURL.Path = "/wispr"
+	wisprURL.Scheme = "http"
+
+	passThrough.WisprURL = &wisprURL
+	passThrough.PortalURL = &portalURL
+	var l *log.Logger
+	l = log.New(os.Stdout, "REQUEST: ", log.Ldate|log.Ltime)
+	testproxy.requestLogger = l
+	testproxy.addToEndpointList("127.0.0.1")
 	os.Exit(m.Run())
 }
 
-func TestLocalhost(t *testing.T) {
-	resp, err := http.Get("http://localhost:8888")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.StatusCode != 403 {
-		t.Fatalf("Received non-403 response: %d\n", resp.StatusCode)
-	}
-}
-
-func TestSimpleForward(t *testing.T) {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://localhost:8888", nil)
-	req.Host = "www.packetfence.org"
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.StatusCode != 200 {
-		t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
+func TestSimpleRedirect(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://www.inverse.ca", bytes.NewBuffer([]byte("")))
+	recorder := httptest.NewRecorder()
+	testproxy.ServeHTTP(recorder, req)
+	if recorder.Code != 302 {
+		t.Fatalf("Received non-302 response: %d\n", recorder.Code)
 	}
 }
 
-func TestSimpleBlacklist(t *testing.T) {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://localhost:8888", nil)
-	req.Host = "127.0.0.1"
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.StatusCode != 404 {
-		t.Fatalf("Received non-404 response: %d\n", resp.StatusCode)
+func TestSimpleNotImplemented(t *testing.T) {
+	req := httptest.NewRequest("POST", "http://www.packetfence.org", bytes.NewBuffer([]byte("")))
+	recorder := httptest.NewRecorder()
+	testproxy.ServeHTTP(recorder, req)
+	if recorder.Code != 501 {
+		t.Fatalf("Received non-501 response: %d\n", recorder.Code)
 	}
 }
+
+func TestSimpleProxy(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://www.padl.com", bytes.NewBuffer([]byte("")))
+	req.Host = "www.padl.com"
+	recorder := httptest.NewRecorder()
+	testproxy.ServeHTTP(recorder, req)
+	if recorder.Code != 200 {
+		t.Fatalf("Received non-200 response: %d\n", recorder.Code)
+	}
+}
+
+
