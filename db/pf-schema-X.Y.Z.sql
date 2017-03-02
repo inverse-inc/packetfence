78cda82c05cf42554e1e24d3b5250411f84c14c9
Derek Wuelfrath <dwuelfrath@inverse.ca>
2017-03-02 10:21:50 -0500

    Missing ip6log tables schema

--- db/pf-schema-X.Y.Z.sql
+++ db/pf-schema-X.Y.Z.sql
@@ -296,6 +296,70 @@ CREATE TABLE ip4log_archive (
   KEY start_time (start_time)
 ) ENGINE=InnoDB;
 
+--
+-- Table structure for table `ip6log`
+--
+
+CREATE TABLE ip6log (
+  mac varchar(17) NOT NULL,
+  ip varchar(45) NOT NULL,
+  type varchar(32) NOT NULL,
+  start_time datetime NOT NULL,
+  end_time datetime default "0000-00-00 00:00:00",
+  PRIMARY KEY (ip),
+  KEY ip6log_mac_end_time (mac,end_time),
+  KEY ip6log_end_time (end_time)
+) ENGINE=InnoDB;
+
+--
+-- Trigger to insert old record from 'ip6log' in 'ip6log_history' before updating the current one
+--
+
+DROP TRIGGER IF EXISTS ip6log_insert_in_ip6log_history_before_update_trigger;
+DELIMITER /
+CREATE TRIGGER ip6log_insert_in_ip6log_history_before_update_trigger BEFORE UPDATE ON ip6log
+FOR EACH ROW
+BEGIN
+  INSERT INTO ip6log_history SET ip = OLD.ip, mac = OLD.mac, type = OLD.type, start_time = OLD.start_time, end_time = CASE
+    WHEN OLD.end_time = '0000-00-00 00:00:00' THEN NOW()
+    WHEN OLD.end_time > NOW() THEN NOW()
+    ELSE OLD.end_time
+  END;
+END /
+DELIMITER ;
+
+--
+-- Table structure for table `ip6log_history`
+--
+
+CREATE TABLE ip6log_history (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  mac varchar(17) NOT NULL,
+  ip varchar(45) NOT NULL,
+  type varchar(32) NOT NULL,
+  start_time datetime NOT NULL,
+  end_time datetime NOT NULL,
+  KEY ip6log_history_mac_end_time (mac,end_time),
+  KEY end_time (end_time),
+  KEY start_time (start_time)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `ip6log_archive`
+--
+
+CREATE TABLE ip6log_archive (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  mac varchar(17) NOT NULL,
+  ip varchar(45) NOT NULL,
+  type varchar(32) NOT NULL,
+  start_time datetime NOT NULL,
+  end_time datetime NOT NULL,
+  KEY end_time (end_time),
+  KEY start_time (start_time)
+) ENGINE=InnoDB;
+
+
 CREATE TABLE `locationlog` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `mac` varchar(17) default NULL,
