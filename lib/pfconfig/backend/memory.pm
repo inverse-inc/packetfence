c5e66254359f8b9ea1768aa11ed57ba6c2872e6e
James Rouzier <jrouzier@inverse.ca>
2017-03-03 16:05:56 -0500

    Add a memory based backend to pfconfig

---
+++ lib/pfconfig/backend/memory.pm
@@ -0,0 +1,88 @@
+package pfconfig::backend::memory;
+
+=head1 NAME
+
+pfconfig::backend::memory;
+
+=cut
+
+=head1 DESCRIPTION
+
+pfconfig::backend::memory;
+
+Defines a CHI Memory backend to use as a layer 2 cache
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'pfconfig::backend';
+use CHI;
+use pfconfig::empty_string;
+
+my $empty_string = pfconfig::empty_string->new;
+
+sub init {
+    my ($self) = @_;
+    $self->{cache} = CHI->new(driver => 'Memory', datastore => {},'serializer' => 'Sereal');
+}
+
+sub set {
+    my ( $self, $key, $value ) = @_;
+
+    # BDB doesn't write empty strings
+    # We workaround it using a class that represents an empty string
+    if ( defined($value) && "$value" eq '' ) {
+        $value = $empty_string;
+    }
+    $self->SUPER::set( $key, $value );
+}
+
+sub get {
+    my ( $self, $key ) = @_;
+    my $value = $self->SUPER::get($key);
+
+    # BDB doesn't write empty strings
+    # We workaround it using a class that represents an empty string
+    if ( ref($value) eq "pfconfig::empty_string" && $value->isa("pfconfig::empty_string") ) {
+        $value = '';
+    }
+    return $value;
+}
+
+
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2017 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
+
