f92037b300d714d97deb19736dbc6acbe3a4f749
Durand Fabrice <fdurand@inverse.ca>
2017-03-06 14:22:05 -0500

    First draft of go dhcp server

---
+++ go/dhcp/rawClient.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"net"
+
+	"golang.org/x/net/ipv4"
+
+	"github.com/mdlayher/ethernet"
+	"github.com/mdlayher/raw"
+)
+
+const UDP_HEADER_LEN = 8
+
+// A RawClient is a Wake-on-LAN client which operates directly on top of
+// Ethernet frames using raw sockets.  It can be used to send WoL magic packets
+// to other machines on a local network, using their hardware addresses.
+type RawClient struct {
+	ifi *net.Interface
+	p   net.PacketConn
+}
+
+type udphdr struct {
+	src  uint16
+	dst  uint16
+	ulen uint16
+	csum uint16
+}
+
+type pseudohdr struct {
+	ipsrc   [4]byte
+	ipdst   [4]byte
+	zero    uint8
+	ipproto uint8
+	plen    uint16
+}
+
+type iphdr struct {
+	vhl   uint8
+	tos   uint8
+	iplen uint16
+	id    uint16
+	off   uint16
+	ttl   uint8
+	proto uint8
+	csum  uint16
+	src   [4]byte
+	dst   [4]byte
+}
+
+// NewRawClient creates a new RawClient using the specified network interface.
+//
+// Note that raw sockets typically require elevated user privileges, such as
+// the 'root' user on Linux, or the 'SET_CAP_RAW' capability.
+//
+// For this reason, it is typically recommended to use the regular Client type
+// instead, which operates over UDP.
+func NewRawClient(ifi *net.Interface) (*RawClient, error) {
+	// Open raw socket to send Wake-on-LAN magic packets
+	p, err := raw.ListenPacket(ifi, raw.ProtocolARP)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RawClient{
+		ifi: ifi,
+		p:   p,
+	}, nil
+}
+
+// Close closes a RawClient's raw socket.
+func (c *RawClient) Close() error {
+	return c.p.Close()
+}
+
+// sendDHCP create a udp packet and stores it in an
+// Ethernet frame, and sends the frame over a raw socket to attempt to wake
+// a machine.
+func (c *RawClient) sendDHCP(target net.HardwareAddr, dhcp []byte, dstIP net.IP, srcIP net.IP) error {
+
+	proto := 17
+
+	udpsrc := uint(67)
+	udpdst := uint(68)
+
+	udp := udphdr{
+		src: uint16(udpsrc),
+		dst: uint16(udpdst),
+	}
+
+	udplen := 8 + len(dhcp)
+
+	h := &ipv4.Header{
+		Version:  ipv4.Version,
+		Len:      ipv4.HeaderLen,
+		TotalLen: ipv4.HeaderLen + UDP_HEADER_LEN + len(dhcp),
+		ID:       0,
+		Protocol: proto,
+		Dst:      dstIP.To4(),
+		Src:      srcIP.To4(),
+		TTL:      128,
+	}
+
+	ip := iphdr{
+		vhl:   0x45,
+		tos:   0,
+		id:    0x0000, // the kernel overwrites id if it is zero
+		off:   0,
+		ttl:   128,
+		proto: uint8(proto),
+	}
+	copy(ip.src[:], srcIP.To4())
+	copy(ip.dst[:], dstIP.To4())
+
+	udp.ulen = uint16(udplen)
+	udp.checksum(&ip, dhcp)
+
+	totalLen := 20 + udplen
+
+	ip.iplen = uint16(totalLen)
+	ip.checksum()
+	h.Checksum = int(ip.csum)
+
+	buf := bytes.NewBuffer([]byte{})
+	err := binary.Write(buf, binary.BigEndian, &udp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	udpHeader := buf.Bytes()
+	dataWithHeader := append(udpHeader, dhcp...)
+
+	out, err := h.Marshal()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	packet := append(out, dataWithHeader...)
+
+	// Create Ethernet frame
+	f := &ethernet.Frame{
+		Destination: target,
+		Source:      c.ifi.HardwareAddr,
+		EtherType:   ethernet.EtherTypeIPv4,
+		Payload:     packet,
+	}
+	fb, err := f.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	// Send packet to target
+	_, err = c.p.WriteTo(fb, &raw.Addr{
+		HardwareAddr: target,
+	})
+	return err
+}
+
+func (u *udphdr) checksum(ip *iphdr, payload []byte) {
+	u.csum = 0
+
+	phdr := pseudohdr{
+		ipsrc:   ip.src,
+		ipdst:   ip.dst,
+		zero:    0,
+		ipproto: ip.proto,
+		plen:    u.ulen,
+	}
+	var b bytes.Buffer
+	binary.Write(&b, binary.BigEndian, &phdr)
+	binary.Write(&b, binary.BigEndian, u)
+	binary.Write(&b, binary.BigEndian, &payload)
+	u.csum = checksum(b.Bytes())
+}
+
+func checksum(buf []byte) uint16 {
+	sum := uint32(0)
+
+	for ; len(buf) >= 2; buf = buf[2:] {
+		sum += uint32(buf[0])<<8 | uint32(buf[1])
+	}
+	if len(buf) > 0 {
+		sum += uint32(buf[0]) << 8
+	}
+	for sum > 0xffff {
+		sum = (sum >> 16) + (sum & 0xffff)
+	}
+	csum := ^uint16(sum)
+	/*
+	 * From RFC 768:
+	 * If the computed checksum is zero, it is transmitted as all ones (the
+	 * equivalent in one's complement arithmetic). An all zero transmitted
+	 * checksum value means that the transmitter generated no checksum (for
+	 * debugging or for higher level protocols that don't care).
+	 */
+	if csum == 0 {
+		csum = 0xffff
+	}
+	return csum
+}
+
+func (h *iphdr) checksum() {
+	h.csum = 0
+	var b bytes.Buffer
+	binary.Write(&b, binary.BigEndian, h)
+	h.csum = checksum(b.Bytes())
+}
---
+++ go/dhcp/serverif.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"syscall"
+
+	"golang.org/x/net/ipv4"
+)
+
+type serveIfConn struct {
+	ifIndex int
+	conn    *ipv4.PacketConn
+	cm      *ipv4.ControlMessage
+}
+
+func (s *serveIfConn) ReadFrom(b []byte) (n int, addr net.Addr, err error) {
+	n, s.cm, addr, err = s.conn.ReadFrom(b)
+	// if s.cm != nil { // Filter all other interfaces
+	// 	n = 0 // Packets < 240 are filtered in Serve().
+	//
+	// }
+
+	return
+}
+
+func (s *serveIfConn) WriteTo(b []byte, addr net.Addr) (n int, err error) {
+
+	// ipv4 docs state that Src is "specify only", however testing by tfheen
+	// shows that Src IS populated.  Therefore, to reuse the control message,
+	// we set Src to nil to avoid the error "write udp4: invalid argument"
+
+	return s.conn.WriteTo(b, s.cm, addr)
+}
+
+// ServeIf does the same job as Serve(), but listens and responds on the
+// specified network interface (by index).  It also doubles as an example of
+// how to leverage the dhcp4.ServeConn interface.
+//
+// If your target only has one interface, use Serve(). ServeIf() requires an
+// import outside the std library.  Serving DHCP over multiple interfaces will
+// require your own dhcp4.ServeConn, as listening to broadcasts utilises all
+// interfaces (so you cannot have more than on listener).
+func ServeIf(ifIndex int, p *ipv4.PacketConn, handler Handler, jobs chan job) error {
+	if err := p.SetControlMessage(ipv4.FlagInterface, true); err != nil {
+		return err
+	}
+	return Serve(&serveIfConn{ifIndex: ifIndex, conn: p}, handler, jobs)
+}
+
+// ListenAndServeIf listens on the UDP network address addr and then calls
+// Serve with handler to handle requests on incoming packets.
+// i.e. ListenAndServeIf("eth0",handler)
+func ListenAndServeIf(interfaceName string, handler Handler, jobs chan job) error {
+	// spew.Dump(handler)
+	iface, err := net.InterfaceByName(interfaceName)
+	if err != nil {
+		return err
+	}
+
+	// ip := net.ParseIP("0.0.0.0")
+	p, err := broadcastOpen(net.IPv4zero, 67, interfaceName)
+	if err != nil {
+		return err
+	}
+	defer p.Close()
+	return ServeIf(iface.Index, p, handler, jobs)
+}
+
+func broadcastOpen(bindAddr net.IP, port int, ifname string) (*ipv4.PacketConn, error) {
+	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+		log.Fatal(err)
+	}
+	//syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
+	if err = syscall.SetsockoptString(s, syscall.SOL_SOCKET, syscall.SO_BINDTODEVICE, ifname); err != nil {
+		log.Fatal(err)
+	}
+
+	lsa := syscall.SockaddrInet4{Port: port}
+	copy(lsa.Addr[:], bindAddr.To4())
+
+	if err = syscall.Bind(s, &lsa); err != nil {
+		syscall.Close(s)
+		log.Fatal(err)
+	}
+	f := os.NewFile(uintptr(s), "")
+	c, err := net.FilePacketConn(f)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := ipv4.NewPacketConn(c)
+
+	return p, nil
+}
