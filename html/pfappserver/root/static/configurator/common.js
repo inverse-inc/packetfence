91c1c9def693c90760e1482e3aa1e7bcd468e0d7
Francis Lachapelle <flachapelle@inverse.ca>
2017-03-30 12:29:35 -0400

    (web admin) Fix networks step of configurator
    Fixes #2166

--- html/pfappserver/root/static/configurator/common.js
+++ html/pfappserver/root/static/configurator/common.js
@@ -1,8 +1,9 @@
+/* -*- Mode: js; indent-tabs-mode: nil; js-indent-level: 4 -*- */
+
 $(function () {
     /* Register tracker tooltips */
     $('#tracker [rel=tooltip]').tooltip({placement: 'bottom'});
 
-    if (typeof initModals == 'function') initModals();
     if (typeof initStep == 'function') initStep();
     if (typeof registerExits == 'function') registerExits();
 });
