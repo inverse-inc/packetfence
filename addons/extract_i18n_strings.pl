f850b9c7e987846fc08f5225066ca32f84c1ab40
Antoine Amacher <aamacher@inverse.ca>
2017-03-30 10:53:15 -0400

    added scans type to extract.pl for translation

--- addons/extract_i18n_strings.pl
+++ addons/extract_i18n_strings.pl
@@ -32,6 +32,7 @@ use pf::radius_audit_log;
 use pf::constants::admin_roles qw(@ADMIN_ACTIONS);
 use pf::dhcp_option82;
 use pf::factory::detect::parser;
+use pf::factory::scan;
 
 use constant {
     APP => 'html/pfappserver',
@@ -337,6 +338,7 @@ sub extract_modules {
     const('pf::radius_audit_log', 'RADIUS Audit Log', \@pf::radius_audit_log::FIELDS);
     const('pf::dhcp_option82', 'DHCP Option 82', [values %pf::dhcp_option82::HEADINGS]);
     const('pf::factory::detect::parser', 'Detect Parsers', [map { /^pf::detect::parser::(.*)/;"pfdetect_type_$1"  } @pf::factory::detect::parser::MODULES]);
+    const('pf::factory::scan', 'Scans Engine', [map { /^pf::scan/;"scan_type_$1"  } @pf::factory::scan::MODULES]);
 
     my @values = map { "${_}_action" } @pf::action::VIOLATION_ACTIONS;
     const('pf::action', 'VIOLATION_ACTIONS', \@values);
