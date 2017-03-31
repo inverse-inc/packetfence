2b493932df094c8d2e9b8e2bbbd27503e12f2296
Antoine Amacher <aamacher@inverse.ca>
2017-03-31 15:34:57 -0400

    edit icon for required fixes #2180

--- html/pfappserver/lib/pfappserver/Base/Form.pm
+++ html/pfappserver/lib/pfappserver/Base/Form.pm
@@ -105,7 +105,7 @@ sub update_field {
 
     if ($field->required) {
         $field->set_element_attr('data-required' => 'required');
-        $field->tags->{label_after} = ' <i class="icon-exclamation-sign"></i>';
+        $field->tags->{label_after} = ' <i class="icon-exclamation-circle"></i>';
     }
     if ($field->type eq 'PosInteger') {
         $field->type_attr($field->html5_type_attr);
