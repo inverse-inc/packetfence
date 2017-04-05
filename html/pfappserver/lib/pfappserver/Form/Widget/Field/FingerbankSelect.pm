684d94490d6746061da9865556d6097447ded0e1
Francis Lachapelle <flachapelle@inverse.ca>
2017-04-04 23:03:01 -0400

    (web admin) Improve icon buttons

--- html/pfappserver/lib/pfappserver/Form/Widget/Field/FingerbankSelect.pm
+++ html/pfappserver/lib/pfappserver/Form/Widget/Field/FingerbankSelect.pm
@@ -34,7 +34,7 @@ sub render_element {
         ' class="fingerbank-type-ahead" name="fingerbank-add-'.$self->id.'" data-provide="typeahead"'.
         ' id="fingerbank-add-'.$self->id.'" data-btn="#btn-fingerbank-add-'.$self->id.'" placeholder="'.$self->_localize('Add').' '.
         $self->label.' '.$self->_localize('from Fingerbank').'" value="" type="text">'.
-        '<a href="#" class="btn btn-primary" style="margin-left:3px;" id="btn-fingerbank-add-'.$self->id.'"'.$self->id.'><i class="icon-plus icon-white"></i></a>'.
+        '<a href="#" class="btn-icon btn-icon-circle" style="margin-left:3px;" id="btn-fingerbank-add-'.$self->id.'"'.$self->id.'><i class="icon-plus icon-white"></i></a>'.
         '</div>';
     $select .= HTML::FormHandler::Widget::Field::Select::render_element($self,$result);
     return $select;
