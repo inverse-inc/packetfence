8ad928b1768b09c379cc73dfe555b0ea00bdd58b
Julien Semaan <julien@semaan.ca>
2017-04-11 12:57:54 -0400

    add scripts to build ZEN with vagrant

---
+++ addons/ZEN-vagrant-builder/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm -f \
+        work/box.ovf \
+        work/box-disk1.vmdk \
+        work/box.ova \
+        work/package.box \
+        work/vagrant_private_key \
+        work/Vagrantfile
+
+vagrant destroy -f
+
+vagrant up
+
+vagrant halt
+
+VBoxManage modifyvm PacketFence-ZEN --memory 8096
+
+VBoxManage modifyvm PacketFence-ZEN --uartmode1 disconnected
+
+vagrant package
+
+mkdir -p work
+mv package.box work/
+cd work/
+tar -xvf package.box
+
+../fix_ovf_alt box.ovf
+\mv vmx_box.ovf box.ovf
+
+ovftool --lax box.ovf box.ova
+
+mv box.ova ../PacketFence-ZEN.ova
+
---
+++ addons/ZEN-vagrant-builder/installer/install-pf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cat /vagrant/installer/rc.local > /etc/rc.local
+
+#epel is there in the base image but we don't want it...
+#rm /etc/yum.repos.d/epel.repo
+
+#repo used for the initial install of PacketFence
+cp /vagrant/installer/packetfence.repo /etc/yum.repos.d/
+
+yum install perl -y
+yum install --enablerepo=packetfence-devel packetfence -y
+
+hostname PacketFence-ZEN
+echo "PacketFence-ZEN" > /etc/hostname
+echo "NETWORKING=yes HOSTNAME=PacketFence-ZEN" > /etc/sysconfig/network
+
+chmod +x /etc/rc.d/rc.local
+
+# Set the root password
+echo "p@ck3tf3nc3" | passwd root --stdin
+
+# Enable password authentication
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
