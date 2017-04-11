1b82e034451d385177681e577ec12b119203754d
James Rouzier <jrouzier@inverse.ca>
2017-04-11 11:33:25 -0400

    Add logic for tasks pfsnmp_parsing and pfsnmp

--- lib/pf/util/pfqueue.pm
+++ lib/pf/util/pfqueue.pm
@@ -25,16 +25,28 @@ BEGIN {
     @EXPORT_OK = qw(task_counter_id consumer_redis_client);
 }
 
+=head1 SUBROUTINES
+
 =head2 $id = task_counter_id($queue, $type, $args)
+
+Create the counter id for a queue task
 
 =cut
 
 sub task_counter_id {
     my ($queue, $type, $args) = @_;
     my $counter_id = "${queue}:${type}";
-    if ($type eq 'api' && ref ($args) eq 'ARRAY') {
+    my $args_ref = ref ($args);
+    if ($type eq 'api' && $args_ref eq 'ARRAY') {
         $counter_id .= ":" . $args->[0];
     }
+    elsif ($type eq 'pfsnmp_parsing' && $args_ref eq 'ARRAY') {
+        $counter_id .= ":" . $args->[0]->{switchIp};
+    }
+    elsif ($type eq 'pfsnmp' eq 'HASH') {
+        $counter_id .= join(":","", $args->{switchId}, $args->{trapIfIndex}, $args->{'trapType'});
+    }
+
     return $counter_id;
 }
 
@@ -46,9 +58,6 @@ sub consumer_redis_client {
     my ($self) = @_;
     return pf::Redis->new( %{$ConfigPfqueue{consumer}{redis_args}});
 }
-
-
-=head1 SUBROUTINES
 
 =head1 AUTHOR
 
