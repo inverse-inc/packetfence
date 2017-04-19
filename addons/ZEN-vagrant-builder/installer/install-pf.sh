c8831bedb0c6e27fca965ec3a499d078145c749b
Julien Semaan <julien@semaan.ca>
2017-04-19 10:25:46 -0400

    adjustments to ZEN builder

--- addons/ZEN-vagrant-builder/installer/install-pf.sh
+++ addons/ZEN-vagrant-builder/installer/install-pf.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-cat /vagrant/installer/rc.local > /etc/rc.local
+# Install the PacketFence repos
+yum localinstall http://packetfence.org/downloads/PacketFence/RHEL7/`uname -i`/RPMS/packetfence-release-1.2-6.el7.centos.noarch.rpm -y
 
-#repo used for the initial install of PacketFence
-cp /vagrant/installer/packetfence.repo /etc/yum.repos.d/
+# Update the release to be sure we run its latest version
+yum update packetfence-release --enablerepo=packetfence -y
 
+# PacketFence installation
 yum install perl -y
 yum install --enablerepo=packetfence-devel packetfence -y
 
+# Setting the hostname
 hostname PacketFence-ZEN
 echo "PacketFence-ZEN" > /etc/hostname
 echo "NETWORKING=yes HOSTNAME=PacketFence-ZEN" > /etc/sysconfig/network
 
+# Setting up rc.local so it modifies /etc/issue to display instructions on setting up the ZEN
+cat /vagrant/installer/rc.local > /etc/rc.local
+
+# Systemd won't execute /etc/rc.local unless we do this
 chmod +x /etc/rc.d/rc.local
 
 # Set the root password
 echo "p@ck3tf3nc3" | passwd root --stdin
 
-# Enable password authentication
+# Enable password authentication for SSH
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
