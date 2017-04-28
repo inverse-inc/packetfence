5b2a92ffac9adf85eaf09caa053d58626b6fd025
Julien Semaan <julien@semaan.ca>
2017-04-28 15:10:54 -0400

    explicit call to host id variable

--- lib/pf/api.pm
+++ lib/pf/api.pm
@@ -742,7 +742,7 @@ sub expire_cluster : Public {
 
     my @failed;
     foreach my $server (@cluster_servers){
-        next if($host_id eq $server->{host});
+        next if($pf::cluster::host_id eq $server->{host});
         my $apiclient = pf::api::jsonrpcclient->new(proto => 'https', host => $server->{management_ip});
         my %data = (
             namespace => $postdata{namespace},
@@ -760,7 +760,7 @@ sub expire_cluster : Public {
 
         %data = (
             conf_file => $postdata{conf_file},
-            server => $host_id,
+            server => $pf::cluster::host_id,
         );
 
         eval {
