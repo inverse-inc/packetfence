ed43cebcf9c571a1b574dabeb7bc317c641a0f0b
James Rouzier <jrouzier@inverse.ca>
2017-05-01 13:29:03 -0400

    Disconnect in the child process

--- lib/pf/db.pm
+++ lib/pf/db.pm
@@ -56,7 +56,8 @@ sub CLONE {
 sub AT_FORK_CHILD {
     if ($DBH) {
         $DBH->{InactiveDestroy} = 1;
-        $DBH = undef;
+        $DBH->disconnect;
+        undef $DBH;
         $LAST_CONNECT = 0;
     }
 }
