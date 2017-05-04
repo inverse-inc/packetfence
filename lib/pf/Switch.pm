c37a46bceb8249589cbff14f20b4adcf5f1c229a
Derek Wuelfrath <dwuelfrath@inverse.ca>
2017-05-04 17:17:49 -0400

    Separation of logic

--- lib/pf/Switch.pm
+++ lib/pf/Switch.pm
@@ -3498,6 +3498,32 @@ sub getExclusiveLockForScope {
     return $fh;
 }
 
+=item getLldpLocPortDesc
+
+Query the switch for lldpLocPortDesc table and cache the result
+
+=cut
+
+sub getLldpLocPortDesc {
+    my ( $self ) = @_;
+    my $logger = $self->logger;
+
+    # if can't SNMP read abort
+    return if ( !$self->connectRead() );
+
+    my $oid_lldpLocPortDesc = '1.0.8802.1.1.2.1.3.7.1.4'; # from LLDP-MIB
+    $logger->trace("SNMP get_table for lldpLocPortDesc: $oid_lldpLocPortDesc");
+    my $cache = $self->cache;
+    my $result = $cache->compute([$self->{'_id'},$oid_lldpLocPortDesc], sub { $self->{_sessionRead}->get_table( -baseoid => $oid_lldpLocPortDesc) } );
+    # here's what we are getting here. Looking for the last element of the OID: lldpRemLocalPortNum
+    # iso.0.8802.1.1.2.1.3.7.1.4.10 = STRING: "FastEthernet1/0/8"
+    # iso.0.8802.1.1.2.1.3.7.1.4.11 = STRING: "FastEthernet1/0/9"
+    # iso.0.8802.1.1.2.1.3.7.1.4.12 = STRING: "FastEthernet1/0/10"
+    # iso.0.8802.1.1.2.1.3.7.1.4.13 = STRING: "FastEthernet1/0/11"
+
+    return $result;
+}
+
 =item ifIndexToLldpLocalPort
 
 Translate an ifIndex into an LLDP Local Port number.
@@ -3516,16 +3542,10 @@ sub ifIndexToLldpLocalPort {
     my $ifDescr = $self->getIfDesc($ifIndex);
     return if (!defined($ifDescr) || $ifDescr eq '');
 
+    # Get lldpLocPortDesc
     my $oid_lldpLocPortDesc = '1.0.8802.1.1.2.1.3.7.1.4'; # from LLDP-MIB
+    my $result = $self->getLldpLocPortDesc();
 
-    $logger->trace("SNMP get_table for lldpLocPortDesc: $oid_lldpLocPortDesc");
-    my $cache = $self->cache;
-    my $result = $cache->compute([$self->{'_id'},$oid_lldpLocPortDesc], sub { $self->{_sessionRead}->get_table( -baseoid => $oid_lldpLocPortDesc) } );
-    # here's what we are getting here. Looking for the last element of the OID: lldpRemLocalPortNum
-    # iso.0.8802.1.1.2.1.3.7.1.4.10 = STRING: "FastEthernet1/0/8"
-    # iso.0.8802.1.1.2.1.3.7.1.4.11 = STRING: "FastEthernet1/0/9"
-    # iso.0.8802.1.1.2.1.3.7.1.4.12 = STRING: "FastEthernet1/0/10"
-    # iso.0.8802.1.1.2.1.3.7.1.4.13 = STRING: "FastEthernet1/0/11"
     foreach my $entry ( keys %{$result} ) {
         if ( $result->{$entry} eq $ifDescr ) {
             if ( $entry =~ /^$oid_lldpLocPortDesc\.([0-9]+)$/ ) {
