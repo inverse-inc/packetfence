2b442871046c29b29ca7f20468433049251b3cfc
Derek Wuelfrath <dwuelfrath@inverse.ca>
2017-05-05 09:48:28 -0400

    set maxrepetitions to use get-next-requests

--- lib/pf/Switch.pm
+++ lib/pf/Switch.pm
@@ -3514,12 +3514,13 @@ sub getLldpLocPortDesc {
     my $oid_lldpLocPortDesc = '1.0.8802.1.1.2.1.3.7.1.4'; # from LLDP-MIB
     $logger->trace("SNMP get_table for lldpLocPortDesc: $oid_lldpLocPortDesc");
     my $cache = $self->cache;
-    my $result = $cache->compute([$self->{'_id'},$oid_lldpLocPortDesc], sub { $self->{_sessionRead}->get_table( -baseoid => $oid_lldpLocPortDesc) } );
+    my $result = $cache->compute([$self->{'_id'},$oid_lldpLocPortDesc], sub { $self->{_sessionRead}->get_table( -baseoid => $oid_lldpLocPortDesc, -maxrepetitions  => 1 ) } );
     # here's what we are getting here. Looking for the last element of the OID: lldpRemLocalPortNum
     # iso.0.8802.1.1.2.1.3.7.1.4.10 = STRING: "FastEthernet1/0/8"
     # iso.0.8802.1.1.2.1.3.7.1.4.11 = STRING: "FastEthernet1/0/9"
     # iso.0.8802.1.1.2.1.3.7.1.4.12 = STRING: "FastEthernet1/0/10"
     # iso.0.8802.1.1.2.1.3.7.1.4.13 = STRING: "FastEthernet1/0/11"
+    # NOTE: We set the maxrepetitions to '1' to use 'get-next-requests' instead of 'get-bulk-requests' which tend to return empty results if response is to big
 
     return $result;
 }
