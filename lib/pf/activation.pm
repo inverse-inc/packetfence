7ecbd267651fa7e2a1e41ca26818d8d14bd85f8f
James Rouzier <jrouzier@inverse.ca>
2017-05-08 14:05:24 -0400

    Do not create

--- lib/pf/activation.pm
+++ lib/pf/activation.pm
@@ -135,12 +135,6 @@ sub activation_db_prepare {
         WHERE mac = ? AND type = ? activation_code = ? AND status = ? AND expiration >= NOW()
     ]);
 
-    $activation_statements->{'activation_find_code_sql'} = get_db_handle()->prepare(qq[
-        SELECT code_id, pid, mac, contact_info, activation_code, expiration, status, type, portal, email_pattern as carrier_email_pattern, unregdate
-        FROM activation LEFT JOIN sms_carrier ON carrier_id=sms_carrier.id
-        WHERE type = ? AND activation_code = ?
-    ]);
-
     $activation_statements->{'activation_view_by_code_sql'} = get_db_handle()->prepare(qq[
         SELECT code_id, pid, mac, contact_info, activation_code, expiration, status, type, portal, email_pattern as carrier_email_pattern, unregdate
         FROM activation LEFT JOIN sms_carrier ON carrier_id=sms_carrier.id
@@ -205,24 +199,6 @@ sub view {
     $query->finish();
     return ($ref);
 }
-
-=head2 find_code
-
-view an pending activation record by activation code without hash-format. Returns an hashref
-
-=cut
-
-sub find_code {
-    my ($type, $activation_code) = @_;
-    my $query = db_query_execute(ACTIVATION, $activation_statements,
-        'activation_find_code_sql',$type, $activation_code);
-    my $ref = $query->fetchrow_hashref();
-
-    # just get one row and finish
-    $query->finish();
-    return ($ref);
-}
-
 
 =head2 find_unverified_code
 
@@ -636,7 +612,21 @@ sub set_status_verified {
     my ($type, $activation_code) = @_;
     my $logger = get_logger();
 
-    my $activation_record = find_code($type, $activation_code);
+    my $activation_record = view_by_code($type, $activation_code);
+    modify_status($activation_record->{'code_id'}, $VERIFIED);
+}
+
+=head2 set_status_verified_by_mac
+
+Change the status of a given pending activation code to VERIFIED which means it can't be used anymore using the mac
+
+=cut
+
+sub set_status_verified_by_mac {
+    my ($type, $activation_code, $mac) = @_;
+    my $logger = get_logger();
+
+    my $activation_record = view_by_code_mac($type, $activation_code, $mac);
     modify_status($activation_record->{'code_id'}, $VERIFIED);
 }
 
