94b9c39a309ab6b8c24064e2e7f51e303ee2eaa0
Durand Fabrice <fdurand@inverse.ca>
2017-05-25 14:35:45 -0400

    Added an API in order to query ip2mac and mac2ip

---
+++ go/dhcp/api.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Node struct
+type Node struct {
+	Mac string `json:"MAC"`
+	Ip  string `json:"IP"`
+}
+
+func handleIP2Mac(res http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+
+	if index, found := GlobalIpCache.Get(vars["ip"]); found {
+		var node = map[string]*Node{
+			"result": &Node{Mac: index.(string), Ip: vars["ip"]},
+		}
+
+		outgoingJSON, error := json.Marshal(node)
+
+		if error != nil {
+			http.Error(res, error.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprint(res, string(outgoingJSON))
+		return
+	}
+	log.Println("Not found")
+	http.Error(res, "Not found", http.StatusInternalServerError)
+	return
+}
+
+func handleMac2Ip(res http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+
+	if index, found := GlobalMacCache.Get(vars["mac"]); found {
+		var node = map[string]*Node{
+			"result": &Node{Mac: vars["mac"], Ip: index.(string)},
+		}
+
+		outgoingJSON, error := json.Marshal(node)
+
+		if error != nil {
+			http.Error(res, error.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprint(res, string(outgoingJSON))
+		return
+	}
+	log.Println("Not found")
+	http.Error(res, "Not found", http.StatusInternalServerError)
+	return
+}
