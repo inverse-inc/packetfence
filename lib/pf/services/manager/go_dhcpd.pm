62f2e56f6ff9757f86a6cddc4ef7914f8c4503f4
Durand Fabrice <fdurand@inverse.ca>
2017-05-25 11:51:03 -0400

    Added go_dhcpd service manager

---
+++ lib/pf/services/manager/go_dhcpd.pm
@@ -0,0 +1,60 @@
+package pf::services::manager::go_dhcpd;
+=head1 NAME
+
+pf::services::manager::go_dhcpd
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::services::manager::go_dhcpd
+
+=cut
+
+use strict;
+use warnings;
+use Moo;
+use pf::cluster;
+use pf::config qw(
+    %Config
+);
+use pf::util;
+
+extends 'pf::services::manager';
+
+has '+name' => ( default => sub { 'go_dhcpd' } );
+
+sub isManaged {
+    my ($self) = @_;
+    return  isdisabled($Config{'services'}{'dhcpd'}) && $self->SUPER::isManaged();
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2017 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
