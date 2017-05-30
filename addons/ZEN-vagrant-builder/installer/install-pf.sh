f0b4442a473f7d8ef3bbe7aa7bdf8aa2a7bd8c54
Julien Semaan <julien@semaan.ca>
2017-05-30 12:48:25 -0400

    import the demouser SQL directly in the schema

--- addons/ZEN-vagrant-builder/installer/install-pf.sh
+++ addons/ZEN-vagrant-builder/installer/install-pf.sh
@@ -30,14 +30,11 @@ echo "p@ck3tf3nc3" | passwd root --stdin
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 # Put the demo user insert statement into the schema file
-read -r -d '' DEMOUSER_SQL <<- EOM
-
+cat <<EOT >> /usr/local/pf/db/pf-schema.sql
 --
 -- Insert the demo user for testing
 --
 
 INSERT INTO password (pid, password, valid_from, expiration, access_duration, access_level, category) VALUES ('demouser', 'demouser', NOW(), '2038-01-01', '1D', NULL, 1);
 
-EOM
-
-printf $DEMOUSER_SQL >> /usr/local/pf/db/pf-schema.sql
+EOT
