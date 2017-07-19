a0927212e09d46b47bdec2b2ece3fc960ae45119
Julien Semaan <jul.semaan@gmail.com>
2017-07-19 12:52:41 -0400

    missing a few spots in deletion of pf::useragent

--- lib/pf/pfcmd/report.pm
+++ lib/pf/pfcmd/report.pm
@@ -65,8 +65,6 @@ BEGIN {
         report_statics_active
         report_unknownprints_all
         report_unknownprints_active
-        report_unknownuseragents_all
-        report_unknownuseragents_active
 
         translate_connection_type
     );
@@ -164,22 +162,6 @@ sub report_db_prepare {
 
     $report_statements->{'report_openviolations_active_sql'} = get_db_handle()->prepare(
         qq [SELECT n.pid as owner, n.mac as mac, v.status as status, v.start_date as start_date, c.description as violation from (violation v, ip4log i) LEFT JOIN node n ON v.mac=n.mac LEFT JOIN class c on c.vid=v.vid WHERE v.status="open" and n.mac=i.mac and (i.end_time=0 or i.end_time > now()) order by n.pid ]);
-
-    $report_statements->{'report_unknownuseragents_all_sql'} = get_db_handle()->prepare(qq[
-        SELECT n.user_agent, nu.browser, nu.os, n.computername, device_type as description, n.dhcp_fingerprint
-        FROM node as n
-            JOIN node_useragent AS nu USING (mac)
-        WHERE (nu.browser IS NULL OR nu.os IS NULL) AND n.user_agent != ''
-    ]);
-
-    $report_statements->{'report_unknownuseragents_active_sql'} = get_db_handle()->prepare(qq[
-        SELECT n.user_agent, nu.browser, nu.os, n.computername, device_type as description, n.dhcp_fingerprint
-        FROM node as n
-            JOIN node_useragent AS nu USING (mac)
-            LEFT JOIN ip4log USING (mac)
-        WHERE (nu.browser IS NULL OR nu.os IS NULL) AND n.user_agent != ''
-            AND (ip4log.end_time=0 OR ip4log.end_time > now())
-    ]);
 
     $report_statements->{'report_connectiontype_sql'} = get_db_handle()->prepare(qq[
         SELECT connection_type, connection_type as connection_type_orig, COUNT(DISTINCT mac) AS connections,
@@ -621,14 +603,6 @@ sub report_unknownprints_active {
         $datum->{'vendor'} = oui_to_vendor( $datum->{'mac'} );
     }
     return (@data);
-}
-
-sub report_unknownuseragents_all {
-    return db_data(REPORT, $report_statements, 'report_unknownuseragents_all_sql');
-}
-
-sub report_unknownuseragents_active {
-    return db_data(REPORT, $report_statements, 'report_unknownuseragents_active_sql');
 }
 
 sub report_connectiontype {
