4b763fc4e122ad3aa04cc2b80e241c82cdf4ecb7
Julien Semaan <jul.semaan@gmail.com>
2017-07-19 12:46:46 -0400

    Revert "config::PfDefault is dependent of config::Documentation"
    This reverts commit c01c862c3b9add08ca63a17632ce085c24e64706.

--- lib/pfconfig/namespaces/config/Documentation.pm
+++ lib/pfconfig/namespaces/config/Documentation.pm
@@ -26,7 +26,6 @@ use base 'pfconfig::namespaces::config';
 sub init {
     my ($self) = @_;
     $self->{file} = $pf_doc_file;
-    $self->{child_resources} = [ 'config::PfDefault' ];
 }
 
 sub build_child {
