c1707d4ff097cd6830e60b77b014a595bf200f1c
James Rouzier <jrouzier@inverse.ca>
2017-08-30 14:16:47 -0400

    Revert "No status message"
    This reverts commit c008addc27aa106211f494228271ed812bf65469.

--- html/pfappserver/lib/pfappserver/Model/Node.pm
+++ html/pfappserver/lib/pfappserver/Model/Node.pm
@@ -269,7 +269,7 @@ sub update {
     if ($previous_node_ref->{status} ne $node_ref->{status}) {
         # Status was modified
         if ($node_ref->{status} eq $pf::node::STATUS_REGISTERED) {
-            $result = pf::node::node_register($mac, $node_ref->{pid}, %{$node_ref});
+            ( $result, $status_msg ) = pf::node::node_register($mac, $node_ref->{pid}, %{$node_ref});
         }
         elsif ($node_ref->{status} eq $pf::node::STATUS_UNREGISTERED) {
             $result = node_deregister($mac, %{$node_ref});
