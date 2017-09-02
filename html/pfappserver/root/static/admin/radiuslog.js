c8099d0fb792ff6caa74b4f4cc0c7722297e3280
James Rouzier <jrouzier@inverse.ca>
2017-09-01 20:33:56 -0400

    Use the time of the server for searching

--- html/pfappserver/root/static/admin/radiuslog.js
+++ html/pfappserver/root/static/admin/radiuslog.js
@@ -4,26 +4,20 @@ $(function() { // DOM ready
     $('#section').on('section.loaded', function(event) {
         $('#report_radius_audit_log .radiud_audit_log_datetimepicker a').click(function(event) {
             event.preventDefault();
-            var a = event.currentTarget;
-            var timespec = a.hash.replace(/^#last/,"");
-            var amount = parseInt(/^\d+/.exec(timespec)[0]);
-            var type = /[^\d]+$/.exec(timespec)[0];
-            var milliseconds;
-            if(type === "mins") {
-                milliseconds = amount * 60 * 1000;
-            } else if( type === "hours" || type === "hour" ) {
-                milliseconds = amount * 60 * 60 * 1000;
-            }
-            var end_time = new Date();
-            var start_time = new Date(end_time.getTime() - milliseconds);
-            var start_date_input = $('#start_date');
-            var start_time_input = $('#start_time');
-            start_date_input.datepicker("setDate", start_time);
-            start_time_input.timepicker("setTime",start_time.toTimeString());
-            var end_date_input = $('#end_date');
-            var end_time_input = $('#end_time');
-            end_date_input.attr("value", "");
-            end_time_input.attr("value", "");
+            var a = $(event.currentTarget);
+            $.ajax({
+                url : a.attr('href'),
+                type : 'POST'
+            }).done(function(data){
+                var start_date_input = $('#start_date');
+                var start_time_input = $('#start_time');
+                start_date_input.datepicker("setDate", data.time_offset.start.date);
+                start_time_input.timepicker("setTime", data.time_offset.start.time);
+                var end_date_input = $('#end_date');
+                var end_time_input = $('#end_time');
+                end_date_input.datepicker("setDate", data.time_offset.end.date);
+                end_time_input.timepicker("setTime", data.time_offset.end.time);
+            });
             return false;
         });
     });
