20e9ae9bbbb14d82efce11fd78a22b9680fed65f
James Rouzier <jrouzier@inverse.ca>
2017-09-14 11:21:12 -0400

    Move inline css and javascript to seperate files

---
+++ html/pfappserver/root/static/admin/user-print.js
@@ -0,0 +1,14 @@
+  $('[name="with-aup"]').change(function(){
+    if($(this).prop('checked')) {
+      $('.page-break').removeClass('ignore');
+      $('dd.aup').removeClass('ignore');
+    }
+    else {
+      $('.page-break').each(function(){
+        if(parseInt($(this).data('index')) % 4 != 3) {
+          $(this).addClass('ignore');
+          $('dd.aup').addClass('ignore');
+        }
+      });
+    }
+  });
