2d9ccee27fd2b617bad012b6660db785555f4d5d
James Rouzier <jrouzier@inverse.ca>
2017-09-18 17:04:42 -0400

    For internal sources check to see if realm is allowed

--- lib/pf/Authentication/InternalRole.pm
+++ lib/pf/Authentication/InternalRole.pm
@@ -17,6 +17,8 @@ use warnings;
 
 use Moose::Role;
 use pf::util qw(isenabled strip_username);
+use pf::constants qw($TRUE $FALSE);
+use List::MoreUtils qw(any);
 
 has 'stripped_user_name' => (isa => 'Str', is => 'rw', default => 'yes');
 has 'realms' => (isa => 'ArrayRef[Str]', is => 'rw');
@@ -30,6 +32,24 @@ sub search_attributes {
     my $realm;
     ($username,$realm) = strip_username($username) if isenabled($self->{'stripped_user_name'});
     return $self->search_attributes_in_subclass($username);
+}
+
+=head2 realmIsAllowed
+
+Checks to see if a realm is allowed for the source
+
+A realm is allowed if realms is empty (undef or zero length)
+Or if the realm is in the list of realms
+
+=cut
+
+sub realmIsAllowed {
+    my ($self, $realm) = @_;
+    my $realms = $self->realms;
+    return $TRUE if !defined $realms || @$realms == 0;
+    $realm //= 'null';
+    $realm = lc($realm);
+    return ( any { $_ =~ /^$realm$/i } @$realms ) ? $TRUE : $FALSE;
 }
 
 =head1 AUTHOR
