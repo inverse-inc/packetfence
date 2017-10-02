1d4f4e05303ce0fc3a14ee3722629bcac5b1a8e9
James Rouzier <jrouzier@inverse.ca>
2017-10-02 10:22:27 -0400

    Remove unused function

--- html/pfappserver/lib/pfappserver/Model/Node.pm
+++ html/pfappserver/lib/pfappserver/Model/Node.pm
@@ -79,30 +79,6 @@ sub field_names {
     return [qw(mac detect_date regdate unregdate computername pid last_ip status device_class category)];
 }
 
-=head2 countAll
-
-=cut
-
-sub countAll {
-    my ( $self, %params ) = @_;
-
-    my $logger = get_logger();
-    my ($status, $status_msg);
-
-    my $count;
-    eval {
-        my @result = node_count_all(undef, %params);
-        $count = pop @result;
-    };
-    if ($@) {
-        $status_msg = "Can't count nodes from database.";
-        $logger->error($status_msg);
-        return ($STATUS::INTERNAL_SERVER_ERROR, $status_msg);
-    }
-
-    return ($STATUS::OK, $count->{nb});
-}
-
 =head2 search
 
 Used to perform a simple search
