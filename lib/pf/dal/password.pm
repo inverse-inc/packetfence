5e4faa3fdf2562bed8f9f80fdb369771c47d259c
James Rouzier <jrouzier@inverse.ca>
2017-10-06 22:36:32 -0400

    Include person fields

--- lib/pf/dal/password.pm
+++ lib/pf/dal/password.pm
@@ -18,7 +18,60 @@ use strict;
 use warnings;
 
 use base qw(pf::dal::_password);
- 
+
+my @PERSON_FIELDS = qw(firstname lastname email telephone company address notes);
+
+our @COLUMN_NAMES = (
+    (map {"password.$_|$_"} @pf::dal::_password::FIELD_NAMES),
+    'node_category.name|category',
+    (map {"person.$_|$_"} @PERSON_FIELDS)
+);
+
+use Class::XSAccessor {
+    getters => \@PERSON_FIELDS,
+};
+
+=head2 find_columns
+
+Override the standard field names
+
+=cut
+
+sub find_columns {
+    [@COLUMN_NAMES]
+}
+
+=head2 find_from_tables
+
+Join the node_category table information in the node results
+
+=cut
+
+sub find_from_tables {
+    [-join => qw(password =>{password.category=node_category.category_id} node_category =>{password.pid=person.pid} person)],
+}
+
+=head2 build_primary_keys_where_clause
+
+build_primary_keys_where_clause
+
+=cut
+
+sub build_primary_keys_where_clause {
+    my ($self, $ids) = @_;
+    return {'password.pid' => $ids->{pid} };
+}
+
+=head2 to_hash_fields
+
+to_hash_fields
+
+=cut
+
+sub to_hash_fields {
+    return [@pf::dal::_password::FIELD_NAMES, @PERSON_FIELDS];
+}
+
 =head1 AUTHOR
 
 Inverse inc. <info@inverse.ca>
