67a8b01265edbb87c559250c07b764b788cc2248
James Rouzier <jrouzier@inverse.ca>
2017-10-09 10:27:15 -0400

    Move to use pf::dal::class

--- lib/pf/class.pm
+++ lib/pf/class.pm
@@ -23,11 +23,7 @@ BEGIN {
     our ( @ISA, @EXPORT );
     @ISA = qw(Exporter);
     @EXPORT = qw(
-        class_db_prepare
-        $class_db_prepared
-
         class_view       class_view_all
-        class_trappable  class_view_actions
         class_add        class_delete
         class_merge      class_next_vid
     );
@@ -35,121 +31,56 @@ BEGIN {
 
 use pf::action;
 use pf::db;
-
-# The next two variables and the _prepare sub are required for database handling magic (see pf::db)
-our $class_db_prepared = 0;
-# in this hash reference we hold the database statements. We pass it to the query handler and he will repopulate
-# the hash if required
-our $class_statements = {};
-
-sub class_db_prepare {
-    my $logger = get_logger();
-    $logger->debug("Preparing pf::class database queries");
-
-    $class_statements->{'class_view_sql'} = get_db_handle()->prepare(
-        qq [ select class.vid,class.description,class.auto_enable,class.max_enables,class.grace_period,class.window,class.vclose,class.priority,class.template,class.max_enable_url,class.redirect_url,class.button_text,class.enabled,class.vlan,class.target_category,class.delay_by,class.external_command,group_concat(action.action order by action.action asc) as action from class left join action on class.vid=action.vid where class.vid=? GROUP BY class.vid,class.description,class.auto_enable,class.max_enables,class.grace_period,class.priority,class.template,class.max_enable_url,class.redirect_url,class.button_text,class.enabled]);
-
-    $class_statements->{'class_view_all_sql'} = get_db_handle()->prepare(
-        qq [ select class.vid,class.description,class.auto_enable,class.max_enables,class.grace_period,class.window,class.vclose,class.priority,class.template,class.max_enable_url,class.redirect_url,class.button_text,class.enabled,class.vlan,class.target_category,class.delay_by,class.external_command,group_concat(action.action order by action.action asc) as action from class left join action on class.vid=action.vid GROUP BY class.vid,class.description,class.auto_enable,class.max_enables,class.grace_period,class.priority,class.template,class.max_enable_url,class.redirect_url,class.button_text,class.enabled]);
-
-    $class_statements->{'class_view_actions_sql'} = get_db_handle()->prepare(qq [ select vid,action from action where vid=? ]);
-
-    $class_statements->{'class_exist_sql'} = get_db_handle()->prepare(qq [ select vid from class where vid=? ]);
-
-    $class_statements->{'class_delete_sql'} = get_db_handle()->prepare(qq [ delete from class where vid=? ]);
-
-    $class_statements->{'class_add_sql'} = get_db_handle()->prepare(
-        qq [ insert into class(vid,description,auto_enable,max_enables,grace_period,window,vclose,priority,template,max_enable_url,redirect_url,button_text,enabled,vlan,target_category,delay_by,external_command) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ]);
-
-    $class_statements->{'class_modify_sql'} = get_db_handle()->prepare(
-        qq [ update class set description=?,auto_enable=?,max_enables=?,grace_period=?,window=?,vclose=?,priority=?,template=?,max_enable_url=?,redirect_url=?,button_text=?,enabled=?,vlan=?,target_category=?,delay_by=?,external_command=? where vid=? ]);
-
-    $class_statements->{'class_cleanup_sql'} = get_db_handle()->prepare(
-        qq [ delete from class where vid not in (?) and vid < 1200000 and vid > 1200100 ]);
-
-    $class_statements->{'class_trappable_sql'} = get_db_handle()->prepare(
-        qq [select c.vid,c.description,c.auto_enable,c.max_enables,c.grace_period,c.window,c.vclose,c.priority,c.template,c.max_enable_url,c.redirect_url,c.button_text,c.enabled,c.vlan,c.target_category,c.delay_by,c.external_command from class c left join action a on c.vid=a.vid where a.action="trap" ]);
-
-    $class_statements->{'class_next_vid'} = get_db_handle()->prepare(
-        qq [SELECT MAX(`vid`)+1 `auto_increment_id` FROM `class`]);
-
-    $class_db_prepared = 1;
-}
+use pf::dal::class;
+use pf::error qw(is_error is_success);
 
 sub class_exist {
     my ($id) = @_;
-
-    my $query = db_query_execute(CLASS, $class_statements, 'class_exist_sql', $id) || return (0);
-    my ($val) = $query->fetchrow_hashref();
-    $query->finish();
-    return ($val);
+    my $status = pf::dal::class->does_exists({vid => $id});
+    return (is_success($id));
 }
 
 sub class_view {
     my ($id) = @_;
-
-    my $query = db_query_execute(CLASS, $class_statements, 'class_view_sql', $id) || return (0);
-    my ($val) = $query->fetchrow_hashref();
-    $query->finish();
-    return ($val);
+    my ($status, $item) = pf::dal::class->find({vid => $id});
+    if (is_error($status)) {
+        return (0);
+    }
+    return ($item->to_hash);
 }
 
 sub class_view_all {
-    return unless db_ping();
-    return db_data(CLASS, $class_statements, 'class_view_all_sql');
-}
-
-sub class_trappable {
-    return db_data(CLASS, $class_statements, 'class_trappable_sql');
-}
-
-sub class_next_vid {
-    my @row = db_data(CLASS, $class_statements, 'class_next_vid');
-    return $row[0]->{'auto_increment_id'};
-}
-
-sub class_view_actions {
-    my ($id) = @_;
-    return db_data(CLASS, $class_statements, 'class_view_actions_sql', $id);
+    my ($status, $item) = pf::dal::class->search(
+        {}, 
+        {
+            -from => pf::dal::class->find_from_tables(),
+            -columns => pf::dal::class->find_columns,
+            -group_by => 'class.vid',
+        }
+    );
+    if (is_error($status)) {
+        return;
+    }
+    return @{ $item->get_all_items(undef) // []};
 }
 
 sub class_add {
-    my $id = $_[0];
     my $logger = get_logger();
-    if ( class_exist($id) ) {
-        $logger->warn("attempt to add existing class $id");
+    my %values;
+    @values{qw(vid description auto_enable max_enables grace_period window vclose priority template max_enable_url redirect_url button_text enabled vlan target_category delay_by external_command)} = @_;
+    my $status = pf::dal::class->create(\%values);
+    if ($status == $STATUS::CONFLICT) {
         return (2);
     }
-    db_query_execute(CLASS, $class_statements, 'class_add_sql', @_) || return (0);
-    $logger->debug("class $id added");
     return (1);
 }
 
 sub class_delete {
     my ($id) = @_;
     my $logger = get_logger();
-    db_query_execute(CLASS, $class_statements, 'class_delete_sql', $id) || return (0);
+    my $status = pf::dal::class->remove_by_id({vid => $id});
     $logger->debug("class $id deleted");
-    return (1);
-}
-
-sub class_cleanup {
-    my $logger = get_logger();
-    db_query_execute(CLASS, $class_statements, 'class_cleanup_sql') || return (0);
-    $logger->debug("class cleanup completed");
-    return (1);
-}
-
-sub class_modify {
-    my $id = shift(@_);
-    my $logger = get_logger();
-    push( @_, $id );
-    if ( class_exist($id) ) {
-        $logger->debug("modify existing existing class $id");
-    }
-    db_query_execute(CLASS, $class_statements, 'class_modify_sql', @_) || return (0);
-    $logger->debug("class $id modified");
-    return (1);
+    return (is_success($status));
 }
 
 sub class_merge {
@@ -163,23 +94,29 @@ sub class_merge {
         $logger->error("error deleting actions for class $id");
         return (0);
     }
+    my %values;
+    @values{qw(vid description auto_enable max_enables grace_period window vclose priority template max_enable_url redirect_url button_text enabled vlan target_category delay_by external_command)} = @_;
+    my $item = pf::dal::action->new(\%values);
+    my $status = $item->save();
 
-    # Check for violations
-    if (class_exist($id) ) {
-        class_modify(@_);
-    }
-    else {
-        # Insert violation class
-        class_add(@_);
-    }
-
-    # Add violation actions
     if ($actions) {
         foreach my $action ( split( /\s*,\s*/, $actions ) ) {
-            pf::action::action_add( $id, $action );
+            pf::action::action_add($id, $action);
         }
     }
 
+}
+
+sub class_next_vid {
+    my ($status, $iter) = pf::dal::class->search(
+        -columns => ['MAX(`vid`+1|auto_increment_id)'],
+        -with_class => undef,
+    );
+    if (is_error($status)) {
+        return undef;
+    }
+    my $item = $iter->next // {};
+    return $item->{auto_increment_id};
 }
 
 =head1 AUTHOR
