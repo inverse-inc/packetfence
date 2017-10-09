823dc07806ad9681918ae529f3980ba7e442b57d
James Rouzier <jrouzier@inverse.ca>
2017-10-09 10:39:32 -0400

    Add a join and group by to find

--- lib/pf/dal/class.pm
+++ lib/pf/dal/class.pm
@@ -18,6 +18,57 @@ use strict;
 use warnings;
 
 use base qw(pf::dal::_class);
+use Class::XSAccessor {
+    getters => [qw(action)],
+};
+
+sub find_from_tables {
+    [-join => qw(class =>{class.vid=action.vid} action)],
+}
+
+our @COLUMN_NAMES = (
+    (map {"class.$_|$_"} @pf::dal::_class::FIELD_NAMES),
+    'group_concat(action.action order by action.action asc)|action'
+);
+
+
+=head2 find_select_args
+
+find_select_args
+
+=cut
+
+sub find_select_args {
+    my ($self, @args) = @_;
+    my $select_args = $self->SUPER::find_select_args(@args);
+    $select_args->{'-group_by'} = 'class.vid';
+    return $select_args;
+}
+
+=head2 find_columns
+
+Override the standard field names for node
+
+=cut
+
+sub find_columns {
+    [@COLUMN_NAMES]
+}
+
+sub build_primary_keys_where_clause {
+    my ($self, $ids) = @_;
+    return { 'class.vid' => $ids->{vid}}
+}
+
+=head2 to_hash_fields
+
+to_hash_fields
+
+=cut
+
+sub to_hash_fields {
+    return [@pf::dal::_class::FIELD_NAMES, qw(action)];
+}
 
 =head1 AUTHOR
 
