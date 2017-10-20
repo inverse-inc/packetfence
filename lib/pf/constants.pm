fb257be22c190ca05c2a0c93de7d7c265009b8ba
James Rouzier <jrouzier@inverse.ca>
2017-10-20 12:59:02 -0400

    Add constant zero date

--- lib/pf/constants.pm
+++ lib/pf/constants.pm
@@ -17,7 +17,7 @@ use Readonly;
 use base qw(Exporter);
 our @EXPORT = qw(
     $FALSE $TRUE $YES $NO $default_pid $admin_pid $YELLOW_COLOR $RED_COLOR $GREEN_COLOR
-    $HTTP $HTTPS $HTTP_PORT $HTTPS_PORT
+    $HTTP $HTTPS $HTTP_PORT $HTTPS_PORT $ZERO_DATE
 );
 
 # some global constants
@@ -30,6 +30,7 @@ Readonly::Scalar our $admin_pid => 'admin';
 Readonly::Scalar our $YELLOW_COLOR => 'yellow';
 Readonly::Scalar our $RED_COLOR => 'red';
 Readonly::Scalar our $GREEN_COLOR => 'green';
+Readonly::Scalar our $ZERO_DATE => '0000-00-00 00:00:00';
 
 Readonly::Hash our %BUILTIN_USERS => (
     $default_pid => 1, 
