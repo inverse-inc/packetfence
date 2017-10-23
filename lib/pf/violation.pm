6c48c1156fa856f37dd22b1c202ce6b385c7ca74
James Rouzier <jrouzier@inverse.ca>
2017-10-23 16:09:31 -0400

    Fix typo

--- lib/pf/violation.pm
+++ lib/pf/violation.pm
@@ -140,7 +140,7 @@ sub violation_modify {
     return (0) if ( !$id );
     my ($status, $existing) = pf::dal::violation->find_or_create({
         %data,
-        id => $id,
+        vid => $id,
     });
 
     if (is_error($status)) {
