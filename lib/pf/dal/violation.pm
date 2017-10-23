75f9f50de687a7a147ea35089ad8978d068284ca
James Rouzier <jrouzier@inverse.ca>
2017-10-23 17:20:06 -0400

    Join the description field

--- lib/pf/dal/violation.pm
+++ lib/pf/dal/violation.pm
@@ -18,6 +18,34 @@ use strict;
 use warnings;
 
 use base qw(pf::dal::_violation);
+use Class::XSAccessor {
+    getters => [qw(description)]
+};
+
+our @COLUMN_NAMES = (
+    @pf::dal::_violation::COLUMN_NAMES,
+    qw(class.description|description)
+);
+
+=head2 find_from_tables
+
+Join the node_category table information in the node results
+
+=cut
+
+sub find_from_tables {
+     [-join => qw(violation <=>{violation.vid=class.vid} class)]
+}
+
+=head2 find_columns
+
+Override the standard field names for violation
+
+=cut
+
+sub find_columns {
+    [@COLUMN_NAMES]
+}
  
 =head1 AUTHOR
 
