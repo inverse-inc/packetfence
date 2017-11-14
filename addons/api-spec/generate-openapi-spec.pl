806ea1c17ad3de099ff832ddfa6c96e436cd81c7
Julien Semaan <julien@semaan.ca>
2017-11-14 14:37:22 -0500

    make components be in a separate directory

---
+++ addons/api-spec/generate-openapi-spec.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::Find;
+use File::Slurp qw(read_file);
+use Data::Dumper;
+
+sub dir_yaml_files {
+    my ($dir) = @_;
+    my @files;
+    find({ wanted => sub { push @files, $_ if $_ =~ /\.yaml$/ }, follow => 1, no_chdir => 1}, $dir);
+    return @files;
+}
+
+sub indent_content {
+    my ($content, $amount_of_spaces) = @_;
+    return join("\n", map { " " x $amount_of_spaces .  $_ } split("\n", $content));
+}
+
+sub append_to_spec {
+    my ($spec, $file, $indent_level) = @_;
+    
+    my $content = read_file($file);
+    # prefix all lines with the indent level times 2 spaces
+    my $indented_content = indent_content($content, $indent_level * 2);
+    $spec .= "\n# $file \n";
+    $spec .= "$indented_content\n";
+
+    return $spec;
+}
+
+my $spec = read_file("openapi.yaml");
+
+$spec .= <<EOT;
+
+components:
+
+EOT
+
+for my $type (("schemas", "responses", "securitySchemes")) {
+
+    my @objects = dir_yaml_files("components/$type/");
+
+    $spec .= <<EOT;
+
+  $type:
+
+EOT
+
+    foreach my $object (@objects) {
+        $spec = append_to_spec($spec, $object, 2)
+    }
+
+}
+print $spec;
