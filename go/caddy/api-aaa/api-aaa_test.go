e2498ccdbe81799b4f91ee86d023b76b6e790e89
Julien Semaan <jul.semaan@gmail.com>
2017-11-16 15:46:41 -0500

    improve API AAA tests

--- go/caddy/api-aaa/api-aaa_test.go
+++ go/caddy/api-aaa/api-aaa_test.go
@@ -3,6 +3,7 @@ package apiaaa
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,6 +40,43 @@ func TestApiAAALogin(t *testing.T) {
 
 	if recorder.Code != http.StatusUnauthorized {
 		t.Error("Wrong status code from handleStart")
+	}
+
+}
+
+func TestApiAAAHandleAAA(t *testing.T) {
+
+	// The webservices credentials should have access to everything
+	req, _ := http.NewRequest(
+		"POST",
+		"/login",
+		bytes.NewBuffer([]byte(`{"username":"web", "password":"services"}`)),
+	)
+
+	recorder := httptest.NewRecorder()
+	apiAAA.handleLogin(recorder, req, httprouter.Params{})
+
+	if recorder.Code != http.StatusOK {
+		t.Error("Wrong status code from handleStart")
+	}
+
+	var tokenResp map[string]string
+	json.Unmarshal([]byte(recorder.Body.String()), &tokenResp)
+
+	token := tokenResp["token"]
+
+	if token == "" {
+		t.Error("Unable to get token to validate AAA")
+	}
+
+	req, _ = http.NewRequest("GET", "/something-that-will-hopefuly-never-exist", nil)
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	recorder = httptest.NewRecorder()
+	apiAAA.HandleAAA(recorder, req)
+
+	if recorder.Code != http.StatusOK {
+		t.Error("Wrong status code from HandleAAA")
 	}
 
 }
