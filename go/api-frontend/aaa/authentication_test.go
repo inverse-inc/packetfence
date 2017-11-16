b10982f7740ec7b133d5342012b77bd337ac7e79
Julien Semaan <jul.semaan@gmail.com>
2017-11-16 13:09:48 -0500

    add method to login in AAA

--- go/api-frontend/aaa/authentication_test.go
+++ go/api-frontend/aaa/authentication_test.go
@@ -41,3 +41,50 @@ func TestTokenAuthenticationMiddlewareIsAuthenticated(t *testing.T) {
 	}
 
 }
+
+func TestTokenAuthenticationMiddlewareLogin(t *testing.T) {
+	ctx := log.LoggerNewContext(context.Background())
+
+	backend := NewMemTokenBackend(1 * time.Second)
+	tam := NewTokenAuthenticationMiddleware(backend)
+
+	tam.AddAuthenticationBackend(NewMemAuthenticationBackend(
+		map[string]string{
+			"bob": "garauge",
+		},
+		[]string{
+			"SYSTEM_READ",
+		},
+	))
+
+	// valid login
+	auth, token, err := tam.Login(ctx, "bob", "garauge")
+
+	if !auth {
+		t.Error("Valid auth didn't pass login")
+	}
+
+	if token == "" {
+		t.Error("Valid auth returned empty token")
+	}
+
+	if err != nil {
+		t.Error("Valid auth returned error")
+	}
+
+	// invalid login
+	auth, token, err = tam.Login(ctx, "bob", "badpwd")
+
+	if auth {
+		t.Error("Invalid auth succeeded")
+	}
+
+	if token != "" {
+		t.Error("Invalid auth returned a token")
+	}
+
+	if err == nil {
+		t.Error("Invalid auth didn't return an error")
+	}
+
+}
