53f7a5dae071f4e0b3e46461f382dddd6854fa7f
James Rouzier <jrouzier@inverse.ca>
2017-11-17 11:23:04 -0500

    setCurrentTenant in authorize, update_locationlog_accounting

--- lib/pf/radius.pm
+++ lib/pf/radius.pm
@@ -123,7 +123,6 @@ sub authorize {
     }
 
     $switch->setCurrentTenant();
-
     my ($nas_port_type, $eap_type, $mac, $port, $user_name, $nas_port_id, $session_id, $ifDesc) = $switch->parseRequest($radius_request);
 
     $user_name = normalize_username($user_name, $radius_request);
@@ -386,6 +385,7 @@ sub accounting {
         return [ $RADIUS::RLM_MODULE_FAIL, ( 'Reply-Message' => "Switch is not managed by PacketFence" ) ];
     }
 
+    $switch->setCurrentTenant();
     my ($nas_port_type, $eap_type, $mac, $port, $user_name, $nas_port_id, $session_id) = $switch->parseRequest($radius_request);
 
     # update last_seen of MAC address as some activity from it has been seen
@@ -494,6 +494,7 @@ sub update_locationlog_accounting {
         return [ $RADIUS::RLM_MODULE_FAIL, ( 'Reply-Message' => "Switch is not managed by PacketFence" ) ];
     }
 
+    $switch->setCurrentTenant();
     if ($switch->supportsRoamingAccounting()) {
         my ($nas_port_type, $eap_type, $mac, $port, $user_name, $nas_port_id, $session_id, $ifDesc) = $switch->parseRequest($radius_request);
         my $locationlog_mac = locationlog_last_entry_mac($mac);
