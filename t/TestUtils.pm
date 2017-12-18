38ea674688542897aa7adc4e23362664b43c97e3
James Rouzier <jrouzier@inverse.ca>
2017-12-18 11:04:43 -0500

    Do not test binaries that start with .

--- t/TestUtils.pm
+++ t/TestUtils.pm
@@ -101,7 +101,7 @@ and return all the normal files under
 
 =cut
 
-my @excluded_binaries = qw(
+my %exclusions = map { $_ => 1 } qw(
    /usr/local/pf/bin/pfcmd
    /usr/local/pf/bin/pfhttpd
    /usr/local/pf/sbin/pfdns
@@ -109,7 +109,6 @@ my @excluded_binaries = qw(
    /usr/local/pf/bin/mysql_fingerbank_import.sh
    /usr/local/pf/addons/sourcefire/pfdetect.pl
 );
-my %exclusions = map { $_ => 1 } @excluded_binaries;
 
 sub get_all_perl_binaries {
 
@@ -129,8 +128,9 @@ sub get_all_perl_binaries {
     File::Find::find({
         wanted => sub {
             # add to list if it's a regular file
-            push(@list, $File::Find::name) if ((-f $File::Find::name) &&
-                not exists $exclusions{ $File::Find::name } );
+            my $name = $File::Find::name;
+            push(@list, $name) if ((-f $name) &&
+                (not exists $exclusions{ $name }) && $_ !~ /^\./ );
         }}, '/usr/local/pf/bin', '/usr/local/pf/sbin'
     );
 
