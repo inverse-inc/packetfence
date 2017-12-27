c81ffb7fa9b64a9ef5f742202167f6d4feb09de8
James Rouzier <jrouzier@inverse.ca>
2017-12-27 10:27:23 -0500

    Display logs

--- html/pfappserver/lib/pfappserver/Form/Config/Syslog.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Syslog.pm
@@ -18,6 +18,22 @@ with 'pfappserver::Base::Form::Role::Help';
 
 use pf::log;
 
+our @logs = qw(
+  collectd fingerbank httpd.aaa.access
+  httpd.aaa.error httpd.admin httpd.admin.access
+  httpd.admin.catalyst httpd.admin.error httpd.collector
+  httpd.collector.error httpd.graphite.access httpd.graphite.error
+  httpd.parking.access httpd.parking.error httpd.portal.access
+  httpd.portal.catalyst httpd.portal.error httpd.proxy.access
+  httpd.proxy.error httpd.webservices.access httpd.webservices.error
+  packetfence pfbandwidthd pfconfig
+  pfdetect pfdhcplistener pfdns
+  pffilter pfmon radius
+  radius-acct radius-cli radius-eduroam
+  radius-load_balancer
+  redis_cache redis_ntlm_cache redis_queue
+);
+
 ## Definition
 has_field 'id' =>
   (
@@ -26,21 +42,26 @@ has_field 'id' =>
    required => 1,
    messages => { required => 'Please specify the name of the syslog entry' },
   );
-has_field 'param1' =>
+has_field 'logs' =>
   (
-   type => 'Text',
-   required => 1,
-   messages => { required => 'Parameter 1 is required.' },
+   type => 'Select',
+   multiple => 1,
+   label => 'Logs',
+   options_method => \&options_logs,
+   default_method => sub {[ @logs] },
+   element_class => [qw(chzn-select input-xxlarge)],
+   element_attr => {'data-placeholder' => 'Click to add a log'},
+   tags => { after_element => \&help,
+             help => 'Logs' },
   );
-has_field 'param2' =>
-  (
-   type => 'Text',
-  );
-
 has_block  definition =>
   (
-    render_list => [qw(param1 param2)],
+    render_list => [qw(logs)],
   );
+
+sub options_logs {
+    return map { { label => $_, value => $_ } } @logs;
+}
 
 =head1 COPYRIGHT
 
