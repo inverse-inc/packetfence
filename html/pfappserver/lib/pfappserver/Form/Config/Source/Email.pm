aa5e72e7d40862732c8c70a02516aa91c38f5be2
Julien Semaan <jul.semaan@gmail.com>
2018-01-15 13:38:28 -0500

    Instruct to restart haproxy after changing activation domain
    fixes #2841

--- html/pfappserver/lib/pfappserver/Form/Config/Source/Email.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Source/Email.pm
@@ -47,7 +47,7 @@ has_field 'activation_domain' =>
    required => 0,
     tags => {
         after_element => \&help,
-        help => 'Set this value if you want to change the hostname in the validation link.',
+        help => 'Set this value if you want to change the hostname in the validation link. Changing this requires to restart haproxy to be fully effective.',
     },
   );
 
