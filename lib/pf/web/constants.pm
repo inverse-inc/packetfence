815a96b14bbd5a6665a820a2683e5fa5b5e3d55d
Julien Semaan <jul.semaan@gmail.com>
2018-01-16 15:37:40 -0500

    Add /Meraki::MR_v2 as a valid portal URL
    fixes #2903

--- lib/pf/web/constants.pm
+++ lib/pf/web/constants.pm
@@ -104,6 +104,7 @@ Readonly::Scalar our $EXT_URL_CISCO_CATALYST_2960   => '^/Cisco::Catalyst_2960';
 Readonly::Scalar our $EXT_URL_CISCO_WLC             => '^/Cisco::WLC';
 Readonly::Scalar our $EXT_URL_COOVACHILLI           => '^/CoovaChilli';
 Readonly::Scalar our $EXT_URL_MERAKI                => '^/Meraki::MR';
+Readonly::Scalar our $EXT_URL_MERAKI_V2             => '^/Meraki::MR_v2';
 Readonly::Scalar our $EXT_URL_RUCKUS                => '^/Ruckus';
 Readonly::Scalar our $EXT_URL_RUCKUS_SMARTZONE      => '^/RuckusSmartZone';
 Readonly::Scalar our $EXT_URL_XIRRUS                => '^/Xirrus';
