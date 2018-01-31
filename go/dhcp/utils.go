09eeedf31ccf210e25856942daf48e0c25329341
Durand Fabrice <fdurand@inverse.ca>
2018-01-31 11:48:30 -0500

    Moved etcd functions in his own file

--- go/dhcp/utils.go
+++ go/dhcp/utils.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/binary"
 	"fmt"
@@ -11,7 +10,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/coreos/etcd/client"
 	"github.com/inverse-inc/packetfence/go/pfconfigdriver"
 )
 
@@ -99,57 +97,6 @@ func InterfaceScopeFromMac(MAC string) string {
 		}
 	}
 	return NetWork
-}
-
-// etcdInit initiate the connection to etcd
-func etcdInit() *client.Config {
-	cfg := client.Config{
-		Endpoints: []string{"http://127.0.0.1:2379"},
-		Transport: client.DefaultTransport,
-		// set timeout per request to fail fast when the target endpoint is unavailable
-		HeaderTimeoutPerRequest: time.Second,
-	}
-	return &cfg
-}
-
-func etcdInsert(key string, value string) bool {
-	c, err := client.New(*Capi)
-	if err != nil {
-		return false
-	}
-	kapi := client.NewKeysAPI(c)
-	_, err = kapi.Set(context.Background(), "/dhcpd/"+key, value, nil)
-	if err != nil {
-		return false
-	} else {
-		return true
-	}
-}
-
-func etcdGet(key string) (string, string) {
-	c, err := client.New(*Capi)
-	if err != nil {
-		return "", ""
-	}
-	kapi := client.NewKeysAPI(c)
-	resp, err := kapi.Get(context.Background(), "/dhcpd/"+key, nil)
-	if err != nil {
-		return "", ""
-	}
-	return resp.Node.Key, resp.Node.Value
-}
-
-func etcdDel(key string) bool {
-	c, err := client.New(*Capi)
-	if err != nil {
-		return false
-	}
-	kapi := client.NewKeysAPI(c)
-	_, err = kapi.Delete(context.Background(), "/dhcpd/"+key, nil)
-	if err != nil {
-		return false
-	}
-	return true
 }
 
 // Detect the vip on each interfaces
