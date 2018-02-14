077feb3d08c081d2a7caa391ef409ec188045d45
James Rouzier <jrouzier@inverse.ca>
2018-02-14 11:01:32 -0500

    Do not instatisate the config store until it is needed

--- lib/pf/UnifiedApi/Controller/Config.pm
+++ lib/pf/UnifiedApi/Controller/Config.pm
@@ -85,11 +85,11 @@ sub create {
     if (!defined $item) {
         return 0;
     }
-    my $cs = $self->config_store;
     my $id = $item->{id};
     if ($cs->hasId($id)) {
         return $self->render_error(409, "An attempt to add a duplicate entry was stopped. Entry already exists and should be modified instead of created");
     }
+    my $cs = $self->config_store;
     delete $item->{id};
     $cs->create($id, $item);
     $cs->commit;
