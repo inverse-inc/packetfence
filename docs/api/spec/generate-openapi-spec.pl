2193ad4d100c16e0404b5676f65204209c2346b7
James Rouzier <jrouzier@inverse.ca>
2018-02-26 10:37:34 -0500

    Do not use text concatenation to build yaml

--- docs/api/spec/generate-openapi-spec.pl
+++ docs/api/spec/generate-openapi-spec.pl
@@ -3,117 +3,32 @@
 use strict;
 use warnings;
 
+use lib qw(/usr/local/pf/lib);
+use pf::file_paths qw($install_dir);
 use File::Find;
 use File::Slurp qw(read_file write_file);
 use Data::Dumper;
-use YAML::XS;
+use JSON::MaybeXS qw();
+use YAML::XS qw(:all);
+use JSON::PP qw();
+$YAML::XS::Boolean = "JSON::PP";
+my $base_path = "$install_dir/docs/api/spec";
 
-sub dir_yaml_files {
-    my ($dir) = @_;
-    my @files;
-    find({ wanted => sub { push @files, $_ if $_ =~ /\.yaml$/ }, follow => 1, no_chdir => 1}, $dir);
-    return sort @files;
-}
+my $spec = LoadFile("$base_path/openapi-base.yaml");
+use Data::Dumper;
+print Dumper($spec);
 
-sub indent_content {
-    my ($content, $amount_of_spaces) = @_;
-    return join("\n", map { " " x $amount_of_spaces .  $_ } split("\n", $content));
-}
+merge_yaml_into_component($spec->{paths}, "paths/");
 
-sub append_to_spec {
-    my ($spec, $file, $indent_level) = @_;
-    
-    my $content = read_file($file);
-    # prefix all lines with the indent level times 2 spaces
-    my $indented_content = indent_content($content, $indent_level * 2);
-    $spec .= "\n# beginning of: $file \n";
-    $spec .= "$indented_content\n";
-    $spec .= "\n# end of: $file \n";
-
-    return $spec;
-}
-
-sub common_parameter {
-    my ($yaml_spec, $parameter) = @_;
-    for my $path (keys(%{$yaml_spec->{paths}})) {
-        for my $method (keys(%{$yaml_spec->{paths}->{$path}})) {
-            unless(defined($yaml_spec->{paths}->{$path}->{$method}->{parameters})) {
-                $yaml_spec->{paths}->{$path}->{$method}->{parameters} = [];
-            }
-            push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, $parameter;
-        }
-    }
-}
-
-sub insert_search_parameters {
-    my ($yaml_spec) = @_;
-    for my $path (keys(%{$yaml_spec->{paths}})) {
-        if($path =~ /\/search$/) {
-            for my $method (keys(%{$yaml_spec->{paths}->{$path}})) {
-                unless(defined($yaml_spec->{paths}->{$path}->{$method}->{parameters})) {
-                    $yaml_spec->{paths}->{$path}->{$method}->{parameters} = [];
-                }
-                push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, {'$ref' => "#/components/parameters/cursor"};
-                push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, {'$ref' => "#/components/parameters/limit"};
-                push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, {'$ref' => "#/components/parameters/search_query"};
-                push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, {'$ref' => "#/components/parameters/fields"};
-                push @{$yaml_spec->{paths}->{$path}->{$method}->{parameters}}, {'$ref' => "#/components/parameters/sort"};
-            }
-        }
-    }
-}
-
-my $spec = read_file("openapi-base.yaml");
-
-$spec .= <<EOT;
-
-paths:
-
-EOT
-
-my @objects = dir_yaml_files("paths/");
-
-foreach my $object (@objects) {
-    $spec = append_to_spec($spec, $object, 1)
-}
-
-$spec = join("\n", map { $_ !~ /^\s*$/ ? $_ : () } split("\n", $spec));
-
-$spec .= <<EOT;
-
-components:
-
-EOT
+my $components = $spec->{components};
 
 for my $type (("schemas", "responses", "securitySchemes", "parameters")) {
-
-    my @objects = dir_yaml_files("components/$type/");
-
-    $spec .= <<EOT;
-
-  $type:
-
-EOT
-
-    foreach my $object (@objects) {
-        $spec = append_to_spec($spec, $object, 2)
-    }
-
+    my %component;
+    $components->{$type} = \%component;
+    merge_yaml_into_component(\%component, "components/$type/");
 }
 
-my $yaml_spec ;
-
-eval {
-    $yaml_spec = YAML::XS::Load($spec);
-};
-
-if($@) {
-    write_file("err.yaml", $spec);
-    print STDERR "Error while decoding YAML: $@ \n";
-    exit 1;
-}
-
-common_parameter($yaml_spec, {
+common_parameters($spec, {
     'required' => 0,
     'in' => 'header',
     'name' => 'X-PacketFence-Tenant-Id',
@@ -123,9 +38,55 @@ common_parameter($yaml_spec, {
     'description' => 'The tenant ID to use for this request. Can only be used if the API user has access to other tenants. When empty, it will default to use the tenant attached to the token.'
 });
 
-insert_search_parameters($yaml_spec);
+insert_search_parameters($spec);
 
-YAML::XS::DumpFile("openapi.yaml", $yaml_spec);
+YAML::XS::DumpFile("$base_path/openapi.yaml", $spec);
 
-system("js-yaml openapi.yaml > openapi.json");
+write_file("$base_path/openapi.json", JSON::MaybeXS->new->pretty(1)->canonical(1)->encode($spec));
+
+
+sub dir_yaml_files {
+    my ($dir) = @_;
+    my @files;
+    find({ wanted => sub { push @files, $_ if $_ =~ /\.yaml$/ }, follow => 1, no_chdir => 1}, "$base_path/$dir");
+    return sort @files;
+}
+
+sub common_parameters {
+    my ($yaml_spec, @parameters) = @_;
+    for my $path (values %{$yaml_spec->{paths}}) {
+        for my $method (values %$path) {
+            push @{$method->{parameters}}, @parameters;
+        }
+    }
+}
+
+sub insert_search_parameters {
+    my ($yaml_spec) = @_;
+    while ( my ( $name, $path ) = each %{ $yaml_spec->{paths} } ) {
+        next if $name !~ m#/search#;
+        for my $method ( values( %{$path} ) ) {
+            push @{ $method->{parameters} },
+              { '$ref' => "#/components/parameters/cursor" },
+              { '$ref' => "#/components/parameters/limit" },
+              { '$ref' => "#/components/parameters/search_query" },
+              { '$ref' => "#/components/parameters/fields" },
+              { '$ref' => "#/components/parameters/sort" };
+        }
+    }
+}
+
+sub merge_yaml_into_component {
+    my ($component, $path) = @_;
+    my @files = dir_yaml_files($path);
+    for my $file (@files) {
+        my $object = eval{ LoadFile($file) };
+        if ($@) {
+            die "$file : $@\n"
+        }
+        while (my ($k, $v) = each %$object) {
+            $component->{$k} = $v;
+        }
+    }
+}
 
