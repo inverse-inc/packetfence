b32d2f169b2a4682cdf67e2bb0cd4cc24d15b964
Darren Satkunas <darren@satkunas.com>
2018-02-27 03:27:47 -0500

    Fix parking route

--- lib/pf/parking.pm
+++ lib/pf/parking.pm
@@ -48,7 +48,7 @@ sub park {
     my ($mac,$ip) = @_;
     get_logger->debug("Setting client in parking");
     if(isenabled($Config{parking}{place_in_dhcp_parking_group})){
-        pf::api::unifiedapiclient->default_client->call("POST", "/api/v1/dhcp/options/mac/".$mac."/", [{
+        pf::api::unifiedapiclient->default_client->call("POST", "/api/v1/dhcp/options/mac/".$mac, [{
             "option"      => "51",
             "value"       => "3600",
             "type"        => "int",
