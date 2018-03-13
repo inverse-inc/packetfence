645a7fece9b4e5a7ec639496b3596bcba06e3fb3
James Rouzier <jrouzier@inverse.ca>
2018-03-13 15:57:04 -0400

    Fix using the wrong field for id

--- lib/pf/violation.pm
+++ lib/pf/violation.pm
@@ -140,7 +140,7 @@ sub violation_modify {
     return (0) if ( !$id );
     my ($status, $existing) = pf::dal::violation->find_or_create({
         %data,
-        vid => $id,
+        id => $id,
     });
 
     if (is_error($status)) {
