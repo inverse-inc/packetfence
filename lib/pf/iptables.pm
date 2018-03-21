3779bc353b71aaf4056675525150891ded42fc7e
Julien Semaan <jul.semaan@gmail.com>
2018-03-21 09:15:10 -0400

    add unified API port to iptables

--- lib/pf/iptables.pm
+++ lib/pf/iptables.pm
@@ -124,6 +124,7 @@ sub iptables_generate {
     $tags{'web_admin_port'} = $Config{'ports'}{'admin'};
     $tags{'webservices_port'} = $Config{'ports'}{'soap'};
     $tags{'aaa_port'} = $Config{'ports'}{'aaa'};
+    $tags{'unifiedapi_port'} = $Config{'ports'}{'unifiedapi'};
     $tags{'httpd_portal_modstatus'} = $Config{'ports'}{'httpd_portal_modstatus'};
     $tags{'httpd_collector_port'} = $Config{'ports'}{'collector'};
     # FILTER
