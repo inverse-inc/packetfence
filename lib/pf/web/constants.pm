4fe58eaa7743b50efc6a7c204d386102b31df201
lzammit <lzammit@inverse.ca>
2018-03-22 11:44:09 -0400

    Improve the Ubiquiti webauth URL detection
    I love baguettes

--- lib/pf/web/constants.pm
+++ lib/pf/web/constants.pm
@@ -111,7 +111,7 @@ Readonly::Scalar our $EXT_URL_XIRRUS                => '^/Xirrus';
 Readonly::Scalar our $EXT_URL_MIKROTIK              => '^/Mikrotik';
 Readonly::Scalar our $EXT_URL_FORTIGATE             => '^/Fortinet::FortiGate';
 # Ubiquiti doesn't support setting the URL so we much detect it using this URL which will then map to the Ubiquiti module in pf::web::externalportal
-Readonly::Scalar our $EXT_URL_UBIQUITI              => '^/guest/s/default';
+Readonly::Scalar our $EXT_URL_UBIQUITI              => '^/guest/s/[a-zA-Z0-9]+/';
 
 # Provisioning engine
 Readonly::Scalar our $URL_WIRELESS_PROFILE => '/wireless-profile.mobileconfig';
