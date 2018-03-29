32b6fd6c851ed9181e024fe66ec229a297c7ce35
James Rouzier <jrouzier@inverse.ca>
2018-03-29 14:31:41 -0400

    Add the openapi_path_generator_class

--- lib/pf/UnifiedApi/Controller/Config.pm
+++ lib/pf/UnifiedApi/Controller/Config.pm
@@ -15,9 +15,11 @@ pf::UnifiedApi::Controller::Config
 use strict;
 use warnings;
 use Mojo::Base qw(pf::UnifiedApi::Controller::RestRoute);
+use pf::UnifiedApi::OpenAPI::PathGenerator::Config;
 
 has 'config_store_class';
 has 'form_class';
+has 'openapi_path_generator_class' => 'pf::UnifiedApi::OpenAPI::PathGenerator::Config';
 
 sub list {
     my ($self) = @_;
