da2d7ce07c3fa66b574a3d9dbb13c14f69105ca6
James Rouzier <jrouzier@inverse.ca>
2018-04-08 15:11:05 -0400

    Add a rest api call

--- go/detect/detect.go
+++ go/detect/detect.go
@@ -1,19 +1,28 @@
 package detect
 
 type Parser interface {
-    Parse(string) ([]ApiCall, error)
+	Parse(string) ([]ApiCall, error)
 }
 
-type ApiCall interface{
-   Call() error 
+type ApiCall interface {
+	Call() error
 }
 
 type JsonRpcApiCall struct {
-    Method string
-    Params interface{} 
+	Method string
+	Params interface{}
 }
 
 func (*JsonRpcApiCall) Call() error {
-    return nil
+	return nil
 }
 
+type RestApiCall struct {
+	HttpMethod string
+	Path       string
+	Content    string
+}
+
+func (*RestApiCall) Call() error {
+	return nil
+}
