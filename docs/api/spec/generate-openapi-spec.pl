8a099431d57a303c45457aa1f2569ae5df0e2c68
James Rouzier <jrouzier@inverse.ca>
2018-04-25 13:58:56 -0400

    Change how components and paths are merged

--- docs/api/spec/generate-openapi-spec.pl
+++ docs/api/spec/generate-openapi-spec.pl
@@ -16,25 +16,23 @@ my $base_path = "$install_dir/docs/api/spec";
 
 my $spec = LoadFile("$base_path/openapi-base.yaml");
 
-merge_yaml_into_component($spec->{paths}, "paths/");
+merge_yaml_into_paths($spec->{paths}, "paths");
 
-my $components = $spec->{components};
+$spec->{components} = merge_yaml_components("components");
 
-for my $type (("schemas", "responses", "securitySchemes", "parameters")) {
-    my %component;
-    $components->{$type} = \%component;
-    merge_yaml_into_component(\%component, "components/$type/");
-}
-
-common_parameters($spec, {
-    'required' => 0,
-    'in' => 'header',
-    'name' => 'X-PacketFence-Tenant-Id',
-    'schema' => {
-                  'type' => 'string'
-                },
-    'description' => 'The tenant ID to use for this request. Can only be used if the API user has access to other tenants. When empty, it will default to use the tenant attached to the token.'
-});
+common_parameters(
+    $spec,
+    {
+        'required' => 0,
+        'in'       => 'header',
+        'name'     => 'X-PacketFence-Tenant-Id',
+        'schema'   => {
+            'type' => 'string'
+        },
+        'description' =>
+'The tenant ID to use for this request. Can only be used if the API user has access to other tenants. When empty, it will default to use the tenant attached to the token.'
+    }
+);
 
 insert_search_parameters($spec);
 
@@ -79,7 +77,26 @@ sub insert_search_parameters {
     }
 }
 
-sub merge_yaml_into_component {
+sub merge_yaml_components {
+    my ($dir) = @_;
+    my %all;
+    my $full_path = "$base_path/$dir";
+    for my $file (dir_yaml_files($dir)) {
+        my $path_parts = $file;
+        $path_parts =~ s#\Q$full_path\E/##;
+        my @parts = split ('/', $path_parts);
+        my $object = LoadFile($file);
+        my $root = \%all;
+        pop @parts;
+        for my $part (@parts) {
+            $root = $root->{$part} //= {};
+        }
+        %$root = (%$root, %$object);
+    }
+    return \%all;
+}
+
+sub merge_yaml_into_paths {
     my ($component, $path) = @_;
     my @files = dir_yaml_files($path);
     for my $file (@files) {
