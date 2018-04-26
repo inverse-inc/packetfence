36c65e7e5bf8e668ff1e98109c1f1f027867d48a
James Rouzier <jrouzier@inverse.ca>
2018-04-26 16:28:44 -0400

    Add new dhcp parser

---
+++ go/detect/dhcp.go
@@ -0,0 +1,49 @@
+package detect
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var dhcpRegexPattern1 = regexp.MustCompile(`(DHCPDISCOVER|DHCPOFFER|DHCPREQUEST|DHCPACK|DHCPRELEASE|DHCPINFORM|DHCPEXPIRE) on ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) to ([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})`)
+
+var dhcpRegexPattern2 = regexp.MustCompile(`(DHCPDISCOVER|DHCPOFFER|DHCPREQUEST|DHCPACK|DHCPRELEASE|DHCPINFORM|DHCPEXPIRE) to ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) \(([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})\)`)
+
+type DhcpParser struct {
+	Pattern1, Pattern2 *regexp.Regexp
+}
+
+func (s *DhcpParser) Parse(line string) ([]ApiCall, error) {
+	if matches := s.Pattern1.FindStringSubmatch(line); matches != nil && matches[1] == "DHCPACK" {
+		return []ApiCall{
+			&JsonRpcApiCall{
+				Method: "update_ip4log",
+				Params: []interface{}{
+					"mac", matches[3],
+					"ip", matches[2],
+				},
+			},
+		}, nil
+	}
+
+	if matches := s.Pattern2.FindStringSubmatch(line); matches != nil && matches[1] == "DHCPACK" {
+		return []ApiCall{
+			&JsonRpcApiCall{
+				Method: "update_ip4log",
+				Params: []interface{}{
+					"mac", matches[3],
+					"ip", matches[2],
+				},
+			},
+		}, nil
+	}
+
+	return nil, fmt.Errorf("Error parsing")
+}
+
+func NewDhcpParser() Parser {
+	return &DhcpParser{
+		Pattern1: dhcpRegexPattern1.Copy(),
+		Pattern2: dhcpRegexPattern2.Copy(),
+	}
+}
---
+++ go/detect/dhcp_test.go
@@ -0,0 +1,50 @@
+package detect
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestDhcpParse(t *testing.T) {
+	var parseTests = []struct {
+		line  string
+		calls []ApiCall
+	}{
+		{
+			line: "Sep  1 03:27:04 172.22.0.3 dhcpd[20512]: DHCPACK to 172.19.16.171 (00:11:22:33:44:55) via eth1",
+			calls: []ApiCall{
+				&JsonRpcApiCall{
+					Method: "update_ip4log",
+					Params: []interface{}{
+						"mac", "00:11:22:33:44:55",
+						"ip", "172.19.16.171",
+					},
+				},
+			},
+		},
+		{
+			line: "Sep  1 03:27:05 172.26.0.139 dhcpd[14557]: DHCPACK on 10.16.86.122 to 00:11:22:33:44:55 (blabla-computer) via eth2 relay eth2 lease-duration 86400 (RENEW) uid 00:11:22:33:44:55",
+			calls: []ApiCall{
+				&JsonRpcApiCall{
+					Method: "update_ip4log",
+					Params: []interface{}{
+						"mac", "00:11:22:33:44:55",
+						"ip", "10.16.86.122",
+					},
+				},
+			},
+		},
+	}
+	parser := NewDhcpParser()
+	for i, test := range parseTests {
+		calls, err := parser.Parse(test.line)
+		if err != nil {
+			t.Errorf("Error Parsing %d) %s: %v", i, test.line)
+			continue
+		}
+
+		if !cmp.Equal(calls, test.calls) {
+			t.Errorf("Expected ApiCall Failed for %d %v) %s", i, test.line, calls)
+		}
+	}
+}
