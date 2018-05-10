52c92a0dbb81f1d500202d5031fa5ac27b5856f6
Durand Fabrice <oeufdure@gmail.com>
2018-05-10 13:12:09 -0400

    Fixed condition

--- html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Authentication/Login.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Authentication/Login.pm
@@ -202,7 +202,7 @@ sub authenticate {
         if (!defined $return || $return == $LOGIN_FAILURE) {
             pf::auth_log::record_auth(join(',',map { $_->id } @{$sources}), $self->current_mac, $username, $pf::auth_log::FAILED, $self->app->profile->name);
             while(my ($action, $params) = each %{$self->actions}){
-                if ($action eq 'on_failure' && @{$params} > 1) {
+                if ($action eq 'on_failure' && @{$params} > 0) {
                      $self->app->session->{'sub_root_module_id'} = @{$params}[0];
                      $self->redirect_root();
                      return;
@@ -230,7 +230,7 @@ sub authenticate {
             # Logging USER/IP/MAC of the just-authenticated user
             get_logger->info("Successfully authenticated ".$username);
             while(my ($action, $params) = each %{$self->actions}){
-                if ($action eq 'on_success' && @{$params} > 1) {
+                if ($action eq 'on_success' && @{$params} > 0) {
                      $self->app->session->{'sub_root_module_id'} = @{$params}[0];
                      $self->redirect_root();
                      return;
