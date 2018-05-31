4e54cd83fd7b649cc24d8c993129f82cb99ee067
Durand Fabrice <oeufdure@gmail.com>
2018-05-30 22:52:54 -0400

    moved sql request in upgrade sql schema

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -50,8 +50,9 @@ call ValidateVersion;
 ALTER TABLE node
     MODIFY device_score int DEFAULT NULL;
 
-INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
-
 ALTER TABLE person
     ADD `psk` varchar(255) NULL DEFAULT NULL
 ;
+
+INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
+
