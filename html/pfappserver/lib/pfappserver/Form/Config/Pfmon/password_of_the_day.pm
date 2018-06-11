05a66f7a7ee20ffac063c9c56713db5180f9ff97
Durand Fabrice <oeufdure@gmail.com>
2018-06-11 11:45:37 -0400

    Email generation

---
+++ html/pfappserver/lib/pfappserver/Form/Config/Pfmon/password_of_the_day.pm
@@ -0,0 +1,60 @@
+package pfappserver::Form::Config::Pfmon::password_of_the_day;
+
+=head1 NAME
+
+pfappserver::Form::Config::Pfmon::password_of_the_day - Web form for password_of_the_day pfmon task
+
+=head1 DESCRIPTION
+
+Web form for password_of_the_day pfmon task
+
+=cut
+
+use HTML::FormHandler::Moose;
+
+extends 'pfappserver::Form::Config::Pfmon';
+with 'pfappserver::Base::Form::Role::Help';
+
+
+=head2 default_type
+
+default value of type
+
+=cut
+
+sub default_type {
+    return "password_of_the_day";
+}
+
+has_block  definition =>
+  (
+    render_list => [qw(type status interval)],
+  );
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2018 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+__PACKAGE__->meta->make_immutable unless $ENV{"PF_SKIP_MAKE_IMMUTABLE"};
+
+1;
