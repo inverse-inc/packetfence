4a7abf87fd5b83cb4c039e843a3d36cfe74a7987
Durand Fabrice <oeufdure@gmail.com>
2018-06-11 16:37:28 -0400

    Added password size support

--- lib/pf/Authentication/Source/PotdSource.pm
+++ lib/pf/Authentication/Source/PotdSource.pm
@@ -23,6 +23,7 @@ has '+type' => ( default => 'Potd' );
 has 'user' => (isa => 'Str', is => 'rw', required => 1);
 has 'password_rotation' => (isa => 'Str', is => 'rw', default => '10m');
 has 'password_email_update' => (isa => 'Maybe[Str]', is => 'rw');
+has 'password_length' => (isa => 'Maybe[Int]', is => 'rw', default => 8);
 
 =head2 authenticate
 
