281468f5eb2292bf8042a4b2a86749c901a18412
Durand Fabrice <oeufdure@gmail.com>
2018-06-11 11:45:37 -0400

    Email generation

--- lib/pf/pfmon/task/password_of_the_day.pm
+++ lib/pf/pfmon/task/password_of_the_day.pm
@@ -19,6 +19,8 @@ use pf::person;
 use pf::password;
 use pf::authentication;
 use pf::util;
+use pf::web qw (i18n_format );
+use pf::web::guest;
 use DateTime;
 use DateTime::Format::MySQL;
 use pf::log;
@@ -32,25 +34,48 @@ run the password generation task
 =cut
 
 sub run {
-    my $now = DateTime->now();
+    my ( $self ) = @_;
+    my $now = DateTime->now(time_zone => "local");
     my $logger = get_logger();
-    my @sources = pf::authentication::getAuthenticationSourcesByType("Potd");
+    my $sources = pf::authentication::getAuthenticationSourcesByType("Potd");
     my $new_password;
-    foreach my $source (@sources) {
+    foreach my $source (@{$sources}) {
         unless (person_exist($source->{user})) {
             $logger->info("Create Person $source->{user}");
             person_add($source->{user});
-            $new_password = pf::password::generate($source->{user},{type => 'valid_from', value => $now},undef,'0');
+            $new_password = pf::password::generate($source->{user},[{type => 'valid_from', value => $now},{type => 'expiration', value => pf::config::access_duration($source->{password_rotation})}],undef,'0');
+            $self->send_email((pid => $source->{user}, password => $new_password, email => $source->{password_email_update}));
         }
         my $password = pf::password::view($source->{user});
         if(defined($password)){
             my $valid_from = $password->{valid_from};
             $valid_from = DateTime::Format::MySQL->parse_datetime($valid_from);
-            if ($now->subtract_datetime_absolute( $valid_from ) > pf::util::normalize_time($source->{password_rotation})) {
-                $new_password = pf::password::generate($source->{user},{type => 'valid_from', value => $now},undef,'0');
+            if ( ($now->epoch - $valid_from->epoch) > pf::util::normalize_time($source->{password_rotation})) {
+                $new_password = pf::password::generate($source->{user},[{type => 'valid_from', value => $now},{type => 'expiration', value => pf::config::access_duration($source->{password_rotation})}],undef,'0');
+                $self->send_email((pid => $source->{user},password => $new_password, email => $source->{password_email_update}));
             }
         }
     }
+}
+
+=head2 send_email
+
+send the password of the day to the email addresses
+
+=cut
+
+sub send_email {
+    my ( $self, %info ) = @_;
+    %info = (
+        'subject'   => i18n_format(
+            "New password of the day"
+        ),
+        %info
+    );
+    pf::web::guest::send_template_email(
+            $pf::web::guest::TEMPLATE_EMAIL_PASSWORD_OF_THE_DAY, $info{'subject'}, \%info
+    );
+
 }
 
 =head1 AUTHOR
