4e99eb901f9137c1fa461777974c6565964d38c4
Julien Semaan <jul.semaan@gmail.com>
2018-06-15 11:51:45 -0400

    add getVersion sub in Cambium

--- lib/pf/Switch/Cambium.pm
+++ lib/pf/Switch/Cambium.pm
@@ -50,6 +50,28 @@ sub supportsExternalPortal { return $TRUE; }
 sub supportsWebFormRegistration { return $TRUE }
 sub inlineCapabilities { return ($MAC,$PORT); }
 
+sub getVersion {
+    my ($self)       = @_;
+    my $oid_sysDescr = '1.3.6.1.2.1.1.1.0';
+    my $logger       = $self->logger;
+    if ( !$self->connectRead() ) {
+        return '';
+    }
+    $logger->trace("SNMP get_request for sysDescr: $oid_sysDescr");
+    my $result = $self->{_sessionRead}->get_request( -varbindlist => [$oid_sysDescr] );
+    my $sysDescr = ( $result->{$oid_sysDescr} || '' );
+
+    # sysDescr sample output:
+    #EdgeSwitch 48-Port 750W, 1.7.0.4922887, Linux 3.6.5-f4a26ed5, 0.0.0.0000000
+
+    if ( $sysDescr =~ m/, (\d+\.\d+-\d+),/ ) {
+        return $1;
+    } else {
+        $logger->warn("couldn't extract exact version information, returning SNMP System Description instead");
+        return $sysDescr;
+    }
+}
+
 =item deauthenticateMacDefault
 
 De-authenticate a MAC address from wireless network (including 802.1X).
