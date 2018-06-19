a31b1710a27c60518b10175a2439cc1175907c67
Darren Satkunas <darren@satkunas.com>
2018-06-19 10:52:35 -0400

    Replace dropped API methods

--- lib/pf/UnifiedApi.pm
+++ lib/pf/UnifiedApi.pm
@@ -102,7 +102,7 @@ our @API_V1_ROUTES = (
         controller => 'Violations',
         collection => {
             subroutes    => {
-                'by_mac/#search' => { get => 'by_mac' },                
+                'by_mac/#search' => { get => 'by_mac' },
                 'search' => {
                     'post' => 'search'
                 },
@@ -162,10 +162,12 @@ our @API_V1_ROUTES = (
     {
         controller  => 'Ip4logs',
         collection => {
-            subroutes    => {
+            subroutes => {
                 'history/#search' => { get => 'history' },
                 'archive/#search' => { get => 'archive' },
-                'open/#search' => { get => 'open' }, 
+                'open/#search' => { get => 'open' },
+                'mac2ip/#mac' => { get => 'mac2ip' },
+                'ip2mac/#ip'  => { get => 'ip2mac' },
                 'search' => {
                     'post' => 'search'
                 },
@@ -175,10 +177,12 @@ our @API_V1_ROUTES = (
     {
         controller  => 'Ip6logs',
         collection => {
-            subroutes    => {
+            subroutes => {
                 'history/#search' => { get => 'history' },
                 'archive/#search' => { get => 'archive' },
                 'open/#search' => { get => 'open' }, 
+                'mac2ip/#mac' => { get => 'mac2ip' },
+                'ip2mac/#ip'  => { get => 'ip2mac' },
                 'search' => {
                     'post' => 'search'
                 },
@@ -252,6 +256,17 @@ our @API_V1_ROUTES = (
             },
             subroutes => undef,
         },
+    },
+    {
+        controller => 'Queues',
+        collection => {
+            subroutes    => {
+                'stats' => {
+                    get => 'stats'
+                },
+            },
+        },
+        resource => undef,
     },
 );
 
