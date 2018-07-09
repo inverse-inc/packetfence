52832c1ec007df4336780516f8971890d14285c5
Julien Semaan <jul.semaan@gmail.com>
2018-07-09 14:11:04 -0400

    fix failing golang test in AAA

--- go/api-frontend/aaa/paths_test.go
+++ go/api-frontend/aaa/paths_test.go
@@ -1,22 +1,21 @@
 package aaa
 
 import (
-    "testing"
+	"testing"
 )
 
-
 func TestIsPublic(t *testing.T) {
-    for _, test := range []struct{
-                        path string
-                        pass bool
-                    }{
-                        {"/api/v1/translation", false},
-                        {"/api/v1/translation/fr", true},
-                        {"/api/v1/translations", true},
-                    } {
-        if isPathPublic(test.path) != test.pass {
-            t.Errorf("%s failed public test expected %v",test.path, test.pass);
-        }
-    }
+	for _, test := range []struct {
+		path string
+		pass bool
+	}{
+		{"/api/v1/translation", false},
+		{"/api/v1/translation/fr", true},
+		{"/api/v1/translations", true},
+	} {
+		if IsPathPublic(test.path) != test.pass {
+			t.Errorf("%s failed public test expected %v", test.path, test.pass)
+		}
+	}
 
 }
