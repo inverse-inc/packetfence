38d6b94a357a6fcc31ab430d4c8e4e158509e77f
Durand Fabrice <oeufdure@gmail.com>
2018-07-18 21:40:23 -0400

    Fix syntax

--- html/pfappserver/lib/pfappserver/Form/Config/Realm.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Realm.pm
@@ -21,7 +21,7 @@ use pf::ConfigStore::Domain;
 
 has domains => ( is => 'rw', builder => '_build_domains');
 tie our %ConfigAuthenticationLdap, 'pfconfig::cached_hash', 'resource::authentication_sources_ldap';
-tie %ConfigAuthenticationRadius, 'pfconfig::cached_hash', 'resource::authentication_sources_radius';
+tie our %ConfigAuthenticationRadius, 'pfconfig::cached_hash', 'resource::authentication_sources_radius';
 
 ## Definition
 has_field 'id' =>
@@ -62,7 +62,7 @@ has_field 'radius_auth' =>
    options_method => \&options_radius,
    element_class => ['chzn-select'],
    element_attr => {'data-placeholder' => 'Click to select a RADIUS Server'},
-a   tags => { after_element => \&help,
+   tags => { after_element => \&help,
              help => 'The RADIUS Server(s) to proxy authentication' },
   );
 
