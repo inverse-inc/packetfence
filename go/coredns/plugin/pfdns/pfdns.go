4a6aaad2290d9e0ec5c292d60d95c7ed9ad32f1b
Julien Semaan <jul.semaan@gmail.com>
2018-07-25 12:43:23 -0400

    have pfdns match domain passthroughs first

--- go/coredns/plugin/pfdns/pfdns.go
+++ go/coredns/plugin/pfdns/pfdns.go
@@ -117,6 +117,29 @@ func (pf *pfdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 		fmt.Printf("ERROR cannot find mac for ip %s\n", srcIP)
 	}
 
+	// Domain bypass
+	for k, v := range pf.FqdnDomainPort {
+		if k.MatchString(state.QName()) {
+			answer, err := pf.LocalResolver(state)
+			if err != nil {
+				fmt.Println("Local resolver error with the following error" + err.Error())
+			} else {
+				for _, ans := range append(answer.Answer, answer.Extra...) {
+					switch ansb := ans.(type) {
+					case *dns.A:
+						for _, valeur := range v {
+							if err := pf.SetPassthrough(ctx, "passthrough", ansb.A.String(), valeur, true); err != nil {
+								fmt.Println("Not able to contact localhost", err)
+							}
+						}
+					}
+				}
+			}
+			fmt.Println(srcIP + ":" + mac + " Domain bypass")
+			return pf.Next.ServeDNS(ctx, w, r)
+		}
+	}
+
 	violation := pf.HasViolations(mac)
 	if violation {
 		// Passthrough bypass
@@ -162,29 +185,6 @@ func (pf *pfdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 				}
 			}
 			fmt.Println(srcIP + " : " + mac + " passthrough")
-			return pf.Next.ServeDNS(ctx, w, r)
-		}
-	}
-
-	// Domain bypass
-	for k, v := range pf.FqdnDomainPort {
-		if k.MatchString(state.QName()) {
-			answer, err := pf.LocalResolver(state)
-			if err != nil {
-				fmt.Println("Local resolver error with the following error" + err.Error())
-			} else {
-				for _, ans := range append(answer.Answer, answer.Extra...) {
-					switch ansb := ans.(type) {
-					case *dns.A:
-						for _, valeur := range v {
-							if err := pf.SetPassthrough(ctx, "passthrough", ansb.A.String(), valeur, true); err != nil {
-								fmt.Println("Not able to contact localhost", err)
-							}
-						}
-					}
-				}
-			}
-			fmt.Println(srcIP + ":" + mac + " Domain bypass")
 			return pf.Next.ServeDNS(ctx, w, r)
 		}
 	}
