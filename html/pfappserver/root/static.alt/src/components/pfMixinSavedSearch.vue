10891490bc13c9272b1b1f75338d8f474f20c106
Francis Lachapelle <flachapelle@inverse.ca>
2018-08-01 16:06:42 -0400

    (web admin) Improve active links of sidebar

--- html/pfappserver/root/static.alt/src/components/pfMixinSavedSearch.vue
+++ html/pfappserver/root/static.alt/src/components/pfMixinSavedSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <b-nav vertical class="bd-sidenav" v-if="savedSearches && savedSearches.length > 0">
     <div class="bd-toc-link" v-t="'Saved Searches'"></div>
-    <b-nav-item v-for="search in savedSearches" :key="search.name" :to="routeSavedSearch(search)" replace>
+    <b-nav-item v-for="search in savedSearches" :key="search.name" :to="routeSavedSearch(search)" active-class="secondary" exact>
       {{search.name}}
       <icon class="float-right mt-1" name="trash-alt" @click.native.stop.prevent="deleteSavedSearch(search)"></icon>
     </b-nav-item>
--- html/pfappserver/root/static.alt/src/views/Nodes/index.vue
+++ html/pfappserver/root/static.alt/src/views/Nodes/index.vue
@@ -14,10 +14,10 @@
 
                             <hr/>
                             <div class="bd-toc-link" v-t="'Standard Searches'"></div>
-                            <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.open_count","op":"greater_than_equals","value":"1"}]}]})}}' replace>{{ $t('Open Violations') }}</b-nav-item>
-                            <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.close_count","op":"greater_than_equals","value":"1"}]}]})}}' replace>{{ $t('Closed Violations') }}</b-nav-item>
-                            <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"not_equals","value":"on"}]}]})}}' replace>{{ $t('Offline Nodes') }}</b-nav-item>
-                            <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"equals","value":"on"}]}]})}}' replace>{{ $t('Online Nodes') }}</b-nav-item>
+                            <b-nav-item  @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.open_count","op":"greater_than_equals","value":"1"}]}]})}}' active-class="secondary" exact>{{ $t('Open Violations') }}</b-nav-item>
+                            <b-nav-item  @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.close_count","op":"greater_than_equals","value":"1"}]}]})}}' active-class="secondary" exact>{{ $t('Closed Violations') }}</b-nav-item>
+                            <b-nav-item  @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"not_equals","value":"on"}]}]})}}' active-class="secondary" exact>{{ $t('Offline Nodes') }}</b-nav-item>
+                            <b-nav-item  @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"equals","value":"on"}]}]})}}' active-class="secondary" exact>{{ $t('Online Nodes') }}</b-nav-item>
 
                             <!-- Standard Searches > Switch Groups -->
                             <div class="bd-toc-link" v-b-toggle="'accordionSwitchGroups'">
@@ -31,7 +31,7 @@
                                   <icon class="float-right mt-1" name="chevron-down"></icon>
                                 </div>
                                 <b-collapse :id="`accordionSwitchGroup${switchGroup.group}`" :ref="`accordionSwitchGroup${switchGroup.group}`" is-nav>
-                                  <b-nav-item @click.stop v-for="sw in switchGroup.switches" :key="sw.id" v-if="sw.id !== 'default'" :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"locationlog.switch","op":"equals","value":getIpFromCIDR(sw.id)}]}]})}}' replace>
+                                  <b-nav-item @click.stop v-for="sw in switchGroup.switches" :key="sw.id" v-if="sw.id !== 'default'" :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"locationlog.switch","op":"equals","value":getIpFromCIDR(sw.id)}]}]})}}' active-class="secondary" exact>
                                     <blockquote class="mb-0">
                                       {{ sw.id }}<br/>
                                       {{ sw.description }}
@@ -47,7 +47,7 @@
                               <icon class="float-right mt-1" name="chevron-down"></icon>
                             </div>
                             <b-collapse id="accordionRoles" ref="accordionRoles" is-nav>
-                                <b-nav-item @click.stop v-for="role in roles" :key="role.name" :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"category_id","op":"equals","value":role.category_id}]}]})}}' replace>{{role.name}}</b-nav-item>
+                                <b-nav-item @click.stop v-for="role in roles" :key="role.name" :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"category_id","op":"equals","value":role.category_id}]}]})}}' active-class="secondary" exact>{{role.name}}</b-nav-item>
                             </b-collapse>
 
                             <!-- Standard Searches > OS -->
@@ -56,12 +56,12 @@
                               <icon class="float-right mt-1" name="chevron-down"></icon>
                             </div>
                             <b-collapse id="accordionOs" ref="accordionOs" is-nav>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Windows OS"}]}]})}}' replace>{{ $t('Windows') }}</b-nav-item>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Linux OS"}]}]})}}' replace>{{ $t('Linux') }}</b-nav-item>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Mac OS X or macOS"}]}]})}}' replace>{{ $t('MacOS') }}</b-nav-item>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Windows Phone OS"},{"field":"device_class","op":"equals","value":"WebOS"},{"field":"device_class","op":"equals","value":"iOS"},{"field":"device_class","op":"equals","value":"Palm OS"},{"field":"device_class","op":"equals","value":"Android OS"},{"field":"device_class","op":"equals","value":"watchOS"},{"field":"device_class","op":"equals","value":"Phone, Tablet or Wearable"},{"field":"mac","op":"equals","value":"BlackBerry OS"}]}]})}}' replace>{{ $t('Mobile Devices') }}</b-nav-item>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Gaming Console"}]}]})}}' replace>{{ $t('Gaming Console') }}</b-nav-item>
-                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"VoIP Device"}]}]})}}' replace>{{ $t('VoIP Device') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Windows OS"}]}]})}}' active-class="secondary" exact>{{ $t('Windows') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Linux OS"}]}]})}}' active-class="secondary" exact>{{ $t('Linux') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Mac OS X or macOS"}]}]})}}' active-class="secondary" exact>{{ $t('MacOS') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Windows Phone OS"},{"field":"device_class","op":"equals","value":"WebOS"},{"field":"device_class","op":"equals","value":"iOS"},{"field":"device_class","op":"equals","value":"Palm OS"},{"field":"device_class","op":"equals","value":"Android OS"},{"field":"device_class","op":"equals","value":"watchOS"},{"field":"device_class","op":"equals","value":"Phone, Tablet or Wearable"},{"field":"mac","op":"equals","value":"BlackBerry OS"}]}]})}}' active-class="secondary" exact>{{ $t('Mobile Devices') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"Gaming Console"}]}]})}}' active-class="secondary" exact>{{ $t('Gaming Console') }}</b-nav-item>
+                                <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"device_class","op":"equals","value":"VoIP Device"}]}]})}}' active-class="secondary" exact>{{ $t('VoIP Device') }}</b-nav-item>
                             </b-collapse>
                           </b-nav>
                         <hr/>
