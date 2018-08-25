56d4065688b52f61dc069b9383dece8215b03305
James Rouzier <jrouzier@inverse.ca>
2018-08-25 19:56:26 -0400

    Add the endpoints for SyslogForwarders
    * /api/v1/config/syslog_forwarders
    * /api/v1/config/syslog_forwarder

--- lib/pf/UnifiedApi.pm
+++ lib/pf/UnifiedApi.pm
@@ -240,6 +240,7 @@ our @API_V1_ROUTES = (
         Config::Scans
         Config::Sources
         Config::SwitchGroups
+        Config::SyslogForwarders
         Config::TrafficShapingPolicies
         Config::Violations
     ),
