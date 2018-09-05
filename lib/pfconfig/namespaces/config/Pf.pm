c3252379db4a0e4babad8ae1955cd54aae398768
Julien Semaan <jul.semaan@gmail.com>
2018-09-05 12:34:58 -0400

    fix dns_on_vip_only handling in pfdhcp
    fixes #3596

--- lib/pfconfig/namespaces/config/Pf.pm
+++ lib/pfconfig/namespaces/config/Pf.pm
@@ -74,7 +74,7 @@ sub init {
     $self->{doc_config}      = $self->{cache}->get_cache('config::Documentation');
     $self->{cluster_config}  = $self->{cache}->get_cache('config::Cluster');
 
-    $self->{child_resources} = [ 'resource::CaptivePortal', 'resource::Database', 'resource::fqdn', 'config::Pfdetect', 'resource::trapping_range', 'resource::stats_levels', 'resource::passthroughs', 'resource::isolation_passthroughs' ];
+    $self->{child_resources} = [ 'resource::CaptivePortal', 'resource::Database', 'resource::fqdn', 'config::Pfdetect', 'resource::trapping_range', 'resource::stats_levels', 'resource::passthroughs', 'resource::isolation_passthroughs', 'resource::network_config' ];
     if(defined($host_id)){
         push @{$self->{child_resources}}, "interfaces($host_id)";
     }
