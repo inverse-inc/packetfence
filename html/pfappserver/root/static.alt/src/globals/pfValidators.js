d51fc240d9188a4e0a86b6ce8b0f706773b16421
Darren Satkunas <darren@satkunas.com>
2018-09-11 17:02:44 -0400

    Add validators

--- html/pfappserver/root/static.alt/src/globals/pfValidators.js
+++ html/pfappserver/root/static.alt/src/globals/pfValidators.js
@@ -1,10 +1,53 @@
+/**
+ * Custom Vuelidate Validators
+ *
+ * See Builtin Validators: https://monterail.github.io/vuelidate/#sub-builtin-validators
+ *
+**/
 import store from '@/store'
+import moment from 'moment'
 
-export const pfValidateMacAddressIsUnique = (value, component) => {
+const _common = require('vuelidate/lib/validators/common')
+
+export const categoryIdNumberExists = (value, component) => {
+  if (!value || isNaN(parseInt(value))) return true
+  if (store.state.config.roles.filter(role => role.category_id === value).length === 0) return false
+  return true
+}
+
+export const categoryIdStringExists = (value, component) => {
+  if (!value || !isNaN(parseInt(value))) return true
+  if (store.state.config.roles.filter(role => role.name.toLowerCase() === value.toLowerCase()).length === 0) return false
+  return true
+}
+
+export const inArray = (array) => {
+  return (0, _common.withParams)({
+    type: 'inArray',
+    array: array
+  }, function (value) {
+    return !(0, _common.req)(value) || array.includes(value)
+  })
+}
+
+export const isDate = (value) => (!value) || moment(value, 'YYYY-MM-DD', true).isValid()
+
+export const isDateTime = (value) => (!value) || moment(value, 'YYYY-MM-DD HH:mm:ss', true).isValid()
+
+export const macAddressIsUnique = (value, component) => {
   if (!value || value.length !== 17) return true
   return store.dispatch('$_nodes/exists', value).then(results => {
     return false
   }).catch(() => {
     return true
+  })
+}
+
+export const userExists = (value, component) => {
+  if (!value) return true
+  return store.dispatch('$_users/exists', value).then(results => {
+    return true
+  }).catch(() => {
+    return false
   })
 }
