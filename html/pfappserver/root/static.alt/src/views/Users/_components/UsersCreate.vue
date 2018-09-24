46ace4652b59a6c27c83f2e8ae3f385cc8346620
Darren Satkunas <darren@satkunas.com>
2018-09-24 11:52:48 -0400

    Drop console.log

--- html/pfappserver/root/static.alt/src/views/Users/_components/UsersCreate.vue
+++ html/pfappserver/root/static.alt/src/views/Users/_components/UsersCreate.vue
@@ -8,11 +8,11 @@
         <b-form @submit.prevent="create()">
           <b-form-row align-v="center">
             <b-col sm="8">
-              <pf-form-toggle v-model="single.pid_overwrite" label="Username (PID) overwrite" 
+              <pf-form-toggle v-model="single.pid_overwrite" :column-label="$t('Username (PID) overwrite')" 
                 :color="{checked: '#28a745', unchecked: '#dc3545'}" :values="{checked: 1, unchecked: 0}"
                 text="Overwrite the username (PID) if it already exists."
                 >{{ (single.pid_overwrite === 1) ? $t('Overwrite') : $t('Ignore') }}</pf-form-toggle>
-              <pf-form-input v-model.trim="single.pid" label="Username (PID)"
+              <pf-form-input v-model.trim="single.pid" :column-label="$t('Username (PID)')"
                 :validation="$v.single.pid"
                 :invalid-feedback="[
                   { [$t('Username required.')]: !$v.single.pid.required },
@@ -20,56 +20,57 @@
                   { [$t('Maximum {max} characters', {max: globals.schema.person.pid.maxLength})]: !$v.single.pid.maxLength }
                 ]"
                 text="The username to use for login to the captive portal."/>
-              <pf-form-input type="password" v-model="single.password" label="Password"
+              <pf-form-input type="password" v-model="single.password" :column-label="$t('Password')"
                 :validation="$v.single.password"
                 :invalid-feedback="[
                   { [$t('The password must be at least 6 characters.')]: !$v.single.password.minLength }
                 ]"
                 text="Leave it empty if you want to generate a random password."/>
-              <pf-form-input v-model.trim="single.email" label="Email"
+              <pf-form-input v-model.trim="single.email" :column-label="$t('Email')"
                 :validation="$v.single.email"
                 :invalid-feedback="[
                   { [$t('Email address required.')]: !$v.single.email.required },
                   { [$t('Specify a valid email address.')]: !$v.single.email.email },
                   { [$t('Maximum {max} characters', {max: globals.schema.person.email.maxLength})]: !$v.single.email.maxLength }
                 ]"/>
-              <pf-form-input v-model="single.firstname" label="Firstname"
+              <pf-form-input v-model="single.firstname" :column-label="$t('Firstname')"
                 :validation="$v.single.firstname"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.firstname.maxLength})]: !$v.single.firstname.maxLength }
                 ]"
               />
-              <pf-form-input v-model="single.lastname" label="Lastname"
+              <pf-form-input v-model="single.lastname" :column-label="$t('Lastname')"
                 :validation="$v.single.lastname"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.lastname.maxLength})]: !$v.single.lastname.maxLength }
                 ]"
               />
-              <pf-form-input v-model="single.company" label="Company"
+              <pf-form-input v-model="single.company" :column-label="$t('Company')"
                 :validation="$v.single.company"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.company.maxLength})]: !$v.single.company.maxLength }
                 ]"
               />
-              <pf-form-input v-model="single.telephone" label="Telephone"
+              <pf-form-input v-model="single.telephone" :column-label="$t('Telephone')"
                 :filter="globals.regExp.stringPhone"
                 :validation="$v.single.telephone"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.telephone.maxLength})]: !$v.single.telephone.maxLength }
                 ]"
               />
-              <pf-form-textarea v-model="single.address" :label="$t('Address')" rows="4" max-rows="6"
+              <pf-form-textarea v-model="single.address" :column-label="$t('Address')" rows="4" max-rows="6"
                 :validation="$v.single.address"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.address.maxLength})]: !$v.single.address.maxLength }
                 ]"
               />
-              <pf-form-textarea v-model="single.notes" :label="$t('Notes')" rows="8" max-rows="12"
+              <pf-form-textarea v-model="single.notes" :column-label="$t('Notes')" rows="8" max-rows="12"
                 :validation="$v.single.notes"
                 :invalid-feedback="[
                   { [$t('Maximum {max} characters', {max: globals.schema.person.notes.maxLength})]: !$v.single.notes.maxLength }
                 ]"
               />
+
             </b-col>
           </b-form-row>
         </b-form>
@@ -149,7 +150,8 @@ export default {
         pid_overwrite: 0,
         pid: '',
         email: '',
-        password: ''
+        password: '',
+        chosen: []
       },
       valid_from: new Date()
     }
@@ -193,10 +195,9 @@ export default {
     create () {
       if (this.modeIndex === 0) {
         this.$store.dispatch('$_users/createUser', this.single).then(response => {
-          console.debug('user created')
+          // user created
         }).catch(err => {
-          console.debug(err)
-          console.debug(this.$store.state.$_users.message)
+          // noop
         })
       }
     }
