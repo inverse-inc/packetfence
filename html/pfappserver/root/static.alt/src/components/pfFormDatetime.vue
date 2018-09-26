86b674962fa172b06946517101f8550762764854
Darren Satkunas <darren@satkunas.com>
2018-09-26 11:14:03 -0400

    Rework styling for pfFormDatetime component

--- html/pfappserver/root/static.alt/src/components/pfFormDatetime.vue
+++ html/pfappserver/root/static.alt/src/components/pfFormDatetime.vue
@@ -16,11 +16,11 @@
  *      example :moments="['-1 hours', '1 hours', '1 days', '1 weeks', '1 months', '1 quarters', '1 years']"
  */
  <template>
-  <b-form-group horizontal :label-cols="(columnLabel) ? labelCols : 0" :label="$t(columnLabel)"
-    :state="isValid()" :invalid-feedback="getInvalidFeedback()" :class="{ 'mb-0': !columnLabel }">
-    <b-input-group>
+  <b-form-group horizontal :label-cols="(columnLabel) ? labelCols : 0" :label="$t(columnLabel)" 
+    :state="isValid()" :invalid-feedback="getInvalidFeedback()" :class="[{ 'mb-0': !columnLabel }, { 'is-focus': focus}]">
+    <b-input-group class="input-group-datetime">
       <b-input-group-prepend v-if="prependText">
-        <div :class="['input-group-text', 'border-right-0', {'border-info': focus}, {'border-danger': isValid() === false}]">
+        <div class="input-group-text">
           {{ prependText }}
         </div>
       </b-input-group-prepend>
@@ -30,7 +30,6 @@
         ref="datetime"
         :config="datetimeConfig"
         :state="isValid()"
-        :class="[{'border-left-0': prependText}, 'border-right-0', {'is-invalid': isValid() === false}]"
         @input.native="validate()"
         @keyup.native="onChange($event)"
         @change.native="onChange($event)"
@@ -40,9 +39,9 @@
       <b-input-group-append>
         <b-button class="input-group-text" v-if="initialValue && initialValue !== inputValue" @click.stop="reset($event)" v-b-tooltip.hover.top.d300 :title="$t('Reset')"><icon name="undo-alt" variant="light"></icon></b-button>
         <b-button-group v-if="moments.length > 0" rel="moments" v-b-tooltip.hover.top.d300 :title="$t('Cumulate [CTRL] + [CLICK]')">
-          <b-button v-for="(moment, index) in moments" :key="index" :class="['border-left-0', 'border-right-0', {'border-info': focus}, {'border-danger': isValid() === false}]" variant="light" @click="onClickMoment($event, index)" v-b-tooltip.hover.bottom.d300 :title="momentTooltip(index)">{{ momentLabel(index) }}</b-button>
+          <b-button v-for="(moment, index) in moments" :key="index" variant="light" @click="onClickMoment($event, index)" v-b-tooltip.hover.bottom.d300 :title="momentTooltip(index)">{{ momentLabel(index) }}</b-button>
         </b-button-group>
-        <b-button :class="['input-group-text', 'border-left-0', {'border-info': focus}, {'border-danger': isValid() === false}]" @click.stop="toggle($event)"><icon name="calendar-alt" variant="light"></icon></b-button>
+        <b-button class="input-group-text" @click.stop="toggle($event)"><icon name="calendar-alt" variant="light"></icon></b-button>
       </b-input-group-append>
     </b-input-group>
     <b-form-text v-if="text" v-t="text"></b-form-text>
@@ -307,7 +306,48 @@ export default {
 
 <style lang="scss">
 @import "../../node_modules/bootstrap/scss/functions";
+@import "../../node_modules/bootstrap/scss/mixins/border-radius";
+@import "../../node_modules/bootstrap/scss/mixins/transition";
 @import "../styles/variables";
+
+/**
+ * Adjust is-invalid and is-focus borders
+ */
+.form-group {
+  .input-group-datetime {
+    background-color: $input-focus-bg;
+    border: 1px solid $input-focus-bg;
+    @include border-radius($border-radius);
+    @include transition($custom-forms-transition);
+    padding: 1px;
+    outline: 0;
+
+    * {
+      border: 0px;
+    }
+    &:not(:first-child):not(:last-child):not(:only-child) {
+      border-radius: 0;
+    }
+    &:first-child {
+      border-top-left-radius: $border-radius;
+      border-bottom-left-radius: $border-radius;
+    }
+    &:last-child {
+      border-top-right-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
+    }
+  }
+  &.is-focus .input-group-datetime,
+  &.is-focus .bootstrap-datetimepicker-widget {
+    border: 1px solid $input-focus-border-color;
+    box-shadow: 0 0 0 $input-focus-width rgba($input-focus-border-color, .25);
+  }
+  &.is-invalid .input-group-datetime,
+  &.is-invalid .bootstrap-datetimepicker-widget {
+    border: 1px solid $form-feedback-invalid-color;
+    box-shadow: 0 0 0 $input-focus-width rgba($form-feedback-invalid-color, .25);
+  }
+}
 
 /**
  * Add btn-primary color(s) on hover
@@ -316,6 +356,7 @@ export default {
   color: $input-btn-hover-text-color;
   background-color: $input-btn-hover-bg-color;
   border-color: $input-btn-hover-bg-color;
+  border-radius: 0;
 }
 /**
  * vue-bootstrap-datetimepicker only supports fontawesome icons,
