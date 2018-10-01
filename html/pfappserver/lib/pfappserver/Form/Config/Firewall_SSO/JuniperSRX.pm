9c30b47988759c12b39b4f9fb600b5bd04934ca3
Durand Fabrice <oeufdure@gmail.com>
2018-10-01 13:24:18 -0400

    Juniper retrieved his firewall from Barracuda.

--- html/pfappserver/lib/pfappserver/Form/Config/Firewall_SSO/JuniperSRX.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Firewall_SSO/JuniperSRX.pm
@@ -33,12 +33,12 @@ has_field 'username' =>
    type => 'Text',
    label => 'Username',
    required => 1,
-   messages => { required => 'Please specify the username for the Barracuda' },
+   messages => { required => 'Please specify the username for the Juniper SRX' },
   );
 has_field 'password' =>
   (
    type => 'ObfuscatedText',
-   label => 'Secret or Key',
+   label => 'Password',
    required => 1,
    messages => { required => 'You must specify the password' },
   );
@@ -46,7 +46,7 @@ has_field 'port' =>
   (
    type => 'PosInteger',
    label => 'Port of the service',
-   default => 80,
+   default => 8443,
    tags => { after_element => \&help,
              help => 'If you use an alternative port, please specify' },
   );
@@ -54,35 +54,6 @@ has_field 'type' =>
   (
    type => 'Hidden',
   );
-has_field 'categories' =>
-  (
-   type => 'Select',
-   multiple => 1,
-   label => 'Roles',
-   options_method => \&options_categories,
-   element_class => ['chzn-select'],
-   element_attr => {'data-placeholder' => 'Click to add a role'},
-   tags => { after_element => \&help,
-             help => 'Nodes with the selected roles will be affected' },
-  );
-
-has_block definition =>
-  (
-   render_list => [ qw(id type username password port categories networks cache_updates cache_timeout username_format default_realm) ],
-  );
-
-=head2 options_categories
-
-=cut
-
-sub options_categories {
-    my $self = shift;
-
-    my ($status, $result) = $self->form->ctx->model('Config::Roles')->listFromDB();
-    my @roles = map { $_->{name} => $_->{name} } @{$result} if ($result);
-    return ('' => '', @roles);
-}
-
 
 =over
 
