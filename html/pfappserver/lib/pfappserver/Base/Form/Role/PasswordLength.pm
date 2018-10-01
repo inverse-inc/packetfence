d18b226f024588aab922423dae3080f034362b82
Durand Fabrice <oeufdure@gmail.com>
2018-10-01 16:14:12 -0400

    Limit password length

--- html/pfappserver/lib/pfappserver/Base/Form/Role/PasswordLength.pm
+++ html/pfappserver/lib/pfappserver/Base/Form/Role/PasswordLength.pm
@@ -20,10 +20,12 @@ with 'pfappserver::Base::Form::Role::Help';
 
 has_field 'password_length' =>
   (
-   type => 'PosInteger',
+   type => 'IntRange',
    label => 'Password length',
    required => 1,
    default => 8,
+   range_start => 1,
+   range_end => 15,
    tags => { after_element => \&help,
              help => 'The length of the password to generate.' },
   );
