7733c23cc06636cbbeddc50590abc17aca587b3d
nqb <nqb@users.noreply.github.com>
2018-10-03 09:21:06 -0400

    update privileges at installation

--- html/pfappserver/lib/pfappserver/Model/DB.pm
+++ html/pfappserver/lib/pfappserver/Model/DB.pm
@@ -59,6 +59,13 @@ sub assign {
             $logger->warn("$DBI::errstr");
             return ( $STATUS::INTERNAL_SERVER_ERROR, $status_msg );
         }
+        $sql_query = "GRANT SELECT ON mysql.proc TO ?\@${host} IDENTIFIED BY ?";
+        $dbHandler->do($sql_query, undef, $user, $password);
+        if ( $DBI::errstr ) {
+            $status_msg = "Error creating the user $user on database $db";
+            $logger->warn("$DBI::errstr");
+            return ( $STATUS::INTERNAL_SERVER_ERROR, $status_msg );
+        }
     }
     # Apply the new privileges
     $dbHandler->do("FLUSH PRIVILEGES");
