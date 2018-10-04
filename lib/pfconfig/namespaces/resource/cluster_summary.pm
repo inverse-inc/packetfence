2151dca5bee108267d1e40103ffd2eb33f21813c
Julien Semaan <jul.semaan@gmail.com>
2018-10-04 13:52:41 -0400

    added missing pfconfig namespace

---
+++ lib/pfconfig/namespaces/resource/cluster_summary.pm
@@ -0,0 +1,66 @@
+package pfconfig::namespaces::resource::cluster_summary;
+
+=head1 NAME
+
+pfconfig::namespaces::resource::cluster_summary
+
+=cut
+
+=head1 DESCRIPTION
+
+pfconfig::namespaces::resource::cluster_summary
+
+=cut
+
+use strict;
+use warnings;
+use pfconfig::namespaces::config::Cluster;
+
+use base 'pfconfig::namespaces::resource';
+
+
+sub init {
+    my ($self) = @_;
+}
+
+sub build {
+    my ($self) = @_;
+    my $cluster_config = pfconfig::namespaces::config::Cluster->new( $self->{cache} );
+    $cluster_config->build();
+    my @params = ("cluster_enabled", "multi_zone_enabled");
+    return { map{$_ => $cluster_config->{$_}} @params };
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2018 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# # vim: set expandtab:
+# # vim: set backspace=indent,eol,start:
+
