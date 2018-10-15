bae5914b54bc2c3c551d10444b97525417d33400
Darren Satkunas <darren@satkunas.com>
2018-10-15 17:15:05 -0400

    Add fields

--- html/pfappserver/root/static.alt/src/globals/pfField.js
+++ html/pfappserver/root/static.alt/src/globals/pfField.js
@@ -5,24 +5,46 @@ export const pfFieldType = {
   NONE:                    'none',
   INTEGER:                 'integer',
   SUBSTRING:               'substring',
+  DATE:                    'date',
   DATETIME:                'datetime',
   PREFIXMULTIPLIER:        'prefixmultiplier',
   DURATION:                'duration',
   SELECTMANY:              'selectmany',
   ADMINROLE:               'adminrole',
   ROLE:                    'role',
-  TENANT:                  'tenant'
+  SOURCE:                  'source',
+  TENANT:                  'tenant',
+  GENDER:                  'gender'
 }
 
 export const pfFieldTypeValues = {}
 
 pfFieldTypeValues[pfFieldType.ADMINROLE] = (store) => {
+  if (store === undefined) {
+    throw new Error('Missing `store` in pfFieldTypeValues[pfFieldType.ADMINROLE](store)')
+    return
+  }
   return store.getters['config/adminRolesList']
 }
 pfFieldTypeValues[pfFieldType.ROLE] = (store) => {
+  if (store === undefined) {
+    throw new Error('Missing `store` in pfFieldTypeValues[pfFieldType.ROLE](store)')
+    return
+  }
   return store.getters['config/rolesList']
 }
+pfFieldTypeValues[pfFieldType.SOURCE] = (store) => {
+  if (store === undefined) {
+    throw new Error('Missing `store` in pfFieldTypeValues[pfFieldType.SOURCE](store)')
+    return
+  }
+  return store.getters['config/sourcesList']
+}
 pfFieldTypeValues[pfFieldType.TENANT] = (store) => {
+  if (store === undefined) {
+    throw new Error('Missing `store` in pfFieldTypeValues[pfFieldType.TENANT](store)')
+    return
+  }
   return store.getters['config/tenantsList']
 }
 pfFieldTypeValues[pfFieldType.DURATION] = () => {
@@ -34,5 +56,12 @@ pfFieldTypeValues[pfFieldType.DURATION] = () => {
     { name: i18n.t('2 days'), value: '2D' },
     { name: i18n.t('3 days'), value: '3D' },
     { name: i18n.t('5 days'), value: '5D' }
+  ]
+}
+pfFieldTypeValues[pfFieldType.GENDER] = () => {
+  return [
+    { name: i18n.t('Male'), value: 'm' },
+    { name: i18n.t('Female'), value: 'f' },
+    { name: i18n.t('Other'), value: 'o' }
   ]
 }
