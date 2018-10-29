f39761f5c0424a425a00457a145674e7258efd3e
Francis Lachapelle <flachapelle@inverse.ca>
2018-10-29 16:46:45 -0400

    (web admin) Add support for env var VUE_APP_DEBUG

--- html/pfappserver/root/static.alt/vue.config.js
+++ html/pfappserver/root/static.alt/vue.config.js
@@ -2,12 +2,21 @@ module.exports = {
   baseUrl: '/static/alt',
   outputDir: 'dist',
   indexPath: '../../admin/v-index.tt',
+  css: {
+    sourceMap: process.env.VUE_APP_DEBUG
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: false
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.VUE_APP_DEBUG) {
+      config.optimization.minimize(false)
+      config.optimization.delete('minimizer')
     }
   }
 }
