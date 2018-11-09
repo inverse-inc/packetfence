50ca837264d338609b82a23b0d9240933f3d4074
Darren Satkunas <darren@satkunas.com>
2018-11-09 15:41:14 -0500

    Config configuration config

--- html/pfappserver/root/static.alt/src/globals/pfConfiguration.js
+++ html/pfappserver/root/static.alt/src/globals/pfConfiguration.js
@@ -1,7 +1,20 @@
 import i18n from '@/utils/locale'
+import { pfRegExp as regExp } from '@/globals/pfRegExp'
 import { pfSearchConditionType as conditionType } from '@/globals/pfSearch'
+import pfFormInput from '@/components/pfFormInput'
+import pfFormSelect from '@/components/pfFormSelect'
+import pfFormTextarea from '@/components/pfFormTextarea'
+import pfFormToggle from '@/components/pfFormToggle'
+import { isFQDN } from '@/globals/pfValidators'
+const {
+  required,
+  alphaNum,
+  integer,
+  macAddress,
+  ipAddress
+} = require('vuelidate/lib/validators')
 
-export const pfConfigurationColumns = {
+export const pfConfigurationListColumns = {
   admin_strip_username: {
     key: 'admin_strip_username',
     label: i18n.t('Strip Admin'),
@@ -62,6 +75,18 @@ export const pfConfigurationColumns = {
     sortable: true,
     visible: true
   },
+  taggedVlan: {
+    key: 'taggedVlan',
+    label: i18n.t('Tagged VLAN\'s'),
+    sortable: false,
+    visible: true
+  },
+  trunkPort: {
+    key: 'trunkPort',
+    label: i18n.t('Trunk Port'),
+    sortable: true,
+    visible: true
+  },
   type: {
     key: 'type',
     label: i18n.t('Type'),
@@ -84,40 +109,42 @@ export const pfConfigurationColumns = {
   }
 }
 
-export const pfConfigurationColumnsAuthenticationSources = [
-  pfConfigurationColumns.id,
-  pfConfigurationColumns.description,
-  pfConfigurationColumns.class,
-  pfConfigurationColumns.type,
-  pfConfigurationColumns.buttons
+export const pfConfigurationAuthenticationSourcesListColumns = [
+  pfConfigurationListColumns.id,
+  pfConfigurationListColumns.description,
+  pfConfigurationListColumns.class,
+  pfConfigurationListColumns.type,
+  pfConfigurationListColumns.buttons
 ]
 
-export const pfConfigurationColumnsDomains = [
-  pfConfigurationColumns.id,
-  pfConfigurationColumns.workgroup
+export const pfConfigurationDomainsListColumns = [
+  pfConfigurationListColumns.id,
+  pfConfigurationListColumns.workgroup
 ]
 
-export const pfConfigurationColumnsFloatingDevices = [
-  Object.assign(pfConfigurationColumns.id, { label: i18n.t('MAC') }),
-  pfConfigurationColumns.ip,
-  pfConfigurationColumns.pvid
+export const pfConfigurationFloatingDevicesListColumns = [
+  Object.assign(pfConfigurationListColumns.id, { label: i18n.t('MAC') }), // re-label
+  pfConfigurationListColumns.ip,
+  pfConfigurationListColumns.pvid,
+  pfConfigurationListColumns.taggedVlan,
+  pfConfigurationListColumns.trunkPort
 ]
 
-export const pfConfigurationColumnsRealmsList = [
-  pfConfigurationColumns.id,
-  pfConfigurationColumns.portal_strip_username,
-  pfConfigurationColumns.admin_strip_username,
-  pfConfigurationColumns.radius_strip_username
+export const pfConfigurationRealmsListColumns = [
+  pfConfigurationListColumns.id,
+  pfConfigurationListColumns.portal_strip_username,
+  pfConfigurationListColumns.admin_strip_username,
+  pfConfigurationListColumns.radius_strip_username
 ]
 
-export const pfConfigurationColumnsRoles = [
-  pfConfigurationColumns.id,
-  pfConfigurationColumns.notes,
-  pfConfigurationColumns.max_nodes_per_pid,
-  pfConfigurationColumns.buttons
+export const pfConfigurationRolesListColumns = [
+  pfConfigurationListColumns.id,
+  pfConfigurationListColumns.notes,
+  pfConfigurationListColumns.max_nodes_per_pid,
+  pfConfigurationListColumns.buttons
 ]
 
-export const pfConfigurationFields = {
+export const pfConfigurationListFields = {
   id: {
     value: 'id',
     text: i18n.t('Name'),
@@ -155,28 +182,419 @@ export const pfConfigurationFields = {
   }
 }
 
-export const pfConfigurationFieldsAuthenticationSources = [
-  pfConfigurationFields.id,
-  pfConfigurationFields.description,
-  pfConfigurationFields.class,
-  pfConfigurationFields.type
+export const pfConfigurationAuthenticationSourcesListFields = [
+  pfConfigurationListFields.id,
+  pfConfigurationListFields.description,
+  pfConfigurationListFields.class,
+  pfConfigurationListFields.type
 ]
 
-export const pfConfigurationFieldsDomains = [
-  pfConfigurationFields.id,
-  pfConfigurationFields.workgroup
+export const pfConfigurationDomainsListFields = [
+  pfConfigurationListFields.id,
+  pfConfigurationListFields.workgroup
 ]
 
-export const pfConfigurationFieldsFloatingDevices = [
-  Object.assign(pfConfigurationFields.id, { text: i18n.t('MAC') }),
-  pfConfigurationFields.ip
+export const pfConfigurationFloatingDevicesListFields = [
+  Object.assign(pfConfigurationListFields.id, { text: i18n.t('MAC') }), // re-label
+  pfConfigurationListFields.ip
 ]
 
-export const pfConfigurationFieldsRealmsList = [
-  pfConfigurationFields.id
+export const pfConfigurationRealmsListFields = [
+  pfConfigurationListFields.id
 ]
 
-export const pfConfigurationFieldsRoles = [
-  pfConfigurationFields.id,
-  pfConfigurationFields.notes
+export const pfConfigurationRolesListFields = [
+  pfConfigurationListFields.id,
+  pfConfigurationListFields.notes
 ]
+
+export const pfConfigurationAuthenticationSourcesViewFields = ({ isNew = false, isClone = false, sourceClass = null } = {}) => {
+  switch (sourceClass) {
+    case 'AD':
+      return [
+        {
+          label: i18n.t('Name'),
+          fields: [
+            {
+              key: 'id',
+              component: pfFormInput,
+              attrs: {
+                disabled: (!isNew)
+              },
+              validators: {
+                [i18n.t('Name is required.')]: required,
+                [i18n.t('Alphanumeric value required.')]: alphaNum
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('Description'),
+          fields: [
+            {
+              key: 'description',
+              component: pfFormInput
+            }
+          ]
+        },
+        {
+          label: i18n.t('Host'),
+          fields: [
+            {
+              key: 'host',
+              component: pfFormInput
+            },
+            {
+              key: 'port',
+              component: pfFormInput
+            },
+            {
+              key: 'encryption',
+              component: pfFormInput
+            }
+          ]
+        }
+      ]
+    default:
+      return [
+        {
+          label: i18n.t('Name'),
+          fields: [
+            {
+              key: 'id',
+              component: pfFormInput,
+              attrs: {
+                disabled: (!isNew)
+              },
+              validators: {
+                [i18n.t('Name is required.')]: required,
+                [i18n.t('Alphanumeric value required.')]: alphaNum
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('Description'),
+          fields: [
+            {
+              key: 'description',
+              component: pfFormInput
+            }
+          ]
+        }
+      ]
+  }
+}
+
+export const pfConfigurationDomainsViewFields = ({ isNew = false, isClone = false } = {}) => {
+  return [
+    {
+      label: i18n.t('Identifier'),
+      text: i18n.t('Specify a unique identifier for your configuration.<br/>This doesn\'t have to be related to your domain.'),
+      fields: [
+        {
+          key: 'id',
+          component: pfFormInput,
+          attrs: {
+            disabled: (!isNew)
+          },
+          validators: {
+            [i18n.t('Name is required.')]: required,
+            [i18n.t('Alphanumeric value required.')]: alphaNum
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Workgroup'),
+      fields: [
+        {
+          key: 'workgroup',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('Workgroup is required.')]: required
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('DNS name of the domain'),
+      text: i18n.t('The DNS name (FQDN) of the domain.'),
+      fields: [
+        {
+          key: 'dns_name',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('DNS name is required.')]: required,
+            [i18n.t('Fully Qualified Domain Name required.')]: isFQDN
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('This server\'s name'),
+      text: i18n.t('This server\'s name (account name) in your Active Directory. Use \'%h\' to automatically use this server hostname.'),
+      fields: [
+        {
+          key: 'server_name',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('Server name is required.')]: required
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Sticky DC'),
+      text: i18n.t('This is used to specify a sticky domain controller to connect to. If not specified, default \'*\' will be used to connect to any available domain controller.'),
+      fields: [
+        {
+          key: 'sticky_dc',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('Sticky DC is required.')]: required
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Active Directory server'),
+      text: i18n.t('The IP address or DNS name of your Active Directory server.'),
+      fields: [
+        {
+          key: 'ad_server',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('Active Directory server is required.')]: required
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Username'),
+      text: i18n.t('The username of a Domain Admin to use to join the server to the domain.'),
+      fields: [
+        {
+          key: 'bind_dn',
+          component: pfFormInput
+        }
+      ]
+    },
+    {
+      label: i18n.t('Password'),
+      text: i18n.t('The password of a Domain Admin to use to join the server to the domain. Will not be stored permanently and is only used while joining the domain.'),
+      fields: [
+        {
+          key: 'bind_pass',
+          component: pfFormInput,
+          attrs: {
+            type: 'password'
+          }
+        }
+      ]
+    }
+  ]
+}
+
+export const pfConfigurationFloatingDevicesViewFields = ({ isNew = false, isClone = false } = {}) => {
+  return [
+    {
+      label: i18n.t('MAC Address'),
+      fields: [
+        {
+          key: 'id',
+          component: pfFormInput,
+          attrs: {
+            disabled: (!isNew)
+          },
+          validators: {
+            [i18n.t('MAC address is required.')]: required,
+            [i18n.t('Enter a valid MAC address.')]: macAddress()
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('IP Address'),
+      fields: [
+        {
+          key: 'ip',
+          component: pfFormInput,
+          validators: {
+            [i18n.t('IP address is required.')]: required,
+            [i18n.t('Enter a valid IP address.')]: ipAddress
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Native VLAN'),
+      text: i18n.t('VLAN in which PacketFence should put the port.'),
+      fields: [
+        {
+          key: 'pvid',
+          component: pfFormInput,
+          attrs: {
+            filter: regExp.integerPositive
+          },
+          validators: {
+            [i18n.t('Native VLAN is required.')]: required,
+            [i18n.t('Enter a valid Native VLAN.')]: integer
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Trunk Port'),
+      text: i18n.t('The port must be configured as a muti-vlan port.'),
+      fields: [
+        {
+          key: 'trunkPort',
+          component: pfFormToggle,
+          attrs: {
+            values: { checked: 'yes', unchecked: 'no' }
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Tagged VLANs'),
+      text: i18n.t('Comma separated list of VLANs. If the port is a multi-vlan, these are the VLANs that have to be tagged on the port.'),
+      fields: [
+        {
+          key: 'taggedVlan',
+          component: pfFormInput
+        }
+      ]
+    }
+  ]
+}
+
+export const pfConfigurationRealmViewFields = ({ isNew = false, isClone = false, domains = [] } = {}) => {
+  return [
+    {
+      label: i18n.t('Realm'),
+      fields: [
+        {
+          key: 'id',
+          component: pfFormInput,
+          attrs: {
+            disabled: (!isNew)
+          },
+          validators: {
+            [i18n.t('Realm is required.')]: required,
+            [i18n.t('Alphanumeric value required.')]: alphaNum
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Realm Options'),
+      text: i18n.t('You can add FreeRADIUS options in the realm definition.'),
+      fields: [
+        {
+          key: 'options',
+          component: pfFormTextarea
+        }
+      ]
+    },
+    {
+      label: i18n.t('Domain'),
+      text: i18n.t('The domain to use for the authentication in that realm.'),
+      fields: [
+        {
+          key: 'domain',
+          component: pfFormSelect,
+          attrs: {
+            options: domains
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Strip on the portal'),
+      text: i18n.t('Should the usernames matching this realm be stripped when used on the captive portal.'),
+      fields: [
+        {
+          key: 'portal_strip_username',
+          component: pfFormToggle,
+          attrs: {
+            values: { checked: 'enabled', unchecked: 'disabled' }
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Strip on the admin'),
+      text: i18n.t('Should the usernames matching this realm be stripped when used on the administration interface.'),
+      fields: [
+        {
+          key: 'admin_strip_username',
+          component: pfFormToggle,
+          attrs: {
+            values: { checked: 'enabled', unchecked: 'disabled' }
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Strip in RADIUS authorization'),
+      text: i18n.t('Should the usernames matching this realm be stripped when used in the authorization phase of 802.1x. Note that this doesn\'t control the stripping in FreeRADIUS, use the options above for that.'),
+      fields: [
+        {
+          key: 'radius_strip_username',
+          component: pfFormToggle,
+          attrs: {
+            values: { checked: 'enabled', unchecked: 'disabled' }
+          }
+        }
+      ]
+    }
+  ]
+}
+
+export const pfConfigurationRoleViewFields = ({ isNew = false, isClone = false } = {}) => {
+  return [
+    {
+      label: i18n.t('Name'),
+      fields: [
+        {
+          key: 'id',
+          component: pfFormInput,
+          attrs: {
+            disabled: (!isNew)
+          },
+          validators: {
+            [i18n.t('Name is required.')]: required,
+            [i18n.t('Alphanumeric value required.')]: alphaNum
+          }
+        }
+      ]
+    },
+    {
+      label: i18n.t('Description'),
+      fields: [
+        {
+          key: 'notes',
+          component: pfFormInput
+        }
+      ]
+    },
+    {
+      label: i18n.t('Max nodes per user'),
+      fields: [
+        {
+          key: 'max_nodes_per_pid',
+          component: pfFormInput,
+          attrs: {
+            type: 'number'
+          },
+          validators: {
+            [i18n.t('Max nodes per user required.')]: required,
+            [i18n.t('Integer value required.')]: integer
+          }
+        }
+      ]
+    }
+  ]
+}
