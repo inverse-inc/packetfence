1f661e3008b54fc71badbc2d73446fe26eb00676
James Rouzier <jrouzier@inverse.ca>
2018-11-12 11:14:53 -0500

    Refactor test to variables for file names

--- t/test_apply_schema.sh
+++ t/test_apply_schema.sh
@@ -22,10 +22,15 @@ MYSQL="mysql -upf_smoke_tester -ppacket -h127.0.0.1"
 
 MYSQLDUMP="mysqldump -upf_smoke_tester -h127.0.0.1 --no-data -a --skip-comments --routines -ppacket"
 
-if ! [ -f "$PF_DIR/db/upgrade-X.X.X-X.Y.Z.sql" ]; then
+UPGRADE_SCRIPT="$PF_DIR/db/upgrade-X.X.X-X.Y.Z.sql"
+
+CURRENT_SCHEMA="$PF_DIR/db/pf-schema-X.Y.Z.sql"
+
+if ! [ -f "$UPGRADE_SCRIPT" ]; then
   echo "X.X.X to X.Y.Z upgrade script doesn't exist. Not testing schema upgrade"
   exit 0
 fi
+
 
 for db in $UPGRADED_DB $PRISTINE_DB;do
     echo "Created test db $db"
@@ -35,30 +40,30 @@ done
 
 LAST_SCHEMA=$(ls $PF_DIR/db/pf-schema-[0-9]*sql | sort --version-sort -r | head -1)
 
-echo "Creating db of last schema $LAST_SCHEMA"
+echo "Applying last schema $LAST_SCHEMA"
 
 $MYSQL $UPGRADED_DB < "$LAST_SCHEMA"
 
-echo "Applying upgrade script $PF_DIR/db/upgrade-X.X.X-X.Y.Z.sql"
+echo "Applying upgrade script $UPGRADE_SCRIPT"
 
-$MYSQL $UPGRADED_DB < "$PF_DIR/db/upgrade-X.X.X-X.Y.Z.sql"
+$MYSQL $UPGRADED_DB < "$UPGRADE_SCRIPT"
 
-echo "Applying $PF_DIR/db/pf-schema-X.Y.Z.sql"
+echo "Applying current schema $CURRENT_SCHEMA"
 
-$MYSQL $PRISTINE_DB < "$PF_DIR/db/pf-schema-X.Y.Z.sql"
+$MYSQL $PRISTINE_DB < "$CURRENT_SCHEMA"
 
 for db in $UPGRADED_DB $PRISTINE_DB;do
     $MYSQLDUMP $db > "${db}.dump"
 done
 
-DIFF=$(diff "${PRISTINE_DB}.dump" "${UPGRADED_DB}.dump" | tee upgrade.diff )
+DIFF=$(diff "${PRISTINE_DB}.dump" "${UPGRADED_DB}.dump" | tee "${UPGRADED_DB}.diff" )
 
 if [ -z "$DIFF" ];then
     echo "Upgrade is successful"
-    rm -f upgrade.diff ${PRISTINE_DB}.dump ${UPGRADED_DB}.dump
+    rm -f "${UPGRADED_DB}.diff" "${PRISTINE_DB}.dump" "${UPGRADED_DB}.dump"
 else
     echo "${RED_COLOR}Upgrade did not create the same db"
-    echo "Please look at upgrade.diff for the differences"
+    echo "Please look at ${UPGRADED_DB}.diff for the differences"
     echo "You can also look at ${PRISTINE_DB}.dump and ${UPGRADED_DB}.dump${RESET_COLOR}"
     exit 1
 fi
