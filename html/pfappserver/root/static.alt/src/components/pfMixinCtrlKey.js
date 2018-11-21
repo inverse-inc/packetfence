1fe95756febb1a28ac7004259cd860f52db207f6
Darren Satkunas <darren@satkunas.com>
2018-11-21 08:54:43 -0500

    Fix onblur bug sticking ctrlKey

--- html/pfappserver/root/static.alt/src/components/pfMixinCtrlKey.js
+++ html/pfappserver/root/static.alt/src/components/pfMixinCtrlKey.js
@@ -8,14 +8,19 @@ export default {
   methods: {
     onCtrlKey (event) {
       this.ctrlKey = event.ctrlKey
+    },
+    onWindowBlur (event) {
+      this.ctrlKey = false
     }
   },
   mounted () {
     document.addEventListener('keydown', this.onCtrlKey)
     document.addEventListener('keyup', this.onCtrlKey)
+    window.addEventListener('blur', this.onWindowBlur)
   },
   beforeDestroy () {
     document.removeEventListener('keydown', this.onCtrlKey)
     document.removeEventListener('keyup', this.onCtrlKey)
+    window.removeEventListener('blur', this.onWindowBlur)
   }
 }
