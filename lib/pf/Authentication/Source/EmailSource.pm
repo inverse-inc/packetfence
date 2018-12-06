7984b950f9b560b2d32a872020aa65b0903b2e92
James Rouzier <jrouzier@inverse.ca>
2018-12-06 14:01:49 -0500

    Move EmailFilteringRole to a role

--- lib/pf/Authentication/Source/EmailSource.pm
+++ lib/pf/Authentication/Source/EmailSource.pm
@@ -9,53 +9,21 @@ pf::Authentication::Source::EmailSource
 =cut
 
 use pf::Authentication::constants;
-use pf::config qw(%Config);
 use pf::constants qw($TRUE $FALSE);
 use pf::constants::authentication::messages;
 use pf::log;
-use List::MoreUtils qw(any);
-use pf::util;
 
 use Moose;
 extends 'pf::Authentication::Source';
-with 'pf::Authentication::CreateLocalAccountRole';
+with qw(
+    pf::Authentication::CreateLocalAccountRole
+    pf::Authentication::EmailFilteringRole
+);
 
 has '+class' => (default => 'external');
 has '+type' => (default => 'Email');
-has 'allow_localdomain' => (isa => 'Str', is => 'rw', default => 'yes');
 has 'email_activation_timeout' => (isa => 'Str', is => 'rw', default => '10m');
 has 'activation_domain' => (isa => 'Maybe[Str]', is => 'rw');
-has 'allowed_domains' => (isa => 'Maybe[ArrayRef[Str]]', is => 'rw');
-has 'banned_domains' => (isa => 'Maybe[ArrayRef[Str]]', is => 'rw');
-
-=head2 BUILDARGS
-
-BUILDARGS
-
-=cut
-
-around BUILDARGS => sub {
-    my $orig  = shift;
-    my $class = shift;
-    my %hash;
-    if (@_ == 1) {
-        if (ref($_[0]) ne 'HASH') {
-            return $class->$orig(@_);
-        }
-
-        %hash = %{$_[0]};
-    } else {
-        %hash = @_;
-    }
-
-    for my $f (qw(allowed_domains banned_domains)) {
-        next unless $hash{$f};
-        $hash{$f} = [ map { split(/\r?\n/) } expand_csv($hash{$f})];
-    }
-
-    my $args = $class->$orig(\%hash);
-    return $args;
-};
 
 =head2 dynamic_routing_module
 
@@ -143,101 +111,6 @@ sub authenticate {
     return $TRUE;
 }
 
-
-=head2 isEmailAllowed
-
-checks if email is allowed
-
-=cut
-
-sub isEmailAllowed {
-    my ($self, $email) = @_;
-    $email = lc($email);
-    if (any {$email =~ $_} $self->banned_email_regexes) {
-        return $FALSE;
-    }
-
-    my @allowed = $self->allowed_email_regexes;
-    if (@allowed) {
-        return (any {$email =~ $_} @allowed) ? $TRUE : $FALSE;
-    }
-
-    return $TRUE;
-}
-
-=head2 banned_email_regexes
-
-banned email regexes
-
-=cut
-
-sub banned_email_regexes {
-    my ($self) = @_;
-    my @banned = make_email_regexes($self->banned_domains);
-    if (!isenabled($self->allow_localdomain)) {
-        push @banned, localdomain_regexes();
-    }
-
-    return @banned;
-}
-
-=head2 allowed_email_regexes
-
-allowed email regexes
-
-=cut
-
-sub allowed_email_regexes {
-    my ($self) = @_;
-    my @allowed = make_email_regexes($self->allowed_domains);
-    if (@allowed && isenabled($self->allow_localdomain)) {
-        push @allowed, localdomain_regexes();
-    }
-
-    return @allowed;
-}
-
-=head2 localdomain_regexes
-
-Return list of email regexes for matching the the local domain
-
-=cut
-
-sub localdomain_regexes {
-    my $domain = lc($Config{general}{domain});
-    return make_email_regexes([$domain,"*.$domain"]);
-}
-
-=head2 make_email_regexes
-
-Create an array of email regexes from a list of domain wildcards
-
-=cut
-
-sub make_email_regexes {
-    my ($domain_wildcards) = @_;
-    return map { make_email_regex($_) } @{$domain_wildcards // []};
-}
-
-=head2 make_email_regex
-
-Make an email regex from a domain wildcard
-
-=cut
-
-sub make_email_regex {
-    my ($domain_wildcard) = @_;
-    local $_;
-    $domain_wildcard = lc($domain_wildcard);
-    if ($domain_wildcard =~ /\*/) {
-        $domain_wildcard =~ s/\./\\./g;
-        $domain_wildcard =~ s/\*/\.\*/g;
-        $domain_wildcard = "\@$domain_wildcard\$";
-        return qr/$domain_wildcard/;
-    }
-
-    return qr/@\Q$domain_wildcard\E$/;
-}
 
 =head1 AUTHOR
 
