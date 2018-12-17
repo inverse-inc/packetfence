6e58a63148e2b5fd6096f03052524fa91c25e0aa
Julien Semaan <jul.semaan@gmail.com>
2018-12-17 08:33:36 -0500

    use button to submit form instead

--- html/common/aup-autosubmit.js
+++ html/common/aup-autosubmit.js
@@ -1,6 +1,6 @@
 $('label[for="aup"]').closest('div').click(function(e) {
   e.preventDefault();
   $('#aup').attr('checked', 'checked');
-  $(this).closest('form').submit(); 
+  $('#button').click();
   return false;
 });
