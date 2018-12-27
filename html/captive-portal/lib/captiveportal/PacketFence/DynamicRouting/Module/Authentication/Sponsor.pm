44b66b761ce28b7d490ecfeaf70db2ab0506b288
James Rouzier <jrouzier@inverse.ca>
2018-12-27 12:32:34 -0500

    Fix calling the wrong object for checking the email
    Fixes #3878

--- html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Authentication/Sponsor.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/DynamicRouting/Module/Authentication/Sponsor.pm
@@ -162,7 +162,7 @@ sub do_sponsor_registration {
         return;
     }
 
-    if ((any { $_ eq 'email' } @{$self->required_fields // []}) && !$self->isEmailAllowed($email)) {
+    if ((any { $_ eq 'email' } @{$self->required_fields // []}) && !$source->isEmailAllowed($email)) {
         $logger->warn("EmailSource ($source->{id}) failed to authenticate PID '$email' is banned");
         $self->app->flash->{error} = $pf::constants::authentication::messages::EMAIL_UNAUTHORIZED;
         $self->prompt_fields();
