763707619a361948f7696250e5d4b22caa685e16
James Rouzier <jrouzier@inverse.ca>
2018-12-28 13:52:44 -0500

    New attribute lang

--- lib/pf/Authentication/Source/SponsorEmailSource.pm
+++ lib/pf/Authentication/Source/SponsorEmailSource.pm
@@ -29,6 +29,7 @@ has 'activation_domain' => (isa => 'Maybe[Str]', is => 'rw');
 has 'sponsorship_bcc' => (isa => 'Maybe[Str]', is => 'rw');
 has 'email_activation_timeout' => (isa => 'Str', is => 'rw', default => '30m');
 has 'validate_sponsor' => (isa => 'Str', is => 'rw', default => 'yes');
+has 'lang' => (isa => 'Maybe[Str]', is => 'rw', default => '');
 
 =head2 dynamic_routing_module
 
