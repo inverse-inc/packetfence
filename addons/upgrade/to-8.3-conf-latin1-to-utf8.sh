39f62652fae89d346f4877a1165cd11dda4c7daf
James Rouzier <jrouzier@inverse.ca>
2019-01-07 14:36:19 -0500

    Indicate when everythung is done and include html/captive-portal/profile-templates

--- addons/upgrade/to-8.3-conf-latin1-to-utf8.sh
+++ addons/upgrade/to-8.3-conf-latin1-to-utf8.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-find /usr/local/pf/conf -type f -print0 | while read -d $'\0' file; do 
+PF_DIR="/usr/local/pf"
+find "$PF_DIR/html/captive-portal/profile-templates" "$PF_DIR/conf" -type f -print0 | while read -d $'\0' file; do
     if file --mime-encoding  "$file" | grep -q iso-8859-1  ;then
         echo "converting $file from iso-8859-1 to utf-8" 
         iconv --from-code=iso-8859-1 --to-code=utf-8 "$file" -o "$file"
    fi
 done
+echo "Finished converting files"
