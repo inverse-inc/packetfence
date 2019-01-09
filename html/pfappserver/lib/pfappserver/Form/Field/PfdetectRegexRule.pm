aed588c173d22a1fdd84fb9f8725d70d930a37df
James Rouzier <jrouzier@inverse.ca>
2019-01-09 14:15:39 -0500

    Add rate_limit

--- html/pfappserver/lib/pfappserver/Form/Field/PfdetectRegexRule.pm
+++ html/pfappserver/lib/pfappserver/Form/Field/PfdetectRegexRule.pm
@@ -99,6 +99,16 @@ has_field 'ip_mac_translation' => (
     },
 );
 
+has_field 'rate_limit' => (
+    type    => 'Duration',
+    label   => 'Rate Limit',
+    default => '0s',
+    tags    => {
+        after_element => \&help,
+        help => 'Rate limit requests.'
+    },
+);
+
 =head2 validate
 
 Validate the rule is valid
