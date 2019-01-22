a49fabe1d2a0ff0dbdb1bf5ca89108072c54813e
Darren Satkunas <darren@satkunas.com>
2019-01-22 12:24:21 -0500

    Update InputRange

--- html/pfappserver/root/static.alt/src/components/InputRange.vue
+++ html/pfappserver/root/static.alt/src/components/InputRange.vue
@@ -1,32 +1,20 @@
 <template>
-  <div style="width:100%"> <!-- external style applied here -->
-    <div range id="range"
-      :style="{
-        '--border-color': borderColor,
-        '--border-width': `${borderWidth}px`,
-        '--range-height': `${rangeHeight}px`,
-        '--range-background-color': rangeBackgroundColor,
-        '--range-transition-delay': `${rangeTransitionDelay}s`,
-        '--handle-height': `${handleHeight}px`,
-        '--handle-background-color': handleBackgroundColor,
-        '--handle-transition-delay': `${handleTransitionDelay}s`,
-        '--hint-background-color': hintBackgroundColor,
-        '--tooltip-transition-delay': `${tooltipTransitionDelay}s`
-      }"
-      :class="{
-        'disabled': disabled,
-        'enabled': !disabled,
-        'min': parseFloat(inputValue) === parseFloat(min),
-        'max': parseFloat(inputValue) === parseFloat(max)
-      }"
+  <div style="width:100%" class="input-range"> <!-- external style applied here -->
+    <div id="range"
+      range
+      :enabled="!disabled"
+      :disabled="disabled"
+      :min="parseFloat(inputValue) === parseFloat(min)"
+      :max="parseFloat(inputValue) === parseFloat(max)"
+      :index="inputValue"
     >
       <div>
         <div hint v-for="(hint, index) in hints" :key="index" :style="hintStyle(index)"></div>
         <span handle :style="{ left: `${percent(inputValue)}%` }">
           <slot/>
         </span>
-        <div tooltip :style="{ left: `${percent(inputValue)}%` }">
-          <span id="value">{{ tooltipFunction(inputValue) }}</span>
+        <div v-if="tooltip" tooltip :style="{ left: `${percent(inputValue)}%` }">
+          <span id="value">{{ $t(tooltipFunction(inputValue)) }}</span>
         </div>
       </div>
       <input
@@ -38,7 +26,7 @@
         :min="min"
         :step="step"
         :disabled="disabled"
-        @input="inputValue = $event.target.value"
+        @input="input"
       />
       <div catch-min @click="inputValue = min"><!-- catch left-side clicks outside input --></div>
       <div catch-max @click="inputValue = max"><!-- catch right-side clicks outside input --></div>
@@ -74,45 +62,9 @@ export default {
       type: Boolean,
       default: false
     },
-    borderColor: {
-      type: String,
-      default: 'transparent'
-    },
-    borderWidth: {
-      type: Number,
-      default: 0
-    },
-    rangeHeight: {
-      type: Number,
-      default: 22
-    },
-    rangeBackgroundColor: {
-      type: String,
-      default: '#adb5bd'
-    },
-    rangeTransitionDelay: {
-      type: Number,
-      default: 0.3
-    },
-    handleHeight: {
-      type: Number,
-      default: 16
-    },
-    handleBackgroundColor: {
-      type: String,
-      value: '#ffffff'
-    },
-    handleTransitionDelay: {
-      type: Number,
-      default: 0
-    },
-    hintBackgroundColor: {
-      type: String,
-      default: '#ffffff'
-    },
-    tooltipTransitionDelay: {
-      type: Number,
-      default: 0.3
+    tooltip: {
+      type: Boolean,
+      default: false
     },
     tooltipFunction: {
       type: Function,
@@ -121,6 +73,10 @@ export default {
     hints: {
       type: Array,
       default: () => { return [] }
+    },
+    listenInput: {
+      type: Boolean,
+      default: true /* disable to track events manually (eg: toggle) */
     }
   },
   computed: {
@@ -138,7 +94,14 @@ export default {
     }
   },
   methods: {
+    input ($event) {
+      if (this.listenInput) {
+        this.inputValue = $event.target.value
+      }
+    },
     percent (value = this.inputValue) {
+      if (value >= this.max) return 100
+      if (value <= this.min) return 0
       return (100 / (this.max - this.min)) * parseInt(value) - (100 / (this.max - this.min)) * this.min
     },
     hintStyle (index) {
@@ -164,35 +127,34 @@ export default {
 @import "../styles/variables";
 @import "../../node_modules/bootstrap/scss/root";
 
-:root {
- --border-color: transparent;
- --border-width: 0px;
- --range-height: 22px;
- --range-background-color: #adb5bd;
- --range-transition-delay: 0.3s;
- --handle-height: 16px;
- --handle-background-color: #ffffff;
- --handle-transition-delay: 0s;
- --hint-background-color: #ffffff;
- --tooltip-transition-delay: 0.3s;
+:root { /* defaults */
+  --range-height: 22px;
+  --range-background-color: var(--success);
+  --range-transition-delay: 0.3s;
+  --handle-height: 16px;
+  --handle-background-color: var(--white);
+  --handle-transition-delay: 0s;
+  --hint-background-color: var(--light);
+  --tooltip-transition-delay: 0.3s;
 }
 
 [range] {
   position: relative;
   height: var(--range-height);
-  box-shadow: 0 0 0 var(--border-width) var(--border-color); /* pseudo border */
+  box-shadow: 0 0 0 1px transparent; /* pseudo border */
   border-radius: calc(var(--range-height) / 2);
   background-color: var(--range-background-color);
   text-align: left;
   margin: 0px;
-  transition: background-color var(--range-transition-delay) ease-out, outline var(--range-transition-delay) ease-out;
+  transition: background-color var(--range-transition-delay) ease-out,
+    box-shadow var(--range-transition-delay) ease-out,
+    outline var(--range-transition-delay) ease-out;
   > div {
     position: absolute;
     left: calc(var(--range-height) / 2);
     right: calc(var(--range-height) / 2);
     height: var(--range-height);
     > [handle] {
-      transition: left var(--handle-transition-delay) ease-in-out;
       position: absolute;
       top: calc((var(--range-height) - var(--handle-height)) / 2);
       height: var(--handle-height);
@@ -202,13 +164,11 @@ export default {
       background-color: var(--handle-background-color);
       border-radius: 50%;
       outline: none;
-      display: -webkit-box;
-      display: -moz-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
       display: flex;
       align-items: center;
       justify-content: center;
+      transition: left var(--handle-transition-delay) ease-in-out,
+        background-color var(--handle-transition-delay) ease-out;
     }
     > [hint] {
       position: absolute;
@@ -224,7 +184,6 @@ export default {
     > [tooltip] {
       opacity: 0;
       visibility: hidden;
-      transition: left var(--handle-transition-delay) ease-in-out, visibility var(--tooltip-transition-delay) linear, opacity var(--tooltip-transition-delay) ease-in-out;
       position: absolute;
       transform: translateX(-50%);
       bottom: calc(50% + (var(--handle-height) / 2) + 8px);
@@ -233,17 +192,22 @@ export default {
       align-items: center;
       -webkit-justify-content: center;
       justify-content: center;
+      font-size: .7875rem;
       text-align: center;
+      transition: left var(--handle-transition-delay) ease-in-out,
+        width var(--handle-transition-delay) ease-in-out,
+        visibility var(--tooltip-transition-delay) linear,
+        opacity var(--tooltip-transition-delay) ease-in-out;
       &:after { /* tooltip arrow */
         position: absolute;
         content: "";
         border-color: transparent;
+        border-top-color: #000;
         border-style: solid;
-        top: 22px;
+        border-width: .4rem .4rem 0;
+        top: 20px;
         left: 50%;
         transform: translateX(-50%);
-        border-width: .4rem .4rem 0;
-        border-top-color: #000;
       }
       > span {
         padding: .25rem .5rem;
@@ -252,6 +216,7 @@ export default {
         background-color: #000;
         border-radius: .25rem;
         font-family: var(--font-family-sans-serif);
+        font-size: .7875rem;
         font-style: normal;
         font-weight: 400;
         line-height: 1.5;
@@ -262,7 +227,6 @@ export default {
         word-spacing: normal;
         white-space: nowrap;
         line-break: auto;
-        font-size: .7875rem;
         &::selection { background: transparent; }
         &::-moz-selection { background: transparent; }
       }
@@ -347,15 +311,15 @@ export default {
     width: calc(var(--range-height) / 2);
   }
   > div > [hint],
-  &.disabled {
+  &[disabled] {
     opacity: 0.6;
   }
-  &.enabled {
-    &:not(.min) > [catch-min] {
+  &[enabled] {
+    &:not([min]) > [catch-min] {
       cursor: move; /* fallback if w-resize cursor is unsupported */
       cursor: w-resize;
     }
-    &:not(.max) > [catch-max] {
+    &:not([max]) > [catch-max] {
       cursor: move; /* fallback if e-resize cursor is unsupported */
       cursor: e-resize;
     }
@@ -366,8 +330,6 @@ export default {
     > input[type="range"] {
       cursor: move; /* fallback if grab cursor is unsupported */
       cursor: grab;
-      cursor: -moz-grab;
-      cursor: -webkit-grab;
     }
   }
 }
