2e497b0650c28fd5b792be9c1f0b335e3247002d
Durand Fabrice <oeufdure@gmail.com>
2019-01-28 14:55:28 -0500

    Set the domain-name according to the portal_fqdn

--- lib/pf/ConfigStore/Network.pm
+++ lib/pf/ConfigStore/Network.pm
@@ -140,21 +140,24 @@ Set default values before update or creating
 sub cleanupBeforeCommit {
     my ($self, $id, $network) = @_;
     my $config = $self->cachedConfig;
+    my @types = split(',',$network->{type});
     unless ( $config->SectionExists($id) ) {
         # Set default values when creating a new network
         $network->{type} =~ s/\s+//;
-        my @types = split(',',$network->{type});
         $network->{named} = 'enabled' unless ($network->{named});
         $network->{dhcpd} = 'enabled' unless ($network->{dhcpd});
         $network->{fake_mac_enabled} = 'disabled' if ($network->{type} ne $pf::config::NET_TYPE_INLINE_L3);
         if ($network->{'portal_fqdn'} ne "") {
             $network->{'domain-name'} = $types[0] . "." . $network->{'portal_fqdn'};
         }
-	$network->{'domain-name'} = $types[0] . "." . $Config{general}{domain}
+        $network->{'domain-name'} = $types[0] . "." . $Config{general}{domain}
             unless $network->{'domain-name'};
     } else {
         if ($network->{type} && $network->{type} eq $pf::config::NET_TYPE_INLINE_L3) {
             $network->{dhcpd} = isenabled($network->{'fake_mac_enabled'}) ? 'disabled' : 'enabled';
+        }
+        if ($network->{'portal_fqdn'} ne "") {
+            $network->{'domain-name'} = $types[0] . "." . $network->{'portal_fqdn'};
         }
     }
 }
