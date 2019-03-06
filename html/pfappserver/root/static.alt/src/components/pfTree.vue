f440e7441dac083c4b8a8c335a34cae0f25eec05
Francis Lachapelle <flachapelle@inverse.ca>
2019-03-06 15:24:46 -0500

    (web admin) Improve connection profile editor
    Fixes #791

--- html/pfappserver/root/static.alt/src/components/pfTree.vue
+++ html/pfappserver/root/static.alt/src/components/pfTree.vue
@@ -1,55 +1,67 @@
 <template>
   <div>
-  <b-table :items="items" :fields="fields" :class="'mb-0 table-clickable table-rowindent-' + level" :sort-by="sortBy" :sort-desc="false"
-    small fixed hover show-empty no-local-sorting
-    @sort-changed="onSortingChanged" @row-clicked="onRowClick">
-    <template slot="name" slot-scope="row">
-      <div class="text-lowercase" variant="link"
-        v-if="childrenIf(row.item)"
-        :disabled="row.item[childrenKey].length === 0">
-        <icon class="mr-1" name="regular/folder-open" v-if="row.detailsShowing"></icon>
-        <icon class="mr-1" name="regular/folder" v-else></icon> {{ row.item.name }}
-      </div>
-      <div class="text-lowercase" variant="link"
-        v-else>
-          <icon class="mr-1" name="regular/file"></icon> {{ row.item.name }}
-      </div>
-    </template>
-    <template slot="row-details" slot-scope="row">
-      <transition name="fade" mode="out-in">
-      <pf-tree
-        v-if="childrenIf(row.item)"
-        class="table-headless bg-transparent mb-0"
-        :path="fullPath(row.item)"
-        :items="row.item[childrenKey]"
-        :fields="fields"
-        :children-key="childrenKey"
-        :children-if="childrenIf"
-        :on-node-click="onNodeClick"
-        :on-node-create="onNodeCreate"
-        :level="level + 1"></pf-tree>
-      </transition>
-    </template>
-    <template slot="empty">
-      <b-container v-if="isLoading" class="my-5">
-        <b-row class="justify-content-md-center">
-          <b-col cols="12" md="auto">
-            <icon name="circle-notch" scale="1.5" spin></icon>
-          </b-col>
-        </b-row>
-      </b-container>
-      <div v-else class="font-weight-light text-secondary">{{ $t('Directory is empty') }}</div>
-    </template>
-  </b-table>
-    <div :class="'my-1 indent-' + level" v-if="onNodeCreate">
-      <b-button size="sm" variant="outline-secondary" @click="onNodeCreate(path)">{{ $t('New') }}</b-button>
+    <b-table :items="items" :fields="fields" :class="'mb-0 table-clickable table-rowindent-' + level" :sort-by="sortBy" :sort-desc="false"
+      small fixed hover show-empty no-local-sorting
+      @sort-changed="onSortingChanged" @row-clicked="onRowClick">
+      <template slot="name" slot-scope="row">
+        <div class="text-lowercase" variant="link"
+          v-if="childrenIf(row.item)"
+          :disabled="row.item[childrenKey].length === 0">
+          <icon class="mr-1" name="regular/folder-open" v-if="row.detailsShowing"></icon>
+          <icon class="mr-1" name="regular/folder" v-else></icon> {{ row.item.name }}
+        </div>
+        <div class="text-lowercase" variant="link"
+          v-else>
+            <icon class="mr-1" name="regular/file"></icon> {{ row.item.name }}
+        </div>
+      </template>
+      <template slot="row-details" slot-scope="row">
+        <transition name="fade" mode="out-in">
+        <pf-tree
+          v-if="childrenIf(row.item)"
+          class="table-headless bg-transparent mb-0"
+          :path="fullPath(row.item)"
+          :items="row.item[childrenKey]"
+          :fields="fields"
+          :children-key="childrenKey"
+          :children-if="childrenIf"
+          :node-create-label="nodeCreateLabel"
+          :container-create-label="containerCreateLabel"
+          :on-node-click="onNodeClick"
+          :on-node-create="onNodeCreate"
+          :on-container-create="onContainerCreate"
+          :on-delete="onDelete"
+          :level="level + 1"></pf-tree>
+        </transition>
+      </template>
+      <template slot="empty">
+        <b-container v-if="isLoading" class="my-5">
+          <b-row class="justify-content-md-center">
+            <b-col cols="12" md="auto">
+              <icon name="circle-notch" scale="1.5" spin></icon>
+            </b-col>
+          </b-row>
+        </b-container>
+        <div v-else class="font-weight-light text-secondary">{{ $t('Directory is empty') }}</div>
+      </template>
+    </b-table>
+    <div :class="'my-1 indent-' + level" v-if="showActions">
+      <b-button size="sm" variant="outline-danger" class="mr-1" v-if="deletable" @click="onDelete(path)">{{ $t('Delete') }}</b-button>
+      <pf-button-prompt size="sm" variant="outline-secondary" class="mr-1" v-model="newContainerName" v-if="onContainerCreate" @on-confirm="onContainerCreate(items, path, newContainerName)">{{ containerCreateLabel }}</pf-button-prompt>
+      <b-button size="sm" variant="outline-secondary" v-if="onNodeCreate" @click="onNodeCreate(path)">{{ nodeCreateLabel }}</b-button>
     </div>
   </div>
 </template>
 
 <script>
+import i18n from '@/utils/locale'
+import pfButtonPrompt from '@/components/pfButtonPrompt'
+
 export default {
   name: 'pf-tree',
+  components: {
+    pfButtonPrompt
+  },
   props: {
     path: {
       type: String,
@@ -83,7 +95,23 @@ export default {
       type: Function,
       default: null
     },
+    nodeCreateLabel: {
+      type: String,
+      default: i18n.t('Create file')
+    },
     onNodeCreate: {
+      type: Function,
+      default: null
+    },
+    onContainerCreate: {
+      type: Function,
+      default: null
+    },
+    containerCreateLabel: {
+      type: String,
+      default: i18n.t('Create directory')
+    },
+    onDelete: {
       type: Function,
       default: null
     },
@@ -96,12 +124,23 @@ export default {
       default: null
     }
   },
+  data () {
+    return {
+      newContainerName: ''
+    }
+  },
   computed: {
     isLoading () {
       if (this.isLoadingStoreGetter) {
         return this.$store.getters[this.isLoadingStoreGetter]
       }
       return false
+    },
+    deletable () {
+      return this.onDelete && this.items.length === 0
+    },
+    showActions () {
+      return (this.onNodeCreate || this.deletable) && !this.isLoading
     }
   },
   methods: {
@@ -138,7 +177,7 @@ export default {
 @for $i from 1 through 5 {
     .table-rowindent-#{$i} td:first-child,
     .indent-#{$i} {
-        padding-left: $i * 2rem;
+        padding-left: 2rem;
     }
 }
 </style>
