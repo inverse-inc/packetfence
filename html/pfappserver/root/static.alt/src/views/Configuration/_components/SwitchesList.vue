4385f123ea6444880e866e920e6f3e4465a336a5
Francis Lachapelle <flachapelle@inverse.ca>
2019-03-12 16:24:19 -0400

    (web admin) Remove inner cards (network devices)

--- html/pfappserver/root/static.alt/src/views/Configuration/_components/SwitchesList.vue
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/SwitchesList.vue
@@ -1,30 +1,25 @@
 <template>
-  <b-card no-body>
-    <pf-config-list
-      :config="config"
-    >
-      <template slot="pageHeader">
-        <b-card-header><h4 class="mb-0" v-t="'Switches'"></h4></b-card-header>
-      </template>
-      <template slot="buttonAdd">
-        <b-dropdown :text="$t('Add Switch')" variant="outline-primary" class="my-2">
-          <b-dropdown-header class="text-secondary">{{ $t('To group') }}</b-dropdown-header>
-            <b-dropdown-item :to="{ name: 'newSwitch', params: { switchGroup: 'default' } }">{{ $t('default') }}</b-dropdown-item>
-            <b-dropdown-item v-for="(switchGroup, index) in switches" :key="index"
-              :to="{ name: 'newSwitch', params: { switchGroup: switchGroup.id } }">{{ switchGroup.id }}</b-dropdown-item>
-        </b-dropdown>
-      </template>
-      <template slot="emptySearch" slot-scope="state">
-        <pf-empty-table :isLoading="state.isLoading">{{ $t('No switches found') }}</pf-empty-table>
-      </template>
-      <template slot="buttons" slot-scope="item">
-        <span class="float-right text-nowrap">
-          <b-button size="sm" variant="outline-primary" class="mr-1" @click.stop.prevent="clone(item)">{{ $t('Clone') }}</b-button>
-          <pf-button-delete  v-if="!item.not_deletable" size="sm" variant="outline-danger" :disabled="isLoading" :confirm="$t('Delete Switch?')" @on-delete="remove(item)" reverse/>
-        </span>
-      </template>
-    </pf-config-list>
-  </b-card>
+  <pf-config-list
+    :config="config"
+  >
+    <template slot="buttonAdd">
+      <b-dropdown :text="$t('Add Switch')" variant="outline-primary" class="my-2">
+        <b-dropdown-header class="text-secondary">{{ $t('To group') }}</b-dropdown-header>
+          <b-dropdown-item :to="{ name: 'newSwitch', params: { switchGroup: 'default' } }">{{ $t('default') }}</b-dropdown-item>
+          <b-dropdown-item v-for="(switchGroup, index) in switches" :key="index"
+            :to="{ name: 'newSwitch', params: { switchGroup: switchGroup.id } }">{{ switchGroup.id }}</b-dropdown-item>
+      </b-dropdown>
+    </template>
+    <template slot="emptySearch" slot-scope="state">
+      <pf-empty-table :isLoading="state.isLoading">{{ $t('No switches found') }}</pf-empty-table>
+    </template>
+    <template slot="buttons" slot-scope="item">
+      <span class="float-right text-nowrap">
+        <b-button size="sm" variant="outline-primary" class="mr-1" @click.stop.prevent="clone(item)">{{ $t('Clone') }}</b-button>
+        <pf-button-delete  v-if="!item.not_deletable" size="sm" variant="outline-danger" :disabled="isLoading" :confirm="$t('Delete Switch?')" @on-delete="remove(item)" reverse/>
+      </span>
+    </template>
+  </pf-config-list>
 </template>
 
 <script>
