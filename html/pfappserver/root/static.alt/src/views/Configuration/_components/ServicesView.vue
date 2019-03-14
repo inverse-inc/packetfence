268a9a10ee6c471e7cb4574211f703f89591a63c
Darren Satkunas <darren@satkunas.com>
2019-03-14 17:56:33 -0400

    Add Services

---
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/ServicesView.vue
@@ -0,0 +1,91 @@
+<template>
+  <pf-config-view
+    :isLoading="isLoading"
+    :form="getForm"
+    :model="form"
+    :vuelidate="$v.form"
+    @validations="formValidations = $event"
+    @save="save"
+  >
+    <template slot="header" is="b-card-header">
+      <h4 class="mb-0">
+        <span>{{ $t('Services') }}</span>
+      </h4>
+    </template>
+    <template slot="footer">
+      <b-card-footer @mouseenter="$v.form.$touch()">
+        <pf-button-save :disabled="invalidForm" :isLoading="isLoading">
+          <template>{{ $t('Save') }}</template>
+        </pf-button-save>
+        <b-button :disabled="isLoading" class="ml-1" variant="outline-primary" @click="init()">{{ $t('Reset') }}</b-button>
+      </b-card-footer>
+    </template>
+  </pf-config-view>
+</template>
+
+<script>
+import pfConfigView from '@/components/pfConfigView'
+import pfButtonSave from '@/components/pfButtonSave'
+import {
+  pfConfigurationServiceViewFields as fields
+} from '@/globals/configuration/pfConfigurationServices'
+
+const { validationMixin } = require('vuelidate')
+
+export default {
+  name: 'ServicesView',
+  mixins: [
+    validationMixin
+  ],
+  components: {
+    pfConfigView,
+    pfButtonSave
+  },
+  data () {
+    return {
+      form: {}, // will be overloaded with the data from the store
+      formValidations: {}, // will be overloaded with data from the pfConfigView
+      options: {}
+    }
+  },
+  validations () {
+    return {
+      form: this.formValidations
+    }
+  },
+  computed: {
+    isLoading () {
+      return this.$store.getters['$_bases/isLoading']
+    },
+    invalidForm () {
+      return this.$v.form.$invalid || this.$store.getters['$_bases/isWaiting']
+    },
+    getForm () {
+      return {
+        labelCols: 3,
+        fields: fields(this)
+      }
+    }
+  },
+  methods: {
+    init () {
+      this.$store.dispatch('$_bases/optionsServices').then(options => {
+        // store options
+        this.options = JSON.parse(JSON.stringify(options))
+        this.$store.dispatch('$_bases/getServices').then(data => {
+          this.form = JSON.parse(JSON.stringify(data))
+        })
+      })
+    },
+    save () {
+      let form = JSON.parse(JSON.stringify(this.form)) // dereference
+      this.$store.dispatch('$_bases/updateServices', form).then(response => {
+        // TODO - notification
+      })
+    }
+  },
+  created () {
+    this.init()
+  }
+}
+</script>
