338a73ae92934229d63d34ecf78cb23241003b21
minche <simbazad@protonmail.com>
2019-03-18 11:44:43 +0200

    Fix dereference of a string

--- html/pfappserver/lib/pfappserver/Model/Node/Tab/WMI.pm
+++ html/pfappserver/lib/pfappserver/Model/Node/Tab/WMI.pm
@@ -106,9 +106,10 @@ sub parseWmi {
     }
     $scan_config->{_scanIp} = $host;
     my $scan_result = $scan->runWmi($scan_config, $rule_config);
+    
     if ($scan_result =~ /ACCESS_DENIED/) {
         $rule_config->{item_exist} = "Access denied";
-    }elsif ($scan_result =~ /$WMI_NS_ERR/ || !@$scan_result) {
+    }elsif ($scan_result =~ /$WMI_NS_ERR/ || !$scan_result) {
         $rule_config->{item_exist} = 'No';
     }elsif ($scan_result =~ /TIMEOUT/ || $scan_result =~ /UNREACHABLE/) {
         $rule_config->{item_exist} = 'Request failed';
