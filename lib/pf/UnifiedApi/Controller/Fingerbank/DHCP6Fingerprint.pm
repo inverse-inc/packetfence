4313ddd68f5b9801ac5b5be6514eeed5a6cd0759
James Rouzier <jrouzier@inverse.ca>
2019-03-18 13:13:24 -0400

    The fingerbank controller

---
+++ lib/pf/UnifiedApi/Controller/Fingerbank/DHCP6Fingerprint.pm
@@ -0,0 +1,48 @@
+package pf::UnifiedApi::Controller::Fingerbank::DHCP6Fingerprint;
+
+=head1 NAME
+
+pf::UnifiedApi::Controller::Fingerbank::DHCP6Fingerprint -
+
+=head1 DESCRIPTION
+
+pf::UnifiedApi::Controller::Fingerbank::DHCP6Fingerprint
+
+=cut
+
+use strict;
+use warnings;
+use Mojo::Base 'pf::UnifiedApi::Controller::Fingerbank';
+use fingerbank::Model::DHCP6_Fingerprint;
+
+has 'fingerbank_model' => 'fingerbank::Model::DHCP6_Fingerprint';
+has 'url_param_name' => 'dhcp6_fingerprint_id';
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2019 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
