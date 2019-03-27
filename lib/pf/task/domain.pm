8b6817bf14794325e25a448ddbd87a09067dc643
Julien Semaan <jul.semaan@gmail.com>
2019-03-27 14:05:49 -0400

    add task for domain operation

---
+++ lib/pf/task/domain.pm
@@ -0,0 +1,91 @@
+package pf::task::domain;
+
+=head1 NAME
+
+pf::task::log - Task for looking up informations on a person
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::task::log
+
+=cut
+
+use strict;
+use warnings;
+use base 'pf::task';
+use pf::domain;
+use pf::log;
+use pf::config qw(%ConfigDomain);
+use pf::constants::pfqueue qw($STATUS_FAILED);
+
+our %OP_MAP = (
+    join => \&pf::domain::join_domain,
+    unjoin => \&pf::domain::unjoin_domain,
+    rejoin => \&pf::domain::rejoin_domain,
+);
+
+=head2 doTask
+
+Log to pfqueue.log
+
+=cut
+
+sub doTask {
+    my ($self, $args) = @_;
+    my $logger = get_logger;
+
+    my $op = $args->{operation};
+    my $domain = $args->{domain};
+
+    unless(exists($OP_MAP{$op})) {
+        my $msg = "Invalid operation $op for domain";
+        $logger->error($msg);
+        $self->status_updater->set_status($STATUS_FAILED);
+        $self->status_updater->set_status_msg($msg);
+        $self->status_updater->finalize();
+        return;
+    }
+
+    unless(exists($ConfigDomain{$domain})) {
+        my $msg = "Invalid domain $domain for domain task";
+        $logger->error($msg);
+        $self->status_updater->set_status($STATUS_FAILED);
+        $self->status_updater->set_status_msg($msg);
+        $self->status_updater->finalize();
+        return;
+    }
+
+    return $OP_MAP{$op}->($domain);
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2019 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
