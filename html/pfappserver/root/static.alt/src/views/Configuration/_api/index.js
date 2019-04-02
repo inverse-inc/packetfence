ca9d652235cb9b14b5a3d8c3170d8ca3b83a80bd
Darren Satkunas <darren@satkunas.com>
2019-04-02 13:32:15 -0400

    add missing api calls

--- html/pfappserver/root/static.alt/src/views/Configuration/_api/index.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_api/index.js
@@ -834,6 +834,43 @@ export default {
   },
 
   /**
+   * Routed Networks
+   */
+  routedNetworks: params => {
+    return apiCall.get('config/routed_networks', { params }).then(response => {
+      return response.data
+    })
+  },
+  routedNetworksOptions: () => {
+    return apiCall.options('config/routed_networks').then(response => {
+      return response.data
+    })
+  },
+  routedNetwork: id => {
+    return apiCall.get(`config/routed_network/${id}`).then(response => {
+      return response.data.item
+    })
+  },
+  routedNetworkOptions: id => {
+    return apiCall.options(`config/routed_network/${id}`).then(response => {
+      return response.data
+    })
+  },
+  createRoutedNetwork: data => {
+    return apiCall.post('config/routed_networks', data).then(response => {
+      return response.data
+    })
+  },
+  updateRoutedNetwork: data => {
+    return apiCall.patch(`config/routed_network/${data.id}`, data).then(response => {
+      return response.data
+    })
+  },
+  deleteRoutedNetwork: id => {
+    return apiCall.delete(`config/routed_network/${id}`)
+  },
+
+  /**
    * Scans
    */
   scanEngines: params => {
