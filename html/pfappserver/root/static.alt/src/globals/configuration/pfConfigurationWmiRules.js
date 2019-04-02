6fd0b36e48f66b7fc17e500d72be1222156ad9c5
Darren Satkunas <darren@satkunas.com>
2019-04-02 16:04:06 -0400

    add WMI Rules

---
+++ html/pfappserver/root/static.alt/src/globals/configuration/pfConfigurationWmiRules.js
@@ -0,0 +1,197 @@
+import i18n from '@/utils/locale'
+import pfFormInput from '@/components/pfFormInput'
+import pfFormRangeToggle from '@/components/pfFormRangeToggle'
+import pfFormTextarea from '@/components/pfFormTextarea'
+import {
+  pfConfigurationAttributesFromMeta,
+  pfConfigurationValidatorsFromMeta
+} from '@/globals/configuration/pfConfiguration'
+import { pfSearchConditionType as conditionType } from '@/globals/pfSearch'
+import {
+  and,
+  not,
+  conditional,
+  hasWmiRules,
+  wmiRuleExists
+} from '@/globals/pfValidators'
+
+const {
+  required
+} = require('vuelidate/lib/validators')
+
+export const pfConfigurationWmiRulesListColumns = [
+  {
+    key: 'id',
+    label: i18n.t('WMI Rule'),
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'namespace',
+    label: i18n.t('Namespace'),
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'on_tab',
+    label: i18n.t('On Node Tab'),
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'buttons',
+    label: '',
+    sortable: false,
+    visible: true,
+    locked: true
+  }
+]
+
+export const pfConfigurationWmiRulesListFields = [
+  {
+    value: 'id',
+    text: i18n.t('WMI Rule'),
+    types: [conditionType.SUBSTRING]
+  },
+  {
+    value: 'namespace',
+    text: i18n.t('Namespace'),
+    types: [conditionType.SUBSTRING]
+  }
+]
+
+export const pfConfigurationWmiRuleListConfig = (context = {}) => {
+  return {
+    columns: pfConfigurationWmiRulesListColumns,
+    fields: pfConfigurationWmiRulesListFields,
+    rowClickRoute (item, index) {
+      return { name: 'wmiRule', params: { id: item.id } }
+    },
+    searchPlaceholder: i18n.t('Search by WMI rule or namespace'),
+    searchableOptions: {
+      searchApiEndpoint: 'config/wmi_rules',
+      defaultSortKeys: ['id'],
+      defaultSearchCondition: {
+        op: 'and',
+        values: [{
+          op: 'or',
+          values: [
+            { field: 'id', op: 'contains', value: null },
+            { field: 'namespace', op: 'contains', value: null }
+          ]
+        }]
+      },
+      defaultRoute: { name: 'wmiRules' }
+    },
+    searchableQuickCondition: (quickCondition) => {
+      return {
+        op: 'and',
+        values: [
+          {
+            op: 'or',
+            values: [
+              { field: 'id', op: 'contains', value: quickCondition },
+              { field: 'namespace', op: 'contains', value: quickCondition }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+
+export const pfConfigurationWmiRuleViewFields = (context = {}) => {
+  const {
+    isNew = false,
+    isClone = false,
+    options: {
+      meta = {}
+    }
+  } = context
+
+  return [
+    {
+      tab: null, // ignore tabs
+      fields: [
+        {
+          label: i18n.t('WMI Rule'),
+          fields: [
+            {
+              key: 'id',
+              component: pfFormInput,
+              attrs: {
+                ...pfConfigurationAttributesFromMeta(meta, 'id'),
+                ...{
+                  disabled: (!isNew && !isClone)
+                }
+              },
+              validators: {
+                ...pfConfigurationValidatorsFromMeta(meta, 'id', 'WMI Rule'),
+                ...{
+                  [i18n.t('WMI Rule exists.')]: not(and(required, conditional(isNew || isClone), hasWmiRules, wmiRuleExists))
+                }
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('On node tab'),
+          text: i18n.t('Scan this WMI element while editing a node.'),
+          fields: [
+            {
+              key: 'on_tab',
+              component: pfFormRangeToggle,
+              attrs: {
+                  values: { checked: '1', unchecked: '0' }
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('Namespace'),
+          fields: [
+            {
+              key: 'namespace',
+              component: pfFormInput,
+              attrs: pfConfigurationAttributesFromMeta(meta, 'namespace'),
+              validators: pfConfigurationValidatorsFromMeta(meta, 'namespace', 'Namespace')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Request'),
+          fields: [
+            {
+              key: 'request',
+              component: pfFormTextarea,
+              attrs: {
+                ...pfConfigurationAttributesFromMeta(meta, 'namespace'),
+                ...{
+                  rows: 3
+                }
+              },
+              validators: pfConfigurationValidatorsFromMeta(meta, 'namespace', 'Namespace')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Rules Actions'),
+          text: i18n.t('Add an action based on the result of the request.'),
+          fields: [
+            {
+              key: 'action',
+              component: pfFormTextarea,
+              attrs: {
+                ...pfConfigurationAttributesFromMeta(meta, 'action'),
+                ...{
+                  rows: 5
+                }
+              },
+              validators: pfConfigurationValidatorsFromMeta(meta, 'action', 'Action')
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/WmiRuleView.vue
@@ -0,0 +1,175 @@
+<template>
+  <pf-config-view
+    :isLoading="isLoading"
+    :disabled="isLoading"
+    :isDeletable="isDeletable"
+    :form="getForm"
+    :model="form"
+    :vuelidate="$v.form"
+    :isNew="isNew"
+    :isClone="isClone"
+    @validations="formValidations = $event"
+    @close="close"
+    @create="create"
+    @save="save"
+    @remove="remove"
+  >
+    <template slot="header" is="b-card-header">
+      <b-button-close @click="close" v-b-tooltip.hover.left.d300 :title="$t('Close [ESC]')"><icon name="times"></icon></b-button-close>
+      <h4 class="mb-0">
+        <span v-if="!isNew && !isClone">{{ $t('WMI Rule {id}', { id: id }) }}</span>
+        <span v-else-if="isClone">{{ $t('Clone WMI Rule {id}', { id: id }) }}</span>
+        <span v-else>{{ $t('New WMI Rule') }}</span>
+      </h4>
+    </template>
+    <template slot="footer">
+      <b-card-footer @mouseenter="$v.form.$touch()">
+        <pf-button-save :disabled="invalidForm" :isLoading="isLoading">
+          <template v-if="isNew">{{ $t('Create') }}</template>
+          <template v-else-if="isClone">{{ $t('Clone') }}</template>
+          <template v-else-if="ctrlKey">{{ $t('Save & Close') }}</template>
+          <template v-else>{{ $t('Save') }}</template>
+        </pf-button-save>
+        <b-button :disabled="isLoading" class="ml-1" variant="outline-primary" @click="init()">{{ $t('Reset') }}</b-button>
+        <b-button v-if="!isNew && !isClone" :disabled="isLoading" class="ml-1" variant="outline-primary" @click="clone()">{{ $t('Clone') }}</b-button>
+        <pf-button-delete v-if="isDeletable" class="ml-1" :disabled="isLoading" :confirm="$t('Delete WMI Rule?')" @on-delete="remove()"/>
+      </b-card-footer>
+    </template>
+  </pf-config-view>
+</template>
+
+<script>
+import pfConfigView from '@/components/pfConfigView'
+import pfButtonSave from '@/components/pfButtonSave'
+import pfButtonDelete from '@/components/pfButtonDelete'
+import pfMixinCtrlKey from '@/components/pfMixinCtrlKey'
+import pfMixinEscapeKey from '@/components/pfMixinEscapeKey'
+import {
+  pfConfigurationDefaultsFromMeta as defaults
+} from '@/globals/configuration/pfConfiguration'
+import {
+  pfConfigurationWmiRuleViewFields as fields
+} from '@/globals/configuration/pfConfigurationWmiRules'
+const { validationMixin } = require('vuelidate')
+
+export default {
+  name: 'WmiRuleView',
+  mixins: [
+    validationMixin,
+    pfMixinCtrlKey,
+    pfMixinEscapeKey
+  ],
+  components: {
+    pfConfigView,
+    pfButtonSave,
+    pfButtonDelete
+  },
+  props: {
+    storeName: { // from router
+      type: String,
+      default: null,
+      required: true
+    },
+    isNew: { // from router
+      type: Boolean,
+      default: false
+    },
+    isClone: { // from router
+      type: Boolean,
+      default: false
+    },
+    id: { // from router
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      form: {}, // will be overloaded with the data from the store
+      formValidations: {}, // will be overloaded with data from the pfConfigView
+      options: {}
+    }
+  },
+  validations () {
+    return {
+      form: this.formValidations
+    }
+  },
+  computed: {
+    isLoading () {
+      return this.$store.getters[`${this.storeName}/isLoading`]
+    },
+    invalidForm () {
+      return this.$v.form.$invalid || this.$store.getters[`${this.storeName}/isWaiting`]
+    },
+    getForm () {
+      return {
+        labelCols: 3,
+        fields: fields(this)
+      }
+    },
+    isDeletable () {
+      if (this.isNew || this.isClone || ('not_deletable' in this.form && this.form.not_deletable)) {
+        return false
+      }
+      return true
+    }
+  },
+  methods: {
+    init () {
+      this.$store.dispatch(`${this.storeName}/options`, this.id).then(options => {
+        // store options
+        this.options = JSON.parse(JSON.stringify(options))
+        if (this.id) {
+          // existing
+          this.$store.dispatch(`${this.storeName}/getWmiRule`, this.id).then(form => {
+            this.form = JSON.parse(JSON.stringify(form))
+          })
+        } else {
+          // new
+          this.form = defaults(options.meta) // set defaults
+        }
+      })
+    },
+    close () {
+      this.$router.push({ name: 'wmiRules' })
+    },
+    clone () {
+      this.$router.push({ name: 'cloneWmiRule' })
+    },
+    create () {
+      const ctrlKey = this.ctrlKey
+      this.$store.dispatch(`${this.storeName}/createWmiRule`, this.form).then(response => {
+        if (ctrlKey) { // [CTRL] key pressed
+          this.close()
+        } else {
+          this.$router.push({ name: 'wmiRule', params: { id: this.form.id } })
+        }
+      })
+    },
+    save () {
+      const ctrlKey = this.ctrlKey
+      this.$store.dispatch(`${this.storeName}/updateWmiRule`, this.form).then(response => {
+        if (ctrlKey) { // [CTRL] key pressed
+          this.close()
+        }
+      })
+    },
+    remove () {
+      this.$store.dispatch(`${this.storeName}/deleteWmiRule`, this.id).then(response => {
+        this.close()
+      })
+    }
+  },
+  created () {
+    this.init()
+  },
+  watch: {
+    isClone: {
+      handler: function (a, b) {
+        this.init()
+      }
+    }
+  }
+}
+</script>
