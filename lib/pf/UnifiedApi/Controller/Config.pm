4fd47602af1c0e0d404084f5314c344948dc58fe
James Rouzier <jrouzier@inverse.ca>
2019-04-04 18:22:44 -0400

    Fix syntax error

--- lib/pf/UnifiedApi/Controller/Config.pm
+++ lib/pf/UnifiedApi/Controller/Config.pm
@@ -118,7 +118,7 @@ sub form {
         return 422, "Invalid requests";
     }
 
-    my $form = $self->form_class->new(@$parameters, @args, user_roles => $c->stash->{'admin_roles'});
+    my $form = $self->form_class->new(@$parameters, @args, user_roles => $self->stash->{'admin_roles'});
     return 200, $form;
 }
 
