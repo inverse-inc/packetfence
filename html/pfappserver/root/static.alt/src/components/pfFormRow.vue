46d71193fdcce31d4f720ece9c5a2006a7a9570f
Darren Satkunas <darren@satkunas.com>
2019-04-09 14:42:14 -0400

    format pre

--- html/pfappserver/root/static.alt/src/components/pfFormRow.vue
+++ html/pfappserver/root/static.alt/src/components/pfFormRow.vue
@@ -1,12 +1,11 @@
 <template>
     <b-row :class="rowClass" :align-v="alignV">
         <b-col sm="3" class="col-form-label"><label class="mb-0" :for="id" v-t="columnLabel"></label></b-col>
-        <b-col sm="9"><slot/></b-col>
+        <b-col sm="9" class="text-pre"><slot/></b-col>
     </b-row>
 </template>
 
 <script>
-
 export default {
   name: 'pf-form-row',
   props: {
@@ -33,3 +32,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.text-pre {
+  white-space: pre-line; /* allow \n to break */
+}
+</style>
