8b89437fa8a947a8ccda2e2cb67bb4859f8f692d
Darren Satkunas <darren@satkunas.com>
2019-05-09 09:03:29 -0400

    (web admin) fix oses chosen to include current value in options

--- html/pfappserver/root/static.alt/src/globals/configuration/pfConfiguration.js
+++ html/pfappserver/root/static.alt/src/globals/configuration/pfConfiguration.js
@@ -141,13 +141,16 @@ export const pfConfigurationAttributesFromMeta = (meta = {}, key = null) => {
           switch (key) {
             case 'oses':
               return [
-                { text: 'Windows Phone OS', value: '33507' },
-                { text: 'Mac OS X or macOS', value: '2' },
-                { text: 'Android OS', value: '33453' },
-                { text: 'Windows OS', value: '1' },
-                { text: 'BlackBerry OS', value: '33471' },
-                { text: 'iOS', value: '33450' },
-                { text: 'Linux OS', value: '5' }
+                ...[
+                  { text: 'Windows Phone OS', value: '33507' },
+                  { text: 'Mac OS X or macOS', value: '2' },
+                  { text: 'Android OS', value: '33453' },
+                  { text: 'Windows OS', value: '1' },
+                  { text: 'BlackBerry OS', value: '33471' },
+                  { text: 'iOS', value: '33450' },
+                  { text: 'Linux OS', value: '5' }
+                ],
+                ...f(chosen, query)
               ]
             default:
               return f(chosen, query)
