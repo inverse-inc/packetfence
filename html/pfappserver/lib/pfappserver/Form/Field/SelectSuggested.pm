b4fb6fc78fb2a71eb67bcfd5523d488ae988bbf4
James Rouzier <jrouzier@inverse.ca>
2019-05-14 12:55:52 -0400

    New select with no validation needed

---
+++ html/pfappserver/lib/pfappserver/Form/Field/SelectSuggested.pm
@@ -0,0 +1,56 @@
+package pfappserver::Form::Field::SelectSuggested;
+
+=head1 NAME
+
+pfappserver::Form::Field::SelectSuggested -
+
+=head1 DESCRIPTION
+
+pfappserver::Form::Field::SelectSuggested
+
+=cut
+
+use strict;
+use warnings;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler::Field::Select';
+
+
+=head2 validate
+
+No validation needed
+
+=cut
+
+sub validate { }
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2019 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
