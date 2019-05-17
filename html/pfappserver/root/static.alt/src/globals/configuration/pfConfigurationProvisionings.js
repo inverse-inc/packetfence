02526a803c56251b5fdef3072d82ff0855c638fd
Darren Satkunas <darren@satkunas.com>
2019-05-17 13:18:09 -0400

    add signing tab to mobileconfig provisioner

--- html/pfappserver/root/static.alt/src/globals/configuration/pfConfigurationProvisionings.js
+++ html/pfappserver/root/static.alt/src/globals/configuration/pfConfigurationProvisionings.js
@@ -3,6 +3,7 @@ import pfFormChosen from '@/components/pfFormChosen'
 import pfFormInput from '@/components/pfFormInput'
 import pfFormPassword from '@/components/pfFormPassword'
 import pfFormRangeToggle from '@/components/pfFormRangeToggle'
+import pfFormTextarea from '@/components/pfFormTextarea'
 import {
   pfConfigurationAttributesFromMeta,
   pfConfigurationValidatorsFromMeta
@@ -262,6 +263,21 @@ export const pfConfigurationProvisioningFields = {
       ]
     }
   },
+  can_sign_profile: ({ options: { meta = {} } } = {}) => {
+    return {
+      label: i18n.t('Sign Profile'),
+      text: i18n.t('Check this box if you want the profiles signed'),
+      fields: [
+        {
+          key: 'can_sign_profile',
+          component: pfFormRangeToggle,
+          attrs: {
+            values: { checked: '1', unchecked: '0' }
+          }
+        }
+      ]
+    }
+  },
   category: ({ options: { meta = {} } } = {}) => {
     return {
       label: i18n.t('Roles'),
@@ -272,6 +288,44 @@ export const pfConfigurationProvisioningFields = {
           component: pfFormChosen,
           attrs: pfConfigurationAttributesFromMeta(meta, 'category'),
           validators: pfConfigurationValidatorsFromMeta(meta, 'category', 'Roles')
+        }
+      ]
+    }
+  },
+  cert_chain: ({ options: { meta = {} } } = {}) => {
+    return {
+      label: i18n.t('The certificate chain for the signer certificate'),
+      text: i18n.t('The certificate chain of the signer certificate in pem format.'),
+      fields: [
+        {
+          key: 'cert_chain',
+          component: pfFormTextarea,
+          attrs: {
+            ...pfConfigurationAttributesFromMeta(meta, 'cert_chain'),
+            ...{
+              rows: 5
+            }
+          },
+          validators: pfConfigurationValidatorsFromMeta(meta, 'cert_chain', 'Chain')
+        }
+      ]
+    }
+  },
+  certificate: ({ options: { meta = {} } } = {}) => {
+    return {
+      label: i18n.t('The certificate for signing profiles'),
+      text: i18n.t('The Certificate for signing in pem format.'),
+      fields: [
+        {
+          key: 'certificate',
+          component: pfFormTextarea,
+          attrs: {
+            ...pfConfigurationAttributesFromMeta(meta, 'certificate'),
+            ...{
+              rows: 5
+            }
+          },
+          validators: pfConfigurationValidatorsFromMeta(meta, 'certificate', 'Certificate')
         }
       ]
     }
@@ -459,6 +513,25 @@ export const pfConfigurationProvisioningFields = {
           component: pfFormInput,
           attrs: pfConfigurationAttributesFromMeta(meta, 'port'),
           validators: pfConfigurationValidatorsFromMeta(meta, 'port', 'Port')
+        }
+      ]
+    }
+  },
+  private_key: ({ options: { meta = {} } } = {}) => {
+    return {
+      label: i18n.t('The private key for signing profiles'),
+      text: i18n.t('The Private Key for signing in pem format.'),
+      fields: [
+        {
+          key: 'private_key',
+          component: pfFormTextarea,
+          attrs: {
+            ...pfConfigurationAttributesFromMeta(meta, 'private_key'),
+            ...{
+              rows: 5
+            }
+          },
+          validators: pfConfigurationValidatorsFromMeta(meta, 'private_key', 'Key')
         }
       ]
     }
@@ -737,7 +810,7 @@ export const pfConfigurationProvisioningViewFields = (context) => {
     case 'mobileconfig':
       return [
         {
-          tab: null, // ignore tabs
+          tab: i18n.t('Settings'),
           fields: [
             ...[
               pfConfigurationProvisioningFields.id(context),
@@ -772,6 +845,15 @@ export const pfConfigurationProvisioningViewFields = (context) => {
               ]
               : [] // ignore
             )
+          ]
+        },
+        {
+          tab: i18n.t('Signing'),
+          fields: [
+            pfConfigurationProvisioningFields.can_sign_profile(context),
+            pfConfigurationProvisioningFields.certificate(context),
+            pfConfigurationProvisioningFields.private_key(context),
+            pfConfigurationProvisioningFields.cert_chain(context)
           ]
         }
       ]
