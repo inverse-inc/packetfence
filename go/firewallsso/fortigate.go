68572bf10c729953c75b0a36dd16542a7cd9f3d2
Julien Semaan <jul.semaan@gmail.com>
2019-05-24 15:15:42 -0400

    send acct session ID in fortigate start

--- go/firewallsso/fortigate.go
+++ go/firewallsso/fortigate.go
@@ -38,6 +38,7 @@ func (fw *FortiGate) Start(ctx context.Context, info map[string]string, timeout 
 func (fw *FortiGate) startRadiusPacket(ctx context.Context, info map[string]string, timeout int) *radius.Packet {
 	r := radius.New(radius.CodeAccountingRequest, []byte(fw.Password))
 	rfc2866.AcctStatusType_Add(r, rfc2866.AcctStatusType_Value_Start)
+	rfc2866.AcctSessionID_AddString(r, "acct_pf-"+info["mac"])
 	rfc2865.UserName_AddString(r, info["username"])
 	rfc2865.Class_AddString(r, info["role"])
 	rfc2865.CalledStationID_AddString(r, "00:11:22:33:44:55")
