6e6834dacb5e9eba2dcd1e77a3b3b44fcecc6d40
Durand Fabrice <oeufdure@gmail.com>
2019-05-30 16:42:38 -0400

    Generate the http get to the familyzone

--- go/firewallsso/familyzone.go
+++ go/firewallsso/familyzone.go
@@ -1,12 +1,12 @@
 package firewallsso
 
 import (
-	"bytes"
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/inverse-inc/packetfence/go/log"
-	"net/url"
-	"text/template"
 )
 
 type FamilyZone struct {
@@ -27,8 +27,7 @@ func (fw *FamilyZone) initChild(ctx context.Context) error {
 	return nil
 }
 
-// Send an SSO start to the FamilyZone using either syslog or HTTP depending on the Transport value of the struct
-// This will return any value from startSyslog or startHttp depending on the type of the transport
+// Send an SSO start to the FamilyZone using HTTP
 func (fw *FamilyZone) Start(ctx context.Context, info map[string]string, timeout int) (bool, error) {
 	log.LoggerWContext(ctx).Info("Sending SSO to FamilyZone using HTTP")
 	return fw.startHttp(ctx, info, timeout)
@@ -37,8 +36,15 @@ func (fw *FamilyZone) Start(ctx context.Context, info map[string]string, timeout
 // Send a start to the FamilyZone using the HTTP transport
 // Will return an error if it fails to get a valid reply from it
 func (fw *FamilyZone) startHttp(ctx context.Context, info map[string]string, timeout int) (bool, error) {
-	resp, err := fw.getHttpClient(ctx).PostForm("https://"+fw.PfconfigHashNS+":"+fw.Port+"/api/?type=user-id&vsys=vsys"+fw.Region+"&action=set&key="+fw.Password,
-		url.Values{"cmd": {fw.startHttpPayload(ctx, info, timeout)}})
+	id, err := uuid.NewUUID()
+	if err != nil {
+		log.LoggerWContext(ctx).Info("Oups too bad")
+	}
+	s := fw.Password + "__" + info["username"] + "_PacketFence_" + id.String()
+	h := sha1.New()
+	h.Write([]byte(s))
+	sha1_hash := hex.EncodeToString(h.Sum(nil))
+	resp, err := fw.getHttpClient(ctx).Get("https://login." + fw.Region + ".linewize.net/login/agent?deviceid=" + info["computername"] + "&mac=" + info["mac"] + "&ip=" + info["ip"] + "&username=" + info["username"] + "&agent=PacketFence&hash=" + sha1_hash + "&salt=" + id.String())
 
 	if err != nil {
 		log.LoggerWContext(ctx).Error(fmt.Sprintf("Error contacting FamilyZone: %s", err))
@@ -50,68 +56,11 @@ func (fw *FamilyZone) startHttp(ctx context.Context, info map[string]string, tim
 	}
 
 	return err == nil, err
-}
-
-// Get the SSO start payload for the firewall
-func (fw *FamilyZone) startHttpPayload(ctx context.Context, info map[string]string, timeout int) string {
-	// FamilyZone XML API expects the timeout in minutes
-	timeout = timeout / 60
-	t := template.New("FamilyZone")
-	t.Parse(`
-<uid-message>
-		<version>1.0</version>
-		<type>update</type>
-		<payload>
-				<login>
-						<entry name="{{.Username}}" ip="{{.Ip}}" timeout="{{.Timeout}}"/>
-				</login>
-		</payload>
-</uid-message>
-`)
-	b := new(bytes.Buffer)
-	t.Execute(b, fw.InfoToTemplateCtx(ctx, info, timeout))
-	return b.String()
 }
 
 // Send an SSO stop to the firewall if the transport mode is HTTP. Otherwise, this outputs a warning
 // Will return the values from stopHttp for HTTP and no error if its syslog
 func (fw *FamilyZone) Stop(ctx context.Context, info map[string]string) (bool, error) {
-	log.LoggerWContext(ctx).Info("Sending SSO to FamilyZone using HTTP")
-	return fw.stopHttp(ctx, info)
-}
-
-// Get the SSO stop payload for the firewall
-func (fw *FamilyZone) stopHttpPayload(ctx context.Context, info map[string]string) string {
-	t := template.New("FamilyZone.stopHttp")
-	t.Parse(`
-<uid-message>
-		<version>1.0</version>
-		<type>update</type>
-		<payload>
-				<logout>
-						<entry name="{{.Username}}" ip="{{.Ip}}"/>
-				</logout>
-		</payload>
-</uid-message>
-`)
-	b := new(bytes.Buffer)
-	t.Execute(b, fw.InfoToTemplateCtx(ctx, info, -1))
-	return b.String()
-}
-
-// Send an SSO stop using HTTP to the FamilyZone firewall
-// Returns an error if it fails to get a valid reply from the firewall
-func (fw *FamilyZone) stopHttp(ctx context.Context, info map[string]string) (bool, error) {
-	resp, err := fw.getHttpClient(ctx).PostForm("https://"+fw.PfconfigHashNS+":"+fw.Port+"/api/?type=user-id&vsys=vsys"+fw.Region+"&action=set&key="+fw.Password,
-		url.Values{"cmd": {fw.stopHttpPayload(ctx, info)}})
-
-	if err != nil {
-		log.LoggerWContext(ctx).Error(fmt.Sprintf("Error contacting FamilyZone: %s", err))
-		//Not returning now so that body closes below
-	}
-
-	if resp != nil && resp.Body != nil {
-		resp.Body.Close()
-	}
-	return err == nil, err
+	log.LoggerWContext(ctx).Info("Not supported sending SSO to FamilyZone using HTTP")
+	return true, nil
 }
