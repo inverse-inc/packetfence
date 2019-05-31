f34668e0c4fbe3afe1e9681a8a93245d8f76b202
Durand Fabrice <oeufdure@gmail.com>
2019-05-31 09:49:36 -0400

    Fixed upgrade-X.X.X-X.Y.Z.sql file

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -1,51 +1,51 @@
---                                                                                                                     
+--
 -- PacketFence SQL schema upgrade from X.X.X to X.Y.Z
---                                                                                                                     
-                                                                                                                       
+--
+
 
 --
--- Setting the major/minor/sub-minor version of the DB                                                                 
---                                                                                                                     
+-- Setting the major/minor/sub-minor version of the DB
+--
 
-SET @MAJOR_VERSION = 9; 
+SET @MAJOR_VERSION = 9;
 SET @MINOR_VERSION = 0;
-SET @SUBMINOR_VERSION = 9;                                                                                             
-                                                                                                                       
-SET @PREV_MAJOR_VERSION = 9;                                                                                           
+SET @SUBMINOR_VERSION = 9;
+
+SET @PREV_MAJOR_VERSION = 9;
 SET @PREV_MINOR_VERSION = 0;
-SET @PREV_SUBMINOR_VERSION = 0;                                                                                        
-                                                                                                                       
+SET @PREV_SUBMINOR_VERSION = 0;
 
---                                                                                                                     
+
+--
 -- The VERSION_INT to ensure proper ordering of the version in queries
---                                                                                                                     
+--
 
-SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;                                     
+SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
 
-SET @PREV_VERSION_INT = @PREV_MAJOR_VERSION << 16 | @PREV_MINOR_VERSION << 8 | @PREV_SUBMINOR_VERSION;                 
+SET @PREV_VERSION_INT = @PREV_MAJOR_VERSION << 16 | @PREV_MINOR_VERSION << 8 | @PREV_SUBMINOR_VERSION;
 
-DROP PROCEDURE IF EXISTS ValidateVersion;                                                                              
+DROP PROCEDURE IF EXISTS ValidateVersion;
 --
 -- Updating to current version
---  
+--
 DELIMITER //
 CREATE PROCEDURE ValidateVersion()
-BEGIN                                                                                                                  
+BEGIN
     DECLARE PREVIOUS_VERSION int(11);
     DECLARE PREVIOUS_VERSION_STRING varchar(11);
     DECLARE _message varchar(255);
-    SELECT id, version INTO PREVIOUS_VERSION, PREVIOUS_VERSION_STRING FROM pf_version ORDER BY id DESC LIMIT 1;        
-              
-      IF PREVIOUS_VERSION != @PREV_VERSION_INT THEN                                                                    
+    SELECT id, version INTO PREVIOUS_VERSION, PREVIOUS_VERSION_STRING FROM pf_version ORDER BY id DESC LIMIT 1;
+
+      IF PREVIOUS_VERSION != @PREV_VERSION_INT THEN
         SELECT CONCAT('PREVIOUS VERSION ', PREVIOUS_VERSION_STRING, ' DOES NOT MATCH ', CONCAT_WS('.', @PREV_MAJOR_VERSION, @PREV_MINOR_VERSION, @PREV_SUBMINOR_VERSION)) INTO _message;
-        SIGNAL SQLSTATE VALUE '99999'                                                                                  
-              SET MESSAGE_TEXT = _message;                                                                             
-      END IF;                                                                                                          
+        SIGNAL SQLSTATE VALUE '99999'
+              SET MESSAGE_TEXT = _message;
+      END IF;
 END
-//                                                                                                                     
+//
 
-DELIMITER ;                                                                                                            
-call ValidateVersion;                                                                                                  
+DELIMITER ;
+call ValidateVersion;
+DROP PROCEDURE IF EXISTS ValidateVersion;
 
-
-INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
+INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
