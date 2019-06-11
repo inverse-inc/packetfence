bbd68948d6f2b7bbef2ea165259bf955e1fe898f
James Rouzier <jrouzier@inverse.ca>
2019-06-11 16:42:06 -0400

    Add example of usage

---
+++ go/netflow5/processor/example_test.go
@@ -0,0 +1,34 @@
+package processor_test
+
+import (
+    "fmt"
+    "github.com/inverse-inc/packetfence/go/netflow5"
+    "github.com/inverse-inc/packetfence/go/netflow5/processor"
+)
+
+func HandleNetFlowV5(header *netflow5.Header, i int, flow *netflow5.Flow) {
+	fmt.Printf("%02d) src : %s dst :%s, next : %s \n", i, flow.SrcIP().String(), flow.DstIP().String(),flow.NextIP().String())
+}
+
+func ExampleProcessor_Start() {
+	processor := processor.Processor{
+		Handler: HandleNetFlowV5,
+	}
+
+	processor.Start()
+}
+
+func ExampleProcessor_Start_conn() {
+    conn, err := net.ListenPacket("udp","127.0.0.2:2055")
+    if err != nil {
+        panic(err)
+    }
+
+	processor := processor.Processor{
+		Handler: HandleNetFlowV5,
+        Conn : conn,
+	}
+
+	processor.Start()
+}
+
