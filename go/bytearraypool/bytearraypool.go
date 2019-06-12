b10fa9f26f439e2e6c54f627a84eec6bf303c7d4
James Rouzier <jrouzier@inverse.ca>
2019-06-12 14:04:20 -0400

    Update godoc

--- go/bytearraypool/bytearraypool.go
+++ go/bytearraypool/bytearraypool.go
@@ -2,20 +2,23 @@ package bytearraypool
 
 import "sync"
 
+// ByteArrayPool caches at most N []byte of M size.
 type ByteArrayPool struct {
 	arraySize int
 	chanPool  chan []byte
 	syncPool  sync.Pool
 }
 
-func NewByteArrayPool(poolSize, arraySize int) *ByteArrayPool {
+// NewByteArrayPool create a *ByteArrayPool 
+func NewByteArrayPool(maxSize, arraySize int) *ByteArrayPool {
 	return &ByteArrayPool{
-		chanPool:  make(chan []byte, poolSize),
+		chanPool:  make(chan []byte, maxSize),
 		syncPool:  sync.Pool{},
 		arraySize: arraySize,
 	}
 }
 
+// Get gets a previously cached []byte or create a new one
 func (p *ByteArrayPool) Get() []byte {
 	if b := p.getFromSyncPool(); b != nil {
 		return b
@@ -25,14 +28,16 @@ func (p *ByteArrayPool) Get() []byte {
 	case b := <-p.chanPool:
 		return b
 	default:
-		return p.NewItem()
+		return p.newItem()
 	}
 }
 
-func (p *ByteArrayPool) NewItem() []byte {
+// newItem createa a new []byte
+func (p *ByteArrayPool) newItem() []byte {
 	return make([]byte, p.arraySize)
 }
 
+// Fill fills the ByteArrayPool with N number of []byte
 func (p *ByteArrayPool) Fill(fill int) {
 	left := cap(p.chanPool) - len(p.chanPool)
 	if fill > left {
@@ -42,7 +47,7 @@ func (p *ByteArrayPool) Fill(fill int) {
 	for i := 0; i < fill; i++ {
 		var b = p.getFromSyncPool()
 		if b == nil {
-			b = p.NewItem()
+			b = p.newItem()
 		}
 
 		select {
@@ -53,6 +58,7 @@ func (p *ByteArrayPool) Fill(fill int) {
 	}
 }
 
+// getFromSyncPool get a []byte from the overflow pool
 func (p *ByteArrayPool) getFromSyncPool() []byte {
 	if i := p.syncPool.Get(); i != nil {
 		b, ok := i.([]byte)
@@ -64,6 +70,7 @@ func (p *ByteArrayPool) getFromSyncPool() []byte {
 	return nil
 }
 
+// Put put a []byte in the pool must no longer be referenced
 func (p *ByteArrayPool) Put(b []byte) {
 	b = b[:]
 	select {
