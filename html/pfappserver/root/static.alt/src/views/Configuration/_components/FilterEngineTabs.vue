f2cac620947821cbdafa38b02cc2569349287576
Francis Lachapelle <flachapelle@inverse.ca>
2019-06-13 14:43:24 -0400

    (web admin) Consume names of /api/config/filters
    See #4589

--- html/pfappserver/root/static.alt/src/views/Configuration/_components/FilterEngineTabs.vue
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/FilterEngineTabs.vue
@@ -5,11 +5,11 @@
       <p class="mb-0" v-t="'This is an advanced section. Proceed with caution when editing.'"></p>
     </b-card-header>
     <b-tabs ref="tabs" v-model="tabIndex">
-      <b-tab v-for="filter in sortedFilters" :key="filter" :title="$t('{filter} filters', { filter: capitalize(filter) })" class="h-100 mb-3" @click="changeTab(filter)">
+      <b-tab v-for="filter in sortedFilters" :key="filter" :title="filter.name" class="h-100 mb-3" @click="changeTab(filter.id)">
         <filter-engine-view
           :storeName="storeName"
-          :id="filter"
-          :ref="filter"
+          :id="filter.id"
+          :ref="filter.id"
         />
       </b-tab>
     </b-tabs>
@@ -46,13 +46,13 @@ export default {
     },
     sortedFilters () {
       let filters = this.filters
-      return filters.sort()
+      return filters.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
     }
   },
   methods: {
     init () {
       this.$store.dispatch(`${this.storeName}/all`).then(filters => {
-        this.filters = filters.map(filter => { return filter.id })
+        this.filters = filters
       })
     },
     changeTab (id) {
