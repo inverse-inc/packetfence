2b38c6c637209a13e5d630dcbe5f38557cf05974
Durand Fabrice <oeufdure@gmail.com>
2019-07-10 12:58:01 -0400

    First draft of httpdportalpreview golang

---
+++ go/httpdportalpreview/httpdportalpreview.go
@@ -0,0 +1,128 @@
+package httpdportalpreview
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+type Proxy struct {
+}
+
+// NewProxy creates a new instance of proxy.
+// It sets request logger using rLogPath as output file or os.Stdout by default.
+// If whitePath of blackPath is not empty they are parsed to set endpoint lists.
+func NewProxy(ctx context.Context) *Proxy {
+	var p Proxy
+
+	return &p
+}
+
+// ServeHTTP satisfy HandlerFunc interface and
+// log, authorize and forward requests
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	host := r.Host
+	rp := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   host,
+	})
+
+	rp.ModifyResponse = UpdateResponse
+
+	// Uses most defaults of http.DefaultTransport with more aggressive timeouts
+	rp.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   2 * time.Second,
+			KeepAlive: 2 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   2 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	// Pass the context in the request
+	r = r.WithContext(ctx)
+	rp.ServeHTTP(w, r)
+}
+
+func UpdateResponse(r *http.Response) error {
+
+	var URL []*url.URL
+
+	buf, _ := ioutil.ReadAll(r.Body)
+
+	bufCopy := bytes.NewBuffer(buf)
+
+	z := html.NewTokenizer(bufCopy)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			for _, v := range URL {
+				// spew.Dump(v)
+				urlOrig := v.String()
+				v.Path = "/portal_preview" + v.EscapedPath()
+				buf = bytes.Replace(buf, []byte(urlOrig), []byte(v.String()), -1)
+			}
+			boeuf := bytes.NewBufferString("")
+			boeuf.Write(buf)
+
+			r.Body = ioutil.NopCloser(boeuf)
+			r.Header["Content-Length"] = []string{fmt.Sprint(boeuf.Len())}
+			return nil
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			// Check if the token is an <a> tag
+			isAnchor := t.Data == "a"
+			if !isAnchor {
+				continue
+			}
+
+			// Extract the href value, if there is one
+			ok, link := getHref(t)
+			if !ok {
+				continue
+			}
+
+			// Make sure the url begines in http**
+			hasProto := strings.Index(link, "http") == 0
+
+			if hasProto {
+				foundLink, _ := url.Parse(link)
+				URL = append(URL, foundLink)
+			}
+		}
+	}
+}
+
+// Helper function to pull the href attribute from a Token
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	// "bare" return will return the variables (ok, href) as defined in
+	// the function definition
+	return
+}
