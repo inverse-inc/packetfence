c2148bd79a382fc27c35e2964dc46f4c5c2bff62
Durand Fabrice <oeufdure@gmail.com>
2019-07-16 14:23:37 -0400

    Clean code

--- go/firewallsso/familyzone.go
+++ go/firewallsso/familyzone.go
@@ -11,6 +11,7 @@ import (
 	"github.com/inverse-inc/packetfence/go/log"
 )
 
+// FamilyZone struct
 type FamilyZone struct {
 	FirewallSSO
 	Username string `json:"username"`
@@ -29,25 +30,25 @@ func (fw *FamilyZone) initChild(ctx context.Context) error {
 	return nil
 }
 
-// Send an SSO start to the FamilyZone using HTTP
+// Start send an SSO start to the FamilyZone using HTTP
 func (fw *FamilyZone) Start(ctx context.Context, info map[string]string, timeout int) (bool, error) {
 	log.LoggerWContext(ctx).Info("Sending SSO to FamilyZone using HTTP")
 	return fw.startHttp(ctx, info, timeout)
 }
 
-// Send a start to the FamilyZone using the HTTP transport
+// StartHttp send a start to the FamilyZone using the HTTP transport
 // Will return an error if it fails to get a valid reply from it
 func (fw *FamilyZone) startHttp(ctx context.Context, info map[string]string, timeout int) (bool, error) {
 	id, err := uuid.NewUUID()
 	if err != nil {
-		log.LoggerWContext(ctx).Error(err.Error)
+		log.LoggerWContext(ctx).Error(err.Error())
 		return false, err
 	}
 	s := fw.Password + "__" + info["username"] + "_PacketFence_" + id.String()
 	h := sha1.New()
 	h.Write([]byte(s))
-	sha1_hash := hex.EncodeToString(h.Sum(nil))
-	resp, err := fw.getHttpClient(ctx).Get("https://login." + fw.Region + ".linewize.net/login/agent?deviceid=" + fw.DeviceID + "&mac=" + info["mac"] + "&ip=" + info["ip"] + "&username=" + strings.ToLower(info["username"]) + "&agent=PacketFence&hash=" + sha1_hash + "&salt=" + id.String())
+	sha1Hash := hex.EncodeToString(h.Sum(nil))
+	resp, err := fw.getHttpClient(ctx).Get("https://login." + fw.Region + ".linewize.net/login/agent?deviceid=" + fw.DeviceID + "&mac=" + info["mac"] + "&ip=" + info["ip"] + "&username=" + strings.ToLower(info["username"]) + "&agent=PacketFence&hash=" + sha1Hash + "&salt=" + id.String())
 
 	if err != nil {
 		log.LoggerWContext(ctx).Error(fmt.Sprintf("Error contacting FamilyZone: %s", err))
@@ -61,7 +62,7 @@ func (fw *FamilyZone) startHttp(ctx context.Context, info map[string]string, tim
 	return err == nil, err
 }
 
-// Send an SSO stop to the firewall if the transport mode is HTTP. Otherwise, this outputs a warning
+// Stop send an SSO stop to the firewall if the transport mode is HTTP. Otherwise, this outputs a warning
 // Will return the values from stopHttp for HTTP and no error if its syslog
 func (fw *FamilyZone) Stop(ctx context.Context, info map[string]string) (bool, error) {
 	log.LoggerWContext(ctx).Info("Not supported sending SSO to FamilyZone using HTTP")
