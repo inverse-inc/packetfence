d41e7e8ba32ad340e844e3a2680b23f48f893845
nqb <nqb+git@azyx.fr>
2019-07-25 08:20:44 +0200

    [packer] refactor shell provisionner

--- ci/packer/provisionners/install-go-dependencies.sh
+++ ci/packer/provisionners/install-go-dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 GOBIN=${GOBIN:-/usr/local/go/bin}
-GOPATH=$HOME/go
+GOPATH=${GOPATH:-$HOME/go}
 GOVENDOR=$GOPATH/bin/govendor
-GOPFDIR=${GOPFDIR:-/root/go/src/github.com/inverse-inc/packetfence/go}
+APP=${APP:-inverse-inc/packetfence}
+GO_APP_WORKSPACE=${GO_APP_WORKSPACE:-src/github.com/$APP/go}
+# needed by govendor to find go binary
 PATH=$GOBIN:$PATH
 
 die() {
@@ -25,10 +27,11 @@ get_govendor_binary() {
     $GOBIN/go get -u github.com/kardianos/govendor
 }
 
-get_pf_dependencies() {
-    log_section "Get PF dependencies"
-    ( cd $GOPFDIR; $GOVENDOR sync )
+get_app_dependencies() {
+    local app=$1
+    log_section "Get $1 Golang dependencies"
+    ( cd $GOPATH/$GO_APP_WORKSPACE; $GOVENDOR sync )
 }
 
 get_govendor_binary
-get_pf_dependencies
+get_app_dependencies $APP
