10a95c4851f45639c48a4ed49da404281a974fcc
nqb <nqb+git@azyx.fr>
2019-08-03 16:21:39 +0200

    [backup] fix wrong test

--- addons/backup-and-maintenance.sh
+++ addons/backup-and-maintenance.sh
@@ -142,7 +142,7 @@ backup_db(){
             mkdir -p $INNO_TMP
             if [ $MARIADB_LOCAL_CLUSTER -eq 1 ]; then
                 innobackupex --defaults-file=/usr/local/pf/var/conf/mariadb.conf --user=$REP_USER --password=$REP_PWD  --no-timestamp --stream=xbstream --tmpdir=$INNO_TMP $INNO_TMP 2>> /usr/local/pf/logs/innobackup.log | gzip - > $BACKUP_DIRECTORY/$BACKUP_DB_FILENAME-innobackup-`date +%F_%Hh%M`.xbstream.gz
-            elif [ $MARIADB_REMOTE -eq 1 ]; then
+            elif [ $MARIADB_REMOTE_CLUSTER -eq 1 ]; then
                 echo "innobackupex can't backup remote databases, uninstall Percona XtraBackup and retry"
             else
                 innobackupex --defaults-file=/usr/local/pf/var/conf/mariadb.conf --user=$DB_USER --password=$DB_PWD --no-timestamp --stream=xbstream --tmpdir=$INNO_TMP $INNO_TMP 2>> /usr/local/pf/logs/innobackup.log | gzip - > $BACKUP_DIRECTORY/$BACKUP_DB_FILENAME-innobackup-`date +%F_%Hh%M`.xbstream.gz
