17a81f958bf4b061653bf002f565c8af0ec244d1
James Rouzier <jrouzier@inverse.ca>
2019-08-08 18:33:53 -0400

    Change the pfperl-api port from 8080 to 22224

--- go/caddy/api-aaa/api-aaa.go
+++ go/caddy/api-aaa/api-aaa.go
@@ -101,7 +101,7 @@ func buildApiAAAHandler(ctx context.Context) (ApiAAAHandler, error) {
 		apiAAA.webservicesBackend.SetUser(pfconfigdriver.Config.PfConf.Webservices.User, pfconfigdriver.Config.PfConf.Webservices.Pass)
 	}
 
-	url, err := url.Parse("http://127.0.0.1:8080/api/v1/authentication/admin_authentication")
+	url, err := url.Parse("http://127.0.0.1:22224/api/v1/authentication/admin_authentication")
 	sharedutils.CheckError(err)
 	apiAAA.authentication.AddAuthenticationBackend(aaa.NewPfAuthenticationBackend(ctx, url, false))
 
