a407cb908b6dd6acb45fb51af65e92f451e47f04
Darren Satkunas <darren@satkunas.com>
2019-08-29 16:33:30 -0400

    (web admin) add initial tooltip components

---
+++ html/pfappserver/root/static.alt/src/components/pfNetworkGraphTooltipSwitch.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-card no-body>
+    <b-card-header>
+      <h4 class="mb-0">{{ id }}</h4>
+    </b-card-header>
+    <div class="card-body">
+      ...
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'pf-network-graph-tooltip-switch',
+  props: {
+    id: {
+      type: String
+    }
+  }
+}
+</script>
