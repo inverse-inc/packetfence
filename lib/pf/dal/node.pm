4025372dc0ec4e59c5ad21e8eccbec995c4c4932
James Rouzier <jrouzier@inverse.ca>
2019-10-29 05:11:04 -0400

    Don't use end_time

--- lib/pf/dal/node.pm
+++ lib/pf/dal/node.pm
@@ -205,7 +205,7 @@ sub _load_locationlog {
             "UNIX_TIMESTAMP(`locationlog`.`start_time`)|last_start_timestamp",
           ],
         -from => 'locationlog',
-        -where => { mac => $self->mac, tenant_id => $self->tenant_id, end_time => $ZERO_DATE},
+        -where => { mac => $self->mac, tenant_id => $self->tenant_id },
         -no_auto_tenant_id => 1,
     );
     return $status, undef if is_error($status);
