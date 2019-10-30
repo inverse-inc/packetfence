33438b3f5fd9b4d2aa1d984c058806df9a47c83e
Durand Fabrice <oeufdure@gmail.com>
2019-10-30 11:19:03 -0400

    Support for KEY_ECDSA and KEY_DSA and KEY_RSA

--- go/pki/models.go
+++ go/pki/models.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bytes"
+	"crypto"
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/asn1"
 	"encoding/pem"
+	"errors"
+	"io"
 	"math/big"
 	"strconv"
 	"time"
@@ -15,6 +22,22 @@ import (
 	"github.com/inverse-inc/packetfence/go/log"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Type int
+
+type DSAKeyFormat struct {
+	Version       int
+	P, Q, G, Y, X *big.Int
+}
+
+var PRNG io.Reader = rand.Reader
+
+const (
+	KEY_UNSUPPORTED Type = iota - 1
+	KEY_ECDSA
+	KEY_RSA
+	KEY_DSA
 )
 
 // CA struct
@@ -28,7 +51,7 @@ type CA struct {
 	Locality            []string `json:"locality"`
 	StreatAddress       []string `json:"streataddress"`
 	PostalCode          []string `json:"postalcode"`
-	KeyType             int      `json:"keytype"`
+	KeyType             Type     `json:"keytype"`
 	KeySize             int      `json:"keysize"`
 	Digest              string   `json:"digest"`
 	KeyUsage            []string `json:"keyusage,omitempty"`
@@ -64,9 +87,32 @@ func (c *CA) new() {
 	}
 
 	// TODO DSA
-	priv, _ := rsa.GenerateKey(rand.Reader, c.KeySize)
+	priv, _ := GenerateKey(c.KeyType, c.KeySize)
+	// priv, _ := rsa.GenerateKey(rand.Reader, c.KeySize)
+	var pub crypto.PublicKey
 
-	pub := &priv.PublicKey
+	keyOut := new(bytes.Buffer)
+
+	switch c.KeyType {
+	case KEY_RSA:
+		pub = &priv.(*rsa.PrivateKey).PublicKey
+		// Private key
+		pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv.(*rsa.PrivateKey))})
+
+	case KEY_ECDSA:
+		pub = &priv.(*ecdsa.PrivateKey).PublicKey
+		bytes, _ := x509.MarshalECPrivateKey(priv.(*ecdsa.PrivateKey))
+		pem.Encode(keyOut, &pem.Block{Type: "EC PRIVATE KEY", Bytes: bytes})
+
+	case KEY_DSA:
+		pub = &priv.(*dsa.PrivateKey).PublicKey
+		val := DSAKeyFormat{
+			P: priv.(*dsa.PrivateKey).P, Q: priv.(*dsa.PrivateKey).Q, G: priv.(*dsa.PrivateKey).G,
+			Y: priv.(*dsa.PrivateKey).Y, X: priv.(*dsa.PrivateKey).X,
+		}
+		bytes, _ := asn1.Marshal(val)
+		pem.Encode(keyOut, &pem.Block{Type: "DSA PRIVATE KEY", Bytes: bytes})
+	}
 	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
 	if err != nil {
 		log.LoggerWContext(ctx).Error("create ca failed", err)
@@ -78,12 +124,9 @@ func (c *CA) new() {
 	db.AutoMigrate(&CA{})
 
 	cert := new(bytes.Buffer)
-	keyOut := new(bytes.Buffer)
+
 	// Public key
 	pem.Encode(cert, &pem.Block{Type: "CERTIFICATE", Bytes: caBytes})
-
-	// Private key
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
 
 	db.Create(&CA{Cn: c.Cn, Mail: c.Mail, Organisation: c.Organisation, Country: c.Country, State: c.State, Locality: c.Locality, StreatAddress: c.StreatAddress, PostalCode: c.PostalCode, KeyType: c.KeyType, KeySize: c.KeySize, Digest: c.Digest, KeyUsage: c.KeyUsage, ExtendedKeyUsage: c.ExtendedKeyUsage, Days: c.Days, CaKey: keyOut.String(), CaCert: cert.String(), IssuerKeyHashmd5: c.IssuerKeyHashmd5, IssuerKeyHashsha1: c.IssuerKeyHashsha1, IssuerKeyHashsha256: c.IssuerKeyHashsha256, IssuerKeyHashsha512: c.IssuerKeyHashsha512})
 
@@ -108,6 +151,67 @@ func (c *CA) keyusage() int {
 	}
 
 	return keyUsage
+}
+
+// GenerateKey based on the Type and the size.
+func GenerateKey(keytype Type, size int) (key interface{}, err error) {
+	switch keytype {
+	case KEY_RSA:
+		if size < 2048 {
+			return nil, errors.New("invalid private key size")
+		}
+		var rsakey *rsa.PrivateKey
+		rsakey, err = rsa.GenerateKey(PRNG, size)
+		if err != nil {
+			return
+		}
+		key = rsakey
+	case KEY_ECDSA:
+		var eckey *ecdsa.PrivateKey
+		switch size {
+		case 256:
+			eckey, err = ecdsa.GenerateKey(elliptic.P256(), PRNG)
+		case 384:
+			eckey, err = ecdsa.GenerateKey(elliptic.P384(), PRNG)
+		case 521:
+			eckey, err = ecdsa.GenerateKey(elliptic.P521(), PRNG)
+		default:
+			return nil, errors.New("invalid private key size")
+		}
+		key = eckey
+	case KEY_DSA:
+		var sizes dsa.ParameterSizes
+		switch size {
+		case 1024:
+			sizes = dsa.L1024N160
+		case 2048:
+			sizes = dsa.L2048N256
+		case 3072:
+			sizes = dsa.L3072N256
+		default:
+			err = errors.New("invalid private key size")
+			return
+		}
+
+		params := dsa.Parameters{}
+		err = dsa.GenerateParameters(&params, rand.Reader, sizes)
+		if err != nil {
+			return
+		}
+
+		dsakey := &dsa.PrivateKey{
+			PublicKey: dsa.PublicKey{
+				Parameters: params,
+			},
+		}
+		err = dsa.GenerateKey(dsakey, rand.Reader)
+		if err != nil {
+			return
+		}
+		key = dsakey
+	}
+
+	return
 }
 
 // func (c *CA) save() {
