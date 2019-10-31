47f9ef25afa922d70d426c29d8185e0974f80723
nqb <nqb+git@azyx.fr>
2019-10-31 12:08:33 +0100

    [packer] manage different Ansible inventories for stable and devel pipeline

--- config.mk
+++ config.mk
@@ -2,3 +2,7 @@ GOVERSION = go1.13.1
 DOCKER_TAG = latest
 REGISTRY = docker.io
 SBINDIR = /usr/local/pf/sbin
+
+ANSIBLE_CENTOS_GROUP=devel_centos
+ANSIBLE_DEBIAN_GROUP=devel_debian
+ANSIBLE_RUBYGEMS_GROUP=devel_rubygems
