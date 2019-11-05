740160139e6bd249ab91dc7e33236a067a5c33b8
nqb <nqb+git@azyx.fr>
2019-11-05 15:47:03 +0100

    remove useless and

--- config.mk
+++ config.mk
@@ -59,29 +59,29 @@ parking_files = $(shell find $(SRC_HTML_PARKINGDIR)/* \
 # exclude node_modules dir and subdirs
 common_files = $(shell find $(SRC_HTML_COMMONDIR)/* \
 	-type f \
-	-and -not -path "$(SRC_HTML_COMMONDIR)/node_modules/*")
+	-not -path "$(SRC_HTML_COMMONDIR)/node_modules/*")
 
 # captive portal files
 cp_files = $(shell find $(SRC_HTML_CPDIR)/* \
 	-type f \
 	-not -path "$(SRC_HTML_CPDIR)/content/node_modules/*" \
-	-and -not -path "$(SRC_HTML_CPDIR)/t/*")
+	-not -path "$(SRC_HTML_CPDIR)/t/*")
 
 # pfappserver files without static and static.alt
 pfapp_files = $(shell find $(SRC_HTML_PFAPPDIR)/* \
 	-type f \
 	-not -name "Changes" \
 	-not -path "$(SRC_HTML_PFAPPDIR)/root-custom*" \
-	-and -not -path "$(SRC_HTML_PFAPPDIR)/t/*" \
-	-and -not -path "$(SRC_HTML_PFAPPDIR_STATIC)*" \
-	-and -not -path "$(SRC_HTML_PFAPPDIR_ALT)*")
+	-not -path "$(SRC_HTML_PFAPPDIR)/t/*" \
+	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)*" \
+	-not -path "$(SRC_HTML_PFAPPDIR_ALT)*")
 
 pfapp_static_files = $(shell find $(SRC_HTML_PFAPPDIR_STATIC)/* \
 	-type f \
-	-and -not -path "$(SRC_HTML_PFAPPDIR_STATIC)/bower_components/*" \
-	-and -not -path "$(SRC_HTML_PFAPPDIR_STATIC)/node_modules/*")
+	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)/bower_components/*" \
+	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)/node_modules/*")
 
 pfapp_alt_files = $(shell find $(SRC_HTML_PFAPPDIR_ALT)/* \
 	-type f \
-	-and -not -path "$(SRC_HTML_PFAPPDIR_ALT)/node_modules/*")
+	-not -path "$(SRC_HTML_PFAPPDIR_ALT)/node_modules/*")
 
