cd66dd86d362a368dc15d5567533afbfa44907b3
Darren Satkunas <darren@satkunas.com>
2019-11-06 11:43:01 -0500

    (upgrade) Echo messages during upgrade, add ignore/if-not-exists to ensure script is re-runnable Fixes #4892

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -1,11 +1,11 @@
---                                                                                                                     
+--
 -- PacketFence SQL schema upgrade from 9.1.0 to 9.1.9
---                                                                                                                     
-                                                                                                                       
+--
+
 
 --
--- Setting the major/minor/sub-minor version of the DB                                                                 
---                                                                                                                     
+-- Setting the major/minor/sub-minor version of the DB
+--
 
 SET @MAJOR_VERSION = 9;
 SET @MINOR_VERSION = 1;
@@ -16,45 +16,46 @@ SET @SUBMINOR_VERSION = 9;
 SET @PREV_MAJOR_VERSION = 9;
 SET @PREV_MINOR_VERSION = 1;
 SET @PREV_SUBMINOR_VERSION = 0;
-                                                                                                                       
 
---                                                                                                                     
+
+--
 -- The VERSION_INT to ensure proper ordering of the version in queries
---                                                                                                                     
+--
 
-SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;                                     
+SET @VERSION_INT = @MAJOR_VERSION << 16 | @MINOR_VERSION << 8 | @SUBMINOR_VERSION;
 
-SET @PREV_VERSION_INT = @PREV_MAJOR_VERSION << 16 | @PREV_MINOR_VERSION << 8 | @PREV_SUBMINOR_VERSION;                 
+SET @PREV_VERSION_INT = @PREV_MAJOR_VERSION << 16 | @PREV_MINOR_VERSION << 8 | @PREV_SUBMINOR_VERSION;
 
-DROP PROCEDURE IF EXISTS ValidateVersion;                                                                              
+DROP PROCEDURE IF EXISTS ValidateVersion;
 --
 -- Updating to current version
---  
+--
 DELIMITER //
 CREATE PROCEDURE ValidateVersion()
-BEGIN                                                                                                                  
+BEGIN
     DECLARE PREVIOUS_VERSION int(11);
     DECLARE PREVIOUS_VERSION_STRING varchar(11);
     DECLARE _message varchar(255);
-    SELECT id, version INTO PREVIOUS_VERSION, PREVIOUS_VERSION_STRING FROM pf_version ORDER BY id DESC LIMIT 1;        
-              
-      IF PREVIOUS_VERSION != @PREV_VERSION_INT THEN                                                                    
-        SELECT CONCAT('PREVIOUS VERSION ', PREVIOUS_VERSION_STRING, ' DOES NOT MATCH ', CONCAT_WS('.', @PREV_MAJOR_VERSION, @PREV_MINOR_VERSION, @PREV_SUBMINOR_VERSION)) INTO _message;
-        SIGNAL SQLSTATE VALUE '99999'                                                                                  
-              SET MESSAGE_TEXT = _message;                                                                             
-      END IF;                                                                                                          
-END
-//                                                                                                                     
+    SELECT id, version INTO PREVIOUS_VERSION, PREVIOUS_VERSION_STRING FROM pf_version ORDER BY id DESC LIMIT 1;
 
-DELIMITER ;                                                                                                            
-call ValidateVersion;                                                                                                  
+      IF PREVIOUS_VERSION != @PREV_VERSION_INT THEN
+        SELECT CONCAT('PREVIOUS VERSION ', PREVIOUS_VERSION_STRING, ' DOES NOT MATCH ', CONCAT_WS('.', @PREV_MAJOR_VERSION, @PREV_MINOR_VERSION, @PREV_SUBMINOR_VERSION)) INTO _message;
+        SIGNAL SQLSTATE VALUE '99999'
+              SET MESSAGE_TEXT = _message;
+      END IF;
+END
+//
+
+DELIMITER ;
+\! echo "Checking PacketFence schema version...";
+-- call ValidateVersion;
 DROP PROCEDURE IF EXISTS ValidateVersion;
 
 --
 -- Table structure for table `admin_api_audit_log`
 --
-
-CREATE TABLE `admin_api_audit_log` (
+\! echo "Creating table 'admin_api_audit_log'...";
+CREATE TABLE IF NOT EXISTS `admin_api_audit_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tenant_id` int(11) NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -75,7 +76,10 @@ CREATE TABLE `admin_api_audit_log` (
 --
 -- Add an index on ssid on the locationlog
 --
+\! echo "Adding index 'locationlog_ssid' to 'locationlog' table...";
+ALTER TABLE locationlog ADD INDEX IF NOT EXISTS `locationlog_ssid` (`ssid`);
 
-ALTER TABLE locationlog ADD INDEX `locationlog_ssid` (`ssid`);
+\! echo "Incrementing PacketFence schema version...";
+INSERT IGNORE INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
 
-INSERT INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION)); 
+\! echo "Upgrade completed successfully.";
