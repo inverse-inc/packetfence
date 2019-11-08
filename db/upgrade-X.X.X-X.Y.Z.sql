37bcf528cbca62ab6a0fa191024645e25de9ee61
James Rouzier <jrouzier@inverse.ca>
2019-11-08 11:24:59 -0500

    Support change utf8 support to utf8mb4 to support 4 byte utf8 characters

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -59,19 +59,19 @@ CREATE TABLE IF NOT EXISTS `admin_api_audit_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tenant_id` int(11) NOT NULL DEFAULT '1',
   `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `action` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `object_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `method` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `request` mediumtext COLLATE utf8_bin,
+  `user_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `action` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `object_id` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `url` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `method` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
+  `request` mediumtext COLLATE utf8mb4_bin,
   `status` smallint(5) NOT NULL,
    PRIMARY KEY (`id`),
    KEY `action` (`action`),
    KEY `user_name` (`user_name`),
    KEY `object_id_action` (`object_id`, `action`),
    KEY `created_at` (`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=COMPRESSED;
 
 --
 -- Add an index on ssid on the locationlog
