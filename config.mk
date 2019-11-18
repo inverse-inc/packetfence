798db06064069deabba8ddd542de7ab61160855d
nqb <nqb+git@azyx.fr>
2019-11-18 08:23:13 +0100

    [pkg] remove useless wildcard to find dots files
    fixes #4935

--- config.mk
+++ config.mk
@@ -53,25 +53,24 @@ HTML_PFAPPDIR_STATIC = $(HTML_PFAPPDIR_ROOT)/static
 HTML_PFAPPDIR_ALT = $(HTML_PFAPPDIR_ROOT)/static.alt
 
 # parking files
-parking_files = $(shell find $(SRC_HTML_PARKINGDIR)/* \
+parking_files = $(shell find $(SRC_HTML_PARKINGDIR) \
 	-type f)
 
 # common files
-# '*' after dir name don't match current directory
 # exclude node_modules dir and subdirs
-common_files = $(shell find $(SRC_HTML_COMMONDIR)/* \
+common_files = $(shell find $(SRC_HTML_COMMONDIR) \
 	-type f \
 	-not -path "$(SRC_HTML_COMMONDIR)/node_modules/*")
 
 # captive portal files
-cp_files = $(shell find $(SRC_HTML_CPDIR)/* \
+cp_files = $(shell find $(SRC_HTML_CPDIR) \
 	-type f \
 	-not -path "$(SRC_HTML_CPDIR)/content/node_modules/*" \
 	-not -path "$(SRC_HTML_CPDIR)/profile-templates/*" \
 	-not -path "$(SRC_HTML_CPDIR)/t/*")
 
 # pfappserver files without static and static.alt
-pfapp_files = $(shell find $(SRC_HTML_PFAPPDIR)/* \
+pfapp_files = $(shell find $(SRC_HTML_PFAPPDIR) \
 	-type f \
 	-not -name "Changes" \
 	-not -path "$(SRC_HTML_PFAPPDIR)/root-custom*" \
@@ -79,12 +78,12 @@ pfapp_files = $(shell find $(SRC_HTML_PFAPPDIR)/* \
 	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)*" \
 	-not -path "$(SRC_HTML_PFAPPDIR_ALT)*")
 
-pfapp_static_files = $(shell find $(SRC_HTML_PFAPPDIR_STATIC)/* \
+pfapp_static_files = $(shell find $(SRC_HTML_PFAPPDIR_STATIC) \
 	-type f \
 	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)/bower_components/*" \
 	-not -path "$(SRC_HTML_PFAPPDIR_STATIC)/node_modules/*")
 
-pfapp_alt_files = $(shell find $(SRC_HTML_PFAPPDIR_ALT)/* \
+pfapp_alt_files = $(shell find $(SRC_HTML_PFAPPDIR_ALT) \
 	-type f \
 	-not -path "$(SRC_HTML_PFAPPDIR_ALT)/node_modules/*")
 
