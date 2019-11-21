b7a11a992b28f329760fcae5af4676062fd7696c
Durand Fabrice <oeufdure@gmail.com>
2019-11-21 08:59:37 -0500

    Revert

--- go/coredns/plugin/pfdns/pfdns.go
+++ go/coredns/plugin/pfdns/pfdns.go
@@ -354,12 +354,7 @@ func (pf *pfdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 					if d.MatchString(state.QName()) {
 						rr.(*dns.A).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeA, Class: state.QClass(), Ttl: 60}
 					} else {
-						var ttl uint32
-						ttl = 15
-						if PortalDetection {
-							ttl = 5
-						}
-						rr.(*dns.A).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeA, Class: state.QClass(), Ttl: ttl}
+						rr.(*dns.A).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeA, Class: state.QClass(), Ttl: 15}
 					}
 					found = true
 					break
@@ -398,12 +393,7 @@ func (pf *pfdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 					if d.MatchString(state.QName()) {
 						rr.(*dns.AAAA).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeAAAA, Class: state.QClass(), Ttl: 60}
 					} else {
-						var ttl uint32
-						ttl = 15
-						if PortalDetection {
-							ttl = 5
-						}
-						rr.(*dns.AAAA).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeAAAA, Class: state.QClass(), Ttl: ttl}
+						rr.(*dns.AAAA).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeAAAA, Class: state.QClass(), Ttl: 15}
 					}
 					found = true
 					break
