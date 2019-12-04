b6cff1fb1842c9af05606676b0c2ddfbe3778501
Darren Satkunas <darren@satkunas.com>
2019-12-04 14:50:01 -0500

    (web admin) refactor pfFormAutocomplete with pfMixinForm

--- html/pfappserver/root/static.alt/src/components/pfFormAutocomplete.vue
+++ html/pfappserver/root/static.alt/src/components/pfFormAutocomplete.vue
@@ -1,12 +1,15 @@
 <template>
-  <b-form-group :label-cols="(columnLabel) ? labelCols : 0" :label="columnLabel"
-    :state="isValid()" :invalid-feedback="getInvalidFeedback()" :class="{ 'mb-0': !columnLabel }">
-    <div class="pf-autocomplete">
-      <b-form-input
+  <b-form-group :label-cols="(columnLabel) ? labelCols : 0" :label="columnLabel" :state="inputState"
+    class="pf-form-autocomplete" :class="{ 'mb-0': !columnLabel }">
+    <template v-slot:invalid-feedback>
+      <icon name="circle-notch" spin v-if="!inputInvalidFeedback"></icon> {{ inputInvalidFeedback }}
+    </template>
+    <b-input-group>
+      <b-form-input ref="input"
         v-model="inputValue"
         v-bind="$attrs"
+        :state="inputState"
         :class="{ 'form-control-with-suggestions': suggestions.length && visible }"
-        :state="isValid()"
         @blur.native="hideSuggestions"
         @focus.native="showSuggestions"
         @keyup.native.up.stop="highlightPrevious"
@@ -14,27 +17,27 @@
         @keyup.native.enter.stop="selectHighlighted"
         @keyup.native.delete="hideSuggestions"
       ></b-form-input>
-      <ul class="pf-autocomplete-suggestions dropdown-menu" :class="{ show: suggestions.length && visible }"
+      <ul class="pf-form-autocomplete-suggestions dropdown-menu" :class="{ show: suggestions.length && visible }"
         @mouseout="resetHightlight" @mousedown="selectHighlighted">
-        <li class="pf-autocomplete-suggestion form-control" v-for="(match, index) in suggestions" :key="match"
-          :class="{ 'pf-autocomplete-suggestion-highlighted': isHighlighted(index) }"
+        <li class="pf-form-autocomplete-suggestion form-control" v-for="(match, index) in suggestions" :key="match"
+          :class="{ 'pf-form-autocomplete-suggestion-highlighted': isHighlighted(index) }"
           @mouseover="highlightIndex(index)">
           <span v-html="highlight(match)"></span>
         </li>
       </ul>
-    </div>
+    </b-input-group>
     <b-form-text v-if="text" v-html="text"></b-form-text>
   </b-form-group>
 </template>
 
 <script>
 import { createDebouncer } from 'promised-debounce'
-import pfMixinValidation from '@/components/pfMixinValidation'
+import pfMixinForm from '@/components/pfMixinForm'
 
 export default {
   name: 'pf-form-autocomplete',
   mixins: [
-    pfMixinValidation
+    pfMixinForm
   ],
   inheritAttrs: false,
   props: {
@@ -57,7 +60,7 @@ export default {
     },
     minLength: {
       type: Number,
-      default: 2
+      default: 3
     },
     debounce: {
       type: Number,
@@ -74,17 +77,24 @@ export default {
   computed: {
     inputValue: {
       get () {
-        return this.value
+        if (this.formStoreName) {
+          return this.formStoreValue // use FormStore
+        } else {
+          return this.value // use native (v-model)
+        }
       },
       set (newValue) {
-        const _this = this
+        if (this.formStoreName) {
+          this.formStoreValue = newValue // use FormStore
+        } else {
+          this.$emit('input', newValue) // use native (v-model)
+        }
         this.$debouncer({
           handler: () => {
-            if (newValue.length > _this.minLength && newValue !== _this.value) {
-              _this.visible = true
+            if (newValue.length >= this.minLength && newValue !== this.value) {
+              this.visible = true
               this.resetHightlight()
-              _this.$emit('input', newValue)
-              _this.$emit('search', newValue)
+              this.$emit('search', newValue)
             }
           },
           time: this.debounce
@@ -93,6 +103,9 @@ export default {
     }
   },
   methods: {
+    focus () {
+      this.$refs.input.focus()
+    },
     hideSuggestions () {
       this.visible = false
     },
@@ -105,9 +118,9 @@ export default {
       this.invalid = true
     },
     highlight (match) {
-      let pos = match.toLowerCase().indexOf(this.value.toLowerCase())
+      let pos = match.toLowerCase().indexOf(this.inputValue.toLowerCase())
       if (pos >= 0) {
-        let escapedValue = this.value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+        let escapedValue = this.inputValue.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
         let re = new RegExp(`(${escapedValue})`, 'gi')
         return match.replace(re, '<b>$1</b>')
       } else {
@@ -135,7 +148,11 @@ export default {
         this.highlightedIndex = 0
       }
       if (this.highlightedIndex >= 0) {
-        this.$emit('input', this.suggestions[this.highlightedIndex])
+        if (this.formStoreName) {
+          this.formStoreValue = this.suggestions[this.highlightedIndex] // use FormStore
+        } else {
+          this.$emit('input', this.suggestions[this.highlightedIndex]) // use native (v-model)
+        }
         this.hideSuggestions()
         this.resetSuggestions()
         this.resetHightlight()
@@ -152,7 +169,7 @@ export default {
 </script>
 
 <style lang="scss">
-.pf-autocomplete {
+.pf-form-autocomplete {
     position: relative;
     // Input field
     .form-control-with-suggestions {
@@ -163,7 +180,7 @@ export default {
         }
     }
     // Matching values
-    .pf-autocomplete-suggestions.dropdown-menu {
+    .pf-form-autocomplete-suggestions.dropdown-menu {
         width: 100%;
         padding: 0;
         border-top: none;
@@ -175,7 +192,7 @@ export default {
         box-shadow: $input-box-shadow, $input-focus-box-shadow;
         border-color: $input-focus-border-color;
         border-top-color: $input-border-color;
-        .pf-autocomplete-suggestion.form-control {
+        .pf-form-autocomplete-suggestion.form-control {
             border: 0;
             border-top-left-radius: 0%;
             border-top-right-radius: 0%;
@@ -184,7 +201,7 @@ export default {
               border-bottom-left-radius: 0%;
               border-bottom-right-radius: 0%;
             }
-            &.pf-autocomplete-suggestion-highlighted {
+            &.pf-form-autocomplete-suggestion-highlighted {
                 background-color: $component-active-bg;
                 color: $component-active-color;
             }
