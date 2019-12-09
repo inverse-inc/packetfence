70cc5f2a1cc05bb31038399d08d905141e482c25
Darren Satkunas <darren@satkunas.com>
2019-12-09 14:38:48 -0500

    (web admin) add pfLocales

---
+++ html/pfappserver/root/static.alt/src/globals/pfLocales.js
@@ -0,0 +1,12 @@
+export const pfLocales = [
+  'en_US',
+  'de_DE',
+  'es_ES',
+  'fr_CA',
+  'fr_FR',
+  'he_IL',
+  'it_IT',
+  'nl_NL',
+  'pl_PL',
+  'pt_BR'
+].map(locale => { return { text: locale, value: locale } })
