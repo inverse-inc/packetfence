b6b531c250933a06d91db1d97fc65cb978729d26
nqb <nqb+git@azyx.fr>
2019-12-11 16:26:51 +0100

    [tests] improve wrapper to run unit tests

--- ci/lib/install/run-unit-tests.sh
+++ ci/lib/install/run-unit-tests.sh
@@ -3,6 +3,9 @@ set -o nounset -o pipefail -o errexit
 
 SRC_DIR=${SRC_DIR:-/src}
 PF_DIR=${PF_DIR:-/usr/local/pf}
+GO_DIR=${GO_DIR:-"$PF_DIR/go"}
+PERL_TESTS=${PERL_TESTS:-yes}
+GOLANG_TESTS=${GOLANG_TESTS:-yes}
 
 # display environment
 env | grep 'PF_'
@@ -12,9 +15,22 @@ env | grep 'PF_'
 cp -a ${SRC_DIR}/t ${PF_DIR}
 
 # Preliminary steps before running unit tests
-# use MYSQL_PWD env variable
+# used MYSQL_PWD env variable
 mysql -uroot < ${PF_DIR}/t/db/smoke_test.sql;
 
-# Makefile is always part of packetfence package
-# use for post-install tasks
-make -C ${PF_DIR} test
+if [ "$PERL_TESTS" = "yes" ]; then
+    echo "Running Perl unit tests"
+
+    # Makefile is always part of packetfence package
+    # use for post-install tasks
+    make -C ${PF_DIR} test
+else
+    echo "Perl unit tests disabled"
+fi
+
+if [ "$GOLANG_TESTS" = "yes" ]; then
+    echo "Running Golang unit tests"
+    make -C ${GO_DIR} test
+else
+    echo "Golang unit tests disabled"
+fi
