b192642e3ec71db9135f9f258ba6c2b19d58f5b0
Darren Satkunas <darren@satkunas.com>
2019-12-18 14:21:42 -0500

    (web admin) migrate general

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/general.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/general.js
@@ -7,75 +7,75 @@ import {
   pfConfigurationValidatorsFromMeta
 } from '@/globals/configuration/pfConfiguration'
 
-export const pfConfigurationGeneralViewFields = (context = {}) => {
-  const {
-    options: {
-      meta = {}
-    }
-  } = context
+export const view = (form = {}, meta = {}) => {
   return [
     {
       tab: null,
-      fields: [
+      rows: [
         {
           label: i18n.t('Domain'),
           text: i18n.t('Domain name of PacketFence system. Changing this requires to restart haproxy-portal.'),
-          fields: [
+          cols: [
             {
-              key: 'domain',
+              namespace: 'domain',
               component: pfFormInput,
-              attrs: pfConfigurationAttributesFromMeta(meta, 'domain'),
-              validators: pfConfigurationValidatorsFromMeta(meta, 'domain', i18n.t('Domain'))
+              attrs: pfConfigurationAttributesFromMeta(meta, 'domain')
             }
           ]
         },
         {
           label: i18n.t('Hostname'),
           text: i18n.t('Hostname of PacketFence system. This is concatenated with the domain in Apache rewriting rules and therefore must be resolvable by clients. Changing this requires to restart haproxy-portal.'),
-          fields: [
+          cols: [
             {
-              key: 'hostname',
+              namespace: 'hostname',
               component: pfFormInput,
-              attrs: pfConfigurationAttributesFromMeta(meta, 'hostname'),
-              validators: pfConfigurationValidatorsFromMeta(meta, 'hostname', i18n.t('Hostname'))
+              attrs: pfConfigurationAttributesFromMeta(meta, 'hostname')
             }
           ]
         },
         {
           label: i18n.t('DHCP servers'),
           text: i18n.t('Comma-separated list of DHCP servers.'),
-          fields: [
+          cols: [
             {
-              key: 'dhcpservers',
+              namespace: 'dhcpservers',
               component: pfFormTextarea,
               attrs: {
                 ...pfConfigurationAttributesFromMeta(meta, 'dhcpservers'),
                 ...{
                   rows: 3
                 }
-              },
-              validators: pfConfigurationValidatorsFromMeta(meta, 'dhcpservers', i18n.t('Servers'))
+              }
             }
           ]
         },
         {
           label: i18n.t('Timezone'),
           text: i18n.t(`System's timezone in string format. List generated from Perl library DateTime::TimeZone. When left empty, it will use the timezone of the server.`),
-          fields: [
+          cols: [
             {
-              key: 'timezone',
+              namespace: 'timezone',
               component: pfFormChosen,
               attrs: {
                 ...pfConfigurationAttributesFromMeta(meta, 'timezone'),
                 ...{
                   optionsLimit: 500
                 }
-              },
-              validators: pfConfigurationValidatorsFromMeta(meta, 'timezone', i18n.t('Timezone'))
+              }
             }
           ]
         }
       ]
     }
   ]
+}
+
+export const validators = (form = {}, meta = {}) => {
+  return {
+    domain: pfConfigurationValidatorsFromMeta(meta, 'domain', i18n.t('Domain')),
+    hostname: pfConfigurationValidatorsFromMeta(meta, 'hostname', i18n.t('Hostname')),
+    dhcpservers: pfConfigurationValidatorsFromMeta(meta, 'dhcpservers', i18n.t('Servers')),
+    timezone: pfConfigurationValidatorsFromMeta(meta, 'timezone', i18n.t('Timezone'))
+  }
 }
