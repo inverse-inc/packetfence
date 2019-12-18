c8e0b2e021e332c628142cfc73d9737b81cf746f
Darren Satkunas <darren@satkunas.com>
2019-12-18 15:56:01 -0500

    (web admin) migrate parking

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/parking.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/parking.js
@@ -6,46 +6,39 @@ import {
   pfConfigurationValidatorsFromMeta
 } from '@/globals/configuration/pfConfiguration'
 
-export const pfConfigurationParkingViewFields = (context = {}) => {
-  const {
-    options: {
-      meta = {}
-    }
-  } = context
+export const view = (form = {}, meta = {}) => {
   return [
     {
       tab: null,
-      fields: [
+      rows: [
         {
           label: i18n.t('Parking lease length'),
           text: i18n.t('Lease length (in seconds) when a device is in parking.'),
-          fields: [
+          cols: [
             {
-              key: 'lease_length',
+              namespace: 'lease_length',
               component: pfFormInput,
-              attrs: pfConfigurationAttributesFromMeta(meta, 'lease_length'),
-              validators: pfConfigurationValidatorsFromMeta(meta, 'lease_length', i18n.t('Length'))
+              attrs: pfConfigurationAttributesFromMeta(meta, 'lease_length')
             }
           ]
         },
         {
           label: i18n.t('Parking threshold'),
           text: i18n.t('The threshold (in seconds) after which a device will be placed in parking. A value of 0 deactivates the parking detection. The detection works by looking at the time in seconds a device has been in the registration role and comparing it with this threshold.'),
-          fields: [
+          cols: [
             {
-              key: 'threshold',
+              namespace: 'threshold',
               component: pfFormInput,
-              attrs: pfConfigurationAttributesFromMeta(meta, 'threshold'),
-              validators: pfConfigurationValidatorsFromMeta(meta, 'threshold', i18n.t('Threshold'))
+              attrs: pfConfigurationAttributesFromMeta(meta, 'threshold')
             }
           ]
         },
         {
           label: i18n.t('Place in DHCP parking group'),
           text: i18n.t('Place the device in the DHCP parking group when it is detected doing parking.'),
-          fields: [
+          cols: [
             {
-              key: 'place_in_dhcp_parking_group',
+              namespace: 'place_in_dhcp_parking_group',
               component: pfFormRangeToggle,
               attrs: {
                 values: { checked: 'enabled', unchecked: 'disabled' }
@@ -56,9 +49,9 @@ export const pfConfigurationParkingViewFields = (context = {}) => {
         {
           label: i18n.t('Show parking portal'),
           text: i18n.t('Show the parking portal to the device instead of the usual portal.'),
-          fields: [
+          cols: [
             {
-              key: 'show_parking_portal',
+              namespace: 'show_parking_portal',
               component: pfFormRangeToggle,
               attrs: {
                 values: { checked: 'enabled', unchecked: 'disabled' }
@@ -69,4 +62,11 @@ export const pfConfigurationParkingViewFields = (context = {}) => {
       ]
     }
   ]
+}
+
+export const validators = (form = {}, meta = {}) => {
+  return {
+    lease_length: pfConfigurationValidatorsFromMeta(meta, 'lease_length', i18n.t('Length')),
+    threshold: pfConfigurationValidatorsFromMeta(meta, 'threshold', i18n.t('Threshold'))
+  }
 }
