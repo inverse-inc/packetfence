e1c48a12cf0f07fbb06ebe80887b9346e9666719
Francis Lachapelle <flachapelle@inverse.ca>
2020-01-09 14:25:33 -0500

    (web admin) Fix usage trigger desription of security event

--- html/pfappserver/root/static.alt/src/components/pfFormSecurityEventTrigger.vue
+++ html/pfappserver/root/static.alt/src/components/pfFormSecurityEventTrigger.vue
@@ -185,10 +185,16 @@ export default {
       return state.$invalid && !state.$pending
     },
     usageDescription () {
-      const { usage: { direction, limit, interval } = {} } = this.formStoreValue || {}
-      return (direction && limit && interval)
-        ? `${bytes.toHuman(limit, 0, true)}B ${triggerDirections[direction]}/${triggerIntervals[interval]}`
-        : this.$i18n.t('Any data usage')
+      const { usage: { direction, limit, interval, type } = {} } = this.formStoreValue || {}
+      if (direction && limit && interval) {
+        return `${bytes.toHuman(limit, 0, true)}B ${triggerDirections[direction]}/${triggerIntervals[interval]}`
+       } else if (type == 'BandwidthExpired') {
+         return this.$i18n.t('Bandwidth balance has expired')
+       } else if (type == 'TimeExpired') {
+         return this.$i18n.t('Time balance has expired')
+       } else {
+         return this.$i18n.t('Any data usage')
+       }
     },
     usageInvalid () {
       const state = this.$store.getters[`${this.formStoreName}/$stateNS`](`${this.formNamespace}.usage`)
