11d391eccb3a9dfa9cf1a00b86d23e0b254286ab
Darren Satkunas <darren@satkunas.com>
2020-01-16 09:17:31 -0500

    (web admin) add initial templates

---
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/switchTemplate.js
@@ -0,0 +1,131 @@
+import i18n from '@/utils/locale'
+import pfFormChosen from '@/components/pfFormChosen'
+import pfFormInput from '@/components/pfFormInput'
+import pfFormRangeToggle from '@/components/pfFormRangeToggle'
+import {
+  attributesFromMeta,
+  validatorsFromMeta
+} from './'
+import { pfSearchConditionType as conditionType } from '@/globals/pfSearch'
+import {
+  and,
+  not,
+  conditional,
+  hasSwitchTemplates,
+  switchTemplateExists
+} from '@/globals/pfValidators'
+import { required } from 'vuelidate/lib/validators'
+
+export const columns = [
+  {
+    key: 'id',
+    label: i18n.t('Identifier'),
+    required: true,
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'coa',
+    label: i18n.t('CoA'),
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'buttons',
+    label: '',
+    locked: true
+  }
+]
+
+export const fields = [
+  {
+    value: 'id',
+    text: i18n.t('Identifier'),
+    types: [conditionType.SUBSTRING]
+  },
+  {
+    value: 'coa',
+    text: i18n.t('CoA'),
+    types: [conditionType.SUBSTRING]
+  }
+]
+
+export const config = () => {
+  return {
+    columns,
+    fields,
+    rowClickRoute (item) {
+      return { name: 'switchTemplate', params: { id: item.id } }
+    },
+    searchPlaceholder: i18n.t('Search by Identifier'),
+    searchableOptions: {
+      searchApiEndpoint: 'config/template_switches',
+      defaultSortKeys: ['id'],
+      defaultSearchCondition: {
+        op: 'and',
+        values: [{
+          op: 'or',
+          values: [
+            { field: 'id', op: 'contains', value: null }
+          ]
+        }]
+      },
+      defaultRoute: { name: 'switchTemplates' }
+    },
+    searchableQuickCondition: (quickCondition) => {
+      return {
+        op: 'and',
+        values: [{
+          op: 'or',
+          values: [
+            { field: 'id', op: 'contains', value: quickCondition }
+          ]
+        }]
+      }
+    }
+  }
+}
+
+export const view = (form = {}, meta = {}) => {
+  const {
+    isNew = false,
+    isClone = false
+  } = meta
+  return [
+    {
+      tab: null, // ignore tabs
+      rows: [
+        {
+          label: i18n.t('Identifier'),
+          cols: [
+            {
+              namespace: 'id',
+              component: pfFormInput,
+              attrs: {
+                ...attributesFromMeta(meta, 'id'),
+                ...{
+                  disabled: (!isNew && !isClone)
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+export const validators = (form, meta = {}) => {
+  const {
+    isNew = false,
+    isClone = false
+  } = meta
+  return {
+    id: {
+      ...validatorsFromMeta(meta, 'id', 'ID'),
+      ...{
+        [i18n.t('Syslog Forwarder exists.')]: not(and(required, conditional(isNew || isClone), hasSwitchTemplates, switchTemplateExists))
+      }
+    }
+  }
+}
