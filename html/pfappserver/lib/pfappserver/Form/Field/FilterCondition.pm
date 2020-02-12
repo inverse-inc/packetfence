ad961b4cd47ea2b072b3a75fad182be20370cba2
James Rouzier <jrouzier@inverse.ca>
2020-02-12 13:40:33 -0500

    Initial work on FilterCondition form

---
+++ html/pfappserver/lib/pfappserver/Form/Field/FilterCondition.pm
@@ -0,0 +1,81 @@
+package pfappserver::Form::Field::FilterCondition;
+
+=head1 NAME
+
+pfappserver::Form::Field::FilterCondition -
+
+=head1 DESCRIPTION
+
+pfappserver::Form::Field::FilterCondition
+
+=cut
+
+use strict;
+use warnings;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler::Field::Compound';
+with 'pfappserver::Base::Form::Role::Help';
+
+
+=head2 name
+
+Name
+
+=cut
+
+has_field 'field' => (
+    type     => 'Text',
+    label    => 'Field',
+    required => 1,
+);
+
+has_field 'value' => (
+    type     => 'Text',
+    label    => 'Value',
+    required => 1,
+);
+
+has_field 'op' => (
+    type     => 'Select',
+    label    => 'Value',
+    required => 1,
+);
+
+has_field values => (
+    type => 'Repeatable',
+    inactive => 1,
+);
+
+has_field 'values.contains' => (
+    type => 'NoValue',
+    inactive => 1,
+);
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2020 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
