fe969e041aef612bd42e9911100cab9ddeb710d3
Julien Semaan <jul.semaan@gmail.com>
2020-03-03 09:08:38 -0500

    improved cleanup

--- addons/monit/check-httpd.portal.sh
+++ addons/monit/check-httpd.portal.sh
@@ -8,6 +8,7 @@ master_pid=`ps -edf | grep httpd.portal | grep -P 'root\s+[0-9]+\s+1 ' | awk '{ 
 
 if [ -z "$master_pid" ]; then
   logger "$script_name Unable to find master Apache PID"
+  rm $tmpfile
   exit 1
 fi
 
@@ -16,6 +17,7 @@ netstat -nlp | grep $master_pid | grep :80 | awk '{ print $4 }' > $tmpfile
 while read host; do
   if ! curl -m 10 -I "$host/captive-portal" | head -n 1 | grep "HTTP/1.1 200 OK" ; then
     logger "$script_name Wrong return code for captive-portal on host $host"
+    rm $tmpfile
     exit 1
   fi
 done < $tmpfile
