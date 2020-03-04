cda8168ee2f5942246a07031fb94989f432ba0f2
Durand Fabrice <oeufdure@gmail.com>
2020-03-03 19:45:50 -0500

    Profiles to Templates (pki

--- html/pfappserver/root/static.alt/src/views/Configuration/_components/PkiProfilesList.vue
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/PkiProfilesList.vue
@@ -5,11 +5,11 @@
     >
       <template v-slot:pageHeader>
         <b-card-header>
-          <h4 class="mb-0">{{ $t('Profiles') }}</h4>
+          <h4 class="mb-0">{{ $t('Templates') }}</h4>
         </b-card-header>
       </template>
       <template v-slot:buttonAdd>
-        <b-dropdown :text="$t('New Profile')" variant="outline-primary" :disabled="cas.length === 0">
+        <b-dropdown :text="$t('New Template')" variant="outline-primary" :disabled="cas.length === 0">
           <b-dropdown-header>{{ $t('Choose Certificate Authority') }}</b-dropdown-header>
           <b-dropdown-item v-for="ca in cas" :key="ca.ID" :to="{ name: 'newPkiProfile', params: { ca_id: ca.ID } }">{{ ca.cn }}</b-dropdown-item>
         </b-dropdown>
