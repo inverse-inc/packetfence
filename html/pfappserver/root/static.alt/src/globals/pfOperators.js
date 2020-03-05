1eb013e9c274b250a8d93f21ac96411ca09ac77f
Darren Satkunas <darren@satkunas.com>
2020-03-05 11:52:15 -0500

    (web admin) rename fingerbank ops

--- html/pfappserver/root/static.alt/src/globals/pfOperators.js
+++ html/pfappserver/root/static.alt/src/globals/pfOperators.js
@@ -20,14 +20,13 @@ export const pfOperators = {
   not_equals: i18n.t('not equals'),
   regex: i18n.t('matches'),
   not_regex: i18n.t('not matches'),
+  fingerbank_device_is_a: i18n.t('Fingerbank device is'),
+  not_fingerbank_device_is_a: i18n.t('Fingerbank device is not'),
   date_is_before: i18n.t('date is before'),
   not_date_is_before: i18n.t('date is not before'),
   date_is_after: i18n.t('date is after'),
   not_date_is_after: i18n.t('date is not after'),
   time_period: i18n.t('time period within'),
-  not_time_period: i18n.t('time period not within'),
-
-  'fingerbank::device_is_a': i18n.t('Fingerbank device is'),
-  'not_fingerbank::device_is_a': i18n.t('Fingerbank device is not')
+  not_time_period: i18n.t('time period not within')
 }
 
