70bf6331815a4888108b15340894fce5cb642b7a
nqb <nqb+git@azyx.fr>
2020-03-05 13:42:18 +0100

    helper scripts to provision switch and nodes

---
+++ addons/vagrant/cumulus/helper_scripts/config_node.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+echo "#################################"
+echo "  Running config_node.sh"
+echo "#################################"
+sudo su
+
+# Make DHCP Try Over and Over Again
+echo "retry 1;" >> /etc/dhcp/dhclient.conf
+
+#Replace existing network interfaces file
+echo -e "auto lo" > /etc/network/interfaces
+echo -e "iface lo inet loopback\n\n" >> /etc/network/interfaces
+
+#Add vagrant interface
+echo -e "\n\nauto eth0" >> /etc/network/interfaces
+echo -e "iface eth0 inet dhcp\n\n" >> /etc/network/interfaces
+
+useradd cumulus -m -s /bin/bash
+echo "cumulus:CumulusLinux!" | chpasswd
+sed "s/PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config
+
+## Convenience code. This is normally done in ZTP.
+echo "cumulus ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10_cumulus
+mkdir /home/cumulus/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzH+R+UhjVicUtI0daNUcedYhfvgT1dbZXgY33Ibm4MOo+X84Iwuzirm3QFnYf2O3uyZjNyrA6fj9qFE7Ekul4bD6PCstQupXPwfPMjns2M7tkHsKnLYjNxWNql/rCUxoH2B6nPyztcRCass3lIc2clfXkCY9Jtf7kgC2e/dmchywPV5PrFqtlHgZUnyoPyWBH7OjPLVxYwtCJn96sFkrjaG9QDOeoeiNvcGlk4DJp/g9L4f2AaEq69x8+gBTFUqAFsD8ecO941cM8sa1167rsRPx7SK3270Ji5EUF3lZsgpaiIgMhtIB/7QNTkN9ZjQBazxxlNVN6WthF8okb7OSt" >> /home/cumulus/.ssh/authorized_keys
+chmod 700 -R /home/cumulus
+chown -R cumulus:cumulus /home/cumulus
+chmod 600 /home/cumulus/.ssh/*
+chmod 700 /home/cumulus/.ssh
+
+
+# Other stuff
+ping 8.8.8.8 -c2
+if [ "$?" == "0" ]; then
+  apt-get update -qy
+  apt-get install lldpd ntp ntpdate -qy
+  echo "configure lldp portidsubtype ifname" > /etc/lldpd.d/port_info.conf 
+fi
+
+# Set Timezone
+cat << EOT > /etc/timezone
+Etc/UTC
+EOT
+
+# Apply Timezone Now
+# dpkg-reconfigure -f noninteractive tzdata
+
+# Write NTP Configuration
+cat << EOT > /etc/ntp.conf
+# /etc/ntp.conf, configuration for ntpd; see ntp.conf(5) for help
+
+driftfile /var/lib/ntp/ntp.drift
+
+statistics loopstats peerstats clockstats
+filegen loopstats file loopstats type day enable
+filegen peerstats file peerstats type day enable
+filegen clockstats file clockstats type day enable
+
+server 192.168.0.254 iburst
+
+# By default, exchange time with everybody, but don't allow configuration.
+restrict -4 default kod notrap nomodify nopeer noquery
+restrict -6 default kod notrap nomodify nopeer noquery
+
+# Local users may interrogate the ntp server more closely.
+restrict 127.0.0.1
+restrict ::1
+
+# Specify interfaces, don't listen on switch ports
+interface listen eth0
+EOT
+
+sudo systemctl enable ntp.service
+sudo systemctl start ntp.service
+
+# Once initial provisioning is done using vagrant management network
+# we apply new network configuration
+echo " ### Overwriting /etc/network/interfaces ###"
+cat <<EOT > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0
+    alias VLAN 17
+    address 172.17.1.251/24
+
+EOT
+
+echo "#################################"
+echo "   Finished"
+echo "#################################"
---
+++ addons/vagrant/cumulus/helper_scripts/config_switch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "#################################"
+echo "  Running Switch Post Config (config_switch.sh)"
+echo "#################################"
+sudo su
+
+# Config for OOB Switch
+cat <<EOT > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+    alias Interface used by Vagrant
+
+# we bridge interfaces on VLAN17
+# to use for Ansible provisioning
+auto bridge1
+iface bridge1
+    alias Untagged Bridge VLAN 17
+    bridge-ports swp1 swp11
+    hwaddress a0:00:00:00:00:11
+    address 172.17.1.201/24
+    
+EOT
+
+echo "#################################"
+echo "   Finished"
+echo "#################################"
