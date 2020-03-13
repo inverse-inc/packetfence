915b808bfa2c90f171773e45b43540f9a933af64
James Rouzier <jrouzier@inverse.ca>
2020-03-13 05:36:06 -0400

    Include the ip address in log message

--- go/acct/radius.go
+++ go/acct/radius.go
@@ -218,7 +218,7 @@ func (h *PfAcct) RADIUSSecret(ctx context.Context, remoteAddr net.Addr, raw []by
 
 	switchInfo, err := h.SwitchLookup(macStr, ip)
 	if err != nil {
-		logError(h.LoggerCtx, "RADIUSSecret: Switch not found"+err.Error())
+		logError(h.LoggerCtx, "RADIUSSecret: Switch '" + ip +"' not found :"+err.Error())
 		return nil, nil, err
 	}
 
