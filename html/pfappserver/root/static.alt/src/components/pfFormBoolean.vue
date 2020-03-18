cdd163a37482ae1081d855d808e2b3d66734f15c
Darren Satkunas <darren@satkunas.com>
2020-03-18 10:07:26 -0400

    (web admin) fix focus stealing

--- html/pfappserver/root/static.alt/src/components/pfFormBoolean.vue
+++ html/pfappserver/root/static.alt/src/components/pfFormBoolean.vue
@@ -69,7 +69,7 @@
         </pf-form-boolean>
 
         <!-- recurse -->
-        <pf-form-boolean v-if="value" v-bind="attrs(index)" :key="JSON.stringify(value)" :isRoot="false" :dataBus="dataBus" :class="{ 'drag-source': index === sourceIndex }" :disabled="disabled"
+        <pf-form-boolean v-if="value" v-bind="attrs(index)" :key="index" :isRoot="false" :dataBus="dataBus" :class="{ 'drag-source': index === sourceIndex }" :disabled="disabled"
           @addOperator="addOperator(index)"
           @cloneOperator="cloneOperator(index)"
           @deleteOperator="deleteOperator(index)"
