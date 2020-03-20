c6dd6b3dbad99f764f251e1f3aadb28eac2f12e4
Francis Lachapelle <flachapelle@inverse.ca>
2020-03-20 18:10:40 -0400

    (configurator) Add validation to database configuration

--- html/pfappserver/root/static.alt/src/views/Configurator/_components/DatabaseView.vue
+++ html/pfappserver/root/static.alt/src/views/Configurator/_components/DatabaseView.vue
@@ -9,6 +9,9 @@
     <template v-slot:header>
       <h4 class="mb-0">
         <span>{{ $t('Database') }}</span>
+        <div class="float-right">
+          <pf-form-toggle v-model="advancedMode">{{ $t('Advanced') }}</pf-form-toggle>
+        </div>
       </h4>
     </template>
   </pf-config-view>
@@ -16,6 +19,7 @@
 
 <script>
 import pfConfigView from '@/components/pfConfigView'
+import pfFormToggle from '@/components/pfFormToggle'
 import {
   view,
   validators
@@ -24,7 +28,8 @@ import {
 export default {
   name: 'database-view',
   components: {
-    pfConfigView
+    pfConfigView,
+    pfFormToggle
   },
   props: {
     formStoreName: {
@@ -45,16 +50,55 @@ export default {
     },
     isLoading () {
       return this.$store.getters['$_bases/isLoading']
+    },
+    advancedMode: { // mutating this property will re-evaluate view() and validators()
+      get () {
+        const { meta: { database: { advancedMode = false } = {} } = {} } = this
+        return advancedMode
+      },
+      set (newValue) {
+        this.$set(this.meta.database, 'advancedMode', newValue)
+      }
+    // },
+    // hostState () {
+    //   return this.$store.getters[`${this.formStoreName}/$stateNS`]('database.host')
+    // },
+    // portState () {
+    //   return this.$store.getters[`${this.formStoreName}/$stateNS`]('database.port')
     }
+
   },
   methods: {
     init () {
       this.$store.dispatch('$_bases/optionsDatabase').then(({ meta }) => {
         this.$store.dispatch(`${this.formStoreName}/appendMeta`, { database: { properties: meta } })
         this.$store.dispatch(`${this.formStoreName}/appendFormValidations`, validators)
+        // Fetch configuration
+        this.$store.dispatch('$_bases/getDatabase').then(form => {
+          this.$store.dispatch(`${this.formStoreName}/appendForm`, { database: form })
+          this.initialValidation()
+          // TODO: if host or port is changed, call this.save and this.initialValidation
+          // this.$watch('hostState')
+          // this.$watch('portState')
+        })
       })
-      this.$store.dispatch('$_bases/getDatabase').then(form => {
-        this.$store.dispatch(`${this.formStoreName}/appendForm`, { database: form })
+    },
+    initialValidation () {
+      const form = this.form.database
+      // Check if root has no password
+      this.$store.dispatch('$_bases/testDatabase', { username: 'root' }).then(() => {
+        this.$set(this.meta.database, 'setRootPassword', true) // need to define a root password
+        this.$store.dispatch('$_bases/testDatabase', { username: 'root', database: form.db || 'pf' }).then(() => {
+          this.$set(this.meta.database, 'databaseExists', true) // database exists
+        })
+      }).catch(() => {})
+      // Check if database name and credentials are valid
+      this.$store.dispatch('$_bases/testDatabase', { username: form.user || 'pf', password: form.pass, database: form.db || 'pf' }).then(() => {
+        this.$set(this.meta.database, 'databaseExists', true)
+        this.$set(this.meta.database, 'userIsValid', true)
+        this.$set(this.meta.database, 'rootPasswordIsRequired', false) // we no longer need the root password
+      }).catch(() => {
+        this.$set(this.meta.database, 'setUserPassword', true) // credentials don't work, user probably doesn't exist
       })
     },
     save () {
--- html/pfappserver/root/static.alt/src/views/Configurator/_config/database.js
+++ html/pfappserver/root/static.alt/src/views/Configurator/_config/database.js
@@ -1,17 +1,55 @@
+import Vue from 'vue'
+import store from '@/store'
 import i18n from '@/utils/locale'
+import pfButton from '@/components/pfButton'
+import pfFormHtml from '@/components/pfFormHtml'
 import pfFormInput from '@/components/pfFormInput'
 import pfFormPassword from '@/components/pfFormPassword'
 import {
   attributesFromMeta,
   validatorsFromMeta
 } from '@/views/Configuration/_config'
+import {
+  or,
+  conditional
+} from '@/globals/pfValidators'
+import {
+  required
+} from 'vuelidate/lib/validators'
 
 export const view = (form = {}, meta = {}) => {
+  let {
+    database: {
+      db,
+      root_pass = '',
+      user,
+      pass = ''
+    } = {}
+  } = form
+  const {
+    database: {
+      advancedMode = false,
+      setRootPassword = false,
+      rootPasswordIsRequired = true,
+      rootPasswordIsValid = false,
+      rootPasswordIsInvalid = false,
+      databaseExists = false,
+      databaseCreationError = false,
+      userIsValid = false,
+      userCreationError = false
+    } = {}
+  } = meta
+  db = db || 'pf'
+  user = user || 'pf'
   return [
     {
       tab: null,
       rows: [
+        /**
+         * Advanced settings
+         */
         {
+          if: advancedMode,
           label: i18n.t('Hostname'),
           text: i18n.t('Server the MySQL server is running on.'),
           cols: [
@@ -23,6 +61,7 @@ export const view = (form = {}, meta = {}) => {
           ]
         },
         {
+          if: advancedMode,
           label: i18n.t('Port'),
           text: i18n.t('Port the MySQL server is running on.'),
           cols: [
@@ -33,6 +72,81 @@ export const view = (form = {}, meta = {}) => {
             }
           ]
         },
+        /**
+         * Root password
+         */
+        {
+          if: rootPasswordIsRequired && setRootPassword,
+          label: i18n.t('Root Password'),
+          text: i18n.t('Define a root password for the MySQL server.'),
+          cols: [
+            {
+              namespace: 'database.root_pass',
+              component: pfFormPassword,
+              attrs: {
+                generate: true,
+                readonly: rootPasswordIsValid,
+                testLabel: i18n.t('Set Password'),
+                test: () => {
+                  return store.dispatch('$_bases/secureDatabase', { username: 'root', password: root_pass }).then(() => {
+                    Vue.set(meta.database, 'rootPasswordIsValid', true)
+                  })
+                }
+              }
+            }
+          ]
+        },
+        {
+          if: rootPasswordIsRequired && !setRootPassword,
+          label: i18n.t('Root Password'),
+          text: i18n.t('Current root password of the MySQL server.'),
+          cols: [
+            {
+              namespace: 'database.root_pass',
+              component: pfFormPassword,
+              attrs: {
+                readonly: rootPasswordIsValid,
+                stateMap: { false: false, true: rootPasswordIsValid ? true : null }
+              }
+            }
+          ]
+        },
+        {
+          if: rootPasswordIsRequired && !(setRootPassword || rootPasswordIsValid),
+          label: true, // trick to keep bottom margin in pfConfigView
+          cols: [
+            {
+              component: pfButton,
+              attrs: {
+                label: i18n.t('Verify'),
+                class: 'col-2',
+                variant: rootPasswordIsInvalid ? 'outline-danger' : 'outline-primary',
+                disabled: root_pass.length === 0
+              },
+              listeners: {
+                click: () => {
+                  return store.dispatch('$_bases/testDatabase', { username: 'root', password: root_pass }).then(() => {
+                    Vue.set(meta.database, 'rootPasswordIsValid', true)
+                    store.dispatch('$_bases/testDatabase', { username: 'root', database: db || 'pf' }).then(() => {
+                      Vue.set(this.meta.database, 'databaseExists', true) // database exists
+                    })
+                  }).catch(() => {
+                    Vue.set(meta.database, 'rootPasswordIsInvalid', true)
+                  })
+                }
+              }
+            },
+            {
+              component: pfFormHtml,
+              attrs: {
+                html: rootPasswordIsInvalid ? '<div class="small text-danger p-2">' + i18n.t('Wrong password') + '</div>' : ''
+              }
+            }
+          ]
+        },
+        /**
+         * Database name
+         */
         {
           label: i18n.t('Database name'),
           text: i18n.t('Name of the MySQL database used by PacketFence.'),
@@ -40,10 +154,54 @@ export const view = (form = {}, meta = {}) => {
             {
               namespace: 'database.db',
               component: pfFormInput,
-              attrs: attributesFromMeta(meta, 'database.db')
+              attrs: {
+                ...attributesFromMeta(meta, 'database.db'),
+                stateMap: { false: false, true: databaseExists ? true : null },
+                disabled: databaseExists
+              }
             }
           ]
         },
+        {
+          if: !databaseExists,
+          label: true, // trick to keep bottom margin in pfConfigView
+          cols: [
+            {
+              component: pfButton,
+              attrs: {
+                label: i18n.t('Create'),
+                class: 'col-2',
+                variant: 'outline-primary',
+                disabled: !rootPasswordIsValid
+              },
+              listeners: {
+                click: () => {
+                  return store.dispatch('$_bases/createDatabase', { username: 'root', password: root_pass, database: db }).then(() => {
+                    Vue.set(meta.database, 'databaseExists', true)
+                  }).catch(err => {
+                    const {
+                      response: {
+                        data: {
+                          message = false
+                        } = {}
+                      } = {}
+                    } = err
+                    Vue.set(meta.database, 'databaseCreationError', message)
+                  })
+                }
+              }
+            },
+            {
+              component: pfFormHtml,
+              attrs: {
+                html: databaseCreationError ? '<div class="small text-danger p-2">' + databaseCreationError + '</div>' : ''
+              }
+            }
+          ]
+        },
+        /**
+         * Username and password
+         */
         {
           label: i18n.t('User'),
           text: i18n.t('Username of the account with access to the MySQL database used by PacketFence.'),
@@ -51,18 +209,62 @@ export const view = (form = {}, meta = {}) => {
             {
               namespace: 'database.user',
               component: pfFormInput,
-              attrs: attributesFromMeta(meta, 'database.user')
+              attrs: {
+                disabled: userIsValid,
+                class: 'col-6 pr-1',
+                stateMap: { false: false, true: userIsValid ? true : null },
+                ...attributesFromMeta(meta, 'database.user')
+              }
+            },
+            {
+              namespace: 'database.pass',
+              component: pfFormPassword,
+              attrs: {
+                disabled: userIsValid,
+                class: 'col-6 pl-1',
+                stateMap: { false: false, true: userIsValid ? true : null },
+                ...attributesFromMeta(meta, 'database.pass')
+                // test: () => {
+                //   return store.dispatch('$_bases/testDatabase', { username: user, password: pass, database: db })
+                // }
+              }
             }
           ]
         },
         {
-          label: i18n.t('Password'),
-          text: i18n.t('Password for the MySQL database used by PacketFence.'),
+          if: !userIsValid,
+          label: true, // trick to keep bottom margin in pfConfigView
           cols: [
             {
-              namespace: 'database.pass',
-              component: pfFormPassword,
-              attrs: attributesFromMeta(meta, 'database.pass')
+              component: pfButton,
+              attrs: {
+                label: i18n.t('Create'),
+                class: 'col-2',
+                variant: 'outline-primary',
+                disabled: !rootPasswordIsValid
+              },
+              listeners: {
+                click: () => {
+                  return store.dispatch('$_bases/assignDatabase', { username: 'root', password: root_pass, pf_username: user, pf_password: pass, database: db }).then(() => {
+                    Vue.set(meta.database, 'userIsValid', true)
+                  }).catch(err => {
+                    const {
+                      response: {
+                        data: {
+                          message = false
+                        } = {}
+                      } = {}
+                    } = err
+                    Vue.set(meta.database, 'userCreationError', message)
+                  })
+                }
+              }
+            },
+            {
+              component: pfFormHtml,
+              attrs: {
+                html: userCreationError ? '<div class="small text-danger p-2">' + userCreationError + '</div>' : ''
+              }
             }
           ]
         }
@@ -71,14 +273,34 @@ export const view = (form = {}, meta = {}) => {
   ]
 }
 
-export const validators = (form = {}, meta = {}) => {
+export const validators = (form, meta = {}) => {
+  const {
+    database: {
+      setUserPassword = false,
+      rootPasswordIsRequired = false,
+      databaseExists = false,
+      userIsValid = false
+    } = {}
+  } = meta
   return {
     database: {
       host: validatorsFromMeta(meta, 'database.host', i18n.t('Host')),
       port: validatorsFromMeta(meta, 'database.port', i18n.t('Port')),
       db: validatorsFromMeta(meta, 'database.db', i18n.t('Database')),
-      user: validatorsFromMeta(meta, 'database.user', i18n.t('User')),
-      pass: validatorsFromMeta(meta, 'database.user', i18n.t('Password'))
+      user: {
+        [i18n.t('Database username required')]: or(conditional(setUserPassword === false), required),
+        ...validatorsFromMeta(meta, 'database.user', i18n.t('User'))
+      },
+      pass: validatorsFromMeta(meta, 'database.pass', i18n.t('Password')),
+      root_pass: {
+        [i18n.t('Root password required.')]: or(conditional(!rootPasswordIsRequired), required)
+      },
+      database_exists: {
+        [i18n.t('Create the database.')]: conditional(databaseExists)
+      },
+      user_is_valid: {
+        [i18n.t('Create the database user.')]: conditional(userIsValid)
+      }
     }
   }
 }
