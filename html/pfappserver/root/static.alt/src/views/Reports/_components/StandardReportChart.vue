7c3251e5c3bcdfc20145b05ca05151700fd1cb88
Darren Satkunas <darren@satkunas.com>
2020-03-20 13:41:15 -0400

    (web admin) improve standard report ui to avoid automatically loading indefinite date ranges, various other ui improvements

--- html/pfappserver/root/static.alt/src/views/Reports/_components/StandardReportChart.vue
+++ html/pfappserver/root/static.alt/src/views/Reports/_components/StandardReportChart.vue
@@ -5,7 +5,7 @@
     </b-card-header>
 
     <b-tabs ref="tabs" v-model="tabIndex" card>
-      <b-tab v-for="tab in tabs" :key="report.category + report.name + tab.name" :title="tab.name" no-body>
+      <b-tab v-for="tab in tabs" :key="tab.name" :title="tab.name" no-body>
         <template v-slot:title>
           {{ $t(tab.name) }}
         </template>
@@ -15,7 +15,7 @@
 
     <pf-report-chart v-if="report.chart"
       :report="report"
-      :range="range && (range.optional || range.mandatory)"
+      :range="range"
       :items="items"
       :datetime-start="datetimeStart"
       :datetime-end="datetimeEnd"
@@ -128,7 +128,8 @@ export default {
       return this.report.tabs
     },
     range () {
-      return this.tabs[this.tabIndex].range
+      const { tabs: { [Math.max(0, this.tabIndex)] : { range = false } = {} } = {} } = this
+      return range
     },
     /**
      * build report using routers' path,
@@ -162,16 +163,30 @@ export default {
       this.items = []
       this.isLoading = true
       apiCall.get(this.apiEndpoint, {}).then(response => {
-        this.items = response.data.items
+        const len = response.data.items.length - 1
+        this.items = response.data.items.map((item, index) => {
+          return (index === len)
+            ? { ...item, ...{ _rowVariant: 'primary' } } // highlight totals
+            : item
+        })
         this.requestPage = 1
-        this.isLoading = false
       }).catch(err => {
-        this.isLoading = false
         return err
+      }).finally(() => {
+        this.isLoading = false
       })
     },
-    getReportByName (name) {
-      return reportCategories().map(category => category.reports.map(report => Object.assign({ category: category.name }, report))).reduce((l, n) => l.concat(n), []).filter(report => report.name === name)[0]
+    getReportCategoryByPath (reportPath) {
+      return reportCategories().find(category => {
+        const { reports = [] } = category
+        return reports.find(report => {
+          const { tabs = [] } = report
+          return tabs.find(tab => {
+            const { path } = tab
+            return path === reportPath
+          })
+        })
+      })
     },
     getReportByPath (path) {
       return reportCategories().map(category => category.reports.map(report => Object.assign({ category: category.name }, report))).reduce((l, n) => l.concat(n), []).filter(report => report.tabs.map(tab => tab.path).includes(path))[0]
@@ -179,40 +194,59 @@ export default {
     onChangeDatetimeStart (datetime) {
       this.datetimeStart = datetime
       const rpath = this.getApiEndpointRangePath(this.range)
-      this.apiEndpoint = `reports/${this.path}${rpath}`
+      if (rpath) {
+        this.apiEndpoint = `reports/${this.path}${rpath}`
+      }
     },
     onChangeDatetimeEnd (datetime) {
       this.datetimeEnd = datetime
       const rpath = this.getApiEndpointRangePath(this.range)
-      this.apiEndpoint = `reports/${this.path}${rpath}`
+      if (rpath) {
+        this.apiEndpoint = `reports/${this.path}${rpath}`
+      }
     },
     getApiEndpointRangePath (range) {
-      let rpath = ''
-      if (range && (range.required || range.optional)) {
-        rpath += (this.datetimeStart) ? '/' + this.datetimeStart : '/0000-00-00 00:00:00'
-        rpath += (this.datetimeEnd) ? '/' + this.datetimeEnd : '/9999-12-12 23:59:59'
+      const { datetimeStart, datetimeEnd } = this
+      if (range && datetimeStart && datetimeEnd) {
+        return `/${datetimeStart}/${datetimeEnd}`
       }
-      return rpath
+      return false
     }
   },
   beforeRouteUpdate (to, from, next) {
     // trigger on every page-leave and only within same route '/reports'
-    if (this.getReportByPath(to.params.path).name !== this.getReportByPath(from.params.path).name) {
+    if (
+      this.getReportCategoryByPath(from.params.path).name !== this.getReportCategoryByPath(to.params.path).name
+      ||
+      this.getReportByPath(to.params.path).name !== this.getReportByPath(from.params.path).name
+    ) {
       this.tabIndex = 0
     }
     const report = reportCategories().map(category => category.reports).reduce((l, n) => l.concat(n), []).filter(report => report.tabs.map(tab => tab.path).includes(to.params.path))[0]
-    const range = report.tabs[this.tabIndex].range
-    const rpath = this.getApiEndpointRangePath(range)
-    this.apiEndpoint = `reports/${to.params.path}${rpath}`
+    const { tabs: { [this.tabIndex]: { range = false } = {} } = {} } = report
+    if (range) {
+      const rpath = this.getApiEndpointRangePath(range)
+      if (rpath) {
+        this.apiEndpoint = `reports/${to.params.path}${rpath}`
+      }
+    } else {
+      this.apiEndpoint = `reports/${to.params.path}`
+    }
     next()
   },
   beforeRouteEnter (to, from, next) {
     // triggered only once on page-load to this route '/reports'
     next(vm => {
-      vm.tabIndex = vm.report.tabs.findIndex(tab => tab.path === to.params.path)
+      vm.tabIndex = Math.max(0, vm.report.tabs.findIndex(tab => tab.path === to.params.path))
       const range = vm.report.tabs[vm.tabIndex].range
-      const rpath = vm.getApiEndpointRangePath(range)
-      vm.apiEndpoint = `reports/${to.params.path}${rpath}`
+      if (range) {
+        const rpath = vm.getApiEndpointRangePath(range)
+        if (rpath) {
+          vm.apiEndpoint = `reports/${to.params.path}${rpath}`
+        }
+      } else {
+        vm.apiEndpoint = `reports/${to.params.path}`
+      }
     })
   },
   watch: {
@@ -222,13 +256,17 @@ export default {
       }
     },
     tabIndex (a, b) {
+      a = Math.max(0, a)
       if (a !== b) {
         this.items = []
         /**
          * mandatory `replace`,
          * `push` confuses beforeRouteEnter, beforeRouteUpdate w/ history.go(-1)
          */
-        this.$router.replace(`/reports/standard/chart/${this.report.tabs[a].path}`)
+        const newPath = `/reports/standard/chart/${this.report.tabs[a].path}`
+        if (newPath !== this.$route.path) {
+          this.$router.replace(newPath)
+        }
       }
     }
   },
