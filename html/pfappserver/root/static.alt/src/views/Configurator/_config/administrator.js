e9ad9a85d9f876005ea480c0ac0f6e49caf1a06e
Francis Lachapelle <flachapelle@inverse.ca>
2020-03-23 17:14:30 -0400

    (configurator) Improve AdministratorView

--- html/pfappserver/root/static.alt/src/views/Configurator/_config/administrator.js
+++ html/pfappserver/root/static.alt/src/views/Configurator/_config/administrator.js
@@ -1,15 +1,11 @@
 import i18n from '@/utils/locale'
 import pfFormInput from '@/components/pfFormInput'
 import pfFormPassword from '@/components/pfFormPassword'
-// import {
-//   attributesFromMeta,
-//   validatorsFromMeta
-// } from '@/views/Configuration/_config'
 import {
   required
 } from 'vuelidate/lib/validators'
 
-export const view = (form = {}, meta = {}) => {
+export const view = () => {
   return [
     {
       tab: null,
@@ -21,7 +17,9 @@ export const view = (form = {}, meta = {}) => {
             {
               namespace: 'administrator.pid',
               component: pfFormInput,
-              // attrs: attributesFromMeta(meta, 'pid')
+              attrs: {
+                readonly: true
+              }
             }
           ]
         },
@@ -45,9 +43,6 @@ export const view = (form = {}, meta = {}) => {
 export const validators = () => {
   return {
     administrator: {
-      pid: {
-        [i18n.t('Username must be defined.')]: required
-      },
       password: {
         [i18n.t('Password must be defined.')]: required
       }
