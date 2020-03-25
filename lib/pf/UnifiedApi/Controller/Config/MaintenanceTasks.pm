183f2a38f8f723fe514b1a841e2df0453bb66f3d
James Rouzier <jrouzier@inverse.ca>
2020-03-25 14:04:09 -0400

    Add form for bandwidth_maintenance

--- lib/pf/UnifiedApi/Controller/Config/MaintenanceTasks.pm
+++ lib/pf/UnifiedApi/Controller/Config/MaintenanceTasks.pm
@@ -45,6 +45,7 @@ use pfappserver::Form::Config::Pfmon::password_of_the_day;
 use pfappserver::Form::Config::Pfmon::acct_cleanup;
 use pfappserver::Form::Config::Pfmon::dns_audit_log_cleanup;
 use pfappserver::Form::Config::Pfmon::admin_api_audit_log_cleanup;
+use pfappserver::Form::Config::Pfmon::bandwidth_maintenance;
 
 our %TYPES_TO_FORMS = (
     map { $_ => "pfappserver::Form::Config::Pfmon::$_" } qw(
@@ -71,6 +72,7 @@ our %TYPES_TO_FORMS = (
       password_of_the_day
       acct_cleanup
       admin_api_audit_log_cleanup
+      bandwidth_maintenance
     )
 );
 
