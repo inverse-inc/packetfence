e85ef32a9199d690d199cc29dbf173840e2aa40f
Francis Lachapelle <flachapelle@inverse.ca>
2020-03-27 15:00:08 -0400

    (configurator) Fix reference to database form in last step

--- html/pfappserver/root/static.alt/src/views/Configurator/_components/PasswordsView.vue
+++ html/pfappserver/root/static.alt/src/views/Configurator/_components/PasswordsView.vue
@@ -1,14 +1,14 @@
 <template>
-    <b-card no-body class="mt-3" v-if="db.root_pass || administrator.password">
+    <b-card no-body class="mt-3" v-if="database.root_pass || administrator.password">
         <b-card-header>
         <h4 class="d-inline mb-0" v-t="'Passwords'"></h4>
         <small class="text-muted ml-2" v-t="'Make sure to keep them in a secure place'"></small>
         </b-card-header>
         <div class="card-body">
-        <template v-if="db.root_pass">
+        <template v-if="database.root_pass">
             <pf-form-row :column-label="$t('Database Root Account')" label-class="col-form-label-lg offset-sm-3"></pf-form-row>
             <pf-form-row :column-label="$t('Username')">root</pf-form-row>
-            <pf-form-row :column-label="$t('Password')">{{ db.root_pass }}</pf-form-row>
+            <pf-form-row :column-label="$t('Password')">{{ database.root_pass }}</pf-form-row>
         </template>
         <template v-if="administrator.password">
             <pf-form-row :column-label="$t('Administrator Account')" label-class="col-form-label-lg"></pf-form-row>
@@ -38,8 +38,8 @@ export default {
     administrator () {
       return this.$store.getters[`${this.storeName}/$formNS`]('administrator')
     },
-    db () {
-      return this.$store.getters[`${this.storeName}/$formNS`]('db')
+    database () {
+      return this.$store.getters[`${this.storeName}/$formNS`]('database')
     }
   }
 }
