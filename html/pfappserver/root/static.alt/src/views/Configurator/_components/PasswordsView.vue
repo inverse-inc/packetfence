1e7865a217dc8ddcfcba5054466f7da7e4f823dd
Francis Lachapelle <flachapelle@inverse.ca>
2020-03-30 16:33:51 -0400

    (configurator) Add buttons to copy passwords to clipboard on last step

--- html/pfappserver/root/static.alt/src/views/Configurator/_components/PasswordsView.vue
+++ html/pfappserver/root/static.alt/src/views/Configurator/_components/PasswordsView.vue
@@ -8,12 +8,18 @@
         <template v-if="database.root_pass">
             <pf-form-row :column-label="$t('Database Root Account')" label-class="col-form-label-lg offset-sm-3"></pf-form-row>
             <pf-form-row :column-label="$t('Username')">root</pf-form-row>
-            <pf-form-row :column-label="$t('Password')">{{ database.root_pass }}</pf-form-row>
+            <pf-form-row :column-label="$t('Password')">
+              {{ database.root_pass }}
+              <b-button size="sm" variant="outline-primary" class="ml-2 text-nowrap" @click.stop.prevent="clipboard(database.root_pass)">{{ $t('Copy to Clipboard') }}</b-button>
+            </pf-form-row>
         </template>
         <template v-if="administrator.password">
             <pf-form-row :column-label="$t('Administrator Account')" label-class="col-form-label-lg"></pf-form-row>
             <pf-form-row :column-label="$t('Username')">{{ administrator.pid }}</pf-form-row>
-            <pf-form-row :column-label="$t('Password')">{{ administrator.password }}</pf-form-row>
+            <pf-form-row :column-label="$t('Password')">
+              {{ administrator.password }}
+              <b-button size="sm" variant="outline-primary" class="ml-2 text-nowrap" @click.stop.prevent="clipboard(administrator.password)">{{ $t('Copy to Clipboard') }}</b-button>
+            </pf-form-row>
         </template>
         </div>
     </b-card>
@@ -40,6 +46,19 @@ export default {
     },
     database () {
       return this.$store.getters[`${this.storeName}/$formNS`]('database')
+    }
+  },
+  methods: {
+    clipboard (text) {
+      try {
+        navigator.clipboard.writeText(text).then(() => {
+          this.$store.dispatch('notification/info', { message: this.$i18n.t('Password copied to clipboard') })
+        }).catch(err => {
+          this.$store.dispatch('notification/danger', { message: this.$i18n.t('Could not copy password to clipboard.') })
+        })
+      } catch (e) {
+        this.$store.dispatch('notification/danger', { message: this.$i18n.t('Clipboard not supported.') })
+      }
     }
   }
 }
