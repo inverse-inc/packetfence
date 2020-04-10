425375a418c5af244b7de5f3ad2d3a3b39d965ee
Darren Satkunas <darren@satkunas.com>
2020-04-10 17:08:15 -0400

    (web admin) replace template

--- html/pfappserver/root/static.alt/src/views/Reset/index.vue
+++ html/pfappserver/root/static.alt/src/views/Reset/index.vue
@@ -1,4 +1,16 @@
 <template>
+  <div class="container-fluid position-fixed mt-3" style="top:50%;">
+    <b-row class="justify-content-md-center text-secondary">
+      <b-col cols="12" md="auto">
+        <b-media no-body>
+          <icon name="circle-notch" scale="2" spin v-slot:aside></icon>
+          <div class="mx-2">
+            <h4 v-t="'Resetting View...'"></h4>
+          </div>
+        </b-media>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
