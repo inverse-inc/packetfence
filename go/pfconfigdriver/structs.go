d35df83847b0e2d014e65ce52d93aa5844d2daf6
Durand Fabrice <oeufdure@gmail.com>
2020-04-20 07:32:48 -0400

    Typo

--- go/pfconfigdriver/structs.go
+++ go/pfconfigdriver/structs.go
@@ -596,7 +596,7 @@ type Certificate struct {
 	CertType     string `json:"type"`
 	Key          string `json:"key"`
 	Ca           string `json:"ca"`
-	Intermediate string `json:"Intermediate"`
+	Intermediate string `json:"intermediate"`
 }
 
 type OCSP struct {
