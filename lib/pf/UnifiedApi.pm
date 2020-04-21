04e1e0d27a6a90d5750bc7f5a5cf048437c1116e
James Rouzier <jrouzier@inverse.ca>
2020-04-21 08:01:21 -0400

    New endpoints for /api/v1/config/radiusd/eap_profile(s)

--- lib/pf/UnifiedApi.pm
+++ lib/pf/UnifiedApi.pm
@@ -1150,6 +1150,14 @@ sub setup_api_v1_config_radiusd_routes {
         "api.v1.Config.Radiusd.OCSPProfiles"
     );
 
+      $self->setup_api_v1_std_config_routes(
+        $radiusd_route,
+        "Config::Radiusd::EAPProfiles",
+        "/eap_profiles",
+        "/eap_profile/#ocsp_profile_id",
+        "api.v1.Config.Radiusd.EAPProfiles"
+    );
+
     return ($collection_route, $resource_route);
 }
 
