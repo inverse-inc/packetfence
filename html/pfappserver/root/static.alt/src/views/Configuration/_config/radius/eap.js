c5837a152e718f4df3460b05e078519cacfda06e
Darren Satkunas <darren@satkunas.com>
2020-04-21 13:46:35 -0400

    (web admin) expand eap form

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/radius/eap.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/radius/eap.js
@@ -1,5 +1,7 @@
 import i18n from '@/utils/locale'
+import pfFormChosen from '@/components/pfFormChosen'
 import pfFormInput from '@/components/pfFormInput'
+import pfFormRangeToggle from '@/components/pfFormRangeToggle'
 import { pfSearchConditionType as conditionType } from '@/globals/pfSearch'
 import {
   attributesFromMeta,
@@ -10,6 +12,36 @@ export const columns = [
   {
     key: 'id',
     label: 'Identifier', // i18n defer
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'default_eap_type',
+    label: 'Default EAP', // i18n defer
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'tls_tlsprofile',
+    label: 'TLS Profile', // i18n defer
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'ttls_tlsprofile',
+    label: 'TTLS Profile', // i18n defer
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'peap_tlsprofile',
+    label: 'PEAP Profile', // i18n defer
+    sortable: true,
+    visible: true
+  },
+  {
+    key: 'fast_config',
+    label: 'Fast Profile', // i18n defer
     sortable: true,
     visible: true
   },
@@ -78,6 +110,110 @@ export const view = (form = {}, meta = {}) => {
               namespace: 'id',
               component: pfFormInput,
               attrs: attributesFromMeta(meta, 'id')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Default EAP Type'),
+          cols: [
+            {
+              namespace: 'default_eap_type',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'default_eap_type')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Expires'),
+          cols: [
+            {
+              namespace: 'expire_time',
+              component: pfFormInput,
+              attrs: attributesFromMeta(meta, 'expire_time')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Ignore Unknown EAP Types'),
+          cols: [
+            {
+              namespace: 'ignore_unknown_eap_types',
+              component: pfFormRangeToggle,
+              attrs: {
+                values: { checked: 'yes', unchecked: 'no' }
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('Cisco Accounting Username Bug'),
+          cols: [
+            {
+              namespace: 'cisco_accounting_username_bug',
+              component: pfFormRangeToggle,
+              attrs: {
+                values: { checked: 'yes', unchecked: 'no' }
+              }
+            }
+          ]
+        },
+        {
+          label: i18n.t('Max Sessions'),
+          cols: [
+            {
+              namespace: 'max_sessions',
+              component: pfFormInput,
+              attrs: attributesFromMeta(meta, 'max_sessions')
+            }
+          ]
+        },
+        {
+          label: i18n.t('EAP Authentication Types'),
+          cols: [
+            {
+              namespace: 'eap_authentication_types',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'eap_authentication_types')
+            }
+          ]
+        },
+        {
+          label: i18n.t('TLS Profile'),
+          cols: [
+            {
+              namespace: 'tls_tlsprofile',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'tls_tlsprofile')
+            }
+          ]
+        },
+        {
+          label: i18n.t('TTLS Profile'),
+          cols: [
+            {
+              namespace: 'ttls_tlsprofile',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'ttls_tlsprofile')
+            }
+          ]
+        },
+        {
+          label: i18n.t('PEAP Profile'),
+          cols: [
+            {
+              namespace: 'peap_tlsprofile',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'peap_tlsprofile')
+            }
+          ]
+        },
+        {
+          label: i18n.t('Fast Profile'),
+          cols: [
+            {
+              namespace: 'fast_config',
+              component: pfFormChosen,
+              attrs: attributesFromMeta(meta, 'fast_config')
             }
           ]
         }
