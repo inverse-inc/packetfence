ef81df373fec022642e89b408acf6fba1bfaddd1
James Rouzier <jrouzier@inverse.ca>
2020-04-29 13:30:20 -0400

    Support maint.Job interface

--- go/maint/pfmonjob.go
+++ go/maint/pfmonjob.go
@@ -6,14 +6,19 @@ import (
 
 type PfmonJob struct {
     Name string
+    TimeSpec string
 }
 
-func NewPfmonJob(name string) *PfmonJob {
-    return &PfmonJob{Name:name}
+func NewPfmonJob(name string, timeSpec string) *PfmonJob {
+    return &PfmonJob{Name:name, TimeSpec: timeSpec}
 }
 
 func (j *PfmonJob) Run() {
     cmd := exec.Command("/usr/local/pf/bin/pfcmd", "pfmon", j.Name)
     err := cmd.Run()
     _ = err
+}
+
+func (j *PfmonJob) Spec() string {
+    return j.TimeSpec
 }
