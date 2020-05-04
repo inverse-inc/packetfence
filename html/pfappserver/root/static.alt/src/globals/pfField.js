9d4d4b7df2b30813a84bcd14557fe9602d74ba2e
Darren Satkunas <darren@satkunas.com>
2020-05-04 10:34:02 -0400

    (web admin) fix typo

--- html/pfappserver/root/static.alt/src/globals/pfField.js
+++ html/pfappserver/root/static.alt/src/globals/pfField.js
@@ -122,7 +122,7 @@ export const pfFieldTypeOperators = {
     { text: 'is member of', value: 'is member of' }
   ],
   [pfFieldType.LDAPFILTER]: [
-    { text: 'match_fitler', value: 'match_filter' }
+    { text: 'match_filter', value: 'match_filter' }
   ],
   [pfFieldType.SUBSTRING]: [
     { text: 'starts', value: 'starts' },
