1c1b4c7ec76e533cdc5dafbd09da6f18f362c2c2
Darren Satkunas <darren@satkunas.com>
2020-05-07 15:10:00 -0400

    (web admin) enable id, fixes #5395

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/filterEngine.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/filterEngine.js
@@ -134,12 +134,7 @@ export const viewFields = {
         {
           namespace: 'id',
           component: pfFormInput,
-          attrs: {
-            ...attributesFromMeta(meta, 'id'),
-            ...{
-              disabled: (!isNew && !isClone)
-            }
-          }
+          attrs: attributesFromMeta(meta, 'id')
         }
       ]
     }
@@ -493,7 +488,7 @@ export const validators = (form = {}, meta = {}) => {
     run_actions
   } = form
   // meta indicates which fields are preset
-  return Object.keys(meta).filter(field => {
+  return [ 'id', ...Object.keys(meta) ].filter(field => {
     switch (field) {
       case 'actions':
         return run_actions === 'enabled'
