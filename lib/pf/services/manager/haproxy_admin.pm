662a0cf1310e5db5e9e4fddb73155dda0ffa26f7
Durand Fabrice <oeufdure@gmail.com>
2020-05-07 15:32:25 -0400

    Use all server mgmt ip for haproxy-admin

--- lib/pf/services/manager/haproxy_admin.pm
+++ lib/pf/services/manager/haproxy_admin.pm
@@ -42,6 +42,7 @@ has '+haproxy_config_template' => (default => sub { "$conf_dir/haproxy-admin.con
 my $host_id = $pf::config::cluster::host_id;
 
 tie our %NetworkConfig, 'pfconfig::cached_hash', "resource::network_config($host_id)";
+tie @config_cluster_servers, 'pfconfig::cached_array', "resource::all_cluster_servers";
 
 sub generateConfig {
     my ($self,$quick) = @_;
@@ -65,7 +66,7 @@ sub generateConfig {
         my $mgmt_cfg = $Config{"interface $mgmt_int"};
         $tags{'mgmt_active_ip'} = pf::cluster::management_cluster_ip() || $mgmt_cfg->{'vip'} || $mgmt_cfg->{'ip'};
         $mgmt_cluster_ip = pf::cluster::cluster_ip($mgmt_int) || $mgmt_cfg->{'vip'} || $mgmt_cfg->{'ip'};
-        my @mgmt_backend_ip = values %{pf::cluster::members_ips($mgmt_int)};
+        my @mgmt_backend_ip = map { $_->{'management_ip'} } @config_cluster_servers;
         push @mgmt_backend_ip, '127.0.0.1' if !@mgmt_backend_ip;
 
         $tags{'management_ip'}
