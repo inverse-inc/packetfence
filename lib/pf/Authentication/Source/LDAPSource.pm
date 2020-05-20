d7400099ef0a14cb6297a29ce4dabfc858435713
James Rouzier <jrouzier@inverse.ca>
2020-05-20 16:04:56 -0400

    Return attribute from entry

--- lib/pf/Authentication/Source/LDAPSource.pm
+++ lib/pf/Authentication/Source/LDAPSource.pm
@@ -354,8 +354,11 @@ sub _match_in_subclass {
         $self->_cached_connection($cached_connection);
     }
     my ( $connection, $LDAPServer, $LDAPServerPort ) = @$cached_connection;
-
     my @attributes = map { $_->{'attribute'} } @{$own_conditions};
+    if (my $action = firstval { $_->type eq $Actions::SET_ROLE_FROM_SOURCE } @{$self->{actions} // []}) {
+        push @attributes, $action->value;
+    }
+
     my $result = do {
         my $timer = pf::StatsD::Timer->new({ 'stat' => "${timer_stat_prefix}.search",  level => 6});
         $connection->search(
@@ -443,7 +446,7 @@ sub _match_in_subclass {
             $logger->trace("[$self->{'id'} $rule->{'id'}] Found a match ($dn)");
             if (any {$_->type eq $Actions::SET_ROLE } @{$rule->{actions} // []} ) {
                 push @{ $matching_conditions }, @{ $own_conditions };
-                return ($params->{'username'} || $params->{'email'}, $Actions::SET_ROLE_ON_NOT_FOUND);
+                return (($params->{'username'} || $params->{'email'}) ? $entry : undef, $Actions::SET_ROLE_ON_NOT_FOUND);
             } else {
                 $logger->trace("[$self->{'id'} $rule->{'id'}] match ($dn) but no set role action, continue");
             }
@@ -654,7 +657,11 @@ sub _makefilter {
 }
 
 sub lookupRole {
-    my ($self, $role_info, $params, $extra) = @_;
+    my ($self, $role_info, $params, $extra, $entry) = @_;
+    if (my $action = firstval { $_->type eq $Actions::SET_ROLE_FROM_SOURCE } @{$self->{actions} // []}) {
+        return scalar $entry->get_value($action->value);
+    }
+
     return undef;
 }
 
