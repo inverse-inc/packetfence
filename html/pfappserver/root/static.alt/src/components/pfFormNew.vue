5cda129f65a0f9ad5af3cf201732efd99c8bc298
Darren Satkunas <darren@satkunas.com>
2020-05-26 10:38:44 -0400

    (web admin) new component

---
+++ html/pfappserver/root/static.alt/src/components/pfFormNew.vue
@@ -0,0 +1,80 @@
+<template>
+  <b-form-group :label-cols="(columnLabel) ? labelCols : 0" :label="columnLabel" :state="inputState"
+    class="pf-form-input" :class="{ 'mb-0': !columnLabel }">
+    <template v-slot:invalid-feedback>
+      {{ inputInvalidFeedback }}
+    </template>
+    <b-input-group>
+
+...
+
+    </b-input-group>
+    <b-form-text v-show="text" v-html="text"></b-form-text>
+  </b-form-group>
+</template>
+
+<script>
+import pfMixinForm from '@/components/pfMixinForm'
+
+export default {
+  name: 'pf-form-boolean',
+  mixins: [
+    pfMixinForm
+  ],
+  data () {
+    return {
+    }
+  },
+  props: {
+    value: {
+      type: Object,
+      default: null
+    },
+    columnLabel: {
+      type: String
+    },
+    labelCols: {
+      type: Number,
+      default: 3
+    },
+    text: {
+      type: String,
+      default: null
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    inputValue: {
+      get () {
+        if (this.formStoreName) {
+          return this.formStoreValue // use FormStore
+        } else {
+          return this.value // use native (v-model)
+        }
+      },
+      set (newValue = null) {
+        if (this.formStoreName) {
+          this.formStoreValue = newValue // use FormStore
+        } else {
+          this.$emit('input', newValue) // use native (v-model)
+        }
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  .pf-form-new {
+
+  }
+</style>
