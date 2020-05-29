fc904b0c6bbe84db93175bf8c037d7f54c9c991c
Darren Satkunas <darren@satkunas.com>
2020-05-29 13:35:11 -0400

    (web admin) deprecate radius filter answers allow_custom

--- html/pfappserver/root/static.alt/src/views/Configuration/_config/filterEngine.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_config/filterEngine.js
@@ -194,7 +194,11 @@ export const viewFields = {
               attrs: {
                 prefixLabel: i18n.t('Select a prefix'),
                 typeLabel: i18n.t('Select a type'),
+                /*
+                 * fallback to default behavior
+                 * TODO - reimplement when allow_custom is removed, and allowed_lookup is updated.
                 typeAttrs: attributesFromMeta(meta, 'answers.type'),
+                */
                 valueLabel: i18n.t('Select a value'),
                 prefixes,
                 fields: answerFieldsFromMeta(meta)
