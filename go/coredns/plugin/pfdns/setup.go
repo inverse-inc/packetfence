42e5c4aeed0911936e87227eb066bd6b44802d96
Durand Fabrice <oeufdure@gmail.com>
2020-06-05 09:08:09 -0400

    Fix pfdns crash

--- go/coredns/plugin/pfdns/setup.go
+++ go/coredns/plugin/pfdns/setup.go
@@ -9,12 +9,14 @@ import (
 	"github.com/inverse-inc/packetfence/go/coredns/core/dnsserver"
 	"github.com/inverse-inc/packetfence/go/coredns/plugin"
 	"github.com/inverse-inc/packetfence/go/pfconfigdriver"
+	"github.com/inverse-inc/packetfence/go/timedlock"
 	"github.com/inverse-inc/packetfence/go/unifiedapiclient"
 	"github.com/mholt/caddy"
 	cache "github.com/patrickmn/go-cache"
 )
 
 func init() {
+	GlobalTransactionLock = timedlock.NewRWLock()
 	caddy.RegisterPlugin("pfdns", caddy.Plugin{
 		ServerType: "dns",
 		Action:     setuppfdns,
