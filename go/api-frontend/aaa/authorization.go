5ed05427b5279a0fb3904c2cf6629ed4e0155234
James Rouzier <jrouzier@inverse.ca>
2020-06-05 10:15:46 -0400

    isAuthorizedTenantId is true when not in multitenant mode
    Related #5533

--- go/api-frontend/aaa/authorization.go
+++ go/api-frontend/aaa/authorization.go
@@ -272,6 +272,8 @@ func (tam *TokenAuthorizationMiddleware) isAuthorizedTenantId(ctx context.Contex
 	// Token doesn't have access to any tenant
 	if tokenTenantId == AccessNoTenants {
 		return false, errors.New("Token is prohibited from accessing data from any tenant")
+    } else if !multipleTenants {
+        return true, nil
 	} else if tokenTenantId == AccessAllTenants {
 		return true, nil
 	} else if requestTenantId == tokenTenantId {
