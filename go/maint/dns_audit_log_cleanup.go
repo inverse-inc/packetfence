f8d0803e76e1028d633eaac104b54d3c1687dc71
James Rouzier <jrouzier@inverse.ca>
2020-06-08 07:57:52 -0400

    WIP

---
+++ go/maint/dns_audit_log_cleanup.go
@@ -0,0 +1,34 @@
+package maint
+
+import (
+	"time"
+)
+
+type DNSAuditLogCleanup struct {
+	Task
+	Window  int
+	Batch   int
+	Timeout time.Duration
+	StmtSetup
+}
+
+func NewDNSAuditLogCleanup(config map[string]interface{}) JobSetupConfig {
+	return &DNSAuditLogCleanup{
+		Task: Task{
+			Type:         config["type"].(string),
+			Status:       config["status"].(string),
+			Description:  config["description"].(string),
+			ScheduleSpec: config["schedule"].(string),
+		},
+		Batch:   int(config["batch"].(float64)),
+		Timeout: time.Duration((config["timeout"].(float64))) * time.Second,
+		Window:  int(config["window"].(float64)),
+	}
+}
+
+func (c *DNSAuditLogCleanup) Run() {
+	stmt := c.Stmt(`DELETE FROM dns_audit_log_cleanup WHERE created_at < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`)
+	if stmt != nil {
+		BatchRemove(stmt, c.Timeout, time.Now(), c.Window, c.Batch)
+	}
+}
---
+++ go/maint/locationlog_cleanup.go
@@ -0,0 +1,34 @@
+package maint
+
+import (
+	"time"
+)
+
+type LocationLogCleanup struct {
+	Task
+	Window  int
+	Batch   int
+	Timeout time.Duration
+	StmtSetup
+}
+
+func NewLocationLogCleanup(config map[string]interface{}) JobSetupConfig {
+	return &LocationLogCleanup{
+		Task: Task{
+			Type:         config["type"].(string),
+			Status:       config["status"].(string),
+			Description:  config["description"].(string),
+			ScheduleSpec: config["schedule"].(string),
+		},
+		Batch:   int(config["batch"].(float64)),
+		Timeout: time.Duration((config["timeout"].(float64))) * time.Second,
+		Window:  int(config["window"].(float64)),
+	}
+}
+
+func (c *LocationLogCleanup) Run() {
+	stmt := c.Stmt(`DELETE FROM locationlog_history WHERE end_time < DATE_SUB(?, INTERVAL ? SECOND) AND != '0000-00-00 00:00:00' LIMIT ?`)
+	if stmt != nil {
+		BatchRemove(stmt, c.Timeout, time.Now(), c.Window, c.Batch)
+	}
+}
