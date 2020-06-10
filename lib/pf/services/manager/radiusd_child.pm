5950ed477c9fe4251b51df83b38b49b45ef8635d
nqb <nqb+git@azyx.fr>
2020-06-10 07:31:04 +0200

    fix coding style

--- lib/pf/services/manager/radiusd_child.pm
+++ lib/pf/services/manager/radiusd_child.pm
@@ -1218,7 +1218,7 @@ sub generate_eap_choice {
             my $choice = $key;
             $choice = $pf::config::ConfigRealm{$key}->{'regex'} if (defined $pf::config::ConfigRealm{$key}->{'regex'} && $pf::config::ConfigRealm{$key}->{'regex'} ne '');
             my $eap = ( defined($pf::config::ConfigRealm{$key}->{'eap'}) && $pf::config::ConfigRealm{$key}->{'eap'} ne '') ? $pf::config::ConfigRealm{$key}->{'eap'} : 'eap';
-	    $$authorize_eap_choice .= <<"EOT";
+            $$authorize_eap_choice .= <<"EOT";
             $if (Realm =~ /$choice/) {
                 $eap {
                     ok = return
