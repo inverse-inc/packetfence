f6e69ee07ab3bcd6acb0b7a8ce1f0b50e6614a42
Darren Satkunas <darren@satkunas.com>
2020-06-10 10:26:04 -0400

    (web admin) increase limit of switch groups in New Switch button, fixes #5563
    Conflicts:
    	html/pfappserver/root/static.alt/src/views/Configuration/_components/SwitchesList.vue

--- html/pfappserver/root/static.alt/src/views/Configuration/_store/switchGroups.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/_store/switchGroups.js
@@ -27,7 +27,8 @@ const actions = {
   all: () => {
     const params = {
       sort: 'id',
-      fields: ['id', 'description'].join(',')
+      fields: ['id', 'description'].join(','),
+      limit: 1000
     }
     return api.switchGroups(params).then(response => {
       return response.items
