c6f0760b99489472643eca6a129af51b94e37413
James Rouzier <jrouzier@inverse.ca>
2020-06-18 10:23:32 -0400

    A new multi stmt window job

--- go/maint/config.go
+++ go/maint/config.go
@@ -17,8 +17,16 @@ var builders = map[string]func(map[string]interface{}) JobSetupConfig{
 	"admin_api_audit_log_cleanup": MakeWindowSqlJobSetupConfig(`DELETE FROM admin_api_audit_log WHERE created_at < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`),
 	"auth_log_cleanup":            MakeWindowSqlJobSetupConfig(`DELETE FROM auth_log WHERE attempted_at < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`),
 	"dns_audit_log":               MakeWindowSqlJobSetupConfig(`DELETE FROM dns_audit_log_cleanup WHERE created_at < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`),
-	"locationlog_cleanup":         MakeWindowSqlJobSetupConfig(`DELETE FROM locationlog_history WHERE end_time < DATE_SUB(?, INTERVAL ? SECOND) AND end_time != '0000-00-00 00:00:00' LIMIT ?`),
 	"radius_audit_log_cleanup":    MakeWindowSqlJobSetupConfig(`DELETE FROM radius_audit_log WHERE created_at < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`),
+	"locationlog_cleanup": MakeMultiWindowSqlJobSetupConfig(
+		`DELETE FROM locationlog_history WHERE end_time < DATE_SUB(?, INTERVAL ? SECOND) AND end_time != '0000-00-00 00:00:00' LIMIT ?`,
+		`DELETE FROM locationlog WHERE end_time < DATE_SUB(?, INTERVAL ? SECOND) AND end_time != '0000-00-00 00:00:00' LIMIT ?`,
+	),
+	"acct_cleanup": MakeMultiWindowSqlJobSetupConfig(
+		`UPDATE radacct SET acctstoptime = NOW() WHERE acctstarttime < DATE_SUB(?, INTERVAL ? SECOND) AND acctstoptime IS NULL LIMIT ?`,
+		`DELETE FROM radacct WHERE acctstarttime < DATE_SUB(?, INTERVAL ? SECOND) AND acctstoptime IS NOT NULL LIMIT ?`,
+		`DELETE FROM radacct_log WHERE timestamp < DATE_SUB(?, INTERVAL ? SECOND) LIMIT ?`,
+	),
 }
 
 func GetConfiguredJobs() []JobSetupConfig {
--- go/maint/window_batch_task.go
+++ go/maint/window_batch_task.go
@@ -40,3 +40,45 @@ func (c *WindowSqlCleanup) Run() {
 		BatchStmt(stmt, c.Timeout, time.Now(), c.Window, c.Batch)
 	}
 }
+
+type MultiWindowSqlCleanup struct {
+	Task
+	Window     int
+	Batch      int
+	Timeout    time.Duration
+	Sqls       []string
+	StmtSetups []StmtSetup
+}
+
+func NewMultiWindowSqlCleanup(config map[string]interface{}, sqls ...string) JobSetupConfig {
+	sqlStrings := make([]string, len(sqls))
+	copy(sqlStrings, sqls)
+	return &MultiWindowSqlCleanup{
+		Task: Task{
+			Type:         config["type"].(string),
+			Status:       config["status"].(string),
+			Description:  config["description"].(string),
+			ScheduleSpec: config["schedule"].(string),
+		},
+		Batch:      int(config["batch"].(float64)),
+		Timeout:    time.Duration((config["timeout"].(float64))) * time.Second,
+		Window:     int(config["window"].(float64)),
+		Sqls:       sqlStrings,
+		StmtSetups: make([]StmtSetup, len(sqls)),
+	}
+}
+
+func (c *MultiWindowSqlCleanup) Run() {
+	now := time.Now()
+	for i, sql := range c.Sqls {
+		if stmt := c.StmtSetups[i].Stmt(sql); stmt != nil {
+			BatchStmt(stmt, c.Timeout, now, c.Window, c.Batch)
+		}
+	}
+}
+
+func MakeMultiWindowSqlJobSetupConfig(sqls ...string) func(config map[string]interface{}) JobSetupConfig {
+	return func(config map[string]interface{}) JobSetupConfig {
+		return NewMultiWindowSqlCleanup(config, sqls...)
+	}
+}
