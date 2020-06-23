a84580c1800fcdb9c8607b59efa646e3073d469f
James Rouzier <jrouzier@inverse.ca>
2020-06-23 07:11:30 -0400

    Batching using count

--- go/maint/batch.go
+++ go/maint/batch.go
@@ -1,11 +1,12 @@
 package maint
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
 
-func BatchStmt(stmt *sql.Stmt, time_limit time.Duration, args ...interface{}) int64 {
+func BatchStmt(ctx context.Context, stmt *sql.Stmt, time_limit time.Duration, args ...interface{}) int64 {
 	start := time.Now()
 	rows_affected := int64(0)
 	for {
@@ -16,6 +17,7 @@ func BatchStmt(stmt *sql.Stmt, time_limit time.Duration, args ...interface{}) in
 
 		rows, err := results.RowsAffected()
 		if err != nil {
+			logError(ctx, "Database error: "+err.Error())
 			break
 		}
 
@@ -24,6 +26,30 @@ func BatchStmt(stmt *sql.Stmt, time_limit time.Duration, args ...interface{}) in
 		}
 
 		rows_affected += rows
+		if time.Now().Sub(start) > time_limit {
+			break
+		}
+	}
+
+	return rows_affected
+}
+
+func BatchStmtQueryWithCount(ctx context.Context, stmt *sql.Stmt, time_limit time.Duration, args ...interface{}) int64 {
+	start := time.Now()
+	rows_affected := int64(0)
+	for {
+		var count int64
+		err := stmt.QueryRow(args...).Scan(&count)
+		if err != nil {
+			logError(ctx, "Database error: "+err.Error())
+			break
+		}
+
+		if count <= 0 {
+			break
+		}
+
+		rows_affected += count
 		if time.Now().Sub(start) > time_limit {
 			break
 		}
