c25be7759581ce2ee53ea75170e3d0ef3477f8af
James Rouzier <jrouzier@inverse.ca>
2020-06-30 13:45:50 -0400

    Return cluster status

--- go/maint/cluster.go
+++ go/maint/cluster.go
@@ -24,31 +24,40 @@ func (s *Server) IsDisabled() bool {
 }
 
 func CallCluster(ctx context.Context, method string, args interface{}) {
-	clientApi := jsonrpc2.NewClientFromConfig(context.Background())
-	clientApi.Proto = "https"
-	for _, member := range enabledServers(ctx) {
-		clientApi.Host = member.ManagementIp
-		if _, err := clientApi.Call(ctx, method, args, 1); err != nil {
-			logError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
+	servers, cluster_mode := enabledServers(ctx)
+	if cluster_mode {
+		clientApi := jsonrpc2.NewClientFromConfig(context.Background())
+		clientApi.Proto = "https"
+		for _, member := range servers {
+			clientApi.Host = member.ManagementIp
+			if _, err := clientApi.Call(ctx, method, args, 1); err != nil {
+				logError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
+			}
 		}
 	}
 }
 
 func NotifyCluster(ctx context.Context, method string, args interface{}) {
-	clientApi := jsonrpc2.NewClientFromConfig(context.Background())
-	clientApi.Proto = "https"
-	for _, member := range enabledServers(ctx) {
-		clientApi.Host = member.ManagementIp
-		if _, err := clientApi.Notify(ctx, method, args, 1); err != nil {
-			logError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
+	servers, cluster_mode := enabledServers(ctx)
+	if cluster_mode {
+		clientApi := jsonrpc2.NewClientFromConfig(context.Background())
+		clientApi.Proto = "https"
+		for _, member := range servers {
+			clientApi.Host = member.ManagementIp
+			if err := clientApi.Notify(ctx, method, args, 1); err != nil {
+				logError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
+			}
 		}
 	}
 }
 
-func enabledServers(ctx context.Context) []Server {
+func enabledServers(ctx context.Context) ([]Server, bool) {
 	servers := []Server{}
 	cluster_servers := pfconfigdriver.AllClusterServers{}
 	pfconfigdriver.FetchDecodeSocketCache(ctx, &cluster_servers)
+	if len(servers) == 0 {
+		return nil, false
+	}
 	for _, s := range cluster_servers.Element {
 		server := Server{Host: s.Host, ManagementIp: s.ManagementIp}
 		if !server.IsDisabled() {
@@ -56,5 +65,5 @@ func enabledServers(ctx context.Context) []Server {
 		}
 	}
 
-	return servers
+	return servers, true
 }
