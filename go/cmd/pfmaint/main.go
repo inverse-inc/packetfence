68b8489121893cebb15591688394d8222752b3ef
James Rouzier <jrouzier@inverse.ca>
2020-07-15 14:14:48 -0400

    Allow arguements to be overriden.

--- go/cmd/pfmaint/main.go
+++ go/cmd/pfmaint/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/robfig/cron/v3"
 	"os"
 	"sync"
@@ -10,6 +11,7 @@ import (
 	"github.com/inverse-inc/packetfence/go/maint"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 )
 
@@ -27,15 +29,70 @@ func wrapJob(logger log.PfLogger, j maint.JobSetupConfig) cron.Job {
 	})
 }
 
+func mergeArgs(config, args map[string]interface{}) map[string]interface{} {
+	newArgs := make(map[string]interface{})
+	for k, v := range config {
+		newArgs[k] = v
+	}
+
+	for k, v := range args {
+		newArgs[k] = v
+	}
+
+	return newArgs
+}
+
+func runJobNow(name string, additionalArgs map[string]interface{}) int {
+	jobsConfig := maint.GetMaintenanceConfig()
+	if config, found := jobsConfig[name]; found {
+		job := maint.GetJob(name, mergeArgs(config.(map[string]interface{}), additionalArgs))
+		if job != nil {
+			job.Run()
+			return 0
+		}
+
+		fmt.Printf("Error creating job '%s'\n", name)
+	} else {
+		fmt.Printf("'%s' is not a valid job task\n", name)
+	}
+
+	return 1
+}
+
+func makeArgs(args []string) (map[string]interface{}, error) {
+	config := make(map[string]interface{})
+	for _, arg := range args {
+		pair := strings.SplitN(arg, "=", 2)
+		if len(pair) != 2 {
+			return nil, fmt.Errorf("'%s' is incorrectly formatted\n", arg)
+		}
+
+		config[pair[0]] = pair[1]
+	}
+
+	return config, nil
+}
+
 func main() {
 	log.SetProcessName("pfmaint")
+	if len(os.Args) > 1 {
+		jobName := os.Args[1]
+		if additionalArgs, err := makeArgs(os.Args[2:]); err != nil {
+			fmt.Printf("%s\n", err.Error())
+			os.Exit(1)
+		} else {
+			os.Exit(runJobNow(jobName, additionalArgs))
+		}
+	}
+
 	ctx := context.Background()
 	logger := log.LoggerWContext(ctx)
 	c := cron.New(cron.WithSeconds())
-	for _, job := range maint.GetConfiguredJobs() {
+	for _, job := range maint.GetConfiguredJobs(maint.GetMaintenanceConfig()) {
 		id := c.Schedule(job.Schedule(), wrapJob(logger, job))
 		logger.Info("Job id " + strconv.FormatInt(int64(id), 10))
 	}
+
 	w := sync.WaitGroup{}
 	w.Add(1)
 	c.Start()
@@ -45,6 +102,7 @@ func main() {
 		<-ch
 		w.Done()
 	}()
+
 	w.Wait()
 	doneCtx := c.Stop()
 	<-doneCtx.Done()
