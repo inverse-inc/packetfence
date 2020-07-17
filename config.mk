ec6bdd27ca14c8f918a47117367183cf85506b66
James Rouzier <jrouzier@inverse.ca>
2020-07-17 13:32:51 -0400

    Does not exists yet

--- config.mk
+++ config.mk
@@ -14,7 +14,7 @@ SBINDIR = $(PF_PREFIX)/sbin
 # Golang
 #
 GOVERSION = go1.13.1
-GOBINARIES = pfhttpd pfdhcp pfdns pfstats pfdetect galera-autofix pfacct pfcertmanager pfmaint
+GOBINARIES = pfhttpd pfdhcp pfdns pfstats pfdetect galera-autofix pfacct pfcertmanager
 
 # SRC HTML dirs
 SRC_HTMLDIR = html
