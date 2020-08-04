942e7642ec6e17ff675b15b3fad535d952b056fe
James Rouzier <jrouzier@inverse.ca>
2020-08-04 16:23:53 -0400

    Add check for the Database

--- lib/pf/UnifiedApi/Controller/Config/Roles.pm
+++ lib/pf/UnifiedApi/Controller/Config/Roles.pm
@@ -16,7 +16,8 @@ pf::UnifiedApi::Controller::Config::Roles
 
 use strict;
 use warnings;
-
+use pf::dal::node_category;
+use pf::error (is_error);
 
 use Mojo::Base qw(pf::UnifiedApi::Controller::Config);
 
@@ -27,6 +28,44 @@ has 'primary_key' => 'role_id';
 use pf::ConfigStore::Roles;
 use pfappserver::Form::Config::Roles;
 
+sub can_delete {
+    my ($self) = @_;
+    my ($status, $msg) = $self->can_delete_from_db();
+    if (is_error($status)) {
+        return ($status, $msg);
+    }
+
+    return (200, '');
+}
+
+my $CAN_DELETE_FROM_DB_SQL = <<SQL;
+SELECT count(*) from node_category
+WHERE
+    name = ? AND
+    (
+        EXISTS (SELECT mac from node WHERE node.category_id = node_category.category_id) OR
+        EXISTS (SELECT mac from node WHERE node.bypass_role_id = node_category.category_id) OR
+        EXISTS (SELECT security_event_id from `class` WHERE `class`.target_category = node_category.name) OR
+        EXISTS (SELECT category from password WHERE password.category = node_category.category_id)
+    );
+SQL
+
+sub can_delete_from_db {
+    my ($self) = @_;
+    my $id = $self->id;
+    my ($status, $sth) = pf::dal::node_category->db_execute($sql, $id);
+    if (is_error($status)) {
+        return ($status, "Unable to check role in the database");
+    }
+
+    my ($count) = $sth->fetchrow_array();
+    $sth->finish;
+    if ($count) {
+        (422, 'Role still in use');
+    }
+
+    return (200, '');
+}
  
 =head1 AUTHOR
 
