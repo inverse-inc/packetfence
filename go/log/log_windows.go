65e3d4a6056fc541b95b2cfe086452ecc54978f1
Julien Semaan <jul.semaan@gmail.com>
2020-09-01 12:35:11 -0400

    add empty log windows

---
+++ go/log/log_windows.go
@@ -0,0 +1,5 @@
+// +build !windows
+
+// This file is intentionally empty so that packages that import github.com/inverse-inc/packetfence/go don't error out on this package being empty when compiling on windows
+
+package log
