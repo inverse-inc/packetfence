719df847476d04a55e7e161cc96d2e99d8e12f17
Darren Satkunas <darren@satkunas.com>
2020-09-09 11:23:01 -0400

    (web admin) add reassign modal on role deletion error

--- html/pfappserver/root/static.alt/src/views/Configuration/_components/RolesList.vue
+++ html/pfappserver/root/static.alt/src/views/Configuration/_components/RolesList.vue
@@ -25,6 +25,41 @@
         </span>
       </template>
     </pf-config-list>
+
+    <b-modal v-model="showDeleteErrorsModal" size="lg"
+      centered lazy scrollable
+      :no-close-on-backdrop="isLoading"
+      :no-close-on-esc="isLoading"
+    >
+      <template v-slot:modal-title>
+        {{ $t('Delete Role') }} <b-badge variant="secondary">{{ deleteId }}</b-badge>
+      </template>
+      <b-media no-body class="alert alert-danger">
+        <icon name="exclamation-triangle" scale="2" v-slot:aside></icon>
+        <div class="mx-2">{{ $t('The role could not be deleted. Either manually handle the following errors and try again, or re-reassign the resources to another existing role.') }}</div>
+      </b-media>
+      <h5>{{ $t('Role is still in use for:') }}</h5>
+      <b-row v-for="error in deleteErrors" :key="error.reason">
+        <b-col cols="auto" class="mr-auto">{{ reasons[error.reason] }}</b-col>
+        <b-col cols="auto">{{ error.reason }}</b-col>
+      </b-row>
+
+      <template v-slot:modal-footer>
+        <b-row class="w-100">
+          <b-col cols="auto" class="mr-auto pl-0">
+            <pf-form-select size="sm" class="d-inline"
+              v-model="reassignRole"
+              :options="reassignableRoles"
+            />
+            <b-button size="sm" class="ml-1" variant="outline-primary"  @click="reassign()" :disabled="isLoading">{{ $i18n.t('Reassign Role') }}</b-button>
+          </b-col>
+          <b-col cols="auto" class="pr-0">
+            <b-button variant="secondary"  @click="showDeleteErrorsModal = false" :disabled="isLoading">{{ $i18n.t('Fix Manually') }}</b-button>
+          </b-col>
+        </b-row>
+      </template>
+    </b-modal>
+
   </b-card>
 </template>
 
@@ -33,7 +68,8 @@ import pfButtonDelete from '@/components/pfButtonDelete'
 import pfButtonHelp from '@/components/pfButtonHelp'
 import pfConfigList from '@/components/pfConfigList'
 import pfEmptyTable from '@/components/pfEmptyTable'
-import { config } from '../_config/role'
+import pfFormSelect from '@/components/pfFormSelect'
+import { config, reasons } from '../_config/role'
 
 export default {
   name: 'roles-list',
@@ -41,17 +77,32 @@ export default {
     pfButtonDelete,
     pfButtonHelp,
     pfConfigList,
-    pfEmptyTable
+    pfEmptyTable,
+    pfFormSelect
   },
   data () {
     return {
       config: config(this),
-      trafficShapingPolicies: []
+      reasons,
+      trafficShapingPolicies: [],
+      deleteId: '',
+      deleteErrors: [],
+      showDeleteErrorsModal: false,
+      roles: [],
+      reassignRole: 'default'
     }
   },
   computed: {
     isInline () {
       return this.$store.getters['system/isInline']
+    },
+    isLoading () {
+      return this.$store.getters['$_roles/isLoading']
+    },
+    reassignableRoles () {
+      return this.roles
+        .filter(role => role.id !== this.deleteId)
+        .map(role => ({ text: role.id, value: role.id }))
     }
   },
   methods: {
@@ -60,6 +111,20 @@ export default {
     },
     remove (item) {
       this.$store.dispatch('$_roles/deleteRole', item.id).then(() => {
+        const { $refs: { pfConfigList: { refreshList = () => {} } = {} } = {} } = this
+        refreshList() // soft reload
+      }).catch(error => {
+        const { response: { data: { errors = [] } = {} } = {} } = error
+        if (errors.length) {
+        this.deleteId = item.id
+          this.deleteErrors = errors
+          this.showDeleteErrorsModal = true
+        }
+      })
+    },
+    reassign () {
+      this.$store.dispatch('$_roles/reassignRole', { from: this.deleteId, to: this.reassignRole}).then(() => {
+        this.showDeleteErrorsModal = false
         const { $refs: { pfConfigList: { refreshList = () => {} } = {} } = {} } = this
         refreshList() // soft reload
       })
@@ -71,6 +136,9 @@ export default {
     }
   },
   created () {
+    this.$store.dispatch('$_roles/all').then(roles => {
+      this.roles = roles
+    })
     this.$store.dispatch('$_traffic_shaping_policies/all').then(response => {
       this.trafficShapingPolicies = response.map(policy => policy.id)
     })
