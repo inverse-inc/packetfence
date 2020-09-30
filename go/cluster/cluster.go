077f9d718c221a1d0e63c33b02db3249055c9143
James Rouzier <jrouzier@inverse.ca>
2020-09-30 11:07:49 -0400

    Fix EnabledServers

--- go/cluster/cluster.go
+++ go/cluster/cluster.go
@@ -24,14 +24,14 @@ func (s *Server) IsDisabled() bool {
 	return !info.IsDir()
 }
 
-func CallCluster(ctx context.Context, method string, args interface{}) bool {
+func CallCluster(ctx context.Context, method string, args interface{}, tenant_id int) bool {
 	servers, cluster_mode := EnabledServers(ctx)
 	if cluster_mode {
 		clientApi := jsonrpc2.NewClientFromConfig(ctx)
 		clientApi.Proto = "https"
 		for _, member := range servers {
 			clientApi.Host = member.ManagementIp
-			if _, err := clientApi.Call(ctx, method, args, 1); err != nil {
+			if _, err := clientApi.Call(ctx, method, args, tenant_id); err != nil {
 				log.LogError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
 			}
 		}
@@ -40,14 +40,14 @@ func CallCluster(ctx context.Context, method string, args interface{}) bool {
 	return cluster_mode
 }
 
-func NotifyCluster(ctx context.Context, method string, args interface{}) bool {
+func NotifyCluster(ctx context.Context, method string, args interface{}, tenant_id int) bool {
 	servers, cluster_mode := EnabledServers(ctx)
 	if cluster_mode {
 		clientApi := jsonrpc2.NewClientFromConfig(ctx)
 		clientApi.Proto = "https"
 		for _, member := range servers {
 			clientApi.Host = member.ManagementIp
-			if err := clientApi.Notify(ctx, method, args, 1); err != nil {
+			if err := clientApi.Notify(ctx, method, args, tenant_id); err != nil {
 				log.LogError(ctx, "Error calling "+clientApi.Host+": "+err.Error())
 			}
 		}
@@ -59,7 +59,7 @@ func EnabledServers(ctx context.Context) ([]Server, bool) {
 	servers := []Server{}
 	cluster_servers := pfconfigdriver.AllClusterServers{}
 	pfconfigdriver.FetchDecodeSocketCache(ctx, &cluster_servers)
-	if len(servers) == 0 {
+	if len(cluster_servers.Element) == 0 {
 		return nil, false
 	}
 	for _, s := range cluster_servers.Element {
