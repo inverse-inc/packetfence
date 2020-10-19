4ccf1288f2ed870133ea988f25415117ca9131c6
Julien Semaan <jul.semaan@gmail.com>
2020-10-19 08:53:34 -0400

    fill name + address in Stripe if its there
    fixes #5924

--- lib/pf/Authentication/Source/StripeSource.pm
+++ lib/pf/Authentication/Source/StripeSource.pm
@@ -229,6 +229,14 @@ sub subscribe_customer {
         description => $tier->{description},
         'metadata[mac_address]' => $session->{billed_mac},
     };
+
+    if($session->{request_fields}->{firstname} && $session->{request_fields}->{lastname}) {
+        $object->{name} = $session->{request_fields}->{firstname} . " " . $session->{request_fields}->{lastname};
+    }
+    if($session->{request_fields}->{address}) {
+        $object->{'address[line1]'} = $session->{request_fields}->{address};
+    }
+
     if(my $cus = $self->get_customer_by_email($session->{email})) {
         my ($code, $data) = $self->_send_form($self->curl, "v1/customers/".$cus->{id}, $object);
     }
