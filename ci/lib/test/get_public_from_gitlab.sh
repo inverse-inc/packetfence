558ebc5f6811fcd2f3081b95cef304a9312ca85e
jeremy <13801368+JeGoi@users.noreply.github.com>
2020-10-28 13:42:55 -0400

    Venom Add tests on directory And Stop script if public exists

--- ci/lib/test/get_public_from_gitlab.sh
+++ ci/lib/test/get_public_from_gitlab.sh
@@ -4,9 +4,26 @@ set -o nounset -o pipefail
 REPO_URL=https://gitlab.com/inverse-inc/packetfence/-/jobs/artifacts/devel/download?job=pages
 ZIP_FILE=$(mktemp --suff=".zip")
 
-# get root of git directory
-DIR=$(pwd | grep -oP '.*?(?=ci/lib/test)')
+# get git root path directory
+PWD=$(pwd)
+DIR=""
+if [[ "${PWD}" == *\/ci ]] ; then
+  DIR=$(echo ${PWD} | grep -oP '.*?(?=\/ci)')
+else
+  DIR=$(echo ${PWD} | grep -oP '.*?(?=\/ci\/)')
+fi
+if [ "${DIR}" == "" ] || [ -z $DIR ]; then
+  DIR=${PWD}
+fi
+echo "The directory used will be "${DIR}
 
+# test if public is already there
+if [ -d "${DIR}/public/" ]; then
+  echo "The public directory is already there. The script will stop."
+  exit 0
+fi
+
+# test if curl installed
 if ! type curl 2> /dev/null ; then
   echo "Install curl before running this script"
   exit 1 
@@ -15,11 +32,11 @@ fi
 # Download the archive zipfile, extract and remove
 curl -Ls ${REPO_URL} --output ${ZIP_FILE}
 if [ -f ${ZIP_FILE} ]; then
-  echo "Public zipfile is there"
+  echo "Public zipfile is there "${ZIP_FILE}
   unzip -oq ${ZIP_FILE} -d ${DIR}
-  echo "Unzip is done"
+  echo "Unzip is done in "${DIR}
   rm -f ${ZIP_FILE}
-  echo "Zipfile is removed"
+  echo "Zipfile "${ZIP_FILE}" is removed"
   exit 0
 else
   echo "The zipfile is no available"
