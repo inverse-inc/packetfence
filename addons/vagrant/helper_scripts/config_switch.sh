ffdd9cabfeb362c9d9835f2ac8b04eddb52fd293
nqb <nqb+git@azyx.fr>
2020-10-29 07:38:16 +0100

    [Vagrant] add node02 with network card on switch01

--- addons/vagrant/helper_scripts/config_switch.sh
+++ addons/vagrant/helper_scripts/config_switch.sh
@@ -19,7 +19,7 @@ iface lo inet loopback
 auto bridge
 iface bridge
     bridge-vlan-aware yes
-    bridge-ports swp48 swp1 swp2 swp3 swp6 swp11 swp12 swp13
+    bridge-ports swp48 swp1 swp2 swp3 swp6 swp11 swp12 swp13 swp14
     bridge-vids 2 3 6 17 100
     bridge-pvid 1
 
