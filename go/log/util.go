f64dd2bd47485712fd881b8c0284703b6f46b612
James Rouzier <jrouzier@inverse.ca>
2020-11-09 16:34:38 -0500

    Add additional helper functions

--- go/log/util.go
+++ go/log/util.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"context"
+	"fmt"
 )
 
 func LogError(ctx context.Context, msg string) {
@@ -18,4 +19,20 @@ func LogInfo(ctx context.Context, msg string) {
 
 func LogDebug(ctx context.Context, msg string) {
 	LoggerWContext(ctx).Debug(msg)
+}
+
+func LogErrorf(ctx context.Context, msg string, args ...interface{}) {
+	LoggerWContext(ctx).Error(fmt.Sprintf(msg, args...))
+}
+
+func LogWarnf(ctx context.Context, msg string, args ...interface{}) {
+	LoggerWContext(ctx).Warn(fmt.Sprintf(msg, args...))
+}
+
+func LogInfof(ctx context.Context, msg string, args ...interface{}) {
+	LoggerWContext(ctx).Info(fmt.Sprintf(msg, args...))
+}
+
+func LogDebugf(ctx context.Context, msg string, args ...interface{}) {
+	LoggerWContext(ctx).Debug(fmt.Sprintf(msg, args...))
 }
