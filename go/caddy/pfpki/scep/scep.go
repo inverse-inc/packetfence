b2b048e8777f15bb24c50dd542fcb656c4321ae8
Durand Fabrice <oeufdure@gmail.com>
2020-12-30 15:17:42 -0500

    Reworked scep server

--- go/caddy/pfpki/scep/scep.go
+++ go/caddy/pfpki/scep/scep.go
@@ -1,10 +1,12 @@
 package scep
 
 import (
-	"context"
 	"net/http"
+	"os"
 
 	scepserver "github.com/fdurand/scep/server"
+	kitlog "github.com/go-kit/kit/log"
+	kitloglevel "github.com/go-kit/kit/log/level"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/models"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/types"
 	"github.com/inverse-inc/packetfence/go/log"
@@ -12,62 +14,47 @@ import (
 
 func ScepHandler(pfpki *types.Handler, w http.ResponseWriter, r *http.Request) {
 
-	svcOptions := []scepserver.ServiceOption{
-		scepserver.ChallengePassword(""),
-		//scepserver.WithCSRVerifier(csrVerifier),
-		scepserver.CAKeyPassword([]byte("")),
-		scepserver.ClientValidity(365),
-		scepserver.AllowRenewal(0),
-		//scepserver.WithLogger(logger),
+	var logger kitlog.Logger
+	{
+
+		logger = kitlog.NewLogfmtLogger(os.Stderr)
+		logger = kitloglevel.NewFilter(logger, kitloglevel.AllowInfo())
+		logger = kitlog.With(logger, "ts", kitlog.DefaultTimestampUTC)
+		logger = kitlog.With(logger, "caller", kitlog.DefaultCaller)
 	}
+	lginfo := kitloglevel.Info(logger)
 
 	log.LoggerWContext(*pfpki.Ctx).Info("SCEP GET From ", r.Method, " To: ", r.URL.String())
 
-	operation := r.URL.Query().Get("operation")
-	message := r.URL.Query().Get("message")
+	var err error
 
-	switch operation {
-	// case "GetCACaps":
-	// 	o := models.NewCAModel(pfpki)
-	// 	svc, err := scepserver.NewService(o, svcOptions...)
-	// 	res, err := svc.GetCACaps(context.Background())
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	w.Write(res)
-	case "GetCACert":
-		o := models.NewCAModel(pfpki)
-		if message != "" {
-			o.Cn = message
-		}
-		svc, err := scepserver.NewService(o, svcOptions...)
+	o := models.NewCAModel(pfpki)
 
-		if err != nil {
-			panic(err)
+	var svc scepserver.Service // scep service
+	{
+		svcOptions := []scepserver.ServiceOption{
+			scepserver.ChallengePassword(""),
+			// scepserver.WithCSRVerifier(csrVerifier),
+			scepserver.CAKeyPassword([]byte("")),
+			scepserver.ClientValidity(365),
+			scepserver.AllowRenewal(0),
+			// scepserver.WithLogger(logger),
 		}
-		res, _, err := svc.GetCACert(context.Background())
+		svc, err = scepserver.NewService(o, svcOptions...)
 		if err != nil {
-			panic(err)
+			log.LoggerWContext(*pfpki.Ctx).Info("err ", err)
 		}
-		w.Header().Set("Content-Type", "application/x-x509-ca-cert")
-		w.Write(res)
-	// case "PKIOperation":
-	// 	body, err := ioutil.ReadAll(r.Body)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	res, err := svc.PKIOperation(context.Background(), body)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	w.Write(res)
-	// case "GetNextCACert":
-	// 	res, err := svc.GetNextCACert(context.Background())
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	w.Write(res)
-	default:
-		http.Error(w, "Invalid Operation", 500)
+		svc = scepserver.NewLoggingService(kitlog.With(lginfo, "component", "scep_service"), svc)
 	}
+
+	var h http.Handler // http handler
+	{
+		e := scepserver.MakeServerEndpoints(svc)
+		e.GetEndpoint = scepserver.EndpointLoggingMiddleware(lginfo)(e.GetEndpoint)
+		e.PostEndpoint = scepserver.EndpointLoggingMiddleware(lginfo)(e.PostEndpoint)
+		h = scepserver.MakeHTTPHandler(e, svc, kitlog.With(lginfo, "component", "http"))
+	}
+
+	h.ServeHTTP(w, r)
+
 }
