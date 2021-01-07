30894a1e333bdbcd135bf8aed37c903de76e6e79
James Rouzier <jrouzier@inverse.ca>
2021-01-07 06:59:00 -0500

    Add CREATE FUNCTION for pf_logger.

--- t/db/smoke_test.sql
+++ t/db/smoke_test.sql
@@ -1,4 +1,6 @@
-
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM pf_smoke_tester@'%', pf_smoke_tester@'localhost';
 GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'%' IDENTIFIED BY 'packet';
 GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
+GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
+GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'%' IDENTIFIED BY 'packet';
 FLUSH PRIVILEGES;
