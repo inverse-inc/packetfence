48775613b7bc0454de7c4edca8615f83c8144d4c
Darren Satkunas <darren@satkunas.com>
2021-01-13 15:06:29 -0500

    (admin) deprecate unused code in active cluster

--- html/pfappserver/root/static.alt/src/views/Configuration/activeActive/schema.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/activeActive/schema.js
@@ -1,4 +1,12 @@
-export const schema = () => {}
+import i18n from '@/utils/locale'
+import yup from '@/utils/yup'
+
+export const schema = () => yup.object({
+  password: yup.string().nullable().label(i18n.t('Key')),
+  virtual_router_id: yup.string().nullable().label(i18n.t('Identifier')),
+  galera_replication_username: yup.string().nullable().label(i18n.t('Username')),
+  galera_replication_password: yup.string().nullable().label(i18n.t('Password'))
+})
 
 export default schema
 
