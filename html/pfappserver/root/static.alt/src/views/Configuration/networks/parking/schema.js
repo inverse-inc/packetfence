b656b98c52f1675d19f27b97d7859f80fbee00f9
Darren Satkunas <darren@satkunas.com>
2021-01-15 14:20:54 -0500

    (admin) deprecate unused code in parking

--- html/pfappserver/root/static.alt/src/views/Configuration/networks/parking/schema.js
+++ html/pfappserver/root/static.alt/src/views/Configuration/networks/parking/schema.js
@@ -1,6 +1,10 @@
+import i18n from '@/utils/locale'
 import yup from '@/utils/yup'
 
-export const schema = () => yup.object({})
+export const schema = () => yup.object({
+  lease_length: yup.string().nullable().label(i18n.t('Length')),
+  threshold: yup.string().nullable().label(i18n.t('Threshold'))
+})
 
 export default schema
 
