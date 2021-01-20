135c6e9728dd34a5173216ef37332fd912518d54
Darren Satkunas <darren@satkunas.com>
2021-01-20 16:19:35 -0500

    (admin) improve MutationObserver

--- html/pfappserver/root/static.alt/src/composables/useDom.js
+++ html/pfappserver/root/static.alt/src/composables/useDom.js
@@ -1,43 +1,57 @@
 import { computed, onMounted, onBeforeUnmount, ref } from '@vue/composition-api'
 
-export const useMutationObserver = (el, callback, config = { attributes: false, childList: true, subtree: true }) => {
+export const useMutationObserver = (el, callback, config = {}) => {
   const observer = new MutationObserver(callback)
   const removeObserver = () => observer.disconnect()
 
-  onMounted(() => el && observer.observe((el.value || el), config))
+  onMounted(() => {
+    const _el = (el.constructor === Function) ? el() : el
+    return Promise.resolve(_el).then(el => observer.observe(el, { attributes: false, childList: true, subtree: true, ...config }))
+  })
   onBeforeUnmount(removeObserver)
 
   return removeObserver
 }
 
-export const useResizeObserver = (el, callback, config = { box: 'border-box' }) => {
+export const useResizeObserver = (el, callback, config = {}) => {
   const observer = new ResizeObserver(callback)
   const removeObserver = () => observer.disconnect()
 
-  onMounted(() => el && observer.observe((el.value || el), config))
+  onMounted(() => {
+    const _el = (el.constructor === Function) ? el() : el
+    return Promise.resolve(_el).then(el => observer.observe((el.value || el), { box: 'border-box', ...config }))
+  })
   onBeforeUnmount(removeObserver)
 
   return removeObserver
 }
 
 export const useQuerySelector = (el, selector) => {
-  const $el = computed(() => (el && el.value && '$el' in el.value) ? el.value.$el : el)
   const result = ref(null)
-
-  useMutationObserver($el, () => {
-    result.value = $el.value.querySelector(selector)
-  })
-
+  useMutationObserver(
+    () => {
+      const { value: { $el } = {} } = el
+      return $el || el
+    },
+    () => {
+      const { value: { $el } = {} } = el
+      result.value = ($el || el).querySelector(selector)
+    }
+  )
   return result
 }
 
 export const useQuerySelectorAll = (el, selector) => {
-  const $el = computed(() => (el && el.value && '$el' in el.value) ? el.value.$el : el)
-  const result = ref([])
-
-  useMutationObserver($el, () => {
-    result.value = $el.value.querySelectorAll(selector)
-  })
-
+  const result = ref(null)
+  useMutationObserver(
+    () => {
+      const { value: { $el } = {} } = el
+      return $el || el
+    },
+    () => {
+      const { value: { $el } = {} } = el
+      result.value = ($el || el).querySelectorAll(selector)
+    }
+  )
   return result
 }
--- html/pfappserver/root/static.alt/src/composables/useFormTab.js
+++ html/pfappserver/root/static.alt/src/composables/useFormTab.js
@@ -1,4 +1,4 @@
-import { computed, ref, unref } from '@vue/composition-api'
+import { computed, ref } from '@vue/composition-api'
 import { useQuerySelectorAll } from './useDom'
 
 export const useFormTabProps = {}
@@ -7,9 +7,9 @@ export const useFormTab = () => {
   // template refs
   const rootRef = ref(null)
 
-  const isInvalidElements = useQuerySelectorAll(rootRef, '.form-group.is-invalid')
-  const isValid = computed(() => unref(rootRef) && unref(isInvalidElements).length === 0)
-  const numInvalid = computed(() => (unref(isValid)) ? 0 : unref(isInvalidElements).length)
+  const _isValidNodes = useQuerySelectorAll(rootRef, '.form-group.is-invalid')
+  const isValid = computed(() => (!_isValidNodes.value || Array.prototype.slice.call(_isValidNodes.value).length === 0))
+  const numInvalid = computed(() => (isValid.value) ? 0 : _isValidNodes.value.length)
 
   return {
     rootRef,
