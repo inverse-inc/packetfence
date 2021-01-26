2972f74609df38e060fdb61bae234edffd2b1886
Darren Satkunas <darren@satkunas.com>
2021-01-26 11:50:48 -0500

    (admin) initial commit

--- html/pfappserver/root/static.alt/src/views/Configurator/_router/index.js
+++ html/pfappserver/root/static.alt/src/views/Configurator/_router/index.js
@@ -1,5 +1,4 @@
 import TheView from '../'
-
 import NetworkRoutes from '../network/_router'
 import PacketfenceRoutes from '../packetfence/_router'
 import FingerbankRoutes from '../fingerbank/_router'
