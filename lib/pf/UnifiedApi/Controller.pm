24aed9fd3ae2e45fce633c7cd039a34e44980c71
James Rouzier <jrouzier@inverse.ca>
2021-02-02 11:37:08 -0500

    Add task_id method

--- lib/pf/UnifiedApi/Controller.pm
+++ lib/pf/UnifiedApi/Controller.pm
@@ -22,6 +22,7 @@ use pf::dal::admin_api_audit_log;
 use Mojo::JSON qw(encode_json);
 has activity_timeout => 300;
 has 'openapi_generator_class' => undef;
+use Data::UUID;
 
 our $ERROR_400_MSG = "Bad Request. One of the submitted parameters has an invalid format";
 our $ERROR_404_MSG = "Not Found. The requested resource could not be found";
@@ -34,6 +35,8 @@ our %STATUS_TO_MSG = (
     409 => $ERROR_409_MSG,
     422 => $ERROR_422_MSG,
 );
+
+my $GENERATOR = Data::UUID->new;
 
 use Mojo::JSON qw(decode_json);
 use pf::util;
@@ -203,6 +206,10 @@ sub escape_url_param {
     return $param unless defined $param;
     $param =~ s/%2[fF]|~/\//g;
     return $param;
+}
+
+sub task_id {
+    "ApiTask:" . $GENERATOR->create_str
 }
 
 =head1 AUTHOR
