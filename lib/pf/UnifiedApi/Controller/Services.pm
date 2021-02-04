99c2a1ea4202b188a9fcf05526c8e6d5d78182cb
James Rouzier <jrouzier@inverse.ca>
2021-02-04 09:15:28 -0500

    Allow start/stop/restart to be async

--- lib/pf/UnifiedApi/Controller/Services.pm
+++ lib/pf/UnifiedApi/Controller/Services.pm
@@ -16,15 +16,19 @@ use strict;
 use warnings;
 use Mojo::Base 'pf::UnifiedApi::Controller::RestRoute';
 use pf::services;
+use pf::error qw(is_error);
+use pf::pfqueue::status_updater::redis;
+use pf::util::pfqueue qw(consumer_redis_client);
 
 sub resource {
     my ($self) = @_;
-
     my $service_id = $self->param('service_id');
-
     my $class = $self->_get_service_class($service_id);
+    if (defined $class) {
+        $self->stash->{item} = $class;
+        return 1;
+    }
 
-    return 1 if defined($class);
     $self->render_error(404, $self->status_to_error_msg(404));
     return undef;
 }
@@ -90,28 +94,75 @@ sub status_all {
     return $self->render(json => { items => \@services });
 }
 
+sub get_data {
+    my ($self) = @_;
+    my $body = $self->req->body();
+    if (!defined $body || $body eq '') {
+        return 200, {}
+    }
+
+    return $self->parse_json($body);
+}
+
 sub start {
     my ($self) = @_;
-    my $service = $self->_get_service_class($self->param('service_id'));
-    if ($service) {
-        return $self->render(json => { start => $service->start(), pid => $service->pid() });
-    }
+    return $self->do_action('do_start');
 }
 
 sub stop {
     my ($self) = @_;
-    my $service = $self->_get_service_class($self->param('service_id'));
-    if ($service) {
-        return $self->render(json => { stop => $service->stop() });
-    }
+    return $self->do_action('do_stop');
 }
 
 sub restart {
     my ($self) = @_;
-    my $service = $self->_get_service_class($self->param('service_id'));
-    if ($service) {
-        return $self->render(json => { restart => $service->restart(), pid => $service->pid() });
+    return $self->do_action('do_restart');
+}
+
+sub do_action {
+    my ($self, $action) = @_;
+    my ($status, $data) = $self->get_data();
+    if (is_error($status)) {
+        return $self->render(json => $data, status => $status);
     }
+
+    if ($data->{async}) {
+        my $task_id = $self->task_id;
+        my $subprocess = Mojo::IOLoop->subprocess;
+        $subprocess->run(
+            sub {
+                my ($subprocess) = @_;
+                my $updater = pf::pfqueue::status_updater::redis->new( connection => consumer_redis_client(), task_id => $task_id );
+                $updater->start;
+                my $data = $self->$action();
+                $updater->completed($data);
+            },
+            sub {},
+        );
+
+        return $self->render( json => {status => 202, task_id => $task_id }, status => 202);
+    }
+
+    my $results = $self->$action();
+    return $self->render(json => $results);
+}
+
+sub do_start {
+    my ($self) = @_;
+    my $service = $self->stash->{item};
+    return { start => $service->start(), pid => $service->pid() };
+}
+
+sub do_stop {
+    my ($self) = @_;
+    my $service = $self->stash->{item};
+    return { stop => $service->stop() };
+}
+
+sub do_restart {
+    my ($self) = @_;
+    my $service = $self->stash->{item};
+    return { restart => $service->restart(), pid => $service->pid() };
 }
 
 sub enable {
