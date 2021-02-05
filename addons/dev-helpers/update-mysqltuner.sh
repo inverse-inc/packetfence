22097cf0b19c99fa3e4a396ea0b2379c3d278e42
James Rouzier <jrouzier@inverse.ca>
2021-02-05 09:05:50 -0500

    Create a script to update the mysqltuner.pl and support files

---
+++ addons/dev-helpers/update-mysqltuner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MYSQLTUNER_DIR=/usr/local/pf/bin/mysqltuner/
+wget http://mysqltuner.pl/ -O "$MYSQLTUNER_DIR/mysqltuner.pl"
+
+for f in basic_passwords.txt vulnerabilities.csv;do
+    wget "https://raw.githubusercontent.com/major/MySQLTuner-perl/master/$f" -O "$MYSQLTUNER_DIR/$f"
+done
