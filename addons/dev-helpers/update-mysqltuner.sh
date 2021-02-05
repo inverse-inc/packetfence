fa4de9b7f338da42e20e89085285ef8f24eb8a3b
James Rouzier <jrouzier@inverse.ca>
2021-02-05 09:09:08 -0500

    Add the license of mysqltuner.pl

--- addons/dev-helpers/update-mysqltuner.sh
+++ addons/dev-helpers/update-mysqltuner.sh
@@ -2,6 +2,6 @@
 MYSQLTUNER_DIR=/usr/local/pf/bin/mysqltuner/
 wget http://mysqltuner.pl/ -O "$MYSQLTUNER_DIR/mysqltuner.pl"
 
-for f in basic_passwords.txt vulnerabilities.csv;do
+for f in basic_passwords.txt vulnerabilities.csv LICENSE;do
     wget "https://raw.githubusercontent.com/major/MySQLTuner-perl/master/$f" -O "$MYSQLTUNER_DIR/$f"
 done
