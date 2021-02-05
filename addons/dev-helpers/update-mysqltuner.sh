a043aec53bc5a79e70e0fa9104a71d0790494c99
James Rouzier <jrouzier@inverse.ca>
2021-02-05 11:54:06 -0500

    Only use the latest release

--- addons/dev-helpers/update-mysqltuner.sh
+++ addons/dev-helpers/update-mysqltuner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+LATEST=$(curl --silent "https://api.github.com/repos/major/MySQLTuner-perl/releases/latest" | jq -r .tag_name)
 MYSQLTUNER_DIR=/usr/local/pf/bin/mysqltuner/
 wget http://mysqltuner.pl/ -O "$MYSQLTUNER_DIR/mysqltuner.pl"
 
-for f in basic_passwords.txt vulnerabilities.csv LICENSE;do
-    wget "https://raw.githubusercontent.com/major/MySQLTuner-perl/master/$f" -O "$MYSQLTUNER_DIR/$f"
+for f in basic_passwords.txt vulnerabilities.csv mysqltuner.pl LICENSE;do
+    wget "https://raw.githubusercontent.com/major/MySQLTuner-perl/$LATEST/$f" -O "$MYSQLTUNER_DIR/$f"
 done
