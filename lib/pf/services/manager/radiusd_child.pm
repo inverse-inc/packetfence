79fc7c9553fa8c521ec93aaf8bda0cdd51d17f6c
Durand Fabrice <oeufdure@gmail.com>
2021-02-08 12:23:08 -0500

    Fixed bracket

--- lib/pf/services/manager/radiusd_child.pm
+++ lib/pf/services/manager/radiusd_child.pm
@@ -975,9 +975,11 @@ EOT
             if (isenabled($pf::config::ConfigRealm{$realm}->{'radius_acct_home_server_pool_virtual_server'})) {
                 $tags{'config'} .= <<"EOT";
 virtual_server = virtual_server_pool_acct_pool_$realm
-}
 EOT
             }
+            $tags{'config'} .= <<"EOT";
+}
+EOT
 #            if (isenabled($pf::config::ConfigRealm{$realm}->{'radius_acct_home_server_pool_fallback'})) {
 #                $tags{'config'} .= <<"EOT";
 #fallback = fallback_server_pool_acct_pool_$realm
