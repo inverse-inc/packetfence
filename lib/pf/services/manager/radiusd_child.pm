7ea9223265fbaeb16530b00f58c8d3037713069b
Durand Fabrice <oeufdure@gmail.com>
2021-02-08 12:20:35 -0500

    Fixed syntax

--- lib/pf/services/manager/radiusd_child.pm
+++ lib/pf/services/manager/radiusd_child.pm
@@ -975,6 +975,7 @@ EOT
             if (isenabled($pf::config::ConfigRealm{$realm}->{'radius_acct_home_server_pool_virtual_server'})) {
                 $tags{'config'} .= <<"EOT";
 virtual_server = virtual_server_pool_acct_pool_$realm
+}
 EOT
             }
 #            if (isenabled($pf::config::ConfigRealm{$realm}->{'radius_acct_home_server_pool_fallback'})) {
@@ -982,9 +983,6 @@ EOT
 #fallback = fallback_server_pool_acct_pool_$realm
 #EOT
 #            }
-            $tags{'config'} .= <<"EOT";
-}
-EOT
             if (isenabled($pf::config::ConfigRealm{$realm}->{'radius_acct_home_server_pool_virtual_server'})) {
                 $tags{'config'} .= <<"EOT";
 server virtual_server_pool_acct_pool_$realm {
