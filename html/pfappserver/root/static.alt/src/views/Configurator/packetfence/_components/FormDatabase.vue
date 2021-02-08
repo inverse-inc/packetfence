9de959f6fbd3166ff7ca30a9572821742291417f
Darren Satkunas <darren@satkunas.com>
2021-02-08 10:07:12 -0500

    (admin) remove dup props

--- html/pfappserver/root/static.alt/src/views/Configurator/packetfence/_components/FormDatabase.vue
+++ html/pfappserver/root/static.alt/src/views/Configurator/packetfence/_components/FormDatabase.vue
@@ -201,12 +201,6 @@ const props = {
   }
 }
 
-const props = {
-  disabled: {
-    type: Boolean
-  }
-}
-
 import { computed, inject, ref } from '@vue/composition-api'
 import apiCall from '@/utils/api'
 import i18n from '@/utils/locale'
