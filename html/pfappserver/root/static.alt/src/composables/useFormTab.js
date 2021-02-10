155ee71581aa17785619d50b6b49cba58c8dc946
Darren Satkunas <darren@satkunas.com>
2021-02-10 14:13:35 -0500

    (admin) fix tab error count

--- html/pfappserver/root/static.alt/src/composables/useFormTab.js
+++ html/pfappserver/root/static.alt/src/composables/useFormTab.js
@@ -7,13 +7,19 @@ export const useFormTab = () => {
   // template refs
   const rootRef = ref(null)
 
-  const _isValidNodes = useQuerySelectorAll(rootRef, '.form-group.is-invalid')
-  const isValid = computed(() => (!_isValidNodes.value || Array.prototype.slice.call(_isValidNodes.value).length === 0))
-  const numInvalid = computed(() => (isValid.value) ? 0 : _isValidNodes.value.length)
+  const _invalidNodes = useQuerySelectorAll(rootRef, '.form-group.is-invalid')
+  const _visibleInvalidNodes = computed(() => Array.prototype.slice.call(_invalidNodes.value)
+    .filter(el => el.closest('fieldset').style.display !== 'none') // handle v-show <.. style="display: none;">
+  )
+  const isValid = computed(() => (!_invalidNodes.value || _visibleInvalidNodes.value.length === 0))
+  const numInvalid = computed(() => (isValid.value) ? 0 : _visibleInvalidNodes.value.length)
 
   return {
     rootRef,
     isValid,
-    numInvalid
+    numInvalid,
+
+    _invalidNodes,
+    _visibleInvalidNodes
   }
 }
