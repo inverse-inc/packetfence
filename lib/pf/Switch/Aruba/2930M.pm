546efe01c74719cfafc744153c3e243979e02dbc
Durand Fabrice <oeufdure@gmail.com>
2021-03-09 14:26:04 -0500

    Fixes for PR

--- lib/pf/Switch/Aruba/2930M.pm
+++ lib/pf/Switch/Aruba/2930M.pm
@@ -66,7 +66,7 @@ sub returnRadiusAccessAccept {
     }
 
     if ( isenabled($self->{_AccessListMap}) && $self->supportsAccessListBasedEnforcement ){
-        if( defined($args->{'user_role'}) && $args->{'user_role'} ne "" && defined($self->getAccessListByName($args->{'user_role'}))){
+        if( defined($args->{'user_role'}) && $args->{'user_role'} ne "" && defined(my $access_list = $self->getAccessListByName($args->{'user_role'}, $args->{mac}))) {
             my $access_list = $self->getAccessListByName($args->{'user_role'});
             if ($access_list) {
                 while($access_list =~ /([^\n]+)\n?/g){
@@ -198,7 +198,7 @@ sub radiusDisconnect {
         my $roleResolver = pf::roles::custom->instance();
         my $role = $roleResolver->getRoleForNode($mac, $self);
 
-        # transforming MAC to the expected format 00-11-22-33-CA-FE
+        # transforming MAC to the expected format 00-11-22-33-ca-fe
         my $mac = lc($mac);
         my $username = $locationlog->{dot1x_username};
         $mac =~ s/:/-/g;
@@ -284,7 +284,22 @@ Inverse inc. <info@inverse.ca>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2020 Inverse inc.
+Copyright (C) 2005-2021 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
 
 =cut
 
