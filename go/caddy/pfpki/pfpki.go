0fb12827e196946a5efc563d69e5654b125706e2
Durand Fabrice <oeufdure@gmail.com>
2021-03-11 15:59:31 -0500

    Allow multiples uri

--- go/caddy/pfpki/pfpki.go
+++ go/caddy/pfpki/pfpki.go
@@ -131,6 +131,18 @@ func buildPfpkiHandler(ctx context.Context) (types.Handler, error) {
 
 	api.Handle("/scep/{id}", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
 
+	api.Handle("/pki/scep", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
+	api.Handle("/pki/scep/{id}", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
+	api.Handle("/scep/", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
+	api.Handle("/scep/{id}/", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
+	api.Handle("/pki/scep/", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
+	api.Handle("/pki/scep/{id}/", handlers.ManageSCEP(PFPki)).Methods("GET", "POST")
+
 	go func() {
 		for {
 			pfpki.DB.DB().Ping()
