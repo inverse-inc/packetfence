426981fd9e433cc12b39989a23f319bff87a04ab
Darren Satkunas <darren@satkunas.com>
2021-03-11 09:34:20 -0500

    (admin) update User validations

--- html/pfappserver/root/static.alt/src/views/Users/schema.js
+++ html/pfappserver/root/static.alt/src/views/Users/schema.js
@@ -1,51 +1,17 @@
 import store from '@/store'
 import i18n from '@/utils/locale'
-import yup, { MysqlEnum, MysqlNumber, MysqlString, MysqlEmail } from '@/utils/yup'
-import { mysqlLimits as MysqlLimits } from '@/globals/mysqlLimits'
+import yup from '@/utils/yup'
+import { MysqlDatabase } from '@/globals/mysql'
 import { pfActionsSchema as schemaActions } from '@/globals/pfActions'
 
-const mysqlTable = {
-  tenant_id:        Object.assign(MysqlLimits.int, { type: MysqlNumber, default: 1 }),
-  pid:              { type: MysqlString, maxLength: 255 },
-  firstname:        { type: MysqlString, maxLength: 255, default: null },
-  lastname:         { type: MysqlString, maxLength: 255, default: null },
-  email:            { type: MysqlEmail, maxLength: 255, default: null },
-  telephone:        { type: MysqlString, maxLength: 255, default: null },
-  company:          { type: MysqlString, maxLength: 255, default: null },
-  address:          { type: MysqlString, maxLength: 255, default: null },
-  notes:            { type: MysqlString, maxLength: 255, default: null },
-  sponsor:          { type: MysqlString, maxLength: 255, default: null },
-  anniversary:      { type: MysqlString, maxLength: 255, default: null },
-  birthday:         { type: MysqlString, maxLength: 255, default: null },
-  gender:           { type: MysqlString, maxLength: 1, default: null },
-  lang:             { type: MysqlString, maxLength: 255, default: null },
-  nickname:         { type: MysqlString, maxLength: 255, default: null },
-  cell_phone:       { type: MysqlString, maxLength: 255, default: null },
-  work_phone:       { type: MysqlString, maxLength: 255, default: null },
-  title:            { type: MysqlString, maxLength: 255, default: null },
-  building_number:  { type: MysqlString, maxLength: 255, default: null },
-  apartment_number: { type: MysqlString, maxLength: 255, default: null },
-  room_number:      { type: MysqlString, maxLength: 255, default: null },
-  custom_field_1:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_2:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_3:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_4:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_5:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_6:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_7:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_8:   { type: MysqlString, maxLength: 255, default: null },
-  custom_field_9:   { type: MysqlString, maxLength: 255, default: null },
-  portal:           { type: MysqlString, maxLength: 255, default: null },
-  source:           { type: MysqlString, maxLength: 255, default: null },
-  psk:              { type: MysqlString, maxLength: 255, default: null },
-  potd:             { type: MysqlEnum, enum: ['no', 'yes'], default: 'no' }
-}
+// combine person and password tables
+const mysqlDatabase = { ...MysqlDatabase.password, ...MysqlDatabase.person }
 
 // build schema from mysql table
-const schemaMysqlTable = Object.keys(mysqlTable).reduce((schema, key) => {
+const mysqlDatabaseSchema = Object.keys(mysqlDatabase).reduce((schema, key) => {
   return { ...schema,
     [key]: yup.string().nullable()
-      .mysql(mysqlTable[key])
+      .mysql(mysqlDatabase[key])
   }
 }, {})
 
@@ -74,7 +40,7 @@ export const single = (props, form) => {
     expiration
   } = form || {}
 
-  return yup.object().shape(schemaMysqlTable).concat(
+  return yup.object().shape(mysqlDatabaseSchema).concat(
     yup.object().shape({
       pid: yup.string()
         .when('pid_overwrite', () => ((pid_overwrite)
@@ -106,10 +72,10 @@ export const multiple = (props, form, domainName) => {
   } = form || {}
   
   const maxLength = (domainName)
-    ? mysqlTable.pid.maxLength - Math.floor(Math.log10(quantity || 1) + 1) - `@${domainName}`.length
-    : mysqlTable.pid.maxLength - Math.floor(Math.log10(quantity || 1) + 1)
+    ? mysqlDatabase.pid.maxLength - Math.floor(Math.log10(quantity || 1) + 1) - `@${domainName}`.length
+    : mysqlDatabase.pid.maxLength - Math.floor(Math.log10(quantity || 1) + 1)
   
-  return yup.object().shape(schemaMysqlTable).concat(
+  return yup.object().shape(mysqlDatabaseSchema).concat(
     yup.object().shape({
       prefix: yup.string().nullable().required(i18n.t('Username prefix required.'))
         .max(maxLength, i18n.t('Maximum {maxLength} characters.', { maxLength })),
@@ -123,4 +89,20 @@ export const multiple = (props, form, domainName) => {
         .isDateCompare('>', valid_from, 'YYYY-MM-DD', i18n.t('Date must be greater than start date.'))
     })
   )
-}   
+} 
+
+export const csvImport = (props, form) => {
+  const {
+    expiration,
+    valid_from
+  } = form
+
+  return yup.object().shape({
+    actions: schemaActions,
+    valid_from: yup.string().nullable()
+      .isDateCompare('>=', new Date(), 'YYYY-MM-DD', i18n.t('Date must be today or later.'))
+      .isDateCompare('<', expiration, 'YYYY-MM-DD', i18n.t('Date must be less than end date.')),
+    expiration: yup.string().nullable().required(i18n.t('Date required.'))
+      .isDateCompare('>', valid_from, 'YYYY-MM-DD', i18n.t('Date must be greater than start date.'))
+  })
+}
