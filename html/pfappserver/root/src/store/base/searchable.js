5575b491100c65ada631dac0f036aefdf12ecb72
Darren Satkunas <darren@satkunas.com>
2021-03-19 12:03:41 -0400

    (admin) fix searchable GET sort fixes #6090

--- html/pfappserver/root/src/store/base/searchable.js
+++ html/pfappserver/root/src/store/base/searchable.js
@@ -27,7 +27,8 @@ class SearchableApi {
     if (params.fields) {
       params.fields = params.fields.join(',')
     }
-    return apiCall.get(this.url, { headers: this.headers, ...params }).then(response => {
+    const query = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key].toString())}`).join('&')
+    return apiCall.get(`${this.url}?${query}`, { headers: this.headers }).then(response => {
       return response.data
     })
   }
