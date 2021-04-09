6022f187326e9805a275f16a0c96f601cef5a3e2
Darren Satkunas <darren@satkunas.com>
2021-04-09 09:51:23 -0400

    fix(admin(js)): revert decomposition w/ openid_attribute, fixes #6265

--- html/pfappserver/root/src/views/Configuration/advanced/_composables/useResource.js
+++ html/pfappserver/root/src/views/Configuration/advanced/_composables/useResource.js
@@ -8,15 +8,8 @@ const useStore = (props, context, form) => {
   return {
     isLoading: computed(() => $store.getters['$_bases/isLoading']),
     getOptions: () => $store.dispatch('$_bases/optionsAdvanced'),
-    getItem: () => $store.dispatch('$_bases/getAdvanced')
-      .then(response => {
-        const { openid_attributes = '' } = response || {}
-        return { ...response, openid_attributes: openid_attributes.split(',') }
-      }),
-    updateItem: () => {
-      const { openid_attributes = [] } = form.value || {}
-      return $store.dispatch('$_bases/updateAdvanced', { ...form.value, openid_attributes: openid_attributes.join(',') })
-    }
+    getItem: () => $store.dispatch('$_bases/getAdvanced'),
+    updateItem: () => $store.dispatch('$_bases/updateAdvanced', form.value)
   }
 }
 
