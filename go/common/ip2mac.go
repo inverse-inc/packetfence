77c4a3049601da93ea9e21b01ef3132c8f867f8b
Julien Semaan <jul.semaan@gmail.com>
2021-04-22 12:18:05 -0400

    add missing module for ip2mac

---
+++ go/common/ip2mac.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/inverse-inc/packetfence/go/log"
+	"github.com/inverse-inc/packetfence/go/unifiedapiclient"
+)
+
+func IP2MAC(ctx context.Context, ip net.IP) (string, error) {
+	var apiClient = unifiedapiclient.NewFromConfig(context.Background())
+	foundMac := unifiedapiclient.Ip2MacResponse{}
+	err := apiClient.Call(ctx, "GET", "/api/v1/ip4logs/ip2mac/"+ip.String(), &foundMac)
+	if err != nil {
+		msg := fmt.Sprintf("Problem getting the mac for ip '%s': %s", ip, err)
+		log.LoggerWContext(ctx).Error(msg)
+		return "", fmt.Errorf("%s", msg)
+	}
+
+	return foundMac.Mac, nil
+}
