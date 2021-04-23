inverse-inc/packetfence

commit 89a7b918865bb32b83d997713d77fab7d19660c4

warn users about usage of ARP lookup

html/pfappserver/root/src/views/Configuration/fingerbank/generalSettings/_components/TheForm.vue
@@ -95,7 +95,7 @@
 
     <form-group-collector-arp-lookup namespace="collector.arp_lookup"
       :column-label="$i18n.t('ARP lookups by the collector')"
-      :text="$i18n.t(`Whether or not the collector should perform ARP lookups for devices it doesn't have DHCP information.`)"
+      :text="$i18n.t('Whether or not the collector should perform ARP lookups for devices it doesn't have DHCP information. Use only on small deployments.')"
     />
 
     <form-group-collector-network-behavior-analysis namespace="collector.network_behavior_analysis"

commit e000b1079f7db123c59af48ce9e86e1eb704b93f

fix typo caused by 89a7b918865bb32b83d997713d77fab7d19660c4

html/pfappserver/root/src/views/Configuration/fingerbank/generalSettings/_components/TheForm.vue
@@ -95,7 +95,7 @@
 
     <form-group-collector-arp-lookup namespace="collector.arp_lookup"
       :column-label="$i18n.t('ARP lookups by the collector')"
-      :text="$i18n.t('Whether or not the collector should perform ARP lookups for devices it doesn't have DHCP information. Use only on small deployments.')"
+      :text="$i18n.t(`Whether or not the collector should perform ARP lookups for devices it doesn't have DHCP information. Use only on small deployments.`)"
     />
 
     <form-group-collector-network-behavior-analysis namespace="collector.network_behavior_analysis"