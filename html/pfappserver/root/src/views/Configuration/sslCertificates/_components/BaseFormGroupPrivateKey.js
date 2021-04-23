70b50751162b793d2f80934e37d1fdef5876aa09
Darren Satkunas <darren@satkunas.com>
2021-04-23 12:59:32 -0400

    fix(admin(js)): remove mime accept from SSL certificates, fixes #6308

--- html/pfappserver/root/src/views/Configuration/sslCertificates/_components/BaseFormGroupPrivateKey.js
+++ html/pfappserver/root/src/views/Configuration/sslCertificates/_components/BaseFormGroupPrivateKey.js
@@ -4,12 +4,6 @@ import i18n from '@/utils/locale'
 export const props = {
   ...BaseFormGroupTextareaUploadProps,
 
-  // overload :accept default
-  accept: {
-    type: String,
-    default: 'application/x-x509-ca-cert, application/vnd.apple.keynote, text/*'
-  },
-
   // overload :tooltip default
   tooltip: {
     type: String,
