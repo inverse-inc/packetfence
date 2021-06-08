a5fd1ec5eb0b46cb05000218edb0591e47113724
James Rouzier <jrouzier@inverse.ca>
2021-06-08 16:14:03 -0400

    Handle time/bandwidth balance signed/unsigned issues
    Fixes #6391

--- go/cmd/pfacct/radius.go
+++ go/cmd/pfacct/radius.go
@@ -552,7 +552,7 @@ func (rs *RadiusStatements) Setup(db *sql.DB) {
 	}
 
 	rs.softNodeTimeBalanceUpdate, err = db.Prepare(`
-        UPDATE node set time_balance = 0 WHERE tenant_id = ? AND mac = ? AND time_balance - ? <= 0;
+        UPDATE node set time_balance = 0 WHERE tenant_id = ? AND mac = ? AND time_balance <= ?;
     `)
 
 	if err != nil {
@@ -560,7 +560,7 @@ func (rs *RadiusStatements) Setup(db *sql.DB) {
 	}
 
 	rs.softNodeBandwidthBalanceUpdate, err = db.Prepare(`
-        UPDATE node set bandwidth_balance = 0 WHERE tenant_id = ? AND mac = ? AND bandwidth_balance - ? <= 0;
+        UPDATE node set bandwidth_balance = 0 WHERE tenant_id = ? AND mac = ? AND bandwidth_balance <= ?;
     `)
 
 	if err != nil {
@@ -568,7 +568,7 @@ func (rs *RadiusStatements) Setup(db *sql.DB) {
 	}
 
 	rs.nodeTimeBalanceSubtract, err = db.Prepare(`
-        UPDATE node set time_balance = GREATEST(time_balance - ?, 0) WHERE tenant_id = ? AND mac = ? AND time_balance IS NOT NULL;
+        UPDATE node set time_balance = GREATEST(CAST(time_balance AS SIGNED) - ?, 0) WHERE tenant_id = ? AND mac = ? AND time_balance IS NOT NULL;
     `)
 
 	if err != nil {
@@ -576,7 +576,7 @@ func (rs *RadiusStatements) Setup(db *sql.DB) {
 	}
 
 	rs.nodeBandwidthBalanceSubtract, err = db.Prepare(`
-        UPDATE node set bandwidth_balance = GREATEST(bandwidth_balance - ?, 0) WHERE tenant_id = ? AND mac = ? AND bandwidth_balance IS NOT NULL;
+        UPDATE node set bandwidth_balance = GREATEST(CAST(bandwidth_balance AS SIGNED) - ?, 0) WHERE tenant_id = ? AND mac = ? AND bandwidth_balance IS NOT NULL;
     `)
 
 	if err != nil {
