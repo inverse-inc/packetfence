aff8bd1bfe726818c0094305f680b5a5f0f9402c
James Rouzier <jrouzier@inverse.ca>
2021-06-29 10:24:04 -0400

    fix/6410 (#6413)
    * Use FormGroupUsernameAttribute2 for the text field of username_attribute
    
    * fix(admin(js,vue)): adjust tag name
    
    Co-authored-by: Darren Satkunas <darren@satkunas.com>

--- html/pfappserver/root/src/views/Configuration/sources/_components/index.js
+++ html/pfappserver/root/src/views/Configuration/sources/_components/index.js
@@ -137,6 +137,7 @@ export {
   BaseFormGroupInput                        as FormGroupTwilioPhoneNumber,
   BaseFormGroupInput                        as FormGroupUserHeader,
   BaseFormGroupChosenOne                    as FormGroupUsernameAttribute,
+  BaseFormGroupInput                        as FormGroupUsernameAttributeString,
   BaseFormGroupToggleNoYes                  as FormGroupValidateSponsor,
   BaseFormGroupInputNumber                  as FormGroupWriteTimeout,
 
