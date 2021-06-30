4cd44027c547468e6b2425f52279c58fe854ef25
Darren Satkunas <darren@satkunas.com>
2021-06-30 09:30:15 -0400

    adjust indentation

--- html/pfappserver/root/src/views/Auditing/index.vue
+++ html/pfappserver/root/src/views/Auditing/index.vue
@@ -1,12 +1,12 @@
 <template>
-        <b-row>
-            <pf-sidebar v-model="sections"></pf-sidebar>
-            <b-col cols="12" md="9" xl="10" class="py-3">
-                <transition name="slide-bottom">
-                    <router-view></router-view>
-                </transition>
-            </b-col>
-        </b-row>
+  <b-row>
+    <pf-sidebar v-model="sections"></pf-sidebar>
+    <b-col cols="12" md="9" xl="10" class="py-3">
+      <transition name="slide-bottom">
+        <router-view></router-view>
+      </transition>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
