77749e64709e19abe70d680a812685558ce380ac
James Rouzier <jrouzier@inverse.ca>
2021-06-30 08:45:10 -0400

    Update import path

--- addons/upgrade/to-11.0-firewall_sso-conf.pl
+++ addons/upgrade/to-11.0-firewall_sso-conf.pl
@@ -12,7 +12,10 @@ Add the new field tenant_id to Firewall SSO config
 
 use strict;
 use warnings;
-use lib qw(/usr/local/pf/lib);
+use lib qw(
+    /usr/local/pf/lib
+    /usr/local/pf/lib_perl/lib/perl5
+);
 use pf::IniFiles;
 use pf::file_paths qw(
     $firewall_sso_config_file
