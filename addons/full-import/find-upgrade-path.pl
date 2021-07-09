99a7cbaf60f876fad244e560a51f6f2295078a87
Julien Semaan <jul.semaan@gmail.com>
2021-07-09 09:55:57 -0400

    database import + upgrade

--- addons/full-import/find-upgrade-path.pl
+++ addons/full-import/find-upgrade-path.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 use lib '/usr/local/pf/lib_perl/lib';
 
+my $current_version = $ARGV[1];
 
+print "Attempting to find upgrade path from $current_version\n";
