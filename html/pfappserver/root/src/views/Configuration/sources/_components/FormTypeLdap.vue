f6b5e132a34ca7ff475431110955990026372b95
James Rouzier <jrouzier@inverse.ca>
2021-07-14 11:29:12 -0400

    Add tabs

--- html/pfappserver/root/src/views/Configuration/sources/_components/FormTypeLdap.vue
+++ html/pfappserver/root/src/views/Configuration/sources/_components/FormTypeLdap.vue
@@ -5,127 +5,134 @@
     :schema="schema"
     :isLoading="isLoading"
   >
-    <form-group-identifier namespace="id"
-      :column-label="$i18n.t('Name')"
-      :disabled="!isNew && !isClone"
-    />
+    <b-tabs>
+      <base-form-tab :title="$i18n.t('General')" active>
 
-    <form-group-description namespace="description"
-      :column-label="$i18n.t('Description')"
-    />
+        <form-group-identifier namespace="id"
+          :column-label="$i18n.t('Name')"
+          :disabled="!isNew && !isClone"
+        />
 
-    <form-group-host-port-encryption :namespaces="['host', 'port', 'encryption']"
-      :column-label="$i18n.t('Host')"
-    />
+        <form-group-description namespace="description"
+          :column-label="$i18n.t('Description')"
+        />
 
-    <form-group-dead-duration namespace="dead_duration"
-      :column-label="$i18n.t('Dead duration')"
-      :text="$i18n.t('How much time in seconds should a server be marked dead before it is retried. When specifying multiple LDAP servers or a DNS name pointing to multiple IPs, then this option can be used to offer more consistent failover. A value of 0 disables this feature.')"
-    />
+        <form-group-host-port-encryption :namespaces="['host', 'port', 'encryption']"
+          :column-label="$i18n.t('Host')"
+        />
 
-    <form-group-connection-timeout namespace="connection_timeout"
-      :column-label="$i18n.t('Connection timeout')"
-      :text="$i18n.t('LDAP connection Timeout.')"
-    />
+        <form-group-dead-duration namespace="dead_duration"
+          :column-label="$i18n.t('Dead duration')"
+          :text="$i18n.t('How much time in seconds should a server be marked dead before it is retried. When specifying multiple LDAP servers or a DNS name pointing to multiple IPs, then this option can be used to offer more consistent failover. A value of 0 disables this feature.')"
+        />
 
-    <form-group-write-timeout namespace="write_timeout"
-      :column-label="$i18n.t('Request timeout')"
-      :text="$i18n.t('LDAP request timeout.')"
-    />
+        <form-group-connection-timeout namespace="connection_timeout"
+          :column-label="$i18n.t('Connection timeout')"
+          :text="$i18n.t('LDAP connection Timeout.')"
+        />
 
-    <form-group-read-timeout namespace="read_timeout"
-      :column-label="$i18n.t('Response timeout')"
-      :text="$i18n.t('LDAP response timeout.')"
-    />
+        <form-group-write-timeout namespace="write_timeout"
+          :column-label="$i18n.t('Request timeout')"
+          :text="$i18n.t('LDAP request timeout.')"
+        />
 
-    <form-group-base-dn namespace="basedn"
-      :column-label="$i18n.t('Base DN')"
-    />
+        <form-group-read-timeout namespace="read_timeout"
+          :column-label="$i18n.t('Response timeout')"
+          :text="$i18n.t('LDAP response timeout.')"
+        />
 
-    <form-group-scope namespace="scope"
-      :column-label="$i18n.t('Scope')"
-    />
+        <form-group-base-dn namespace="basedn"
+          :column-label="$i18n.t('Base DN')"
+        />
 
-    <form-group-username-attribute namespace="usernameattribute"
-      :column-label="$i18n.t('Username Attribute')"
-      :text="$i18n.t('Main reference attribute that contain the username.')"
-    />
+        <form-group-scope namespace="scope"
+          :column-label="$i18n.t('Scope')"
+        />
 
-    <form-group-search-attributes namespace="searchattributes"
-      :column-label="$i18n.t('Search Attributes')"
-      :text="$i18n.t('Other attributes that can be used as the username (requires to restart the radiusd service to be effective).')"
-    />
+        <form-group-username-attribute namespace="usernameattribute"
+          :column-label="$i18n.t('Username Attribute')"
+          :text="$i18n.t('Main reference attribute that contain the username.')"
+        />
 
-    <form-group-search-attributes-append namespace="append_to_searchattributes"
-      :column-label="$i18n.t('Append search attributes LDAP filter')"
-      :text="$i18n.t('Append this ldap filter to the generated generated ldap filter generated for the search attributes.')"
-    />
+        <form-group-search-attributes namespace="searchattributes"
+          :column-label="$i18n.t('Search Attributes')"
+          :text="$i18n.t('Other attributes that can be used as the username (requires to restart the radiusd service to be effective).')"
+        />
 
-    <form-group-email-attribute namespace="email_attribute"
-      :column-label="$i18n.t('Email Attribute')"
-      :text="$i18n.t('LDAP attribute name that stores the email address against which the filter will match.')"
-    />
+        <form-group-search-attributes-append namespace="append_to_searchattributes"
+          :column-label="$i18n.t('Append search attributes LDAP filter')"
+          :text="$i18n.t('Append this ldap filter to the generated generated ldap filter generated for the search attributes.')"
+        />
 
-    <form-group-bind-dn namespace="binddn"
-      :column-label="$i18n.t('Bind DN')"
-      :text="$i18n.t('Leave this field empty if you want to perform an anonymous bind.')"
-    />
+        <form-group-email-attribute namespace="email_attribute"
+          :column-label="$i18n.t('Email Attribute')"
+          :text="$i18n.t('LDAP attribute name that stores the email address against which the filter will match.')"
+        />
 
-    <form-group-password namespace="password"
-      :column-label="$i18n.t('Password')"
-    />
+        <form-group-bind-dn namespace="binddn"
+          :column-label="$i18n.t('Bind DN')"
+          :text="$i18n.t('Leave this field empty if you want to perform an anonymous bind.')"
+        />
 
-    <form-group-cache-match namespace="cache_match"
-      :column-label="$i18n.t('Cache match')"
-      :text="$i18n.t('Will cache results of matching a rule.')"
-    />
+        <form-group-password namespace="password"
+          :column-label="$i18n.t('Password')"
+        />
 
-    <form-group-monitor namespace="monitor"
-      :column-label="$i18n.t('Monitor')"
-      :text="$i18n.t('Do you want to monitor this source?')"
-    />
+        <form-group-cache-match namespace="cache_match"
+          :column-label="$i18n.t('Cache match')"
+          :text="$i18n.t('Will cache results of matching a rule.')"
+        />
 
-    <form-group-shuffle namespace="shuffle"
-      :column-label="$i18n.t('Shuffle')"
-      :text="$i18n.t('Randomly choose LDAP server to query.')"
-    />
+        <form-group-monitor namespace="monitor"
+          :column-label="$i18n.t('Monitor')"
+          :text="$i18n.t('Do you want to monitor this source?')"
+        />
 
-    <form-group-realms namespace="realms"
-      :column-label="$i18n.t('Associated Realms')"
-      :text="$i18n.t('Realms that will be associated with this source.')"
-    />
+        <form-group-shuffle namespace="shuffle"
+          :column-label="$i18n.t('Shuffle')"
+          :text="$i18n.t('Randomly choose LDAP server to query.')"
+        />
 
-    <form-group-verify namespace="verify"
-      :column-label="$i18n.t('Verify')"
-      :text="$i18n.t('SSL Verify.')"
-    />
+        <form-group-realms namespace="realms"
+          :column-label="$i18n.t('Associated Realms')"
+          :text="$i18n.t('Realms that will be associated with this source.')"
+        />
 
-    <form-group-client-cert namespace="client_cert"
-      :column-label="$i18n.t('Client Certificate')"
-      :text="$i18n.t('Path to the client certificate if a client certificate is required.')"
-    />
+        <form-group-verify namespace="verify"
+          :column-label="$i18n.t('Verify')"
+          :text="$i18n.t('SSL Verify.')"
+        />
 
-    <form-group-client-key namespace="client_key"
-      :column-label="$i18n.t('Client Key')"
-      :text="$i18n.t('Path to the client key if a client certificate is required.')"
-    />
+        <form-group-authentication-rules namespace="authentication_rules"
+          :column-label="$i18n.t('Authentication Rules')"
+        />
 
-    <form-group-ca-file namespace="ca_file"
-      :column-label="$i18n.t('CA File')"
-      :text="$i18n.t('Path to file with additional Certificate Authorities')
-    />
+        <form-group-administration-rules namespace="administration_rules"
+          :column-label="$i18n.t('Administration Rules')"
+        />
+      </base-form-tab>
 
-    <form-group-authentication-rules namespace="authentication_rules"
-      :column-label="$i18n.t('Authentication Rules')"
-    />
+      <base-form-tab :title="$i18n.t('Client Certificate')">
+        <form-group-client-cert namespace="client_cert"
+          :column-label="$i18n.t('Client Certificate')"
+          :text="$i18n.t('Path to the client certificate if a client certificate is required.')"
+        />
 
-    <form-group-administration-rules namespace="administration_rules"
-      :column-label="$i18n.t('Administration Rules')"
-    />
+        <form-group-client-key namespace="client_key"
+          :column-label="$i18n.t('Client Key')"
+          :text="$i18n.t('Path to the client key if a client certificate is required.')"
+        />
+
+        <form-group-ca-file namespace="ca_file"
+          :column-label="$i18n.t('CA File')"
+          :text="$i18n.t('Path to file with additional Certificate Authorities')"
+        />
+      </base-form-tab>
+    </b-tabs>
   </base-form>
 </template>
 <script>
-import { BaseForm } from '@/components/new/'
+import { BaseForm, BaseFormTab} from '@/components/new/'
 import {
   FormGroupAdministrationRules,
   FormGroupAuthenticationRules,
@@ -157,6 +164,7 @@ import {
 
 const components = {
   BaseForm,
+  BaseFormTab,
 
   FormGroupAdministrationRules,
   FormGroupAuthenticationRules,
