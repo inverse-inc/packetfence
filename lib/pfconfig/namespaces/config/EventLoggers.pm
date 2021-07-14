f9c4f910a62a166ab883e9364455b9f755213c5b
James Rouzier <jrouzier@inverse.ca>
2021-07-14 14:39:09 -0400

    Remove unsued perl code

--- lib/pfconfig/namespaces/config/EventLoggers.pm
+++ lib/pfconfig/namespaces/config/EventLoggers.pm
@@ -20,7 +20,6 @@ use warnings;
 use pfconfig::namespaces::config;
 use pf::file_paths qw($event_loggers_config_file);
 use pf::constants::eventLogger;
-use pf::factory::eventlogger;
 
 use base 'pfconfig::namespaces::config';
 
@@ -38,7 +37,7 @@ sub build_child {
         my $ns = $v->{namespaces};
         my @names = defined $ns && $ns ne '' ? split( /\s*,\s*/, $ns ) : @pf::constants::eventLogger::Namespaces;
         for my $n (@names) {
-            push @{$namespaces{$n}}, pf::factory::eventlogger->new( $k, $v );
+            push @{$namespaces{$n}}, {%$v} ;
         }
     }
 
