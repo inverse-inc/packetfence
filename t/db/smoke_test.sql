439315d2a0d1143e8ca2f0413c312d6c19a47a2c
nqb <nquiniou@akamai.com>
2021-07-19 09:01:35 +0200

    [tests] replace REVOKE ALL by DROP USER
    Unit tests failed because REVOKE ALL works only if user already exist

--- t/db/smoke_test.sql
+++ t/db/smoke_test.sql
@@ -1,4 +1,5 @@
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM pf_smoke_tester@'%', pf_smoke_tester@'localhost';
+DROP USER IF EXISTS pf_smoke_tester@'%';
+DROP USER IF EXISTS pf_smoke_tester@'localhost';
 GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'%' IDENTIFIED BY 'packet';
 GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
 GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
