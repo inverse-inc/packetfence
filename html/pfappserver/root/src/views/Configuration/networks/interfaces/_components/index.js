7415131b37d4f7cacf668c16bc168c2e6c394e08
Darren Satkunas <darren@satkunas.com>
2021-08-10 08:44:29 -0400

    fix(admin(js)): high-availability uses INTs [0|1], ref #6334

--- html/pfappserver/root/src/views/Configuration/networks/interfaces/_components/index.js
+++ html/pfappserver/root/src/views/Configuration/networks/interfaces/_components/index.js
@@ -7,31 +7,34 @@ import {
   BaseFormGroupInputNumber,
   BaseFormGroupToggleDisabledEnabled
 } from '@/components/new/'
+import {
+  BaseFormGroupToggleZeroOneIntegerAsOffOn
+} from '@/views/Configuration/_components/new/'
 import TheForm from './TheForm'
 import TheView from './TheView'
 import ToggleStatus from './ToggleStatus'
 
 export {
-  BaseViewCollectionItem              as BaseView,
-  BaseFormButtonBar                   as FormButtonBar,
+  BaseViewCollectionItem                    as BaseView,
+  BaseFormButtonBar                         as FormButtonBar,
 
-  BaseFormGroupChosenMultiple         as FormGroupAdditionalListeneningDaemons,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupCoa,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupDhcpdEnabled,
-  BaseFormGroupInput                  as FormGroupDns,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupHighAvailability,
-  BaseFormGroupInput                  as FormGroupIdentifier,
-  BaseFormGroupInput                  as FormGroupIpAddress,
-  BaseFormGroupInput                  as FormGroupIpv6Address,
-  BaseFormGroupInputNumber            as FormGroupIpv6Prefix,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupNatEnabled,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupNatDns,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupNetflowAccountingEnabled,
-  BaseFormGroupInput                  as FormGroupNetmask,
-  BaseFormGroupInput                  as FormGroupRegNetwork,
-  BaseFormGroupToggleDisabledEnabled  as FormGroupSplitNetwork,
-  BaseFormGroupChosenOne              as FormGroupType,
-  BaseFormGroupInput                  as FormGroupVlan,
+  BaseFormGroupChosenMultiple               as FormGroupAdditionalListeneningDaemons,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupCoa,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupDhcpdEnabled,
+  BaseFormGroupInput                        as FormGroupDns,
+  BaseFormGroupToggleZeroOneIntegerAsOffOn  as FormGroupHighAvailability,
+  BaseFormGroupInput                        as FormGroupIdentifier,
+  BaseFormGroupInput                        as FormGroupIpAddress,
+  BaseFormGroupInput                        as FormGroupIpv6Address,
+  BaseFormGroupInputNumber                  as FormGroupIpv6Prefix,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupNatEnabled,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupNatDns,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupNetflowAccountingEnabled,
+  BaseFormGroupInput                        as FormGroupNetmask,
+  BaseFormGroupInput                        as FormGroupRegNetwork,
+  BaseFormGroupToggleDisabledEnabled        as FormGroupSplitNetwork,
+  BaseFormGroupChosenOne                    as FormGroupType,
+  BaseFormGroupInput                        as FormGroupVlan,
 
   TheForm,
   TheView,
