cd344cde75b5cb93355048fd8f1b06b67952a7c3
nqb <nqb+git@azyx.fr>
2021-08-19 14:14:33 +0200

    fix(admin(js)): use name in dropdown in place of ID

--- html/pfappserver/root/src/views/Configuration/sources/config.js
+++ html/pfappserver/root/src/views/Configuration/sources/config.js
@@ -4,7 +4,7 @@ import { pfActions } from '@/globals/pfActions'
 export const internalTypes = {
   AD:                  i18n.t('Active Directory'),
   Authorization:       i18n.t('Authorization'),
-  AzureAD:             i18n.t('AzureAD'),
+  AzureAD:             i18n.t('Azure AD'),
   EAPTLS:              'EAPTLS',
   EDIR:                'Edirectory',
   Htpasswd:            'Htpasswd',
