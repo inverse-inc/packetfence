7e51a227f05d62c1403706887b9e3365fb92fcb1
Julien Semaan <jul.semaan@gmail.com>
2021-08-31 09:51:54 -0400

    sort backup names when creating export archive

--- addons/full-import/export.sh
+++ addons/full-import/export.sh
@@ -20,14 +20,14 @@ source /usr/local/pf/addons/full-import/database.functions
 
 output="$1"
 
-db_dump=`find /root/backup/ -name 'packetfence-db-dump-*' $mtime | tail -1`
+db_dump=`find /root/backup/ -name 'packetfence-db-dump-*' $mtime | sort -h | tail -1`
 
 if [ -z "$db_dump" ]; then
   echo "Unable to find a database dump that was done in the last 24 hours. Add --force to ignore this."
   exit 1
 fi
 
-files_dump=`find /root/backup/ -name 'packetfence-files-dump-*' $mtime | tail -1`
+files_dump=`find /root/backup/ -name 'packetfence-files-dump-*' $mtime | sort -h | tail -1`
 
 if [ -z "$files_dump" ]; then
   echo "Unable to find a files dump that was done in the last 24 hours. Add --force to ignore this."
