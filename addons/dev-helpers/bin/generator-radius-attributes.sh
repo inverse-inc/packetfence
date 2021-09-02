9cfa3a5d9a96989d1f8eabb2ba232083d05ab7c0
James Rouzier <jrouzier@inverse.ca>
2021-09-02 09:41:08 -0400

    Adjust for newer go version and perl dependencies

--- addons/dev-helpers/bin/generator-radius-attributes.sh
+++ addons/dev-helpers/bin/generator-radius-attributes.sh
@@ -2,7 +2,12 @@
 tmpfile=$(mktemp /tmp/generator-radius-attributes.XXXXXX)
 PF_DIR=/usr/local/pf
 DEV_HELP="$PF_DIR/addons/dev-helpers"
-go run $DEV_HELP/bin/generator-radius-attributes.go | json_xs -tdump > $tmpfile
+PATH="$PATH:$PF_DIR/lib_perl/bin"
+PERL5LIB="$PF_DIR/lib_perl/lib/perl5"
+export PERL5LIB
+export PATH
+
+(cd  $PF_DIR/go/cmd/generator-radius-attributes; go run generator-radius-attributes.go | json_xs -tdump > $tmpfile)
 
 tpage --absolute --define "dump=$tmpfile" --define "year=$(date +"%Y")" \
     --include_path="${DEV_HELP}/templates" "${DEV_HELP}/templates/pf-util-radius_dictionary.pm.tt" \
