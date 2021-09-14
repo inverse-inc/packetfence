fd4ab8222b5aeeb23317d6afb6a4f6af915619c2
James Rouzier <jrouzier@inverse.ca>
2021-09-14 13:50:32 -0400

    Remove long_description

--- lib/pf/Report.pm
+++ lib/pf/Report.pm
@@ -14,8 +14,6 @@ has 'id', (is => 'rw', isa => 'Str');
 
 has 'description', (is => 'rw', isa => 'Str');
 
-has 'long_description', (is => 'rw', isa => 'Str');
-
 has 'group_field', (is => 'rw', isa => 'Str');
 
 has 'order_fields', (is => 'rw', isa => 'ArrayRef[Str]');
