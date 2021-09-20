ef4bea92497a36efed374233b42e5ab6a6ab05d6
Durand Fabrice <oeufdure@gmail.com>
2021-09-20 08:41:14 -0400

    Fixed qrcode

--- html/common/qrcode.js
+++ html/common/qrcode.js
@@ -1,4 +1,7 @@
 /* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 $(function(){
-    jQuery('#qrcode').qrcode("otpauth://totp/[% username %].packetfence?secret=[% otp %]");
+    const el = jQuery('#qrcode')
+    const otp = el.attr('data-otp')
+    const username = el.attr('data-username')
+    el.qrcode(`otpauth://totp/${username}.packetfence?secret=${otp}`);
 });
