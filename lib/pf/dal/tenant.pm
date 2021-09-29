3b50d4909519b275f8ed990b85d6b1fbd303dfe9
Durand Fabrice <oeufdure@gmail.com>
2021-09-29 14:58:57 -0400

    On tenant create use the correct virtual_server

--- lib/pf/dal/tenant.pm
+++ lib/pf/dal/tenant.pm
@@ -36,6 +36,8 @@ sub after_create_hook {
 
     my $cs = pf::ConfigStore::Radiusd::EAPProfile->new();
     my $default = pf::ConfigStore::Radiusd::EAPProfile->new->read("default");
+    $default->{peap_virtual_server} = "packetfence-".$self->{name};
+    $default->{ttls_virtual_server} = "packetfence-".$self->{name};
     $cs->update_or_create("default-".$self->{name}, $default);
     $cs->commit();
 }
