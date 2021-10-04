fdb932a198dedd8d7c69d88341c6ba4cb3a2bb6d
James Rouzier <jrouzier@inverse.ca>
2021-10-04 08:14:07 -0400

    Add new test file for floating_network_device.conf

--- t/test_paths.pm
+++ t/test_paths.pm
@@ -63,6 +63,7 @@ BEGIN {
     $pf::file_paths::radius_server_cert = catfile($test_dir,'data/radius_server.crt');
     $pf::file_paths::radius_server_key = catfile($test_dir,'data/radius_server.key');
     $pf::file_paths::radius_ca_cert = catfile($test_dir,'data/radius_ca.pem');
+    $pf::file_paths::floating_devices_config_file = catfile($test_dir, 'data/floating_network_device.conf');
 
     $pfconfig::constants::CONFIG_FILE_PATH = catfile($test_paths::test_dir, 'data/pfconfig.conf');
     $pfconfig::constants::SOCKET_PATH = "/usr/local/pf/var/run/pfconfig-test.sock";
