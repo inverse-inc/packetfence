a5c320e5f26f45d9ad7b253d2e6942c303b92cf4
nqb <nqb+git@azyx.fr>
2021-10-14 10:46:28 +0200

    comment related to issue

--- t/venom/venom-wrapper.sh
+++ t/venom/venom-wrapper.sh
@@ -26,6 +26,7 @@ configure_and_check() {
     # paths
     VENOM_ROOT_DIR=$(readlink -e $(dirname ${BASH_SOURCE[0]}))
     VENOM_BINARY="${VENOM_BINARY:-`which venom`}"
+    # temp, overcome tap issue: https://github.com/ovh/venom/issues/428
     VENOM_COMMON_FLAGS="${VENOM_COMMON_FLAGS:---format=tap}"
     VENOM_EXIT_FLAGS="${VENOM_EXIT_FLAGS:-}"
 
