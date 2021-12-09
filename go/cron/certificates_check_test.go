3fc7dd25aba87574be88b6b67f288c40dd5ead21
James Rouzier <jrouzier@inverse.ca>
2021-12-09 11:17:24 -0500

    Add TestSendEmail

--- go/cron/certificates_check_test.go
+++ go/cron/certificates_check_test.go
@@ -1,6 +1,18 @@
 package maint
 
 import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
 	"testing"
 	"time"
 )
@@ -189,4 +201,96 @@ AIU+2GKjyT3iMuzZxxFxPFMCAwEAAQ==
 		t.Error("Certificate data found")
 	}
 
+}
+
+func publicKey(priv interface{}) interface{} {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	default:
+		return nil
+	}
+}
+
+func pemBlockForKey(priv interface{}) *pem.Block {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
+	case *ecdsa.PrivateKey:
+		b, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+			os.Exit(2)
+		}
+		return &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	default:
+		return nil
+	}
+}
+
+func TestSendEmail(t *testing.T) {
+
+	// priv, err := rsa.GenerateKey(rand.Reader, *rsaBits)
+	priv, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	now := time.Now()
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Acme Co"},
+		},
+		NotBefore: now.Add(time.Hour * -2),
+		NotAfter:  now.Add(time.Hour * -1),
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	/*
+	   hosts := strings.Split(*host, ",")
+	   for _, h := range hosts {
+	   	if ip := net.ParseIP(h); ip != nil {
+	   		template.IPAddresses = append(template.IPAddresses, ip)
+	   	} else {
+	   		template.DNSNames = append(template.DNSNames, h)
+	   	}
+	   }
+
+	   if *isCA {
+	   	template.IsCA = true
+	   	template.KeyUsage |= x509.KeyUsageCertSign
+	   }
+	*/
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
+	if err != nil {
+		log.Fatalf("Failed to create certificate: %s", err)
+	}
+	out := &bytes.Buffer{}
+	pem.Encode(out, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	f, err := os.CreateTemp("", "test*.crt")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	name := f.Name()
+	defer os.Remove(name)
+	_, err = f.Write(out.Bytes())
+	f.Sync()
+	f.Close()
+	c := &CertificatesCheck{}
+	c.VerifyCertFiles([]string{name}, time.Now())
+	/*
+			err = c.VerifyCertFiles("", out.Bytes(), time.Now())
+			if err == nil {
+				t.("Certificate expired")
+			} else {
+		        fmt.Printf("%s\n", err.Error())
+		    }
+	*/
 }
