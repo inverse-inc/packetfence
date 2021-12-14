33860caba7beb5a6dcfbbce7f9530b16e9213508
James Rouzier <jrouzier@inverse.ca>
2021-12-14 11:08:13 -0500

    Add scripts to find issues to find with perl modules

---
+++ t/find_unused_variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PFDIR=/usr/local/pf/
+export PERL5LIB=$PFDIR/lib_perl/lib/perl5 POLICY=Perl::Critic::Policy::Variables::ProhibitUnusedVariables
+
+find $PFDIR/lib/{pf,pfconfig,captiveportal,pfappserver} -iname '*.pm' | xargs perlcritic -l --single-policy $POLICY | xargs perlcritic --single-policy $POLICY
