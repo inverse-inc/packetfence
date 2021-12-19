92fb5c84de540cded87cc2ef6b44c71a60c889a6
nqb <nquiniou@akamai.com>
2021-12-19 10:28:44 +0100

    check psono vars for wireless01 and nodes
    change logic for secrets removal

--- t/venom/utils/sanitize-venom-logs.sh
+++ t/venom/utils/sanitize-venom-logs.sh
@@ -10,17 +10,10 @@ venom_result_dir=${venom_root}/results
 venom_result_archive=${venom_root}/results.tar.gz
 venom_local_vars_file=${venom_root}/vars/local.yml
 
-# https://unix.stackexchange.com/a/680002
-# In the POSIX aka C locale, the POSIX [:punct:] character class contains the characters in the following set:
-# !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-secret_is_valid () {
-    # used a sed delimiter, should be avoid
-    local avoid='[~]'
-    [[ $1 =~ [[:upper:]] ]] &&
-    [[ $1 =~ [[:lower:]] ]] &&
-    [[ $1 =~ [[:digit:]] ]] &&
-    [[ $1 =~ [[:punct:]] ]] &&
-    [[ ! $1 =~ $avoid ]]
+# https://stackoverflow.com/a/2705678
+escape_secret () {
+    local secret=$1
+    printf '%s\n' "$secret" | sed -e 's/[]\/$*.^[]/\\&/g'
 }
 
 # to simplify export of logs later
@@ -29,15 +22,29 @@ create_archive() {
     tar c -zf ${venom_result_archive} $result_dir
 }
 
-# get list of secret_id in local.yml file
-for secret_id in $(grep secret_id ${venom_local_vars_file} | awk -F ':' '{print $2}'); do
-    # get real secret
-    secret=$(psonoci secret get ${secret_id} password)
-    secret_is_valid $secret
+check_psono_vars() {
+    if [ -n "${PSONO_CI_API_KEY_ID}" ]; then
+        echo "Psono variables detected in environment"
+    else
+        echo "No Psono variables in environment"
+    fi
+}
 
-    # replace secret in results **files**
-    # we used ~ as sed delimiter, this character is not allowed in secrets
-    find ${venom_result_dir} -type f -print0 | xargs -0 sed --silent -i " s~${secret}~REDACTED~g" {} \;
-done
+remove_secrets() {
+    # get list of secret_id in local.yml file
+    for secret_id in $(grep secret_id ${venom_local_vars_file} | awk -F ':' '{print $2}'); do
+        # get real secret
+        secret=$(psonoci secret get ${secret_id} password)
+        escaped_secret=$(escape_secret $secret)
+        # replace secret in results **files**
+        find ${venom_result_dir} -type f -print0 | xargs -0 sed --silent -i "s/${escaped_secret}/REDACTED/g"
+    done
+}
 
+# If Psono variables are defined in environment, we can get secrets and we will need to remove it
+if check_psono_vars; then
+    remove_secrets
+else
+    echo "No secrets to remove"
+fi
 create_archive ${venom_result_dir}
