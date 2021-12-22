3b5eaf7507ab2b49abc90915d226a307819a6824
nqb <nquiniou@akamai.com>
2021-12-22 07:08:57 +0100

    sort test suites when argument is a directory
    only take YAML files at first level
    
    take symlinks

--- t/venom/venom-wrapper.sh
+++ t/venom/venom-wrapper.sh
@@ -18,7 +18,8 @@ log_subsection() {
 # script usage definition
 usage() { 
     echo "Usage: $(basename $0) <path_to_test_suite1> [<path_to_test_suite2>]"
-    echo "If you want to run all test suites in subdirectories, use <path_to_test_suite_dir/*>"
+    echo "A test suite can be a directory or a YAML file"
+    echo "When a directory is passed, this script will sort alphabetically all test suites files in it"
 } 
 
 
@@ -26,7 +27,6 @@ configure_and_check() {
     # paths
     VENOM_ROOT_DIR=$(readlink -e $(dirname ${BASH_SOURCE[0]}))
     VENOM_BINARY="${VENOM_BINARY:-`which venom`}"
-    # temp, overcome tap issue: https://github.com/ovh/venom/issues/428
     VENOM_COMMON_FLAGS="${VENOM_COMMON_FLAGS:-}"
     VENOM_EXIT_FLAGS="${VENOM_EXIT_FLAGS:-}"
 
@@ -43,7 +43,20 @@ die_on_error() {
 run_test_suites() {
     local test_suites=$(readlink -e ${@:-.})
     log_section "Running Venom test suite ${test_suites}"
-    CMD="${VENOM_BINARY} run ${VENOM_COMMON_FLAGS} ${VENOM_EXIT_FLAGS} ${test_suites}"
+
+    # empty array
+    test_suites_files=()
+    for test_suite in ${test_suites}; do
+        # if it's a directory, we sort alphabetically files in it
+        if [ -d "${test_suite}" ]; then
+            sorted_files=$(find ${test_suite} -maxdepth 1 -type f,l -name "*.yml" | sort)
+            test_suites_files+=($sorted_files)
+        else
+            test_suites_files+=($test_suite)
+        fi
+    done
+
+    CMD="${VENOM_BINARY} run ${VENOM_COMMON_FLAGS} ${VENOM_EXIT_FLAGS} ${test_suites_files[@]}"
     ${CMD} || die_on_error
 }
 
