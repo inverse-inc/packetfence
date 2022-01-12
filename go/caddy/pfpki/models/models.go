7e077a29e53899ec49a15448238a83446cb8f257
Durand Fabrice <oeufdure@gmail.com>
2022-01-12 14:00:41 -0500

    Updated sql schema to use datetime instead of timestamp

--- go/caddy/pfpki/models/models.go
+++ go/caddy/pfpki/models/models.go
@@ -1074,7 +1074,6 @@ func (c Cert) New() (types.Info, error) {
 	}
 	NotAfter := time.Now().AddDate(0, 0, prof.Validity)
 
-	c.ValidUntil = NotAfter
 	// Prepare certificate
 	cert := &x509.Certificate{
 		SerialNumber:       SerialNumber,
@@ -1116,7 +1115,7 @@ func (c Cert) New() (types.Info, error) {
 	// Public key
 	pem.Encode(certBuff, &pem.Block{Type: "CERTIFICATE", Bytes: certByte})
 
-	if err := c.DB.Create(&Cert{Cn: c.Cn, Ca: ca, CaName: ca.Cn, ProfileName: prof.Name, SerialNumber: SerialNumber.String(), DNSNames: c.DNSNames, IPAddresses: c.IPAddresses, Mail: c.Mail, StreetAddress: StreetAddress, Organisation: Organization, OrganisationalUnit: OrganizationalUnit, Country: Country, State: Province, Locality: Locality, PostalCode: PostalCode, Profile: prof, Key: keyOut.String(), Cert: certBuff.String(), ValidUntil: c.ValidUntil}).Error; err != nil {
+	if err := c.DB.Create(&Cert{Cn: c.Cn, Ca: ca, CaName: ca.Cn, ProfileName: prof.Name, SerialNumber: SerialNumber.String(), DNSNames: c.DNSNames, IPAddresses: c.IPAddresses, Mail: c.Mail, StreetAddress: StreetAddress, Organisation: Organization, OrganisationalUnit: OrganizationalUnit, Country: Country, State: Province, Locality: Locality, PostalCode: PostalCode, Profile: prof, Key: keyOut.String(), Cert: certBuff.String(), ValidUntil: cert.NotAfter}).Error; err != nil {
 		Information.Error = err.Error()
 		return Information, errors.New(dbError)
 	}
