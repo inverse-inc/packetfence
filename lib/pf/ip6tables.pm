1e34ac1b284c180d05eb80d50e59b9c73bf61ce7
Julien Semaan <jul.semaan@gmail.com>
2022-01-19 11:26:16 -0500

    initial version of the ip6tables service

---
+++ lib/pf/ip6tables.pm
@@ -0,0 +1,109 @@
+package pf::ip6tables;
+
+=head1 NAME
+
+pf::ip6tables - module for ip6tables rules management.
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::ip6tables contains the functions necessary to manipulate the ip6tables service
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+F<pf.conf> configuration file and ip6tables template F<ip6tables.conf>.
+
+=cut
+
+use strict;
+use warnings;
+
+use pf::log;
+use Readonly;
+use pf::constants;
+use File::Slurp qw(read_file);
+use pf::util;
+
+use pf::config qw(
+    $management_network
+    @portal_ints
+);
+use pf::file_paths qw($generated_conf_dir $conf_dir);
+
+# This is the content that needs to match in the iptable rules for the service
+# to be considered as running
+Readonly our $FW_FILTER_INPUT_MGMT      => 'input-v6-management-if';
+Readonly our $FW_FILTER_INPUT_PORTAL    => 'input-v6-portal-if';
+
+sub generate {
+    my ($class) = @_;
+    my $logger = get_logger();
+    my %tags;
+    $tags{'input_management_include'} = "#BEGIN include ip6tables-input-management.conf.inc\n" . read_file("$conf_dir/ip6tables-input-management.conf.inc") . "#END include ip6tables-input-management.conf.inc\n";
+
+    $tags{management_chain} = <<EOF;
+-N $FW_FILTER_INPUT_MGMT
+-A INPUT -i $management_network->{Tint} -j $FW_FILTER_INPUT_MGMT
+EOF
+
+    $tags{portal_chain} = "-N $FW_FILTER_INPUT_PORTAL\n";
+    foreach my $portal_interface ( @portal_ints ) {
+        $tags{portal_chain} .= <<EOF;
+-A INPUT -i $portal_interface->{Tint} -j $FW_FILTER_INPUT_PORTAL
+EOF
+    }
+
+    parse_template( \%tags, "$conf_dir/ip6tables.conf", "$generated_conf_dir/ip6tables.conf" );
+    $class->restore("$generated_conf_dir/ip6tables.conf");
+}
+
+
+sub save {
+    my ($class, $save_file) = @_;
+    my $logger = get_logger();
+    $logger->info( "saving existing ip6tables to " . $save_file );
+    pf_run("/usr/sbin/ip6tables-save -t nat > $save_file");
+    pf_run("/usr/sbin/ip6tables-save -t mangle >> $save_file");
+    pf_run("/usr/sbin/ip6tables-save -t filter >> $save_file");
+}
+
+sub restore {
+    my ($class, $restore_file) = @_;
+    my $logger = get_logger();
+    if ( -r $restore_file ) {
+        $logger->info( "restoring ip6tables from " . $restore_file );
+        pf_run("/usr/sbin/ip6tables-restore < $restore_file");
+    }
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2021 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
+=cut
+
+1;
---
+++ lib/pf/services/manager/ip6tables.pm
@@ -0,0 +1,167 @@
+package pf::services::manager::ip6tables;
+
+=head1 NAME
+
+pf::services::manager::ip6tables add documentation
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::services::manager::ip6tables
+
+=cut
+
+use strict;
+use warnings;
+use Moo;
+use pf::file_paths qw($install_dir);
+use pf::log;
+use pf::util;
+use pf::ip6tables;
+use pf::config qw(%Config);
+
+extends 'pf::services::manager';
+
+has '+name' => (default => sub { 'ip6tables' } );
+
+has '+shouldCheckup' => ( default => sub { 1 }  );
+
+has 'runningServices' => (is => 'rw', default => sub { 0 } );
+
+
+=head2 start
+
+start ip6tables
+
+=cut
+
+sub startService {
+    my ($self) = @_;
+    unless ($self->isAlive()) {
+        pf::ip6tables->save($install_dir . '/var/ip6tables.bak');
+    }
+    pf::ip6tables->generate();
+    return 1;
+}
+
+=head2
+
+generateConfig
+
+=cut
+
+sub generateConfig {
+    my $technique;
+    pf::ip6tables->generate();
+    return 1;
+}
+
+=head2 start
+
+Wrapper around systemctl. systemctl should in turn call the actuall _start.
+
+=cut
+
+sub start {
+    my ($self,$quick) = @_;
+    system('sudo systemctl start packetfence-ip6tables');
+    return $? == 0;
+}
+
+=head2 _start
+
+start the service (called from systemd)
+
+=cut
+
+sub _start {
+    my ($self) = @_;
+    my $result = 0;
+    unless ( $self->isAlive() ) {
+        $result = $self->startService();
+    }
+    return $result;
+}
+
+sub startAndCheck {
+    my ($self) = @_;
+
+    while(1) {
+        $self->_start() unless($self->isAlive());
+        sleep 60;
+    }
+}
+
+=head2 stop
+
+Wrapper around systemctl. systemctl should in turn call the actual _stop.
+
+=cut
+
+sub stop {
+    my ($self) = @_;
+    system('sudo systemctl stop packetfence-ip6tables');
+    return 1;
+}
+
+=head2 _stop
+
+stop ip6tables (called from systemd)
+
+=cut
+
+sub _stop {
+    my ($self) = @_;
+    my $logger = get_logger();
+    pf::ip6tables->restore( $install_dir . '/var/ip6tables.bak' );
+    return 1;
+}
+
+=head2 isAlive
+
+Check if ip6tables is alive.
+Since it's never really stopped then we check if the fake PID exists
+
+=cut
+
+sub isAlive {
+    my ($self) = @_;
+    my $logger = get_logger();
+    my $result;
+    my $pid = $self->pid;
+    my $_EXIT_CODE_EXISTS = "0";
+    my $rules_applied = defined( pf_run( "sudo " . $Config{'services'}{"ip6tables_binary"} . " -S | grep " . $pf::ip6tables::FW_FILTER_INPUT_MGMT ,accepted_exit_status => [$_EXIT_CODE_EXISTS]) );
+    return ($pid && $rules_applied) ? 1 : 0;
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2021 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
