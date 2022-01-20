a768822d6ef05b25ac3544cd25eaf1b335b83130
dsatkunas <_dsatkunas@runner2.inverse>
2022-01-20 10:26:40 -0500

    improve logging, add DELETE route

--- t/mock_servers/node_radius/src/api.js
+++ t/mock_servers/node_radius/src/api.js
@@ -2,10 +2,24 @@ const db = require('node-persist')
 const express = require('express')
 const api = express()
 
-api.get('/history', async (req, res) => {
+const logRequest = (req, res, next) => {
+  const { body, cookies, headers, hostname, ip, method, originalUrl, params, path, protocol, query, secure, subdomains, xhr } = req
+  console.debug({ body, cookies, headers, hostname, ip, method, originalUrl, params, path, protocol, query, secure, subdomains, xhr })
+  next()
+}
+
+api.get('/history', logRequest, async (req, res) => {
   Promise.resolve(db.init()).then(() => {
     db.getItem('history').then(history => {
       res.send(JSON.stringify(history, null, 2))
+    })
+  })
+})
+
+api.delete('/history', logRequest, async (req, res) => {
+  Promise.resolve(db.init()).then(() => {
+    db.setItem('history', []).then(() => {
+      res.send(JSON.stringify([], null, 2))
     })
   })
 })
