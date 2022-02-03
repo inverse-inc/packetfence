55cafe57520e7172a62f2f671fccdf93a4df516c
James Rouzier <jrouzier@inverse.ca>
2022-02-03 09:30:08 -0500

    Shorten the window from 2 hours to 5 minutes

--- go/cron/bandwidth_maintenance.go
+++ go/cron/bandwidth_maintenance.go
@@ -56,10 +56,10 @@ func (j *BandwidthMaintenance) Run() {
 	j.BandwidthMaintenanceSessionCleanup(ctx)
 	j.ProcessBandwidthAccountingNetflow(ctx)
 	j.TriggerBandwidth(ctx)
-	// j.BandwidthAggregation(ctx, "hourly", "DATE_SUB(NOW(), INTERVAL ? HOUR)", 2)
+	j.BandwidthAccountingRadiusToHistory(ctx)
+	//j.BandwidthAggregation(ctx, "hourly", "DATE_SUB(NOW(), INTERVAL ? HOUR)", 2)
 	j.BandwidthAggregation(ctx, "daily", "DATE_SUB(NOW(), INTERVAL ? DAY)", 2)
 	j.BandwidthAggregation(ctx, "monthly", "DATE_SUB(NOW(), INTERVAL ? MONTH)", 1)
-	j.BandwidthAccountingRadiusToHistory(ctx)
 	j.BandwidthHistoryAggregation(ctx, "daily", "SUBDATE(NOW(), INTERVAL ? DAY)", 1)
 	j.BandwidthHistoryAggregation(ctx, "monthly", "SUBDATE(NOW(), INTERVAL ? MONTH)", 1)
 	j.BandwidthAccountingHistoryCleanup(ctx)
@@ -109,14 +109,17 @@ func (j *BandwidthMaintenance) BandwidthAggregation(ctx context.Context, roundin
 	)
 }
 
+const BandwidthAccountingRadiusToHistoryWindow = 5 * 60;
+
 func (j *BandwidthMaintenance) BandwidthAccountingRadiusToHistory(ctx context.Context) {
-	sql := "CALL bandwidth_accounting_radius_to_history(DATE_SUB(NOW(), INTERVAL ? SECOND), ?);"
+	sql := "CALL bandwidth_accounting_radius_to_history(DATE_SUB(?, INTERVAL ? SECOND), ?);"
 	BatchSqlCount(
 		ctx,
 		"bandwidth_accounting_radius_to_history",
 		j.Timeout,
 		sql,
-		j.Window,
+		time.Now(),
+		BandwidthAccountingRadiusToHistoryWindow,
 		j.Batch,
 	)
 }
--- go/cron/bandwidth_maintenance_test.go
+++ go/cron/bandwidth_maintenance_test.go
@@ -55,7 +55,7 @@ FROM macs JOIN dates;
 		0,
 		[]sqlCountTest{
 			{
-				name:          "bandwidth just keep the last 2hrs",
+				name:          "bandwidth just keep the last 5 minutes",
 				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
 				expectedCount: 20,
 			},
@@ -139,9 +139,9 @@ FROM macs JOIN dates;
 		0,
 		[]sqlCountTest{
 			{
-				name:          "bandwidth just keep the last 2hrs",
+				name:          "bandwidth just keep the last 5 minutes",
 				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
-				expectedCount: 180,
+				expectedCount: 20,
 			},
 			{
 				name:          "bandwidth was kept in bandwidth_accounting",
@@ -163,6 +163,277 @@ FROM macs JOIN dates;
                         SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting GROUP BY node_id
                     ) as y GROUP BY node_id
                 ) as x WHERE in_bytes = 9600`,
+				expectedCount: 20,
+			},
+		},
+		[]string{},
+	)
+}
+
+func TestBandwidthMaintenanceAggregationZeroIn(t *testing.T) {
+	testWindowSqlCleanup(
+		t,
+		"bandwidth_maintenance",
+		map[string]interface{}{
+			"timeout": 60.0,
+		},
+		[]string{
+			"TRUNCATE bandwidth_accounting",
+			"TRUNCATE bandwidth_accounting_history",
+			`
+INSERT INTO bandwidth_accounting (
+        tenant_id,
+        node_id,
+        unique_session_id,
+        mac,
+        time_bucket,
+        in_bytes,
+        out_bytes,
+        last_updated,
+        source_type
+) WITH macs AS (
+    SELECT
+        (1 << 48 | seq) as node_id,
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(seq & 255), 2, '0')
+        )) AS mac FROM seq_1_to_20
+), dates AS (
+    SELECT seq as session_id, DATE_SUB(NOW(), INTERVAL seq * 15 MINUTE ) as time_bucket from seq_0_to_95
+)
+SELECT
+    1 AS tenant_id,
+    node_id,
+    session_id,
+    mac,
+    time_bucket,
+    0 in_bytes,
+    100 out_bytes,
+    '0000-00-00 00:00:00' as last_updated,
+    'radius' as source_type
+FROM macs JOIN dates;
+`,
+		},
+		0,
+		[]sqlCountTest{
+			{
+				name:          "bandwidth just keep the last 5 minutes",
+				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was kept in bandwidth_accounting",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was moved to the history table",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting_history`,
+				expectedCount: 20,
+			},
+			{
+				name: "bandwidth merged in bytes",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(in_bytes) as in_bytes, node_id FROM (
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE in_bytes = 0`,
+				expectedCount: 20,
+			},
+			{
+				name: "bandwidth merged out bytes",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(out_bytes) as out_bytes, node_id FROM (
+                        SELECT node_id, SUM(out_bytes) as out_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(out_bytes) as out_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE out_bytes = 9600`,
+				expectedCount: 20,
+			},
+		},
+		[]string{},
+	)
+}
+
+func TestBandwidthMaintenanceAggregationZeroOut(t *testing.T) {
+	testWindowSqlCleanup(
+		t,
+		"bandwidth_maintenance",
+		map[string]interface{}{
+			"timeout": 60.0,
+		},
+		[]string{
+			"TRUNCATE bandwidth_accounting",
+			"TRUNCATE bandwidth_accounting_history",
+			`
+INSERT INTO bandwidth_accounting (
+        tenant_id,
+        node_id,
+        unique_session_id,
+        mac,
+        time_bucket,
+        in_bytes,
+        out_bytes,
+        last_updated,
+        source_type
+) WITH macs AS (
+    SELECT
+        (1 << 48 | seq) as node_id,
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(seq & 255), 2, '0')
+        )) AS mac FROM seq_1_to_20
+), dates AS (
+    SELECT seq as session_id, DATE_SUB(NOW(), INTERVAL seq * 15 MINUTE ) as time_bucket from seq_0_to_95
+)
+SELECT
+    1 AS tenant_id,
+    node_id,
+    session_id,
+    mac,
+    time_bucket,
+    100 in_bytes,
+    0 out_bytes,
+    '0000-00-00 00:00:00' as last_updated,
+    'radius' as source_type
+FROM macs JOIN dates;
+`,
+		},
+		0,
+		[]sqlCountTest{
+			{
+				name:          "bandwidth just keep the last 5 minutes",
+				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was kept in bandwidth_accounting",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was moved to the history table",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting_history`,
+				expectedCount: 20,
+			},
+			{
+				name: "Zero out bytes bandwidth merged in bytes",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(in_bytes) as in_bytes, node_id FROM (
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE in_bytes = 9600`,
+				expectedCount: 20,
+			},
+			{
+				name: "Zero out bytes bandwidth merged out bytes",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(out_bytes) as out_bytes, node_id FROM (
+                        SELECT node_id, SUM(out_bytes) as out_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(out_bytes) as out_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE out_bytes = 0`,
+				expectedCount: 20,
+			},
+		},
+		[]string{},
+	)
+}
+
+func TestBandwidthMaintenanceAggregationZero(t *testing.T) {
+	testWindowSqlCleanup(
+		t,
+		"bandwidth_maintenance",
+		nil,
+		[]string{
+			"TRUNCATE bandwidth_accounting",
+			"TRUNCATE bandwidth_accounting_history",
+			`
+INSERT INTO bandwidth_accounting (
+        tenant_id,
+        node_id,
+        unique_session_id,
+        mac,
+        time_bucket,
+        in_bytes,
+        out_bytes,
+        last_updated,
+        source_type
+) WITH macs AS (
+    SELECT
+        (1 << 48 | seq) as node_id,
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(seq & 255), 2, '0')
+        )) AS mac FROM seq_1_to_20
+), dates AS (
+    SELECT seq as session_id, DATE_SUB(NOW(), INTERVAL seq * 15 MINUTE ) as time_bucket from seq_0_to_95
+)
+SELECT
+    1 AS tenant_id,
+    node_id,
+    session_id,
+    mac,
+    time_bucket,
+    0 in_bytes,
+    0 out_bytes,
+    '0000-00-00 00:00:00' as last_updated,
+    'radius' as source_type
+FROM macs JOIN dates;
+`,
+		},
+		0,
+		[]sqlCountTest{
+			{
+				name:          "bandwidth just keep the last 5 minutes",
+				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was kept in bandwidth_accounting",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "bandwidth was moved to the history table",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting_history`,
+				expectedCount: 0,
+			},
+			{
+				name: "bandwidth merged",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(in_bytes) as in_bytes, node_id FROM (
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE in_bytes = 0`,
 				expectedCount: 20,
 			},
 		},
