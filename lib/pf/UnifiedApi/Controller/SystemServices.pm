aaa6d8a0e01eeae24672b531e0fd17a65150b487
James Rouzier <jrouzier@inverse.ca>
2022-02-07 16:19:48 -0500

    Add async for start, restart, stop, enable, disable

--- lib/pf/UnifiedApi/Controller/SystemServices.pm
+++ lib/pf/UnifiedApi/Controller/SystemServices.pm
@@ -16,6 +16,9 @@ use strict;
 use warnings;
 use Mojo::Base 'pf::UnifiedApi::Controller::RestRoute';
 use pf::util;
+use pf::error qw(is_error);
+use pf::pfqueue::status_updater::redis;
+use pf::util::pfqueue qw(consumer_redis_client);
 
 sub resource {
     return 1;
@@ -42,34 +45,80 @@ sub status {
     return $self->render(json => {message => ($pid ? "Service is running" : "Service is not running"), pid => $pid+0}, status => ($pid ? 200 : 500));
 }
 
+sub do_action {
+    my ($self, @args) = @_;
+
+    my ($status, $data) = $self->get_data();
+    if (is_error($status)) {
+        return $self->render(json => $data, status => $status);
+    }
+
+    if ($data->{async}) {
+        my $task_id = $self->task_id;
+        my $subprocess = Mojo::IOLoop->subprocess;
+        $subprocess->run(
+            sub {
+                my ($subprocess) = @_;
+                my $updater = pf::pfqueue::status_updater::redis->new( connection => consumer_redis_client(), task_id => $task_id );
+                $updater->start;
+                my $data = $self->do_systemctl_action(@args);
+                $updater->completed($data);
+            },
+            sub {},
+        );
+
+        return $self->render( json => {status => 202, task_id => $task_id }, status => 202);
+    }
+
+    my $results = $self->do_systemctl_action(@args);
+    return $self->render(json => $results, status => $results->{status});
+}
+
 sub start {
     my ($self) = @_;
-    my $return = systemctl("start", $self->param('system_service_id'));
-    return $self->render(json => {message => ($return ? "Service couldn't be started" : "Service has been started")}, status => ($return ? 500 : 200));
+    $self->do_action('start', 'started');
 }
 
 sub stop {
     my ($self) = @_;
-    my $return = systemctl("stop", $self->param('system_service_id'));
-    return $self->render(json => {message => ($return ? "Service couldn't be stopped" : "Service has been stopped")}, status => ($return ? 500 : 200));
+    $self->do_action('stop', 'stopped');
 }
 
 sub restart {
     my ($self) = @_;
-    my $return = systemctl("restart", $self->param('system_service_id'));
-    return $self->render(json => {message => ($return ? "Service couldn't be restarted" : "Service has been restarted")}, status => ($return ? 500 : 200));
+    $self->do_action('restart', 'restarted');
 }
 
 sub enable {
     my ($self) = @_;
-    my $return = systemctl("enable", $self->param('system_service_id'));
-    return $self->render(json => {message => ($return ? "Service couldn't be enabled" : "Service has been enabled")}, status => ($return ? 500 : 200));
+    $self->do_action('enable', 'enabled');
 }
 
 sub disable {
     my ($self) = @_;
-    my $return = systemctl("enable", $self->param('system_service_id'));
-    return $self->render(json => {message => ($return ? "Service couldn't be disabled" : "Service has been disabled")}, status => ($return ? 500 : 200));
+    $self->do_action('disable', 'disabled');
+}
+
+sub do_systemctl_action {
+    my ($self, $action, $info) = @_;
+    my $return = systemctl($action, $self->param('system_service_id'));
+    return {message => ($return ? "Service couldn't be $info" : "Service has been $info"), status => ($return ? 500 : 200)};
+}
+
+sub do_start {
+    my ($self, $action, $info) = @_;
+    my $return = systemctl($action, $self->param('system_service_id'));
+    return {message => ($return ? "Service couldn't be $info" : "Service has been $info"), status => ($return ? 500 : 200)};
+}
+
+sub get_data {
+    my ($self) = @_;
+    my $body = $self->req->body();
+    if (!defined $body || $body eq '') {
+        return 200, {}
+    }
+
+    return $self->parse_json($body);
 }
 
 =head1 AUTHOR
