69e00ba8185944abc1251588577b936754e43d02
Julien Semaan <jul.semaan@gmail.com>
2022-02-10 09:33:13 -0500

    add let's encrypt renew to debian cron.d

--- debian/packetfence.cron.d
+++ debian/packetfence.cron.d
@@ -4,3 +4,6 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 30 0 * * * root /usr/local/pf/addons/backup-and-maintenance.sh
+
+# Renew any Let's Encrypt certificates the first of the month
+1 0 1 * * root /usr/local/pf/bin/pfcmd renew_lets_encrypt
