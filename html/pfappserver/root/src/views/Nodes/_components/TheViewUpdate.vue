5b577738eeef8f8d572c6551e488be0c1bf27269
Darren Satkunas <darren@satkunas.com>
2022-02-11 10:16:04 -0500

    fix(admin(vue)): add statuses to node header, fixes #6701

--- html/pfappserver/root/src/views/Nodes/_components/TheViewUpdate.vue
+++ html/pfappserver/root/src/views/Nodes/_components/TheViewUpdate.vue
@@ -2,7 +2,31 @@
   <b-card no-body>
     <b-card-header>
       <b-button-close @click="onClose" v-b-tooltip.hover.left.d300 :title="$t('Close [ESC]')"><icon name="times"/></b-button-close>
-      <h4 class="d-inline mb-0">MAC <strong v-text="id"></strong></h4>
+      <h4 class="d-inline mb-0">
+        MAC <strong v-text="id" />
+      </h4>
+      <b-container fluid class="px-0 mt-3">
+        <span v-if="node.status === 'reg'" class="mr-1">
+          <icon name="check-circle" /> {{ $t('registered') }}
+        </span>
+        <span v-else-if="node.status === 'unreg'" class="mr-1">
+          <icon name="regular/times-circle" /> {{ $t('unregistered') }}
+        </span>
+        <span v-else class="ml-1">
+          <icon name="regular/dot-circle" /> {{ $t('pending') }}
+        </span>
+
+        <span v-if="node.online === 'on'" class="ml-1">
+          <icon name="circle" class="text-success" /> {{ $t('on') }}
+        </span>
+        <span v-else-if="node.online === 'off'" class="ml-1">
+          <icon name="circle" class="text-danger" /> {{ $t('off') }}
+        </span>
+        <span v-else
+          v-b-tooltip.right.d300 class="ml-1">
+          <icon name="question-circle" class="text-warning" /> {{ $t('unknown') }}
+        </span>
+      </b-container>
     </b-card-header>
     <b-tabs ref="tabsRef" v-model="tabIndex" card>
       <base-form-tab :title="$i18n.t('Edit')" active>
@@ -51,12 +75,16 @@ const props = {
   }
 }
 
-import { ref, watch } from '@vue/composition-api'
+import { computed, ref, toRefs, watch } from '@vue/composition-api'
 import useEventEscapeKey from '@/composables/useEventEscapeKey'
 import { usePropsWrapper } from '@/composables/useProps'
 import { useStore } from '../_composables/useCollection'
 
 const setup = (props, context) => {
+
+  const {
+    id
+  } = toRefs(props)
 
   const { root: { $router, $store } = {} } = context
 
@@ -77,12 +105,15 @@ const setup = (props, context) => {
   const escapeKey = useEventEscapeKey()
   watch(escapeKey, () => onClose())
 
+  const node = computed(() => $store.state.$_nodes.nodes[id.value] || {})
+
   return {
     tabsRef,
     tabIndex,
     isLoading,
     onClose,
-    onRefresh
+    onRefresh,
+    node
   }
 }
 
