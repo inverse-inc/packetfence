cebaaddf01461de67e4d90c6547ac8bc70b3dad6
James Rouzier <jrouzier@inverse.ca>
2022-02-11 11:06:39 -0500

    Added an additional test for Aggregation

--- go/cron/bandwidth_maintenance_test.go
+++ go/cron/bandwidth_maintenance_test.go
@@ -170,6 +170,87 @@ FROM macs JOIN dates;
 	)
 }
 
+func TestBandwidthMaintenanceAggregation2(t *testing.T) {
+	testWindowSqlCleanup(
+		t,
+		"bandwidth_maintenance",
+		nil,
+		[]string{
+			"TRUNCATE bandwidth_accounting",
+			"TRUNCATE bandwidth_accounting_history",
+			`
+INSERT INTO bandwidth_accounting (
+        tenant_id,
+        node_id,
+        unique_session_id,
+        mac,
+        time_bucket,
+        in_bytes,
+        out_bytes,
+        last_updated,
+        source_type
+) WITH macs AS (
+    SELECT
+        (1 << 48 | seq) as node_id,
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(seq & 255), 2, '0')
+        )) AS mac FROM seq_1_to_20
+), dates AS (
+    SELECT seq2 as session_id, DATE_SUB(NOW(), INTERVAL (seq2 * 4 + seq) * 15 MINUTE ) as time_bucket from seq_0_to_3 JOIN (SELECT seq as seq2 from seq_0_to_23) as x
+)
+SELECT
+    1 AS tenant_id,
+    node_id,
+    session_id,
+    mac,
+    time_bucket,
+    100 in_bytes,
+    100 out_bytes,
+    time_bucket as last_updated,
+    'radius' as source_type
+FROM macs JOIN dates;
+`,
+		},
+		0,
+		[]sqlCountTest{
+			{
+				name:          "aggregated records",
+				sql:           `SELECT COUNT(*) FROM bandwidth_accounting`,
+				expectedCount: 1000,
+			},
+			{
+				name:          "bandwidth was kept in bandwidth_accounting",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting`,
+				expectedCount: 20,
+			},
+			{
+				name:          "Nothing was moved to the history table",
+				sql:           `SELECT COUNT(DISTINCT node_id) FROM bandwidth_accounting_history`,
+				expectedCount: 0,
+			},
+			{
+				name: "bandwidth merged",
+				sql: `
+                SELECT COUNT(*) FROM (
+                    SELECT SUM(in_bytes) as in_bytes, node_id FROM (
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting_history GROUP BY node_id
+                        UNION ALL
+                        SELECT node_id, SUM(in_bytes) as in_bytes FROM bandwidth_accounting GROUP BY node_id
+                    ) as y GROUP BY node_id
+                ) as x WHERE in_bytes = 9600`,
+				expectedCount: 20,
+			},
+		},
+		[]string{},
+	)
+}
+
 func TestBandwidthMaintenanceAggregationZeroIn(t *testing.T) {
 	testWindowSqlCleanup(
 		t,
