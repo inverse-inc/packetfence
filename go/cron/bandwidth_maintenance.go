3d9d48ab87017210971113e8db06bc1c17dd8169
James Rouzier <jrouzier@inverse.ca>
2022-02-11 07:07:41 -0500

    Convert BandwidthHistoryAggregation

--- go/cron/bandwidth_maintenance.go
+++ go/cron/bandwidth_maintenance.go
@@ -60,8 +60,8 @@ func (j *BandwidthMaintenance) Run() {
 	//j.BandwidthAggregation(ctx, "hourly", "DATE_SUB(NOW(), INTERVAL ? HOUR)", 2)
 	j.BandwidthAggregation(ctx, "daily", "DATE_SUB(NOW(), INTERVAL ? DAY)", 2)
 	j.BandwidthAggregation(ctx, "monthly", "DATE_SUB(NOW(), INTERVAL ? MONTH)", 1)
-	j.BandwidthHistoryAggregation(ctx, "daily", "SUBDATE(NOW(), INTERVAL ? DAY)", 1)
-	j.BandwidthHistoryAggregation(ctx, "monthly", "SUBDATE(NOW(), INTERVAL ? MONTH)", 1)
+	j.BandwidthHistoryAggregation(ctx, "DATE", "DAY" , 1)
+	j.BandwidthHistoryAggregation(ctx, "ROUND_TO_MONTH", "MONTH", 1)
 	j.BandwidthAccountingHistoryCleanup(ctx)
 }
 
@@ -109,7 +109,7 @@ func (j *BandwidthMaintenance) BandwidthAggregation(ctx context.Context, roundin
 	)
 }
 
-const BandwidthAccountingRadiusToHistoryWindow = 5 * 60;
+const BandwidthAccountingRadiusToHistoryWindow = 5 * 60
 
 func (j *BandwidthMaintenance) BandwidthAccountingRadiusToHistory(ctx context.Context) {
 	sql := `
@@ -163,15 +163,57 @@ END;
 	)
 }
 
-func (j *BandwidthMaintenance) BandwidthHistoryAggregation(ctx context.Context, rounding string, date_sql string, interval int) {
-	sql := "CALL bandwidth_aggregation_history(?, " + date_sql + ", ?)"
+func (j *BandwidthMaintenance) BandwidthHistoryAggregation(ctx context.Context, rounding_func, unit string, interval int) {
+	sql := fmt.Sprintf(
+		`
+BEGIN NOT ATOMIC
+    DECLARE EXIT HANDLER
+    FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    SET @count = 0;
+    SET @end_bucket = DATE_SUB(?, INTERVAL ? %s);
+    START TRANSACTION;
+    INSERT INTO bandwidth_accounting_history
+    (node_id, time_bucket, tenant_id, mac, in_bytes, out_bytes)
+     SELECT
+         node_id,
+         new_time_bucket,
+         tenant_id,
+         mac,
+         sum(in_bytes) AS in_bytes,
+         sum(out_bytes) AS out_bytes
+        FROM (
+        SELECT node_id, %s(time_bucket) as new_time_bucket, tenant_id, mac, in_bytes, out_bytes FROM bandwidth_accounting_history WHERE time_bucket <= @end_bucket AND time_bucket != %s(time_bucket) ORDER BY node_id, time_bucket LIMIT ? FOR UPDATE ) AS to_delete_bandwidth_aggregation_history
+        GROUP BY node_id, new_time_bucket
+        ON DUPLICATE KEY UPDATE
+            in_bytes = in_bytes + VALUES(in_bytes),
+            out_bytes = out_bytes + VALUES(out_bytes)
+        ;
+
+    DELETE bandwidth_accounting_history
+        FROM bandwidth_accounting_history RIGHT JOIN (SELECT node_id, time_bucket FROM bandwidth_accounting_history WHERE time_bucket <= @end_bucket AND time_bucket != %s(time_bucket) ORDER BY node_id, time_bucket LIMIT ? FOR UPDATE ) AS to_delete_bandwidth_aggregation_history USING (node_id, time_bucket);
+        SET @count = ROW_COUNT();
+        COMMIT;
+    SELECT @count;
+END;`,
+        unit,
+		rounding_func,
+		rounding_func,
+		rounding_func,
+	)
+    log.LogInfo(ctx, "Running " + sql)
 	BatchSqlCount(
 		ctx,
-		"bandwidth_aggregation_history-"+rounding,
+		"bandwidth_aggregation_history-"+unit,
 		j.Timeout,
 		sql,
-		rounding,
+		time.Now(),
 		interval,
+		j.Batch,
 		j.Batch,
 	)
 }
