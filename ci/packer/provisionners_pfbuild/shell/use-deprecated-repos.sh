e2c4331758d3efd7f8ade6b81e4d9ce3da29ff92
nqb <nquiniou@akamai.com>
2022-02-15 15:37:27 +0100

    don't use vault.centos.org to build
    have issue sometimes
    build_img_docker=yes

--- ci/packer/provisionners_pfbuild/shell/use-deprecated-repos.sh
+++ ci/packer/provisionners_pfbuild/shell/use-deprecated-repos.sh
@@ -4,4 +4,4 @@ set -o nounset -o pipefail -o errexit
 # Workaround for EOL of CentOS 8
 # Use snapshot of deprecated repositories
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://mirror.nsc.liu.se/centos-store|g' /etc/yum.repos.d/CentOS-Linux-*
