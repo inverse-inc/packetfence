f778ec52d6bbe64c3466ca88b99683dc31f94b3e
nqb <nquiniou@akamai.com>
2022-03-24 15:16:45 +0100

    enh: refresh dev environment recipe

--- addons/dev-helpers/setup-dev-env.sh
+++ addons/dev-helpers/setup-dev-env.sh
@@ -9,31 +9,54 @@ log_section() {
    printf "=\t%s\n" "" "$@" ""
 }
 
+## Is npm Installed
+if ! type npm 2> /dev/null ; then
+  echo "Install npm before running this script"
+  echo "You can follow instructions here: https://github.com/nodesource/distributions#table-of-contents"
+  exit 1
+fi
+
+log_section "Stop services"
+systemctl stop packetfence-mariadb packetfence-redis-cache
+systemct stop packetfence-config
+/usr/local/pf/bin/pfcmd service pf restart
+
+log_section "Replace /usr/local/pf by git repository"
+mv /usr/local/pf /usr/local/pf-pkg
+git clone https://github.com/inverse-inc/packetfence /usr/local/pf
+
 log_section "Create all necessary files"
+
+# to have all Perl dependencies at correct location
+cp -r /usr/local/pf-pkg/lib_perl /usr/local/pf/
+
 cd /usr/local/pf
 make devel
 make conf/ssl/server.pem
 mkdir /usr/local/pf/var/ssl_mutex
-# to keep settings made by packetfence_install role
-cp ../pf-pkg/conf/pf.conf conf/
-cp ../pf-pkg/conf/pfconfig.conf conf/
+# to keep settings set up during configurator
+cp /usr/local/pf-pkg/conf/pf.conf conf/
+cp /usr/local/pf-pkg/conf/pfconfig.conf conf/
+cp /usr/local/pf-pkg/conf/networks.conf conf/
+cp -r /usr/local/pf-pkg/conf/certmanager/ conf/
 # to keep iptables rule for vagrant management
-cp ../pf-pkg/conf/iptables.conf conf/
+cp /usr/local/pf-pkg/conf/iptables.conf conf/
 
 log_section "Build web admin"
 cd /usr/local/pf/html/pfappserver/root/
-sudo make vendor
-sudo make dev
+make vendor
+make dev
 
 log_section "Build captive portal"
 cd /usr/local/pf/html/common
-sudo make vendor
-sudo make dev
+make vendor
+make dev
 
 log_section "Build Golang environment"
 cd /usr/local/pf/go
 make go-env
-/usr/local/pf/addons/packages/build-go.sh build /usr/local/pf /usr/local/pf/sbin/
+make all
+make copy
 
 log_section "Fix permissions and start unmanaged services"
 cd /usr/local/pf
