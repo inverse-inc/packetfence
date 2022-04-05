1f0bb6a77fe2f71d035aeba3e55bd9c25960b566
James Rouzier <jrouzier@inverse.ca>
2022-04-04 23:09:10 -0400

    Remove tenant_id

--- lib/pf/person.pm
+++ lib/pf/person.pm
@@ -69,7 +69,7 @@ our $PID_RE = qr{ [a-zA-Z0-9\-\_\.\@\/\\]+ }x;
 
 our @FIELDS = @pf::dal::_person::FIELD_NAMES;
 
-our @NON_PROMPTABLE_FIELDS = qw(pid sponsor portal source tenant_id);
+our @NON_PROMPTABLE_FIELDS = qw(pid sponsor portal source);
 
 our @PROMPTABLE_FIELDS;
 foreach my $field (@FIELDS){
@@ -328,7 +328,7 @@ Get all the persons who are not the owner of at least one node.
 
 sub persons_without_nodes {
     my ($status, $iter) = pf::dal::person->search(
-        -from => [-join => 'person', '=>{node.pid=person.pid,node.tenant_id=person.tenant_id}', 'node'],
+        -from => [-join => 'person', '=>{node.pid=person.pid}', 'node'],
         -columns => ['person.pid'],
         -group_by => 'pid',
         -having => 'count(node.mac)=0',
