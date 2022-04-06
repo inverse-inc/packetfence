ccc90abfb2d9c958dca6182208783cf4c2711b1c
Julien Semaan <jul.semaan@gmail.com>
2022-04-05 20:14:52 -0400

    connectors admin interface improvements

--- html/pfappserver/root/src/views/Configuration/connectors/_components/index.js
+++ html/pfappserver/root/src/views/Configuration/connectors/_components/index.js
@@ -8,6 +8,7 @@ import {
   BaseFormGroupIntervalUnit,
   BaseFormGroupOses
 } from '@/views/Configuration/_components/new/'
+import BaseFormGroupNetworks from './BaseFormGroupNetworks'
 import TheForm from './TheForm'
 import TheView from './TheView'
 
@@ -16,7 +17,9 @@ export {
   BaseFormButtonBar           as FormButtonBar,
 
   BaseFormGroupInput          as FormGroupIdentifier,
+  BaseFormGroupInput          as FormGroupDescription,
   BaseFormGroupInput          as FormGroupSecret,
+  BaseFormGroupNetworks       as FormGroupNetworks,
 
   TheForm,
   TheView
