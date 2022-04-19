805856b598816c12f8805bf079eb245f0732746d
Julien Semaan <jul.semaan@gmail.com>
2022-04-19 15:28:37 -0400

    reuse connections to connector servers

--- lib/pf/connector.pm
+++ lib/pf/connector.pm
@@ -5,6 +5,7 @@ use URI;
 use pf::api::unifiedapiclient;
 use pf::config::pfqueue;
 use pf::pfqueue::consumer::redis;
+use POSIX::AtFork;
 
 has id => (is => 'rw');
 
@@ -12,14 +13,27 @@ has secret => (is => 'rw');
 
 has networks => (is => 'rw');
 
+my %connections;
+sub CLONE {
+    %connections = ();
+}
+POSIX::AtFork->add_to_child(\&CLONE);
+CLONE();
+
+
 sub connectorServerApiClient {
     my ($self) = @_;
     #TODO: get this out of redis_queue
     my $redis = pf::pfqueue::consumer::redis->new({ %{$ConfigPfqueue{"consumer"}} })->redis;
     if(my $server = $redis->get("pfconnector:activeTunnels:".$self->id)) {
+        if(exists($connections{$server})) {
+            print "from hash \n";
+            return $connections{$server};
+        }
         #TODO: get connections to be reused
         my $uri = URI->new($server);
-        return pf::api::unifiedapiclient->new(proto => $uri->scheme, host => $uri->host, port => $uri->port);
+        $connections{$server} = pf::api::unifiedapiclient->new(proto => $uri->scheme, host => $uri->host, port => $uri->port);
+        return $connections{$server};
     }
     else {
         return pf::api::unifiedapiclient->management_client;
