3db1a6d6160592308b9b396b9e859741c2d63c88
Julien Semaan <jul.semaan@gmail.com>
2022-04-19 13:56:50 -0400

    remove outdated todo

--- lib/pf/connector.pm
+++ lib/pf/connector.pm
@@ -10,7 +10,6 @@ has networks => (is => 'rw');
 
 sub dynreverse {
     my ($self, $to) = @_;
-    #TODO: implement reverse forwards reuse here or in pfconnector
     my $connector_conn = pf::api::unifiedapiclient->management_client->call("POST", "/api/v1/pfconnector/dynreverse", {
         to => $to,
         connector_id => $self->id,
