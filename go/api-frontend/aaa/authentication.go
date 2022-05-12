6b8d0ac77a53b5e4036fa4750889a5f15d70fe7d
Julien Semaan <jul.semaan@gmail.com>
2022-05-12 17:08:08 +0000

    have go images use a docker image layer for their dependencies

--- go/api-frontend/aaa/authentication.go
+++ go/api-frontend/aaa/authentication.go
@@ -25,6 +25,7 @@ func NewTokenAuthenticationMiddleware(tb TokenBackend) *TokenAuthenticationMiddl
 }
 
 func (tam *TokenAuthenticationMiddleware) AddAuthenticationBackend(ab AuthenticationBackend) {
+	fmt.Println("bleh")
 	tam.authBackends = append(tam.authBackends, ab)
 }
 
