c97b19fe56bfa9781fa233d4b00b1d7edc1a4aa2
Julien Semaan <jul.semaan@gmail.com>
2022-05-24 19:32:23 +0000

    revert deletion of pf::cmd

---
+++ lib/pf/cmd.pm
@@ -0,0 +1,71 @@
+package pf::cmd;
+
+=head1 NAME
+
+pf::cmd add documentation
+
+=head1 DESCRIPTION
+
+pf::cmd
+
+=cut
+
+use strict;
+use warnings;
+use Role::Tiny::With;
+with 'pf::cmd::roles::show_help';
+
+sub new {
+    my ($class,$args) = @_;
+    my $self = bless $args,$class;
+    return $self;
+}
+
+sub run {
+    my ($self) = @_;
+    if ($self->parseArgs) {
+        $self->_run;
+    } else {
+        $self->showHelp;
+    }
+}
+
+sub parseArgs {
+    my ($self) = @_;
+    return $self->args == 0;
+}
+
+sub args {
+    return @{$_[0]->{args} || []};
+}
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2022 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
