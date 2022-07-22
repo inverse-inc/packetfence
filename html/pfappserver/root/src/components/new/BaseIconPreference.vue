0c865b01b0d617296ddb63ea0b75b2421ae004a7
Darren Satkunas <dsatkuna@akamai.com>
2022-07-22 14:50:32 +0000

    improve animation

--- html/pfappserver/root/src/components/new/BaseIconPreference.vue
+++ html/pfappserver/root/src/components/new/BaseIconPreference.vue
@@ -1,6 +1,6 @@
 <template>
   <icon name="save" v-bind="$atrrs"
-    id="icon-preference" :class="(isReading || isWriting) ? 'active' : 'inactive'"
+    id="icon-preference" :class="className"
     v-b-tooltip.hover.right.d300 :title="$t(`User preference (${id}) stored in database.`)"
     />
 </template>
@@ -13,7 +13,7 @@ const props = {
   }
 }
 
-import { computed, toRefs } from '@vue/composition-api'
+import { computed, ref, toRefs, watch } from '@vue/composition-api'
 
 const setup = (props, context) => {
 
@@ -25,10 +25,25 @@ const setup = (props, context) => {
 
   const isReading = computed(() => $store.getters['preferences/isReadingId'](id.value))
   const isWriting = computed(() => $store.getters['preferences/isWritingId'](id.value))
+  const className = ref('is-inactive')
+
+
+  watch(isReading, () => {
+    if (isReading.value)
+      className.value = 'is-reading'
+    else
+      className.value = 'was-reading'
+  })
+
+  watch(isWriting, () => {
+    if (isWriting.value)
+      className.value = 'is-writing'
+    else
+      className.value = 'was-writing'
+  })
 
   return {
-    isReading,
-    isWriting
+    className
   }
 }
 
@@ -41,25 +56,46 @@ export default {
 </script>
 
 <style lang="scss">
-@keyframes active {
-  from { color: rgb(248, 249, 250); }
-  to { color: rgb(0, 123, 255); }
+$color_active_r: rgb(40, 167, 69);
+$color_active_w: rgb(220, 53, 69);
+$color_inactive: rgb(216, 216, 216);
+
+@keyframes is-reading {
+  0% { color: $color_inactive; transform: scale(1); }
+  100% { color: $color_active_r; transform: scale(1.5); }
 }
-@keyframes inactive {
-  from { color: rgb(0, 123, 255); }
-  to { color: rgb(248, 249, 250); }
+@keyframes was-reading {
+  0% { color: $color_inactive; transform: scale(1); }
+  25% { color: $color_active_r; transform: scale(1.5); }
+  75% { color: $color_active_r; transform: scale(1.5); }
+  100% { color: $color_inactive; transform: scale(1); }
+}
+@keyframes is-writing {
+  0% { color: $color_inactive; transform: scale(1); }
+  100% { color: $color_active_w; transform: scale(1.5); }
+}
+@keyframes was-writing {
+  0% { color: $color_inactive; transform: scale(1); }
+  25% { color: $color_active_w; transform: scale(1.5); }
+  75% { color: $color_active_w; transform: scale(1.5); }
+  100% { color: $color_inactive; transform: scale(1); }
 }
 
 #icon-preference {
-  &.active {
-    color: rgb(0, 123, 255);
-    animation-name: active;
-    animation-duration: 3s;
+  color: $color_inactive;
+  &.is-reading {
+    color: $color_active_r;
+    animation: is-reading 1000ms;
   }
-  &.inactive {
-    color: rgb(248, 249, 250);
-    animation-name: inactive;
-    animation-duration: 3s;
+  &.was-reading {
+    animation: was-reading 3000ms;
+  }
+  &.is-writing {
+    color: $color_active_w;
+    animation: is-writing 1000ms;
+  }
+  &.was-writing {
+    animation: was-writing 3000ms;
   }
 }
 </style>
