cd7ef3860e6e0e3327c3090398f1939805354f65
James Rouzier <jrouzier@inverse.ca>
2022-07-26 21:03:17 +0000

    Remove double per_device_class

--- lib/pf/UnifiedApi/Controller/Nodes.pm
+++ lib/pf/UnifiedApi/Controller/Nodes.pm
@@ -1112,20 +1112,6 @@ sub bulk_delete {
     return $self->render(status => 200, json => { items => $results });
 }
 
-sub per_device_class {
-    my ($self) = @_;
-    my ($status, $sth) = $self->dal->db_execute(
-        "SELECT device_class, COUNT(1) as count from node WHERE device_class IS NOT NULL GROUP BY device_class;"
-    );
-    if (is_error($status)) {
-        return $self->render_error($status, "Cannot complete query");
-    }
-
-    my $items = $sth->fetchall_arrayref({});
-    $sth->finish();
-    return $self->render(json => { items => $items });
-}
-
 sub do_remove {
     my ($self) = @_;
     return $self->_do_remove($self->build_item_lookup);
