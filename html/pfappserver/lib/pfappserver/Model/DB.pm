d500699b2c16ea44c4b4e216b34dedbc905dd706
James Rouzier <jrouzier@inverse.ca>
2022-07-27 05:36:25 +0000

    Use DBI->connect instead of the driver
    Fixes #7068

--- html/pfappserver/lib/pfappserver/Model/DB.pm
+++ html/pfappserver/lib/pfappserver/Model/DB.pm
@@ -118,9 +118,8 @@ sub create {
 
     my ( $status_msg, $result );
 
-    my $dbDriver = DBI->install_driver("mysql");
-    # Create the requested database
-    $result = $dbDriver->func('createdb', "$db DEFAULT CHARACTER SET = 'utf8mb4' ", 'localhost', $root_user, $root_password, 'admin');
+    my $dbh = DBI->connect("dbi:mysql:mysql_socket=/var/lib/mysql/mysql.sock", $root_user, $root_password);
+    $result = $dbh->do("CREATE DATABASE $db DEFAULT CHARACTER SET = 'utf8mb4'");
     if ( !$result ) {
         $status_msg = ["Error in creating the database [_1]",$db];
         $logger->warn($DBI::errstr);
