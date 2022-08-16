9898f56b654bc35d8c5b3c62c54592a407740763
nqb <nquiniou@akamai.com>
2022-08-16 17:53:15 +0200

    don't install freeradius pkgs inside pfdebian images

--- addons/dev-helpers/debian/install-pf-dependencies.sh
+++ addons/dev-helpers/debian/install-pf-dependencies.sh
@@ -3,17 +3,21 @@ set -o nounset -o pipefail -o errexit
 
 export DEBIAN_FRONTEND=noninteractive
 
+# syntax: pkg1|pkg2|pkg3
+PKGS_TO_EXCLUDE=${PKGS_TO_EXCLUDE:-packetfence}
+
 # This script will install all PacketFence runtime dependencies
 
 # https://askubuntu.com/a/791865
 apt-install-depends() {
     local pkg="$1"
-    apt-get install -s "$pkg" | grep -v 'packetfence' \
+    apt-get install -s "$pkg" | egrep -v "$PKGS_TO_EXCLUDE" \
         | sed -n \
           -e "/^Inst $pkg /d" \
           -e 's/^Inst \([^ ]\+\) .*$/\1/p' \
         | xargs apt-get install -y
 }
 
+declare -p PKGS_TO_EXCLUDE
 apt-get -qq update
 apt-install-depends packetfence
