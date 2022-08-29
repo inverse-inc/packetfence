b7f762e69dbbff4112b5bc55d396dd5c71a022a6
Julien Semaan <jul.semaan@gmail.com>
2022-08-29 16:25:28 +0000

    retry preparing statements on failure in pfacct

--- go/cmd/pfacct/radius.go
+++ go/cmd/pfacct/radius.go
@@ -582,7 +582,9 @@ type RadiusStatements struct {
 func setupStmt(db *sql.DB, stmt **sql.Stmt, sql string) {
 	var err error
 	if *stmt, err = db.Prepare(sql); err != nil {
-		panic(err)
+		log.Logger().Error(fmt.Sprintf("Failed to prepare statement '%s': %s", sql, err))
+		time.Sleep(5 * time.Second)
+		go setupStmt(db, stmt, sql)
 	}
 }
 
