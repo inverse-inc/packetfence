1a6019ebc6f84ce21bcc0e4ee2a8f7c3decd213c
Julien Semaan <jul.semaan@gmail.com>
2022-09-08 15:22:39 +0000

    script to rebuild pf containers from local code

---
+++ addons/dev-helpers/build-local-container.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+source /usr/local/pf/addons/functions/helpers.functions
+
+cd /usr/local/pf/
+
+main_splitter
+
+name="$1"
+dockerfile=containers/$name/Dockerfile
+
+if ! [ -f $dockerfile ]; then
+  echo "'$name' is not a valid container name."
+
+  sub_splitter
+
+  # find all directories with Dockerfile
+  # excluding non necessary images
+  DOCKERFILE_DIRS=$(find containers/ -type f -name "Dockerfile" \
+                         -not -path "*/pfdebian/*" \
+                         -not -path "*/radiusd/*" \
+                         -not -path "*/pfconnector-*/*" \
+                         -printf "%P\n")
+
+  for file in ${DOCKERFILE_DIRS}; do
+    # remove /Dockerfile suffix
+    CONTAINERS_IMAGES+=" ${file%/Dockerfile}"
+  done
+  
+  echo "The following images can be built using this tool:"
+  for img in ${CONTAINERS_IMAGES}; do
+    echo "- $img"
+  done
+  
+  exit 1
+fi
+
+set -o nounset -o pipefail -o errexit
+
+source /usr/local/pf/conf/build_id
+
+img_name=packetfence/$name:$TAG_OR_BRANCH_NAME
+
+echo "Building image $img_name"
+
+sub_splitter
+
+docker build -t $img_name \
+  --build-arg=BUILD_PFAPPSERVER_VUE=yes \
+  --build-arg=KNK_REGISTRY_URL=ghcr.io/inverse-inc/packetfence \
+  --build-arg=IMAGE_TAG=$TAG_OR_BRANCH_NAME \
+  -f containers/$name/Dockerfile .
+
+main_splitter
+
+echo "New image build from $dockerfile -> $img_name"
