aee3f063bca8887e67819c962e62c16d745b968e
James Rouzier <jrouzier@inverse.ca>
2022-09-09 18:24:22 +0000

    Use coredns as a library to make upgrading easier later

--- go/cmd/pfdns2/main.go
+++ go/cmd/pfdns2/main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-        _ "github.com/coredns/example"
-
         "github.com/coredns/coredns/coremain"
         "github.com/coredns/coredns/core/dnsserver"
 )
 
-var directives = []string{
-        "example",
-        "whoami",
-        "startup",
-        "shutdown",
-}
-
 func init() {
-        dnsserver.Directives = directives
+        dnsserver.Directives = append(dnsserver.Directives, "pfdns");
 }
 
 func main() {
