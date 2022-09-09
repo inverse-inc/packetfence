343febdeb030ac9bbf29ee062ba744a114008c28
James Rouzier <jrouzier@inverse.ca>
2022-09-09 18:29:07 +0000

    remove the packetfence specific coredns

--- go/coredns/plugin/file/shutdown.go
+++
@@ -1,9 +0,0 @@
-package file
-
-// OnShutdown shuts down any running go-routines for this zone.
-func (z *Zone) OnShutdown() error {
-	if 0 < z.ReloadInterval {
-		z.reloadShutdown <- true
-	}
-	return nil
-}
