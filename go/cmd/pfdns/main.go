14c22ae448cd12be8e56d4980fc16bd819b4a6ba
James Rouzier <jrouzier@inverse.ca>
2022-09-09 18:38:36 +0000

    go fmt

--- go/cmd/pfdns/main.go
+++ go/cmd/pfdns/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-        "github.com/coredns/coredns/coremain"
-        "github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/coremain"
 )
 
 func init() {
-        dnsserver.Directives = append(dnsserver.Directives, "pfdns");
+	dnsserver.Directives = append(dnsserver.Directives, "pfdns")
 }
 
 func main() {
-        coremain.Run()
+	coremain.Run()
 }
-
