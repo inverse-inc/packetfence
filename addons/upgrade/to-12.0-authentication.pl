d135b96e226c7082edee88da73a0848582cff7ef
James Rouzier <jrouzier@inverse.ca>
2022-09-09 13:45:25 +0000

    Take care of the case where order is not the same

--- addons/upgrade/to-12.0-authentication.pl
+++ addons/upgrade/to-12.0-authentication.pl
@@ -14,7 +14,8 @@ use strict;
 use warnings;
 use lib qw(/usr/local/pf/lib);
 use lib qw(/usr/local/pf/lib_perl/lib/perl5);
-my $old_value = '100056,100057,100061,100058,100059,100060,100062,100063,100071,100064,100116,100066,100117,100112,100067,100065,100068,100069,100070,100118,100115,100072,100073,100074,100075,100076,100077,100085,100086,100080,100079,100081,100083,100082,100084,100087,100088,100111,100089,100090,100091,100092,100093,100094,100095,100096,100098,100097,100099,100100,100101,100113,100102,100103,100104,100106,100105,100107,100108,100109,100114,100110,100078,100119,100120,100121,100122,100123,100124,100125,100126,100127,100128';
+use List::Util qw(all);
+my @old_values = qw(100056 100057 100061 100058 100059 100060 100062 100063 100071 100064 100116 100066 100117 100112 100067 100065 100068 100069 100070 100118 100115 100072 100073 100074 100075 100076 100077 100085 100086 100080 100079 100081 100083 100082 100084 100087 100088 100111 100089 100090 100091 100092 100093 100094 100095 100096 100098 100097 100099 100100 100101 100113 100102 100103 100104 100106 100105 100107 100108 100109 100114 100110 100078 100119 100120 100121 100122 100123 100124 100125 100126 100127 100128);
 use pf::file_paths qw($authentication_config_file);
 use pf::IniFiles;
 
@@ -22,16 +23,21 @@ my $example_file = $authentication_config_file . '.example';
 my $example_ini = pf::IniFiles->new( -file => $example_file);
 my $ini = pf::IniFiles->new( -file => $authentication_config_file);
 my $new_value = $example_ini->val('sms', 'sms_carriers');
+my @new_values = split /\s*,\s*/, $new_value;
 
 my $i = 0;
-for my $section ( grep { /\S/ } $ini->Sections()) {
+for my $section ( grep { /^\S+$/ } $ini->Sections()) {
     my $type = $ini->val($section, 'type');
     next if !defined $type || $type ne 'SMS';
     my $sms_carriers = $ini->val($section, 'sms_carriers');
-    next if ($old_value ne $sms_carriers);
+    my @sms_carriers =  split /\s*,\s*/, $sms_carriers;
+    next if scalar @old_values != @sms_carriers;
+
+    my %sms_carriers = map { $_ => 1 } @sms_carriers;
+    next unless all { exists $sms_carriers{$_} } @old_values;
 
     print "Updating $section\n";
-    $ini->setval($section, 'sms_carriers', $new_value);
+    $ini->setval($section, 'sms_carriers', join(',', $sms_carriers, grep { !exists $sms_carriers{$_} } @new_values ));
     $i |= 1;
 }
 
