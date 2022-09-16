7807e80e68c3913b15e7c0b79d9ba775f84881d8
nqb <nquiniou@akamai.com>
2022-09-16 13:11:44 +0200

    fix(iso): script already run in chroot, run daemon at background

--- containers/run-docker-in-debian-installer.sh
+++ containers/run-docker-in-debian-installer.sh
@@ -6,15 +6,15 @@ set -o nounset -o pipefail -o errexit
 
 # need cgroupfs-mount package installed
 # necessary to run Docker daemon
-chroot /target /usr/bin/cgroupfs-mount
+/usr/bin/cgroupfs-mount
 
 # load overlay module to use overlay2 storage driver with Docker daemon
-chroot /target modprobe overlay
+modprobe overlay
 
 # run containerd
-chroot /target /usr/bin/containerd
+/usr/bin/containerd &
 
 # run Docker daemon
-chroot /target /usr/bin/dockerd --iptables=false -b=none -s overlay2
+/usr/bin/dockerd --iptables=false -b=none -s overlay2 &
 
 
