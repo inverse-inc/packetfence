14e510ceef22049260a062d2852492f77d7207b3
nqb <nquiniou@akamai.com>
2022-10-26 21:58:26 +0200

    add debug

--- containers/docker-minimal-rules.sh
+++ containers/docker-minimal-rules.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 function msg { out "$*" >&1 ;}
 function out { printf '%s\n' "$*" ;}
@@ -6,6 +6,9 @@ function out { printf '%s\n' "$*" ;}
 while iptables -L DOCKER ; [ $? -ne 0 ];do
   msg "Waiting for iptables to be ready"
   eval "iptables -t filter -N DOCKER"
+  iptables -S -t filter
   eval "iptables -t nat -N DOCKER"
+  iptables -S -t nat
   sleep 1;
 done
+echo "end of $0"
