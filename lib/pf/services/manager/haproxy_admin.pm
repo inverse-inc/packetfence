b00778fa6cdb979e2fbb064c691ec0329980e4a6
James Rouzier <jrouzier@inverse.ca>
2022-11-09 16:22:57 +0000

    Remove tabs

--- lib/pf/services/manager/haproxy_admin.pm
+++ lib/pf/services/manager/haproxy_admin.pm
@@ -108,7 +108,7 @@ backend 127.0.0.1-netdata
         server service $netdata_service_host:$netdata_service_port
         http-request set-uri %[var(req.path)]?%[query] if paramsquery
         http-request set-uri %[var(req.path)] unless paramsquery
-	http-response add-header X-Frame-Options SAMEORIGIN
+        http-response add-header X-Frame-Options SAMEORIGIN
 EOT
 
         my $mgmt_api_backend;
@@ -141,7 +141,7 @@ backend $mgmt_back_ip-netdata
         server service $mgmt_back_ip:19999
         http-request set-uri %[var(req.path)]?%[query] if paramsquery
         http-request set-uri %[var(req.path)] unless paramsquery
-	http-response add-header X-Frame-Options SAMEORIGIN
+        http-response add-header X-Frame-Options SAMEORIGIN
 EOT
             }
             $mgmt_api_backend .= <<"EOT";
