f42d1aa8202d8a1358f74d95488b77b0eb83adfc
nqb <nquiniou@akamai.com>
2022-11-24 09:23:53 +0100

    fix(iso): ignore Docker config in Debian installer
    build_pf_img_iso=yes
    Revert f98f305ddf493db6b0827fe0e05ffdf5cdbbfb25
    Related to 1baab63
    
    With previous fix, Docker was trying to use --bip defined in /etc/docker/daemon.json but a module was missing.
    I prefered to generate a fake config. In that way, I'm sure all future additions in daemon.json would be ignored
    and ISO can continue to use same recipe.

--- containers/run-docker-in-debian-installer.sh
+++ containers/run-docker-in-debian-installer.sh
@@ -4,6 +4,10 @@ set -o nounset -o pipefail -o errexit
 # /target is the chroot point used by Debian installer
 # all below commands are run inside it from BusyBox
 
+# generate an empty daemon.json to ignore Docker config inside
+# Debian installer and use only CLI arguments
+echo "{}" > /tmp/daemon.json
+
 # need cgroupfs-mount package installed
 # necessary to run Docker daemon
 /usr/bin/cgroupfs-mount
@@ -15,6 +19,6 @@ modprobe overlay
 /usr/bin/containerd &
 
 # run Docker daemon
-/usr/bin/dockerd --iptables=false -s overlay2 &
+/usr/bin/dockerd -b=none --config-file=/tmp/daemon.json --iptables=false -s overlay2 &
 
 
