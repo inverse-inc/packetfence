06064bc6cf7bbbfc0da9cb350ef0a1eb0361f6c1
Darren Satkunas <dsatkuna@akamai.com>
2022-12-09 06:04:41 -0800

    add systemd button to monit and services

--- html/pfappserver/root/src/views/Configuration/monit/_components/AlertServices.vue
+++ html/pfappserver/root/src/views/Configuration/monit/_components/AlertServices.vue
@@ -6,15 +6,18 @@
       <span v-html="$t('Creating or modifying the monit configuration requires to restart the monit service.')"></span>
     </p>
     <base-button-system-service service="monit" restart start stop
-      :disabled="isLoading" class="mr-1" size="sm"/>
+      :disabled="isLoading" class="mr-1" size="sm" />
+    <base-button-systemd-update class="ml-1" size="sm" />
   </b-alert>
 </template>
 <script>
 import {
-  BaseButtonSystemService
+  BaseButtonSystemService,
+  BaseButtonSystemdUpdate,
 } from '@/components/new/'
 const components = {
-  BaseButtonSystemService
+  BaseButtonSystemService,
+  BaseButtonSystemdUpdate,
 }
 
 const props = {
