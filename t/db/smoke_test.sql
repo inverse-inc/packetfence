c9bb28db272de41f06704e2c4a863a36f2c1f48d
James Rouzier <jrouzier@inverse.ca>
2022-12-11 05:50:23 +0000

    If database_proxysql.status is enabled then configure a single server

--- t/db/smoke_test.sql
+++ t/db/smoke_test.sql
@@ -1,7 +1,9 @@
 DROP USER IF EXISTS pf_smoke_tester@'%';
 DROP USER IF EXISTS pf_smoke_tester@'localhost';
-GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'%' IDENTIFIED BY 'packet';
-GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
-GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'localhost' IDENTIFIED BY 'packet';
-GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'%' IDENTIFIED BY 'packet';
+CREATE USER 'pf_smoke_tester'@'localhost' IDENTIFIED BY 'Packet^1';
+CREATE USER 'pf_smoke_tester'@'%' IDENTIFIED BY 'Packet^1';
+GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'%';
+GRANT ALL PRIVILEGES ON `pf_smoke_test%`.* TO pf_smoke_tester@'localhost';
+GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'localhost';
+GRANT INSERT, ALTER ROUTINE, CREATE ROUTINE ON mysql.* TO pf_smoke_tester@'%';
 FLUSH PRIVILEGES;
