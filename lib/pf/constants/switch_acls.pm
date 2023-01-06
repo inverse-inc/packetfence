d1c0e0407400d52ca9c93998cdd43aa84d38eb9d
James Rouzier <jrouzier@inverse.ca>
2023-01-06 14:31:33 +0000

    Generate switch types that support standard and downloadable acls

---
+++ lib/pf/constants/switch_acls.pm
@@ -0,0 +1,78 @@
+package pf::constants::switch_acls;
+
+=head1 NAME
+
+pf::constants::switch_acls - 
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::constants::switch_acls
+
+
+
+=cut
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(%ACLsSupports);
+
+our %ACLsSupports;
+
+%ACLsSupports = (
+                  'AccessListBasedEnforcement' => {
+                                                    'Aruba 2930M Series' => 1,
+                                                    'Aruba 5400 Switch' => 1,
+                                                    'Aruba CX Switch' => 1,
+                                                    'Cisco ASA Firewall' => 1,
+                                                    'Cisco Catalyst 2960' => 1,
+                                                    'Cisco Catalyst 2960G' => 1,
+                                                    'Cisco Catalyst 2970' => 1,
+                                                    'Cisco Catalyst 3550' => 1,
+                                                    'Cisco Catalyst 3560' => 1,
+                                                    'Cisco Catalyst 3560G' => 1,
+                                                    'Cisco Catalyst 3750' => 1,
+                                                    'Cisco Catalyst 3750G' => 1,
+                                                    'Cisco Catalyst 4500 Series' => 1,
+                                                    'Cisco Catalyst 6500 Series' => 1,
+                                                    'Cisco SG300' => 1,
+                                                    'N1500 Series' => 1
+                                                  },
+                  'DownloadableListBasedEnforcement' => {
+                                                          'Cisco ASA Firewall' => 1
+                                                        }
+                );
+
+
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2023 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
