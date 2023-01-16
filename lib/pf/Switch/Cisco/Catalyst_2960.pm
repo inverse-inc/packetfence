6506dd7b9861fdd29b8e5c3e45a5e2b847c42064
Durand Fabrice <oeufdure@gmail.com>
2023-01-16 16:26:19 -0500

    Remove comment from acls

--- lib/pf/Switch/Cisco/Catalyst_2960.pm
+++ lib/pf/Switch/Cisco/Catalyst_2960.pm
@@ -581,6 +581,9 @@ sub returnRadiusAdvanced {
             for my $i (@a){
                 last if (scalar @{$session->{'acl'}} == 1);
                 my $acl = shift @{$session->{'acl'}};
+                if ($acl !~ /^permit/i && $acl !~ /^deny/i) {
+                    next;
+                }
                 push(@av_pairs, $self->returnAccessListAttribute($session->{'acl_num'})."=".$acl);
                 $session->{'acl_num'} ++;
                 $logger->info("(".$self->{'_id'}.") Adding access list : $acl to the RADIUS reply");
@@ -592,11 +595,15 @@ sub returnRadiusAdvanced {
         }
         if (scalar @{$session->{'acl'}} == 1) {
             my $acl = shift @{$session->{'acl'}};
-            push(@av_pairs, $self->returnAccessListAttribute($session->{'acl_num'})."=".$acl);
-            $logger->info("(".$self->{'_id'}.") Adding access list : $acl to the RADIUS reply");
-            $logger->info("(".$self->{'_id'}.") Added access lists to the RADIUS reply.");
-            $self->setRadiusSession($session);
-        } elsif (scalar @{$session->{'acl'}} == 1) {
+            if ($acl =~ /^permit/i || $acl =~ /^deny/i) {
+                push(@av_pairs, $self->returnAccessListAttribute($session->{'acl_num'})."=".$acl);
+                $logger->info("(".$self->{'_id'}.") Adding access list : $acl to the RADIUS reply");
+                $logger->info("(".$self->{'_id'}.") Added access lists to the RADIUS reply.");
+                $self->setRadiusSession($session);
+            } else {
+                $logger->info("(".$self->{'_id'}.") No more access lists defined for this role ".$args->{'user_role'});
+            }
+        } elsif (scalar @{$session->{'acl'}} == 0) {
             $logger->info("(".$self->{'_id'}.") No more access lists defined for this role ".$args->{'user_role'});
         } else {
             $logger->info("(".$self->{'_id'}.") No access lists defined for this role ".$args->{'user_role'});
