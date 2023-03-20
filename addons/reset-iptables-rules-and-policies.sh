cfe46645fffe3579e1468ef175741d8b6fbab5e0
nqb <nquiniou@akamai.com>
2023-03-20 11:36:22 +0100

    clean ip6tables rules too

--- addons/reset-iptables-rules-and-policies.sh
+++ addons/reset-iptables-rules-and-policies.sh
@@ -11,3 +11,13 @@ iptables -t mangle -X
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
+
+ip6tables -F
+ip6tables -X
+ip6tables -t nat -F
+ip6tables -t nat -X
+ip6tables -t mangle -F
+ip6tables -t mangle -X
+ip6tables -P INPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+ip6tables -P OUTPUT ACCEPT
