0535656ef8fce84746f936d2b901b604a6b7e156
James Rouzier <jrouzier@inverse.ca>
2023-05-02 19:00:12 +0000

    update to 1.20.4

--- config.mk
+++ config.mk
@@ -52,7 +52,7 @@ LOCAL_REGISTRY = packetfence
 #
 # Golang
 #
-GOVERSION = go1.20.3
+GOVERSION = go1.20.4
 PF_BINARIES = pfhttpd pfdhcp pfdns pfstats pfdetect galera-autofix pfacct pfcron mysql-probe pfconnector sdnotify-proxy
 
 #
