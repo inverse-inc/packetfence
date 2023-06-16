c2cf31f9a2d677eb4c12920c83f63212376ad72d
James Rouzier <jrouzier@inverse.ca>
2023-06-16 14:00:13 +0000

    Add isSubType tag

--- html/pfappserver/lib/pfappserver/Form/Config/Pf.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Pf.pm
@@ -43,7 +43,7 @@ sub field_list {
     my $list = [];
     my $section = $self->section;
     return [] if !defined $section;
-    push @$list, id => { id => 'id', type => 'Text'};
+    push @$list, id => { id => 'id', type => 'Text', tags => {  isSubType =>  1 }};
     my $default_pf_config = pf::IniFiles->new(-file => $pf_default_file, -allowempty => 1);
     my @section_fields = $default_pf_config->Parameters($section);
     foreach my $name (@section_fields) {
