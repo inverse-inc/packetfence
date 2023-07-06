29927657e507a6a9de4be2ce00bb78f460448b70
Zhihao Ma <zma@akamai.com>
2023-07-06 16:02:07 -0400

    fix 404 issue when querying logs

--- go/caddy/api/dal_dns_audit_log.go
+++ go/caddy/api/dal_dns_audit_log.go
@@ -5,11 +5,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/inverse-inc/go-utils/log"
 	"github.com/inverse-inc/packetfence/go/caddy/dal/models"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/sql"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/types"
-	"github.com/inverse-inc/packetfence/go/db"
 	"github.com/jinzhu/gorm"
 	"github.com/julienschmidt/httprouter"
 	"io/ioutil"
@@ -23,14 +21,9 @@ type DnsAuditLog struct {
 	Ctx *context.Context
 }
 
-func NewDnsAuditLog() *DnsAuditLog {
-	DB, err := gorm.Open("mysql", db.ReturnURIFromConfig(context.Background()))
-	ctx := context.Background()
-	if err != nil {
-		log.LoggerWContext(ctx).Warn(err.Error())
-	}
+func NewDnsAuditLog(ctx context.Context, db *gorm.DB) *DnsAuditLog {
 	return &DnsAuditLog{
-		DB:  DB,
+		DB:  db,
 		Ctx: &ctx,
 	}
 }
--- go/caddy/api/dal_wrix.go
+++ go/caddy/api/dal_wrix.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/inverse-inc/go-utils/log"
 	"github.com/inverse-inc/packetfence/go/caddy/dal/models"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/sql"
 	"github.com/inverse-inc/packetfence/go/caddy/pfpki/types"
-	"github.com/inverse-inc/packetfence/go/db"
 	"github.com/jinzhu/gorm"
 	"github.com/julienschmidt/httprouter"
 	"io/ioutil"
@@ -21,14 +19,9 @@ type Wrix struct {
 	Ctx *context.Context
 }
 
-func NewWrix() *Wrix {
-	DB, err := gorm.Open("mysql", db.ReturnURIFromConfig(context.Background()))
-	ctx := context.Background()
-	if err != nil {
-		log.LoggerWContext(ctx).Warn(err.Error())
-	}
+func NewWrix(ctx context.Context, db *gorm.DB) *Wrix {
 	return &Wrix{
-		DB:  DB,
+		DB:  db,
 		Ctx: &ctx,
 	}
 }
