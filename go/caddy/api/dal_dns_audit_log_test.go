959d70090965a3587bf475a8dc8699f95e7bc3dd
Zhihao Ma <zma@akamai.com>
2023-07-10 15:39:25 -0400

    print out error logs when connecting to db.

--- go/caddy/api/dal_dns_audit_log_test.go
+++ go/caddy/api/dal_dns_audit_log_test.go
@@ -73,7 +73,10 @@ func removeDBTestEntriesDnsAuditLog(t *testing.T, id int64) error {
 func dalDnsAuditLog() http.HandlerFunc {
 	router := httprouter.New()
 	ctx := context.Background()
-	dbs, _ := gorm.Open("mysql", db.ReturnURIFromConfig(ctx))
+	dbs, err := gorm.Open("mysql", db.ReturnURIFromConfig(ctx))
+	if err != nil {
+		fmt.Println("error occured while connecting to mysql, ", err.Error())
+	}
 
 	NewDnsAuditLog(ctx, dbs).AddToRouter(router)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
--- go/caddy/api/dal_wrix_test.go
+++ go/caddy/api/dal_wrix_test.go
@@ -71,7 +71,10 @@ func removeDBTestEntriesWrix(t *testing.T, id string) error {
 func dalWrix() http.HandlerFunc {
 	router := httprouter.New()
 	ctx := context.Background()
-	dbs, _ := gorm.Open("mysql", db.ReturnURIFromConfig(ctx))
+	dbs, err := gorm.Open("mysql", db.ReturnURIFromConfig(ctx))
+	if err != nil {
+		fmt.Println("error occured while connecting to mysql, ", err.Error())
+	}
 
 	NewWrix(ctx, dbs).AddToRouter(router)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
