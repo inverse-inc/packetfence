c64f4d0a8b67e665aa412d1d05e0ad4851a4d1b5
vakarisz <vakarisz@yahoo.com>
2023-07-27 19:24:23 +0300

    Fix AD ldap explorer to search DNs attributes like memberOf
    This is not an ideal workaround the fact that in ldap you can't actually search DN attributes

--- html/pfappserver/root/src/views/Configuration/sources/_components/ldapCondition/common.js
+++ html/pfappserver/root/src/views/Configuration/sources/_components/ldapCondition/common.js
@@ -1,5 +1,6 @@
-import apiCall from '@/utils/api';
-import {intsToStrings} from '@/utils/convert';
+import apiCall from '@/utils/api'
+import {intsToStrings} from '@/utils/convert'
+import _ from 'lodash'
 
 export const ldapFormsSupported = ['LDAP', 'AD', 'EDIR']
 
@@ -13,11 +14,67 @@ export const parseLdapStringToArray = (ldapString) => {
   }
 }
 
+export const parseLdapResponseToAttributeArray = (ldapResponse, ldapAttribute) => {
+  const ldapEntries = Object.values(ldapResponse)
+  let parsedEntries = new Set()
+  for (let i = 0; i < ldapEntries.length; i++) {
+    let value = ldapEntries[i][ldapAttribute]
+    if (Array.isArray(value)) {
+      parsedEntries = new Set([...parsedEntries, ...value])
+    } else {
+      parsedEntries.add(value)
+    }
+  }
+  return Array.from(parsedEntries).filter((item) => {
+    return Boolean(item)
+  })
+}
+
+export const extractAttributeFromFilter = (filter) => {
+  return _.trim(filter.split('=')[0], '(')
+}
+
+const getAllAttributes = (server, filter, scope, base_dn, limit) => {
+  let searchAttribute = extractAttributeFromFilter(filter)
+  let allAttributeSearchQuery = `(${searchAttribute}=*)`
+  return sendLdapSearchRequest(server, allAttributeSearchQuery,
+    scope, [searchAttribute], base_dn, limit)
+}
+
+export const isAttributeDn = (server, filter, scope, base_dn) => {
+  return getAllAttributes(server, filter, scope, base_dn, 1).then((exampleEntry) => {
+    if(_.isEmpty(exampleEntry)) {
+      return false
+    }
+    exampleEntry = exampleEntry[Object.keys(exampleEntry)[0]]
+    exampleEntry = exampleEntry[Object.keys(exampleEntry)[0]]
+    if(Array.isArray(exampleEntry)) {
+      exampleEntry = exampleEntry[0]
+    }
+    return (exampleEntry.search('=') !== -1 && exampleEntry.search(',') !== -1)
+  })
+}
+
+export const searchDn = (server, filter, scope, base_dn) => {
+  return getAllAttributes(server, filter, scope, base_dn).then((allEntries) => {
+    let attributeSet = new Set();
+    for (let dn in allEntries) {
+      let entry = allEntries[dn]
+      let attribute = entry[Object.keys(entry)[0]]
+      if(!Array.isArray(attribute)) attribute = [attribute]
+      attribute.forEach((item) => attributeSet.add(item))
+    }
+    let regexQuery = new RegExp(_.trim(filter.split('=')[1], ')(*'), "i")
+    return [...attributeSet].filter((item) => regexQuery.test(item))
+  })
+}
+
 export const sendLdapSearchRequest = (server,
                                       filter = null,
                                       scope = null,
                                       attributes = null,
-                                      base_dn = null) => {
+                                      base_dn = null,
+                                      limit = null) => {
   server = intsToStrings(server)
   return apiCall.postQuiet('ldap/search',
     {
@@ -27,10 +84,11 @@ export const sendLdapSearchRequest = (server,
         scope: scope,
         attributes: attributes,
         base_dn: base_dn,
+        size_limit: limit,
       }
     }
   ).then(response => {
     delete response.data.quiet;
-    return response
+    return response.data
   })
 }
