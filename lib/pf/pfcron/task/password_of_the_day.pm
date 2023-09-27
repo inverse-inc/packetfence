b5ae562a01df71093308d91d66b75211148d34e7
Durand Fabrice <oeufdure@gmail.com>
2023-09-26 21:17:42 -0400

    Fixes for password of the day

--- lib/pf/pfcron/task/password_of_the_day.pm
+++ lib/pf/pfcron/task/password_of_the_day.pm
@@ -37,7 +37,7 @@ run the password generation task
 
 sub run {
     my ( $self ) = @_;
-    my $now = DateTime->now(time_zone => "local");
+    my $now = DateTime->now(time_zone => $ENV{TIMEZONE});
     my $logger = get_logger();
     my $sources = pf::authentication::getAuthenticationSourcesByType("Potd");
     my $new_password;
@@ -53,7 +53,7 @@ sub run {
         if(defined($password)){
             my $expiration = $password->{expiration};
             $expiration = DateTime::Format::MySQL->parse_datetime($expiration);
-            $expiration->set_time_zone("local");
+            $expiration->set_time_zone($ENV{TIMEZONE});
             if ( $now->epoch > $expiration->epoch) {
                 $new_password = pf::password::generate($source->{id},[{type => 'valid_from', value => $now},{type => 'expiration', value => pf::config::access_duration($source->{password_rotation})}],undef,'0',$source);
                 $self->send_email(pid => $source->{id},password => $new_password, email => $source->{password_email_update}, expiration => pf::config::access_duration($source->{password_rotation}));
