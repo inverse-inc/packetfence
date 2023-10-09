2e80cfa65ca971ad94c99760a75a9239f13d0315
root <Igor Stegarescu>
2023-10-09 02:51:31 +0000

    packetfence-perl  ci/cd

---
+++ addons/packetfence-perl/build_package.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -o nounset -o pipefail -o errexit
+
+workdir="${WORKDIR:-/root}"
+
+SCRIPT_DIR=/build/packages
+mkdir -p $SCRIPT_DIR
+RPM_BUILD=0
+
+if [ -f /etc/debian_version ]; then
+    echo "Debian system detected"
+    PF_PERL_ARCHIVE=packetfence_perl_deb_module_without_all_path.tar.gz
+elif [ -f /etc/redhat-release ]; then
+    echo "EL system detected"
+    PF_PERL_ARCHIVE=packetfence_perl_el_module_without_all_path.tar.gz
+    RPM_BUILD=1
+else
+    echo "Unknown system, exit"
+    exit 0
+fi
+
+cd "${BASE_DIR}"
+tar cvfz $SCRIPT_DIR/$PF_PERL_ARCHIVE ./
+if [ "${RPM_BUILD}" -eq 1 ]; then
+    cp -av /$workdir/rhel8 $SCRIPT_DIR
+    cp -v $SCRIPT_DIR/$PF_PERL_ARCHIVE $SCRIPT_DIR/rhel8/SOURCES/
+    cd $SCRIPT_DIR/rhel8
+    rpmbuild --define "_topdir `pwd`" -v -ba SPECS/packetfence-perl.spec
+    mkdir -p /$OUTPUT_DIRECTORY/rhel8/packages 
+    find /$OUTPUT_DIRECTORY/rhel8/packages/ -name "packetfence-perl*" -exec rm {} \;
+    cp -av  RPMS/x86_64/packetfence-perl*.rpm  /$OUTPUT_DIRECTORY/rhel8/packages
+fi
+
+if [ -f /etc/debian_version ]; then
+    cp -av /$workdir/debian $SCRIPT_DIR
+    cd $SCRIPT_DIR
+    dpkg-buildpackage --no-sign -rfakeroot
+    mkdir -p /$OUTPUT_DIRECTORY/debian/packages 
+    #remove old packages
+    find /$OUTPUT_DIRECTORY/debian/packages/ -name "packetfence-perl*" -exec rm {} \;
+    cp -av ../packetfence-perl* /$OUTPUT_DIRECTORY/debian/packages
+    cd -
+fi
--- addons/packetfence-perl/rhel8/SPECS/packetfence-perl.spec
+++ addons/packetfence-perl/rhel8/SPECS/packetfence-perl.spec
@@ -1,5 +1,5 @@
 Name:           packetfence-perl
-Version:        1.2.1
+Version:        1.2.2
 Release:        1%{?dist}
 Summary:        All modules loaded with cpan
 BuildArch:      x86_64
@@ -33,6 +33,9 @@ export PKG_CONFIG_PATH=/usr/lib/pkgconfig/
         /usr/local/pf/lib_perl/*
 
 %changelog
+* Wed Nov 07 2023 Inverse <info@inverse.ca> 1.2.2-1
+- Upgrade Net::HTTP 6.21 -> 6.23
+
 * Wed Feb 23 2022 Inverse <info@inverse.ca> 1.2.1-1
 - Add Test::Simple
    
