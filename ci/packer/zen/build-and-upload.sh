0ccffd56153219d2c7ef4a176daf69df2c5ea26d
JeGoi <13801368+JeGoi@users.noreply.github.com>
2023-10-25 15:36:36 -0400

    Add local, change variables, add zip_name

--- ci/packer/zen/build-and-upload.sh
+++ ci/packer/zen/build-and-upload.sh
@@ -29,32 +29,35 @@ generate_manifest() {
 }
 
 compress_vmware_ova() {
-    local ova_file=${1}
-    
-    # replace .ova by .zip
-    local zip_file=$(basename ${ova_file/.ova/.zip})
-    
-    zip -9 -j ${SF_RESULT_DIR}/${zip_file} ${ova_file}
+    local ova_file="${VMWARE_RESULT_DIR}/${VMX_OVA_NAME}"
+    local zip_file="${SF_RESULT_DIR}/${VMX_ZIP_NAME}"
+
+    echo "zip source ${ova_file} =>  dest: ${zip_file}"
+
+    zip -9 -j ${zip_file} ${ova_file}
 }
 
 upload_to_sf() {
     # warning: slashs at end of dirs are significant for rsync
     local src_dir="${SF_RESULT_DIR}/"
-    local dst_repo="${PUBLIC_REPO_DIR}/"
-    local dst_dir="${DEPLOY_SF_USER}@${DEPLOY_SF_HOST}:${dst_repo}"
-    declare -p src_dir dst_dir
-    # Get file size
-    file_size=$(du -bs ${SF_RESULT_DIR}/${zip_file} | cut -f1)
+    local zip_file="${src_dir}${VMX_ZIP_NAME}"
+    # Get file zip size
+    local file_size=$(du -bs ${zip_file} | cut -f1)
+    echo "zip file is ${zip_file} with size ${file_size}"
     # Source Forge limit is 7.0 GB
-    check_size=7516192768
+    local check_size=7516192768
+    echo "Max file is ${check_size}"
 
     if [ "$file_size" -le "$check_size" ]; then
+      local dst_repo="${PUBLIC_REPO_DIR}/"
+      local dst_dir="${DEPLOY_SF_USER}@${DEPLOY_SF_HOST}:${dst_repo}"
+      declare -p src_dir dst_dir
       # quotes to handle space in filename
       echo "rsync: $src_dir -> $dst_dir"
       # quotes to handle space in filename
       rsync -avz $src_dir "$dst_dir"
     else
-      echo "The file is bigger than Sourceforge Limite 7.0 GB. It will not be uploaded"
+      echo "The file is bigger than Sourceforge Limite 7.0 GB.\nIt will not be uploaded.\n${zip_file} size is ${file_size}"
       exit 27
     fi
 }
@@ -76,7 +79,7 @@ echo "===> Generate OVA for VMware"
 ovftool --shaAlgorithm=SHA1 --lax ${VMWARE_RESULT_DIR}/${VBOX_OVF_NAME} ${VMWARE_RESULT_DIR}/${VMX_OVA_NAME}
 
 echo "===> Compress VMware OVA"
-compress_vmware_ova ${VMWARE_RESULT_DIR}/${VMX_OVA_NAME}
+compress_vmware_ova
 
 echo "===> Upload to Sourceforge"
 upload_to_sf
