d0807dd8340f42851ca4cff4f802698079aaa32d
Igor Stegarescu <istegare@akamai.com>
2023-10-31 16:01:31 +0000

    rebuild_image improvements

--- addons/packetfence-perl/rebuild_images.sh
+++ addons/packetfence-perl/rebuild_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -o nounset -o pipefail -o errexit
+set -o nounset -o pipefail -o errexit
 
 SCRIPT_DIR=$(readlink -e $(dirname ${BASH_SOURCE[0]}))
 
@@ -55,70 +55,90 @@ configure_and_check() {
         echo "- $img"
     done
 }
+
+rebuild_base_image_pfdebian() {
+  source  ${PF_SRC_DIR}/containers/systemd-service
+  # build base image pfdebian using new package packetfence-perl
+
+  grep -Fxq 'COPY /packetfence-perl_*.deb /mnt'  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile || echo "COPY /packetfence-perl_*.deb /mnt" >>  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile
+  grep -Fxq 'RUN dpkg -i /mnt/packetfence-perl_*.deb'  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile || echo "RUN dpkg -i /mnt/packetfence-perl_*.deb" >> ${PF_SRC_DIR}/containers/pfdebian/Dockerfile
+
+  cp /mnt/packetfence-perl_*.deb ${PF_SRC_DIR}
+
+  #log_section "Build pfdebian docker image "
+  HTML_MOUNT=''
+  build_base_images
+  #tag image pfdebian
+  docker tag  $LOCAL_REGISTRY/pfdebian:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/pfdebian:$TAG_OR_BRANCH_NAME
+
+}
+
+rebuild_images() {
+  log_section "Build other image that are using pfsebian as base"
+  python3 -u ${SCRIPT_DIR}/rebuild_images.py -im "${CONTAINERS_IMAGES[@]}" -sp  ${PF_SRC_DIR}/addons/dev-helpers/build-local-container.sh 
+
+  for img in ${CONTAINERS_IMAGES}; do
+  #  bash ${PF_SRC_DIR}/addons/dev-helpers/build-local-container.sh ${img}
+    docker tag  $LOCAL_REGISTRY/${img}:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/${img}:$TAG_OR_BRANCH_NAME
+  done
+
+  #tag pfconnector image with pfconnector-client and pfconnector-server
+  for image_pfconnector in  pfconnector-server pfconnector-client 
+  do
+    docker tag  $LOCAL_REGISTRY/pfconnector:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/$image_pfconnector:$TAG_OR_BRANCH_NAME
+    docker tag  $LOCAL_REGISTRY/pfconnector:$TAG_OR_BRANCH_NAME $LOCAL_REGISTRY/$image_pfconnector:$TAG_OR_BRANCH_NAME
+  done
+
+}
+
+restart_packetfence() {
+  log_section "Restart all PF services"
+#  /usr/local/pf/bin/pfcmd service pf restart
+  echo "stop PF services"
+  /usr/local/pf/bin/pfcmd service pf stop
+  echo "stop packetfence-config"
+  systemctl stop packetfence-config
+  echo "start packetfence-config"
+  systemctl start packetfence-config
+  echo "start PF services"
+  /usr/local/pf/bin/pfcmd service pf start
+
+  log_section "Make sure all servises are started"
+  i=1
+  while [[ $i -lt 10 ]]; do
+    ((i++))
+    stopped_pf_services=$(/usr/local/pf/bin/pfcmd service pf status  | grep stopped | awk '{print $1}' | tr '\n' ',')
+    IFS="," read -ra stopped_pf_services <<< "$stopped_pf_services"
+    echo "stopped services: ${stopped_pf_services[@]}"
+    number_stopped_pf_services=${#stopped_pf_services[@]}
+
+    if [[ $number_stopped_pf_services -eq 0 ]]; then
+      echo 'all pf services are started'
+      /usr/local/pf/bin/pfcmd service pf status
+      break
+    else
+      for service in "${stopped_pf_services[@]}"
+      do 
+        echo "trying to start service: $service"
+        systemctl stop $service || true;
+        systemctl start $service || true;
+      done
+      continue
+    fi
+
+    echo "Some services did not started correctly"
+    /usr/local/pf/bin/pfcmd service pf status
+    exit 1
+  done
+}
+
 configure_and_check
 
-source  ${PF_SRC_DIR}/containers/systemd-service
-# build base image pfdebian using new package packetfence-perl
+rebuild_base_image_pfdebian
 
-[ grep -Fxq 'COPY /packetfence-perl_*.deb /mnt'  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile ] || echo "COPY /packetfence-perl_*.deb /mnt" >>  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile
-[ grep -Fxq 'RUN dpkg -i /mnt/packetfence-perl_*.deb'  ${PF_SRC_DIR}/containers/pfdebian/Dockerfile  ] || echo "RUN dpkg -i /mnt/packetfence-perl_*.deb" >> ${PF_SRC_DIR}/containers/pfdebian/Dockerfile
+rebuild_images
 
-cp /mnt/packetfence-perl_*.deb ${PF_SRC_DIR}
-
-#log_section "Build pfdebian docker image "
-build_base_images
-#tag image pfdebian
-docker tag  $LOCAL_REGISTRY/pfdebian:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/pfdebian:$TAG_OR_BRANCH_NAME
-
-log_section "Build other image that are using pfsebian as base"
-python3 -u ${SCRIPT_DIR}/rebuild_images.py -im "${CONTAINERS_IMAGES[@]}" -sp  ${PF_SRC_DIR}/addons/dev-helpers/build-local-container.sh 
-
-for img in ${CONTAINERS_IMAGES}; do
-#  bash ${PF_SRC_DIR}/addons/dev-helpers/build-local-container.sh ${img}
-  docker tag  $LOCAL_REGISTRY/${img}:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/${img}:$TAG_OR_BRANCH_NAME
-done
-
-#tag pfconnector image with pfconnector-client and pfconnector-server
-for image_pfconnector in  pfconnector-server pfconnector-client 
-do
-  docker tag  $LOCAL_REGISTRY/pfconnector:$TAG_OR_BRANCH_NAME $KNK_REGISTRY_URL/$image_pfconnector:$TAG_OR_BRANCH_NAME
-  docker tag  $LOCAL_REGISTRY/pfconnector:$TAG_OR_BRANCH_NAME $LOCAL_REGISTRY/$image_pfconnector:$TAG_OR_BRANCH_NAME
-done
-
-log_section "Restart all PF services"
-/usr/local/pf/bin/pfcmd service pf restart
-#for i in api-frontend haproxy-admin pfperl-api  httpd.admin_dispatcher; do
-#  /usr/local/pf/bin/pfcmd service $i restart
-#done
-
-log_section "Make sure that all servises are started"
-
-i=1
-while [[ $i -lt 10 ]]; do
-  ((i++))
-  stopped_pf_services=$(/usr/local/pf/bin/pfcmd service pf status  | grep stopped | awk '{print $1}' | tr '\n' ',')
-  IFS="," read -ra stopped_pf_services <<< "$stopped_pf_services"
-  echo $stopped_pf_services
-  number_stopped_pf_services=${#stopped_pf_services[@]}
-
-  if [[ $number_stopped_pf_services -eq 0 ]]; then
-    echo 'all pf services are started'
-    /usr/local/pf/bin/pfcmd service pf status
-    break
-  else
-    for service in "${stopped_pf_services[@]}"
-    do 
-      echo "trying to start service: $service"
-      systemctl stop $service || true;
-      systemctl start $service || true;
-    done
-    continue
-  fi
-
-  echo "Some services did not started correctly"
-  /usr/local/pf/bin/pfcmd service pf status
-  exit 1
-done
+restart_packetfence
 
 #log_section "Clean docker cash"
 #docker system prune -f
