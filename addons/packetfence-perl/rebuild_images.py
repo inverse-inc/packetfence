4b1547e8dd798f2c7512b659f24e50c674e1d8d0
Igor Stegarescu <istegare@akamai.com>
2023-10-31 02:13:52 +0000

    another test

--- addons/packetfence-perl/rebuild_images.py
+++ addons/packetfence-perl/rebuild_images.py
@@ -29,7 +29,7 @@ def docker_build(image):
 
 def parallel_execution(list_execute):
 #    with concurrent.futures.ThreadPoolExecutor(max_workers=number_exec) as executor:
-    with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
         # Start the load operations for eache entry from CSV file
         future_to_docker = {executor.submit(docker_build, image_name): image_name for image_name in list_execute}
         for future in concurrent.futures.as_completed(future_to_docker):
