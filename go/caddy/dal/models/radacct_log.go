c201e865a3d4389dd8171978c00a537595384a06
Durand Fabrice <oeufdure@gmail.com>
2023-11-02 16:09:32 -0400

    Forgotten changes

--- go/caddy/dal/models/radacct_log.go
+++ go/caddy/dal/models/radacct_log.go
@@ -43,12 +43,13 @@ func (a RadacctLog) Paginated(vars sql.Vars) (DBRes, error) {
 	var count int64
 
 	a.DB.Model(&RadacctLog{}).Count(&count)
-	res.Total = &count
+	counter := int(count)
+	res.Total = &counter
 	res.PrevCursor = &vars.Cursor
 	nextCursor := vars.Cursor + vars.Limit
 	res.NextCursor = &nextCursor
 
-	if vars.Cursor < count {
+	if vars.Cursor < counter {
 		sqls, err := vars.Sql(a)
 		if err != nil {
 			return DBRes{}, err
@@ -78,12 +79,13 @@ func (a RadacctLog) Search(vars sql.Vars) (DBRes, error) {
 		return res, errors.New("entries not found")
 	}
 
-	res.Total = &count
+	counter := int(count)
+	res.Total = &counter
 	res.PrevCursor = &vars.Cursor
 	nextCursor := vars.Cursor + vars.Limit
 	res.NextCursor = &nextCursor
 
-	if vars.Cursor < count {
+	if vars.Cursor < counter {
 		db := a.DB.Select(sqls.Select).Where(sqls.Where.Query, sqls.Where.Values...).Order(sqls.Order).Offset(sqls.Offset).Limit(sqls.Limit).Find(&items)
 		if db.Error != nil {
 			return DBRes{}, db.Error
