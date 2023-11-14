f72ce7f847aa90be3ad1b737d4ffd44aa986430b
Zhihao Ma <zma@akamai.com>
2023-11-14 16:33:04 -0500

    change migration script to handle multiple AD sections

--- addons/upgrade/to-13.1-move-ntlm-auth-to-rest.pl
+++ addons/upgrade/to-13.1-move-ntlm-auth-to-rest.pl
@@ -18,7 +18,6 @@ use lib qw(/usr/local/pf/lib /usr/local/pf/lib_perl/lib/perl5);
 use pf::IniFiles;
 use pf::file_paths qw($authentication_config_file $domain_config_file);
 use pf::util;
-run_as_pf();
 
 my $ini = pf::IniFiles->new(-file => $domain_config_file, -allowempty => 1);
 
@@ -28,6 +27,57 @@ unless ($ini) {
 
 my $updated = 0;
 for my $section (grep {/^\S+$/} $ini->Sections()) {
+
+    if (($ini->exists($section, 'machine_account')) || ($ini->exists($section, 'machine_account_password'))) {
+        print("Section: ", $section, " already has machine_account and machine_account_password set. skipped\n");
+        next;
+    }
+
+    my $samba_conf_path = "/chroots/$section/etc/samba/$section.conf";
+    my $samba_ini = pf::IniFiles->new(-file => $samba_conf_path, -allowempty => 1);
+    unless ($samba_ini) {
+        print("unable to find correspond samba conf file in $samba_conf_path, terminated\n");
+        exit;
+    }
+
+    my $dns_name = $ini->val($section, "dns_name");
+    my $work_group = $samba_ini->val("global", "workgroup");
+    my $realm = $samba_ini->val("global", "realm");
+
+    print("---- section: $section\n");
+    print("---- dns name is: $dns_name \n");
+    print("---- work_group is: $work_group\n");
+    print("---- realm is: $realm\n");
+
+    # the tdb file should be located at /var/lib/samba/secrets.tdb, but here we use cache instead
+    my $secret_tdb_file = "/chroots/$section/var/cache/samba/secrets.tdb";
+    print("---- tdb file is located in: $secret_tdb_file \n");
+
+    my $exit_code = 0;
+
+    # extract machine account from tdb file
+    my $machine_account_key = "SECRETS/SALTING_PRINCIPAL/DES/$dns_name";
+    my $tdb_secret_host_value;
+    ($exit_code, $tdb_secret_host_value) = tdbdump_get_value("/chroots/$section/var/cache/samba/secrets.tdb", $machine_account_key);
+    if ($exit_code == 0 && $tdb_secret_host_value ne "") {
+        my $machine_account = extract_machine_account($tdb_secret_host_value, $dns_name);
+        if ($machine_account ne "") {
+            print("  ---- machine_account is: $machine_account\n");
+        }
+    }
+
+    # extract machine account password from tdb file
+    my $machine_account_password_key = "SECRETS/MACHINE_PASSWORD/$work_group";
+    my $tdb_secret_machine_password_value;
+    ($exit_code, $tdb_secret_machine_password_value) = tdbdump_get_value("/chroots/$section/var/cache/samba/secrets.tdb", $machine_account_password_key);
+    if ($exit_code == 0 && $tdb_secret_machine_password_value ne "") {
+
+        $tdb_secret_machine_password_value = $tdb_secret_machine_password_value;
+        ($exit_code, my $machine_password) = extract_machine_password($tdb_secret_machine_password_value);
+
+    }
+
+
     if (!$ini->exists($section, 'server_name')) {
         my $machine_account = getServerName $section;
         ini->newval($section, 'server_name', $samba_server_name);
@@ -44,28 +94,54 @@ for my $section (grep {/^\S+$/} $ini->Sections()) {
         ini->newval($section, 'password', $password);
         $updated |= 1;
     }
-
 }
 
 if ($updated) {
     $ini->RewriteConfig();
 }
 
-sub getServerName {
-    my $domain = $_[0];
+sub tdbdump_get_value {
+    my ($tdb_file, $key) = @_;
+    my $cmd = "/usr/bin/tdbdump $tdb_file -k $key";
 
-    my $r = system("/usr/bin/tdbdump /chroots/$domain/var/cache/samba/gencache.tdb| grep NEG_CONN_CACHE | awk '{print $NF}' | sed s/\"//g | sed s/\00// | awk -F , '{print $NF}'");
-    @r
+    my $result = qx($cmd);
+    my $exit_code = $? >> 8;
+
+    return $exit_code, $result;
 }
-sub getMachineAccount {
-    my $domain = $_[0];
-    my @r = readpipe("/usr/bin/tdbdump /chroots/$domain/var/cache/samba/gencache.tdb| grep 'SECRETS/SID/'| awk -F / '{print $3}' |sed s/\"//");
-    $r
+
+sub extract_machine_account {
+    my ($tdb_secret_host_string, $dns_name) = @_;
+
+    if ($tdb_secret_host_string =~ /^host\/(.*?)@(.*?)\\00$/i) {
+        my $hostname = $1;
+        my $domain = $2;
+
+        if ($hostname =~ /^(.*?)\.$domain/i) {
+            return $1 . '$';
+        }
+    }
+    else {
+        return "";
+    }
 }
-sub getMachineAccountPassword {
-    my $domain = $_[0];
-    my $r = "";
-    $r
+
+sub extract_machine_password {
+    my ($raw_password) = @_;
+
+    my $replace_cmd = "sed s/\\\\00//g| sed 's/\\\\//g'";
+    my $decrypt_cmd = 'import hashlib,binascii,sys;input=sys.stdin.read().strip();print(binascii.hexlify(hashlib.new("md4",binascii.unhexlify(input.replace("\\\", "").replace("\00","")).decode("utf-8").encode("utf-16le")).digest()))';
+    my $cmd = "echo -n '$raw_password'| $replace_cmd| python -c '$decrypt_cmd'";
+
+    my $result = qx($cmd);
+    my $exit_code = $? >> 8;
+    my $password = "";
+    if ($exit_code != 0 && $result ne "") {
+        if ($result =~ /b'([a-f0-9]+)'/) {
+            $password = $1;
+        }
+    }
+    return $exit_code, $password;
 }
 
 
