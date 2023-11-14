00c1ebbe34e7e2812b53961d31d093b2dce98628
Igor Stegarescu <istegare@akamai.com>
2023-11-14 15:26:55 +0000

    improve build_package.sh script and dpkg-sig list check

--- addons/packetfence-perl/build_package.sh
+++ addons/packetfence-perl/build_package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -o nounset -o pipefail -o errexit
 
-workdir="${WORKDIR:-/root}"
-
+WORKDIR="${WORKDIR:-/root}"
+BASE_DIR="${BASE_DIR:-/usr/local/pf/lib/perl_modules}"
+OUTPUT_DIRECTORY="${OUTPUT_DIRECTORY:-/mnt/output/}"
 SCRIPT_DIR=/build/packages
+
 mkdir -p $SCRIPT_DIR
 RPM_BUILD=0
 
@@ -22,7 +24,7 @@ fi
 cd "${BASE_DIR}"
 tar cvfz $SCRIPT_DIR/$PF_PERL_ARCHIVE ./
 if [ "${RPM_BUILD}" -eq 1 ]; then
-    cp -av /$workdir/rhel8 $SCRIPT_DIR
+    cp -av /$WORKDIR/rhel8 $SCRIPT_DIR
     cp -v $SCRIPT_DIR/$PF_PERL_ARCHIVE $SCRIPT_DIR/rhel8/SOURCES/
     cd $SCRIPT_DIR/rhel8
     rpmbuild --define "_topdir `pwd`" -v -ba SPECS/packetfence-perl.spec
@@ -32,7 +34,7 @@ if [ "${RPM_BUILD}" -eq 1 ]; then
 fi
 
 if [ -f /etc/debian_version ]; then
-    cp -av /$workdir/debian $SCRIPT_DIR
+    cp -av /$WORKDIR/debian $SCRIPT_DIR
     cd $SCRIPT_DIR
     dpkg-buildpackage --no-sign -rfakeroot
     mkdir -p /$OUTPUT_DIRECTORY/debian/packages 
