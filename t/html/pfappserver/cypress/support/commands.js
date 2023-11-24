45cbe6ba25f626f8e178b53abe042b6e34c3c940
Darren Satkunas <dsatkuna@akamai.com>
2023-11-24 09:25:40 -0800

    add decorations

--- t/html/pfappserver/cypress/support/commands.js
+++ t/html/pfappserver/cypress/support/commands.js
@@ -56,10 +56,11 @@ Cypress.Commands.add('pfConfiguratorDisable', () => {
 
 Cypress.Commands.add('formFillNamespace', (data, selector = 'body') => {
   return cy.get(selector)
-    .get('*[data-namespace]:not([disabled])')
+    .get('*[data-namespace]')
     .should("have.length.gte", 0)
-    .then(element => {
+    .each(element => {
       const namespace = element.attr('data-namespace')
+      const chosen = element.attr('data-chosen')
       if (namespace in data && data[namespace]) {
         const value = data[namespace]
         const type = element.attr('type')
@@ -68,16 +69,27 @@ Cypress.Commands.add('formFillNamespace', (data, selector = 'body') => {
         switch (true) {
           case tagName === 'input' && ['text', 'password'].includes(type):
           case tagName === 'textarea':
-            cy.get(e)
-              .should('be.visible')
+            cy.get(e).as(namespace)
+            cy.get(`@${namespace}`)
               .clear({ log: true, force: true })
               .type(value, { log: true, force: true })
             break
           case tagName === 'input' && ['range'].includes(type):
             // TODO
             break
+          case tagName === 'div' && !!chosen:
+            cy.get(e).within(() => {
+              const values = ((Array.isArray(value)) ? value : [value]).reduce((values, _value) => {
+                return `${values}${_value}{enter}`
+              }, '')
+              cy.get('input.multiselect__input').as(namespace)
+              cy.get(`@${namespace}`)
+                .clear({ log: true, force: true })
+                .type(values, { log: true, force: true })
+            })
+            break
           default:
-            throw new Error(`unhandled element <${tagName} type="${type || ''}" data-namespace="${namespace}" />`)
+            throw new Error(`unhandled element <${tagName} type="${type || ''}" data-chosen="${chosen}" data-namespace="${namespace}" />`)
         }
       }
     })
