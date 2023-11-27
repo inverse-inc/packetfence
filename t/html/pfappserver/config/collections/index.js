6b59aa8f0b7b0645026120af4ca0db975dded2ab
Darren Satkunas <dsatkuna@akamai.com>
2023-11-27 12:49:48 -0800

    add mfas

--- t/html/pfappserver/config/collections/index.js
+++ t/html/pfappserver/config/collections/index.js
@@ -2,6 +2,7 @@ const clouds = require('./modules/clouds')
 const domains = require('./modules/domains')
 const eventLoggers = require('./modules/eventLoggers')
 const firewalls = require('./modules/firewalls')
+const mfas = require('./modules/mfas')
 const realms = require('./modules/realms')
 const roles = require('./modules/roles')
 const scanEngines = require('./modules/scanEngines')
@@ -12,8 +13,9 @@ const syslogParsers = require('./modules/syslogParsers')
 module.exports = {
 //  clouds,
 //  domains,
-  eventLoggers,
+//  eventLoggers,
 //  firewalls,
+  mfas,
 //  realms,
 //  roles,
 //  scanEngines,
