b22328c81c6861ff03e2cd8dd72c7be1de24e150
Darren Satkunas <dsatkuna@akamai.com>
2023-11-27 12:12:33 -0800

    add firewalls

--- t/html/pfappserver/config/collections/index.js
+++ t/html/pfappserver/config/collections/index.js
@@ -1,5 +1,6 @@
 const clouds = require('./modules/clouds')
 const domains = require('./modules/domains')
+const firewalls = require('./modules/firewalls')
 const realms = require('./modules/realms')
 const roles = require('./modules/roles')
 const scanEngines = require('./modules/scanEngines')
@@ -7,8 +8,9 @@ const sources = require('./modules/sources')
 const switches = require('./modules/switches')
 
 module.exports = {
-  clouds,
+//  clouds,
 //  domains,
+  firewalls,
 //  realms,
 //  roles,
 //  scanEngines,
