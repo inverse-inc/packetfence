80fbe8171609218cb6012cfdd943c812aa696dd3
Darren Satkunas <dsatkuna@akamai.com>
2023-11-27 11:35:18 -0800

    add scan engines

--- t/html/pfappserver/config/collections/index.js
+++ t/html/pfappserver/config/collections/index.js
@@ -1,6 +1,7 @@
 const domains = require('./modules/domains')
 const realms = require('./modules/realms')
 const roles = require('./modules/roles')
+const scanEngines = require('./modules/scanEngines')
 const sources = require('./modules/sources')
 const switches = require('./modules/switches')
 
@@ -8,6 +9,7 @@ module.exports = {
 //  domains,
 //  realms,
 //  roles,
-  sources,
+  scanEngines,
+//  sources,
 //  switches
 }
