c2f89f3da59008ff4c8a81845ae558c232740585
Darren Satkunas <dsatkuna@akamai.com>
2023-12-08 10:42:38 -0800

    add billing tiers, add form remap function

--- t/html/pfappserver/config/collections/modules/billingTiers.js
+++ t/html/pfappserver/config/collections/modules/billingTiers.js
@@ -1,23 +1,37 @@
 const { SCOPE_INSERT, SCOPE_UPDATE, SCOPE_DELETE } = require('../config');
-const collection_url = '/configuration/domains';
-const resource_url = id => `/configuration/domain/${id}`;
-const fixture = 'collections/domain.json';
-const timeout = 15E3;
+const collection_url = '/configuration/billing_tiers';
+const resource_url = id => `/configuration/billing_tier/${id}`;
+const fixture = 'collections/billingTier.json';
+
+const map = (value, namespace = '') => {
+  if (namespace.endsWith('.unit')) { // re-map access durations
+      switch (value) {
+        case 's': return 'second'
+        case 'm': return 'minute'
+        case 'h': return 'hour'
+        case 'D': return 'day'
+        case 'W': return 'week'
+        case 'M': return 'month'
+        case 'Y': return 'year'
+      }
+  }
+  return value
+}
 
 module.exports = {
-  id: 'domains',
-  description: 'Domains',
+  id: 'billingTiers',
+  description: 'Billing Tiers',
   tests: [
     {
-      description: 'Domains - Create New',
+      description: 'Billing Tiers - Create New',
       scope: SCOPE_INSERT,
       fixture,
-      timeout,
+      map,
       url: collection_url,
       interceptors: [
         {
           method: 'POST',
-          url: '/api/**/config/domains',
+          url: '/api/**/config/billing_tiers',
           expectRequest: (request, fixture) => {
             Object.keys(fixture).forEach(key => {
               expect(request.body).to.have.property(key)
@@ -31,15 +45,14 @@ module.exports = {
       ]
     },
     {
-      description: 'Domains - Update Existing',
+      description: 'Billing Tiers - Update Existing',
       scope: SCOPE_UPDATE,
       fixture,
-      timeout,
       url: resource_url,
       interceptors: [
         {
           method: '+(PATCH|PUT)',
-          url: '/api/**/config/domain/**',
+          url: '/api/**/config/billing_tier/**',
           expectRequest: (request, fixture) => {
             Object.keys(fixture).forEach(key => {
               expect(request.body).to.have.property(key)
@@ -53,14 +66,13 @@ module.exports = {
       ]
     },
     {
-      description: 'Domains - Delete Existing',
+      description: 'Billing Tiers - Delete Existing',
       scope: SCOPE_DELETE,
       fixture,
-      timeout,
       url: resource_url,
       interceptors: [
         {
-          method: 'DELETE', url: '/api/**/config/domain/**', expectResponse: (response, fixture) => {
+          method: 'DELETE', url: '/api/**/config/billing_tier/**', expectResponse: (response, fixture) => {
             expect(response.statusCode).to.equal(200)
           }
         }
