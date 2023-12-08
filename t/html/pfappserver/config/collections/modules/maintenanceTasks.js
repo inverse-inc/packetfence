0d1f57dd934234dcd74a63a81c48ff34056d000f
Darren Satkunas <dsatkuna@akamai.com>
2023-12-08 04:55:06 -0800

    add maintenance tasks

---
+++ t/html/pfappserver/config/collections/modules/maintenanceTasks.js
@@ -0,0 +1,35 @@
+const { SCOPE_UPDATE } = require('../config');
+const maintenanceTasks = require('../../../cypress/fixtures/runtime/maintenanceTasks.json');
+const collection_url = '/configuration/maintenance_tasks';
+const resource_url = id => `/configuration/maintenance_task/${id}`;
+const timeout = 10E3;
+
+module.exports = {
+  id: 'maintenaceTasks',
+  description: 'Maintenance Tasks',
+  tests: maintenanceTasks.map(id => {
+    const fixture = require(`../../../cypress/fixtures/runtime/maintenanceTask-${id}.json`)
+    return {
+      description: `Maintenance Task (${id}) - Update Existing`,
+      scope: SCOPE_UPDATE,
+      fixture: `/runtime/maintenanceTask-${id}.json`,
+      timeout,
+      url: resource_url,
+      interceptors: [
+        {
+          method: '+(PATCH|PUT)',
+          url: '/api/**/config/maintenance_task/**',
+          expectRequest: (request, fixture) => {
+            Object.keys(fixture).forEach(key => {
+              expect(request.body).to.have.property(key)
+              expect(request.body[key]).to.deep.equal(fixture[key], key)
+            })
+          },
+          expectResponse: (response, fixture) => {
+            expect(response.statusCode).to.equal(200)
+          }
+        }
+      ]
+    }
+  })
+};
