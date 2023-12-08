56c41985fdd1c52e90b2117916293800b107b883
Darren Satkunas <dsatkuna@akamai.com>
2023-12-08 04:52:59 -0800

    add runtime fixtures to iterate static collections

--- t/html/pfappserver/cypress/config/cypress.config-views.js
+++ t/html/pfappserver/cypress/config/cypress.config-views.js
@@ -1,10 +1,38 @@
-const config = require('./cypress.config.js');
+const fs = require('fs-extra');
+const fetch = require('node-fetch');
+const base = require('./cypress.config.js');
+
+
+
 
 module.exports = {
-  ...config,
+  ...base,
 
   e2e: {
-    ...config.e2e,
+    ...base.e2e,
+
+    setupNodeEvents: (on, config) => {
+      on('before:run', async (details) => {
+        await fs.readFile('/usr/local/pf/conf/unified_api_system_pass', 'utf-8', async (err, password) => {
+          // login, get auth token
+          let request = JSON.stringify({ username: 'system', password: password.trim() });
+          let response = await fetch('https://localhost:1443/api/v1/login', { method: 'POST', body: request });
+          let body = await response.text();
+          let { token } = JSON.parse(body);
+          let headers = { 'Authorization': `Bearer ${token}` };
+
+          // get maintenance tasks, write fixtures
+          response = await fetch('https://localhost:1443/api/v1/config/maintenance_tasks?limit=1000', { method: 'GET', headers });
+          body = await response.text();
+          let { items = [] } = JSON.parse(body);
+          await fs.writeFile('../fixtures/runtime/maintenanceTasks.json', JSON.stringify(items.map(item => item.id), null, 2));
+          await items.forEach(async item => {
+            await fs.writeFile(`../fixtures/runtime/maintenanceTask-${item.id}.json`, JSON.stringify(item, null, 2));
+          })
+        })
+      });
+      return base.e2e.setupNodeEvents(on, config);
+    },
 
     specPattern: [
       'cypress/specs/e2e/*-views/20*.cy.{js,jsx,ts,tsx}',
