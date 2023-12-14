ccd3ea189606042dab7f5e090e3cdad97084820c
Darren Satkunas <dsatkuna@akamai.com>
2023-12-14 11:34:52 -0800

    add fingerbank DHCP6 enterprises

--- t/html/pfappserver/config/collections/modules/fingerbankDhcpv6Enterprises.js
+++ t/html/pfappserver/config/collections/modules/fingerbankDhcpv6Enterprises.js
@@ -1,25 +1,25 @@
 const { SCOPE_INSERT, SCOPE_UPDATE, SCOPE_DELETE } = require('../config');
 
-const collection_url = 'configuration/fingerbank/dhcpv6_fingerprints';
-const resource_url = id => `/configuration/fingerbank/local/dhcpv6_fingerprint/${id}`;
-const fixture = 'collections/fingerbank/dhcpv6Fingerprint.json';
+const collection_url = 'configuration/fingerbank/dhcpv6_enterprises';
+const resource_url = id => `/configuration/fingerbank/local/dhcpv6_enterprise/${id}`;
+const fixture = 'collections/fingerbank/dhcpv6Enterprise.json';
 
 module.exports = {
-  id: 'fingerbankDhcpv6Fingerprints',
-  description: 'Fingerbank DHCPv6 Fingerprints',
+  id: 'fingerbankDhcpv6Enterprises',
+  description: 'Fingerbank DHCPv6 Enterprises',
   tests: [
     {
-      description: 'Fingerbank DHCPv6 Fingerprints - Create New',
+      description: 'Fingerbank DHCPv6 Enterprises - Create New',
       scope: SCOPE_INSERT,
       fixture,
       url: collection_url,
       selectors: {
-        buttonNewSelectors: ['button[type="button"]:contains(New DHCPv6 Fingerprint)'],
+        buttonNewSelectors: ['button[type="button"]:contains(New DHCPv6 Enterprise)'],
       },
       interceptors: [
         {
           method: 'POST',
-          url: '/api/**/fingerbank/local/dhcp6_fingerprints',
+          url: '/api/**/fingerbank/local/dhcp6_enterprises',
           expectRequest: (request, fixture) => {
             Object.keys(fixture).forEach(key => {
               expect(request.body).to.have.property(key)
@@ -34,7 +34,7 @@ module.exports = {
       ]
     },
     {
-      description: 'Fingerbank DHCPv6 Fingerprints - Update Existing',
+      description: 'Fingerbank DHCPv6 Enterprises - Update Existing',
       scope: SCOPE_UPDATE,
       fixture,
       url: resource_url,
@@ -42,7 +42,7 @@ module.exports = {
       interceptors: [
         {
           method: '+(PATCH|PUT)',
-          url: '/api/**/fingerbank/local/dhcp6_fingerprint/**',
+          url: '/api/**/fingerbank/local/dhcp6_enterprise/**',
           expectRequest: (request, fixture) => {
             Object.keys(fixture).forEach(key => {
               expect(request.body).to.have.property(key)
@@ -56,14 +56,14 @@ module.exports = {
       ]
     },
     {
-      description: 'Fingerbank DHCPv6 Fingerprints - Delete Existing',
+      description: 'Fingerbank DHCPv6 Enterprises - Delete Existing',
       scope: SCOPE_DELETE,
       fixture,
       url: resource_url,
       idFrom: (_, cache) => cache.id,
       interceptors: [
         {
-          method: 'DELETE', url: '/api/**/fingerbank/local/dhcp6_fingerprint/**', expectResponse: (response, fixture) => {
+          method: 'DELETE', url: '/api/**/fingerbank/local/dhcp6_enterprise/**', expectResponse: (response, fixture) => {
             expect(response.statusCode).to.equal(200)
           }
         }
