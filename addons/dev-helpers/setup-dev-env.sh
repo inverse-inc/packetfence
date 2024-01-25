91624ffde4b150323478532929253b7c7b9804ce
Zhihao Ma <zma@akamai.com>
2024-01-25 16:07:07 -0500

    Install required header files for building ntlm_auth_wrapper.

--- addons/dev-helpers/setup-dev-env.sh
+++ addons/dev-helpers/setup-dev-env.sh
@@ -31,6 +31,10 @@ git clone https://github.com/inverse-inc/packetfence /usr/local/pf
 log_section "Set the safe.directory in git"
 git config --global --add safe.directory /usr/local/pf
 
+log_section "install epel and required header files"
+dnf install -y epel-release
+dnf install -y libcurl-devel cjson-devel
+
 cd /usr/local/pf/
 
 BRANCH=${BRANCH:-devel}
