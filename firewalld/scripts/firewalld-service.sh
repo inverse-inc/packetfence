7c7a35d63e43a1d1b0639681eadad823ba596188
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-01-29 21:50:10 +0000

    pfdhcp in firewalld for eth0

--- firewalld/scripts/firewalld-service.sh
+++ firewalld/scripts/firewalld-service.sh
@@ -8,7 +8,7 @@ FIREWALLD_CMD_PATH=${5:-/usr/bin/firewall-cmd}
 
 PERMANENT_VAL="--permanent"
 FIREWALLD_DEFAULT_CONFIG_PF_PATH="/usr/local/pf/firewalld/services"
-FIREWALLD_APPLIED_CONFIG_PF_PATH="/usr/local/pf/conf/firewalld/applied/services"
+FIREWALLD_APPLIED_CONFIG_PF_PATH="/usr/local/pf/var/firewalld/services"
 
 #
 # Service part
@@ -20,24 +20,38 @@ then
   exit 1
 fi
 
-if ! [ -f ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}/${SERVICE}.xml ];
-then
-  echo "Service ${SERVICE} Unavailable in configuration";
-  echo "Check if ${SERVICE}.xml is in ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}";
-  exit 1;
-fi
-
 if [ ${PERMANENT} != "yes" ];
 then
   PERMANENT_VAL=""
 fi
 
-printf "Service ${SERVICE} is in default config\n"
-/bin/bash -c "cp ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}/${SERVICE}.xml ${FIREWALLD_APPLIED_CONFIG_PF_PATH}/${SERVICE}.xml"
-printf "Service ${SERVICE} is now in applied config\n"
-/bin/bash -c "${FIREWALLD_CMD_PATH} --zone=${ZONE} --${STATUS}-service ${SERVICE} ${PERMANENT_VAL}"
-echo "Service ${SERVICE} has been added to ${ZONE}"
-/bin/bash -c "${FIREWALLD_CMD_PATH} --reload";
-echo "Firewalld config reloaded";
-exit
+# handle service's file
+if [[ "${STATUS}" == "add" ]];
+then
+  if ! [ -f ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}/${SERVICE}.xml ];
+  then
+    echo "Service ${SERVICE} Unavailable in configuration";
+    echo "Check if ${SERVICE}.xml is in ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}";
+    exit 1;
+  fi
+  printf "Service ${SERVICE} is ${STATUS}ed in Firewalld applied configuration.\n"
+  /bin/bash -c "cp ${FIREWALLD_DEFAULT_CONFIG_PF_PATH}/${SERVICE}.xml ${FIREWALLD_APPLIED_CONFIG_PF_PATH}/${SERVICE}.xml"
+else
+  printf "Service ${SERVICE} is ${STATUS}ed from Firewalld applied configuration.\n"
+  /bin/bash -c "rm -rf ${FIREWALLD_APPLIED_CONFIG_PF_PATH}/${SERVICE}.xml*"
+fi
 
+# handle service in zone
+if [[ "${STATUS}" == "add" ]];
+then
+  echo "Service ${SERVICE} ${STATUS}ed from Zone ${ZONE} configuration status:"
+else
+  echo "Service ${SERVICE} ${STATUS}d from Zone ${ZONE} configuration status:"
+fi
+/bin/bash -c "${FIREWALLD_CMD_PATH} --zone=${ZONE} --${STATUS}-service ${SERVICE} ${PERMANENT_VAL}"
+
+# handle status
+echo "Firewalld config status:";
+/bin/bash -c "${FIREWALLD_CMD_PATH} --reload";
+
+exit
