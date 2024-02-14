bb7be071289954a0be36bfde782605e68a4a1555
James Rouzier <jrouzier@inverse.ca>
2024-02-14 13:54:55 +0000

    add test for non biflow

--- go/cron/aggregator_test.go
+++ go/cron/aggregator_test.go
@@ -28,6 +28,14 @@ func TestAggregator(t *testing.T) {
 					BiFlow:  1,
 				},
 				{
+					SrcIp:   netip.AddrFrom4([4]byte{1, 1, 1, 2}),
+					DstIp:   netip.AddrFrom4([4]byte{1, 1, 1, 1}),
+					SrcPort: 80,
+					DstPort: 1025,
+					Proto:   6,
+					BiFlow:  2,
+				},
+				{
 					SrcIp:   netip.AddrFrom4([4]byte{1, 1, 1, 1}),
 					DstIp:   netip.AddrFrom4([4]byte{1, 1, 1, 2}),
 					SrcPort: 1024,
@@ -56,4 +64,45 @@ func TestAggregator(t *testing.T) {
 	if ne[0].Count != 2 {
 		t.Fatalf("Not aggreated properly")
 	}
+
+	events = []*PfFlows{
+		{
+			Flows: &[]PfFlow{
+				{
+					SrcIp:   netip.AddrFrom4([4]byte{1, 1, 1, 1}),
+					DstIp:   netip.AddrFrom4([4]byte{1, 1, 1, 2}),
+					SrcPort: 1024,
+					DstPort: 80,
+					Proto:   6,
+					BiFlow:  0,
+				},
+				{
+					SrcIp:   netip.AddrFrom4([4]byte{1, 1, 1, 1}),
+					DstIp:   netip.AddrFrom4([4]byte{1, 1, 1, 2}),
+					SrcPort: 1025,
+					DstPort: 80,
+					Proto:   6,
+					BiFlow:  0,
+				},
+				{
+					SrcIp:   netip.AddrFrom4([4]byte{1, 1, 1, 1}),
+					DstIp:   netip.AddrFrom4([4]byte{1, 1, 1, 2}),
+					SrcPort: 1024,
+					DstPort: 80,
+					Proto:   6,
+					BiFlow:  0,
+				},
+			},
+		},
+	}
+	a.PfFlowsChan <- events
+	ne = <-networkEventChan
+	if len(ne) != 1 {
+		t.Fatalf("Not aggreated to a single network event")
+	}
+
+	if ne[0].Count != 2 {
+		t.Fatalf("Not aggreated properly")
+	}
+
 }
