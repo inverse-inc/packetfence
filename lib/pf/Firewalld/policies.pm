76f3fe8136e7f8e8304bb26aeb04aee7d0af3c1c
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-02-16 21:33:13 +0000

    Clean firewalld policies

--- lib/pf/Firewalld/policies.pm
+++ lib/pf/Firewalld/policies.pm
@@ -1,8 +1,8 @@
-package pf::firewalld::services;
+package pf::firewalld::policies;
 
 =head1 NAME
 
-pf::firewalld::services
+pf::firewalld::policies
 
 =cut
 
@@ -29,70 +29,6 @@ use pf::cluster qw($host_id);
 use pf::file_paths qw(
     $firewalld_services_config_file
 );
-
-
-sub get_all_firewalld_services {
-  my $services = `firewall-cmd --get-services`;
-  get_logger->info("Services are: $services");
-  my @all_services = split / /, $services;
-  my %h;
-  foreach $val ( @allservices ) {
-    $h{$val}="1"; 
-  }
-  return %h;
-}
-
-
-# need a function that read the config file and create an xml file related to the service (use of template toolkit)
-# need a function that return structured content of the config file
-# need a function that check xml file integrity
-
-# Vars
-my $path_bin=`which firewalld-cmd`;
-my $path_services_default=`which firewalld-cmd`;
-my $config_path_default="/usr/local/pf/firewalld/services";
-my $config_path_applied="/usr/local/pf/var/firewalld/services"
-
-# Functions
-sub service_in_default {
-  my $service   = shift;
-  if ( -s $config_path_default/$service.xml ) {
-    print "Service $service Available in $config_path_default";
-    return 1 ;
-  } else {
-    print "Service $service Unavailable in $config_path_default";
-    return 0 ;
-  }
-}
-
-sub service_is_in_config {
-  my $service   = shift;
-  if ( -s $config_path_applied/$service.xml ) {
-    print "Service $service Available in $config_path_applied";
-    return 1 ;
-  } else {
-    print "Service $service Unavailable in $config_path_applied";
-    return 0 ;
-  }
-}
-
-sub service_is_available {
-
-}
-
-sub service_copy_from_default_to_applied {
-  print "Service $service is added in Firewalld applied configuration.\n";
-  copy("$config_path_default/$service.xml", "$config_path_applied/$service.xml") or die "copy failed: $!";
-}
-
-sub service_remove_from_applied {
-  print "Service $service is removed from Firewalld applied configuration.\n";
-  unlink "$config_path_applied/$service.xml"
-}
-
-sub service_create_xml {
-  
-}
 
 
 =head1 AUTHOR
