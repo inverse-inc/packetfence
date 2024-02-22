257db27411797f6d02f0709c3b2f5927ef273a40
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-02-22 20:38:25 +0000

    Fix firewalld config path

--- lib/pf/file_paths.pm
+++ lib/pf/file_paths.pm
@@ -136,6 +136,9 @@ our (
     $provisioning_filters_meta_config_file,
     $provisioning_filters_meta_config_default_file,
     $pfperl_api_restart_task,
+    $firewalld_config_path_default,
+    $firewalld_config_path_default_template,
+    $firewalld_config_path_applied,
     $firewalld_services_config_file, $firewalld_services_config_defaults_file,
     $firewalld_policies_config_file, $firewalld_policies_config_defaults_file,
     $firewalld_icmptypes_config_file, $firewalld_icmptypes_config_defaults_file,
@@ -251,6 +254,9 @@ BEGIN {
         $provisioning_filters_meta_config_file
         $provisioning_filters_meta_config_default_file
         $pfperl_api_restart_task
+        $firewalld_config_path_default 
+        $firewalld_config_path_default_template
+        $firewalld_config_path_applied
 	$firewalld_services_config_file $firewalld_services_config_defaults_file
 	$firewalld_zones_config_file $firewalld_zones_config_defaults_file
 	$firewalld_policies_config_file $firewalld_policies_config_defaults_file
@@ -412,7 +418,9 @@ $firewalld_ipsets_config_defaults_file = catfile($conf_dir,"firewalld_ipsets.con
 $firewalld_ipsets_config_file = catfile($conf_dir,"firewalld_ipsets.conf");
 $firewalld_policies_config_defaults_file = catfile($conf_dir,"firewalld_policies.conf.defaults");
 $firewalld_policies_config_file = catfile($conf_dir,"firewalld_policies.conf");
-
+$firewalld_config_path_default = catdir($install_dir,"/firewalld");
+$firewalld_config_path_default_template = catdir($firewalld_config_path_default, "/template");
+$firewalld_config_path_applied = catdir($var_dir,"/firewalld");
 
 @log_files = map {catfile($log_dir, $_)}
   qw(
