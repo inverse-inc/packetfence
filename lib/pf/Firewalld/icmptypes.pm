ff67ee7ee424df078079b7e98f813700afe2645d
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-02-28 18:33:02 +0000

    Firewalld Add icmptypes generation

--- lib/pf/Firewalld/icmptypes.pm
+++ lib/pf/Firewalld/icmptypes.pm
@@ -14,6 +14,8 @@ Module to get/set basic configuration about firewalld servics
 
 use strict;
 use warnings;
+use File::Copy;
+use Template;
 
 BEGIN {
     use Exporter ();
@@ -24,23 +26,16 @@ BEGIN {
     );
 }
 
-
 use pf::log;
 use pf::util;
-use pf::Firewalld::util qw(
-    util_prepare_firewalld_config
-    util_get_firewalld_bin
-    util_get_firewalld_cmd
-    util_listen_ints_hash
-    util_source_or_destination_validation
-    util_prepare_version
-    util_create_string_for_xml
-    util_create_limit_for_xml
-    util_is_firewalld_protocol
-    util_is_fd_source_name
-    util_firewalld_cmd
-    util_firewalld_action
-    util_reload_firewalld
+use pf::Firewalld::util;
+use pf::config qw(
+    %ConfigFirewalld
+);
+use pf::file_paths qw(
+    $firewalld_config_path_generated
+    $firewalld_config_path_default_template
+    $firewalld_config_path_applied
 );
 
 sub firewalld_icmptypes_hash {
@@ -60,11 +55,73 @@ sub firewalld_icmptypes_hash {
 sub is_icmptypes_available {
   my $s = shift;
   my $available_icmptypes = firewalld_icmptypes_hash();
-  if ( !undef $available_icmptypes && exists( $available_icmptypes->{$s} ) ) {
+  if ( defined $available_icmptypes && exists( $available_icmptypes->{$s} ) ) {
     return $s;
   }
   get_logger->error( "Icmp type $s does not exist." );
   return undef;
+}
+
+sub icmptype_all_destinations {
+  my $conf = shift;
+  if ( exists $conf->{"destinations"} ) {
+    my @all_destinations;
+    my $destinations = $conf->{"destinations"};
+    if ( $destinations ne "" ) {
+      my @all_dest = split( /,/, $destinations );
+      foreach my $dest ( @all_dest ) {
+        my ($key, $val ) = split( /:/, $dest );
+        if ( $key eq "ipv4" || $key eq "ipv6" ) {
+          if ( $val eq "no" || $val eq "yes" ) {
+            my $xml_dest = $key.'="'.$val.'"';
+            push( @all_destinations, $xml_dest );
+          } else {
+            get_logger->error( "Icmptype destination needs to be yes or no." );
+          }
+        } else {
+          get_logger->error( "Icmptype destination needs to be ipv4 or ipv6." );
+        }
+      }
+      $conf->{"all_destinations"} = \@all_dest;
+    } else {
+      get_logger->error( "Icmptype destination is empty." );
+    }
+  }
+}
+
+
+# Generate config
+sub generate_icmptype_config {
+  my $conf = $ConfigFirewalld{ "firewalld_icmptypes" };
+  util_prepare_firewalld_config( $conf );
+  foreach my $name ( keys %{ $conf } ) {
+    my $v = $conf->{ $name };
+    if ( exists( $v->{"short"} ) ){
+      create_icmptype_config_file( $v, $name );
+    }
+  }
+}
+
+sub create_icmptype_config_file {
+  my $conf = shift ;
+  my $name = shift ;
+  util_prepare_version( $conf );
+  icmptype_all_destinations( $conf );
+  my $dir = "$firewalld_config_path_generated/icmptypes";
+  pf_make_dir($dir);
+  my $file = "$dir/$name.xml";
+  my $file_template = "$firewalld_config_path_default_template/icmptype.xml";
+  if ( -e $file ) {
+    my $bk_file = $file.".bk";
+    if ( -e $bk_file ) {
+      unlink $bk_file or warn "Could not unlink $file: $!";
+    }
+    copy( $file, $bk_file ) or die "copy failed: $!";
+  }
+  my $tt = Template->new(
+    ABSOLUTE => 1,
+  );
+  $tt->process( $file_template, $conf, $file ) or die $tt->error();
 }
 
 =head1 AUTHOR
