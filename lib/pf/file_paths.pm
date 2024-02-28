629e58dc9c257452f59c30cfacb41ddc47fb53e9
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-02-28 18:33:30 +0000

    update path for firewalld

--- lib/pf/file_paths.pm
+++ lib/pf/file_paths.pm
@@ -138,6 +138,7 @@ our (
     $pfperl_api_restart_task,
     $firewalld_config_path_default,
     $firewalld_config_path_default_template,
+    $firewalld_config_path_generated,
     $firewalld_config_path_applied,
     $firewalld_config_config_file, $firewalld_config_config_defaults_file,
     $firewalld_services_config_file, $firewalld_services_config_defaults_file,
@@ -259,6 +260,7 @@ BEGIN {
         $pfperl_api_restart_task
         $firewalld_config_path_default
         $firewalld_config_path_default_template
+        $firewalld_config_path_generated
         $firewalld_config_path_applied
         $firewalld_config_config_file $firewalld_config_config_defaults_file	
 	$firewalld_services_config_file $firewalld_services_config_defaults_file
@@ -417,8 +419,8 @@ $connectors_config_file = catdir($conf_dir,"connectors.conf");
 # Firewalld Generic path
 $firewalld_config_path_default = catdir($conf_dir,"/firewalld");
 $firewalld_config_path_default_template = catdir($firewalld_config_path_default, "/template");
-$firewalld_config_path_generated = catdir($var_dir,"/firewalld/generated");
-$firewalld_config_path_applied = catdir($var_dir,"/firewalld/applied");
+$firewalld_config_path_generated = catdir($generated_conf_dir,"/firewalld");
+$firewalld_config_path_applied = catdir($generated_conf_dir,"/firewalld");
 # Firewalld Specific path/files
 $firewalld_services_config_defaults_file = catfile($firewalld_config_path_default,"firewalld_services.conf.defaults");
 $firewalld_services_config_file          = catfile($firewalld_config_path_default,"firewalld_services.conf");
