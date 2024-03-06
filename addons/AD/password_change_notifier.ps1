0fa5589d2fc123006cc60051fcc3fc175b51bccd
Zhihao Ma <zma@akamai.com>
2024-03-06 16:24:59 -0500

    change description

--- addons/AD/password_change_notifier.ps1
+++ addons/AD/password_change_notifier.ps1
@@ -1,7 +1,17 @@
 ##########################################################################################
-#Powershell script to unregister disabled Active Directory account based on the UserName.#
+#Powershell script to audit and notify a password change on an Active Directory.         #
 ##########################################################################################
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$base_url = "https://@IP_PACKETFENCE:9999"
+$username = "admin" # Username for the webservice
+$password = "admin" # Password for the webservice
+
+$credential = @{ "username" = $username; "password" = $password}
+$credJson = $credential| ConvertTo-Json
+
+$token_url=$base_url+"/
+
 
 cls
 $eventTypeID = 4724
