05168b19ea7fb5c8e02710646e44c6e5644092b2
Zhihao Ma <zma@akamai.com>
2024-03-07 12:25:07 -0500

    adjust powershell script, authenticate using admin password and token.

--- addons/AD/password_change_notifier.ps1
+++ addons/AD/password_change_notifier.ps1
@@ -1,24 +1,25 @@
 ##########################################################################################
-#Powershell script to audit and notify a password change on an Active Directory.         #
+# Powershell script to audit and notify a password change on an Active Directory.        #
 ##########################################################################################
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 
-$base_url = "https://@IP_PACKETFENCE:9999"
-$username = "admin" # Username for the webservice
-$password = "admin" # Password for the webservice
+$base_url = "https://#PACKETFENCE_IP:9999"
+$username = ""         # Username for the webservice, change to your own username.
+$password = ""         # Password for the webservice, change to your own password.
 
-$credential = @{ "username" = $username; "password" = $password}
+$token_url = $base_url + "/api/v1/login"
+$password_notifier_url = $base_url + "/api/v1/ntlm/password_change"
+
+$credential = @{ "username" = $username; "password" = $password }
 $credJson = $credential| ConvertTo-Json
+$token_response = Invoke-RestMethod -Uri $token_url -Method Post -Body $credJson -ContentType "application/json"
+$token = $token_response.token
 
-$token_url=$base_url+"/
-
-
-cls
 $eventTypeID = 4724
 $events = Get-WinEvent -MaxEvents 10  -FilterHashTable @{ Logname = "Security"; ID = $eventTypeID }
 
 $eventArr = @()
-
 foreach ($event in $events)
 {
     $e = @{ }
@@ -35,12 +36,7 @@ foreach ($event in $events)
     }
     $eventArr += $e
 }
-
 $eventJson = $eventArr | ConvertTo-Json
-$url = "http://@PACKETFENCE_IP:@NTLMAUTH_LISTENING_PORT/password_change"
-
-$response = Invoke-RestMethod -Uri $url -Method Post -Body $eventJson -ContentType "application/json"
+$response = Invoke-RestMethod -Uri $password_notifier_url -Method Post -Body $eventJson -ContentType "application/json" -Headers @{ "Authorization" = $token }
 
 Write-Output($response)
-
-
