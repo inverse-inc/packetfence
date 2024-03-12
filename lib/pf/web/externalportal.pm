f449cbc040d2ce8b572b72302d5e4d051841c1c4
abprasa <123509565+abprasa@users.noreply.github.com>
2024-03-12 12:56:26 +0000

    updated the externalportal for MS_v15.pm

--- lib/pf/web/externalportal.pm
+++ lib/pf/web/externalportal.pm
@@ -47,6 +47,7 @@ Readonly our $SWITCH_REWRITE_MAP => {
     'Cisco::Catalyst_2960' => 'Cisco::Cisco_IOS_15_0',
     'Aruba::CX' => 'Aruba::ArubaOS_CX_10.x',
     'Aruba::2930M' => 'Aruba::ArubaOS_Switch_16.x',
+    'Meraki::MS220_8' => 'Meraki::MS_v15',
 };
 
 =head1 SUBROUTINES
