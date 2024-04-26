ef8c2ec4dd02ba85352d5243b12f8dc6343e6319
James Rouzier <jrouzier@inverse.ca>
2024-04-26 13:37:01 +0000

    Fix case where node table is empty NewNode function

--- go/cron/node_test.go
+++ go/cron/node_test.go
@@ -32,30 +32,95 @@ INSERT INTO node (
     mac, status
 )
 
-SELECT
-    LOWER(CONCAT_WS(
-        ':',
-        LPAD(HEX((seq >> 40) & 255), 2, '0'),
-        LPAD(HEX((seq >> 32) & 255), 2, '0'),
-        LPAD(HEX((seq >> 24) & 255), 2, '0'),
-        LPAD(HEX((seq >> 16) & 255), 2, '0'),
-        LPAD(HEX((seq >> 8) & 255), 2, '0'),
-        LPAD(HEX(seq & 255), 2, '0')
-    )) AS mac , ? as status
-FROM (
+WITH RECURSIVE
+init_nodes AS (
+    (
     SELECT
-        seq + 1 as seq,
-        LEAD(seq, 1) OVER (ORDER BY seq) as next
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(seq & 255), 2, '0')
+        )) AS mac ,
+        seq + 1 as next_seq
     FROM (
         SELECT
-        (CONV(REPLACE(mac, ':', ''), 16, 10) ) as seq
-        FROM node
-        ORDER BY mac
-    ) AS a
+            seq + 1 as seq,
+            LEAD(seq, 1) OVER (ORDER BY seq) as next
+        FROM (
+            SELECT
+            (CONV(REPLACE(mac, ':', ''), 16, 10) ) as seq
+            FROM node
+            ORDER BY mac
+        ) AS a
+        ORDER BY seq
+    ) as b
+    WHERE next != seq OR next IS NULL
     ORDER BY seq
-) as b
-WHERE next != seq
-LIMIT ?
+    LIMIT ?
+    )
+    UNION
+    (
+    SELECT * FROM (
+        SELECT
+            LOWER(CONCAT_WS(
+                ':',
+                LPAD(HEX((seq >> 40) & 255), 2, '0'),
+                LPAD(HEX((seq >> 32) & 255), 2, '0'),
+                LPAD(HEX((seq >> 24) & 255), 2, '0'),
+                LPAD(HEX((seq >> 16) & 255), 2, '0'),
+                LPAD(HEX((seq >> 8) & 255), 2, '0'),
+                LPAD(HEX(seq & 255), 2, '0')
+            )) AS mac ,
+            seq + 1 as next_seq
+        FROM (
+            SELECT
+                  (CONV(REPLACE(mac, ':', ''), 16, 10) ) + 1 as seq
+            FROM node
+            ORDER by mac DESC
+            LIMIT 1
+        ) as c
+        UNION
+        SELECT "00:00:00:00:00:00", 1
+    ) as d LIMIT 1
+    )
+
+),
+
+rest(mac, next_seq, c, l) as (
+  SELECT *, (SELECT COUNT(*) FROM init_nodes) as c, ? as l  FROM init_nodes
+  UNION ALL
+  SELECT
+    LOWER(CONCAT_WS(
+        ':',
+        LPAD(HEX((next_seq >> 40) & 255), 2, '0'),
+        LPAD(HEX((next_seq >> 32) & 255), 2, '0'),
+        LPAD(HEX((next_seq >> 24) & 255), 2, '0'),
+        LPAD(HEX((next_seq >> 16) & 255), 2, '0'),
+        LPAD(HEX((next_seq >> 8) & 255), 2, '0'),
+        LPAD(HEX(next_seq & 255), 2, '0')
+    )) AS mac ,
+    next_seq + 1,
+    c + 1,
+    l
+    FROM rest
+    WHERE c < l AND
+        LOWER(CONCAT_WS(
+            ':',
+            LPAD(HEX((next_seq >> 40) & 255), 2, '0'),
+            LPAD(HEX((next_seq >> 32) & 255), 2, '0'),
+            LPAD(HEX((next_seq >> 24) & 255), 2, '0'),
+            LPAD(HEX((next_seq >> 16) & 255), 2, '0'),
+            LPAD(HEX((next_seq >> 8) & 255), 2, '0'),
+            LPAD(HEX(next_seq & 255), 2, '0')
+        )) NOT IN (SELECT mac FROM node)
+)
+
+SELECT mac, ? AS status from rest LIMIT 10
+
 RETURNING mac
 ;
 `
@@ -69,7 +134,7 @@ RETURNING mac
 		return nil, err
 	}
 
-	rows, err := stmt.Query(status, amount)
+	rows, err := stmt.Query(amount, amount, status)
 	if err != nil {
 		return nil, err
 	}
