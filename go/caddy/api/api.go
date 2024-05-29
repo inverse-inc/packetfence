9c3f5db1f2d012d87232a68be145681feadf448d
Zhihao Ma <zma@akamai.com>
2024-05-29 09:48:43 -0400

    adjust endpoint

--- go/caddy/api/api.go
+++ go/caddy/api/api.go
@@ -68,8 +68,8 @@ func buildHandler(ctx context.Context) (APIHandler, error) {
 	router.POST("/api/v1/ntlm/test", apiHandler.ntlmTest)
 	router.POST("/api/v1/ntlm/event-report", apiHandler.eventReport)
 
-	router.POST("/api/v1/fleetdm/report-policy", apiHandler.eventReport)
-	router.POST("/api/v1/fleetdm/report-cve", apiHandler.eventReport)
+	router.POST("/api/v1/fleetdm-events/policy", apiHandler.eventReport)
+	router.POST("/api/v1/fleetdm-events/cve", apiHandler.eventReport)
 
 	var DBP **gorm.DB
 	var DB *gorm.DB
