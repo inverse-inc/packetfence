0b69ea136d62dbbafc53b2f2e0df728e96dac185
James Rouzier <jrouzier@inverse.ca>
2024-06-12 11:22:20 +0000

    Add missing imports

--- go/plugin/caddy2/plugins.go
+++ go/plugin/caddy2/plugins.go
@@ -2,7 +2,9 @@ package caddy2
 
 import (
 	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/configstore"
+	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/job-status"
 	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/logger"
+	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/pfconfig"
 	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/pfipset"
 	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/pfldapexplorer"
 	_ "github.com/inverse-inc/packetfence/go/plugin/caddy2/pfpki"
