08098c0189f5707ee276c54c14c725391502090e
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-06-17 08:36:46 -0400

    Firewalld Increase a bit minimal docker iptable rules in order to have httpd.admin_dispatcher starting proprely at installation

--- containers/docker-minimal-rules.sh
+++ containers/docker-minimal-rules.sh
@@ -7,5 +7,9 @@ while iptables -L DOCKER ; [ $? -ne 0 ];do
   msg "Waiting for iptables to be ready"
   eval "iptables -t filter -N DOCKER"
   eval "iptables -t nat -N DOCKER"
+  eval "iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER"
+  eval "iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER"
+  eval "iptables -t nat -A POSTROUTING -s 100.64.0.0/10 ! -o docker0 -j MASQUERADE"
+  eval "iptables -t nat -A DOCKER -i docker0 -j RETURN"
   sleep 1;
 done
