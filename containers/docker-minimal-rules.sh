4f977a38cfe3eaf361b9fe6c8f20b89e140bda7a
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-07-09 07:44:17 -0400

    Firewalld bring back the orginal docker minimal rules

--- containers/docker-minimal-rules.sh
+++ containers/docker-minimal-rules.sh
@@ -6,14 +6,6 @@ function out { printf '%s\n' "$*" ;}
 while iptables -L DOCKER ; [ $? -ne 0 ];do
   msg "Waiting for iptables to be ready"
   eval "iptables -t filter -N DOCKER"
-  eval "iptables -A INPUT --in-interface lo --jump ACCEPT"
-  eval "iptables -A INPUT --in-interface docker0 --jump ACCEPT"
-  eval "iptables -A INPUT --match state --state ESTABLISHED,RELATED --jump ACCEPT"
-  eval "iptables -A INPUT --protocol icmp --icmp-type echo-request --jump ACCEPT"
   eval "iptables -t nat -N DOCKER"
-  eval "iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER"
-  eval "iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER"
-  eval "iptables -t nat -A POSTROUTING -s 100.64.0.0/10 ! -o docker0 -j MASQUERADE"
-  eval "iptables -t nat -A DOCKER -i docker0 -j RETURN"
   sleep 1;
 done
