c63239b6cad6453cb8f8cef3298b807b6add27ff
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-07-10 10:20:17 -0400

    Firewalld Fix default minimal rules with exit of iptables or firewalld

--- containers/docker-minimal-rules.sh
+++ containers/docker-minimal-rules.sh
@@ -5,7 +5,20 @@ function out { printf '%s\n' "$*" ;}
 
 while iptables -L DOCKER ; [ $? -ne 0 ];do
   msg "Waiting for iptables to be ready"
+  eval "iptables -F"
+  eval "iptables -X"
+  eval "iptables -t nat -F"
+  eval "iptables -t nat -X"
+  eval "iptables -t mangle -F"
+  eval "iptables -t mangle -X"
+  eval "iptables -P INPUT ACCEPT"
+  eval "iptables -P FORWARD ACCEPT"
+  eval "iptables -P OUTPUT ACCEPT"
   eval "iptables -t filter -N DOCKER"
   eval "iptables -t nat -N DOCKER"
+  eval "iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER"
+  eval "iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER"
+  eval "iptables -t nat -A POSTROUTING -s 100.64.0.0/10 ! -o docker0 -j MASQUERADE"
+  eval "iptables -t nat -A DOCKER -i docker0 -j RETURN"
   sleep 1;
 done
