9859813eb25de44f7126ce018b5a72dd9d30029c
JeGoi <13801368+JeGoi@users.noreply.github.com>
2024-08-30 09:11:30 -0400

    Add restart of pfperl-api during the process

--- html/pfappserver/root/src/views/Configurator/status/_components/TheStep.vue
+++ html/pfappserver/root/src/views/Configurator/status/_components/TheStep.vue
@@ -47,16 +47,18 @@ const setup = (props, context) => {
       progressFeedback.value = i18n.t('Enabling PacketFence')
       return $store.dispatch('cluster/updateSystemd', { id: 'pf' }).then(() => {
         progressFeedback.value = i18n.t('Starting PacketFence')
-        return $store.dispatch('cluster/restartService', { id: 'haproxy-admin' }).then(() => {
-          return $store.dispatch('cluster/startService', { id: 'pf' }).then(() => {
-            progressFeedback.value = i18n.t('Disabling Configurator')
-            return advancedPromise.then(data => {
-              data.configurator = 'disabled'
-              return $store.dispatch('$_bases/updateAdvanced', data).then(() => {
-                progressFeedback.value = i18n.t('Redirecting to login page')
-                setTimeout(() => {
-                  window.location.href = '/'
-                }, 2000)
+        return $store.dispatch('cluster/restartService', { id: 'pfperl-api' }).then(() => {
+          return $store.dispatch('cluster/restartService', { id: 'haproxy-admin' }).then(() => {
+            return $store.dispatch('cluster/startService', { id: 'pf' }).then(() => {
+              progressFeedback.value = i18n.t('Disabling Configurator')
+              return advancedPromise.then(data => {
+                data.configurator = 'disabled'
+                return $store.dispatch('$_bases/updateAdvanced', data).then(() => {
+                  progressFeedback.value = i18n.t('Redirecting to login page')
+                  setTimeout(() => {
+                    window.location.href = '/'
+                  }, 2000)
+                })
               })
             })
           })
