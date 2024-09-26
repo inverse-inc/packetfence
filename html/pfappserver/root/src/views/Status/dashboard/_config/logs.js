811a84454e2783d89dd3225c4b5a07fd7f57b080
Darren+Satkunas <dsatkuna@akamai.com>
2024-09-26 20:10:11 +0000

    improve logs

--- html/pfappserver/root/src/views/Status/dashboard/_config/logs.js
+++ html/pfappserver/root/src/views/Status/dashboard/_config/logs.js
@@ -5,6 +5,18 @@ export default [
     name: 'Logs', // i18n defer
     groups: [
       {
+        name: 'httpd.apache',
+        items: [
+          {
+            title: 'Number of events', // i18n defer
+            metric: 'packetfence.logs.httpd_apache',
+            mode: modes.COMBINED,
+            library: libraries.DYGRAPH,
+            cols: 12
+          }
+        ]
+      },
+      {
         name: 'httpd.apache portal_log',
         items: [
           {
