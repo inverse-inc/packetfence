b1bae43e57c9af4a5561bdc7785451192859ec5f
James Rouzier <jrouzier@inverse.ca>
2024-09-27 11:35:19 +0000

    Add test file

--- t/test_paths.pm
+++ t/test_paths.pm
@@ -66,6 +66,7 @@ BEGIN {
     $pf::file_paths::radius_server_cert = catfile($test_dir,'data/radius_server.crt');
     $pf::file_paths::radius_server_key = catfile($test_dir,'data/radius_server.key');
     $pf::file_paths::radius_ca_cert = catfile($test_dir,'data/radius_ca.pem');
+    $pf::file_paths::system_init_key_file = catfile($test_dir, 'data/system_init_key');
 
     $pfconfig::constants::CONFIG_FILE_PATH = catfile($test_paths::test_dir, 'data/pfconfig.conf');
     $pfconfig::constants::SOCKET_PATH = "/usr/local/pf/var/run/pfconfig-test.sock";
