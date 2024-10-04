df4052ae8da175b25fde170951fd67d2e9531e46
James Rouzier <jrouzier@inverse.ca>
2024-10-04 13:19:24 +0000

    Add usage output

--- go/cmd/pfcrypt/main.go
+++ go/cmd/pfcrypt/main.go
@@ -7,12 +7,23 @@ import (
 	"github.com/inverse-inc/packetfence/go/config/pfcrypt"
 )
 
+const usage = `Usage:
+	pfcrypt encrypt <text>
+	pfcrypt decrypt <ciphertext>
+`
+
 func main() {
 	if len(os.Args) != 3 {
-		panic("Not enough arg")
+		fmt.Fprintf(os.Stderr, "Not enough args\n%s\n", usage)
+		os.Exit(1)
+		return
 	}
 
 	switch os.Args[1] {
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid option\n%s\n", usage)
+		os.Exit(1)
+		return
 	case "encrypt":
 		ciphertext, err := pfcrypt.PfEncrypt([]byte(os.Args[2]))
 		if err != nil {
