7edeb44384b90f1d759b663bc73506b94ca6fc5c
stegar123 <istegare@akamai.com>
2024-10-10 19:20:16 +0000

    add startup.sh script

--- containers/caddy-l4/startup.sh
+++ containers/caddy-l4/startup.sh
@@ -1,4 +1,18 @@
-#! /bin/sh
+#!/bin/sh
 
-/usr/bin/python3 /srv/dns_register_alert.py &
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+# turn on bash's job control
+set -m
+
+# Start the primary process and put it in the background
+/srv/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+
+# Start the helper process
+/usr/bin/python3 /srv/dns_register_alert.py
+
+# the my_helper_process might need to know how to wait on the
+# primary process to start before it does its work and returns
+
+
+# now we bring the primary process back into the foreground
+# and leave it there
+fg %1
