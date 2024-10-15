891e790e8299a19d03de0cb6dcd6663cd02b290e
stegar123 <istegare@akamai.com>
2024-10-15 17:27:42 +0000

    fix domain issue + improvements

--- containers/caddy-l4/dns_register_alert.py
+++ containers/caddy-l4/dns_register_alert.py
@@ -20,11 +20,7 @@ def send_mail(client_id, smtp_user, smtp_password, smtp_from, smtp_host, smtp_po
     body = """
     <html>
     <body>
-<<<<<<< HEAD
         <p>Hi {client_id}!</p>
-=======
-        <p>Hi!</p>
->>>>>>> 22e6e7a546 (search id client from json)
         <p>The domain <b> {domain} </b> failed to resolve. Kindly create a DNS entry and try again.</p>
         <br>
         <p>Best regards,</p>
