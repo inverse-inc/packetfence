14291f2cffda0931ed3edbf55343c09959743702
James Rouzier <jrouzier@inverse.ca>
2024-11-07 19:58:15 +0000

    Only show registered nodes

--- html/captive-portal/lib/captiveportal/PacketFence/Controller/Status.pm
+++ html/captive-portal/lib/captiveportal/PacketFence/Controller/Status.pm
@@ -93,7 +93,7 @@ sub is_lost_stolen {
 sub userIsAuthenticated : Private {
     my ( $self, $c ) = @_;
     my $pid     = $c->user_session->{"username"} // $c->{_session}->{username};
-    my @person_nodes = person_nodes($pid);
+    my @person_nodes = person_reg_nodes($pid);
     my @nodes;
     foreach my $person_node (@person_nodes) {
         my $node = node_view($person_node->{mac});
