6ea9dcdfacb4b8b2e942ad1650759794309481d3
Zhihao Ma <stgmsa@gmail.com>
2025-03-04 07:05:31 -0500

    adds explanation for each field in TEAP configuration adds validator

--- html/pfappserver/root/src/utils/yup.js
+++ html/pfappserver/root/src/utils/yup.js
@@ -16,6 +16,7 @@ import {
   reNumeric,
   reStaticRoute,
   reUUID,
+  reHash,
 } from './regex'
 
 yup.setLocale({ // default validators
@@ -158,6 +159,15 @@ yup.addMethod(yup.string, 'isAlphaNumericHyphenUnderscoreDot', function (message
     test: value => ['', null, undefined].includes(value) || reAlphaNumericHyphenUnderscoreDot(value)
   })
 })
+
+yup.addMethod(yup.string, 'isHash', function (message) {
+  return this.test({
+    name: 'isHash',
+    message: message || i18n.t('Invalid hash character, only letters (A-F, a-f), numbers are allowed.'),
+    test: value => ['', null, undefined].includes(value) || reHash(value)
+  })
+})
+
 
 yup.addMethod(yup.string, 'isCIDR', function (message) {
   return this.test({
