d117cf49adb0a798f15ba6f86dec6cfe887eb655
Zhihao Ma <stgmsa@gmail.com>
2025-03-04 07:58:14 -0500

    quote identity types, adds validator

--- html/pfappserver/root/src/views/Configuration/radius/teap/schema.js
+++ html/pfappserver/root/src/views/Configuration/radius/teap/schema.js
@@ -4,9 +4,7 @@ import yup from '@/utils/yup'
 
 yup.addMethod(yup.string, 'radiusTeapIdNotExistsExcept', function (exceptId = '', message) {
   return this.test({
-    name: 'radiusTeapIdNotExistsExcept',
-    message: message || i18n.t('Identifier exists.'),
-    test: (value) => {
+    name: 'radiusTeapIdNotExistsExcept', message: message || i18n.t('Identifier exists.'), test: (value) => {
       if (!value || value.toLowerCase() === exceptId.toLowerCase()) return true
       return store.dispatch('config/getRadiusTeaps').then(response => {
         return response.filter(teap => teap.id.toLowerCase() === value.toLowerCase()).length === 0
@@ -19,19 +17,19 @@ yup.addMethod(yup.string, 'radiusTeapIdNotExistsExcept', function (exceptId = ''
 
 export default (props) => {
   const {
-    id,
-    isNew,
-    isClone
+    id, isNew, isClone
   } = props
 
   return yup.object().shape({
     id: yup.string()
       .nullable()
       .required(i18n.t('Identifier required.'))
+      .max(20)
+      .isAlphaNumericHyphenUnderscoreDot()
       .radiusTeapIdNotExistsExcept((!isNew && !isClone) ? id : undefined, i18n.t('Identifier exists.')),
     authority_identity: yup.string().nullable().label(i18n.t('Authority Identity')),
     identity_types: yup.string().nullable().label(i18n.t('Identity Types')),
-    pac_opaque_key: yup.string().nullable().label(i18n.t('Key')),
+    pac_opaque_key: yup.string().nullable().min(32).max(32).isHash().label(i18n.t('Key')),
     tls: yup.string().nullable().label(i18n.t('TLS Profile'))
   })
 }
