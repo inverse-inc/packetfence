360b62e5c05fa7534f875f8dd9f0ed999affeac2
Durand Fabrice <oeufdure@gmail.com>
2025-03-10 10:24:58 -0400

    Added 127.127.127.127 in radius_nas table

--- lib/pfconfig/namespaces/config/Switch.pm
+++ lib/pfconfig/namespaces/config/Switch.pm
@@ -132,6 +132,7 @@ sub build_child {
 	#push @management_ips, $self->{management_network}->tag('vip') if(defined($self->{management_network}->tag('vip')));
 	#push @management_ips, $self->{management_network}->tag('ip') if(defined($self->{management_network}->tag('ip')));
         push @management_ips, "100.64.0.1";
+        push @management_ips, "127.127.127.127";
         foreach my $management_ip (@management_ips){
             $tmp_cfg{$management_ip} = {
                 type => 'PacketFence',
