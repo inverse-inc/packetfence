79a22d25ea49162cb62b5623ab8377259a521b5c
JeGoi <13801368+JeGoi@users.noreply.github.com>
2025-05-13 12:07:36 -0400

    Clean service manager iptables

--- lib/pf/services/manager/iptables.pm
+++ lib/pf/services/manager/iptables.pm
@@ -25,89 +25,9 @@ extends 'pf::services::manager';
 
 has '+name' => (default => sub { 'iptables' } );
 
-has '+shouldCheckup' => ( default => sub { 1 }  );
+has '+shouldCheckup' => ( default => sub { 0 }  );
 
 has 'runningServices' => (is => 'rw', default => sub { 0 } );
-
-
-=head2 start
-
-start iptables
-
-=cut
-
-sub startService {
-    my ($self) = @_;
-    my $technique;
-    unless ($self->isAlive()) {
-        $technique = getIptablesTechnique();
-        $technique->iptables_save($install_dir . '/var/iptables.bak');
-    }
-    $technique ||= getIptablesTechnique();
-    $technique->iptables_generate();
-    return 1;
-}
-
-=head2
-
-generateConfig
-
-=cut
-
-sub generateConfig {
-    my $technique;
-    $technique ||= getIptablesTechnique();
-    $technique->iptables_generate();
-    return 1;
-}
-
-=head2 getIptablesTechnique
-
-getIptablesTechnique
-
-=cut
-
-sub getIptablesTechnique {
-    require pf::inline::custom;
-    my $iptables = pf::inline::custom->new();
-    return $iptables->{_technique};
-}
-
-=head2 start
-
-Wrapper around systemctl. systemctl should in turn call the actuall _start.
-
-=cut
-
-sub start {
-    my ($self,$quick) = @_;
-    system('sudo systemctl start packetfence-iptables');
-    return $? == 0;
-}
-
-=head2 _start
-
-start the service (called from systemd)
-
-=cut
-
-sub _start {
-    my ($self) = @_;
-    my $result = 0;
-    unless ( $self->isAlive() ) {
-        $result = $self->startService();
-    }
-    return $result;
-}
-
-sub startAndCheck {
-    my ($self) = @_;
-
-    while(1) {
-        $self->_start() unless($self->isAlive());
-        sleep 60;
-    }
-}
 
 =head2 stop
 
@@ -130,21 +50,6 @@ stop iptables (called from systemd)
 sub _stop {
     my ($self) = @_;
     my $logger = get_logger();
-    safe_pf_run(qw(sudo iptables -F));
-    safe_pf_run(qw(sudo iptables -X));
-    safe_pf_run(qw(sudo iptables -t nat -F));
-    safe_pf_run(qw(sudo iptables -t nat -X));
-    safe_pf_run(qw(sudo iptables -t mangle -F));
-    safe_pf_run(qw(sudo iptables -t mangle -X));
-    safe_pf_run(qw(sudo iptables -P INPUT ACCEPT));
-    safe_pf_run(qw(sudo iptables -P FORWARD ACCEPT));
-    safe_pf_run(qw(sudo iptables -P OUTPUT ACCEPT));
-    safe_pf_run(qw(sudo iptables -t nat -N DOCKER));
-    safe_pf_run(qw(sudo iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER));
-    safe_pf_run(qw(sudo iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER));
-    safe_pf_run(qw(sudo iptables -t nat -A POSTROUTING -s 100.64.0.0/10 ! -o docker0 -j MASQUERADE));
-    safe_pf_run(qw(sudo iptables -t nat -A DOCKER -i docker0 -j RETURN));
-
     return 1;
 }
 
@@ -157,12 +62,8 @@ Since it's never really stopped then we check if the fake PID exists
 
 sub isAlive {
     my ($self) = @_;
-    my $logger = get_logger();
-    my $result;
-    my $pid = $self->pid;
-    my $_EXIT_CODE_EXISTS = "0";
-    my $rules = safe_pf_run('sudo', 'iptables', '-S') // '';
-    return ($rules =~ /-A input-management-if -p tcp -m tcp --dport 1443 -j ACCEPT/ ? 1: 0);
+    my $is_active = safe_pf_run('sudo', 'systemctl', 'is-active', 'packetfence-iptables');
+    return ( $is_active == 0 ? 1: 0 );
 }
 
 =head1 AUTHOR
