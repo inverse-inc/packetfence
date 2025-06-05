0a4029a0559968e383bcac8bacdd67dcedf74768
Durand Fabrice <oeufdure@gmail.com>
2025-06-04 22:16:02 -0400

    typo fix and modified struct

--- go/pfconfigdriver/structs.go
+++ go/pfconfigdriver/structs.go
@@ -1071,10 +1071,10 @@ type PfConfPfDnsConnector struct {
 
 type PfConfDnsConnectors struct {
 	StructConfig
-	PfconfigMethod          string `val:"element"`
-	PfconfigNS              string `val:"config::DnsConnectors"`
-	PfconfigDecodeInElement string `val:"yes"`
-	Element                 []Dns
+	PfconfigMethod          string         `val:"element"`
+	PfconfigNS              string         `val:"config::DnsConnectors"`
+	PfconfigDecodeInElement string         `val:"yes"`
+	Element                 map[string]Dns `json:"-"`
 }
 
 type Dns struct {
