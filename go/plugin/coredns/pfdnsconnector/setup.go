cfc88868f0ba6f7be8f7a81222178e569a8edab5
Durand Fabrice <oeufdure@gmail.com>
2025-06-09 13:20:29 -0400

    Open dns remote port

--- go/plugin/coredns/pfdnsconnector/setup.go
+++ go/plugin/coredns/pfdnsconnector/setup.go
@@ -22,14 +22,20 @@ func setuppfdnsconnector(c *caddy.Controller) error {
 	var pf = &pfdnsconnector{}
 
 	ctx := context.Background()
+	Connectors := connector.NewConnectorsContainer(ctx)
+	ctx = connector.WithConnectorsContainer(ctx, Connectors)
 	pfconfigdriver.AddType[pfconfigdriver.PfConfDnsConnectors](ctx)
 
 	dnsserver.GetConfig(c).AddPlugin(
 		func(next plugin.Handler) plugin.Handler {
 			dnsServers := pfconfigdriver.GetType[pfconfigdriver.PfConfDnsConnectors](context.Background())
-			for _, dns := range dnsServers.Element {
+			for _, dnsRaw := range dnsServers.Element {
+				dns, ok := dnsRaw.(map[string]interface{})
+				if !ok {
+					continue
+				}
 				for _, proto := range []string{"tcp", "udp"} {
-					_, err := connector.OpenConnectionTo(ctx, proto, dns.Ip, dns.PfConnectorPort, dns.PfConnectorPort)
+					_, err := connector.OpenConnectionTo(ctx, proto, dns["ip"].(string), dns["port"].(string), dns["pfconnectorport"].(string))
 					if err != nil {
 						panic(err)
 					}
