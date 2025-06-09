415e3a7b1578b7da03eee691a6620a7cee778a01
Durand Fabrice <oeufdure@gmail.com>
2025-06-09 13:20:29 -0400

    Open dns remote port

--- go/pfconfigdriver/structs.go
+++ go/pfconfigdriver/structs.go
@@ -1071,10 +1071,10 @@ type PfConfPfDnsConnector struct {
 
 type PfConfDnsConnectors struct {
 	StructConfig
-	PfconfigMethod          string         `val:"element"`
-	PfconfigNS              string         `val:"config::DnsConnectors"`
-	PfconfigDecodeInElement string         `val:"yes"`
-	Element                 map[string]Dns `json:"-"`
+	PfconfigMethod          string `val:"element"`
+	PfconfigNS              string `val:"config::DnsConnectors"`
+	PfconfigDecodeInElement string `val:"yes"`
+	Element                 map[string]interface{}
 }
 
 type Dns struct {
