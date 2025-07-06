405fd770dd9ead3fc1dec5b9a7ffd6a0a229aa0e
Durand Fabrice <oeufdure@gmail.com>
2025-07-05 22:42:22 -0400

    Avoid failure on service start

--- addons/ntlm-auth-api/pfconnector-remote-load.sh
+++ addons/ntlm-auth-api/pfconnector-remote-load.sh
@@ -7,7 +7,7 @@ CONNECTOR_ID=$(grep '^AUTH=' /usr/local/pfconnector-remote/conf/pfconnector-clie
 
 if [ -z "$CONNECTOR_ID" ]; then
     echo "Error: no CONNECTOR_ID found in /usr/local/pfconnector-remote/conf/pfconnector-client.env"
-    exit 1
+    exit 0
 fi
 
 curl --fail "localhost:22226/api/v1/pfconnector/remote-ntlm-auth-api-env?CONNECTOR_ID=$CONNECTOR_ID" > /usr/local/ntlm-auth-api/conf/ntlm_auth_api.env
