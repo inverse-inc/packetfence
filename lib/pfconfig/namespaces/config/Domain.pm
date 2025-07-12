ea2ae9c41297067b8e72f1d5c3a5c950c38974af
Durand Fabrice <oeufdure@gmail.com>
2025-07-12 18:44:01 -0400

    Convert int to string

--- lib/pfconfig/namespaces/config/Domain.pm
+++ lib/pfconfig/namespaces/config/Domain.pm
@@ -52,6 +52,7 @@ sub build_child {
         }
         if (exists($cfg->{use_connector}) && isenabled($cfg->{use_connector})) {
             $cfg->{ntlm_auth_port} += 100;
+            $cfg->{ntlm_auth_port} = '' . $cfg->{ntlm_auth_port};
             if (exists $ENV{'PFCONNECTOR_PORT_22226_TCP_ADDR'}) {
                 $cfg->{ntlm_auth_host} = $ENV{'PFCONNECTOR_PORT_22226_TCP_ADDR'};
             } else {
