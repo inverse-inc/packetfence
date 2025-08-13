2d26d2b82039a9160cb8f1578b78ac4c96e56e34
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2025-08-13 08:32:46 -0400

    Modernize main function declaration
    Update the main function from K&R style to standard C declaration:
    
    - Changed from:
        int main(argc, argv, envp)
        int argc;
        char **argv, **envp;
    
    - To:
        int main(int argc, char **argv, char **envp);
    
    This improves readability and prevents compiler warnings, aligning with modern C standards.

--- src/ntlm_auth_wrap.c
+++ src/ntlm_auth_wrap.c
@@ -298,9 +298,8 @@ size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
     return realsize;
 }
 
-int main(argc, argv, envp)
-int argc;
-char **argv, **envp;
+int main(int argc, char **argv, char **envp)
+
 {
     /* Default values. */
     struct arguments arguments;
