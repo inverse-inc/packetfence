1e10145b2e0338bf2a0fef33833637c40959879d
JeGoi <13801368+JeGoi@users.noreply.github.com>
2025-09-09 12:33:50 -0400

    Add pre_open class in any source for documentation

--- docs/asciidoctor-html.rb
+++ docs/asciidoctor-html.rb
@@ -519,16 +519,13 @@ class PfHtml5Converter < (Asciidoctor::Converter.for 'html5')
     if node.style == 'source'
       lang = node.attr 'language'
       numbered = node.attr 'linenums'
-      if (syntax_hl = node.document.syntax_highlighter)
-        opts = syntax_hl.highlight? ? {
-          css_mode: ((doc_attrs = node.document.attributes)[%(#{syntax_hl.name}-css)] || :class).to_sym,
-          style: doc_attrs[%(#{syntax_hl.name}-style)]
-        } : {}
-        opts[:nowrap] = nowrap
-      else
-        pre_open = %(<pre class="highlight#{nowrap ? ' nowrap' : ''} bg-secondary text-white #{node.title? ? 'rounded-bottom': 'rounded'} p-3"><code class="#{numbered ? 'numbered ' : ''}#{lang ? %[language-#{lang}" data-lang="#{lang}] : ''}">)
-        pre_close = '</code></pre>'
-      end
+      opts = syntax_hl.highlight? ? {
+        css_mode: ((doc_attrs = node.document.attributes)[%(#{syntax_hl.name}-css)] || :class).to_sym,
+        style: doc_attrs[%(#{syntax_hl.name}-style)]
+      } : {}
+      opts[:nowrap] = nowrap
+      pre_open = %(<pre class="highlight#{nowrap ? ' nowrap' : ''} bg-secondary text-white #{node.title? ? 'rounded-bottom': 'rounded'} p-3"><code class="#{numbered ? 'numbered ' : ''}#{lang ? %[language-#{lang}" data-lang="#{lang}] : ''}">)
+      pre_close = '</code></pre>'
     else
       pre_open = %(<pre class="bg-secondary text-white rounded p-3#{nowrap ? 'nowrap' : ''}"">)
       pre_close = '</pre>'
