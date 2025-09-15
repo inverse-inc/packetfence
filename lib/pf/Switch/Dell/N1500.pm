50f472dcd472bcbca33a9ca69c08734c0c044219
Durand Fabrice <oeufdure@gmail.com>
2025-09-15 12:52:13 -0400

    Fixed syntax

--- lib/pf/Switch/Dell/N1500.pm
+++ lib/pf/Switch/Dell/N1500.pm
@@ -543,7 +543,7 @@ sub returnRadiusAdvanced {
             }
             $logger->info("(".$self->{'_id'}.") Added access lists to the RADIUS reply.");
             $self->setRadiusSession($session_state);
-            push(@av_pairs, "ACS:CiscoSecure-Defined-ACL="$args->{'user_role'}."-".$session_id);
+            push(@av_pairs, "ACS:CiscoSecure-Defined-ACL=".$args->{'user_role'}."-".$session_id);
             return [$status, %$radius_reply_ref];
         }
         if (scalar @{$session_state->{'acl'}} == 1) {
@@ -554,7 +554,7 @@ sub returnRadiusAdvanced {
                     push(@av_pairs, $formated_acl);
                     $logger->info("(".$self->{'_id'}.") Adding access list : $formated_acl to the RADIUS reply");
                     $logger->info("(".$self->{'_id'}.") Added access lists to the RADIUS reply.");
-                    push(@av_pairs, "ACS:CiscoSecure-Defined-ACL="$args->{'user_role'}."-".$session_id);
+                    push(@av_pairs, "ACS:CiscoSecure-Defined-ACL=".$args->{'user_role'}."-".$session_id);
                 } else {
                     $logger->info("(".$self->{'_id'}.") No more access lists defined for this role ". ( defined($args->{'user_role'}) ? $args->{'user_role'} : 'registration' ));
                 }
