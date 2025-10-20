b4f0c4a92d19b982d18b6ecfc3d3634852fa69b0
James Rouzier <jrouzier@inverse.ca>
2025-10-20 18:59:56 +0000

    Fix database schema naming

--- db/upgrade-X.X-X.Y.sql
+++ db/upgrade-X.X-X.Y.sql
@@ -198,6 +198,54 @@ DROP PROCEDURE IF EXISTS DropColumnIfExists;
 DROP PROCEDURE IF EXISTS AddIndexUnlessExists;
 DROP PROCEDURE IF EXISTS DropIndexIfExists;
 
+ALTER TABLE `locationlog`
+    ADD `teap_username` varchar(255) DEFAULT '' NOT NULL,
+    ADD `teap_machinename` varchar(255) DEFAULT '' NOT NULL;
+
+\! echo "Updating locationlog_history";
+ALTER TABLE `locationlog_history`
+    ADD `teap_username` varchar(255) DEFAULT '' NOT NULL,
+    ADD `teap_machinename` varchar(255) DEFAULT '' NOT NULL;
+
+\! echo "Updating locationlog_insert_in_history_after_insert";
+DELIMITER /
+DROP TRIGGER IF EXISTS locationlog_insert_in_history_after_insert;
+CREATE TRIGGER locationlog_insert_in_history_after_insert AFTER UPDATE on locationlog
+FOR EACH ROW
+BEGIN
+    IF OLD.session_id <=> NEW.session_id THEN
+        INSERT INTO locationlog_history
+        SET
+            mac = OLD.mac,
+            switch = OLD.switch,
+            port = OLD.port,
+            vlan = OLD.vlan,
+            role = OLD.role,
+            connection_type = OLD.connection_type,
+            connection_sub_type = OLD.connection_sub_type,
+            dot1x_username = OLD.dot1x_username,
+            ssid = OLD.ssid,
+            start_time = OLD.start_time,
+            end_time = CASE
+            WHEN OLD.end_time = '0000-00-00 00:00:00' THEN NOW()
+            WHEN OLD.end_time > NOW() THEN NOW()
+            ELSE OLD.end_time
+            END,
+            switch_ip = OLD.switch_ip,
+            switch_mac = OLD.switch_mac,
+            switch_id = OLD.switch_id,
+            teap_username = OLD.teap_username,
+            teap_machinename = OLD.teap_machinename,
+            stripped_user_name = OLD.stripped_user_name,
+            realm = OLD.realm,
+            session_id = OLD.session_id,
+            ifDesc = OLD.ifDesc,
+            voip = OLD.voip
+        ;
+  END IF;
+END /
+DELIMITER ;
+
 \! echo "Incrementing PacketFence schema version...";
 INSERT IGNORE INTO pf_version (id, version, created_at) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION), NOW());
 
