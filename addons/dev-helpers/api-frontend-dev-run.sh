1204ba2b4a098bc5e271dcef44b569978e9b169e
James Rouzier <jrouzier@inverse.ca>
2025-11-04 19:16:35 +0000

    Kill previously running api-frontend

--- addons/dev-helpers/api-frontend-dev-run.sh
+++ addons/dev-helpers/api-frontend-dev-run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+PIDFILE=/usr/local/pf/var/run/api-frontend-dev.pid
 set -e
 cd /usr/local/pf/go
 systemctl stop packetfence-api-frontend
 make pfhttpd
 
-./pfhttpd run --envfile /usr/local/pf/var/conf/api-frontend.env -a caddyfile --config /usr/local/pf/conf/caddy-services/api.conf
+if [-e "$PIDFILE"];then
+    kill $(cat "$PIDFILE")
+fi
+
+./pfhttpd run --pidfile "$PIDFILE" --envfile /usr/local/pf/var/conf/api-frontend.env -a caddyfile --config /usr/local/pf/conf/caddy-services/api.conf
+rm -f "$PIDFILE"
