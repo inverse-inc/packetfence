ef5f46ec15783faf36084b7e24acbd53ea3fba19
JeGoi <13801368+JeGoi@users.noreply.github.com>
2025-11-10 11:21:34 -0500

    Update PacketFence repository URL in postinst script with variable instead of a fixed version

--- ci/debian-installer/postinst-debian-installer.sh
+++ ci/debian-installer/postinst-debian-installer.sh
@@ -10,7 +10,7 @@ sed -i 's/.*inverse\.ca.*//g' /etc/apt/sources.list
 apt-get update
 apt install gnupg sudo curl
 curl -fsSL https://inverse.ca/downloads/GPG_PUBLIC_KEY | gpg --dearmor -o /etc/apt/keyrings/packetfence.gpg
-echo "deb [signed-by=/etc/apt/keyrings/packetfence.gpg] http://inverse.ca/downloads/PacketFence/debian/14.0 bookworm bookworm" > \
+echo "deb [signed-by=/etc/apt/keyrings/packetfence.gpg] http://inverse.ca/downloads/PacketFence/debian/${PF_VERSION} bookworm bookworm" > \
 	/etc/apt/sources.list.d/packetfence.list
 echo "SET PASSWORD FOR root@'localhost' = PASSWORD('');" > /tmp/reset-root.sql
 mkdir /run/mysqld
