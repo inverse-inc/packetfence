02578f76a51ca24a58907c67def4dfe1ed744235
Durand Fabrice <oeufdure@gmail.com>
2025-11-10 16:33:37 -0500

    Access to the interface name

--- lib/pf/iptables.pm
+++ lib/pf/iptables.pm
@@ -1542,6 +1542,7 @@ sub iptables_fingerbank_collector_rules {
         util_safe_push( "-i $tint -p udp -m udp --dport 4739 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
         # Add rules for interfaces with radius additional daemon
         foreach my $tint ( @radius_ints ) {
+            my $tint = $network->{Tint};
             util_safe_push( "-i $tint -p udp -m udp --dport 1192 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
             util_safe_push( "-i $tint -p udp -m udp --dport 2055 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
             util_safe_push( "-i $tint -p tcp -m tcp --dport 4723 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
