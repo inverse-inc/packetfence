6f025e8229af9313f69afeb885a5e76d16611b9d
JeGoi <13801368+JeGoi@users.noreply.github.com>
2025-11-10 16:22:13 -0500

    Fix copilot recommendation on script for post installation

--- ci/debian-installer/postinst-debian-installer.sh
+++ ci/debian-installer/postinst-debian-installer.sh
@@ -8,13 +8,12 @@ sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd.service
 curl -fsSL https://inverse.ca/downloads/GPG_PUBLIC_KEY | gpg --dearmor -o /etc/apt/keyrings/packetfence.gpg
-echo "deb [signed-by=/etc/apt/keyrings/packetfence.gpg] https://inverse.ca/downloads/PacketFence/debian/${PF_VERSION} bookworm bookworm" > \
+echo "deb [signed-by=/etc/apt/keyrings/packetfence.gpg] http://inverse.ca/downloads/PacketFence/debian/${PF_VERSION} bookworm bookworm" > \
 	/etc/apt/sources.list.d/packetfence.list
 echo "SET PASSWORD FOR root@'localhost' = PASSWORD('');" > /tmp/reset-root.sql
 apt-get update
-apt install packetfence -y
+apt-get install packetfence -y
 mkdir /run/mysqld
 chown mysql: /run/mysqld/
 timeout 10 mysqld --skip-networking --init-file /tmp/reset-root.sql --user=mysql > /var/reset-root.log 2>&1
 rm -f /tmp/reset-root.sql
-#pkill -e docker
