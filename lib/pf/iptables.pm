77d95683e2dc76973eb4cd9d91dfc68700a58d7a
Durand Fabrice <oeufdure@gmail.com>
2025-11-11 06:58:32 -0500

    Fixed syntax

--- lib/pf/iptables.pm
+++ lib/pf/iptables.pm
@@ -1541,7 +1541,7 @@ sub iptables_fingerbank_collector_rules {
         util_safe_push( "-i $tint -p udp -m udp --dport 6343 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
         util_safe_push( "-i $tint -p udp -m udp --dport 4739 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
         # Add rules for interfaces with radius additional daemon
-        foreach my $tint ( @radius_ints ) {
+        foreach my $network ( @radius_ints ) {
             my $tint = $network->{Tint};
             util_safe_push( "-i $tint -p udp -m udp --dport 1192 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
             util_safe_push( "-i $tint -p udp -m udp --dport 2055 --jump ACCEPT", $chains->{'filter'}{'INPUT'} );
