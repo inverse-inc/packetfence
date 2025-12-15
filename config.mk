c9fd6950c7c1c926ce42890f3a3a75011ccd170b
James Rouzier <jrouzier@inverse.ca>
2025-12-15 14:47:11 +0000

    Update go version

--- config.mk
+++ config.mk
@@ -52,7 +52,7 @@ LOCAL_REGISTRY = packetfence
 #
 # Golang
 #
-GOVERSION = go1.24.9
+GOVERSION = go1.25.5
 PF_BINARIES = pfhttpd pfqueue-go pfdhcp pfdns pfstats pfdetect galera-autofix pfacct pfcron mysql-probe pfconnector sdnotify-proxy
 PF_GO_CMDS = pfcrypt pfkafka
 
