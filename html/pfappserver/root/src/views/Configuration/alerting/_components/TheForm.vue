26dc096ff708a8613669609b097b3f7188f6c871
dsatkunas <dsatkuna@akamai.com>
2026-01-08 19:02:47 +0000

    change form input text

--- html/pfappserver/root/src/views/Configuration/alerting/_components/TheForm.vue
+++ html/pfappserver/root/src/views/Configuration/alerting/_components/TheForm.vue
@@ -11,13 +11,13 @@
     />
 
     <form-group-from-addr namespace="fromaddr"
-                          :column-label="$i18n.t('Sender')"
+                          :column-label="$i18n.t('Sender address')"
                           :text="$i18n.t('Email address from which notifications of rogue DHCP servers, violations with an action of email, or any other PacketFence-related message are sent. Empty means root@<server-domain-name>.')"
     />
 
     <form-group-smtp-name namespace="smtp_name"
-                          :column-label="$i18n.t('SMTP name')"
-                          :text="$i18n.t('A descriptive name for the SMTP server connection. This can be used to identify the SMTP server in logs and notifications.')"
+                          :column-label="$i18n.t('Sender name')"
+                          :text="$i18n.t('A descriptive name used in the from address to distinguish between different instances using the same address.')"
     />
 
     <form-group-smtp-server namespace="smtpserver"
