c8df195189cdc49070d20f13bfb10664d241ce06
dsatkunas <dsatkuna@akamai.com>
2026-01-26 19:59:24 +0000

    fix store init

--- html/pfappserver/root/src/views/Configuration/discoverNetworkDevices/_router.js
+++ html/pfappserver/root/src/views/Configuration/discoverNetworkDevices/_router.js
@@ -2,6 +2,7 @@ import acl from '@/utils/acl'
 import store from '@/store'
 import StoreModule from './_store'
 import SwitchesStoreModule from '../switches/_store'
+import SwitchGroupsStoreModule from '../switchGroups/_store'
 
 const TheTabs = () => import(/* webpackChunkName: "Configuration" */ '../_components/TheTabsNetworkDevices')
 
@@ -16,6 +17,8 @@ export const beforeEnter = (to, from, next = () => {}) => {
     store.registerModule('$_discover_network_devices', StoreModule)
   if (!store.state.$_switches)
     store.registerModule('$_switches', SwitchesStoreModule)
+  if (!store.state.$_switch_groups)
+    store.registerModule('$_switch_groups', SwitchGroupsStoreModule)
   next()
 }
 
