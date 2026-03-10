b4ac4a3d97807883dc8394248ee8bd5fc22007b8
takrasiu-inv <takrasiu@akamai.com>
2026-03-10 18:20:52 +0200

    Update Font for PDF generation to 'Instrument Sans'.

--- docs/asciidoctor-html.rb
+++ docs/asciidoctor-html.rb
@@ -634,7 +634,7 @@ class PfHtml5Converter < (Asciidoctor::Converter.for 'html5')
     target = node.attr 'target'
     width_attr = (node.attr? 'width') ? %( width="#{node.attr 'width'}") : ''
     height_attr = (node.attr? 'height') ? %( height="#{node.attr 'height'}") : ''
-    if ((node.attr? 'format', 'svg') || (target.include? '.svg')) && node.document.safe < SafeMode::SECURE &&
+    if ((node.attr? 'format', 'svg') || (target.include? '.svg')) && node.document.safe < Asciidoctor::SafeMode::SECURE &&
         ((svg = (node.option? 'inline')) || (obj = (node.option? 'interactive')))
       if svg
         img = (read_svg_contents node, target) || %(<span class="alt">#{node.alt}</span>)
