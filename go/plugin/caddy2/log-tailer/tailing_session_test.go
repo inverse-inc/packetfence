7deabf157738abbafb78b742fe6a07dde83e1577
James Rouzier <jrouzier@inverse.ca>
2026-06-11 15:51:52 +0000

    Make the test compatible with debian

--- go/plugin/caddy2/log-tailer/tailing_session_test.go
+++ go/plugin/caddy2/log-tailer/tailing_session_test.go
@@ -1,10 +1,12 @@
 package logtailer
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 	"testing"
 	"time"
 
@@ -12,13 +14,44 @@ import (
 	"github.com/jcuga/golongpoll"
 )
 
+func IsDebian() bool {
+	file, err := os.Open("/etc/os-release")
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Look for ID=debian or ID_LIKE=...debian...
+		if strings.HasPrefix(line, "ID=") || strings.HasPrefix(line, "ID_LIKE=") {
+			parts := strings.SplitN(line, "=", 2)
+			if len(parts) == 2 {
+				// Clean up any quotes
+				val := strings.Trim(parts[1], "\"")
+				if strings.Contains(val, "debian") {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
 func TestTailingSession(t *testing.T) {
 	eventsManager, err := golongpoll.StartLongpoll(golongpoll.Options{
 		LoggingEnabled: (sharedutils.EnvOrDefault("LOG_LEVEL", "") == "debug"),
 	})
 	sharedutils.CheckError(err)
 
-	files := []string{"/usr/local/pf/logs/packetfence.log", "/var/log/messages"}
+	var files []string
+	if IsDebian() {
+		files = []string{"/usr/local/pf/logs/packetfence.log", "/var/log/syslog"}
+	} else {
+		files = []string{"/usr/local/pf/logs/packetfence.log", "/var/log/messages"}
+	}
 
 	ts := NewTailingSession(files, regexp.MustCompile(`.*`))
 	ts.Start("test", eventsManager)
